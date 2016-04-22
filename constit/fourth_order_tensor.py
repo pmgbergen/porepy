4f6f06715f6dd98f6c42d60913bae8fde42302d4
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2016-04-22 09:24:31 +0200

    Added documentation to 4th order tensor

--- constit/fourth_order_tensor.py
+++ constit/fourth_order_tensor.py
@@ -2,8 +2,37 @@ import numpy as np
 
 
 class FourthOrderTensor(object):
+    """ Cell-wise representation of fourth order tensor.
 
+    For each cell, there are dim^4 degrees of freedom, stored in a
+    dim^2 * dim^2 matrix (exactly how to convert between 2D and 4D matrix
+    is not clear to me a the moment, but in pratcise there is sufficient
+    symmetry in the tensors for the question to be irrelevant).
+
+    The tensor is thus stored in a (dim^2,dim^2,nc) matrix, named c.
+
+    The only constructor available for the moment is based on the lame parameters,
+    e.g. using two degrees of freedom. A third parameter phi is also present,
+    but this has never been used.
+
+    Primary usage for the class is for mpsa discretizations. Other applications
+    have not been tested.
+
+    Attributes:
+        c - numpy.ndarray, dimensions (dim^2,dim^2,nc), cell-wise representation of
+
+    """
     def __init__(self, dim, mu, lmbda, phi=None):
+        """ Constructor for fourth order tensor on Lame-parameter form
+
+        Parameters
+        ----------
+        dim (int) dimension, should be 2 or 3
+        mu (numpy.ndarray), First lame parameter, 1-D, one value per cell
+        lmbda (numpy.ndarray), Second lame parameter, 1-D, one value per cell
+        phi (Optional numpy.ndarray), 1-D one value per cell, never been used.
+
+        """
 
         # Check arguments
         if dim > 3 or dim < 2:
@@ -21,7 +50,7 @@ class FourthOrderTensor(object):
             raise ValueError("Mu and lmbda should have the same length")
 
         if phi is None:
-            phi = 0 * mu
+            phi = 0 * mu  # Default value for phi is zero
         elif not isinstance(phi, np.ndarray):
             raise ValueError("Phi should be a numpy array")
         elif not phi.ndim == 1:
@@ -29,10 +58,7 @@ class FourthOrderTensor(object):
         elif phi.size != lmbda.size:
             raise ValueError("Phi and Lmbda should have the same length")
 
-        nc = mu.size
-        d2 = dim * dim
-        stiffness = np.zeros((d2, d2, nc))
-
+        # Basis for the contributions of mu, lmbda and phi is hard-coded
         if dim == 2:
             mu_mat = np.array([[2, 0, 0, 0],
                                [0, 1, 1, 0],
@@ -47,7 +73,7 @@ class FourthOrderTensor(object):
                                [1, 0, 0, 1],
                                [0, 1, 1, 0]])
 
-        elif dim == 3:
+        else:  # Dimension is either 2 or 3
             mu_mat = np.array([[2, 0, 0, 0, 0, 0, 0, 0, 0],
                                [0, 1, 0, 1, 0, 0, 0, 0, 0],
                                [0, 0, 1, 0, 0, 0, 1, 0, 0],
@@ -78,7 +104,7 @@ class FourthOrderTensor(object):
                                 [1, 0, 0, 0, 1, 0, 0, 0, 1],
                                 [0, 1, 1, 1, 0, 1, 1, 1, 0]
                                 ])
-
+        # Expand dimensions to prepare for cell-wise representation
         mu_mat = mu_mat[:, :, np.newaxis]
         lmbda_mat = lmbda_mat[:, :, np.newaxis]
         phi_mat = phi_mat[:, :, np.newaxis]
