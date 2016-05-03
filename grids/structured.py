646dd762883144b9d2e4ae78ca3e6738d4f3c816
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2016-05-03 09:21:06 +0200

    First implementation of Cartesian 3D grids

--- grids/structured.py
+++ grids/structured.py
@@ -21,7 +21,7 @@ class TensorGrid(Grid):
         if z is None:
             self._create_2d_grid(x, y)
         else:
-            raise NotImplementedError('Only 2D supported for now')
+            self._create_3d_grid(x, y, z)
 
     def _create_2d_grid(self, x, y):
         self.dim = 2
@@ -84,6 +84,99 @@ class TensorGrid(Grid):
         self.cellFaces = sps.csc_matrix((data, cell_faces, indptr),
                                         shape=(num_faces, num_cells))
 
+    def _create_3d_grid(self, x, y, z):
+        self.dim = 3
+
+        sx = x.size - 1
+        sy = y.size - 1
+        sz = z.size - 1
+
+        num_cells = sx * sy * sz
+        num_nodes = (sx + 1) * (sy + 1) * (sz + 1)
+        num_faces_x = (sx + 1) * sy * sz
+        num_faces_y = sx * (sy + 1) * sz
+        num_faces_z = sx * sy * (sz + 1)
+        num_faces = num_faces_x + num_faces_y + num_faces_z
+
+        self.Nc = num_cells
+        self.Nf = num_faces
+        self.Nn = num_nodes
+
+        x_coord, y_coord, z_coord = np.meshgrid(x, y, z)
+        # This rearangement turned out to work. Not the first thing I tried..
+        x_coord = np.swapaxes(x_coord, 1, 0).ravel(order='F')
+        y_coord = np.swapaxes(y_coord, 1, 0).ravel(order='F')
+        z_coord = np.swapaxes(z_coord, 1, 0).ravel(order='F')
+
+        self.nodes = np.vstack((x_coord, y_coord, z_coord))
+
+        # Face nodes
+        node_array = np.arange(num_nodes).reshape(sx + 1, sy + 1, sz + 1,
+                                                  order='F')
+
+        # Define face-node relations for all x-faces
+        fn1 = node_array[:, :-1, :-1].ravel(order='F')
+        fn2 = node_array[:, 1:, :-1].ravel(order='F')
+        fn3 = node_array[:, 1:, 1:].ravel(order='F')
+        fn4 = node_array[:, :-1, 1:].ravel(order='F')
+        face_nodes_x = np.hstack((fn1, fn2, fn3, fn4))
+
+        # Define face-node relations for all y-faces
+        fn1 = node_array[:-1:, :, :-1].ravel(order='F')
+        fn2 = node_array[:-1, :, 1:].ravel(order='F')
+        fn3 = node_array[1:, :, 1:].ravel(order='F')
+        fn4 = node_array[1:, :, :-1].ravel(order='F')
+        face_nodes_y = np.hstack((fn1, fn2, fn3, fn4))
+
+        # Define face-node relations for all y-faces
+        fn1 = node_array[:-1:, :-1, :].ravel(order='F')
+        fn2 = node_array[1:, :-1, :].ravel(order='F')
+        fn3 = node_array[1:, 1:, :].ravel(order='F')
+        fn4 = node_array[:-1, 1:, :].ravel(order='F')
+        face_nodes_z = np.hstack((fn1, fn2, fn3, fn4))
+
+        # Test
+        assert face_nodes_x.size == face_nodes_y.size
+        assert face_nodes_x.size == face_nodes_z.size
+
+        num_nodes_per_face = 4
+        indptr = np.append(np.arange(0, num_nodes_per_face * num_faces,
+                                     num_nodes_per_face),
+                           num_nodes_per_face * num_faces)
+        face_nodes = np.hstack((face_nodes_x, face_nodes_y, face_nodes_z))
+        data = np.ones(face_nodes.shape, dtype=bool)
+        self.faceNodes = sps.csc_matrix((data, face_nodes, indptr),
+                                        shape=(num_nodes, num_faces))
+
+        # Cell faces
+        face_x = np.arange(num_faces_x).reshape(sx + 1, sy, sz, order='F')
+        face_y = num_faces_x + np.arange(num_faces_y).reshape(sx, sy + 1,
+                                                              sx, order='F')
+        face_z = num_faces_x + num_faces_y + \
+                 np.arange(num_faces_y).reshape(sx, sy + 1, sx, order='F')
+
+        face_west = face_x[:-1, :, :].ravel(order='F')
+        face_east = face_x[1:, :, :].ravel(order='F')
+        face_south = face_y[:, :-1, :].ravel(order='F')
+        face_north = face_y[:, 1:, :].ravel(order='F')
+        face_top = face_z[:, :, :-1].ravel(order='F')
+        face_bottom = face_z[:, :, 1:].ravel(order='F')
+
+        cell_faces = np.hstack((face_west, face_east,
+                                face_south, face_north, face_top,
+                                face_bottom))
+
+        num_faces_per_cell = 6
+        indptr = np.append(np.arange(0, num_faces_per_cell * num_cells,
+                                     num_faces_per_cell),
+                           num_faces_per_cell * num_cells)
+        data = np.vstack((-np.ones(num_cells), np.ones(num_cells),
+                          -np.ones(num_cells), np.ones(num_cells),
+                          -np.ones(num_cells), np.ones(num_cells))
+                         ).ravel(order='F')
+        self.cellFaces = sps.csc_matrix((data, cell_faces, indptr),
+                                        shape=(num_faces, num_cells))
+
 
 class CartGrid(TensorGrid):
     def __init__(self, nx, physdims=None):
@@ -95,13 +188,16 @@ class CartGrid(TensorGrid):
 
         dims = nx.shape
         if dims[0] == 1:
-            raise NotImplementedError('only 2D supported for now')
+            raise NotImplementedError('only 2D and 3D supported for now')
         elif dims[0] == 2:
             x = np.linspace(0, physdims[0], nx[0]+1)
             y = np.linspace(0, physdims[1], nx[1]+1)
             super(self.__class__, self).__init__(x, y)
         elif dims[0] == 3:
-            raise NotImplementedError('only 2D supported for now')
+            x = np.linspace(0, physdims[0], nx[0] + 1)
+            y = np.linspace(0, physdims[1], nx[1] + 1)
+            z = np.linspace(0, physdims[2], nx[2] + 1)
+            super(self.__class__, self).__init__(x, y, z)
         else:
             raise ValueError('Cartesian grid only implemented for up to three \
             dimensions')
