dedd482cb917347257888893d53d3e33d6fba9a4
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2016-05-12 14:22:58 +0200

    Added class for structured triangular grids (quads split into two)

--- grids/simplex.py
+++ grids/simplex.py
@@ -105,3 +105,54 @@ class TriangleGrid(Grid):
         return row[scol].reshape(self.num_cells, 3)
 
 
+class StructuredTriangleGrid(TriangleGrid):
+
+    def __init__(self, nx, physdims=None):
+        """
+        Construct a triangular grid by splitting Cartesian cells in two.
+
+        Examples:
+        Grid on the unit cube
+        >>> nx = np.array([2, 3])
+        >>> physdims = np.ones(2)
+        >>> g = simplex.StructuredTriangleGrid(nx, physdims)
+
+        Parameters
+        ----------
+        nx (np.ndarray, size 2): number of cells in each direction of
+        underlying Cartesian grid
+        physdims (np.ndarray, size 2): domain size. Defaults to nx,
+        thus Cartesian cells are unit squares
+        """
+        if physdims is None:
+            physdims = nx
+        x = np.linspace(0, physdims[0], nx[0] + 1)
+        y = np.linspace(0, physdims[1], nx[1] + 1)
+
+        # Node coordinates
+        x_coord, y_coord = np.meshgrid(x, y)
+        p = np.vstack((x_coord.ravel(order='C'), y_coord.ravel(order='C')))
+
+        # Define nodes of the first row of cells.
+        tmp_ind = np.arange(0, nx[0])
+        i1 = tmp_ind  # Lower left node in quad
+        i2 = tmp_ind + 1  # Lower right node
+        i3 = nx[0] + 2 + tmp_ind  # Upper left node
+        i4 = nx[0] + 1 + tmp_ind  # Upper right node
+
+        # The first triangle is defined by (i1, i2, i3), the next by
+        # (i1, i3, i4). Stack these vertically, and reshape so that the
+        # first quad is split into cells 0 and 1 and so on
+        tri_base = np.vstack((i1, i2, i3, i1, i3, i4)).reshape((3, -1),
+                                                               order='F')
+        # Initialize array of triangles. For the moment, we will append the
+        # cells here, but we do know how many cells there are in advance,
+        # so pre-allocation is possible if this turns out to be a bottleneck
+        tri = tri_base
+
+        # Loop over all remaining rows in the y-direction.
+        for iter1 in range(nx[1].astype('int64') - 1):
+            # The node numbers are increased by nx[0] + 1 for each row
+            tri = np.hstack((tri, tri_base + (iter1 + 1) * (nx[0] + 1)))
+
+        super(self.__class__, self).__init__(p, tri)
