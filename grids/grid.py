260505439315f6d52fdbe962d78345a553759340
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2016-05-19 21:17:00 +0200

    Method to obtain boundary faces of a grid

--- grids/grid.py
+++ grids/grid.py
@@ -359,3 +359,7 @@ class Grid(object):
 
     def num_cell_nodes(self):
         return self.cell_nodes().sum(axis=0).A.ravel(1)
+
+    def get_boundary_faces(self):
+        return np.argwhere(np.abs(self.cell_faces).sum(axis=1).A.ravel(1)
+                           == 1).ravel(1)
