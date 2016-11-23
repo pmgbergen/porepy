715942111f3f7463b27f72254454fc44a84759d4
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2016-11-23 10:08:49 +0100

    Separate file for GMSH-related constants

---
+++ constants.py
@@ -0,0 +1,15 @@
+class GmshConstants(object):
+    """
+    This class is a container for storing constant values that are used in
+    the meshing algorithm. The intention is to make them available to all
+    functions and modules.
+
+    This may not be the most pythonic way of doing this, but it works.
+    """
+    def __init__(self):
+        self.DOMAIN_BOUNDARY_TAG = 1
+        self.COMPARTMENT_BOUNDARY_TAG = 2
+        self.FRACTURE_TAG = 3
+
+        self.PHYSICAL_NAME_DOMAIN = 'DOMAIN'
+        self.PHYSICAL_NAME_FRACTURES = 'FRACTURE_'
