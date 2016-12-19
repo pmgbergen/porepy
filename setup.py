8ed9b8cf6b5d3dfc3e809460c7a98dddd0c4f6ac
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2016-12-19 09:22:30 +0100

    Modified setup for cython

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+import numpy
 
 setup(
         cmdclass={'build_ext': build_ext},
         ext_modules=[Extension("cythoninvert",
-                               ["invert_diagonal_blocks.pyx"])]
+                               ["invert_diagonal_blocks.pyx"],
+                               include_dirs=[numpy.get_include()])]
      )
