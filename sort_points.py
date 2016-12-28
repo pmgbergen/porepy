4ddaabb17d4d3e1550d96f2ec9d6d46cecff1ad8
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2016-12-28 19:57:11 +0100

    possible resolution

--- sort_points.py
+++ sort_points.py
@@ -2,13 +2,13 @@ import numpy as np
 
 def sort_point_pairs(lines, check_circular=True):
     """ Sort pairs of numbers to form a chain.
-    
-    The target application is to sort lines, defined by their 
+
+    The target application is to sort lines, defined by their
     start end endpoints, so that they form a continuous polyline.
-    
-    The algorithm is brute-force, using a double for-loop. This can 
+
+    The algorithm is brute-force, using a double for-loop. This can
     surely be imporved.
-    
+
     Parameters:
     lines: np.ndarray, 2xn, the line pairs.
     check_circular: Verify that the sorted polyline form a circle.
@@ -18,24 +18,24 @@ def sort_point_pairs(lines, check_circular=True):
     sorted_lines: np.ndarray, 2xn, sorted line pairs.
 
     """
-    
+
     num_lines = lines.shape[1]
     sorted_lines = -np.ones((2, num_lines))
-    
+
     # Start with the first line in input
     sorted_lines[:, 0] = lines[:, 0]
-    
+
     # The starting point for the next line
     prev = sorted_lines[1, 0]
-    
+
     # Keep track of which lines have been found, which are still candidates
-    found = np.zeros(num_lines, dtype='bool')
+    found = np.zeros(num_lines, dtype=np.bool)
     found[0] = True
-    
-    # The sorting algorithm: Loop over all places in sorted_line to be filled, 
+
+    # The sorting algorithm: Loop over all places in sorted_line to be filled,
     # for each of these, loop over all members in lines, check if the line is still
     # a candidate, and if one of its points equals the current starting point.
-    # More efficient and more elegant approaches can surely be found, but this 
+    # More efficient and more elegant approaches can surely be found, but this
     # will do for now.
     for i in range(1, num_lines):  # The first line has already been found
         for j in range(0, num_lines):
@@ -47,7 +47,7 @@ def sort_point_pairs(lines, check_circular=True):
             elif not found[j] and lines[1, j] == prev:
                 sorted_lines[:, i] = lines[::-1, j]
                 found[j] = True
-                prev = lines[1, j]
+                prev = lines[0, j]
                 break
     # By now, we should have used all lines
     assert(np.all(found))
