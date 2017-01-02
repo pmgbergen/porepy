5474484c9598b66b87d98f54d5ea552155d94954
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2017-01-02 20:22:35 +0100

    Removed old __main__ mode from mpfa

--- mpfa.py
+++ mpfa.py
@@ -323,15 +323,3 @@ def _create_bound_rhs(bnd, bound_exclusion,
     rhs_bound = -sps.vstack([neu_cell, dir_cell]) * bnd_2_all_hf * hf_2_f
     return rhs_bound
 
-
-if __name__ == '__main__':
-    # Method used for debuging
-    nx = np.array([330, 330])
-    g = structured.CartGrid(nx)
-    g.compute_geometry()
-
-    kxx = np.ones(g.num_cells)
-    perm = second_order_tensor.SecondOrderTensor(g.dim, kxx)
-
-    bound = bc.BoundaryCondition(g)
-    mpfa(g, perm, bound)
