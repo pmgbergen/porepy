521241ba6e6a1a0e4641b69a184c9351df45309f
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2017-01-04 10:50:39 +0100

    update the 2d plotting to be coherent with 3d and which works for manifolds

--- plot_grid.py
+++ plot_grid.py
@@ -23,41 +23,26 @@ from compgeom import sort_points
 
 def plot_grid(g, info = None, show=True):
 
-#    if isinstance(g, simplex.TriangleGrid):
-#        figId, ax = plot_tri_grid(g)
-#    elif isinstance(g, structured.TensorGrid) and g.dim == 2:
-#        figId, ax = plot_cart_grid_2d(g)
-#    elif g.dim == 2:
-    if g.dim == 2:
-        figId, ax = plot_grid_2d(g)
-    elif g.dim == 3:
-        figId, ax = plot_grid_3d(g)
-    else:
-        raise NotImplementedError('Under construction')
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
-    if info is not None: add_info( g, info, figId, ax )
+    if g.dim == 2:   plot_grid_2d(g, ax)
+    elif g.dim == 3: plot_grid_3d(g, ax)
+    else:            raise NotImplementedError('Under construction')
+
+    x = [ np.amin(g.nodes[0,:]), np.amax(g.nodes[0,:]) ]
+    y = [ np.amin(g.nodes[1,:]), np.amax(g.nodes[1,:]) ]
+    z = [ np.amin(g.nodes[2,:]), np.amax(g.nodes[2,:]) ]
+
+    if x[0] != x[1]: ax.set_xlim( x ); ax.set_xlabel('x')
+    if y[0] != y[1]: ax.set_ylim( y ); ax.set_ylabel('y')
+    if z[0] != z[1]: ax.set_zlim( z ); ax.set_zlabel('z')
+    ax.set_title( g.name )
+
+    if info is not None: add_info( g, info, ax )
     if show: plt.show()
 
-    return figId
-
-    fig = plt.figure()
-
-
-    # In each direction there is one more node than cell
-    node_dims = g.cart_dims + 1
-
-    x = g.nodes[0].reshape(node_dims)
-    y = g.nodes[1].reshape(node_dims)
-
-    # pcolormesh needs colors for its cells. Let the value be one
-    z = np.ones(x.shape)
-
-    fig = plt.figure()
-    #ax = fig.add_axes([0.1, 0.1, 0.8, 0.8])
-    ax = fig.add_subplot(111)
-    # It would have been better to have a color map which makes all
-    ax.pcolormesh(x, y, z, edgecolors='k', cmap='gray', alpha=0.5)
-    return fig.number, ax
+    return fig.number
 
 #------------------------------------------------------------------------------#
 
@@ -77,57 +62,46 @@ def plot_grid_fractures(g, show=True):
 
 #------------------------------------------------------------------------------#
 
-def add_info( g, info, figId, ax ):
+def add_info( g, info, ax ):
 
-    def mask_index( p ): return p[0:g.dim]
     def disp( i, p, c, m ): ax.scatter( *p, c=c, marker=m ); ax.text( *p, i )
     def disp_loop( v, c, m ): [ disp( i, ic, c, m ) for i, ic in enumerate( v.T ) ]
 
-    fig = plt.figure( figId )
+#    fig = plt.figure( figId )
     info = info.upper()
 
-    if "C" in info: disp_loop( mask_index( g.cell_centers ), 'r', 'o' )
-    if "N" in info: disp_loop( mask_index( g.nodes ), 'b', 's' )
-    if "F" in info: disp_loop( mask_index( g.face_centers ), 'y', 'd' )
+    if "C" in info: disp_loop( g.cell_centers, 'r', 'o' )
+    if "N" in info: disp_loop( g.nodes, 'b', 's' )
+    if "F" in info: disp_loop( g.face_centers, 'y', 'd' )
 
     if "O" in info.upper():
-        normals = 0.1*np.array( [ mask_index(n)/np.linalg.norm(n) \
+        normals = 0.1*np.array( [ n/np.linalg.norm(n) \
                                   for n in g.face_normals.T ] ).T
-        if g.dim == 2:
-            [ ax.quiver( *mask_index( g.face_centers[:,f] ), \
-                         *normals[:,f], color = 'k', scale=1 )
-                                            for f in np.arange( g.num_faces ) ]
-        elif g.dim == 3:
-            [ ax.quiver( *mask_index( g.face_centers[:,f] ), \
-                         *normals[:,f], color = 'k', length=0.25 )
-                                            for f in np.arange( g.num_faces ) ]
+        [ ax.quiver( *g.face_centers[:,f], *normals[:,f], color = 'k', \
+          length=0.25 ) for f in np.arange( g.num_faces ) ]
 
 #------------------------------------------------------------------------------#
 
-def plot_grid_2d( g ):
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
+def plot_grid_2d( g, ax ):
 
     nodes, cells, _  = sps.find( g.cell_nodes() )
-    polygons = []
     for c in np.arange( g.num_cells ):
-        mask = np.where( cells == c )
-        cell_nodes = g.nodes[:, nodes[mask]]
-        index = sort_points.sort_point_plane( cell_nodes, g.cell_centers[:,c] )
-        polygons.append( Polygon( cell_nodes[0:2,index].T, True ) )
+        ptsId = nodes[ cells == c ]
+        mask = sort_points.sort_point_plane( g.nodes[:, ptsId], \
+                                             g.cell_centers[:, c] )
 
-    p = PatchCollection( polygons, cmap=matplotlib.cm.jet, alpha=0.4 )
-    p.set_array( np.zeros( len( polygons ) ) )
-    ax.add_collection(p)
+        pts = g.nodes[:, ptsId[mask]]
+        poly = Poly3DCollection( [pts.T] )
+        poly.set_edgecolor('k')
+        poly.set_facecolors('r')
+        poly.set_alpha(0.5)
+        ax.add_collection3d(poly)
 
-    return fig.number, ax
+    ax.view_init(90, 0)
 
 #------------------------------------------------------------------------------#
 
-def plot_grid_3d( g ):
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
+def plot_grid_3d( g, ax ):
 
     faces_cells, cells, _ = sps.find( g.cell_faces )
     nodes_faces, faces, _ = sps.find( g.face_nodes )
@@ -145,7 +119,5 @@ def plot_grid_3d( g ):
             poly.set_facecolors('r')
             poly.set_alpha(0.5)
             ax.add_collection3d(poly)
-
-    return fig.number, ax
 
 #------------------------------------------------------------------------------#
