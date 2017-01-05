e01aa81eb16940d8ff3c7cab4c7cb628a34ac99e
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2017-01-05 16:37:49 +0100

    Ad hoc fix of 2D/3D problem for mpsa for Biot

--- mpsa.py
+++ mpsa.py
@@ -10,6 +10,7 @@ from core.bc import bc
 
 def mpsa(g, constit, bound, faces=None, eta=0, inverter='numba'):
 
+ 
     if g.dim == 2:
         g = g.copy()
         g.cell_centers = np.delete(g.cell_centers, (2), axis=0)
@@ -19,7 +20,6 @@ def mpsa(g, constit, bound, faces=None, eta=0, inverter='numba'):
 
         constit.c = np.delete(constit.c, (2, 5, 6, 7, 8), axis=0)
         constit.c = np.delete(constit.c, (2, 5, 6, 7, 8), axis=1)
- 
     nd = g.dim
 
     # Define subcell topology
@@ -98,6 +98,15 @@ def __mpsa_elasticity(g, constit, subcell_topology, bound_exclusion, eta,
 
 
 def biot(g, constit, bound, faces=None, eta=0, inverter='numba'):
+    if g.dim == 2:
+        g = g.copy()
+        g.cell_centers = np.delete(g.cell_centers, (2), axis=0)
+        g.face_centers = np.delete(g.face_centers, (2), axis=0)
+        g.face_normals = np.delete(g.face_normals, (2), axis=0)
+        g.nodes = np.delete(g.nodes, (2), axis=0)
+
+        constit.c = np.delete(constit.c, (2, 5, 6, 7, 8), axis=0)
+        constit.c = np.delete(constit.c, (2, 5, 6, 7, 8), axis=1)
     nd = g.dim
 
     # Define subcell topology
