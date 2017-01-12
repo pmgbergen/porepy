4078cda6a66d18ceaec9859ee7b42139488d51e6
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2017-01-12 10:25:59 +0100

    Partition metis use new function for cell2cell connctions

--- grids/partition.py
+++ grids/partition.py
@@ -22,8 +22,8 @@ def partition_metis(g, num_part):
         pip install pymetis
 
     This will install metis itself in addition to the python bindings. There
-    are other python bindings for metis as well, but pymetis has done the job
-    until now.
+    are other python bindings for metis as well, but pymetis has behaved well
+    so far.
 
     Parameters:
         g: core.grids.grid: To be partitioned. Only the cell_face attribute is
@@ -36,16 +36,8 @@ def partition_metis(g, num_part):
 
     """
 
-    # Create a copy of the cell-face relation, so that we can modify it at will
-    cell_face = g.cell_faces.copy()
-
-    # Direction of normal vector does not matter here, only 0s and 1s
-    cell_faces.data = np.abs(cell_faces.data)
-
-    # Find connection between cells via the cell-face map
-    c2c = cell_faces.transpose() * cell_faces
-    # Only care about absolute values
-    c2c.data = np.clip(c2c.data, 0, 1)
+    # Connection map between cells
+    c2c = g.cell_connection_map()
 
     # Convert the cells into the format required by pymetis
     adjacency_list = [c2c.getrow(i).indices for i in range(c2c.shape[0])]
