3fc6223d50c4fc22cb152be5c2d875a021c9fca8
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2017-01-24 17:25:11 +0100

    add the possibility to reconstruct the velocity as P0 projection in each cell

--- dual.py
+++ dual.py
@@ -92,3 +92,57 @@ def extract(up, g):
     return up[:g.num_faces], up[g.num_faces:]
 
 #------------------------------------------------------------------------------#
+
+def projectU(u, g, k):
+    """  Discretize the second order elliptic equation using dual virtual
+    element method.
+
+    Args:
+        g (grid): Grid, or a subclass, with geometry fields computed.
+        k (second_order_tensor): Permeability. Cell-wise.
+    """
+    faces, cells, sgn = sps.find(g.cell_faces)
+    tol = 1e-10
+
+    # Normal vectors, permeability, and diameter for each face of cell
+    diams = g.cell_diameters()
+
+    P0u = np.zeros((3,g.num_cells))
+    idx = 0
+
+    for c in np.arange(g.num_cells):
+        loc = slice(g.cell_faces.indptr[c], g.cell_faces.indptr[c+1])
+
+        c_center = g.cell_centers[:,c]
+        mono = np.array([lambda pt,i=i: (pt[i] - c_center[i])/diams[c] \
+                                                     for i in np.arange(g.dim)])
+        grad = np.eye(g.dim)/diams[c]
+
+        K = k.perm[0:g.dim, 0:g.dim, c]
+        normals = g.face_normals[0:g.dim, faces[loc]]
+
+        # local matrix D
+        D = np.array([np.dot(normals.T, np.dot(K, g)) for g in grad]).T
+
+        # local matrix G
+        G = np.dot(grad, np.dot(K, grad.T))*g.cell_volumes[c]
+
+        # local matrix F
+        faces_loc = faces[loc]
+        sgn_loc = sgn[loc]
+        F = np.array([ s*m( g.face_centers[:,f] ) for m in mono \
+                        for s,f in zip(sgn_loc,faces_loc)] ).reshape((g.dim,-1))
+
+        assert np.all( np.abs( G - np.dot(F,D) ) < tol )
+
+        # local matrix Pi
+        Pi_s = np.dot(np.linalg.inv(G), F)
+
+        # extract the velocity for the current cell
+        P0u[:g.dim,c] = np.dot(Pi_s, u[faces_loc]) / diams[c]
+#        u_full = I_rotation * [ u_tang; 0 ];
+#        O_Pu_h( cellId, : ) = u_full';
+
+    return P0u
+
+#------------------------------------------------------------------------------#
