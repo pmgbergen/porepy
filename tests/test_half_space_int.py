c4ddc6d6bc983cf2db546cdd100b64314863a9f7
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2017-02-03 16:07:02 +0100

    use the standard unit test procedure

--- tests/test_half_space_int.py
+++ tests/test_half_space_int.py
@@ -3,24 +3,27 @@ import numpy as np
 
 from utils import half_space_int
 
+#------------------------------------------------------------------------------#
 
-def test_one_half_space():
+class BasicsTest( unittest.TestCase ):
 
-    n  = np.array([[-1],[0],[0]])
-    x0 = np.array([[0],[0],[0]])
-    pts = np.array([[1,-1],[0,0],[0,0]])
-    out = half_space_int.half_space_int(n,x0,pts)
-    assert np.all(out == np.array([True,False]))
+#------------------------------------------------------------------------------#
 
-def test_two_half_spaces():
-    n  = np.array([[-1,0],[0,-1],[0,0]])
-    x0 = np.array([[0,0],[0,1],[0,0]])
-    pts = np.array([[1,-1,1,0],[2,0,2,0],[0,0,0,0]])
-    out = half_space_int.half_space_int(n,x0,pts)
-    assert np.all(out == np.array([True,False,True,False]))
+    def test_one_half_space(self):
 
+        n  = np.array([[-1],[0],[0]])
+        x0 = np.array([[0],[0],[0]])
+        pts = np.array([[1,-1],[0,0],[0,0]])
+        out = half_space_int.half_space_int(n,x0,pts)
+        assert np.all(out == np.array([True,False]))
 
-if __name__ == '__main__':
-    test_one_half_space()
-    test_two_half_spaces()
+#------------------------------------------------------------------------------#
 
+    def test_two_half_spaces(self):
+        n  = np.array([[-1,0],[0,-1],[0,0]])
+        x0 = np.array([[0,0],[0,1],[0,0]])
+        pts = np.array([[1,-1,1,0],[2,0,2,0],[0,0,0,0]])
+        out = half_space_int.half_space_int(n,x0,pts)
+        assert np.all(out == np.array([True,False,True,False]))
+
+#------------------------------------------------------------------------------#
