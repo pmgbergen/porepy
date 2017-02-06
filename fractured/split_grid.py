90dfd62dcad167ebc93e3990707aa3051436bb2a
Runar <runar.berge@uib.no>
2017-02-06 17:53:31 +0100

    New Functionality: split_grid now handles intersection of fractures
    The function names are still in beta, and should be changed in a later commit.
    
    Added a new class Fractures which contains the information needed to split
    the grid. This is only a TEMPORARY class until the mother class is developed
    completely.

--- fractured/split_grid.py
+++ fractured/split_grid.py
@@ -2,7 +2,11 @@ import numpy as np
 from scipy import sparse as sps
 from utils.half_space import half_space_int
 
-def split_grid(g, frac_tag):
+import numpy as np
+from scipy import sparse as sps
+from utils.half_space import half_space_int
+
+def split_fracture(g, frac_tag,tip_nodes_id=None,offset=0):
     """
     Split faces and nodes to create an internal boundary
     
@@ -29,7 +33,7 @@ def split_grid(g, frac_tag):
     >>> from core.grids import structured
     >>> from viz import plot_grid
     >>> import matplotlib.pyplot as plt
-    >>> from gridding.fractured.split_grid import split_grid 
+    >>> from gridding.fractured.split_grid import split_fracture
     >>> # Set up a Cartesian grid
     >>> n = 10
     >>> g = structured.CartGrid([n, n])
@@ -37,26 +41,25 @@ def split_grid(g, frac_tag):
     >>> # Define fracture
     >>> frac_tag = np.logical_and(np.logical_and(g.face_centers[1,:]==n/2, 
     >>>            g.face_centers[0,:]>n/4), g.face_centers[0,:]<3*n/4)
-    >>> g = split_grid(g, frac_tag)
+    >>> g = split_fracture(g, frac_tag)
     >>> frac_nodes = np.arange((np.sum(frac_tag)-1))+g.num_nodes -(np.sum(frac_tag)-1)
     >>> g.nodes[1,frac_nodes] -=.5
     >>> plot_grid.plot_grid(g)
     >>> plt.show()
     """
-
     # Create convenient mappings
     frac_tag_id    = np.argwhere(frac_tag)
     frac_nodes     = g.face_nodes[:,frac_tag]
     cell_frac      = g.cell_faces[frac_tag,:]
     node_mult      = np.ravel(np.sum(g.face_nodes[:,frac_tag],axis=1))
-    assert all(node_mult <= 2), "Not yet support for intersecting fractures"
-    tip_nodes      = node_mult==1 # tip nodes
-    int_nodes      = node_mult==2 # internal nodes
-    node_coord     = g.nodes[:,int_nodes]
-
-    # add duplicate nodes at the end of array
-    g.nodes = np.concatenate((g.nodes,node_coord),axis=1)
-    g.num_nodes += node_coord.shape[1]
+    assert all(node_mult <= 2), "Not support for intersecting fractures"
+    if tip_nodes_id == None:
+        tip_nodes_id   = np.ravel(np.argwhere(node_mult==1)) # tip nodes
+        int_nodes      = node_mult==2                        # internal nodes
+    else:
+        int_nodes = node_mult>0          # all internal nodes
+        int_nodes[tip_nodes_id] = False  # Remove given tip nodes
+    int_nodes_id = np.ravel(np.argwhere(int_nodes))
     
     # faces connected to fracture nodes
     face_fracnodes = g.face_nodes[int_nodes,:]
@@ -68,36 +71,47 @@ def split_grid(g, frac_tag):
     x0 = g.face_centers[:,frac_tag_id[0]]
     left = half_space_int(n,x0,g.face_centers[:,face_fracnodes_id[:,1]])
 
-    # faces on right side should not be connected to remove connection to left side of fracture
-    col  = face_fracnodes_id[~left,1]
-    row  = face_fracnodes_id[~left,0]
-    face_fracnodes_right = sps.csc_matrix(([True]*row.size,(row,col)),(sum(int_nodes),g.face_nodes.shape[1]))
-    g.face_nodes[int_nodes,:] = face_fracnodes_right
+    # add duplicate nodes at the end of array
+    node_coord            = g.nodes[:,int_nodes]
+    node_coord           -= .5*offset*n
+    g.nodes[:,int_nodes] += .5*offset*n 
+    g.nodes = np.concatenate((g.nodes,node_coord),axis=1)
+    g.num_nodes += node_coord.shape[1]
+
+    # faces on left side of fracture should not be connected to
+    # nodes on right side of fracture. The right nodes keeps the
+    # other face-mappings.
+    leftface_nodes_id = face_fracnodes_id[left,:]
+    leftface_nodes_id[:,0] = int_nodes_id[leftface_nodes_id[:,0]]
+    g.face_nodes[leftface_nodes_id[:,0],leftface_nodes_id[:,1]] = False
 
     # Connect new nodes to left side of fracture
     col  = face_fracnodes_id[left,1]
     row  = face_fracnodes_id[left,0]
-    face_fracnodes_left = sps.csc_matrix(([True]*row.size,(row,col)),(sum(int_nodes),g.face_nodes.shape[1]))
-    g.face_nodes = sps.vstack((g.face_nodes, face_fracnodes_left))
+    face_fracnodes_left = sps.csc_matrix(([True]*row.size,(row,col)),
+                                         (sum(int_nodes),g.face_nodes.shape[1]))
+    g.face_nodes = sps.vstack((g.face_nodes, face_fracnodes_left),format='csc')
 
-    # create new faces along fracture
-    tip_nodes_id = np.argwhere(tip_nodes)
+    # create new faces along fracture and connect them to left
+    # nodes
     frac_int_nodes_id = np.argwhere(frac_nodes[int_nodes,:])
-    frac_tip_nodes_id = np.argwhere(frac_nodes[tip_nodes,:])
+    frac_tip_nodes_id = np.argwhere(frac_nodes[tip_nodes_id,:])
     row  = frac_int_nodes_id[:,0] + g.num_nodes - node_coord.shape[1]
     row  = np.append(row, tip_nodes_id[frac_tip_nodes_id[:,0]])
     col  = frac_int_nodes_id[:,1]
     col  = np.append(col, frac_tip_nodes_id[:,1])
     face_fracnodes_right = sps.csc_matrix(([True]*row.size,(row,col)),(g.num_nodes,frac_tag_id.size))
-    g.face_nodes = sps.hstack((g.face_nodes,face_fracnodes_right))
-    g.num_faces+=frac_tag_id.size
+    g.face_nodes = sps.hstack((g.face_nodes,face_fracnodes_right),format='csc')
 
     # update face info
+    g.num_faces+=frac_tag_id.size
     g.face_normals = np.hstack((g.face_normals, g.face_normals[:,frac_tag]))
     g.face_areas = np.append(g.face_areas, g.face_areas[frac_tag])
     g.face_centers = np.hstack((g.face_centers, g.face_centers[:,frac_tag]))
 
     # Set new cell conectivity
+    # Cells on right side does not change. We first add the new left-faces
+    # to the left cells
     cell_frac_id = np.argwhere(cell_frac)
     left_cell = half_space_int(n,x0,g.cell_centers[:,cell_frac_id[:,1]])
 
@@ -105,13 +119,38 @@ def split_grid(g, frac_tag):
     row  = cell_frac_id[left_cell,0]
     data = np.ravel(g.cell_faces[np.ravel(frac_tag_id[row]), col])
     cell_frac_left = sps.csc_matrix((data,(row,col)),(frac_tag_id.size,g.cell_faces.shape[1]))
-
+    # We remove the right faces of the left cells.
     col  = cell_frac_id[~left_cell,1]
     row  = cell_frac_id[~left_cell,0]
     data = np.ravel(g.cell_faces[np.ravel(frac_tag_id[row]), col])
     cell_frac_right = sps.csc_matrix((data,(row,col)),(frac_tag_id.size,g.cell_faces.shape[1]))
     g.cell_faces[frac_tag,:] = cell_frac_right
-
-    g.cell_faces = sps.vstack((g.cell_faces,cell_frac_left))
+    g.cell_faces = sps.vstack((g.cell_faces,cell_frac_left),format='csc')
 
     return g
+
+
+def split_grid(g,f):
+    for i in range(f.num):
+        g = split_fracture(g,f.tag[i,:],tip_nodes_id=f.tips[i,:],offset=0.5)
+        if i<f.num:
+            new_faces = np.zeros((f.tag.shape[0],sum(f.tag[i,:])),dtype='bool')
+            f.tag=np.hstack((f.tag, new_faces))
+    return g
+
+
+class Fracture:
+    def __init__(self,g):
+        self.num = 0
+        self.tag = np.zeros((0,g.num_faces),dtype='bool')
+        self.tips = np.zeros((0,2),dtype=np.int8)
+    
+    def add_tag(self,tag):
+        self.tag = np.vstack((self.tag,tag))
+        self.num+=1
+
+    def set_tips(self, g):
+        self.tips = np.zeros((self.num,2),dtype=np.int8)
+        for i in range(self.num):
+            node_mult = np.ravel(np.sum(g.face_nodes[:,self.tag[i,:]],axis=1))
+            self.tips[i,:] = np.ravel(np.argwhere(node_mult==1)) # tip nodes
--- tests/test_split_grid.py
+++ tests/test_split_grid.py
@@ -3,9 +3,9 @@ import numpy as np
 from gridding.fractured import split_grid
 from core.grids import structured
 
-def test_split_grid():
+def test_split_fracture():
     """ Check that no error messages are created in the process of creating a
-    split_grid.
+    split_fracture.
     """
     n = 10
     g = structured.CartGrid([n,n])
@@ -14,9 +14,8 @@ def test_split_grid():
                                              g.face_centers[0,:]>n/4),
                               g.face_centers[0,:]<3*n/4)
     h = g.copy()
-    h = split_grid.split_grid(h,frac_tag)
+    h = split_grid.split_fracture(h,frac_tag)
     # Check that correct number of faces are added
-    print(g.num_faces)
     assert h.num_faces == g.num_faces + sum(frac_tag)
     assert h.num_faces == h.face_nodes.shape[1]
     assert h.cell_faces.shape[0] == h.num_faces
@@ -28,6 +27,41 @@ def test_split_grid():
     # check that no faces are hanging
     b = np.abs(h.cell_faces).sum(axis=1).A.ravel(1)==0
     assert np.any(b) == False
-    
+
+
+def test_intersecting_fracture():
+    n = 10
+    g = structured.CartGrid([n,n])
+    g.compute_geometry()
+    frac_tag1 = np.logical_and(np.logical_and(g.face_centers[1,:]==n/2,
+                                              g.face_centers[0,:]>n/4),
+                               g.face_centers[0,:]<3*n/4)
+
+    frac_tag2 = np.logical_and(np.logical_and(g.face_centers[0,:]==n/2,
+                                          g.face_centers[1,:]>n/4),
+                           g.face_centers[1,:]<3*n/4)
+    f = split_grid.Fracture(g)
+    f.add_tag(frac_tag1)
+    f.add_tag(frac_tag2)
+    f.set_tips(g)
+    h = g.copy()
+    h = split_grid.split_grid(h,f)
+    # check that no nodes are hanging
+    # Check that correct number of faces are added
+    assert h.num_faces == g.num_faces + np.sum(f.tag)
+    assert h.num_faces == h.face_nodes.shape[1]
+    assert h.cell_faces.shape[0] == h.num_faces
+    assert h.cell_faces.shape[1] == g.num_cells
+    assert h.num_cells == g.num_cells
+    # Check that correct number of nodes are added
+    print(h.num_nodes)
+    print(g.num_nodes)
+    print(np.sum(f.tag))
+    print(f.tag.shape[0])
+    assert h.num_nodes == h.face_nodes.shape[0]
+    # check that no faces are hanging
+    b = np.abs(h.cell_faces).sum(axis=1).A.ravel(1)==0
+    assert np.any(b) == False
+
 if __name__ == '__main__':
-    test_split_grid()
+    test_split_fracture()
