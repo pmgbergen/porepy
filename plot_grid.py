33f963743d1dc26b617eb3c0766407fa32ffe93d
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2017-02-16 18:01:47 +0100

    update plot_grid to handle grid bucket (need documentation update)

--- plot_grid.py
+++ plot_grid.py
@@ -7,6 +7,7 @@ Created on Fri Feb 26 19:37:05 2016
 
 import numpy as np
 import scipy.sparse as sps
+
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.tri
@@ -16,13 +17,13 @@ from matplotlib.collections import PatchCollection
 import mpl_toolkits.mplot3d as a3
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
-
-from core.grids import simplex, structured
+from core.grids import grid
 from compgeom import sort_points
+from gridding import grid_bucket
 
 #------------------------------------------------------------------------------#
 
-def plot_grid(g, cell_value=None, info=None, alpha=1):
+def plot_grid(g, cell_value=None, info=None, alpha=1, rgb=[1,0,0]):
     """ plot the grid in a 3d framework.
 
     It is possible to add the cell ids at the cells centers (info option 'c'),
@@ -47,43 +48,69 @@ def plot_grid(g, cell_value=None, info=None, alpha=1):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
-    if g.dim == 1:   plot_grid_1d(g, cell_value, ax, alpha)
-    elif g.dim == 2: plot_grid_2d(g, cell_value, ax, alpha)
-    else:            plot_grid_3d(g, ax, alpha)
-
-    x = [np.amin(g.nodes[0,:]), np.amax(g.nodes[0,:])]
-    y = [np.amin(g.nodes[1,:]), np.amax(g.nodes[1,:])]
-    z = [np.amin(g.nodes[2,:]), np.amax(g.nodes[2,:])]
-    if not np.isclose(x[0], x[1]): ax.set_xlim3d( x )
-    if not np.isclose(y[0], y[1]): ax.set_ylim3d( y )
-    if not np.isclose(z[0], z[1]): ax.set_zlim3d( z )
-
     ax.set_title( " ".join( g.name ) )
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
 
-    if info is not None: add_info( g, info, ax )
-    if cell_value is not None and g.dim !=3: fig.colorbar(color_map(cell_value))
-    plt.show()
+    if isinstance(g, grid.Grid):
+        plot_grid_single(g, ax, cell_value, info, alpha, rgb)
 
-    return fig.number
+    if isinstance(g, grid_bucket.Grid_Bucket):
+        plot_grid_bucket(g, ax, cell_value, info, alpha, rgb)
+
+    plt.show()
 
 #------------------------------------------------------------------------------#
 
-def plot_grid_fractures(g, show=True):
-    figId = plot_grid(g, show=False)
-    fig = plt.figure( figId )
-    face_info = g.face_info
-    tags = face_info['tags']
-    face_nodes = g.face_nodes.indices.reshape((2, -1), order='F')
-    xf = g.nodes
-    for ff in np.nditer(np.where(np.isfinite(tags))):
-        fn_loc = face_nodes[:, ff]
-        plt.plot([xf[0, fn_loc[0]], xf[0, fn_loc[1]]],
-                 [xf[1, fn_loc[0]], xf[1, fn_loc[1]]], linewidth=4, color='k')
-        # if show:
-        #     plt.show()
+def plot_grid_single(g, ax, cell_value, info, alpha, rgb):
+
+    plot_grid_xd(g, cell_value, ax, alpha, rgb)
+    x, y, z = lim(ax, g.nodes)
+    if not np.isclose(x[0], x[1]): ax.set_xlim3d( x )
+    if not np.isclose(y[0], y[1]): ax.set_ylim3d( y )
+    if not np.isclose(z[0], z[1]): ax.set_zlim3d( z )
+
+    if info is not None: add_info( g, info, ax )
+    if cell_value is not None and g.dim !=3:
+        fig.colorbar(color_map(cell_value))
+
+#------------------------------------------------------------------------------#
+
+def plot_grid_bucket(gb, ax, cell_value, info, alpha, rgb):
+
+    [plot_grid_xd(g, cell_value[v], ax, alpha, np.divide(rgb, int(v)+1)) \
+                                                                 for g, v in gb]
+
+    val = np.array([lim(ax, g.nodes) for g, _ in gb])
+
+    x = [np.amin(val[:,0,:]), np.amax(val[:,0,:])]
+    y = [np.amin(val[:,1,:]), np.amax(val[:,1,:])]
+    z = [np.amin(val[:,2,:]), np.amax(val[:,2,:])]
+
+    if not np.isclose(x[0], x[1]): ax.set_xlim3d( x )
+    if not np.isclose(y[0], y[1]): ax.set_ylim3d( y )
+    if not np.isclose(z[0], z[1]): ax.set_zlim3d( z )
+
+    if info is not None: [add_info( g, info, ax ) for g, _ in gb]
+
+#    if cell_value is not None and gb.dim_max() !=3:
+#        fig.colorbar(color_map(cell_value))
+
+#------------------------------------------------------------------------------#
+
+def plot_grid_xd(g, cell_value, ax, alpha, rgb):
+    if g.dim == 1:   plot_grid_1d(g, cell_value, ax, alpha, rgb)
+    elif g.dim == 2: plot_grid_2d(g, cell_value, ax, alpha, rgb)
+    else:            plot_grid_3d(g, ax, alpha, rgb)
+
+#------------------------------------------------------------------------------#
+
+def lim(ax, nodes):
+    x = [np.amin(nodes[0,:]), np.amax(nodes[0,:])]
+    y = [np.amin(nodes[1,:]), np.amax(nodes[1,:])]
+    z = [np.amin(nodes[2,:]), np.amax(nodes[2,:])]
+    return x, y, z
 
 #------------------------------------------------------------------------------#
 
@@ -115,7 +142,7 @@ def add_info( g, info, ax ):
 
 #------------------------------------------------------------------------------#
 
-def plot_grid_1d(g, cell_value, ax, alpha):
+def plot_grid_1d(g, cell_value, ax, alpha, rgb):
     cell_nodes = g.cell_nodes()
     nodes, cells, _  = sps.find( cell_nodes )
 
@@ -136,7 +163,7 @@ def plot_grid_1d(g, cell_value, ax, alpha):
 
 #------------------------------------------------------------------------------#
 
-def plot_grid_2d( g, cell_value, ax, alpha ):
+def plot_grid_2d( g, cell_value, ax, alpha, rgb):
     cell_nodes = g.cell_nodes()
     nodes, cells, _  = sps.find( cell_nodes )
 
@@ -145,7 +172,7 @@ def plot_grid_2d( g, cell_value, ax, alpha ):
         def color_face(value): return scalar_map.to_rgba(value, alpha)
     else:
         cell_value = np.zeros(g.num_cells)
-        def color_face(value): return [1,0,0,alpha]
+        def color_face(value): return np.r_[rgb, alpha]
 
     for c in np.arange( g.num_cells ):
         loc = slice(cell_nodes.indptr[c], cell_nodes.indptr[c+1])
@@ -163,10 +190,9 @@ def plot_grid_2d( g, cell_value, ax, alpha ):
 
 #------------------------------------------------------------------------------#
 
-def plot_grid_3d( g, ax, alpha ):
+def plot_grid_3d(g, ax, alpha, rgb):
     faces_cells, cells, _ = sps.find( g.cell_faces )
     nodes_faces, faces, _ = sps.find( g.face_nodes )
-    RGB_face = [1,0,0,alpha]
     for c in np.arange(g.num_cells):
         loc_c = slice(g.cell_faces.indptr[c], g.cell_faces.indptr[c+1])
         fs = faces_cells[loc_c]
@@ -179,7 +205,7 @@ def plot_grid_3d( g, ax, alpha ):
             pts = g.nodes[:, ptsId[mask]]
             poly = Poly3DCollection( [pts.T] )
             poly.set_edgecolor('k')
-            poly.set_facecolors(RGB_face)
+            poly.set_facecolors(np.r_[rgb, alpha])
             ax.add_collection3d(poly)
 
 #------------------------------------------------------------------------------#
