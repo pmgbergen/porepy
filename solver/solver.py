1ccc5b0c4490d496a2a3a94bc1737f4201ddfbb3
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2017-02-16 17:34:34 +0100

    add an interface for a general solver

---
+++ solver/solver.py
@@ -0,0 +1,13 @@
+class Solver(object):
+
+#------------------------------------------------------------------------------#
+
+    def matrix_rhs(self):
+        raise NotImplementedError("Method not implemented")
+
+#------------------------------------------------------------------------------#
+
+    def ndof(self):
+        raise NotImplementedError("Method not implemented")
+
+#------------------------------------------------------------------------------#
