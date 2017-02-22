e040b633fcb3b89a9e2940f59bd1acf48c63a12b
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2017-02-22 13:41:58 +0100

    fix the input parameters for the abstact solver class

--- solver/solver.py
+++ solver/solver.py
@@ -2,12 +2,12 @@ class Solver(object):
 
 #------------------------------------------------------------------------------#
 
-    def matrix_rhs(self):
+    def matrix_rhs(self, g, data)
         raise NotImplementedError("Method not implemented")
 
 #------------------------------------------------------------------------------#
 
-    def ndof(self):
+    def ndof(self, g):
         raise NotImplementedError("Method not implemented")
 
 #------------------------------------------------------------------------------#
