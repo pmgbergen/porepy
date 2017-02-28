bcb69bc5a8f5e74a9d8d8adcfbb51d102857eaf1
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2017-02-28 09:19:23 +0100

    Import from gmsh of 3d fractured grids now handles lines along fracture intersections.
    First try, updates to follow

--- fractured/grid_3d.py
+++ fractured/grid_3d.py
@@ -1,9 +1,12 @@
 import numpy as np
 
 import gridding.constants
-from core.grids import simplex
+from core.grids import simplex, structured
 from gridding.gmsh import gmsh_interface, mesh_io
 from gridding.fractured import fractures
+import compgeom.sort_points
+import compgeom.basics as cg
+
 
 def create_grid(fracs, box, **kwargs):
 
@@ -89,3 +92,85 @@ def create_grid(fracs, box, **kwargs):
                                          loc_tri_loc_ind.transpose())
         g_2d.append(g)
 
+
+    # Recover lines
+    # There will be up to three types of physical lines: intersections (between
+    # fractures), fracture tips, and auxiliary lines (to be disregarded)
+
+    gmsh_const = gridding.constants.GmshConstants()
+
+    line_tags = cell_info['line'][:, 0]
+    line_cells = cells['line']
+
+    gmsh_intersection_num = []
+    gmsh_tip_num = []
+    tip_pts = np.empty(0)
+
+    g_1d = []
+    for i, pn in enumerate(phys_names['line']):
+
+        # Index of the final underscore in the physical name. Chars before this
+        # will identify the line type, the one after will give index
+        offset_index = pn[2].rfind('_')
+        line_num = int(pn[2][offset_index+1:])
+        loc_line_cell_num = np.where(line_tags == pn[1])[0]
+        loc_line_pts = line_cells[loc_line_cell_num, :]
+
+        assert loc_line_pts.size > 1
+
+        line_type = pn[2][:offset_index]
+        if line_type == gmsh_const.PHYSICAL_NAME_FRACTURE_TIP[:-1]:
+            gmsh_tip_num.append(i)
+
+            # We need not know which fracture the line is on the tip of (do
+            # we?)
+            tip_pts = np.append(tip_pts, np.unique(loc_line_pts))
+
+        elif line_type == gmsh_const.PHYSICAL_NAME_FRACTURE_LINE[:-1]:
+
+
+
+            loc_pts_1d = np.unique(loc_line_pts)#.flatten()
+
+            loc_coord = pts[loc_pts_1d, :].transpose()
+            loc_center = np.mean(loc_coord, axis=1).reshape((-1, 1))
+            loc_coord -= loc_center
+
+            # Check that the points indeed form a line
+            assert cg.is_collinear(loc_coord)
+            # Find the tangent of the line
+            tangent = cg.compute_tangent(loc_coord)
+            # Projection matrix 
+            rot = cg.project_plane_matrix(loc_coord, tangent)
+            loc_coord_1d = rot.dot(loc_coord)
+
+            # The points are now 1d along one of the coordinate axis, but we
+            # don't know which yet. Find this.
+
+            sum_coord = np.sum(np.abs(loc_coord_1d), axis=1)
+            active_dimension = np.logical_not(np.isclose(sum_coord, 0))
+            # Check that we are indeed in 1d
+            assert np.sum(active_dimension) == 1
+            
+            # Sort nodes, and create grid
+            sorted_coord = np.sort(loc_coord[active_dimension])
+            g = structured.TensorGrid(sorted_coord)
+
+            # Project back again to 3d coordinates
+            irot = rot.transpose()
+            g.nodes = irot.dot(g.nodes) + loc_coord
+            g_1d.append(g)
+
+            import pdb
+            pdb.set_trace()
+
+        else:  # Auxiliary line
+            pass
+
+
+
+
+    import pdb
+    pdb.set_trace()
+
+    return g_3d, g_2d, g_1d
