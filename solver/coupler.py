a8ad9922dd6069517e5cc041c0d104694be742f5
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2017-03-20 16:50:27 +0100

    update the coupler based on the new version of grid bucket

--- solver/coupler.py
+++ solver/coupler.py
@@ -11,38 +11,49 @@ class Coupler(object):
 
 #------------------------------------------------------------------------------#
 
-    def matrix_rhs(self, gb, data, matrix_format="csr"):
+    def matrix_rhs(self, gb, matrix_format="csr"):
 
-        dof = np.array([self.solver.ndof(g) for g, _ in gb])
+        gb.assign_node_ordering()
+        gb.add_node_prop('dof')
+        for g, d in gb:
+            d['dof'] = self.solver.ndof(g)
 
-        matrix = np.array([sps.coo_matrix((i,j)) for i in dof for j in dof]\
-                         ).reshape((gb.size, gb.size))
-        rhs = np.array([np.empty(i) for i in dof])
+        matrix = np.empty((gb.size(), gb.size()), dtype=np.object)
+        for g_i, d_i in gb:
+            for g_j, d_j in gb:
+                pos_i, pos_j = gb.nodes_prop([g_i, g_j], 'node_number')
+                matrix[pos_i, pos_j] = sps.coo_matrix((d_i['dof'], d_j['dof']))
 
-        for g, v in gb:
-            pos = int(v)
-            matrix[pos, pos], rhs[pos] = self.solver.matrix_rhs(g, data[0][v])
+        rhs = np.array([np.empty(d['dof']) for _, d in gb])
 
-        # loop on the edges
-        for e in gb.e():
-            v_h, v_l = gb.v_of_e(e)
-            g_h, g_l = gb.grids[v_h], gb.grids[v_l]
-            idx = np.ix_([int(v_h), int(v_l)], [int(v_h), int(v_l)])
-            fc = gb.face_cells[e]
-            matrix[idx] += self.coupling.matrix_rhs(g_h, g_l, fc, data[1][e])
+        for g, d in gb:
+            pos = d['node_number']
+            data = gb.node_props(g)
+            matrix[pos, pos], rhs[pos] = self.solver.matrix_rhs(g, data)
+
+        for e in gb.edges():
+            g_l, g_h = gb.sorted_nodes_of_edge(e)
+            pos_l, pos_h = gb.nodes_prop([g_l, g_h], 'node_number')
+            idx = np.ix_([pos_h, pos_l], [pos_h, pos_l])
+
+            data = gb.edge_props(e)
+            fc = data['face_cells']
+            matrix[idx] += self.coupling.matrix_rhs(g_h, g_l, fc, data)
 
         return sps.bmat(matrix, matrix_format), np.concatenate(tuple(rhs))
 
 #------------------------------------------------------------------------------#
 
-    def split(self, gb, values):
+    def split(self, gb, key, values):
 
-        values_split = gb.g_prop()
-        ndof = 0
-        for g, v in gb:
-            dof_v = self.solver.ndof(g)
-            values_split[v] = values[slice(ndof, ndof+dof_v)]
-            ndof += dof_v
-        return values_split
+        dofs = np.empty(gb.size(), dtype=int)
+        for _, d in gb:
+            dofs[d['node_number']] = d['dof']
+        dofs = np.r_[0, np.cumsum(dofs)]
+
+        gb.add_node_prop(key)
+        for g, d in gb:
+            i = d['node_number']
+            d[key] = values[slice(dofs[i], dofs[i+1])]
 
 #------------------------------------------------------------------------------#
