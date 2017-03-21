e671841d2bc59d54f0847a62046dc56cd82e7015
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2017-03-21 13:16:59 +0100

    add comments and modify the interface according to the new data structure

--- dual_coupling.py
+++ dual_coupling.py
@@ -10,24 +10,63 @@ class DualCoupling(object):
 
 #------------------------------------------------------------------------------#
 
-    def matrix_rhs(self, g_h, g_l, face_cells, data):
+    def matrix_rhs(self, g_h, g_l, data_h, data_l, data_edge):
+        """
+        Construct the matrix (and right-hand side) for the coupling conditions.
+        Note: the right-hand side is not implemented now.
 
-        k = data['kn']
+        Parameters:
+            g_h: grid of higher dimension
+            g_l: grid of lower dimension
+            face_cells: face_cells mapping between the two grids
+            data: dictionary which stores the data for the edges of the grid
+                bucket
+
+        Returns:
+            cc: block matrix which store the contribution of the coupling
+                condition in the following order:
+                [ cc_hh  cc_hl ]
+                [ cc_lh  cc_ll ]
+                where:
+                - cc_hh is the contribution to be added to the global block
+                  matrix in related to the grid of higher dimension (g_h).
+                - cc_ll is the contribution to be added to the global block
+                  matrix in related to the grid of lower dimension (g_l).
+                  In this case the term is null.
+                - cc_hl is the contribution to be added to the global block
+                  matrix in related to the coupling between grid of higher
+                  dimension (g_h) and the grid of lower dimension (g_l).
+                - cc_lh is the contribution to be added to the global block
+                  matrix in related to the coupling between grid of lower
+                  dimension (g_l) and the grid of higher dimension (g_h).
+                  In this case cc_lh is the transpose of cc_hl.
+
+        """
+
+        # Normal permeability of the intersection
+        kn = data_edge['kn']
+        assert kn is not None
+
+        # Retrieve the number of degrees of both grids
         dof = np.array([self.solver.ndof(g_h), self.solver.ndof(g_l)])
 
-        cells_l, faces_h, _ = sps.find(face_cells)
+        # Recover the information for the grid-grid mapping
+        cells_l, faces_h, _ = sps.find(data_edge['face_cells'])
         faces, _, sgn = sps.find(g_h.cell_faces)
         sgn = sgn[np.unique(faces, return_index=True)[1]]
         sgn = sgn[faces_h]
 
+        # Create the block matrix for the contributions
         cc = np.array([sps.coo_matrix((i,j)) for i in dof for j in dof]\
                      ).reshape((2, 2))
 
+        # Compute the off-diagonal terms
         dataIJ, I, J = sgn, g_l.num_faces+cells_l, faces_h
         cc[1,0] = sps.csr_matrix((dataIJ, (I, J)), (dof[1], dof[0]))
         cc[0,1] = cc[1,0].T
 
-        dataIJ = 1./np.multiply(g_h.face_areas[faces_h], k[cells_l])
+        # Compute the diagonal terms
+        dataIJ = 1./np.multiply(g_h.face_areas[faces_h], kn[cells_l])
         I, J = faces_h, faces_h
         cc[0,0] = sps.csr_matrix((dataIJ, (I, J)), (dof[0], dof[0]))
 
