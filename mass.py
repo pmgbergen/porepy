314594dc3e64feee2cc601269ecde9b4b2c03306
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2017-03-23 18:28:59 +0100

    add a solver which create the inverse of the mass matrix, useful when the grid bucket is used

--- mass.py
+++ mass.py
@@ -53,7 +53,6 @@ class Mass(Solver):
         --------
         data = {'deltaT': 1e-2, 'phi': 0.3*np.ones(g.num_cells)}
         M, _ = mass.Mass().matrix_rhs(g, data)
-        invM = mass.Mass().inv(M)
 
         """
         ndof = self.ndof(g)
@@ -62,22 +61,62 @@ class Mass(Solver):
 
         return sps.dia_matrix((coeff, 0), shape=(ndof, ndof)), np.zeros(ndof)
 
+################################################################################
+
+class InvMass(Solver):
+
 #------------------------------------------------------------------------------#
 
-    def inv(self, matrix):
+    def ndof(self, g):
         """
-        Return the inverse of the matrix for this method.
+        Return the number of degrees of freedom associated to the method.
+        In this case number of cells.
+
+        Parameter
+        ---------
+        g: grid, or a subclass.
+
+        Return
+        ------
+        dof: the number of degrees of freedom.
+
+        """
+        return g.num_cells
+
+#------------------------------------------------------------------------------#
+
+    def matrix_rhs(self, g, data):
+        """
+        Return the inverse of the matrix and righ-hand side (null) for a
+        discretization of a L2-mass bilinear form with constant test and trial
+        functions.
+
+        The name of data in the input dictionary (data) are:
+        phi: array (self.g.num_cells)
+            Scalar values which represent the porosity.
+            If not given assumed unitary.
+        deltaT: Time step for a possible temporal discretization scheme.
+            If not given assumed unitary.
 
         Parameters
         ----------
         g : grid, or a subclass, with geometry fields computed.
+        data: dictionary to store the data.
 
         Return
         ------
         matrix: sparse dia (g.num_cells, g_num_cells)
-            Inverse of the mass matrix obtained from the discretization.
+            Inverse of mass matrix obtained from the discretization.
+        rhs: array (g_num_cells)
+            Null right-hand side.
+
+        Examples
+        --------
+        data = {'deltaT': 1e-2, 'phi': 0.3*np.ones(g.num_cells)}
+        M, _ = mass.InvMass().matrix_rhs(g, data)
 
         """
-        return sps.dia_matrix((1./matrix.diagonal(), 0), shape=matrix.shape)
+        M, rhs = Mass().matrix_rhs(g, data)
+        return sps.dia_matrix((1./M.diagonal(), 0), shape=M.shape), rhs
 
 #------------------------------------------------------------------------------#
