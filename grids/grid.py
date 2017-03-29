ef1919a6d2d181c321b78ea97c44ce027b345d46
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2017-03-29 16:55:40 +0200

    add new function related to face tag

--- grids/grid.py
+++ grids/grid.py
@@ -665,6 +665,11 @@ class Grid(object):
         self.face_tags[f] = np.bitwise_and(
             self.face_tags[f], np.bitwise_not(tag))
 
+    def remove_face_tag_if_tag(self, tag, if_tag):
+        f = self.has_face_tag(if_tag)
+        self.face_tags[f] = np.bitwise_and(
+            self.face_tags[f], np.bitwise_not(tag))
+
     def has_face_tag(self, tag):
         return np.bitwise_and(self.face_tags, tag).astype(np.bool)
 
