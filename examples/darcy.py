7e283997bc2242f5b5c05dcfc4cf88c9226f0d9c
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2017-03-30 14:53:14 +0200

    add example for the darcy problem

--- examples/darcy.py
+++ examples/darcy.py
@@ -13,18 +13,23 @@ from scipy.sparse.linalg import spsolve
 import time
 import traceback
 import logging
+from inspect import isfunction, getmembers
 
-from core.grids import structured
+from core.grids import structured, simplex
 from core.bc import bc
 from core.constit import second_order_tensor
 from utils.errors import error
 from vem import dual
+import compgeom.basics as cg
 
 from viz.plot_grid import plot_grid
 
 #------------------------------------------------------------------------------#
 
 def darcy_dualVEM_example0(**kwargs):
+    #######################
+    # Simple 2d Darcy problem with known exact solution
+    #######################
     Nx = Ny = 25
     g = structured.CartGrid( [Nx, Ny], [1,1] )
     g.compute_geometry()
@@ -48,11 +53,16 @@ def darcy_dualVEM_example0(**kwargs):
 
     if kwargs['visualize']: plot_grid(g, p, P0u)
 
-    return error.norm_L2(g, p), error.norm_L2(g, P0u)
+    norms = np.array([error.norm_L2(g, p), error.norm_L2(g, P0u)])
+    norms_known = np.array([0.041554943620853595, 0.18738227880674516])
+    assert np.allclose(norms, norms_known)
 
 #------------------------------------------------------------------------------#
 
 def darcy_dualVEM_example1(**kwargs):
+    #######################
+    # Simple 2d Darcy problem with known exact solution
+    #######################
     Nx = Ny = 25
     g = structured.CartGrid( [Nx, Ny], [1,1] )
     g.compute_geometry()
@@ -87,19 +97,111 @@ def darcy_dualVEM_example1(**kwargs):
     p_ex = error.interpolate(g, funP_ex)
     u_ex = error.interpolate(g, funU_ex)
 
-    return error.error_L2(g, p, p_ex), error.error_L2(g, P0u, u_ex)
+    errors = np.array([error.error_L2(g, p, p_ex), error.error_L2(g, P0u, u_ex)])
+    errors_known = np.array([0.0210718223032, 0.00526933885613])
+    assert np.allclose(errors, errors_known)
+
+#------------------------------------------------------------------------------#
+
+def darcy_dualVEM_example2(**kwargs):
+    #######################
+    # Simple 2d Darcy problem on a surface with known exact solution
+    #######################
+    Nx = Ny = 25
+    g = simplex.StructuredTriangleGrid( [Nx, Ny], [1,1] )
+    R = cg.rot( np.pi/6., [0,1,1] )
+    g.nodes = np.dot( R, g.nodes )
+    g.compute_geometry(is_embedded=True)
+
+    T = cg.tangent_matrix(g.nodes)
+
+    kxx = np.ones(g.num_cells)
+    perm = second_order_tensor.SecondOrderTensor(g.dim, kxx)
+
+    def funP_ex(pt): return np.pi*pt[0] - 6*pt[1] + np.exp(1)*pt[2] - 4
+    def funU_ex(pt): return np.dot(T, [-np.pi, 6, -np.exp(1)])
+    def fun(pt): return 0
+
+    f = np.array([fun(pt) for pt in g.cell_centers.T])
+
+    b_faces = g.get_boundary_faces()
+    bnd = bc.BoundaryCondition(g, b_faces, ['dir']*b_faces.size)
+    bnd_val = {'dir': funP_ex(g.face_centers[:, b_faces])}
+
+    solver = dual.DualVEM()
+    data = {'k': perm, 'f': f, 'bc': bnd, 'bc_val': bnd_val}
+    D, rhs = solver.matrix_rhs(g, data)
+
+    up = sps.linalg.spsolve(D, rhs)
+    u, p = solver.extractU(g, up), solver.extractP(g, up)
+    P0u = solver.projectU(g, u, data)
+
+    if kwargs['visualize']: plot_grid(g, p, P0u)
+
+    p_ex = error.interpolate(g, funP_ex)
+    u_ex = error.interpolate(g, funU_ex)
+
+    errors = np.array([error.error_L2(g, p, p_ex), error.error_L2(g, P0u, u_ex)])
+    errors_known = np.array([0, 0])
+    assert np.allclose(errors, errors_known)
+
+#------------------------------------------------------------------------------#
+
+def darcy_dualVEM_example3(**kwargs):
+    #######################
+    # Simple 3d Darcy problem with known exact solution
+    #######################
+    Nx = Ny = Nz = 7
+    g = structured.CartGrid( [Nx, Ny, Nz], [1, 1, 1] )
+    g.compute_geometry()
+
+    kxx = np.ones(g.num_cells)
+    perm = second_order_tensor.SecondOrderTensor(g.dim, kxx)
+
+    def funP_ex(pt): return np.sin(2*np.pi*pt[0])*np.sin(2*np.pi*pt[1]) * \
+                            np.sin(2*np.pi*pt[2])
+    def funU_ex(pt): return [-2*np.pi*np.cos(2*np.pi*pt[0])*\
+                                np.sin(2*np.pi*pt[1])*np.sin(2*np.pi*pt[2]),
+                             -2*np.pi*np.sin(2*np.pi*pt[0])*\
+                                np.cos(2*np.pi*pt[1])*np.sin(2*np.pi*pt[2]),
+                             -2*np.pi*np.sin(2*np.pi*pt[0])*\
+                                np.sin(2*np.pi*pt[1])*np.cos(2*np.pi*pt[2])]
+    def fun(pt): return 12*np.pi**2 * funP_ex(pt)
+
+    f = np.array([fun(pt) for pt in g.cell_centers.T])
+
+    b_faces = g.get_boundary_faces()
+    bnd = bc.BoundaryCondition(g, b_faces, ['dir']*b_faces.size)
+    bnd_val = {'dir': funP_ex(g.face_centers[:, b_faces])}
+
+    solver = dual.DualVEM()
+    data = {'k': perm, 'f': f, 'bc': bnd, 'bc_val': bnd_val}
+    D, rhs = solver.matrix_rhs(g, data)
+
+    up = sps.linalg.spsolve(D, rhs)
+    u, p = solver.extractU(g, up), solver.extractP(g, up)
+    P0u = solver.projectU(g, u, data)
+
+    if kwargs['visualize']: plot_grid(g, p, P0u)
+
+    p_ex = error.interpolate(g, funP_ex)
+    u_ex = error.interpolate(g, funU_ex)
+
+    errors = np.array([error.error_L2(g, p, p_ex), error.error_L2(g, P0u, u_ex)])
+    errors_known = np.array([0.1010936831876412, 0.0680593765009036])
+    assert np.allclose(errors, errors_known)
 
 #------------------------------------------------------------------------------#
 
 if __name__ == '__main__':
     # If invoked as main, run all tests
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'v:',['verbose=','visualize='])
+        opts, args = getopt.getopt(sys.argv[1:], 'v:',['verbose=', 'visualize='])
     except getopt.GetoptError as err:
         print(err)
         sys.exit(2)
 
-    verbose = 0
+    verbose = True
     visualize = False
     # process options
     for o, a in opts:
@@ -113,52 +215,38 @@ if __name__ == '__main__':
 
     time_tot = time.time()
 
-    #######################
-    # Simple 2d Darcy problem with known exact solution
-    example = 0
-    if verbose > 0:
-        print('Example', example )
-        print('Run 2d Darcy example')
-    try:
+    functions_list = [o for o in getmembers(
+        sys.modules[__name__]) if isfunction(o[1])]
+
+    for f in functions_list:
+        func = f
+        if func[0] == 'isfunction' or func[0] == 'getmembers' or \
+           func[0] == 'spsolve' or func[0] == 'plot_grid':
+            continue
+        if verbose:
+            print('Running ' + func[0])
+
         time_loc = time.time()
-        known = np.array([0.041554943620853595, 0.18738227880674516])
-        answer = darcy_dualVEM_example0(visualize=visualize)
-        assert np.allclose( answer, known )
+        try:
+            func[1](visualize=visualize)
 
-        if verbose > 0:
-            print('Example', example, 'completed successfully')
-            print('Elapsed time ' + str(time.time() - time_loc))
+        except Exception as exp:
+            print('\n')
+            print(' ************** FAILURE **********')
+            print('Example ' + func[0] + ' failed')
+            print(exp)
+            logging.error(traceback.format_exc())
+            failure_counter += 1
+            continue
+
+        # If we have made it this far, this is a success
         success_counter += 1
-    except Exception as exp:
-        print('\n')
-        print(' ***** FAILURE ****')
-        print('Example', example, 'failed')
-        print(exp)
-        logging.error(traceback.format_exc())
-        failure_counter += 1
-
-    #######################
-    # Simple 2d Darcy problem with known exact solution
-    example += 1
-    if verbose > 0:
-       print('Example', example )
-       print('Run 2d Darcy example with known solution')
-    try:
-        time_loc = time.time()
-        known = np.array([0.0210718223032, 0.00526933885613])
-        answer = darcy_dualVEM_example1(visualize=visualize)
-        assert np.allclose( answer, known )
-
-        if verbose > 0:
-            print('Example', example, 'completed successfully')
-            print('Elapsed time ' + str(time.time() - time_loc))
-        success_counter += 1
-    except Exception as exp:
-        print('\n')
-        print(' ***** FAILURE ****')
-        print('Example', example, 'failed')
-        print(exp)
-        logging.error(traceback.format_exc())
-        failure_counter += 1
-
-
+    #################################
+    # Summary
+    #
+    print('\n')
+    print(' --- ')
+    print('Ran in total ' + str(success_counter + failure_counter) + ' tests,'
+          + ' out of which ' + str(failure_counter) + ' failed.')
+    print('Total elapsed time is ' + str(time.time() - time_tot) + ' seconds')
+    print('\n')
