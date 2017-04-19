4bbca5ab50e799d8310fa828f99bafae3ef3a7cd
Runar <runar.berge@uib.no>
2017-04-19 11:34:43 +0200

    updated split_grid unit tests

--- tests/test_split_grid.py
+++ tests/test_split_grid.py
@@ -6,17 +6,46 @@ from gridding.fractured import meshing
 
 class TestMeshing(unittest.TestCase):
     def test_x_intersection_3d(self):
-        """ Check that no error messages are created in the process of creating a
-        split_fracture.
+        """
+        Create a x-intersection in 3D
         """
 
-        f_1 = np.array([[-.8, .8, .8, -.8], [0, 0, 0, 0], [-.8, -.8, .8, .8]])
-        f_2 = np.array([[0, 0, 0, 0], [-.8, .8, .8, -.8], [-.8, -.8, .8, .8]])
-
+        f_1 = np.array([[1, 3, 3, 1], [1, 1, 1, 1], [1, 1, 3, 3]])
+        f_2 = np.array([[1, 1, 1, 1], [1, 3, 3, 1], [1, 1, 3, 3]])
         f_set = [f_1, f_2]
-        domain = {'xmin': -1, 'xmax': 1,
-                  'ymin': -1, 'ymax': 1, 'zmin': -1, 'zmax': 1}
-        # ENDRE DENNE
-        path_to_gmsh = '~/gmsh/bin/gmsh'
-        bucket = meshing.simplex_grid(f_set, domain, gmsh_path=path_to_gmsh)
+
+        bucket = meshing.cart_grid(f_set, [3, 3, 3])
         bucket.compute_geometry()
+
+        g_3 = bucket.grids_of_dimension(3)
+        g_2 = bucket.grids_of_dimension(2)
+        g_1 = bucket.grids_of_dimension(1)
+        g_0 = bucket.grids_of_dimension(0)
+
+        assert len(g_3) == 1
+        assert len(g_2) == 2
+        assert len(g_1) == 1
+        assert len(g_0) == 0
+
+    def test_tripple_intersection_3d(self):
+        """
+        Create a x-intersection in 3D
+        """
+
+        f_1 = np.array([[1, 3, 3, 1], [1, 1, 1, 1], [1, 1, 3, 3]])
+        f_2 = np.array([[1, 1, 1, 1], [1, 3, 3, 1], [1, 1, 3, 3]])
+        f_3 = np.array([[1, 3, 3, 1], [1, 1, 3, 3], [1, 1, 1, 1]])
+        f_set = [f_1, f_2, f_3]
+
+        bucket = meshing.cart_grid(f_set, [3, 3, 3])
+        bucket.compute_geometry()
+
+        g_3 = bucket.grids_of_dimension(3)
+        g_2 = bucket.grids_of_dimension(2)
+        g_1 = bucket.grids_of_dimension(1)
+        g_0 = bucket.grids_of_dimension(0)
+
+        assert len(g_3) == 1
+        assert len(g_2) == 3
+        assert len(g_1) == 3
+        assert len(g_0) == 1
