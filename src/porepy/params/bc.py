a748fc7d0f2ad3b256c49dd79494c1affa410c4a
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2017-05-15 09:33:51 +0200

    Improved comments in module for boundary conditions.

--- src/porepy/params/bc.py
+++ src/porepy/params/bc.py
@@ -1,29 +1,51 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 29 14:30:22 2016
+""" Module for representation and handling of boundary conditions.
 
-@author: eke001
 """
-
 import numpy as np
 
 
 class BoundaryCondition(object):
     """ Class to store information on boundary conditions.
 
-        The BCs are specified by face number, and can have type Dirichlet or
-        Neumann (Robin may be included later).
-        The face-based indexing makes this natural for control volume methods.
-        For vector equations, it is ismplicitly assumed that the
+    The BCs are specified by face number, and can have type Dirichlet or
+    Neumann (Robin may be included later). For details on default values etc.,
+    see constructor.
+
+    Attributes:
+        num_faces (int): Number of faces in the grid
+        dim (int): Dimension of the boundary. One less than the dimension of
+            the grid.
+        is_neu (np.ndarray boolean, size g.num_faces): Element i is true if
+            face i has been assigned a Neumann condition. Tacitly assumes that
+            the face is on the boundary. Should be false for internal faces, as
+            well as Dirichlet faces.
+        is_dir (np.ndarary, boolean, size g.num_faces): Element i is true if
+            face i has been assigned a Neumann condition.
 
     """
 
-    """ Initialize boundary conditions
-
-        Note: By default, all boundaries
-
-    """
     def __init__(self, g, faces=None, cond=None):
+        """Constructor for BoundaryConditions.
+
+        The conditions are specified by face numbers. Faces that do not get an
+        explicit condition will have Neumann conditions assigned.
+
+        Parameters:
+            g (grid): For which boundary conditions are set.
+            faces (np.ndarray): Faces for which conditions are assigned.
+            cond (list of str): Conditions on the faces, in the same order as
+                used in faces. Should be as long as faces.
+
+        Example:
+            # Assign Dirichlet condititons on the left side of a grid; implicit
+            # Neumann conditions on the rest
+            g = CartGrid([2, 2])
+            west_face = bc.face_on_side(g, 'west')
+            bound_cond = BoundaryCondition(g, faces=west_face, cond=['dir',
+                                                                     'dir'])
+
+        """
 
         self.num_faces = g.num_faces
         self.dim = g.dim - 1
@@ -34,6 +56,7 @@ class BoundaryCondition(object):
         self.is_neu = np.zeros(self.num_faces, dtype=bool)
         self.is_dir = np.zeros(self.num_faces, dtype=bool)
 
+        # By default, all faces are Neumann.
         self.is_neu[bf] = True
 
         if faces is not None:
