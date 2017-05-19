b1a9228312a5303a2cff48ff3fbd9541e81b2a01
IvarStefansson <ivarstef@gmail.com>
2017-05-19 14:15:14 +0200

    Added the static condensation.

---
+++ src/porepy/numerics/mixed_dim/condensation.py
@@ -0,0 +1,102 @@
+import numpy as np
+import scipy.sparse as sps
+import copy
+
+from porepy.grids import grid_bucket
+
+
+def solve_static_condensation(A, rhs, gb, dim=0, condensation_inverter=sps.linalg.inv, system_inverter=sps.linalg.spsolve):
+
+    to_be_eliminated = dofs_of_dimension(gb, A, dim)
+
+    a_reduced, rhs_reduced, Condensation_matrix, original_to_kept_dofs = eliminate_dofs(
+        A, rhs, to_be_eliminated, condensation_inverter)
+
+    eliminated_dofs = np.nonzero(to_be_eliminated)[0]
+
+    x_reduced = system_inverter(a_reduced, rhs_reduced)
+
+    x = np.zeros(A.shape[0])
+    x[original_to_kept_dofs] = x_reduced
+    x[to_be_eliminated] = Condensation_matrix * x_reduced
+
+    return x, x_reduced, original_to_kept_dofs, eliminated_dofs
+
+
+def dofs_of_dimension(gb, A, dim=0):
+    """
+    Extracts the global dof numbers corresponding to a given dimension.
+    Returns a boolean mask extracting the DOFs to be eliminated.
+    """
+    original_ndof = A.shape[1]  # bytt ut med info fra gb
+    dofs = np.empty(gb.size(), dtype=int)
+    for _, d in gb:
+        dofs[d['node_number']] = d['dof']
+    dofs = np.r_[0, np.cumsum(dofs)]
+
+    to_be_eliminated = np.zeros(original_ndof, dtype=bool)
+
+    for g, d in gb:
+
+        i = d['node_number']
+        if g.dim == dim:
+            to_be_eliminated[slice(dofs[i], dofs[i + 1])
+                             ] = np.ones(d['dof'], dtype=bool)
+
+    return to_be_eliminated
+
+
+def eliminate_dofs(A, rhs, to_be_eliminated, inverter=sps.linalg.inv):
+    """
+    Splits the system matrix A into four blocks according to which dofs
+    are to be eliminated (the "slaves"). The right hand side is split 
+    into two parts. For the computation of the blocks, the diagonal 
+    part corresponding to the slaves has to be inverted, hence the option
+    to choose inverter. This system will usually be quite small.
+
+    Input:
+    A, rhs: original matrix and right hand side of the problem to be
+        solved.
+    to_be_eliminated: boolean mask specifying which degrees of freedom 
+        should be eliminated from the system.
+    Returns:
+    A_reduced: The system matrix for the reduced system, i.e., corresponding 
+        to the master dofs only.
+    rhs_reduced: The right hand side for the reduced system.
+    Condensation_matrix: The matrix used for back-computation of the 
+        unknowns of the slaves once the reduced system has been solved.
+    to_be_kept: Indices of the masters.
+    """
+    to_be_kept = np.invert(to_be_eliminated)
+    # Get indexes of the masters:
+    to_be_kept = np.nonzero(to_be_kept)[0]
+    # and slaves:
+    to_be_eliminated = np.nonzero(to_be_eliminated)[0]
+
+    # Masters and slaves:
+    A_mm = A[to_be_kept, :]
+    A_mm = A_mm[:, to_be_kept]
+    A_ms = A[:, to_be_eliminated]
+
+    A_ms = A_ms[to_be_kept, :]
+
+    A_sm = A[:, to_be_kept]
+
+    A_sm = A_sm[to_be_eliminated, :]
+    A_ss = A[:, to_be_eliminated]
+    A_ss = A_ss[to_be_eliminated, :]
+    A_ss_inv = inverter(A_ss)
+
+    A_ms_A_ss_inv = A_ms * A_ss_inv
+    # Needed for broadcasting (?):
+    if A_ss.size == 1:
+        A_ms_A_ss_inv = A_ms_A_ss_inv[:, np.newaxis]
+
+    sparse_product = sps.csr_matrix(A_ms_A_ss_inv * A_sm)
+
+    A_reduced = A_mm - sparse_product
+    rhs_reduced = rhs[to_be_kept][:, np.newaxis] - \
+        A_ms_A_ss_inv * rhs[to_be_eliminated]
+    Condensation_matrix = sps.csr_matrix(- A_ss_inv * A_sm)
+
+    return A_reduced, rhs_reduced, Condensation_matrix, to_be_kept
