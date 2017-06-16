751011b1a6757eb8c309abd27038046fcb67889a
Runar <runar.berge@uib.no>
2017-06-16 13:55:29 +0200

    Added mapping from left to right side of fractures.

--- test/integration/test_split_grid.py
+++ test/integration/test_split_grid.py
@@ -5,9 +5,9 @@ from porepy.fracs import meshing
 
 
 class TestMeshing(unittest.TestCase):
-    def test_x_intersection_3d(self):
+    def test_L_intersection_3d(self):
         """
-        Create a x-intersection in 3D
+        Create a L-intersection in 3D
         """
 
         f_1 = np.array([[1, 3, 3, 1], [1, 1, 1, 1], [1, 1, 3, 3]])
@@ -26,10 +26,54 @@ class TestMeshing(unittest.TestCase):
         assert len(g_2) == 2
         assert len(g_1) == 1
         assert len(g_0) == 0
+        g_all = g_3 + g_2 + g_1 + g_0
+        for g in g_all:
+            f_p = g.frac_pairs
+            if g.dim == 3:
+                f_p_shape_true = 8
+            else:
+                f_p_shape_true = 0
+            assert f_p.shape[1] == f_p_shape_true
+            assert np.allclose(
+                g.face_centers[:, f_p[0]], g.face_centers[:, f_p[1]])
+
+    def test_X_intersection_3d(self):
+        """
+        Create a x-intersection in 3D
+        """
+
+        f_1 = np.array([[0, 2, 2, 0], [1, 1, 1, 1], [0, 0, 2, 2]])
+        f_2 = np.array([[1, 1, 1, 1], [0, 2, 2, 0], [0, 0, 2, 2]])
+        f_set = [f_1, f_2]
+
+        bucket = meshing.cart_grid(f_set, [2, 2, 2])
+        bucket.compute_geometry()
+
+        g_3 = bucket.grids_of_dimension(3)
+        g_2 = bucket.grids_of_dimension(2)
+        g_1 = bucket.grids_of_dimension(1)
+        g_0 = bucket.grids_of_dimension(0)
+
+        assert len(g_3) == 1
+        assert len(g_2) == 2
+        assert len(g_1) == 1
+        assert len(g_0) == 0
+        g_all = g_3 + g_2 + g_1 + g_0
+        for g in g_all:
+            f_p = g.frac_pairs
+            if g.dim == 3:
+                f_p_shape_true = 8
+            elif g.dim == 2:
+                f_p_shape_true = 2
+            else:
+                f_p_shape_true = 0
+            assert f_p.shape[1] == f_p_shape_true
+            assert np.allclose(
+                g.face_centers[:, f_p[0]], g.face_centers[:, f_p[1]])
 
     def test_tripple_intersection_3d(self):
         """
-        Create a x-intersection in 3D
+        Create a tripple L-intersection in 3D
         """
 
         f_1 = np.array([[1, 3, 3, 1], [1, 1, 1, 1], [1, 1, 3, 3]])
@@ -49,28 +93,13 @@ class TestMeshing(unittest.TestCase):
         assert len(g_2) == 3
         assert len(g_1) == 3
         assert len(g_0) == 1
-
-    def test_L_intersection_2d(self):
-        """
-        Create a L-intersection in 2D
-        """
-
-        f_1 = np.array([[.2, 0.8],
-                        [0.5, 0.5]])
-        f_2 = np.array([[0.2, 0.2],
-                        [0.1, 0.5]])
-
-        f_set = [f_1, f_2]
-        box = {'xmin': 0, 'ymin': 0, 'xmax': 1, 'ymax': 1}
-        bucket = meshing.cart_grid(f_set, [10, 10], physdims=[1, 1])
-        bucket.compute_geometry()
-
-        g_3 = bucket.grids_of_dimension(3)
-        g_2 = bucket.grids_of_dimension(2)
-        g_1 = bucket.grids_of_dimension(1)
-        g_0 = bucket.grids_of_dimension(0)
-
-        assert len(g_3) == 0
-        assert len(g_2) == 1
-        assert len(g_1) == 2
-        assert len(g_0) == 1
+        g_all = g_3 + g_2 + g_1 + g_0
+        for g in g_all:
+            f_p = g.frac_pairs
+            if g.dim == 3:
+                f_p_shape_true = 12
+            else:
+                f_p_shape_true = 0
+            assert f_p.shape[1] == f_p_shape_true
+            assert np.allclose(
+                g.face_centers[:, f_p[0]], g.face_centers[:, f_p[1]])
