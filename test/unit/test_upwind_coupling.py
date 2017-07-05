e70846d799c8de1785c7af2382b31d1b0b0c7a1d
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2017-07-05 14:04:03 +0200

    add two more test for the upwind coupling

--- test/unit/test_upwind_coupling.py
+++ test/unit/test_upwind_coupling.py
@@ -420,6 +420,117 @@ class BasicsTest( unittest.TestCase ):
 
 #------------------------------------------------------------------------------#
 
+    def test_upwind_2d_beta_positive(self):
+
+        f = np.array([[2, 2],
+                      [0, 2]])
+        gb = meshing.cart_grid([f], [4, 2])
+        gb.assign_node_ordering()
+        gb.compute_geometry()
+
+        solver = upwind.Upwind()
+
+        gb.add_node_props(['param'])
+
+        for g, d in gb:
+            param = Parameters(g)
+            aperture = np.ones(g.num_cells)*np.power(1e-2, gb.dim_max() - g.dim)
+            param.set_aperture(aperture)
+            param.set_discharge(solver.discharge(g, [2, 0, 0], aperture))
+
+            bf = g.get_boundary_faces()
+            bc = BoundaryCondition(g, bf, bf.size * ['neu'])
+            param.set_bc(solver, bc)
+            d['param'] = param
+
+        # Assign coupling discharge
+        gb.add_edge_prop('param')
+        for e, d in gb.edges_props():
+            g_h = gb.sorted_nodes_of_edge(e)[1]
+            discharge = gb.node_prop(g_h, 'param').get_discharge()
+            d['param'] = Parameters(g_h)
+            d['param'].set_discharge(discharge)
+
+        coupling = upwind_coupling.UpwindCoupling(solver)
+        solver_coupler = coupler.Coupler(solver, coupling)
+        M = solver_coupler.matrix_rhs(gb)[0].todense()
+
+        M_known = np.array([[ 2,  0,  0,  0,  0,  0,  0,  0,  0,  0.],
+                            [-2,  2,  0,  0,  0,  0,  0,  0,  0,  0.],
+                            [ 0,  0,  2,  0,  0,  0,  0,  0,  0, -2.],
+                            [ 0,  0, -2,  0,  0,  0,  0,  0,  0,  0.],
+                            [ 0,  0,  0,  0,  2,  0,  0,  0,  0,  0.],
+                            [ 0,  0,  0,  0, -2,  2,  0,  0,  0,  0.],
+                            [ 0,  0,  0,  0,  0,  0,  2,  0, -2,  0.],
+                            [ 0,  0,  0,  0,  0,  0, -2,  0,  0,  0.],
+                            [ 0,  0,  0,  0,  0, -2,  0,  0,  2,  0.],
+                            [ 0, -2,  0,  0,  0,  0,  0,  0,  0,  2.]])
+
+        rtol = 1e-15
+        atol = rtol
+        assert np.allclose(M, M_known, rtol, atol)
+
+#------------------------------------------------------------------------------#
+
+    def test_upwind_2d_full_beta_bc_dir(self):
+
+        f = np.array([[2, 2], [0, 2]])
+        gb = meshing.cart_grid([f], [4, 2])
+        gb.assign_node_ordering()
+        gb.compute_geometry()
+
+        solver = upwind.Upwind()
+        gb.add_node_props(['param'])
+
+        for g, d in gb:
+            param = Parameters(g)
+
+            aperture = np.ones(g.num_cells)*np.power(1e-1, gb.dim_max() - g.dim)
+            param.set_aperture(aperture)
+            param.set_discharge(solver.discharge(g, [1, 1, 0], aperture))
+
+            bound_faces = g.get_domain_boundary_faces()
+            labels = np.array(['dir'] * bound_faces.size)
+            bc_val = np.zeros(g.num_faces)
+            bc_val[bound_faces] = 3
+
+            param.set_bc(solver, BoundaryCondition(g, bound_faces, labels))
+            param.set_bc_val(solver, bc_val)
+
+            d['param'] = param
+
+        # Assign coupling discharge
+        gb.add_edge_prop('param')
+        for e, d in gb.edges_props():
+            g_h = gb.sorted_nodes_of_edge(e)[1]
+            discharge = gb.node_prop(g_h, 'param').get_discharge()
+            d['param'] = Parameters(g_h)
+            d['param'].set_discharge(discharge)
+
+        coupling = upwind_coupling.UpwindCoupling(solver)
+        solver_coupler = coupler.Coupler(solver, coupling)
+        M, rhs = solver_coupler.matrix_rhs(gb)
+
+        M_known = np.array([[ 2,  0,  0,  0,  0,  0,  0,  0,  0,  0.],
+                            [-1,  2,  0,  0,  0,  0,  0,  0,  0,  0.],
+                            [ 0,  0,  2,  0,  0,  0,  0,  0,  0, -1.],
+                            [ 0,  0, -1,  2,  0,  0,  0,  0,  0,  0.],
+                            [-1,  0,  0,  0,  2,  0,  0,  0,  0,  0.],
+                            [ 0, -1,  0,  0, -1,  2,  0,  0,  0,  0.],
+                            [ 0,  0, -1,  0,  0,  0,  2,  0, -1,  0.],
+                            [ 0,  0,  0, -1,  0,  0, -1,  2,  0,  0.],
+                            [ 0,  0,  0,  0,  0, -1,  0,  0,1.1,-0.1],
+                            [ 0, -1,  0,  0,  0,  0,  0,  0,  0, 1.1]])
+
+        rhs_known = np.array([6, 3, 3, 3, 3, 0, 0, 0, 0, 0.3])
+
+        rtol = 1e-15
+        atol = rtol
+        assert np.allclose(M.todense(), M_known, rtol, atol)
+        assert np.allclose(rhs, rhs_known, rtol, atol)
+
+#------------------------------------------------------------------------------#
+
 def matrix_rhs_for_test_upwind_coupling_3d_2d_1d_0d():
     U = np.array([\
         [  2.50e-01,   0.00e+00,   0.00e+00,   0.00e+00,   0.00e+00,
