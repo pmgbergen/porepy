55dcd37e166fc00ec8f500abdb5e0ec4e29ec05a
IvarStefansson <ivarstef@gmail.com>
2017-07-11 19:02:33 +0200

    Updated the fv benchmark 1 to param

--- examples/example1/benchmark1/fv_blocking.py
+++ examples/example1/benchmark1/fv_blocking.py
@@ -1,41 +1,46 @@
+"""
+This example contains the set up and computation of the blocking version of the
+benchmark1 with the fv discretizations. Note the choice between tpfa and mpfa in
+line 78. 
+"""
 import numpy as np
 import scipy.sparse as sps
 
 from porepy.viz import plot_grid, exporter
 from porepy.fracs import importer
-
-from porepy.params import bc, second_order_tensor
-
-
+from porepy.params import bc, tensor
+from porepy.params.data import Parameters
 from porepy.numerics.mixed_dim import coupler
 from porepy.numerics.fv import tpfa, mpfa, tpfa_coupling
-
 from porepy.utils.errors import error
 
 #------------------------------------------------------------------------------#
 
-def add_data(gb, domain):
+def add_data(gb, domain, mesh_value):
     """
-    Define the permeability, apertures, boundary conditions
+    Define the permeability, apertures, boundary conditions and sources
     """
-    gb.add_node_props(['k', 'f', 'bc', 'bc_val', 'a'])
+    gb.add_node_props(['param'])
     kf = 1e-4
     tol = 1e-5
     a = 1e-4
 
     for g, d in gb:
-        # Permeability
-        kxx = np.ones(g.num_cells)
-        if g.dim == 2:
-            d['k'] = second_order_tensor.SecondOrderTensor(g.dim, kxx)
-        else:
-            d['k'] = second_order_tensor.SecondOrderTensor(2, kf*kxx)
-
-        # Source term
-        d['f'] = np.zeros(g.num_cells)
+        param = Parameters(g)
 
         # Assign apertures
-        d['a'] = np.ones(g.num_cells) * np.power(a, 2 - g.dim)
+        a_dim = np.power(a, gb.dim_max() - g.dim)
+        aperture = np.ones(g.num_cells)*a_dim
+        param.set_aperture(aperture)
+        
+        # Permeability
+        k_frac = np.power(kf, g.dim<gb.dim_max())
+        p = tensor.SecondOrder(3,np.ones(g.num_cells) * k_frac)
+        param.set_tensor('flow', p)
+        param.set_tensor('flow', p)
+                
+        # Source term
+        param.set_source('flow', np.zeros(g.num_cells))
 
         # Boundaries
         bound_faces = g.get_boundary_faces()
@@ -43,8 +48,7 @@ def add_data(gb, domain):
             continue
 
         bound_face_centers = g.face_centers[:, bound_faces]
-
-        
+       
         left = bound_face_centers[0, :] < domain['xmin'] + tol
         right = bound_face_centers[0, :] > domain['xmax'] - tol
 
@@ -52,48 +56,61 @@ def add_data(gb, domain):
         labels[right] = 'dir'
 
         bc_val = np.zeros(g.num_faces)
+        
         if g.dim == 2:
-            b_mesh_value = 0.045
+            # Account for the double inflow on the matrix-fracture overlap
             left_mid = np.array(np.absolute(g.face_centers[1, bound_faces[left]]
-                                - 0.5) < b_mesh_value)
-            bc_val[bound_faces[left]] = g.face_areas[bound_faces[left]] - left_mid * .5*a
+                                            - 0.5) < mesh_value)
+            bc_val[bound_faces[left]] = -g.face_areas[bound_faces[left]] \
+                                        + left_mid * .5*a
         else:
-            bc_val[bound_faces[left]] = g.face_areas[bound_faces[left]] * a
+            bc_val[bound_faces[left]] = -g.face_areas[bound_faces[left]] * a
 
         bc_val[bound_faces[right]] = np.ones(np.sum(right))
+                               
+        param.set_bc('flow', bc.BoundaryCondition(g, bound_faces, labels))
+        param.set_bc_val('flow', bc_val)
 
-        d['bc'] = bc.BoundaryCondition(g, bound_faces, labels)
-        d['bc_val'] = bc_val.ravel('F')
-
-    
+        d['param'] = param
 
 #------------------------------------------------------------------------------#
 
-mesh_kwargs = {}
-mesh_kwargs['mesh_size'] = {'mode': 'constant',
-                            'value': 0.045, 'bound_value': 0.045}
+multi_point = False
 
+# Define the geometry and produce the meshes
+mesh_kwargs = {}
+mesh_size = 0.045
+mesh_kwargs['mesh_size'] = {'mode': 'constant',
+                            'value': mesh_size, 'bound_value': mesh_size}
 domain = {'xmin': 0, 'xmax': 1, 'ymin': 0, 'ymax': 1}
 gb = importer.from_csv('network.csv', mesh_kwargs, domain)
 gb.compute_geometry()
 gb.assign_node_ordering()
 
 # Assign parameters
-add_data(gb, domain)
+add_data(gb, domain, mesh_size)
 
-# Choose and define the solvers and coupler
-solver = tpfa.Tpfa()
-#solver = mpfa.Mpfa()
+# Choose discretizationand define the solver
+if multi_point:
+    solver = mpfa.Mpfa()
+    l2_norm = 35.7487817896
+else:
+    solver = tpfa.Tpfa()
+    l2_norm = 36.1027322839
+
+# Set coupling conditions and discretize
 coupling_conditions = tpfa_coupling.TpfaCoupling(solver)
 solver_coupler = coupler.Coupler(solver, coupling_conditions)
 A, b = solver_coupler.matrix_rhs(gb)
 
+# Solve the linear system
 p = sps.linalg.spsolve(A, b)
 
+# Store the solution
 gb.add_node_props(["p"])
 solver_coupler.split(gb, "p", p)
 exporter.export_vtk(gb, 'fv', ["p"], folder='fv_blocking')
 
 # Consistency check
-assert np.isclose(np.sum(error.norm_L2(g, d['p']) for g, d in gb), 36.1027322839)
+assert np.isclose(np.sum(error.norm_L2(g, d['p']) for g, d in gb), l2_norm)
 
