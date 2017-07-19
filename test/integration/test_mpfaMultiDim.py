18b9d2ac34adbe2cd0b8d820caf5146d3b488fdf
Runar <runar.berge@uib.no>
2017-07-19 14:19:51 +0200

    Updated MassMatrix to use parameter class

---
+++ test/integration/test_mpfaMultiDim.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+from porepy.numerics.fv.mpfa import MpfaMultiDim
+from porepy.fracs import meshing
+from porepy.params.data import Parameters
+from porepy.params import tensor, bc
+
+
+def setup_cart_2d(nx):
+    frac1 = np.array([[0.2, 0.8], [0.5, 0.5]])
+    frac2 = np.array([[0.5, 0.5], [0.8, 0.2]])
+    fracs = [frac1, frac2]
+    gb = meshing.cart_grid(fracs, nx, physdims=[1, 1])
+    gb.compute_geometry()
+    gb.assign_node_ordering()
+    gb.add_node_props(['param'])
+    for g, d in gb:
+        kxx = np.ones(g.num_cells)
+        perm = tensor.SecondOrder(gb.dim_max(), kxx)
+        a = 0.01 / np.max(nx)
+        a = np.power(a, gb.dim_max() - g.dim)
+        param = Parameters(g)
+        param.set_tensor('flow', perm)
+        param.set_aperture(a)
+        if g.dim == 2:
+            bound_faces = g.get_boundary_faces()
+            bound = bc.BoundaryCondition(g, bound_faces.ravel('F'),
+                                         ['dir'] * bound_faces.size)
+            bc_val = g.face_centers[1]
+            param.set_bc('flow', bound)
+            param.set_bc_val('flow', bc_val)
+
+        d['param'] = param
+    return gb
+
+
+def test_uniform_flow_cart_2d():
+    # Structured Cartesian grid
+    gb = setup_cart_2d(np.array([10, 10]))
+
+    # Python inverter is most efficient for small problems
+    flux_discr = MpfaMultiDim('flow')
+    A, rhs = flux_discr.matrix_rhs(gb)
+    p = np.linalg.solve(A.A, rhs)
+
+    flux_discr.solver.split(gb, 'pressure', p)
+    for g, d in gb:
+        pressure = d['pressure']
+        pressure_analytic = g.cell_centers[1]
+        p_diff = pressure - pressure_analytic
+        assert np.max(np.abs(p_diff)) < 0.05
---
+++ test/unit/test_mass_matrix.py
@@ -0,0 +1,40 @@
+import numpy as np
+import unittest
+
+
+from porepy.numerics.fv.mass_matrix import MassMatrix, InvMassMatrix
+from porepy.grids import structured
+from porepy.params.data import Parameters
+
+
+class MassMatrixTest(unittest.TestCase):
+
+    def test_mass_matrix(self):
+        g = structured.CartGrid([3, 3, 3])
+        g.compute_geometry()
+        phi = np.random.rand(g.num_cells)
+        dt = 0.2
+        param = Parameters(g)
+        param.set_porosity(phi)
+        data = {'param': param, 'deltaT': dt}
+        time_discr = MassMatrix()
+        lhs, rhs = time_discr.matrix_rhs(g, data)
+        assert np.allclose(rhs, 0)
+        assert np.allclose(lhs.diagonal(), g.cell_volumes * phi / dt)
+        off_diag = np.where(~np.eye(lhs.shape[0], dtype=bool))
+        assert np.allclose(lhs.A[off_diag], 0)
+
+    def test_inv_mass_matrix(self):
+        g = structured.CartGrid([3, 3, 3])
+        g.compute_geometry()
+        phi = np.random.rand(g.num_cells)
+        dt = 0.2
+        param = Parameters(g)
+        param.set_porosity(phi)
+        data = {'param': param, 'deltaT': dt}
+        time_discr = InvMassMatrix()
+        lhs, rhs = time_discr.matrix_rhs(g, data)
+        assert np.allclose(rhs, 0)
+        assert np.allclose(lhs.diagonal(), dt / (g.cell_volumes * phi))
+        off_diag = np.where(~np.eye(lhs.shape[0], dtype=bool))
+        assert np.allclose(lhs.A[off_diag], 0)
