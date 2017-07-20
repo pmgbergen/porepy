7ca8ae23bd272fbcffe481b60b4584f928e66639
Runar <runar.berge@uib.no>
2017-07-20 13:27:53 +0200

    Added compressible flow for fractured domain

--- src/porepy/numerics/compressible/problems.py
+++ src/porepy/numerics/compressible/problems.py
@@ -4,6 +4,7 @@ import scipy.sparse as sps
 from porepy.grids import structured
 from porepy.numerics.fv import tpfa, mass_matrix
 from porepy.numerics.compressible import solvers
+from porepy.numerics.mixed_dim.coupler import Coupler
 from porepy.params.data import Parameters
 from porepy.params import tensor
 from porepy.params import bc
@@ -114,13 +115,144 @@ class SlightlyCompressible():
     def end_time(self):
         return 1.0
 
-    def save_results(self):
+    def save(self):
         pressures = self.data['pressure']
         times = np.array(self.data['times'])
         folder = self.parameters['folder_name']
         f_name = self.parameters['file_name']
         for i, p in enumerate(pressures):
             data_to_plot = {'pressure': p}
+            export_vtk(
+                self.grid(), f_name, data_to_plot, time_step=i, folder=folder)
+
+        export_pvd(
+            self.grid(), self.parameters['file_name'], times, folder=folder)
+
+
+###############################################################################
+
+
+class SubProblem(SlightlyCompressible):
+    def __init__(self, g):
+        self.g = g
+        SlightlyCompressible.__init__(self)
+
+    def grid(self):
+        return self.g
+
+
+class TpfaComp(tpfa.Tpfa):
+    def __init__(self, physics='flow'):
+        tpfa.Tpfa.__init__(self, physics)
+
+    def matrix_rhs(self, g, data):
+        param = data['problem'].data()
+        return tpfa.Tpfa.matrix_rhs(self, g, param)
+
+
+class TpfaCompCoupling(tpfa.TpfaCoupling):
+    def __init__(self, solver):
+        tpfa.TpfaCoupling.__init__(self, solver)
+
+    def matrix_rhs(self, g_h, g_l, data_h, data_l, data_edge):
+        param_h = data_h['problem'].data()
+        param_l = data_l['problem'].data()
+        return tpfa.TpfaCoupling.matrix_rhs(
+            self, g_h, g_l, param_h, param_l, data_edge)
+
+
+class SlightlyCompressibleMultiDim():
+    """
+    Base-class for slightly compressible flow. Initialize all needed
+    attributes for a slightly compressible solver.
+    """
+
+    def __init__(self):
+        self._data = dict()
+        self.set_sub_problems()
+        self.solver = solvers.Implicit(self)
+        self.solver.parameters['store_results'] = True
+        self.parameters = {'file_name': 'pressure'}
+        self.parameters['folder_name'] = 'results'
+
+    #---------Discretization---------------
+
+    def flux_disc(self):
+        """
+        Returns the flux discretization.
+        """
+        discr = TpfaComp()
+        coupling_conditions = TpfaCompCoupling(discr)
+        return Coupler(discr, coupling_conditions)
+
+    def time_disc(self):
+        """
+        Returns the flux discretization.
+        """
+        class TimeDisc(mass_matrix.MassMatrix):
+            def __init__(self, deltaT):
+                self.deltaT = deltaT
+
+            def matrix_rhs(self, g, data):
+                sub_d = data['problem'].data()
+                sub_d['deltaT'] = self.deltaT
+                lhs, rhs = mass_matrix.MassMatrix.matrix_rhs(self, g, sub_d)
+                return lhs * sub_d['compressibility'], rhs * sub_d['compressibility']
+        single_dim_discr = TimeDisc(self.time_step())
+        multi_dim_discr = Coupler(single_dim_discr)
+        return multi_dim_discr
+
+    def solve(self):
+        """
+        Call the solver
+        """
+        self.data = self.solver.solve()
+        return self.data
+
+    def update(self, t):
+        for _, d in self.grid():
+            d['problem'].update(t)
+
+    #-----Data-----------------
+
+    def data(self):
+        return self._data
+
+    def set_sub_problems(self):
+        self.grid().add_node_props(['problem'])
+        for g, d in self.grid():
+            d['problem'] = SubProblem(g)
+
+    #--------Initial conditions---------------
+
+    def initial_pressure(self):
+        for _, d in self.grid():
+            d['pressure'] = d['problem'].initial_pressure()
+
+        global_pressure = self.time_disc().merge(self.grid(), 'pressure')
+        return global_pressure
+
+    #---------Overloaded Functions-----------------
+
+    def grid(self):
+        raise NotImplementedError('subclass must overload function grid()')
+
+    #--------Time stepping------------
+
+    def time_step(self):
+        return 1.0
+
+    def end_time(self):
+        return 1.0
+
+    def save(self):
+        pressures = self.data['pressure']
+        times = np.array(self.data['times'])
+        folder = self.parameters['folder_name']
+        f_name = self.parameters['file_name']
+        for i, p in enumerate(pressures):
+            self.time_disc().split(self.grid(), 'pressure', p)
+            data_to_plot = ['pressure']
             export_vtk(
                 self.grid(), f_name, data_to_plot, time_step=i, folder=folder)
 
