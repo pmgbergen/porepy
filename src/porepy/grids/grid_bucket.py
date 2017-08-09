Fixes after extracting the flux computation from grid_bucket.py
Change in the coupling_flux for grids of equal dimension
Added the possibility to avoid overwriting existing node orderings

src/porepy/grids/grid_bucket.py
@@ -560,7 +560,7 @@ class GridBucket(object):
 
 #------------------------------------------------------------------------------#
 
-    def assign_node_ordering(self):
+    def assign_node_ordering(self, overwrite_existing=True):
         """
         Assign an ordering of the nodes in the graph, stored as the attribute
         'node_number'.
@@ -573,8 +573,9 @@ class GridBucket(object):
         can in principle change between two calls to this function.
 
         If an ordering covering all nodes in the graph already exist, but does
-        not coincide with the new ordering, a warning is issued.
-
+        not coincide with the new ordering, a warning is issued. If the optional
+        parameter overwrite_existing is set to False, no update is performed if
+        an node ordering already exists.
         """
 
         # Check whether 'node_number' is defined for the grids already.
@@ -582,7 +583,9 @@ class GridBucket(object):
         for _, n in self:
             if not 'node_number' in n.keys():
                 ordering_exists = False
-
+        if ordering_exists and not overwrite_existing:
+            return
+            
         counter = 0
         # Loop over grids in decreasing dimensions
         for dim in range(self.dim_max(), self.dim_min() - 1, -1):
@@ -595,6 +598,7 @@ class GridBucket(object):
                 # Assign new value
                 n['node_number'] = counter
                 counter += 1
+                
 #------------------------------------------------------------------------------#
 
     def update_node_ordering(self, removed_number):
@@ -681,6 +685,12 @@ class GridBucket(object):
         gb_copy = self.copy()
         grids_of_dim = gb_copy.grids_of_dimension(dim)
         grids_of_dim_old = self.grids_of_dimension(dim)
+        # The node numbers are copied for each grid, so they can be used to
+        # make sure we use the same grids (g and g_old) below.
+        nn_new = gb_copy.nodes_prop(grids_of_dim, 'node_number')
+        nn_old = self.nodes_prop(grids_of_dim_old, 'node_number')
+        _, old_in_new = setmembership.ismember_rows(np.array(nn_new),
+                                                   np.array(nn_old), sort=False)
         neighbours_dict = {}
         neighbours_dict_old = {}
         eliminated_nodes =  {}
@@ -690,7 +700,7 @@ class GridBucket(object):
             # Keep track of which nodes were connected to each of the eliminated
             # nodes. Note that the key is the node number in the old gb, whereas
             # the neighbours lists refer to the copy grids.
-            g_old = grids_of_dim_old[i]
+            g_old = grids_of_dim_old[old_in_new[i]]
             neighbours_dict[i] = neighbours
             neighbours_dict_old[i] = self.node_neighbors(g_old)
             eliminated_nodes[i] = g_old
@@ -698,6 +708,7 @@ class GridBucket(object):
         elimination_data = {'neighbours':neighbours_dict,
                             'neighbours_old':neighbours_dict_old,
                             'eliminated_nodes':eliminated_nodes}
+        
         return gb_copy, elimination_data
 
 #------------------------------------------------------------------------------#

src/porepy/numerics/fv/fvutils.py
@@ -961,10 +961,10 @@ def compute_discharges(gb, physics='flow'):
         to the first of the sorted grids (gb.sorted_nodes_of_edge(e)).
     """
         
-    for gr, da in gb:
-        if gr.dim > 0:
-            pa = da['param']
-            dis = da['flux'] * da['p'] + da['bound_flux'] \
+    for g, d in gb:
+        if g.dim > 0:
+            pa = d['param']
+            dis = d['flux'] * d['p'] + d['bound_flux'] \
                                * pa.get_bc_val(physics)
             pa.set_discharge(dis)
 
@@ -991,16 +991,16 @@ def compute_discharges(gb, physics='flow'):
             # (cell-cells connections) in the sense that the normals are assumed
             # outward from g2, "pointing towards the g1 cells". Note that in
             # general, there are g2.num_cells x g1.num_cells connections/"faces".
-            coupling_flux = gb.edge_prop(e, 'coupling_flux')[0].todense()
-            pressures = gb.nodes_prop([g2, g1], 'p')
+            cc = data['face_cells']
+            cells_1, cells_2 = cc.nonzero()
+            coupling_flux = gb.edge_prop(e, 'coupling_flux')[0]
 
-            # The dense-sparse conversion may possibly be avoidable if einsum is
-            # removed
-            contribution_0 = np.einsum('ij,i->ij',
-                                       coupling_flux,pressures[0])
-            contribution_1 = np.einsum('ij,j->ij',
-                                       coupling_flux,pressures[1])
-            dis = contribution_0-contribution_1
+            pressures = gb.nodes_prop([g2, g1], 'p')
+            p2 = pressures[0][cells_2]
+            p1 = pressures[1][cells_1]
+            contribution_2 = np.multiply(coupling_flux[cc], p2)
+            contribution_1 = np.multiply(coupling_flux[cc], p1)
+            dis = contribution_2-contribution_1
             # Store flux at the edge only. This means that the flux will remain
             # zero in the data of both g1 and g2
-            pa.set_discharge(dis)
+            pa.set_discharge(np.ravel(dis))

src/porepy/numerics/fv/transport/upwind_coupling.py
@@ -48,21 +48,12 @@ class UpwindCoupling(AbstractCoupling):
             cells_l, cells_h = data_edge['face_cells'].nonzero()
             diag_cc11 = np.zeros(g_l.num_cells)
             diag_cc00 = np.zeros(g_h.num_cells)
-            # Need only one discharge, from h to l. Positive values
-            # go from h to l.
-            
-            
-            d_00 = (np.sign(discharge.clip(min=0)) * discharge).sum(axis=1)
-            d_11 = (np.sign(discharge.clip(max=0)) * discharge).sum(axis=0)
-            
-            
+            d_00 = np.bincount(cells_h, np.sign(discharge.clip(min=0)) * discharge,
+                               minlength=g_h.num_cells)
+            d_11 = np.bincount(cells_l, np.sign(discharge.clip(max=0)) * discharge,
+                               minlength=g_l.num_cells)
             np.add.at(diag_cc00, range(g_h.num_cells), d_00)
             np.add.at(diag_cc11, range(g_l.num_cells), d_11)
-            # Compute the outflow from the second to the first grid
-            cc[1, 0] = sps.coo_matrix(-discharge.clip(min=0).T)
-            # Compute the inflow to the first from the second grid
-            cc[0, 1] = sps.coo_matrix(discharge.clip(max=0))    
-
         else:
             # Recover the information for the grid-grid mapping
             cells_l, faces_h, _ = sps.find(data_edge['face_cells'])
@@ -81,18 +72,24 @@ class UpwindCoupling(AbstractCoupling):
 
             diag_cc00 = np.zeros(g_h.num_cells)
             np.add.at(diag_cc00, cells_h, np.sign(discharge.clip(min=0)) * discharge)
-            # Compute the outflow from the higher to the lower dimensional grid
-            cc[1, 0] = sps.coo_matrix((-discharge.clip(min=0), (cells_l, cells_h)),
+        # Compute the outflow from the higher to the lower dimensional grid
+        cc[1, 0] = sps.coo_matrix((-discharge.clip(min=0), (cells_l, cells_h)),
                                       shape=(dof[1], dof[0]))
 
-            # Compute the inflow from the higher to the lower dimensional grid
-            cc[0, 1] = sps.coo_matrix((discharge.clip(max=0), (cells_h, cells_l)),
+        # Compute the inflow from the higher to the lower dimensional grid
+        cc[0, 1] = sps.coo_matrix((discharge.clip(max=0), (cells_h, cells_l)),
                                       shape=(dof[0], dof[1]))
             
         cc[1, 1] = sps.dia_matrix((diag_cc11, 0), shape=(dof[1], dof[1]))
 
         cc[0, 0] = sps.dia_matrix((diag_cc00, 0), shape=(dof[0], dof[0]))
-       
+
+        
+        if data_h['node_number'] == data_l['node_number']:
+            # All contributions to be returned to the same block of the
+            # global matrix in this case
+            cc = np.array([np.sum(cc, axis=(0,1))])
+            
         return cc
 
 #------------------------------------------------------------------------------#
@@ -131,8 +128,7 @@ class UpwindCoupling(AbstractCoupling):
             # of face_cells (see grid_bucket.duplicate_without_dimension).
             phi_h = data_h['param'].get_porosity()
             cells_h, cells_l = data_edge['face_cells'].nonzero()
-            d1 = discharge[cells_h,cells_l]
-            not_zero = ~np.isclose(np.zeros(d1.shape), d1, atol = 0)
+            not_zero = ~np.isclose(np.zeros(discharge.shape), discharge, atol = 0)
             if not np.any(not_zero):
                 return np.Inf
             
@@ -146,7 +142,7 @@ class UpwindCoupling(AbstractCoupling):
             apt_l = aperture_l[cells_l]
             coeff = np.minimum(phi_h,phi_l)*np.minimum(apt_h,apt_l)
             
-            return np.amin(np.abs(np.divide(dist, d1)) * coeff)
+            return np.amin(np.abs(np.divide(dist, discharge)) * coeff)
         
         # Recover the information for the grid-grid mapping
         cells_l, faces_h, _ = sps.find(data_edge['face_cells'])

src/porepy/numerics/mixed_dim/condensation.py
@@ -198,7 +198,7 @@ def new_coupling_fluxes(gb_old, gb_el, neighbours_old, neighbours_el, node_old):
         # Check whether there is an internal hole in the grid. If so, add connections
         # between the cells on either side
         if not np.allclose(c_f, 0, 1e-10, 1e-12):
-            cell_cells = sps.csr_matrix(c_f>0)
+            cell_cells = np.array(c_f>0, dtype=bool)
             # The fluxes c_f*p go from cells_1 to cells_2:
             # c_1, c_2, _ = sparse.find(cell_cells)
             gb_el.add_edge([g_0, g_0], cell_cells)