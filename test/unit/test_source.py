3e267ccc30e09f1502129f617f04c6d1bc131e80
Runar <runar.berge@uib.no>
2017-10-18 14:16:08 +0200

    Added unit test for source discretization

---
+++ test/unit/test_source.py
@@ -0,0 +1,44 @@
+import unittest
+import scipy.sparse as sps
+import numpy as np
+
+from porepy.grids.structured import CartGrid
+from porepy.numerics.fv import source
+from porepy.params.data import Parameters
+
+
+class TestSource(unittest.TestCase):
+    def test_integral(self):
+        g, d = setup_3d_grid()
+        src_disc = source.Integral()
+        lhs, rhs = src_disc.matrix_rhs(g, d)
+
+        rhs_t = np.array([0, 0, 0, 0, 1, 0, 0, 0])
+        assert src_disc.ndof(g) == g.num_cells
+        assert np.all(rhs == rhs_t)
+        assert lhs.shape == (8, 8)
+        assert np.sum(lhs != 0) == 0
+
+    def test_density(self):
+        g, d = setup_3d_grid()
+        src_disc = source.Density()
+        lhs, rhs = src_disc.matrix_rhs(g, d)
+
+        rhs_t = np.array([0, 0, 0, 0, 0.5**3, 0, 0, 0])
+        assert src_disc.ndof(g) == g.num_cells
+        assert np.allclose(rhs, rhs_t)
+        assert lhs.shape == (8, 8)
+        assert np.sum(lhs != 0) == 0
+
+    if __name__ == '__main__':
+        unittest.main()
+
+
+def setup_3d_grid():
+    g = CartGrid([2, 2, 2], physdims=[1, 1, 1])
+    g.compute_geometry()
+    d = {'param': Parameters(g)}
+    src = np.zeros(g.num_cells)
+    src[4] = 1
+    d['param'].set_source('flow', src)
+    return g, d
