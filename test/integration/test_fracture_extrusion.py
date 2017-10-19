b55f9679682b0a72db54a18fe20b56893ce07f9f
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2017-10-19 13:21:06 +0200

    Fix of tolerance in unit test for fracture extrusion.

--- test/integration/test_fracture_extrusion.py
+++ test/integration/test_fracture_extrusion.py
@@ -8,11 +8,12 @@ class TestFractureExtrusion(unittest.TestCase):
     def test_cut_off(self):
         # Two fractures, the second abutting. Check that the second fracture is
         # cut correctly.
+        tol = 1e-4
         p = np.array([[0, 0], [2, 0], [1, 0], [1, 10]]).T
         e = np.array([[0, 1], [2, 3]]).T
 
-        fractures = extrusion.fractures_from_outcrop(p, e)
-        assert np.all(fractures[1].p[1] >= 0)
+        fractures = extrusion.fractures_from_outcrop(p, e, tol=tol)
+        assert np.all(fractures[1].p[1] > -tol)
 
     def test_not_aligned_outcrop(self):
         # The fractures are not aligned with the mesh
