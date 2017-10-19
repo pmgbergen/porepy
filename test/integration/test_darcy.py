72121d2bf0ea0ec34313d7082d63ca61fb24974a
Runar <runar.berge@uib.no>
2017-10-19 09:49:49 +0200

    Removed bugged line in test_darcy. Probably pasted by a mistake?

--- test/integration/test_darcy.py
+++ test/integration/test_darcy.py
@@ -193,6 +193,3 @@ def darcy_dirich_neumann_source_sink_cart_ref_3d():
                       -8.37196805, -24.79222197, -35.8194776, -40.46051172,
                       -8.34414468, -24.57071193, -35.99975111, -44.22506448])
     return p_ref
-
-
-BasicsTest().test_darcy_uniform_flow_simplex()
