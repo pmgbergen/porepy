9a09c50e9f9eceb35d6d0428f628e3ec9478cb47
IvarStefansson <ivarstef@gmail.com>
2017-11-03 16:16:38 +0100

    Removed debugging remnant

--- test/unit/test_upwind_coupling.py
+++ test/unit/test_upwind_coupling.py
@@ -761,4 +761,3 @@ def matrix_rhs_for_test_upwind_coupling_3d_2d_1d_0d():
     return U, rhs
 
 #------------------------------------------------------------------------------#
-BasicsTest().test_upwind_coupling_3d_2d_left_right()
