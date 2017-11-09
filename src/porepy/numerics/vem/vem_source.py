2e4e214e1571e8e22d09c3c910a478712eb4583b
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2017-11-09 11:26:53 +0100

    change file names and introduce the vem_source

---
+++ src/porepy/numerics/vem/vem_source.py
@@ -0,0 +1,84 @@
+'''
+Discretization of the source term of an equation.
+'''
+
+import numpy as np
+import scipy.sparse as sps
+
+from porepy.numerics.mixed_dim.solver import Solver, SolverMixDim
+from porepy.numerics.mixed_dim.coupler import Coupler
+
+
+class Integral(Solver):
+    '''
+    Discretization of the integrated source term
+    int q * dx
+    over each grid cell.
+
+    All this function does is returning a zero lhs and
+    rhs = - param.get_source.physics in a saddle point fashion.
+    '''
+
+    def __init__(self, physics='flow'):
+        self.physics = physics
+        Solver.__init__(self)
+
+    def ndof(self, g):
+        return g.num_cells + g.num_faces
+
+    def matrix_rhs(self, g, data):
+        param = data['param']
+        sources = param.get_source(self)
+        lhs = sps.csc_matrix((self.ndof(g), self.ndof(g)))
+        assert sources.size == g.num_cells, \
+                                 'There should be one soure value for each cell'
+
+        rhs = np.zeros(self.ndof(g))
+        is_p = np.hstack((np.zeros(g.num_faces, dtype=np.bool),
+                          np.ones(g.num_cells, dtype=np.bool)))
+
+        rhs[is_p] = -sources
+
+        return lhs, rhs
+
+#------------------------------------------------------------------------------
+
+class IntegralMixDim(SolverMixDim):
+    def __init__(self, physics='flow'):
+        self.physics = physics
+
+        self.discr = Integral(self.physics)
+        self.discr_ndof = self.discr.ndof
+        self.coupling_conditions = None
+
+        self.solver = Coupler(self.discr)
+        SolverMixDim.__init__(self)
+
+#------------------------------------------------------------------------------
+
+class IntegralDFN(SolverMixDim):
+    def __init__(self, dim_max, physics='flow'):
+        # NOTE: There is no flow along the intersections of the fractures.
+
+        self.physics = physics
+        self.dim_max = dim_max
+
+        self.discr = Integral(self.physics)
+        self.coupling_conditions = None
+
+        kwargs = {"discr_ndof": self.discr.ndof,
+                  "discr_fct": self.__matrix_rhs__}
+        self.solver = Coupler(coupling = None, **kwargs)
+        SolverMixDim.__init__(self)
+
+    def __matrix_rhs__(self, g, data):
+        # The highest dimensional problem compute the matrix and rhs, the lower
+        # dimensional problem and empty matrix. For the latter, the size of the
+        # matrix is the number of cells.
+        if g.dim == self.dim_max:
+            return self.discr.matrix_rhs(g, data)
+        else:
+            ndof = self.discr.ndof(g)
+            return sps.csr_matrix((ndof, ndof)), np.zeros(ndof)
+
+#------------------------------------------------------------------------------
