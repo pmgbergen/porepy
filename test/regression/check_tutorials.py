6522a08e59fc390ad8d457c7433fe1528ed886ba
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2017-11-21 12:27:05 +0100

    Script that tries to run all tutorials.

---
+++ test/regression/check_tutorials.py
@@ -0,0 +1,29 @@
+import os, sys, glob
+
+
+def run_test():
+    os.chdir('../../tutorials')
+
+    for file in glob.glob('*.ipynb'):
+        new_file = file[:-6] + '.py'
+        cmd_convert = 'jupyter nbconvert --to script ' + file
+        os.system(cmd_convert)
+        remove_plots(new_file)
+
+        cmd_run = 'python ' + str(new_file)
+        status = os.system(cmd_run)
+        assert status == 0
+        cmd_delete = 'rm ' + new_file
+        os.system(cmd_delete)
+
+def remove_plots(fn):
+    with open(fn) as f:
+        content = f.readlines()
+
+    with open(fn, 'w') as f:
+        for line in content:
+            if line.strip()[:9] != 'plot_grid' and line.strip()[:4] != 'plt.'\
+                and line.strip()[:8] != 'frac_viz':
+                f.write(line)
+
+run_test()
