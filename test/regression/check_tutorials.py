d0d337cfd1d429387bc7f7e97b5a9b44de33a836
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2017-11-29 13:23:29 +0100

    Fixes to script for testing tutorials

--- test/regression/check_tutorials.py
+++ test/regression/check_tutorials.py
@@ -38,6 +38,10 @@ def remove_plots(fn):
                 continue
             if line.strip()[:11] == 'get_ipython':
                 continue
+            if 'vtk' in line:
+                continue
+            if 'Exporter' in line:
+                continue
             f.write(line)
 
 run_all()
