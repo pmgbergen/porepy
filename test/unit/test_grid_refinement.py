8f8555571abcf8b3d88a99a1e5786e405c038b11
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2017-12-18 11:44:17 +0100

    add unit test for mortar grids in 2d-1d

--- test/unit/test_grid_refinement.py
+++ test/unit/test_grid_refinement.py
@@ -171,6 +171,225 @@ class TestGridRefinement1d(unittest.TestCase):
 #------------------------------------------------------------------------------#
 
     def test_mortar_grid_1d(self):
+
+        f1 = np.array([[0, 1], [.5, .5]])
+
+        gb = meshing.cart_grid([f1], [2, 2], **{'physdims': [1, 1]})
+        gb.compute_geometry()
+        gb.assign_node_ordering()
+
+        for e, d in gb.edges_props():
+
+            high_to_mortar_known = np.matrix([[ 0.,  0.,  0.,  0.,
+                                                0.,  0.,  0.,  0.,
+                                                0.,  1.,  0.,  0.,
+                                                0.,  0.],
+                                              [ 0.,  0.,  0.,  0.,
+                                                0.,  0.,  0.,  0.,
+                                                1.,  0.,  0.,  0.,
+                                                0.,  0.],
+                                              [ 0.,  0.,  0.,  0.,
+                                                0.,  0.,  0.,  0.,
+                                                0.,  0.,  0.,  0.,
+                                                0.,  1.],
+                                              [ 0.,  0.,  0.,  0.,
+                                                0.,  0.,  0.,  0.,
+                                                0.,  0.,  0.,  0.,
+                                                1.,  0.]])
+            mortar_to_low_known = np.matrix([[ 1.,  0.],
+                                             [ 0.,  1.],
+                                             [ 1.,  0.],
+                                             [ 0.,  1.]])
+
+            mg = d['mortar']
+            assert np.allclose(high_to_mortar_known, mg.high_to_mortar.todense())
+            assert np.allclose(mortar_to_low_known, mg.mortar_to_low.todense())
+
+#------------------------------------------------------------------------------#
+
+    def test_mortar_grid_1d_equally_refine_mortar_grids(self):
+
+        f1 = np.array([[0, 1], [.5, .5]])
+
+        gb = meshing.cart_grid([f1], [2, 2], **{'physdims': [1, 1]})
+        gb.compute_geometry()
+        gb.assign_node_ordering()
+
+        for e, d in gb.edges_props():
+
+            mg = d['mortar']
+            new_side_grids = {s: refinement.new_grid_1d(g, num_nodes=4) \
+                              for s, g in mg.side_grids.items()}
+
+            mortars.refine_mortar(mg, new_side_grids)
+
+            high_to_mortar_known = 1./3.*np.matrix([
+        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  2.,  0.,  0.,  0.,  0.,  0.],
+        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  1.,  1.,  0.,  0.,  0.,  0.],
+        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  2.,  0.,  0.,  0.,  0.],
+        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  2.,  0.],
+        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  1.,  1.],
+        [ 0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  0.,  2.]])
+
+            mortar_to_low_known = 1./3.*np.matrix([[ 0.,  2.],
+                                                   [ 1.,  1.],
+                                                   [ 2.,  0.],
+                                                   [ 0.,  2.],
+                                                   [ 1.,  1.],
+                                                   [ 2.,  0.]])
+
+            assert np.allclose(high_to_mortar_known, mg.high_to_mortar.todense())
+            assert np.allclose(mortar_to_low_known, mg.mortar_to_low.todense())
+
+#------------------------------------------------------------------------------#
+
+    def test_mortar_grid_1d_unequally_refine_mortar_grids(self):
+
+        f1 = np.array([[0, 1], [.5, .5]])
+
+        gb = meshing.cart_grid([f1], [2, 2], **{'physdims': [1, 1]})
+        gb.compute_geometry()
+        gb.assign_node_ordering()
+
+        for e, d in gb.edges_props():
+
+            mg = d['mortar']
+            new_side_grids = {s: refinement.new_grid_1d(g, num_nodes=int(s)+3) \
+                              for s, g in mg.side_grids.items()}
+
+            mortars.refine_mortar(mg, new_side_grids)
+
+            high_to_mortar_known = np.matrix(
+                                        [[ 0.        ,  0.        ,  0.        ,
+                                           0.        ,  0.        ,  0.        ,
+                                           0.        ,  0.        ,  0.66666667,
+                                           0.        ,  0.        ,  0.        ,
+                                           0.        ,  0.        ],
+                                         [ 0.        ,  0.        ,  0.        ,
+                                           0.        ,  0.        ,  0.        ,
+                                           0.        ,  0.        ,  0.33333333,
+                                           0.33333333,  0.        ,  0.        ,
+                                           0.        ,  0.        ],
+                                         [ 0.        ,  0.        ,  0.        ,
+                                           0.        ,  0.        ,  0.        ,
+                                           0.        ,  0.        ,  0.        ,
+                                           0.66666667,  0.        ,  0.        ,
+                                           0.        ,  0.        ],
+                                         [ 0.        ,  0.        ,  0.        ,
+                                           0.        ,  0.        ,  0.        ,
+                                           0.        ,  0.        ,  0.        ,
+                                           0.        ,  0.        ,  0.        ,
+                                           0.5       ,  0.        ],
+                                         [ 0.        ,  0.        ,  0.        ,
+                                           0.        ,  0.        ,  0.        ,
+                                           0.        ,  0.        ,  0.        ,
+                                           0.        ,  0.        ,  0.        ,
+                                           0.5       ,  0.        ],
+                                         [ 0.        ,  0.        ,  0.        ,
+                                           0.        ,  0.        ,  0.        ,
+                                           0.        ,  0.        ,  0.        ,
+                                           0.        ,  0.        ,  0.        ,
+                                           0.        ,  0.5       ],
+                                         [ 0.        ,  0.        ,  0.        ,
+                                           0.        ,  0.        ,  0.        ,
+                                           0.        ,  0.        ,  0.        ,
+                                           0.        ,  0.        ,  0.        ,
+                                           0.        ,  0.5       ]])
+            mortar_to_low_known = np.matrix([[ 0.        ,  0.66666667],
+                                             [ 0.33333333,  0.33333333],
+                                             [ 0.66666667,  0.        ],
+                                             [ 0.        ,  0.5       ],
+                                             [ 0.        ,  0.5       ],
+                                             [ 0.5       ,  0.        ],
+                                             [ 0.5       ,  0.        ]])
+
+            assert np.allclose(high_to_mortar_known, mg.high_to_mortar.todense())
+            assert np.allclose(mortar_to_low_known, mg.mortar_to_low.todense())
+
+#------------------------------------------------------------------------------#
+
+    def test_mortar_grid_1d_refine_1d_grid(self):
+
+        f1 = np.array([[0, 1], [.5, .5]])
+
+        gb = meshing.cart_grid([f1], [2, 2], **{'physdims': [1, 1]})
+        gb.compute_geometry()
+        gb.assign_node_ordering()
+
+        for e, d in gb.edges_props():
+
+            # refine the 1d-physical grid
+            old_g = gb.sorted_nodes_of_edge(e)[0]
+            new_g = refinement.new_grid_1d(old_g, num_nodes=5)
+            new_g.compute_geometry()
+
+            gb.update_nodes(old_g, new_g)
+            mg = d['mortar']
+            mortars.refine_co_dimensional_grid(mg, new_g)
+
+            high_to_mortar_known = np.matrix([[ 0.,  0.,  0.,  0.,  0.,  0.,
+                                                0.,  0.,  0.,  1.,  0.,  0.,
+                                                0.,  0.],
+                                              [ 0.,  0.,  0.,  0.,  0.,  0.,
+                                                0.,  0.,  1.,  0.,  0.,  0.,
+                                                0.,  0.],
+                                              [ 0.,  0.,  0.,  0.,  0.,  0.,
+                                                0.,  0.,  0.,  0.,  0.,  0.,
+                                                0.,  1.],
+                                              [ 0.,  0.,  0.,  0.,  0.,  0.,
+                                                0.,  0.,  0.,  0.,  0.,  0.,
+                                                1.,  0.]])
+            mortar_to_low_known = np.matrix([[ 0. ,  0. ,  0.5,  0.5],
+                                             [ 0.5,  0.5,  0. ,  0. ],
+                                             [ 0. ,  0. ,  0.5,  0.5],
+                                             [ 0.5,  0.5,  0. ,  0. ]])
+
+            assert np.allclose(high_to_mortar_known, mg.high_to_mortar.todense())
+            assert np.allclose(mortar_to_low_known, mg.mortar_to_low.todense())
+
+#------------------------------------------------------------------------------#
+
+    def test_mortar_grid_1d_refine_1d_grid_2(self):
+
+        f1 = np.array([[0, 1], [.5, .5]])
+
+        gb = meshing.cart_grid([f1], [2, 2], **{'physdims': [1, 1]})
+        gb.compute_geometry()
+        gb.assign_node_ordering()
+
+        for e, d in gb.edges_props():
+
+            # refine the 1d-physical grid
+            old_g = gb.sorted_nodes_of_edge(e)[0]
+            new_g = refinement.new_grid_1d(old_g, num_nodes=4)
+            new_g.compute_geometry()
+
+            gb.update_nodes(old_g, new_g)
+            mg = d['mortar']
+            mortars.refine_co_dimensional_grid(mg, new_g)
+
+            high_to_mortar_known = np.matrix([[ 0.,  0.,  0.,  0.,  0.,  0.,
+                                                0.,  0.,  0.,  1.,  0.,  0.,
+                                                0.,  0.],
+                                              [ 0.,  0.,  0.,  0.,  0.,  0.,
+                                                0.,  0.,  1.,  0.,  0.,  0.,
+                                                0.,  0.],
+                                              [ 0.,  0.,  0.,  0.,  0.,  0.,
+                                                0.,  0.,  0.,  0.,  0.,  0.,
+                                                0.,  1.],
+                                              [ 0.,  0.,  0.,  0.,  0.,  0.,
+                                                0.,  0.,  0.,  0.,  0.,  0.,
+                                                1.,  0.]])
+            mortar_to_low_known = 1./3.*np.matrix([[ 0.,  1.,  2.],
+                                                   [ 2.,  1.,  0.],
+                                                   [ 0.,  1.,  2.],
+                                                   [ 2.,  1.,  0.]])
+
+            assert np.allclose(high_to_mortar_known, mg.high_to_mortar.todense())
+            assert np.allclose(mortar_to_low_known, mg.mortar_to_low.todense())
+
+#------------------------------------------------------------------------------#
+
         from porepy.viz import plot_grid
 
         f1 = np.array([[0, 1], [.5, .5]])
