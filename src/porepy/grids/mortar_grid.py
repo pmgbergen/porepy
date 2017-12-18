6480dbbcb56ddfa606759729a59a94ac520cbe5f
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2017-12-18 10:49:06 +0100

    update documentation for the mortars

--- src/porepy/grids/mortar_grid.py
+++ src/porepy/grids/mortar_grid.py
@@ -1,31 +1,17 @@
-""" Module containing the parent class for all grids.
-
-See documentation of the Grid class for further details.
-
-Acknowledgements:
-    The data structure for the grid is inspired by that used in the Matlab
-    Reservoir Simulation Toolbox (MRST) developed by SINTEF ICT, see
-    www.sintef.no/projectweb/mrst/ . Some of the methods, in particular
-    compute_geometry() and its subfunctions is to a large degree translations
-    of the corresponding functions in MRST.
-
+""" Module containing the class for the mortar grid.
 """
 from __future__ import division
 import numpy as np
-import itertools
 from enum import Enum
 from scipy import sparse as sps
 
-from porepy.utils import matrix_compression, mcolon
-
-from porepy.utils import comp_geom as cg
-
 class SideTag(np.uint8, Enum):
     """
-    FaceTag contains the following types:
+    SideTag contains the following types:
         NONE: None of the below
         LEFT: Left part of the domain
         RIGHT: Right part of the domain
+        WHOLE: All the tags
     """
     NONE = 0
     LEFT = 1
@@ -34,69 +20,44 @@ class SideTag(np.uint8, Enum):
 
 class MortarGrid(object):
     """
-    Parent class for all grids.
-
-    The grid stores topological information, as well as geometric
-    information (after a call to self.compute_geometry().
-
-    As of yet, there is no structure for tags (face or cell) is the grid.
-    This will be introduced later.
+    Parent class for mortar grids it contains two grids representing the left
+    and right part of the mortar grid and the weighted mapping from the higher
+    dimensional grid (as set of faces) to the mortar grids and from the lower
+    dimensional grid (as set of cells) to the mortar grids. The two mortar grids
+    can be different.
 
     Attributes:
-        Comes in two classes. Topologogical information, defined at
-        construction time:
 
-        dim (int): dimension. Should be 0 or 1 or 2 or 3
-        nodes (np.ndarray): node coordinates. size: dim x num_nodes
-        face_nodes (sps.csc-matrix): Face-node relationships. Matrix size:
-            num_faces x num_cells. To use compute_geometry() later, he field
-            face_nodes.indices should store the nodes of each face sorted.
-            For more information, see information on compute_geometry()
-            below.
-        cell_faces (sps.csc-matrix): Cell-face relationships. Matrix size:
-            num_faces x num_cells. Matrix elements have value +-1, where +
-            corresponds to the face normal vector being outwards.
+        dim (int): dimension. Should be 0 or 1 or 2.
+        side_grids (dictionary of Grid): grid for each side. The key is a
+            SideTag and the value is a Grid.
+        sides (array of SideTag): ordering of the sides.
+        high_to_mortar (sps.csc-matrix): Face-cell relationships between the
+            high dimensional grid and the mortar grids. Matrix size:
+            num_faces x num_cells. In the beginning we assume matching grids,
+            but it can be modified by calling refine_mortar(). The matrix
+            elements represent the ratio between the geometrical objects.
+        mortar_to_low (sps.csc-matrix): Cell-cell relationships between the
+            mortar grids and the low dimensional grid. Matrix size:
+            num_cells x num_cells. Matrix elements represent the ratio between
+            the geometrical objects.
         name (list): Information on the formation of the grid, such as the
             constructor, computations of geometry etc.
-        num_nodes (int): Number of nodes in the grid
-        num_faces (int): Number of faces in the grid
-        num_cells (int): Number of cells in the grid
-
-        ---
-        compute_geometry():
-        Assumes the nodes of each face is ordered according to the right
-        hand rule.
-        face_nodes.indices[face_nodes.indptr[i]:face_nodes.indptr[i+1]]
-        are the nodes of face i, which should be ordered counter-clockwise.
-        By counter-clockwise we mean as seen from cell cell_faces[i,:]==1.
-        Equivalently the nodes will be clockwise as seen from cell
-        cell_faces[i,:] == -1. Note that operations on the face_nodes matrix
-        (such as converting it to a csr-matrix) may change the ordering of
-        the nodes (face_nodes.indices), which will break compute_geometry().
-        Geometric information, available after compute_geometry() has been
-        called on the object:
-
-        face_areas (np.ndarray): Areas of all faces
-        face_centers (np.ndarray): Centers of all faces. Dimensions dim x
-            num_faces
-        face_normals (np.ndarray): Normal vectors of all faces. Dimensions
-            dim x num_faces. See also cell_faces.
-        cell_centers (np.ndarray): Centers of all cells. Dimensions dim x
-            num_cells
-        cell_volumes (np.ndarray): Volumes of all cells
 
     """
 
     def __init__(self, dim, side_grids, face_cells, name=''):
-        """Initialize the grid
+        """Initialize the mortar grid
 
         See class documentation for further description of parameters.
+        The high_to_mortar and mortar_to_low are identity mapping.
 
         Parameters
         ----------
         dim (int): grid dimension
-        nodes (np.ndarray): node coordinates.
-        cell_nodes (sps.csc_matrix): Cell-node relations
+        side_grids (dictionary of Grid): grid on each side.
+        face_cells (sps.csc_matrix): Cell-face relations between the higher
+            dimensional grid and the lower dimensional grid
         name (str): Name of grid
         """
 
@@ -112,11 +73,22 @@ class MortarGrid(object):
         else:
             self.name = [name]
 
-        num_cells = list(self.side_grids.values())[0].num_cells
 
         # face_cells mapping from the higher dimensional grid to the mortar grid
         # also here we assume that, in the beginning the mortar grids are equal
-        # to the co-dimensional grid
+        # to the co-dimensional grid. If this assumption is not satisfied we
+        # need to change the following lines
+
+        # Creation of the high_to_mortar, besically we start from the face_cells
+        # map and we split the relation
+        # low_dimensional_cell -> 2 high_dimensional_face
+        # as
+        # low_dimensional_cell -> high_dimensional_face
+        # The mapping consider the cell ids of the second mortar grid shifted by
+        # the g.num_cells of the first grid. We keep this convention through the
+        # implementation. The ordering is given by sides or the keys of
+        # side_grids.
+        num_cells = list(self.side_grids.values())[0].num_cells
         cells, faces, data = sps.find(face_cells)
         cells[faces > np.median(faces)] += num_cells
 
@@ -124,7 +96,9 @@ class MortarGrid(object):
         self.high_to_mortar = sps.csc_matrix((data.astype(np.float),
                                              (cells, faces)), shape=shape)
 
-        # cell_cells mapping from the mortar grid to the lower dimensional grid
+        # cell_cells mapping from the mortar grid to the lower dimensional grid.
+        # It is composed by two identity matrices since we are assuming matching
+        # grids here.
         identity = sps.identity(num_cells)
         self.mortar_to_low = sps.bmat([[identity], [identity]], format='csc')
 
@@ -164,24 +138,63 @@ class MortarGrid(object):
         return s
 
     def compute_geometry(self):
+        """
+        Compute the geometry of the mortar grids.
+        We assume that they are not aligned with x (1d) or x, y (2d).
+        """
         [g.compute_geometry(is_embedded=True) for g in self.side_grids.values()]
 
     def refine_mortar(self, side_matrix):
+        """
+        Update the mortar_to_low and high_to_mortar maps when the mortar grids
+        are changed.
+
+        Parameter:
+            side_matrix (dict): for each SideTag key a matrix representing the
+            new mapping between the old and new mortar grids.
+        """
+
+        # In the case of different side ordering between the input data and the
+        # stored we need to remap it. The resulting matrix will be a block
+        # diagonal matrix, where in each block we have the mapping between the
+        # (relative to side) old grid and the new one.
         matrix = np.empty((self.num_sides(), self.num_sides()), dtype=np.object)
         for pos, (side, _) in enumerate(self.side_grids.items()):
             matrix[pos, pos] = side_matrix[side]
 
+        # Once the global matrix is constructed the new mortar_to_low and
+        # high_to_mortar maps are updated.
         matrix = sps.bmat(matrix)
         self.mortar_to_low = matrix * self.mortar_to_low
         self.high_to_mortar = matrix * self.high_to_mortar
 
 
     def refine_low(self, side_matrix):
+        """
+        Update the mortar_to_low map when the lower dimensional grid is changed.
+
+        Parameter:
+            side_matrix (dict): for each SideTag key a matrix representing the
+            new mapping between the new lower dimensional grid and the mortar
+            grids.
+        """
+
+        # In the case of different side ordering between the input data and the
+        # stored we need to remap it. The resulting matrix will be a block
+        # matrix, where in each block we have the mapping between the
+        # (relative to side) the new grid and the mortar grid.
         matrix = np.empty((self.num_sides(), 1), dtype=np.object)
         for pos, (side, _) in enumerate(self.side_grids.items()):
             matrix[pos, 0] = side_matrix[side]
 
+        # Update the mortar_to_low map. No need to update the high_to_mortar.
         self.mortar_to_low = sps.bmat(matrix, format='csc')
 
     def num_sides(self):
+        """
+        Shortcut to compute the number of sides, usually 2.
+
+        Return:
+            Number of sides.
+        """
         return len(self.side_grids)
