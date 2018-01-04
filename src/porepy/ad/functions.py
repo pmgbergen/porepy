187575a1e482f8a1689bb13960cde1324b2a6920
Runar <runar.berge@uib.no>
2018-01-04 12:11:13 +0100

    added logarithm to ad functions

--- src/porepy/ad/functions.py
+++ src/porepy/ad/functions.py
@@ -10,3 +10,14 @@ def exp(var):
         return Ad_array(val, der)
     else:
         return np.exp(var)
+
+def log(var):
+    if not isinstance(var, Ad_array):
+        return np.log(var)
+
+    val = np.log(var.val)
+    der = var.l_jac_mul(1 / var.val)
+    return Ad_array(val, der)
+    
+
+    
