8ca7aaffc855af70a43c862578c835d7a94a5a35
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2018-01-23 18:30:10 +0100

    Better treatment of boundary conditions for 3D geiger mortars

--- examples/papers/legacy_mortar/geiger_3d_data.py
+++ examples/papers/legacy_mortar/geiger_3d_data.py
@@ -26,12 +26,13 @@ class DarcyModelData(elliptic.EllipticDataAssigner):
             b_face_centers = g.face_centers[:, b_faces]
 
             val = 0.5 - self.tol
-            self.b_flux = np.logical_and.reduce(tuple(b_face_centers[i, :] < val
+            self.b_in = np.logical_and.reduce(tuple(b_face_centers[i, :] < val
                                                   for i in range(self.max_dim)))
 
             val = 0.75 + self.tol
-            self.b_pressure = np.logical_and.reduce(tuple(b_face_centers[i, :] >
+            self.b_out = np.logical_and.reduce(tuple(b_face_centers[i, :] >
                                               val for i in range(self.max_dim)))
+            self.b_pressure = self.b_in + self.b_out
 
         elliptic.EllipticDataAssigner.__init__(self, g, data)
 
@@ -40,21 +41,21 @@ class DarcyModelData(elliptic.EllipticDataAssigner):
 
     def bc(self):
         b_faces = self.grid().get_domain_boundary_faces()
-        if b_faces.size == 0:
-            return BoundaryCondition(self.grid(), np.empty(0), np.empty(0))
-
         labels = np.array(['neu'] * b_faces.size)
-        labels[self.b_pressure] = 'dir'
+        if self.grid().dim == 3:
+            if b_faces.size == 0:
+                return BoundaryCondition(self.grid(), np.empty(0), np.empty(0))
+
+            labels[self.b_pressure] = 'dir'
         return BoundaryCondition(self.grid(), b_faces, labels)
+
 
     def bc_val(self):
         bc_val = np.zeros(self.grid().num_faces)
-        b_faces = self.grid().get_domain_boundary_faces()
-        if b_faces.size > 0:
-
-            f_faces = b_faces[self.b_flux]
-            bc_val[f_faces] = -self.aperture()*self.grid().face_areas[f_faces]
-            bc_val[b_faces[self.b_pressure]] = 1
+        if self.grid().dim == 3:
+            b_faces = self.grid().get_domain_boundary_faces()
+            if b_faces.size > 0:
+                bc_val[b_faces[self.b_in]] = 1
 
         return bc_val
 
