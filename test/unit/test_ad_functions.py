72f09332d99b4ce65dfd7e05c215c01baefc745b
Runar <runar.berge@uib.no>
2018-01-30 11:33:51 +0100

    Changed Jacobian to be an array of sub jacobians instead of one big.
    also added init function for creating several independent variables

--- test/unit/test_ad_functions.py
+++ test/unit/test_ad_functions.py
@@ -23,11 +23,12 @@ class AdFunctionTest(unittest.TestCase):
     def test_exp_vector(self):
         val = np.array([1, 2, 3])
         J = np.array([[3, 2, 1], [5, 6, 1], [2, 3, 5]])
-        a = Ad_array(val, J)
+        a = Ad_array(val, [J])
         b = af.exp(a)
         jac = np.dot(np.diag(np.exp(val)), J)
         
-        assert np.all(b.val == np.exp(val)) and np.all(b.jac == jac)
+        assert np.all(b.val == np.exp(val)) and np.all(b.jac[0] == jac)
+        assert np.all(J == np.array([[3, 2, 1], [5, 6, 1], [2, 3, 5]]))
 
     def test_exp_sparse_jac(self):
         val = np.array([1, 2, 3])
@@ -40,13 +41,13 @@ class AdFunctionTest(unittest.TestCase):
     def test_exp_scalar_times_ad_var(self):
         val = np.array([1,2,3])
         J = sps.diags(np.array([1,1,1]))
-        a = Ad_array(val, J)
+        a = Ad_array(val, [J])
         c = 2
         b = af.exp(c * a)
         jac = c * sps.diags(np.exp(c * val)) * J
         
-        assert np.allclose(b.val, np.exp(c * val)) and np.allclose(b.jac.A, jac.A)
-        assert np.all(a.val == [1,2,3]) and np.all(a.jac.A == J.A)
+        assert np.allclose(b.val, np.exp(c * val)) and np.allclose(b.jac[0].A, jac.A)
+        assert np.all(a.val == [1,2,3]) and np.all(a.jac[0].A == J.A)
 
     def test_log_scalar(self):
         a = Ad_array(2, 0)
@@ -63,11 +64,12 @@ class AdFunctionTest(unittest.TestCase):
     def test_log_vector(self):
         val = np.array([1, 2, 3])
         J = np.array([[3, 2, 1], [5, 6, 1], [2, 3, 5]])
-        a = Ad_array(val, J)
+        a = Ad_array(val, [J])
+        
         b = af.log(a)
         jac = np.dot(np.diag(1 / val), J)
         
-        assert np.all(b.val == np.log(val)) and np.all(b.jac == jac)
+        assert np.all(b.val == np.log(val)) and np.all(b.jac[0] == jac)
 
     def test_log_sparse_jac(self):
         val = np.array([1, 2, 3])
@@ -80,13 +82,13 @@ class AdFunctionTest(unittest.TestCase):
     def test_log_scalar_times_ad_var(self):
         val = np.array([1,2,3])
         J = sps.diags(np.array([1,1,1]))
-        a = Ad_array(val, J)
+        a = Ad_array(val, [J])
         c = 2
         b = af.log(c * a)
         jac = sps.diags(1 / val) * J
         
-        assert np.allclose(b.val, np.log(c * val)) and np.allclose(b.jac.A, jac.A)
-        assert np.all(a.val == [1,2,3]) and np.all(a.jac.A == J.A)
+        assert np.allclose(b.val, np.log(c * val)) and np.allclose(b.jac[0].A, jac.A)
+        assert np.all(a.val == [1,2,3]) and np.all(a.jac[0].A == J.A)
 
     def test_sign_no_advar(self):
         a = np.array([1, -10, 3, -np.pi])
@@ -107,16 +109,16 @@ class AdFunctionTest(unittest.TestCase):
 
     def test_abs_advar(self):
         J = np.array([[1, -1, -np.pi, 3], [0,0,0,0], [1,2,-3.2, 4], [4,2,300000, 1]])
-        a = Ad_array(np.array([1, -10, 3, -np.pi]), J)
+        a = Ad_array(np.array([1, -10, 3, -np.pi]), [J])
         a_abs = af.abs(a)
         J_abs = np.array([[1, -1, -np.pi, 3],
                           [0, 0, 0, 0],
                           [1, 2, -3.2, 4],
                           [-4, -2, -300000, -1]])
 
-        assert np.allclose(J,np.array([[1, -1, -np.pi, 3],
+        assert np.allclose(J, np.array([[1, -1, -np.pi, 3],
                                        [0,0,0,0],
                                        [1,2,-3.2, 4],
                                        [4,2,300000, 1]]))
         assert np.allclose(a_abs.val, [1, 10, 3, np.pi])
-        assert np.allclose(a_abs.jac, J_abs)        
+        assert np.allclose(a_abs.jac[0], J_abs)        
