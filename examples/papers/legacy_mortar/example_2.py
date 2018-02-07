1bbbb69b34780239d89361d96998d053ebbf9444
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2018-02-07 12:38:49 +0100

    Geiger 3D test for mortars run sequence of grids

--- examples/papers/legacy_mortar/example_2.py
+++ examples/papers/legacy_mortar/example_2.py
@@ -7,13 +7,14 @@ import example_2_data
 from solvers import *
 
 case = 1
-h = 0.1
+h_list = [0.2, 0.15, 0.1]
 list_of_solvers = {"tpfa": solve_tpfa, "p1": solve_p1, "mpfa": solve_mpfa,
                    "rt0": solve_rt0, "vem": solve_vem}
 
-for solver_name, solver_fct in list_of_solvers.items():
-
-    gb, domain = example_2_data.create_gb(h)
-    example_2_data.add_data(gb, domain, solver_name, case)
-    folder = "example_2_"+solver_name
-    solver_fct(gb, folder)
+for h in h_list:
+    for solver_name, solver_fct in list_of_solvers.items():
+        print('Start simulation with ' + solver_name)
+        gb, domain = example_2_data.create_gb(h)
+        example_2_data.add_data(gb, domain, solver_name, case)
+        folder = "example_2_"+solver_name
+        solver_fct(gb, folder)
