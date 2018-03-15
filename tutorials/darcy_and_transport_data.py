f0528b6bbc0b445eeb4ed4dce12d5319fb964c97
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2018-03-15 16:14:40 +0100

    update jupyter notebook for the darcy and transport

--- tutorials/darcy_and_transport_data.py
+++ tutorials/darcy_and_transport_data.py
@@ -3,8 +3,25 @@ import numpy as np
 from porepy.numerics import elliptic
 from porepy.numerics import parabolic
 from porepy.params import bc, tensor
+from porepy.fracs import importer
 
 from porepy.numerics import darcy_and_transport
+
+#------------------------------------------------------------------------------#
+
+def create_grid(file_name, domain, mesh_size, tol):
+
+    # pack the data for the grid generation
+    mesh_kwargs = {'tol': tol}
+    mesh_kwargs['mesh_size'] = {'mode': 'weighted', 'value': mesh_size,
+                                'bound_value': mesh_size}
+
+    # import the fractures and create the grid
+    gb = importer.dfm_2d_from_csv(file_name, mesh_kwargs, domain)
+    gb.compute_geometry()
+    gb.assign_node_ordering()
+
+    return gb
 
 #------------------------------------------------------------------------------#
 
@@ -15,44 +32,37 @@ def assign_data(gb, data_problem, data_class, data_key):
     Transport data_key: transport_data
     """
     gb.add_node_props([data_key])
+
+    cells = np.nan*np.ones(data_problem['wells'].shape[1])
+    distance = np.inf*np.ones(cells.size)
+    for g, _ in gb:
+        cells_loc, distance_loc = assign_well(g, data_problem)
+        print(cells_loc, distance_loc)
+    fix_wells()
+
     for g, d in gb:
         d[data_key] = data_class(g, d, **data_problem)
 
 #------------------------------------------------------------------------------#
 
-def get_dir_boundary(problem):
-    bound_faces = problem.grid().tags['domain_boundary_faces'].nonzero()[0]
-    bound_face_centers = problem.grid().face_centers[:, bound_faces]
+def assign_well(g, data_problem):
 
-    dir_boundary = {}
-    if bound_faces.size == 0:
-        return dir_boundary
+    wells = data_problem['wells']
+    if g.dim == 1 and not ('well' in g.tags):
+        g.tags['well'] = -np.ones(g.num_cells)
+        g.tags['well_distance'] = np.inf*np.ones(g.num_cells)
 
-    if 'left' in problem.dirichlet_boundary:
-        left = bound_face_centers[0, :] < problem.domain['xmin'] + problem.tol
-        dir_boundary['left'] = left
-    if 'right' in problem.dirichlet_boundary:
-        right = bound_face_centers[0, :] > problem.domain['xmax'] - problem.tol
-        dir_boundary['right'] = right
-    if 'top' in problem.dirichlet_boundary:
-        top = bound_face_centers[1, :] > problem.domain['ymax'] - problem.tol
-        dir_boundary['top'] = top
-    if 'bottom' in problem.dirichlet_boundary:
-        bottom = bound_face_centers[1, :] < problem.domain['ymin'] + problem.tol
-        dir_boundary['bottom'] = bottom
-
-    return dir_boundary
+        cells, distance = g.closest_cell(wells, return_distance=True)
+        g.tags['well'][cells] = np.arange(cells.size)
+        g.tags['well_distance'][cells] = distance
+        return cells, distance
+    else:
+        return np.nan*np.ones(wells.shape[1]), np.inf*np.ones(wells.shape[1])
 
 #------------------------------------------------------------------------------#
 
-def compute_outflow(gb):
-    outflow = 0
-    for g, d in gb:
-        boundary_faces = g.tags['domain_boundary_faces'].nonzero()[0]
-        if g.dim > 0 and boundary_faces.size != 0:
-            flow_rate = d['discharge'][boundary_faces]
-            outflow += np.sum(flow_rate[flow_rate > 0])
-    return outflow
+def fix_wells():
+    pass
 
 #------------------------------------------------------------------------------#
 
@@ -64,48 +74,40 @@ class FlowData(elliptic.EllipticDataAssigner):
     def __init__(self, g, d, **kwargs):
 
         self.tol = kwargs.get('tol', 1e-6)
-        self.dim_max = kwargs.get('dim_max', 2)
         self.domain = kwargs['domain']
 
         self.kf = kwargs['kf']
         self.km = kwargs['km']
 
         self.reference_aperture = kwargs['aperture']
-
-        self.dirichlet_boundary = kwargs['dirichlet_boundary']
+        self.bc_pressure = kwargs['bc_pressure']
 
         elliptic.EllipticDataAssigner.__init__(self, g, d)
 
     def aperture(self):
-        a = np.power(self.reference_aperture, self.dim_max - self.grid().dim)
+        a = np.power(self.reference_aperture, 2-self.grid().dim)
         return np.ones(self.grid().num_cells) * a
 
     def permeability(self):
         ones = np.ones(self.grid().num_cells)
-        if self.grid().dim == self.dim_max:
+        if self.grid().dim == 2:
             return tensor.SecondOrder(3, self.km*ones)
         else:
             return tensor.SecondOrder(3, self.kf*ones)
 
     def bc(self):
         bound_faces = self.grid().tags['domain_boundary_faces'].nonzero()[0]
-        dir_boundary = get_dir_boundary(self)
-
-        labels = np.array(['neu']*bound_faces.size)
-        for _, face_ids in dir_boundary.items():
-            labels[face_ids] = 'dir'
-
+        labels = np.array(['dir']*bound_faces.size)
         return bc.BoundaryCondition(self.grid(), bound_faces, labels)
 
     def bc_val(self):
         bound_faces = self.grid().tags['domain_boundary_faces'].nonzero()[0]
-        dir_boundary = get_dir_boundary(self)
-
         bc_val = np.zeros(self.grid().num_faces)
-        for boundary, face_ids in dir_boundary.items():
-            bc_val[bound_faces[face_ids]] = self.dirichlet_boundary[boundary]
-
+        bc_val[bound_faces] = self.bc_pressure
         return bc_val
+
+    def source(self):
+        return np.zeros(self.grid().num_cells)
 
 #------------------------------------------------------------------------------#
 
@@ -117,41 +119,29 @@ class TransportData(parabolic.ParabolicDataAssigner):
     def __init__(self, g, d, **kwargs):
 
         self.tol = kwargs.get('tol', 1e-6)
-        self.dim_max = kwargs.get('dim_max', 2)
         self.domain = kwargs['domain']
 
-        self.initial = kwargs.get('initial_condition', 0)
         self.reference_aperture = kwargs['aperture']
-
-        self.dirichlet_boundary = kwargs['dirichlet_boundary']
+        self.bc_temperature = kwargs['bc_temperature']
 
         parabolic.ParabolicDataAssigner.__init__(self, g, d)
 
     def bc(self):
         bound_faces = self.grid().tags['domain_boundary_faces'].nonzero()[0]
-        dir_boundary = get_dir_boundary(self)
-
-        labels = np.array(['neu']*bound_faces.size)
-        for _, face_ids in dir_boundary.items():
-            labels[face_ids] = 'dir'
-
+        labels = np.array(['dir']*bound_faces.size)
         return bc.BoundaryCondition(self.grid(), bound_faces, labels)
 
     def bc_val(self, t):
         bound_faces = self.grid().tags['domain_boundary_faces'].nonzero()[0]
-        dir_boundary = get_dir_boundary(self)
-
         bc_val = np.zeros(self.grid().num_faces)
-        for boundary, face_ids in dir_boundary.items():
-            bc_val[bound_faces[face_ids]] = 1
-
+        bc_val[bound_faces] = self.bc_temperature
         return bc_val
 
     def initial_condition(self):
-        return self.initial*np.ones(self.grid().num_cells)
+        return self.bc_temperature*np.ones(self.grid().num_cells)
 
     def aperture(self):
-        a = np.power(self.reference_aperture, self.dim_max - self.grid().dim)
+        a = np.power(self.reference_aperture, 2-self.grid().dim)
         return np.ones(self.grid().num_cells) * a
 
 #------------------------------------------------------------------------------#
@@ -180,12 +170,16 @@ class Simulation(darcy_and_transport.DarcyAndTransport):
 
     def __init__(self, gb, data_problem):
 
+        # find the wells
+
         # Darcy problem
         assign_data(gb, data_problem, FlowData, 'problem')
+        data_problem['file_name'] = 'pressure'
         flow = elliptic.EllipticModel(gb, **data_problem)
 
         # transport problem
         assign_data(gb, data_problem, TransportData, 'transport_data')
+        data_problem['file_name'] = 'temperature'
         transport = TransportSolver(gb, **data_problem)
 
         darcy_and_transport.DarcyAndTransport.__init__(self, flow, transport)
