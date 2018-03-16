16f8a04f58f06bd975c8c2da3df6d4cc48fdf816
Runar <runar.berge@uib.no>
2018-03-16 14:27:01 +0100

    Revert "Merge branch 'cleanup' of github.com:pmgbergen/porepy into cleanup"
    This reverts commit 22bc7ed4fabac48caee0275b774781e1283f1155, reversing
    changes made to 2c60ea748ebe33c4aedb13f13f2e2c575f2c56de.

--- src/porepy/numerics/time_stepper.py
+++ src/porepy/numerics/time_stepper.py
@@ -2,7 +2,8 @@ import numpy as np
 import logging
 import time
 
-import porepy as pp
+from porepy.grids.grid_bucket import GridBucket
+from porepy.numerics.linalg.linsolve import Factory as LSFactory
 
 logger = logging.getLogger(__name__)
 
@@ -14,35 +15,44 @@ class AbstractSolver(object):
     where G(T) is a space discretization
     """
 
-    def __init__(self, model, dt=1., end_time=1.):
+    def __init__(self, problem):
         """
         Parameters:
         ----------
-        model: a model class. Must have the attributes
-            model.grid()
-            model.data()
-            model.space_disc()
-            model.mass_disc()
-            model.initial_condition()
+        problem: a problem class. Must have the attributes
+            problem.grid()
+            problem.data()
+            problem.space_disc()
+            problem.time_disc()
+            problem.time_step()
+            problem.end_time()
+            problem.initial_pressure()
         """
         # Get data
-        self.model = model
-        self.gb = model.grid()
-        self.data = model.data()
-        self.dt = dt
-        self.T = end_time
-        self.space_disc = model.space_disc()
-        self.mass_disc = model.mass_disc()
-        self.x0 = model.initial_condition()
-        self.x = self.x0
+        g = problem.grid()
+
+        data = problem.data()
+        data[problem.physics] = []
+        data['times'] = []
+
+        p0 = problem.initial_condition()
+        p = p0
+
+        self.problem = problem
+        self.g = g
+        self.data = data
+        self.dt = problem.time_step()
+        self.T = problem.end_time()
+        self.space_disc = problem.space_disc()
+        self.time_disc = problem.time_disc()
+        self.p0 = p0
+        self.p = p
         # First initial empty lhs and rhs, then initialize them through
-        # assemble
+        # reassemble
         self.lhs = []
         self.rhs = []
-        self.rhs_var = []
-        self.rhs_const = []
 
-    def solve(self, file_name=None, var_name='solution', save_every=1):
+    def solve(self, save_as=None, save_every=1):
         """
         Solve problem.
         """
@@ -50,47 +60,48 @@ class AbstractSolver(object):
         logger.warning('Time stepping using ' + str(nt) + ' steps')
         t = self.dt
         counter = 0
-        if not file_name is None:
-            exporter = pp.Exporter(self.gb, file_name)
-            self.model.split(self.x, var_name)
-            self.model.exporter.write_vtk([var_name], time_step=counter)
+        if not save_as is None:
+            self.problem.split(save_as)
+            self.problem.exporter.write_vtk([save_as], time_step=counter)
             times = [0.0]
 
         while t < self.T *(1 + 1e-14):
             logger.warning('Step ' + str(counter) + ' out of ' + str(nt))
             counter += 1
-            self.assemble()
-            self.update()
+            self.update(t)
+            self.reassemble()
             self.step()
-            logger.debug('Maximum value ' + str(self.x.max()) +\
-                         ', minimum value ' + str(self.x.min()))
-            if not file_name is None and np.mod(counter, save_every)==0:
+            logger.debug('Maximum value ' + str(self.p.max()) +\
+                         ', minimum value ' + str(self.p.min()))
+            if not save_as is None and np.mod(counter, save_every)==0:
                 logger.info('Saving solution')
-                self.model.split(self.x, var_name)
-                exporter.write_vtk([var_name], time_step=counter)
+                self.problem.split(save_as)
+                self.problem.exporter.write_vtk([save_as], time_step=counter)
                 times.append(t)
                 logger.info('Finished saving')
             t += self.dt
 
-        if not file_name is None:
-            exporter.write_pvd(np.asarray(times))
+        if not save_as is None:
+            self.problem.exporter.write_pvd(np.asarray(times))
 
         return self.data
 
     def step(self):
         """
-        Take one time step.
+        Take one time step
         """
-        raise NotImplementedError(
-            'subclass must overload function reasemble()')
+        ls = LSFactory()
+        self.p = ls.direct(self.lhs, self.rhs)
+        return self.p
 
-    def update(self):
+    def update(self, t):
         """
         update parameters for next time step
         """
-        self.x0 = self.x
+        self.problem.update(t)
+        self.p0 = self.p
 
-    def assemble(self):
+    def reassemble(self):
         """
         reassemble matrices. This must be called between every time step to
         update the rhs of the system.
@@ -99,20 +110,20 @@ class AbstractSolver(object):
             'subclass must overload function reasemble()')
 
     def _discretize(self, discs):
-        if isinstance(self.gb, pp.GridBucket):
+        if isinstance(self.g, GridBucket):
             if not isinstance(discs, tuple):
                 discs = [discs]
-            lhs, rhs = np.array(discs[0].matrix_rhs(self.gb))
+            lhs, rhs = np.array(discs[0].matrix_rhs(self.g))
             for disc in discs[1:]:
-                lhs_n, rhs_n = disc.matrix_rhs(self.gb)
+                lhs_n, rhs_n = disc.matrix_rhs(self.g)
                 lhs += lhs_n
                 rhs += rhs_n
         else:
             if not isinstance(discs, tuple):
                 discs = [discs]
-            lhs, rhs = discs[0].matrix_rhs(self.gb, self.data)
+            lhs, rhs = discs[0].matrix_rhs(self.g, self.data)
             for disc in discs[1:]:
-                lhs_n, rhs_n = disc.matrix_rhs(self.gb, self.data)
+                lhs_n, rhs_n = disc.matrix_rhs(self.g, self.data)
                 lhs += lhs_n
                 rhs += rhs_n
         return lhs, rhs
@@ -123,19 +134,16 @@ class Implicit(AbstractSolver):
     Implicit time discretization:
     (y_k+1 - y_k) / dt = F^k+1
     """
-    def step(self):
-        ls = pp.LSFactory()
-        self.rhs = self.rhs_var * self.x0 + self.rhs_const
-        self.x = ls.direct(self.lhs, self.rhs)
-        return self.x
 
-    def assemble(self):
+    def __init__(self, problem):
+        AbstractSolver.__init__(self, problem)
+
+    def reassemble(self):
         lhs_flux, rhs_flux = self._discretize(self.space_disc)
-        lhs_mass, rhs_mass = self._discretize(self.mass_disc)
+        lhs_time, rhs_time = self._discretize(self.time_disc)
 
-        self.rhs_var = lhs_mass / self.dt
-        self.rhs_const = rhs_mass / self.dt + rhs_flux
-        self.lhs = lhs_mass / self.dt + lhs_flux
+        self.lhs = lhs_time + lhs_flux
+        self.rhs = lhs_time * self.p0 + rhs_flux + rhs_time
 
 
 class BDF2(AbstractSolver):
@@ -144,63 +152,83 @@ class BDF2(AbstractSolver):
     (y_k+2 - 4/3 * y_k+1 + 1/3 * y_k) / dt = 2/3 * F^k+2
     """
 
-    def __init__(self, *vargs, **kwargs):
-        AbstractSolver.__init__(self, *vargs, **kwargs)
-        self.x_1 = self.x0
+    def __init__(self, problem):
         self.flag_first = True
-        self.did_step = False
+        AbstractSolver.__init__(self, problem)
+        self.p_1 = self.p0
 
-    def step(self):
-        if self.flag_first:
-            bdf2_rhs = self.rhs_var * self.x0
-        else:
-            bdf2_rhs = 4. / 3 * self.rhs_var * self.x0 - \
-                       1. / 3 * self.rhs_var * self.x_1
-        self.rhs = bdf2_rhs + self.rhs_const
-
-        ls = pp.LSFactory()
-        self.x = ls.direct(self.lhs, self.rhs)
-        self.did_step = True
-
-    def update(self):
+    def update(self, t):
         """
         update parameters for next time step
         """
-        if self.flag_first and self.did_step:
+        if t > self.dt + 1e-6:
             self.flag_first = False
-        self.x_1 = self.x0
-        AbstractSolver.update(self)
-
-    def assemble(self):
-        lhs_flux, rhs_flux = self._discretize(self.space_disc)
-        lhs_mass, rhs_mass = self._discretize(self.mass_disc)
-        self.rhs_var = lhs_mass / self.dt
-        if self.flag_first:
-            self.lhs = lhs_mass / self.dt + lhs_flux
-            self.rhs_const = rhs_flux + rhs_mass / self.dt
         else:
-            self.lhs = lhs_mass / self.dt + 2. / 3 * lhs_flux
-            self.rhs_const = 2. / 3 * rhs_flux + rhs_mass / self.dt
+            self.flag_first = True
+        self.p_1 = self.p0
+        AbstractSolver.update(self, t)
+
+    def reassemble(self):
+
+        lhs_flux, rhs_flux = self._discretize(self.space_disc)
+        lhs_time, rhs_time = self._discretize(self.time_disc)
+
+        if self.flag_first:
+            self.lhs = lhs_time + lhs_flux
+            self.rhs = lhs_time * self.p0 + rhs_flux + rhs_time
+        else:
+            self.lhs = lhs_time + 2. / 3 * lhs_flux
+            bdf2_rhs = 4. / 3 * lhs_time * self.p0 - 1. / 3 * lhs_time * self.p_1
+            self.rhs = bdf2_rhs + 2. / 3 * rhs_flux + rhs_time
+
 
 class Explicit(AbstractSolver):
     """
     Explicit time discretization:
     (y_k - y_k-1)/dt = F^k
     """
-    def step(self):
-        ls = pp.LSFactory()
-        self.rhs = self.rhs_var * self.x0 + self.rhs_const
-        self.x = ls.direct(self.lhs, self.rhs)
-        return self.x
 
-    def assemble(self):
+    def __init__(self, problem):
+        AbstractSolver.__init__(self, problem)
+
+    def solve(self, save_as=None, save_every=1):
+        """
+        Solve problem.
+        """
+        t = 0.0
+        counter = 0
+        # Save initial condition
+        if not save_as is None:
+            self.problem.split(save_as)
+            self.problem.exporter.write_vtk([save_as], time_step=counter)
+            times = [0.0]
+
+        while t < self.T - self.dt + 1e-14:
+            self.update(t)
+            self.reassemble()
+            self.step()
+            # Save time step
+            if not save_as is None and np.mod(counter, save_every)==0:
+                logger.info('Saving solution')
+                self.problem.split(save_as)
+                self.problem.exporter.write_vtk([save_as], time_step=counter)
+                times.append(t)
+                logger.info('Finished saving')
+            t += self.dt
+
+        # Write pvd
+        if not save_as is None:
+            self.problem.exporter.write_pvd(np.asarray(times))
+
+        return self.data
+
+    def reassemble(self):
 
         lhs_flux, rhs_flux = self._discretize(self.space_disc)
-        lhs_mass, rhs_mass = self._discretize(self.mass_disc)
+        lhs_time, rhs_time = self._discretize(self.time_disc)
 
-        self.lhs = lhs_mass / self.dt
-        self.rhs_var = (lhs_mass / self.dt - lhs_flux)
-        self.rhs_const = rhs_flux + rhs_mass
+        self.lhs = lhs_time
+        self.rhs = (lhs_time - lhs_flux) * self.p0 + rhs_flux + rhs_time
 
 
 class CrankNicolson(AbstractSolver):
@@ -209,37 +237,32 @@ class CrankNicolson(AbstractSolver):
     (y_k+1 - y_k) / dt = 0.5 * (F^k+1 + F^k)
     """
 
-    def __init__(self, model, **kwargs):
-        AbstractSolver.__init__(self, model, **kwargs)
-        self.lhs_flux, self.rhs_flux = self._discretize(model.space_disc())
-        self.lhs_mass, self.rhs_mass = self._discretize(model.mass_disc())
+    def __init__(self, problem):
+        self.g = problem.grid()
+        self.lhs_flux, self.rhs_flux = self._discretize(problem.space_disc())
+        self.lhs_time, self.rhs_time = self._discretize(problem.time_disc())
         self.lhs_flux_0 = self.lhs_flux
         self.rhs_flux_0 = self.rhs_flux
-        self.lhs_mass_0 = self.lhs_mass
-        self.rhs_mass_0 = self.rhs_mass
+        self.lhs_time_0 = self.lhs_time
+        self.rhs_time_0 = self.rhs_time
+        AbstractSolver.__init__(self, problem)
 
-    def step(self):
-        self.rhs = self.rhs_var * self.x0 + self.rhs_const
-        ls = pp.LSFactory()
-        self.x = ls.direct(self.lhs, self.rhs)
-        return self.x
-
-    def update(self):
+    def update(self, t):
         """
         update parameters for next time step
         """
-        AbstractSolver.update(self)
+        AbstractSolver.update(self, t)
         self.lhs_flux_0 = self.lhs_flux
         self.rhs_flux_0 = self.rhs_flux
-        self.lhs_mass_0 = self.lhs_mass
-        self.rhs_mass_0 = self.rhs_mass
+        self.lhs_time_0 = self.lhs_time
+        self.rhs_time_0 = self.rhs_time
 
-    def assemble(self):
+    def reassemble(self):
         self.lhs_flux, self.rhs_flux = self._discretize(self.space_disc)
-        self.lhs_mass, self.rhs_mass = self._discretize(self.mass_disc)
+        self.lhs_time, self.rhs_time = self._discretize(self.time_disc)
 
-        self.lhs = self.lhs_mass / self.dt + 0.5 * self.lhs_flux
-        self.rhs_var = self.lhs_mass / self.dt - 0.5 * self.lhs_flux_0
-        rhs1 = 0.5 * (self.rhs_flux + self.rhs_mass / self.dt)
-        rhs0 = 0.5 * (self.rhs_flux_0 + self.rhs_mass_0 / self.dt)
-        self.rhs_const = rhs1 + rhs0
+        rhs1 = 0.5 * (self.rhs_flux + self.rhs_time)
+        rhs0 = 0.5 * (self.rhs_flux_0 + self.rhs_time_0)
+        self.lhs = self.lhs_time + 0.5 * self.lhs_flux
+        self.rhs = (self.lhs_time - 0.5 * self.lhs_flux_0) * \
+            self.p0 + rhs1 + rhs0
--- test/integration/test_compressible_flow.py
+++ test/integration/test_compressible_flow.py
@@ -1,58 +1,53 @@
 import unittest
 import numpy as np
 
-import porepy as pp
+from porepy.numerics import compressible
+from porepy.grids import structured
+from porepy.fracs import meshing
+from porepy.params import tensor
+
 
 class TestCompressibleFlow(unittest.TestCase):
 
     def test_3d_Fracture_problem(self):
-        print('asdfasdæ')
         problem = UnitSquareInjectionMultiDim()
-        time_stepper = pp.Implicit(problem, dt=.005)
-
-        t = 0
-        time_stepper.assemble()
-        while t < 0.05 - 1e-6:
-            t += 0.005
-            time_stepper.update()
-            time_stepper.assemble()
-            time_stepper.step()
-
-        problem.pressure(time_stepper.x, 'pressure')
+        problem.solve()
+        problem.pressure('pressure')
         for g, d in problem.grid():
             if g.dim == 3:
                 pT = d['pressure']
         p_refT = _reference_solution_multi_grid()
+
         assert np.allclose(pT, p_refT)
 
 
 ###############################################################################
 
 
-class MatrixDomain(pp.SlightlyCompressibleDataAssigner):
+class MatrixDomain(compressible.SlightlyCompressibleDataAssigner):
     def __init__(self, g, d):
-        pp.SlightlyCompressibleDataAssigner.__init__(self, g, d)
+        compressible.SlightlyCompressibleDataAssigner.__init__(self, g, d)
 
 
-class FractureDomain(pp.SlightlyCompressibleDataAssigner):
+class FractureDomain(compressible.SlightlyCompressibleDataAssigner):
     def __init__(self, g, d):
-        pp.SlightlyCompressibleDataAssigner.__init__(self, g, d)
+        compressible.SlightlyCompressibleDataAssigner.__init__(self, g, d)
         aperture = np.power(0.001, 3 - g.dim)
         self.data()['param'].set_aperture(aperture)
 
     def permeability(self):
         kxx = 1000 * np.ones(self.grid().num_cells)
-        return pp.SecondOrderTensor(3, kxx)
+        return tensor.SecondOrderTensor(3, kxx)
 
 
 class IntersectionDomain(FractureDomain):
     def __init__(self, g, d):
         FractureDomain.__init__(self, g, d)
 
-    def source(self, _):
+    def source(self, t):
         assert self.grid().num_cells == 1, '0D grid should only have 1 cell'
-        f = .4 * self.grid().cell_volumes  # forgot scaling wiht aperture, but who cares
-        return f
+        f = .4 * self.grid().cell_volumes  # m**3/s
+        return f * (t < .05)
 
 
 def set_sub_problems(gb):
@@ -68,18 +63,35 @@ def set_sub_problems(gb):
             raise ValueError('Unkown grid-dimension %d' % g.dim)
 
 
-class UnitSquareInjectionMultiDim(pp.SlightlyCompressibleModel):
+class UnitSquareInjectionMultiDim(compressible.SlightlyCompressibleModel):
 
     def __init__(self):
+
         f_1 = np.array([[0, 1, 1, 0], [.5, .5, .5, .5], [0, 0, 1, 1]])
         f_2 = np.array([[.5, .5, .5, .5], [0, 1, 1, 0], [0, 0, 1, 1]])
         f_3 = np.array([[0, 1, 1, 0], [0, 0, 1, 1], [.5, .5, .5, .5]])
         f_set = [f_1, f_2, f_3]
 
-        gb = pp.meshing.cart_grid(f_set, [4, 4, 4], physdims=[1, 1, 1])
-        set_sub_problems(gb)
+        g = meshing.cart_grid(f_set, [4, 4, 4], physdims=[1, 1, 1])
+        g.compute_geometry()
+        g.assign_node_ordering()
+        set_sub_problems(g)
+        self.g = g
         # Initialize base class
-        pp.SlightlyCompressibleModel.__init__(self, gb, 'flow')
+        compressible.SlightlyCompressibleModel.__init__(self, self.g, 'flow')
+
+    #--------grid function--------
+
+    def grid(self):
+        return self.g
+
+    #--------Time stepping------------
+
+    def time_step(self):
+        return .005
+
+    def end_time(self):
+        return 0.05
 
 ###############################################################################
 
