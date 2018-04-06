919e87253c59dac3990cd995110eeacfcc6760b7
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2018-04-06 13:41:36 +0200

    add plot_wells functionatily to plot the wells

--- src/porepy/viz/fracture_visualization.py
+++ src/porepy/viz/fracture_visualization.py
@@ -60,4 +60,55 @@ def plot_fractures(d, p, c, colortag=None, **kwargs):
             color=col[tagmap[i]],
         )
     # Finally set axis
-    plt.show()
+    if kwargs.get("plot", True):
+        plt.show()
+
+def plot_wells(d, w, colortag=None, **kwargs):
+    """
+    Plot 2d wells as points in a domain.
+
+    The function is primarily intended for data exploration.
+
+    Parameters:
+        d (dictionary): Domain size. Should contain fields xmin, xmax, ymin,
+            ymax.
+        w (np.ndarray, dims 2 x npt): Coordinates of the wells.
+        colortag (np.ndarray, dim n_w, optional): Colorcoding for wells.
+            If provided, different colors will be asign to the different wells.
+            Defaults to all wells being black.
+        kwargs: Keyword arguments passed on to matplotlib.
+
+    """
+
+    # Assign a color to each tag. We define these by RBG-values (simplest
+    # option in pyplot).
+    # For the moment, some RBG values are hard coded, do something more
+    # intelligent if necessary.
+    if colortag is None:
+        tagmap = np.zeros(c.shape[1], dtype='int')
+        col = [(0, 0, 0)];
+    else:
+        utag, tagmap = np.unique(colortag, return_inverse=True)
+        ntag = utag.size
+        if ntag <= 3:
+            col = [(1, 0, 0), (0, 1, 0), (0, 0, 1)]
+        elif ntag < 6:
+            col = [(1, 0, 0), (0, 1, 0), (0, 0, 1),
+                   (1, 1, 0), (1, 0, 1), (0, 0, 1)]
+        else:
+            raise NotImplementedError('Have not thought of more than six colors')
+
+    fig = plt.figure(kwargs.get("fig_id", 1))
+    plt.axis([d['xmin'], d['xmax'], d['ymin'], d['ymax']])
+    plt.plot([d['xmin'], d['xmax'], d['xmax'], d['xmin'], d['xmin']],
+             [d['ymin'], d['ymin'], d['ymax'], d['ymax'], d['ymin']],
+             '-', color='red')
+
+    # Simple for-loop to draw one well after another. Not fancy, but it
+    # serves its purpose.
+    for i, well in enumerate(w.T):
+        plt.plot(*well, 'o', color=col[tagmap[i]])
+
+    # Finally set axis
+    if kwargs.get("plot", True):
+        plt.show()
