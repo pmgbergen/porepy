c42734c551080cb533b01481bfaa526f03c0bea6
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2018-04-11 13:29:45 +0200

    add water parameter

--- src/porepy/__init__.py
+++ src/porepy/__init__.py
@@ -68,6 +68,7 @@ from porepy.params.bc import BoundaryCondition
 from porepy.params.tensor import SecondOrderTensor, FourthOrderTensor
 from porepy.params.data import Parameters
 from porepy.params.rock import UnitRock, Shale, SandStone, Granite
+from porepy.params.water import Water
 
 # Visualization
 from porepy.viz.exporter import Exporter
