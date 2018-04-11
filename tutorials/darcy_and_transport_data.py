6ff058d8eef97820bb50661ab3b21dad197fcfcc
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2018-04-11 13:30:09 +0200

    update tutorial example of Darcy+heat

--- tutorials/darcy_and_transport_data.py
+++ tutorials/darcy_and_transport_data.py
@@ -5,14 +5,15 @@ from porepy.numerics import darcy_and_transport
 
 #------------------------------------------------------------------------------#
 
-def create_grid(file_name, domain, mesh_size, tol):
+def create_grid(data):
 
     # pack the data for the grid generation
-    mesh_kwargs = {'tol': tol}
-    mesh_kwargs['mesh_size_frac'] = mesh_size
+    mesh_kwargs = {'tol': data['tol']}
+    mesh_kwargs['mesh_size_frac'] = data['mesh_size']
     kwargs = {'assign_fracture_id': True}
 
     # import the fractures and create the grid
+    file_name, domanin = data['file'], data['domain']
     gb = pp.importer.dfm_2d_from_csv(file_name, mesh_kwargs, domain, **kwargs)
     gb.compute_geometry()
     gb.assign_node_ordering()
@@ -21,32 +22,22 @@ def create_grid(file_name, domain, mesh_size, tol):
 
 #------------------------------------------------------------------------------#
 
-def assign_data(gb, data_problem, data_class, data_key):
-    """
-    Loop over grids to assign flow problem data.
-    Darcy data_key: problem
-    Transport data_key: transport_data
-    """
-    gb.add_node_props([data_key])
+def compute_data(data):
 
-    wells_fracture = data_problem['wells_fracture']
-    wells = data_problem['wells']
-    for g, _ in gb:
-        if g.dim == 1:
-            well_id = np.isin(wells_fracture, g.tags['fracture_id'])
-            if np.any(well_id):
-                wells_id = np.where(well_id)[0]
-                wells_cell = g.closest_cell(wells[:, wells_id])
-            else:
-                wells_cell, wells_id = [np.nan]*2
-        else:
-            wells_cell, wells_id = [np.nan]*2
+    theta = data['depth']*data['geothermal_gradient']+\
+            data['atmospheric_temperature']
+    water = pp.Water(theta)
 
-        g.tags['wells_cell'] = wells_cell
-        g.tags['wells_id'] = wells_id
+    data = {'bc_pressure': \
+            water.density()*data['depth']*pp.GRAVITY_ACCELERATION+1*(pp.BAR),
+            'bc_temperature': \
+            theta,
+            'kf': \
+            data['fracture']['permeability']/water.dynamic_viscosity(),
+            'km':\
+            data['rock']['permeability']/water.dynamic_viscosity(),
+            }
 
-    for g, d in gb:
-        d[data_key] = data_class(g, d, **data_problem)
 
 #------------------------------------------------------------------------------#
 
@@ -66,30 +57,22 @@ class FlowData(pp.EllipticDataAssigner):
     Assign flow problem data to a given grid.
     """
 
-    def __init__(self, g, d, **kwargs):
+    def __init__(self, g, d, **data):
 
-        self.tol = kwargs.get('tol', 1e-6)
-        self.domain = kwargs['domain']
-
-        self.kf = kwargs['kf']
-        self.km = kwargs['km']
-
-        self.reference_aperture = kwargs['aperture']
-        self.bc_pressure = kwargs['bc_pressure']
-        self.wells_flow_rate = kwargs['wells_flow_rate']
-
+        self.data = data
+        self.tol = self.data['tol']
         pp.EllipticDataAssigner.__init__(self, g, d)
 
     def aperture(self):
-        a = np.power(self.reference_aperture, 2-self.grid().dim)
+        a = np.power(self.data['fracture']['aperture'], 2-self.grid().dim)
         return np.ones(self.grid().num_cells) * a
 
     def permeability(self):
         ones = np.ones(self.grid().num_cells)
         if self.grid().dim == 2:
-            return pp.SecondOrderTensor(3, self.km*ones)
+            return pp.SecondOrderTensor(3, self.data['km']*ones)
         else:
-            return pp.SecondOrderTensor(3, self.kf*ones)
+            return pp.SecondOrderTensor(3, self.data['kf']*ones)
 
     def bc(self):
         bound_faces = self.grid().tags['domain_boundary_faces'].nonzero()[0]
@@ -99,15 +82,15 @@ class FlowData(pp.EllipticDataAssigner):
     def bc_val(self):
         bound_faces = self.grid().tags['domain_boundary_faces'].nonzero()[0]
         bc_val = np.zeros(self.grid().num_faces)
-        bc_val[bound_faces] = self.bc_pressure
+        bc_val[bound_faces] = self.data['bc_pressure']
         return bc_val
 
     def source(self):
         val = np.zeros(self.grid().num_cells)
-        wells_cell = self.grid().tags['wells_cell']
-        wells_id = self.grid().tags['wells_id']
+        wells_cell = self.grid().tags['wells']['cell']
+        wells_id = self.grid().tags['wells']['id']
         if np.isfinite(wells_cell):
-            val[wells_cell] = self.wells_flow_rate[wells_id]
+            val[wells_cell] = self.data['wells']['flow_rate'][wells_id]
         return val
 
 #------------------------------------------------------------------------------#
@@ -117,15 +100,10 @@ class TransportData(pp.ParabolicDataAssigner):
     Assign transport problem data to given grid.
     """
 
-    def __init__(self, g, d, **kwargs):
+    def __init__(self, g, d, **data):
 
-        self.tol = kwargs.get('tol', 1e-6)
-        self.domain = kwargs['domain']
-
-        self.reference_aperture = kwargs['aperture']
-        self.bc_temperature = kwargs['bc_temperature']
-        self.wells_temperature = kwargs['wells_temperature']
-
+        self.data = data
+        self.tol = self.data['tol']
         pp.ParabolicDataAssigner.__init__(self, g, d)
 
     def bc(self):
@@ -136,22 +114,22 @@ class TransportData(pp.ParabolicDataAssigner):
     def bc_val(self, _):
         bound_faces = self.grid().tags['domain_boundary_faces'].nonzero()[0]
         bc_val = np.zeros(self.grid().num_faces)
-        bc_val[bound_faces] = self.bc_temperature
+        bc_val[bound_faces] = self.data['bc_temperature']
         return bc_val
 
     def initial_condition(self):
-        return self.bc_temperature*np.ones(self.grid().num_cells)
+        return self.data['bc_temperature']*np.ones(self.grid().num_cells)
 
     def aperture(self):
-        a = np.power(self.reference_aperture, 2-self.grid().dim)
+        a = np.power(self.data['fracture']['aperture'], 2-self.grid().dim)
         return np.ones(self.grid().num_cells) * a
 
     def source(self, _):
         val = np.zeros(self.grid().num_cells)
-        wells_cell = self.grid().tags['wells_cell']
-        wells_id = self.grid().tags['wells_id']
+        wells_cell = self.grid().tags['wells']['cell']
+        wells_id = self.grid().tags['wells']['id']
         if np.isfinite(wells_cell):
-            val[wells_cell] = self.wells_temperature[wells_id]*\
+            val[wells_cell] = self.data['wells']['temperature'][wells_id]*\
                               self.grid().cell_volumes[wells_cell]
         return val
 
@@ -162,12 +140,10 @@ class TransportSolver(pp.ParabolicModel):
     Make a ParabolicModel for the transport problem with specified parameters.
     """
 
-    def __init__(self, gb, **data_problem):
+    def __init__(self, gb, **data):
 
-        time_step = data_problem['end_time'] /\
-                    float(data_problem['number_time_steps'])
-        pp.ParabolicModel.__init__(self, gb, time_step=time_step,
-                                          **data_problem)
+        time_step = data['end_time']/float(data['number_time_steps'])
+        pp.ParabolicModel.__init__(self, gb, time_step=time_step, **data)
 
 #------------------------------------------------------------------------------#
 
@@ -176,20 +152,46 @@ class Simulation(darcy_and_transport.DarcyAndTransport):
     Combine the two problems for convenience.
     """
 
-    def __init__(self, gb, data_problem):
+    def __init__(self, data):
 
-        # find the wells
+        # create the computational grid
+        gb = create_grid(data)
 
         # Darcy problem
-        assign_data(gb, data_problem, FlowData, 'problem')
+        self.assign_data(gb, data, FlowData, 'problem')
         data_problem['file_name'] = 'pressure'
-        flow = pp.EllipticModel(gb, **data_problem)
+        flow = pp.EllipticModel(gb, **data)
 
         # transport problem
-        assign_data(gb, data_problem, TransportData, 'transport_data')
+        self.assign_data(gb, data, TransportData, 'transport_data')
         data_problem['file_name'] = 'temperature'
-        transport = TransportSolver(gb, **data_problem)
+        transport = TransportSolver(gb, **data)
 
         darcy_and_transport.DarcyAndTransport.__init__(self, flow, transport)
+
+    def assign_data(self, gb, data, data_class, data_key):
+        """
+        Loop over grids to assign flow problem data.
+        Darcy data_key: problem
+        Transport data_key: transport_data
+        """
+        gb.add_node_props([data_key])
+
+        wells = data['wells']['position']
+        for g, _ in gb:
+            if g.dim == 1:
+                well_id = np.isin(data['wells']['fracture'], g.tags['fracture_id'])
+                if np.any(well_id):
+                    wells_id = np.where(well_id)[0]
+                    wells_cell = g.closest_cell(wells[:, wells_id])
+                else:
+                    wells_cell, wells_id = [np.nan]*2
+            else:
+                wells_cell, wells_id = [np.nan]*2
+
+            g.tags['wells'] = {'cell': wells_cell, 'id': wells_id}
+
+        for g, d in gb:
+            d[data_key] = data_class(g, d, **data_problem)
 
 #------------------------------------------------------------------------------#
