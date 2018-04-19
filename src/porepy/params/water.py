fix in units

src/porepy/params/water.py
@@ -4,7 +4,7 @@ import porepy as pp
 class Water():
 
     def __init__(self, theta_ref=None):
-        if theta_reference is None:
+        if theta_ref is None:
             self.theta_ref = 20*(pp.CELSIUS)
         else:
             self.theta_ref = theta_ref
@@ -19,7 +19,7 @@ class Water():
             theta = self.theta_ref
         theta_0 = 10*(pp.CELSIUS)
         rho_0 = 999.8349*(pp.KILOGRAM/pp.METER**3)
-        return rho_0/(1.+thermal_expansion(theta-theta_0))
+        return rho_0/(1.+self.thermal_expansion(theta-theta_0))
 
     def thermal_conductivity(self, theta=None): # theta in CELSIUS
         if theta is None:
@@ -32,7 +32,7 @@ class Water():
     def specific_heat_capacity(self, theta=None): # theta in CELSIUS
         if theta is None:
             theta = self.theta_ref
-        return (4245-1.841*theta)/density(theta)
+        return (4245-1.841*theta)/self.density(theta)
 
     def dynamic_viscosity(self, theta=None): # theta in CELSIUS
         if theta is None:
@@ -40,4 +40,8 @@ class Water():
         theta = pp.CELSIUS_to_KELVIN(theta)
         mu_0 = 2.414*1e-5*(pp.PASCAL*pp.SECOND)
         return mu_0*np.power(10, 247.8/(theta-140))
+
+    def hydrostatic_pressure(self, depth, theta=None):
+        rho = self.density(theta)
+        return rho*depth*pp.GRAVITY_ACCELERATION+pp.ATMOSPHERIC_PRESSURE
 