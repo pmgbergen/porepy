7be5a4fbd67dfc50c808aef46ef77f131ee996eb
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2018-06-08 13:31:24 +0200

    update the multiscale procedure

--- examples/papers/multiscale/example_1.py
+++ examples/papers/multiscale/example_1.py
@@ -79,7 +79,6 @@ def make_grid_bucket(mesh_size, is_coarse=False):
     mesh_kwargs = {'mesh_size_frac': mesh_size,
                    'mesh_size_min': mesh_size / 20}
 
-
     domain = {'xmin': 0, 'xmax': 1, 'ymin': 0, 'ymax': 1}
 
     file_name = 'network_geiger.csv'
@@ -95,7 +94,7 @@ def make_grid_bucket(mesh_size, is_coarse=False):
 
 def main(kf, is_coarse=False):
 
-    mesh_size = 1#0.045
+    mesh_size = 0.045
     gb, domain = make_grid_bucket(mesh_size)
 
     # Assign parameters
@@ -105,15 +104,17 @@ def main(kf, is_coarse=False):
     solver_flow = pp.DualVEMMixedDim('flow')
     A, b = solver_flow.matrix_rhs(gb, return_bmat=True)
 
-    # Select the grid of higher dimension
+    # Select the grid of higher dimension and the position in the matrix A
     g_h = gb.grids_of_dimension(gb.dim_max())[0]
     pos_hn = [gb.node_props(g_h, 'node_number')]
+    # Determine the number of dofs [u, p] for the higher dimensional grid
     dof_h = A[pos_hn[0], pos_hn[0]].shape[0]
 
-    # Select the grid positions of one dimension down
+    # Select the co-dimensional grid positions
     pos_ln = [d['node_number'] for g, d in gb if g.dim < g_h.dim]
 
-    # select the positions of the mortars blocks
+    # select the positions of the mortars blocks, for both the high dimensional
+    # and lower dimensional grids
     num_nodes = gb.num_graph_nodes()
     pos_he = []
     pos_le = []
@@ -126,12 +127,13 @@ def main(kf, is_coarse=False):
 
     # extract the blocks for the higher dimension
     A_h = sps.bmat(A[np.ix_(pos_hn+pos_he, pos_hn+pos_he)])
+    b_h = np.concatenate(tuple(b[pos_hn+pos_he]))
+
+    # matrix that couple the 1 co-dimensional pressure to the mortar variables
     C_h = sps.bmat(A[np.ix_(pos_he, pos_ln)])
 
-    # Select the grid positions of lower dimension
-    A_l = sps.bmat(A[np.ix_(pos_ln+pos_le, pos_ln+pos_le)])
+    # Select the grid that realise the jump operator given the mortar variables
     C_l = sps.bmat(A[np.ix_(pos_ln, pos_he)])
-    print(C_h.shape)
 
     # construct the rhs with all the active pressure dof in the 1 co-dimension
     if_p = np.zeros(C_h.shape[1], dtype=np.bool)
@@ -139,6 +141,7 @@ def main(kf, is_coarse=False):
     for g, d in gb:
         if g.dim != g_h.dim:
             pos += g.num_faces
+            # only the 1 co-dimensional grids are interesting
             if g.dim == g_h.dim - 1:
                 if_p[pos:pos+g.num_cells] = True
             pos += g.num_cells
@@ -146,38 +149,61 @@ def main(kf, is_coarse=False):
     # compute the bases
     num_bases = np.sum(if_p)
     dof_bases = C_h.shape[0]
-    print(dof_bases)
-    bases = np.zeros((num_bases, C_h.shape[1]))
+    bases = np.zeros((C_h.shape[1], C_h.shape[1]))
 
-#    for e, d in gb.edges():
-#        g_m = d['mortar_grid']
-#        print(g_m.low_to_mortar_avg())
-#        print('---')
-
+    # we solve many times the same problem, better to factorize the matrix
     LU = sps.linalg.factorized(A_h.tocsc())
-    for idx, dof_basis in enumerate(np.where(if_p)[0]):
+
+    # solve to compute the ms bases functions for homogeneous boundary
+    # conditions
+    for dof_basis in np.where(if_p)[0]:
         rhs = np.zeros(if_p.size)
         rhs[dof_basis] = 1.
-        x = LU(np.concatenate((np.zeros(dof_h), -C_h*rhs)))
-        bases[idx, if_p] = x[-dof_bases:]
+        # project from the co-dimensional pressure to the Robin boundary
+        # condition
+        rhs = np.concatenate((np.zeros(dof_h), -C_h*rhs))
+        x = LU(rhs)
+        # compute the jump of the mortars
+        bases[:, dof_basis] = C_l*x[-dof_bases:]
+
+        #save = pp.Exporter(g_h, "vem"+str(dof_basis), folder="vem_ms")
+        #save.write_vtk({'pressure': x[g_h.num_faces:dof_h]})
+
+    # solve for non-zero boundary conditions
+    x_h = LU(b_h)
 
     # construct the problem in the fracture network
-    print(bases)
+    A_l = np.empty((2, 2), dtype=np.object)
+    b_l = np.empty(2, dtype=np.object)
+    # the multiscale bases are thus inserted in the right block of the lower
+    # dimensional problem
+    A_l[0, 0] = sps.bmat(A[np.ix_(pos_ln, pos_ln)])+sps.csr_matrix(bases)
+    dof_l = A_l[0, 0].shape[0]
+    # add to the righ-hand side the non-homogenous solution from the higher
+    # dimensional problem
+    b_l[0] = np.concatenate(tuple(b[pos_ln]))-C_l*x_h[-dof_bases:]
+    # in the case of > 1 co-dimensional problems
+    if len(pos_le) > 0:
+        A_l[0, 1] = sps.bmat(A[np.ix_(pos_ln, pos_le)])
+        A_l[1, 0] = sps.bmat(A[np.ix_(pos_le, pos_ln)])
+        A_l[1, 1] = sps.bmat(A[np.ix_(pos_le, pos_le)])
+        b_l[1] = np.concatenate(tuple(b[pos_le]))
 
+    # assemble and solve for the network
+    A_l = sps.bmat(A_l, 'csr')
+    b_l = np.concatenate(tuple(b_l))
+    x_l = sps.linalg.spsolve(A_l, b_l)
 
-    print(C_l.shape, bases.shape)
+    # compute the higher dimensional solution
+    rhs = x_l[:if_p.size]
+    rhs = np.concatenate((np.zeros(dof_h), -C_h*rhs))
+    x_h = LU(b_h+rhs)
 
-#        save = pp.Exporter(g_h, "vem_"+str(idx)+"_", folder='result')
-#        save.write_vtk({'pressure': x[g_h.num_faces:dof_h]})
-
-    dss
-
-    solver_source = pp.DualSourceMixedDim('flow')
-
-    A_source, b_source = solver_source.matrix_rhs(gb)
-
-    up = sps.linalg.spsolve(A_flow + A_source, b_flow + b_source)
-    solver_flow.split(gb, "up", up)
+    # save and export using standard algorithm
+    x = np.zeros(x_h.size+x_l.size)
+    x[:dof_h] = x_h[:dof_h]
+    x[dof_h:(dof_h+dof_l)] = x_l[:dof_l]
+    solver_flow.split(gb, "up", x)
 
     gb.add_node_props(["discharge", 'pressure', "P0u"])
     solver_flow.extract_u(gb, "up", "discharge")
@@ -190,5 +216,5 @@ def main(kf, is_coarse=False):
 #------------------------------------------------------------------------------#
 
 if __name__ == "__main__":
-    kf = 1e2
+    kf = 1e-2
     main(kf)
