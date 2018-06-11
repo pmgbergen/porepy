7b43fd03079eb8972b3e27f928da8e72312f5e14
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2018-06-11 15:56:43 +0200

    script update

--- examples/papers/benchmark_2/single/script.py
+++ examples/papers/benchmark_2/single/script.py
@@ -57,13 +57,17 @@ def plot_over_line(file_in, file_out, pts, resolution=50000):
 
 #------------------------------------------------------------------------------#
 
-def post_process(file_out, fields):
+def read_csv(file_in, fields):
     # post-process the file by selecting only few columns
     data = list(list() for _ in fields)
-    with open(file_out, 'r') as csvfile:
+    with open(file_in, 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         [d.append(row[f]) for row in reader for f, d in zip(fields, data)]
+    return data
 
+#------------------------------------------------------------------------------#
+
+def write_csv(file_out, fields, data):
     with open(file_out, 'w') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fields)
         writer.writeheader()
@@ -71,31 +75,66 @@ def post_process(file_out, fields):
 
 #------------------------------------------------------------------------------#
 
+def cot_domain(transport_root, file_in, step, field, padding=6):
+
+    vtk_reader = read_file(file_in)
+    cell_volumes = read_data(vtk_reader, "cell_volumes")
+
+    cot = np.zeros(step)
+    for i in np.arange(step):
+        file_in = transport_root+str(i).zfill(padding)+".vtu"
+        vtk_reader = read_file(file_in)
+        c = read_data(vtk_reader, field)
+        cot[i] = np.sum(c*cell_volumes)
+
+    return cot
+
+#------------------------------------------------------------------------------#
+
 if __name__ == "__main__":
 
     # 1) $\int_{\Omega_f} \porosity c \, \mathrm{d}x$ $([-])$ vs. time
-    field = "pressure" # should be c
-    file_in = "./rt0_results/sol_3.vtu"
 
-    vtk_reader = read_file(file_in)
-    c = read_data(vtk_reader, field)
-    cell_volumes = read_data(vtk_reader, "cell_volumes")
-    #phi = read_data(vtk_reader, "phi")
-    print(np.sum(c*cell_volumes))
+    transport_root = "./vem_results/tracer_2_"
+
+    # in this file the constant data are saved
+    file_in = "./vem_results/sol_2.vtu"
+
+    field = "tracer"
+    step = 51
+
+    cot = cot_domain(transport_root, file_in, step, field)
+    times = np.arange(51)*2e6
+    file_out = "./vem_results/cot_frac.csv"
+    write_csv(file_out, ['time', 'c'], [times, cot])
 
     # 2) $\int_{\Omega_3} \porosity c \, \mathrm{d}x$ $([-])$ vs. time
+
+    transport_root = "./vem_results/tracer_3_"
+
+    # in this file the constant data are saved
+    file_in = "./vem_results/sol_2.vtu"
+
+    field = "tracer"
+    step = 51
+
+    cot = cot_domain(transport_root, file_in, step, field)
+    times = np.arange(51)*2e6
+    file_out = "./vem_results/cot_frac.csv"
+    write_csv(file_out, ['time', 'c'], [times, cot])
 
     # 4) plot of the pressure head in the matrix, along
     #    (0, 100, 100)-(100, 0, 0)
 
     field = "pressure"
     # file of the matrix
-    file_in = "./rt0_results/sol_3.vtu"
-    file_out = "./rt0_results/pol.csv"
+    file_in = "./vem_results/sol_3.vtu"
+    file_out = "./vem_results/pol.csv"
     pts = [[0, 100, 100], [100, 0, 0]]
 
     plot_over_line(file_in, file_out, pts)
-    post_process(file_out, ['arc_length', field])
+    data = read_csv(file_out, ['arc_length', field])
+    write_csv(file_out, ['arc_length', field], data)
 
     # 5) plot of $c$ in the matrix, at the final simulation time, along
     #    (0, 100, 100)-(100, 0, 0)
@@ -107,7 +146,8 @@ if __name__ == "__main__":
     pts = [[0, 100, 100], [100, 0, 0]]
 
     plot_over_line(file_in, file_out, pts)
-    post_process(file_out, ['arc_length', field])
+    data = read_csv(file_out, ['arc_length', field])
+    write_csv(file_out, ['arc_length', field], data)
 
     # 6) plot of $c$ within the fracture at the final simulation time along
     #    (0, 100, 80)-(100, 0, 20)
@@ -119,4 +159,5 @@ if __name__ == "__main__":
     pts = [[0, 100, 80], [100, 0, 20]]
 
     plot_over_line(file_in, file_out, pts)
-    post_process(file_out, ['arc_length', field])
+    data = read_csv(file_out, ['arc_length', field])
+    write_csv(file_out, ['arc_length', field], data)
