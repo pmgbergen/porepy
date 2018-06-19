974277fbeef99087d9eb05e3969c4b8a8f069687
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2018-06-19 14:35:55 +0200

    Improved unit tests for fv boundary pressure recovery

--- test/unit/test_fv_boundary_pressure_recovery.py
+++ test/unit/test_fv_boundary_pressure_recovery.py
@@ -362,15 +362,17 @@ class TestMpfaBoundaryPressure(unittest.TestCase):
 
         bf = np.where(g.tags['domain_boundary_faces'].ravel())[0]
         bc_type = bf.size * ['neu']
+        # Set Dirichlet conditions. Note that indices are relative to bf,
+        # that is, counting only boundary faces.
         bc_type[0] = 'dir'
-        bc_type[2] = 'dir'
+        bc_type[2] = 'dir'  # Not [3]
         bound = bc.BoundaryCondition(g, bf, bc_type)
 
         fd = Tpfa()
         param.set_bc(fd, bound)
 
         bc_val = np.zeros(g.num_faces)
-        # Set up unit pressure gradient in x-direction
+        # Set up unit flow in x-direction, thus pressure gradient the other way
         bc_val[[2, 5]] = 1
         param.set_bc_val(fd, bc_val)
         data = {'param': param}
@@ -396,7 +398,7 @@ class TestMpfaBoundaryPressure(unittest.TestCase):
         param.set_bc(fd, bound)
 
         bc_val = np.zeros(g.num_faces)
-        # Set up pressure gradient in x-direction, with value -1
+        # Set up flow in negative x-direction, thus positive gradient of value 1
         bc_val[[2, 5]] = -1
         param.set_bc_val(fd, bc_val)
         data = {'param': param}
@@ -465,6 +467,7 @@ class TestMpfaBoundaryPressure(unittest.TestCase):
         g.compute_geometry()
         d = {'param': Parameters(g)}
         bv = np.zeros(g.num_faces)
+        # Flow from right to left
         bv[[0, 3]] = 1
         bv[[2, 5]] = -1
         d['param'].set_bc_val('flow', bv)
@@ -477,62 +480,119 @@ class TestMpfaBoundaryPressure(unittest.TestCase):
         assert bound_p[0] == x[0] - 0.5
         assert bound_p[2] == x[1] + 0.5
 
-class TestMPFASimplexGrid():
-
-    def grid_2d(self):
-        nodes = np.array([[0, 0, 0], [1, 0, 0], [1, 0.5, 0], [0.5, 0.5, 0],
-                          [0, 0.5, 0]]).T
-
-        fn = np.array([[0, 1], [1, 2], [2, 3], [3, 4], [4, 0], [0, 3], [3, 1]]).T
-        cf = np.array([[3, 4, 5], [0, 6, 5], [1, 2, 6]]).T
-        cols = np.tile(np.arange(fn.shape[1]), (fn.shape[0], 1)).ravel('F')
-        face_nodes = sps.csc_matrix((np.ones_like(cols),
-                                     (fn.ravel('F'), cols)))
-
-        cols = np.tile(np.arange(cf.shape[1]), (cf.shape[0], 1)).ravel('F')
-        cell_faces = sps.csc_matrix((np.array([1, 1, 1, 1, 1, -1, 1, 1, -1]),
-                                     (cf.ravel('F'), cols)))
-
-        g = Grid(2, nodes, face_nodes, cell_faces, 'TriangleGrid')
+    def test_structured_simplex_linear_flow(self):
+        g = self.simplex_grid()
         g.compute_geometry()
-        #g.face_normals[1, [2, 3]] = -0.5
-        #g.face_normals[1, [7, 8]] = 0.5
-        return g
-
-    def set_params(self, g):
-
-
         param = Parameters(g)
-        d = {'param': param}
-        perm = pp.SecondOrderTensor(g.dim, kxx=np.ones(g.num_cells))
-        param.set_tensor("flow", perm)
-
-        bound_faces = np.array([0])
-        #bound_faces = g.get_boundary_faces()
-        labels = np.array(['dir'] * bound_faces.size)
-        param.set_bc("flow", bc.BoundaryCondition(g, bound_faces, labels))
-
-
-        d['param'] = param
-        return d
-
-
-    def test_mpfa(self):
-        g = self.grid_2d()
-        d = self.set_params(g)
         bv = np.zeros(g.num_faces)
-        bv[0] = 0
-        bv[[2, 3]] = -0.5
-        #bv = np.sum(g.face_centers, axis=0)
-        d['param'].set_bc_val('flow', bv)
-        t = Mpfa('flow')
-        A, b = t.matrix_rhs(g, d)
+        # Flow from right to left
+        bf = np.where(g.tags['domain_boundary_faces'].ravel())[0]
+        bc_type = np.asarray(bf.size * ['neu'])
 
-        x = sps.linalg.spsolve(A, b)
-        div = fvutils.scalar_divergence(g)
-        print(x - g.cell_centers[1])
-        print(x)
-        print(g.face_centers)
+        xf = g.face_centers[:, bf]
+        xleft = np.where(xf[0] < 1e-3 + xf[0].min())[0]
+        xright = np.where(xf[0] > xf[0].max() - 1e-3)[0]
+        bc_type[xleft] = 'dir'
+
+        bound = bc.BoundaryCondition(g, bf, bc_type)
+
+        fd = Mpfa()
+        param.set_bc(fd, bound)
+
+        bv = np.zeros(g.num_faces)
+        bv[bf[xright]] = 1
+
+        param.set_bc_val('flow', bv)
+
+        data = {'param':param}
+        t = Mpfa('flow')
+
+        A, b = t.matrix_rhs(g, data)
+        x = spl.spsolve(A, b)
+
+        bound_p = data['bound_pressure_cell'] * x\
+                + data['bound_pressure_face'] * bv
+        assert np.allclose(bound_p[bf], -g.face_centers[0, bf])
+
+    def test_structured_simplex_linear_flow_reverse_sign(self):
+        g = self.simplex_grid()
+        g.compute_geometry()
+        param = Parameters(g)
+        bv = np.zeros(g.num_faces)
+        # Flow from right to left
+        bf = np.where(g.tags['domain_boundary_faces'].ravel())[0]
+        bc_type = np.asarray(bf.size * ['neu'])
+
+        xf = g.face_centers[:, bf]
+        xleft = np.where(xf[0] < 1e-3 + xf[0].min())[0]
+        xright = np.where(xf[0] > xf[0].max() - 1e-3)[0]
+        bc_type[xleft] = 'dir'
+
+        bound = bc.BoundaryCondition(g, bf, bc_type)
+
+        fd = Mpfa()
+        param.set_bc(fd, bound)
+
+        bv = np.zeros(g.num_faces)
+        bv[bf[xright]] = -1
+
+        param.set_bc_val('flow', bv)
+
+        data = {'param':param}
+        t = Mpfa('flow')
+
+        A, b = t.matrix_rhs(g, data)
+        x = spl.spsolve(A, b)
+
+        bound_p = data['bound_pressure_cell'] * x\
+                + data['bound_pressure_face'] * bv
+        assert np.allclose(bound_p[bf], g.face_centers[0, bf])
+
+class TestMpfaSimplexGrid():
+
+    def grid(self):
+        domain = {'xmin':0, 'xmax': 1, 'ymin': 0, 'ymax':1}
+
+        p = np.zeros((2, 0))
+        mesh_size = {'value': 0.3, 'bound_value': 0.3}
+        gb = pp.meshing.simplex_grid(fracs=[], domain=domain, mesh_size=mesh_size, verbose=0)
+        return gb.grids_of_dimension(2)[0]
+
+    def test_linear_flow(self):
+        g = self.grid()
+        g.compute_geometry()
+        param = Parameters(g)
+        bv = np.zeros(g.num_faces)
+        # Flow from right to left
+        bf = np.where(g.tags['domain_boundary_faces'].ravel())[0]
+        bc_type = np.asarray(bf.size * ['neu'])
+
+        xf = g.face_centers[:, bf]
+        xleft = np.where(xf[0] < 1e-3 + xf[0].min())[0]
+        xright = np.where(xf[0] > xf[0].max() - 1e-3)[0]
+        bc_type[xleft] = 'dir'
+
+        bound = bc.BoundaryCondition(g, bf, bc_type)
+
+        fd = Mpfa()
+        param.set_bc(fd, bound)
+
+        bv = np.zeros(g.num_faces)
+        bv[bf[xright]] = 1*g.face_areas[bf[xright]]
+
+        param.set_bc_val('flow', bv)
+
+        data = {'param':param}
+        t = Mpfa('flow')
+
+        A, b = t.matrix_rhs(g, data)
+        x = spl.spsolve(A, b)
+
+        bound_p = data['bound_pressure_cell'] * x\
+                + data['bound_pressure_face'] * bv
+        assert np.allclose(bound_p[bf], -g.face_centers[0, bf])
+
+
 
 
 
@@ -541,4 +601,3 @@ class TestMPFASimplexGrid():
 if __name__ == '__main__':
     unittest.main()
 
-#TestTpfaBoundaryPressure().test_constant_pressure()
