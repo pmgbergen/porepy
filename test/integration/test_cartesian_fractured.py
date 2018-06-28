818a36a301ef91bfa53a4d00b9952e0ba21e87aa
Runar <runar.berge@uib.no>
2018-06-28 17:02:05 +0200

    Fixed integration test: test_cartesian_fractured

--- test/integration/test_cartesian_fractured.py
+++ test/integration/test_cartesian_fractured.py
@@ -1,11 +1,14 @@
 import numpy as np
 import unittest
 
-from porepy.fracs.meshing import cart_grid
-
+import porepy as pp
 
 class TestCartGridFrac(unittest.TestCase):
     def test_tripple_x_intersection_3d(self):
+        """
+        Create a cartesian grid in the unit cube, and insert three fractures.
+        """
+
         f1 = np.array([[0,  1,  1,  0],
                        [0,  0,  1,  1],
                        [.5, .5, .5, .5]])
@@ -16,13 +19,13 @@ class TestCartGridFrac(unittest.TestCase):
                        [.5, .5, .5, .5],
                        [0,  0,  1,  1]])
 
-        gb = cart_grid([f1, f2, f3], [2, 2, 2],
-                       **{'physdims': [1, 1, 1]})
+        gb = pp.meshing.cart_grid([f1, f2, f3], [2, 2, 2],
+                                  physdims= [1, 1, 1])
         g_3 = gb.grids_of_dimension(3)
         g_2 = gb.grids_of_dimension(2)
         g_1 = gb.grids_of_dimension(1)
         g_0 = gb.grids_of_dimension(0)
-
+        
         assert len(g_3) == 1
         assert len(g_2) == 3
         assert len(g_1) == 6
@@ -33,7 +36,7 @@ class TestCartGridFrac(unittest.TestCase):
         assert np.all([g.num_cells == 1 for g in g_1])
         assert np.all([g.num_faces == 2 for g in g_1])
 
-        g_all = g_3 + g_2 + g_1 + g_0
+        g_all = np.hstack([g_3, g_2, g_1, g_0])
         for g in g_all:
             d = np.all(np.abs(g.nodes - np.array([[.5], [.5], [.5]])) < 1e-6,
                        axis=0)
