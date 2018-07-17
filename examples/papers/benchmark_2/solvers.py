643b6a578f0c862104937223f6514a9062d24be9
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2018-07-17 16:35:10 +0200

    update the solvers with the new interface

--- examples/papers/benchmark_2/solvers.py
+++ examples/papers/benchmark_2/solvers.py
@@ -1,4 +1,3 @@
-import numpy as np
 import scipy.sparse as sps
 
 import porepy as pp
@@ -25,14 +24,15 @@ def solve_rt0(gb, folder):
     solver_flow = pp.RT0MixedDim('flow')
     A_flow, b_flow = solver_flow.matrix_rhs(gb)
 
-    solver_source = pp.DualSourceMixedDim('flow')
+    solver_source = pp.DualSourceMixedDim('flow', [None])
     A_source, b_source = solver_source.matrix_rhs(gb)
-    A = A_flow + A_source
 
-    up = sps.linalg.spsolve(A, b_flow+b_source)
+    up = sps.linalg.spsolve(A_flow+A_source, b_flow+b_source)
+
     solver_flow.split(gb, "up", up)
-
     solver_flow.extract_p(gb, "up", "pressure")
+    solver_flow.extract_u(gb, "up", "discharge")
+
     export(gb, folder)
 
 #------------------------------------------------------------------------------#
@@ -43,11 +43,11 @@ def solve_tpfa(gb, folder):
     solver_flow = pp.TpfaMixedDim('flow')
     A_flow, b_flow = solver_flow.matrix_rhs(gb)
 
-    solver_source = pp.IntegralMixedDim('flow')
+    solver_source = pp.IntegralMixedDim('flow', [None])
     A_source, b_source = solver_source.matrix_rhs(gb)
-    A = A_flow + A_source
 
-    p = sps.linalg.spsolve(A, b_flow+b_source)
+    p = sps.linalg.spsolve(A_flow+A_source, b_flow+b_source)
+
     solver_flow.split(gb, "pressure", p)
     pp.fvutils.compute_discharges(gb)
 
@@ -61,33 +61,15 @@ def solve_mpfa(gb, folder):
     solver_flow = pp.MpfaMixedDim('flow')
     A_flow, b_flow = solver_flow.matrix_rhs(gb)
 
-    solver_source = pp.IntegralMixedDim('flow')
+    solver_source = pp.IntegralMixedDim('flow', [None])
     A_source, b_source = solver_source.matrix_rhs(gb)
-    A = A_flow + A_source
 
-    p = sps.linalg.spsolve(A, b_flow+b_source)
+    p = sps.linalg.spsolve(A_flow+A_source, b_flow+b_source)
+
     solver_flow.split(gb, "pressure", p)
     pp.fvutils.compute_discharges(gb)
 
     export(gb, folder)
-
-#------------------------------------------------------------------------------#
-
-def solve_p1(gb, folder):
-
-    # Choose and define the solvers and coupler
-    solver_flow = pp.P1MixedDim('flow')
-    A_flow, b_flow = solver_flow.matrix_rhs(gb)
-
-    solver_source = pp.IntegralMixedDim('flow')
-    A_source, b_source = solver_source.matrix_rhs(gb)
-    A = A_flow + A_source
-
-    p = sps.linalg.spsolve(A, b_flow+b_source)
-    solver_flow.split(gb, "pressure", p)
-
-    save = pp.Exporter(gb, "sol", folder=folder, simplicial=True)
-    save.write_vtk(["pressure"])
 
 #------------------------------------------------------------------------------#
 
@@ -97,21 +79,18 @@ def solve_vem(gb, folder):
     solver_flow = pp.DualVEMMixedDim('flow')
     A_flow, b_flow = solver_flow.matrix_rhs(gb)
 
-    solver_source = pp.DualSourceMixedDim('flow')
+    solver_source = pp.DualSourceMixedDim('flow', [None])
     A_source, b_source = solver_source.matrix_rhs(gb)
-    A = A_flow + A_source
 
-    up = sps.linalg.spsolve(A, b_flow+b_source)
+    up = sps.linalg.spsolve(A_flow+A_source, b_flow+b_source)
+
     solver_flow.split(gb, "up", up)
-
     solver_flow.extract_p(gb, "up", "pressure")
     solver_flow.extract_u(gb, "up", "discharge")
-    solver_flow.project_u(gb, "discharge", "P0u")
 
     export(gb, folder)
 
 #------------------------------------------------------------------------------#
-
 
 def transport(gb, data, solver_name, folder, adv_data_assigner, callback=None):
     physics = 'transport'
@@ -123,7 +102,6 @@ def transport(gb, data, solver_name, folder, adv_data_assigner, callback=None):
                                  callback=callback)
     advective.solve("tracer")
 
-
 class AdvectiveProblem(pp.ParabolicModel):
 
     def space_disc(self):
@@ -132,5 +110,3 @@ class AdvectiveProblem(pp.ParabolicModel):
     def solver(self):
         'Initiate solver'
         return TransportSolver(self)
-
-
