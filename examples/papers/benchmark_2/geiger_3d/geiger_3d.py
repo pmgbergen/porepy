eb2ff82841dfad3c3c18b3de24508556df16bfeb
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2018-07-19 15:54:45 +0200

    update the benchmark 2

--- examples/papers/benchmark_2/geiger_3d/geiger_3d.py
+++ examples/papers/benchmark_2/geiger_3d/geiger_3d.py
@@ -1,5 +1,3 @@
-import numpy as np
-import porepy as pp
 import geiger_3d_data as geiger
 import examples.papers.benchmark_2.solvers as solvers
 
@@ -17,28 +15,26 @@ def main(test_case, file_geo, folder, solver, solver_name):
         phi_f = 0.01
     data = {'domain': domain, 'tol': tol, 'aperture': 1e-4,  'km_low': 1e-1,
             'km': 1, 'kf': kf, 'phi_m': 1e-1, 'phi_f': phi_f,
-            'dt': .1, 't_max': 5}
+            'dt': 0.25/100, 't_max': 0.25}
 
     geiger.add_data(gb, data, solver_name)
     solver(gb, folder)
-
     solvers.transport(gb, data, solver_name, folder,
                       geiger.AdvectiveDataAssigner)
-    return gb, data
-
 
 if __name__ == "__main__":
-    files_geo = ['mesh15.geo', 'mesh1.geo', 'mesh075.geo', 'mesh05.geo']
-#    solver_list = [solvers.solve_rt0, solvers.solve_tpfa, solvers.solve_mpfa,
-#                    solvers.solve_vem]
-    solver_list = [solvers.solve_tpfa, solvers.solve_vem]
-    solver_names = ['tpfa', 'vem']
-    test_cases = [2]
+    files_geo = {'mesh15.geo': '0', 'mesh1.geo': '1', 'mesh075.geo': '2', 'mesh05.geo': '3'}
+    solver_list = [solvers.solve_tpfa, solvers.solve_vem, solvers.solve_rt0,
+                   solvers.solve_mpfa]
+    solver_names = ['tpfa', 'vem', 'rt0', 'mpfa']
+    test_cases = [1, 2]
+
+    files_geo = {'mesh15.geo': '0'} ###
+    solver_list = [solvers.solve_tpfa, solvers.solve_vem] ###
+    solver_names = ['tpfa', 'vem'] ###
 
     for test_case in test_cases:
         for solver, solver_name in zip(solver_list, solver_names):
-            for mesh_id, file_geo in enumerate(files_geo):
-                folder = solver_name + '_case_' + str(test_case) + '_mesh_' \
-                        + str(mesh_id)
-                gb, data = main(test_case, file_geo, folder, solver,
-                                solver_name)
+            for file_geo, mesh_id in files_geo.items():
+                folder = solver_name+'_case_'+str(test_case)+'_mesh_'+mesh_id
+                main(test_case, file_geo, folder, solver, solver_name)
