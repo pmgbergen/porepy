d5ef384a1fe8a2b4609d845824e43f010ccea5a1
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2018-08-06 15:55:34 +0200

    update example 1 ms

--- examples/papers/multiscale/example_1.py
+++ examples/papers/multiscale/example_1.py
@@ -2,6 +2,9 @@ import numpy as np
 import scipy.sparse as sps
 import porepy as pp
 
+from multiscale import Multiscale
+from domain_decomposition import DomainDecomposition
+
 # ------------------------------------------------------------------------------#
 
 
@@ -10,8 +13,8 @@ def add_data(gb, domain, kf):
     Define the permeability, apertures, boundary conditions
     """
     gb.add_node_props(["param"])
-    tol = 1e-5
-    a = 1e-4
+    tol = 1e-8
+    a = 1e-2
 
     for g, d in gb:
         param = pp.Parameters(g)
@@ -39,12 +42,16 @@ def add_data(gb, domain, kf):
             left = bound_face_centers[0, :] < domain["xmin"] + tol
             right = bound_face_centers[0, :] > domain["xmax"] - tol
 
-            labels = np.array(["neu"] * bound_faces.size)
-            labels[right] = "dir"
+            labels = np.array(["dir"] * bound_faces.size)
 
+
+#            labels[right] = "dir"
+#            labels[left] = "dir" ####
+#
             bc_val = np.zeros(g.num_faces)
-            bc_val[bound_faces[left]] = -aperture * g.face_areas[bound_faces[left]]
-            bc_val[bound_faces[right]] = 1
+            bc_val[bound_faces] = g.face_centers[1, bound_faces]
+#            #bc_val[bound_faces[left]] = -aperture * g.face_areas[bound_faces[left]]
+#            bc_val[bound_faces[right]] = 1
 
             param.set_bc("flow", pp.BoundaryCondition(g, bound_faces, labels))
             param.set_bc_val("flow", bc_val)
@@ -63,18 +70,19 @@ def add_data(gb, domain, kf):
         gamma = check_P * gb.node_props(g_l, "param").get_aperture()
         d["kn"] = kf * np.ones(mg.num_cells) / gamma
 
-
 # ------------------------------------------------------------------------------#
 
 
 def write_network(file_name):
     network = "FID,START_X,START_Y,END_X,END_Y\n"
-    network += "0,0,0.5,1,0.5\n"
-    network += "1,0.5,0,0.5,1\n"
-    network += "2,0.5,0.75,1,0.75\n"
-    network += "3,0.75,0.5,0.75,1\n"
-    network += "4,0.5,0.625,0.75,0.625\n"
-    network += "5,0.625,0.5,0.625,0.75\n"
+    network += "0,0.5,0.5,1,0.5"
+
+#    network += "0,0,0.5,1,0.5\n"
+#    network += "1,0.5,0,0.5,1\n"
+#    network += "2,0.5,0.75,1,0.75\n"
+#    network += "3,0.75,0.5,0.75,1\n"
+#    network += "4,0.5,0.625,0.75,0.625\n"
+#    network += "5,0.625,0.5,0.625,0.75\n"
 
     with open(file_name, "w") as text_file:
         text_file.write(network)
@@ -83,7 +91,7 @@ def write_network(file_name):
 # ------------------------------------------------------------------------------#
 
 
-def make_grid_bucket(mesh_size, is_coarse=False):
+def make_grid_bucket(mesh_size):
     mesh_kwargs = {}
     mesh_kwargs = {"mesh_size_frac": mesh_size, "mesh_size_min": mesh_size / 20}
 
@@ -93,16 +101,11 @@ def make_grid_bucket(mesh_size, is_coarse=False):
     write_network(file_name)
     gb = pp.importer.dfm_2d_from_csv(file_name, mesh_kwargs, domain)
     gb.compute_geometry()
-    if is_coarse:
-        pp.coarsening.coarsen(gb, "by_volume")
-    gb.assign_node_ordering()
     return gb, domain
-
 
 # ------------------------------------------------------------------------------#
 
-
-def main(kf, name):
+def main_ms(kf, name):
 
     mesh_size = 0.045
     gb, domain = make_grid_bucket(mesh_size)
@@ -114,119 +117,66 @@ def main(kf, name):
     solver_flow = pp.RT0MixedDim("flow")
     A, b = solver_flow.matrix_rhs(gb, return_bmat=True)
 
-    # Select the grid of higher dimension and the position in the matrix A
-    g_h = gb.grids_of_dimension(gb.dim_max())[0]
-    pos_hn = [gb.node_props(g_h, "node_number")]
-    # Determine the number of dofs [u, p] for the higher dimensional grid
-    dof_h = A[pos_hn[0], pos_hn[0]].shape[0]
+    # off-line computation fo the bases
+    ms = Multiscale(gb)
+    ms.extract_blocks_h(A, b)
+    ms.compute_bases(is_mixed=True)
 
-    # Select the co-dimensional grid positions
-    pos_ln = [d["node_number"] for g, d in gb if g.dim < g_h.dim]
-
-    # select the positions of the mortars blocks, for both the high dimensional
-    # and lower dimensional grids
-    num_nodes = gb.num_graph_nodes()
-    pos_he = []
-    pos_le = []
-    for e, d in gb.edges():
-        gl_h = gb.nodes_of_edge(e)[1]
-        if gl_h.dim == g_h.dim:
-            pos_he.append(d["edge_number"] + num_nodes)
-        else:
-            pos_le.append(d["edge_number"] + num_nodes)
-
-    # extract the blocks for the higher dimension
-    A_h = sps.bmat(A[np.ix_(pos_hn + pos_he, pos_hn + pos_he)])
-    b_h = np.concatenate(tuple(b[pos_hn + pos_he]))
-
-    # matrix that couple the 1 co-dimensional pressure to the mortar variables
-    C_h = sps.bmat(A[np.ix_(pos_he, pos_ln)])
-
-    # Select the grid that realise the jump operator given the mortar variables
-    C_l = sps.bmat(A[np.ix_(pos_ln, pos_he)])
-
-    # construct the rhs with all the active pressure dof in the 1 co-dimension
-    if_p = np.zeros(C_h.shape[1], dtype=np.bool)
-    pos = 0
-    for g, d in gb:
-        if g.dim != g_h.dim:
-            pos += g.num_faces
-            # only the 1 co-dimensional grids are interesting
-            if g.dim == g_h.dim - 1:
-                if_p[pos : pos + g.num_cells] = True
-            pos += g.num_cells
-
-    # compute the bases
-    num_bases = np.sum(if_p)
-    dof_bases = C_h.shape[0]
-    bases = np.zeros((C_h.shape[1], C_h.shape[1]))
-
-    # we solve many times the same problem, better to factorize the matrix
-    LU = sps.linalg.factorized(A_h.tocsc())
-
-    # solve to compute the ms bases functions for homogeneous boundary
-    # conditions
-    for dof_basis in np.where(if_p)[0]:
-        rhs = np.zeros(if_p.size)
-        rhs[dof_basis] = 1.
-        # project from the co-dimensional pressure to the Robin boundary
-        # condition
-        rhs = np.concatenate((np.zeros(dof_h), -C_h * rhs))
-        x = LU(rhs)
-        # compute the jump of the mortars
-        bases[:, dof_basis] = C_l * x[-dof_bases:]
-
-        # save = pp.Exporter(g_h, "vem"+str(dof_basis), folder="vem_ms")
-        # save.write_vtk({'pressure': x[g_h.num_faces:dof_h]})
-
-    # solve for non-zero boundary conditions
-    x_h = LU(b_h)
-
-    # construct the problem in the fracture network
-    A_l = np.empty((2, 2), dtype=np.object)
-    b_l = np.empty(2, dtype=np.object)
-    # the multiscale bases are thus inserted in the right block of the lower
-    # dimensional problem
-    A_l[0, 0] = sps.bmat(A[np.ix_(pos_ln, pos_ln)]) + sps.csr_matrix(bases)
-    dof_l = A_l[0, 0].shape[0]
-    # add to the righ-hand side the non-homogenous solution from the higher
-    # dimensional problem
-    b_l[0] = np.concatenate(tuple(b[pos_ln])) - C_l * x_h[-dof_bases:]
-    # in the case of > 1 co-dimensional problems
-    if len(pos_le) > 0:
-        A_l[0, 1] = sps.bmat(A[np.ix_(pos_ln, pos_le)])
-        A_l[1, 0] = sps.bmat(A[np.ix_(pos_le, pos_ln)])
-        A_l[1, 1] = sps.bmat(A[np.ix_(pos_le, pos_le)])
-        b_l[1] = np.concatenate(tuple(b[pos_le]))
-
-    # assemble and solve for the network
-    A_l = sps.bmat(A_l, "csr")
-    b_l = np.concatenate(tuple(b_l))
+    # solve the co-dimensional problem
+    A_l, b_l = ms.codim_problem(A, b)
     x_l = sps.linalg.spsolve(A_l, b_l)
 
-    # compute the higher dimensional solution
-    rhs = x_l[: if_p.size]
-    rhs = np.concatenate((np.zeros(dof_h), -C_h * rhs))
-    x_h = LU(b_h + rhs)
+    # post-compute the higher dimensional solution
+    x_h = ms.compute_sol_h(x_l)
+    print(x_h)
 
-    # save and export using standard algorithm
-    x = np.zeros(x_h.size + x_l.size)
-    x[:dof_h] = x_h[:dof_h]
-    x[dof_h : (dof_h + dof_l)] = x_l[:dof_l]
+    x = ms.concatenate(x_h, x_l)
     solver_flow.split(gb, "up", x)
 
     gb.add_node_props("pressure")
     solver_flow.extract_p(gb, "up", "pressure")
 
-    save = pp.Exporter(gb, "rt0", folder="rt0_"+name)
+    save = pp.Exporter(gb, "rt0", folder="ms_"+name)
     save.write_vtk("pressure")
 
+# ------------------------------------------------------------------------------#
+
+def main_dd(kf, name):
+
+    mesh_size = 0.045
+    #mesh_size = 0.5
+    gb, domain = make_grid_bucket(mesh_size)
+    #pp.plot_grid(gb, info="all", alpha=0)
+
+    # Assign parameters
+    add_data(gb, domain, kf)
+
+    # Choose and define the solvers and coupler
+    solver_flow = pp.RT0MixedDim("flow")
+    A, b = solver_flow.matrix_rhs(gb, return_bmat=True)
+
+    dd = DomainDecomposition(gb)
+    dd.extract_blocks(A, b)
+
+    x_0 = np.zeros(dd.ndof())
+    tol = 1e-8
+    max_it = 1000
+    x = dd.solve(x_0, tol, max_it)
+
+    solver_flow.split(gb, "up", x)
+
+    gb.add_node_props("pressure")
+    solver_flow.extract_p(gb, "up", "pressure")
+
+    save = pp.Exporter(gb, "rt0", folder="dd_"+name)
+    save.write_vtk("pressure")
 
 # ------------------------------------------------------------------------------#
 
 if __name__ == "__main__":
-    kf = 1e-4
-    main(kf, "blocking")
+    kf = 1e0
+    #main_ms(kf, "blocking")
+    main_dd(kf, "blocking")
 
-#    kf = 1e4
-#    main(kf, "permeable")
+    #kf = 1e4
+    #main_dd(kf, "permeable")
