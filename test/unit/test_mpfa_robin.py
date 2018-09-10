d75868fcbd3f49d1be3942fea705882aab9015c1
Runar <runar.berge@uib.no>
2018-09-10 16:18:09 +0200

    Fixed bug in mpfa when applying only robin + dirichlet
    That is, applying Robin without any Neuman boundaries

--- test/unit/test_mpfa_robin.py
+++ test/unit/test_mpfa_robin.py
@@ -86,6 +86,47 @@ class RobinBoundTest(unittest.TestCase):
                 p_ex = C *(1- g.cell_centers[1]) + p_bound
                 self.solve_robin(g, k, bnd, alpha, p_bound, rob_bound, dir_ind, rob_ind, p_ex, u_ex)
 
+    def test_no_neumann(self):
+        g = pp.CartGrid([2, 2], physdims=[1, 1])
+        g.compute_geometry()
+        k = pp.SecondOrderTensor(2, np.ones(g.num_cells))
+        alpha = 1.5
+
+        bot = g.face_centers[1] < 1e-10
+        top = g.face_centers[1] > 1 - 1e-10
+        left = g.face_centers[0] < 1e-10
+        right = g.face_centers[0] > 1 - 1e-10
+
+        dir_ind = np.ravel(np.argwhere(top + left))
+        rob_ind = np.ravel(np.argwhere(bot + right))
+
+        names = ['dir']*len(dir_ind) + ['rob'] * len(rob_ind)
+        bnd_ind = np.hstack((dir_ind, rob_ind))
+        bnd = pp.BoundaryCondition(g, bnd_ind, names)
+
+        flux, bound_flux, _, _ = pp.numerics.fv.mpfa._mpfa_local(g, k, bnd, alpha=alpha)
+
+        div = pp.fvutils.scalar_divergence(g)
+
+        rob_ex = [alpha * .25, alpha * .75, 1, 1]
+        u_bound = np.zeros(g.num_faces)
+        u_bound[dir_ind] = g.face_centers[1, dir_ind]
+        u_bound[rob_ind] = rob_ex * g.face_areas[rob_ind]
+
+
+        a = div * flux
+        b = -div * bound_flux * u_bound
+
+        p = np.linalg.solve(a.A, b)
+
+        u_ex = [np.dot(g.face_normals[:, f], np.array([0, -1, 0]))
+                for f in range(g.num_faces)]
+        u_ex = np.array(u_ex).ravel('F')
+        p_ex = g.cell_centers[1]
+
+        assert np.allclose(p, p_ex)
+        assert np.allclose(flux * p + bound_flux * u_bound, u_ex)
+                
 
     def solve_robin(self, g, k, bnd, alpha, p_bound, rob_bound, dir_ind, rob_ind, p_ex, u_ex):
         flux, bound_flux, _, _ = pp.numerics.fv.mpfa._mpfa_local(g, k, bnd, alpha=alpha)
