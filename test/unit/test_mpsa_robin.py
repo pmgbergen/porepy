ca106002d3114cfec0380bf632d0b957be8dfd14
Runar <runar.berge@uib.no>
2018-09-11 10:39:09 +0200

    Added unit-tests for Robin conditions in mpsa

--- test/unit/test_mpsa_robin.py
+++ test/unit/test_mpsa_robin.py
@@ -99,6 +99,101 @@ class RobinBoundTest(unittest.TestCase):
         assert np.allclose(u, u_ex(g.cell_centers).ravel('F'))
         assert np.allclose(T, T_ex(np.arange(g.num_faces)).ravel('F'))
 
+    def test_structured_triang(self):
+        nx = 1
+        ny = 1
+        g = pp.StructuredTriangleGrid([nx, ny], physdims=[1, 1])
+        g.compute_geometry()
+        c = pp.FourthOrderTensor(2, np.ones(g.num_cells), np.ones(g.num_cells))
+        alpha = np.pi
+
+        bot = g.face_centers[1] < 1e-10
+        top = g.face_centers[1] > 1 - 1e-10
+        left = g.face_centers[0] < 1e-10
+        right = g.face_centers[0] > 1 - 1e-10
+
+        dir_ind = np.ravel(np.argwhere(left))
+        neu_ind = np.ravel(np.argwhere(top))
+        rob_ind = np.ravel(np.argwhere(right + bot))
+
+        names = ['dir']*len(dir_ind) + ['rob'] * len(rob_ind)
+        bnd_ind = np.hstack((dir_ind, rob_ind))
+        bnd = pp.BoundaryCondition(g, bnd_ind, names)
+
+        def u_ex(x):
+            return np.vstack((x[1], x[0]))
+
+        def T_ex(faces):
+            if np.size(faces) == 0:
+                return np.atleast_2d(np.array([]))
+            sigma = np.array([[0, 2], [2, 0]])
+            T_r = [np.dot(sigma, g.face_normals[:2, f]) for f in faces]
+            return np.vstack(T_r).T
+
+        u_bound = np.zeros((2, g.num_faces))
+
+        sgn_n = pp.numerics.fracture_deformation.sign_of_faces(g, neu_ind)
+        sgn_r = pp.numerics.fracture_deformation.sign_of_faces(g, rob_ind)
+        
+        u_bound[:, dir_ind] = u_ex(g.face_centers[:, dir_ind])
+        u_bound[:, neu_ind] = T_ex(neu_ind) * sgn_n
+        u_bound[:, rob_ind] = (
+        T_ex(rob_ind) * sgn_r
+        + alpha * u_ex(g.face_centers[:, rob_ind]) * g.face_areas[rob_ind]
+        )
+        u, T = self.solve_mpsa(g, c, alpha, bnd, u_bound)
+
+        assert np.allclose(u, u_ex(g.cell_centers).ravel('F'))
+        assert np.allclose(T, T_ex(np.arange(g.num_faces)).ravel('F'))
+
+    def test_unstruct_triang(self):
+        corners = np.array([[0, 0, 1, 1], [0, 1, 1, 0]])
+        points = np.random.rand(2, 2)
+        points = np.hstack((corners, points))
+        g = pp.TriangleGrid(points)
+        g.compute_geometry()
+        c = pp.FourthOrderTensor(2, np.ones(g.num_cells), np.ones(g.num_cells))
+        alpha = np.pi
+
+        bot = g.face_centers[1] < 1e-10
+        top = g.face_centers[1] > 1 - 1e-10
+        left = g.face_centers[0] < 1e-10
+        right = g.face_centers[0] > 1 - 1e-10
+
+        dir_ind = np.ravel(np.argwhere(left))
+        neu_ind = np.ravel(np.argwhere(top))
+        rob_ind = np.ravel(np.argwhere(right + bot))
+
+        names = ['dir']*len(dir_ind) + ['rob'] * len(rob_ind)
+        bnd_ind = np.hstack((dir_ind, rob_ind))
+        bnd = pp.BoundaryCondition(g, bnd_ind, names)
+
+        def u_ex(x):
+            return np.vstack((x[1], x[0]))
+
+        def T_ex(faces):
+            if np.size(faces) == 0:
+                return np.atleast_2d(np.array([]))
+            sigma = np.array([[0, 2], [2, 0]])
+            T_r = [np.dot(sigma, g.face_normals[:2, f]) for f in faces]
+            return np.vstack(T_r).T
+
+        u_bound = np.zeros((2, g.num_faces))
+
+        sgn_n = pp.numerics.fracture_deformation.sign_of_faces(g, neu_ind)
+        sgn_r = pp.numerics.fracture_deformation.sign_of_faces(g, rob_ind)
+        
+        u_bound[:, dir_ind] = u_ex(g.face_centers[:, dir_ind])
+        u_bound[:, neu_ind] = T_ex(neu_ind) * sgn_n
+        u_bound[:, rob_ind] = (
+        T_ex(rob_ind) * sgn_r
+        + alpha * u_ex(g.face_centers[:, rob_ind]) * g.face_areas[rob_ind]
+        )
+        u, T = self.solve_mpsa(g, c, alpha, bnd, u_bound)
+
+        assert np.allclose(u, u_ex(g.cell_centers).ravel('F'))
+        assert np.allclose(T, T_ex(np.arange(g.num_faces)).ravel('F'))
+
     def solve_mpsa(self, g, c, alpha, bnd, u_bound):
         stress, bound_stress = pp.numerics.fv.mpsa._mpsa_local(g, c, bnd, alpha=alpha)
         div = pp.fvutils.vector_divergence(g)
