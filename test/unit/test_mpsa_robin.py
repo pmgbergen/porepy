1a4c74d6a6aea83d1ff6938db9c29872641830a3
Runar <runar.berge@uib.no>
2018-09-11 15:32:24 +0200

    Added 3D test case for mpsa_robin

--- test/unit/test_mpsa_robin.py
+++ test/unit/test_mpsa_robin.py
@@ -194,6 +194,62 @@ class RobinBoundTest(unittest.TestCase):
         assert np.allclose(u, u_ex(g.cell_centers).ravel('F'))
         assert np.allclose(T, T_ex(np.arange(g.num_faces)).ravel('F'))
 
+    def test_unstruct_tetrahedron(self):
+        box = {'xmin': 0, 'xmax': 1,
+               'ymin': 0, 'ymax': 1,
+               'zmin': 0, 'zmax': 1}
+
+        g = pp.meshing.simplex_grid([], box, mesh_size_min=3, mesh_size_frac=3)
+        g = g.grids_of_dimension(3)[0]
+        g = pp.CartGrid([4,4,4], [1,1,1])
+        g.compute_geometry()
+        c = pp.FourthOrderTensor(3, np.ones(g.num_cells), np.ones(g.num_cells))
+        alpha = 1.0
+
+        bot = g.face_centers[2] < 1e-10
+        top = g.face_centers[2] > 1 - 1e-10
+        west = g.face_centers[0] < 1e-10
+        east = g.face_centers[0] > 1 - 1e-10
+        north = g.face_centers[1] > 1 - 1e-10
+        south = g.face_centers[1] < 1e-10
+
+        dir_ind = np.ravel(np.argwhere(west + top))
+        neu_ind = np.ravel(np.argwhere(bot))
+        rob_ind = np.ravel(np.argwhere(east + north + south))
+
+        names = ['dir']*len(dir_ind) + ['rob'] * len(rob_ind)
+        bnd_ind = np.hstack((dir_ind, rob_ind))
+        bnd = pp.BoundaryCondition(g, bnd_ind, names)
+
+        def u_ex(x):
+            return np.vstack((x[1], x[0], 0*x[2]))
+
+        def T_ex(faces):
+            if np.size(faces) == 0:
+                return np.atleast_2d(np.array([]))
+            sigma = np.array([[0, 2, 0],
+                              [2, 0, 0],
+                              [0, 0, 0]])
+            T_r = [np.dot(sigma, g.face_normals[:, f]) for f in faces]
+            return np.vstack(T_r).T
+
+        u_bound = np.zeros((3, g.num_faces))
+
+        sgn_n = pp.numerics.fracture_deformation.sign_of_faces(g, neu_ind)
+        sgn_r = pp.numerics.fracture_deformation.sign_of_faces(g, rob_ind)
+        
+        u_bound[:, dir_ind] = u_ex(g.face_centers[:, dir_ind])
+        u_bound[:, neu_ind] = T_ex(neu_ind) * sgn_n
+        u_bound[:, rob_ind] = (
+        T_ex(rob_ind) * sgn_r
+        + alpha * u_ex(g.face_centers[:, rob_ind]) * g.face_areas[rob_ind]
+        )
+        u, T = self.solve_mpsa(g, c, alpha, bnd, u_bound)
+
+        assert np.allclose(u, u_ex(g.cell_centers).ravel('F'))
+        assert np.allclose(T, T_ex(np.arange(g.num_faces)).ravel('F'))
+
+
     def solve_mpsa(self, g, c, alpha, bnd, u_bound):
         stress, bound_stress = pp.numerics.fv.mpsa._mpsa_local(g, c, bnd, alpha=alpha)
         div = pp.fvutils.vector_divergence(g)
