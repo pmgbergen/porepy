36dbc661913149cdb1f0ec4fa09c7359c69033d9
Runar <runar.berge@uib.no>
2018-09-12 10:18:29 +0200

    Fixed keyword error alpha->robin_weight

--- test/unit/test_mpfa_robin.py
+++ test/unit/test_mpfa_robin.py
@@ -13,7 +13,7 @@ class RobinBoundTest(unittest.TestCase):
                 g = pp.CartGrid([nx, ny], physdims=[1, 1])
                 g.compute_geometry()
                 k = pp.SecondOrderTensor(2, np.ones(g.num_cells))
-                alpha = 1.5
+                robin_weight = 1.5
 
                 left = g.face_centers[0] < 1e-10
                 right = g.face_centers[0] > 1 - 1e-10
@@ -27,10 +27,10 @@ class RobinBoundTest(unittest.TestCase):
 
                 p_bound = 1
                 rob_bound = 0
-                C = (rob_bound - alpha*p_bound) / (alpha - p_bound)
+                C = (rob_bound - robin_weight*p_bound) / (robin_weight - p_bound)
                 u_ex = - C * g.face_normals[0]
                 p_ex = C * g.cell_centers[0] + p_bound
-                self.solve_robin(g, k, bnd, alpha, p_bound, rob_bound, dir_ind, rob_ind, p_ex, u_ex)
+                self.solve_robin(g, k, bnd, robin_weight, p_bound, rob_bound, dir_ind, rob_ind, p_ex, u_ex)
 
     def test_flow_nz_rhs(self):
         nxs = [1, 3]
@@ -40,7 +40,7 @@ class RobinBoundTest(unittest.TestCase):
                 g = pp.CartGrid([nx, ny], physdims=[1, 1])
                 g.compute_geometry()
                 k = pp.SecondOrderTensor(2, np.ones(g.num_cells))
-                alpha = 1.5
+                robin_weight = 1.5
 
                 left = g.face_centers[0] < 1e-10
                 right = g.face_centers[0] > 1 - 1e-10
@@ -55,10 +55,10 @@ class RobinBoundTest(unittest.TestCase):
                 p_bound = 1
                 rob_bound = 1
 
-                C = (rob_bound - alpha*p_bound) / (alpha - p_bound)
+                C = (rob_bound - robin_weight*p_bound) / (robin_weight - p_bound)
                 u_ex = - C * g.face_normals[0]
                 p_ex = C * g.cell_centers[0] + p_bound
-                self.solve_robin(g, k, bnd, alpha, p_bound, rob_bound, dir_ind, rob_ind, p_ex, u_ex)
+                self.solve_robin(g, k, bnd, robin_weight, p_bound, rob_bound, dir_ind, rob_ind, p_ex, u_ex)
     def test_flow_down(self):
         nxs = [1, 3]
         nys = [1, 3]
@@ -67,7 +67,7 @@ class RobinBoundTest(unittest.TestCase):
                 g = pp.CartGrid([nx, ny], physdims=[1, 1])
                 g.compute_geometry()
                 k = pp.SecondOrderTensor(2, np.ones(g.num_cells))
-                alpha = 1.5
+                robin_weight = 1.5
 
                 bot = g.face_centers[1] < 1e-10
                 top = g.face_centers[1] > 1 - 1e-10
@@ -81,16 +81,16 @@ class RobinBoundTest(unittest.TestCase):
 
                 p_bound = 1
                 rob_bound = 1
-                C = (rob_bound - alpha*p_bound) / (alpha - p_bound)
+                C = (rob_bound - robin_weight*p_bound) / (robin_weight - p_bound)
                 u_ex = C * g.face_normals[1]
                 p_ex = C *(1- g.cell_centers[1]) + p_bound
-                self.solve_robin(g, k, bnd, alpha, p_bound, rob_bound, dir_ind, rob_ind, p_ex, u_ex)
+                self.solve_robin(g, k, bnd, robin_weight, p_bound, rob_bound, dir_ind, rob_ind, p_ex, u_ex)
 
     def test_no_neumann(self):
         g = pp.CartGrid([2, 2], physdims=[1, 1])
         g.compute_geometry()
         k = pp.SecondOrderTensor(2, np.ones(g.num_cells))
-        alpha = 1.5
+        robin_weight = 1.5
 
         bot = g.face_centers[1] < 1e-10
         top = g.face_centers[1] > 1 - 1e-10
@@ -104,11 +104,11 @@ class RobinBoundTest(unittest.TestCase):
         bnd_ind = np.hstack((dir_ind, rob_ind))
         bnd = pp.BoundaryCondition(g, bnd_ind, names)
 
-        flux, bound_flux, _, _ = pp.numerics.fv.mpfa._mpfa_local(g, k, bnd, alpha=alpha)
+        flux, bound_flux, _, _ = pp.numerics.fv.mpfa._mpfa_local(g, k, bnd, robin_weight=robin_weight)
 
         div = pp.fvutils.scalar_divergence(g)
 
-        rob_ex = [alpha * .25, alpha * .75, 1, 1]
+        rob_ex = [robin_weight * .25, robin_weight * .75, 1, 1]
         u_bound = np.zeros(g.num_faces)
         u_bound[dir_ind] = g.face_centers[1, dir_ind]
         u_bound[rob_ind] = rob_ex * g.face_areas[rob_ind]
@@ -127,8 +127,8 @@ class RobinBoundTest(unittest.TestCase):
         assert np.allclose(p, p_ex)
         assert np.allclose(flux * p + bound_flux * u_bound, u_ex)
 
-    def solve_robin(self, g, k, bnd, alpha, p_bound, rob_bound, dir_ind, rob_ind, p_ex, u_ex):
-        flux, bound_flux, _, _ = pp.numerics.fv.mpfa._mpfa_local(g, k, bnd, alpha=alpha)
+    def solve_robin(self, g, k, bnd, robin_weight, p_bound, rob_bound, dir_ind, rob_ind, p_ex, u_ex):
+        flux, bound_flux, _, _ = pp.numerics.fv.mpfa._mpfa_local(g, k, bnd, robin_weight=robin_weight)
 
         div = pp.fvutils.scalar_divergence(g)
 
