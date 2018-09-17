3ff6af6958b44eb73e67f62867e022e6383889a9
Runar <runar.berge@uib.no>
2018-09-17 20:01:31 +0200

    Fixed Rock unittest

--- test/unit/test_rock.py
+++ test/unit/test_rock.py
@@ -3,52 +3,51 @@ from __future__ import division
 import numpy as np
 import unittest
 
-from porepy.grids.structured import CartGrid
-from porepy.params import rock
-
+import porepy as pp
 
 class TestRock(unittest.TestCase):
     def test_lame_from_young_poisson(self):
         e = 1
         nu = 0.1
-        lam, mu = rock.lame_from_young_poisson(e, nu)
+        lam, mu = pp.params.rock.lame_from_young_poisson(e, nu)
         self.assertEqual(lam, 0.11363636363636363)
         self.assertEqual(mu, 0.45454545454545453)
 
     def test_poisson_from_lame(self):
         lam = 1
         mu = 0.5
-        nu = rock.poisson_from_lame(mu, lam)
+        nu = pp.params.rock.poisson_from_lame(mu, lam)
         self.assertEqual(nu, 1 / 3)
 
     def test_unit_rock(self):
-        R = rock.UnitRock()
+        R = pp.UnitRock()
         for _, value in vars(R).items():
             self.assertTrue(np.allclose(value, 1))
 
     def test_sand_stone(self):
-        R = rock.SandStone()
-        self.assertTrue(R.PERMEABILITY, 1 * 9.869233e-13)
-        self.assertTrue(R.POROSITY, 0.2)
-        self.assertTrue(R.YOUNG_MODULUS, 50)
-        self.assertTrue(R.POISSON_RATIO, 0.1)
-        self.assertTrue(R.LAMBDA, 568181818.1818181)
-        self.assertTrue(R.MU, 2272727272.7272725)
+        R = pp.SandStone()
+        self.assertEqual(R.PERMEABILITY, 1 * 9.869233e-13)
+        self.assertEqual(R.POROSITY, 0.2)
+        self.assertEqual(R.YOUNG_MODULUS, 5 * pp.KILOGRAM / pp.CENTI ** 2 * 1e5)
+        self.assertEqual(R.POISSON_RATIO, 0.1)
+        self.assertEqual(R.LAMBDA, 568181818.1818181)
+        self.assertEqual(R.MU, 2272727272.7272725)
 
     def test_granite(self):
-        R = rock.SandStone()
-        self.assertTrue(R.PERMEABILITY, 1e-8 * 9.869233e-13)
-        self.assertTrue(R.POROSITY, 0.01)
-        self.assertTrue(R.YOUNG_MODULUS, 50)
-        self.assertTrue(R.POISSON_RATIO, 0.2)
-        self.assertTrue(R.LAMBDA, 1388888888.8888888)
-        self.assertTrue(R.MU, 2083333333.3333335)
+        R = pp.Granite()
+        self.assertEqual(R.PERMEABILITY, 1e-8 * pp.DARCY)
+        self.assertEqual(R.POROSITY, 0.01)
+        self.assertEqual(R.YOUNG_MODULUS, 4 * pp.GIGA * pp.PASCAL)
+        self.assertEqual(R.POISSON_RATIO, 0.2)
+        self.assertEqual(R.DENSITY, 2700 * pp.KILOGRAM / pp.METER ** 3)
+        self.assertEqual(R.LAMBDA, 1111111111.1111112)
+        self.assertEqual(R.MU, 1666666666.6666667)
 
     def test_shale(self):
-        R = rock.SandStone()
-        self.assertTrue(R.PERMEABILITY, 1e-5 * 9.869233e-13)
-        self.assertTrue(R.POROSITY, 0.01)
-        self.assertTrue(R.YOUNG_MODULUS, 15)
-        self.assertTrue(R.POISSON_RATIO, 0.3)
-        self.assertTrue(R.LAMBDA, 865384615.3846153)
-        self.assertTrue(R.MU, 576923076.9230769)
+        R = pp.Shale()
+        self.assertEqual(R.PERMEABILITY, 1e-5 * 9.869233e-13)
+        self.assertEqual(R.POROSITY, 0.01)
+        self.assertEqual(R.YOUNG_MODULUS, 1.5 * pp.KILOGRAM / pp.CENTI ** 2 * 1e5)
+        self.assertEqual(R.POISSON_RATIO, 0.3)
+        self.assertEqual(R.LAMBDA, 865384615.3846153)
+        self.assertEqual(R.MU, 576923076.9230769)
