ad0b992aceb9b8779ccf0ba6c3f2785f76dde544
Runar <runar.berge@uib.no>
2018-09-19 12:25:59 +0200

    Added basis transformation to the mpsa

--- src/porepy/params/bc.py
+++ src/porepy/params/bc.py
@@ -239,7 +239,7 @@ class BoundaryConditionVectorial(object):
         self.is_neu[:, self.bf] = True
         self.set_bc(faces, cond)
 
-        self.basis = np.array(np.tile(np.eye(g.dim), (1, g.num_faces)))
+        self.basis = np.tile(np.eye(g.dim), (1, g.num_faces))
         self.basis = np.reshape(self.basis, (g.dim, g.num_faces, g.dim), "C")
 
     def set_bc(self, faces, cond):
