871ec6864240acf160052e5c3ec6eb593b02a927
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2018-09-24 10:41:06 +0200

    add new exmaple to generate fractures based on a training network

--- examples/papers/flow_upscaling/example2/main.py
+++ examples/papers/flow_upscaling/example2/main.py
@@ -1,17 +1,24 @@
 import numpy as np
+import porepy as pp
 
 from examples.papers.flow_upscaling.import_grid import raw_from_csv
-from examples.papers.flow_upscaling.frac_gen import fit
+from examples.papers.flow_upscaling.frac_gen import fit, generate
 
 if __name__ == "__main__":
-    file_geo = "../example1/Algeroyna.csv"
+    file_geo = "algeroyna_1to100.csv"
 
-    pts, edges = raw_from_csv(file_geo, {'mesh_size_frac': 10}, {"snap": 1e-3})
+    pts, edges, frac = raw_from_csv(file_geo, {'mesh_size_frac': 10}, {"snap": 1e-3})
 
-    # we assume 1 segment 1 fracture, need to change the next line instead
-    frac = np.arange(edges.shape[1])
+    domain = {'xmin': pts[0].min(), 'xmax': pts[0].max(),
+              'ymin': pts[1].min(), 'ymax': pts[1].max()}
+
+    pp.plot_fractures(domain, pts ,edges)
 
     # we assume only 1 family, need to change the next line instead
     family = np.ones(frac.size)
 
-    print( fit(pts, edges, frac, family) )
+    dist_l, dist_a = fit(pts, edges, frac, family)
+    pts_n, edges_n = generate(pts, edges, frac, dist_l, dist_a)
+
+    pp.plot_fractures(domain, pts_n ,edges_n)
+
