4d3b2ebc59cb0bc3eeb7efb9f151160bd746b517
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2018-09-25 08:01:57 +0200

    Remove old DFN class imports from vem __init__

--- src/porepy/numerics/vem/__init__.py
+++ src/porepy/numerics/vem/__init__.py
@@ -1,4 +1,4 @@
-from .vem_dual import DualVEMMixedDim, DualVEMDFN, DualVEM
-from .vem_dual import DualCoupling, DualCouplingDFN
+from .vem_dual import DualVEMMixedDim, DualVEM
+from .vem_dual import DualCoupling
 
 from .vem_source import DualSource, DualSourceMixedDim, Integral
