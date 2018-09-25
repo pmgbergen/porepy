7fef9dfde990d086f0c3c14663531c12a9a17b80
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2018-09-25 09:24:15 +0200

    Update tpfa to use new keyword storage of data

--- src/porepy/numerics/fv/tpfa.py
+++ src/porepy/numerics/fv/tpfa.py
@@ -92,8 +92,8 @@ class Tpfa(FVElliptic):
         aperture = param.get_aperture()
 
         if g.dim == 0:
-            data["flux"] = sps.csr_matrix([0])
-            data["bound_flux"] = 0
+            data[self.key() + "flux"] = sps.csr_matrix([0])
+            data[self.key() + "bound_flux"] = 0
             return None
         if faces is None:
             is_not_active = np.zeros(g.num_faces, dtype=np.bool)
@@ -163,8 +163,8 @@ class Tpfa(FVElliptic):
             (t_b * bndr_sgn, (bndr_ind, bndr_ind)), (g.num_faces, g.num_faces)
         )
 
-        data["flux"] = flux
-        data["bound_flux"] = bound_flux
+        data[self.key() + "flux"] = flux
+        data[self.key() + "bound_flux"] = bound_flux
 
         # Next, construct operator to reconstruct pressure on boundaries
         # Fields for data storage
@@ -180,8 +180,8 @@ class Tpfa(FVElliptic):
             (v_cell, (fi, ci)), (g.num_faces, g.num_cells)
         )
         bound_pressure_face = sps.dia_matrix((v_face, 0), (g.num_faces, g.num_faces))
-        data["bound_pressure_cell"] = bound_pressure_cell
-        data["bound_pressure_face"] = bound_pressure_face
+        data[self.key() + "bound_pressure_cell"] = bound_pressure_cell
+        data[self.key() + "bound_pressure_face"] = bound_pressure_face
 
 
 # ------------------------------------------------------------------------------
