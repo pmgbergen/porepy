37c97a242e72ba5b67616eda02b22b15172db60a
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2018-09-25 13:59:26 +0200

    EllipticAssembler gets more functions to handle variables on general grids.
    For now, mainly copied from the Coupler. Will be replaced with more
    generalized functions in due course.

--- src/porepy/numerics/mixed_dim/elliptic_assembler.py
+++ src/porepy/numerics/mixed_dim/elliptic_assembler.py
@@ -75,6 +75,10 @@ class EllipticAssembler(object):
 
         return matrix, rhs
 
+    ###
+    # The functions below are temporarily copied from Coupler. They will be
+    # generalized and moved to a more appropriate module when the furhter
+    # implementation is clearer.
 
     def initialize_matrix_rhs(self, gb):
         """
@@ -96,29 +100,38 @@ class EllipticAssembler(object):
         # Initialize the global matrix and rhs to store the local problems
         num_nodes = gb.num_graph_nodes()
         num_edges = gb.num_graph_edges()
-        size = num_nodes + self.num_mortars * num_edges
+        size = num_nodes + num_edges
         shapes = np.empty((size, size), dtype=np.object)
+
+        dof = self.key() + "dof"
+        # First loop over all nodes and edges. Assign dof numbers if not already
+        # present
+        for g, d in gb:
+            if not dof in d.keys():
+                d[dof] = d[self.key() + "discr"].ndof(g)
+
+        for e, d in gb.edges():
+            if not dof in d.keys():
+                mg = d["mortar_grid"]
+                d[dof] = d[self.key() + "discr"].ndof(mg)
 
         # Initialize the shapes for the matrices and rhs for all the sub-blocks
         for _, d_i in gb:
             pos_i = d_i["node_number"]
             for _, d_j in gb:
                 pos_j = d_j["node_number"]
-                shapes[pos_i, pos_j] = (d_i["dof"], d_j["dof"])
+                shapes[pos_i, pos_j] = (d_i[dof], d_j[dof])
             for _, d_e in gb.edges():
-                for i in range(self.num_mortars):
-                    pos_e = d_e["edge_number"] + num_nodes + i * num_edges
-                    shapes[pos_i, pos_e] = (d_i["dof"], d_e["dof"][i])
-                    shapes[pos_e, pos_i] = (d_e["dof"][i], d_i["dof"])
+                pos_e = d_e["edge_number"] + num_nodes
+                shapes[pos_i, pos_e] = (d_i[dof], d_e[dof])
+                shapes[pos_e, pos_i] = (d_e[dof], d_i[dof])
 
         for _, d_e in gb.edges():
-            for i in range(self.num_mortars):
-                pos_e = d_e["edge_number"] + num_nodes + i * num_edges
-                dof_e = d_e["dof"][i]
-                for _, d_f in gb.edges():
-                    for j in range(self.num_mortars):
-                        pos_f = d_f["edge_number"] + num_nodes + j * num_edges
-                        shapes[pos_e, pos_f] = (dof_e, d_f["dof"][j])
+            pos_e = d_e["edge_number"] + num_nodes
+            dof_e = d_e[dof]
+            for _, d_f in gb.edges():
+                pos_f = d_f["edge_number"] + num_nodes
+                shapes[pos_e, pos_f] = (dof_e, d_f[dof])
 
         # initialize the matrix and rhs
         matrix = np.empty(shapes.shape, dtype=np.object)
@@ -130,3 +143,93 @@ class EllipticAssembler(object):
                 matrix[i, j] = sps.coo_matrix(shapes[i, j])
 
         return matrix, rhs
+
+    # ------------------------------------------------------------------------------#
+
+    def split(self, gb, key, values, mortar_key="mortar_solution"):
+        """
+        Store in the grid bucket the vector, split in the function, solution of
+        the problem. The values are extracted from the global solution vector
+        according to the numeration given by "node_number".
+
+        Parameters
+        ----------
+        gb : grid bucket with geometry fields computed.
+        key: new name of the solution to be stored in the nodes of the grid
+            bucket.
+        values: array, global solution.
+        mortar_key: (optional) new name of the mortar solution to be stored in
+            the edges of the grid bucket
+
+        """
+        dofs = self._dof_start_of_grids(gb)
+
+        gb.add_node_props(key)
+        for g, d in gb:
+            i = d["node_number"]
+            d[key] = values[slice(dofs[i], dofs[i + 1])]
+
+        gb.add_edge_props(mortar_key)
+        for e, d in gb.edges():
+            i = d["edge_number"] + gb.num_graph_nodes()
+            d[mortar_key] = values[slice(dofs[i], dofs[i + 1])]
+
+    # ------------------------------------------------------------------------------#
+
+    def merge(self, gb, key):
+        """
+        Merge the stored split function stored in the grid bucket to a vector.
+        The values are put into the global  vector according to the numeration
+        given by "node_number".
+
+        Parameters
+        ----------
+        gb : grid bucket with geometry fields computed.
+        key: new name of the solution to be stored in the grid bucket.
+
+        Returns
+        -------
+        values: (ndarray) the values stored in the bucket as an array
+        """
+        dofs = self._dof_start_of_grids(gb)
+        values = np.zeros(dofs[-1])
+        for g, d in gb:
+            i = d["node_number"]
+            values[slice(dofs[i], dofs[i + 1])] = d[key]
+
+        return values
+
+    # ------------------------------------------------------------------------------#
+
+    def _dof_start_of_grids(self, gb):
+        " Helper method to get first global dof for all grids. "
+#        self.ndof(gb)
+        size = gb.num_graph_nodes() + gb.num_graph_edges()
+        dofs = np.zeros(size, dtype=int)
+
+        for _, d in gb:
+            dofs[d["node_number"]] = d[self.key() + "dof"]
+
+        for e, d in gb.edges():
+            i = d["edge_number"] + gb.num_graph_nodes()
+            dofs[i] = d[self.key() + "dof"]
+
+        return np.r_[0, np.cumsum(dofs)]
+
+    # ------------------------------------------------------------------------------#
+
+    def dof_of_grid(self, gb, g):
+        """ Obtain global indices of dof associated with a given grid.
+
+        Parameters:
+            gb: Grid_bucket representation of mixed-dimensional data.
+            g: Grid, one member of gb.
+
+        Returns:
+            np.array of ints: Indices of all dof for the given grid
+
+        """
+        dof_list = self._dof_start_of_grids(gb)
+        nn = gb.node_props(g)["node_number"]
+        return np.arange(dof_list[nn], dof_list[nn + 1])
+
