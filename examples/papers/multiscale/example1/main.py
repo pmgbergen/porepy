bac18195c45786e88120af2ea83c64bbdcb7544a
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2018-10-09 09:37:52 +0200

    remove useless parameter from the solve, previously used to the ILU factorization.

--- examples/papers/multiscale/example1/main.py
+++ examples/papers/multiscale/example1/main.py
@@ -102,7 +102,6 @@ def main_dd(pb_data, name):
     # parameters for the dd algorightm
     tol = 1e-6
     maxiter = 1e4
-    drop_tol = 0.1*np.amin([1e-3, data.eff_kf_t(), data.eff_kf_n()])
 
     # Choose and define the solvers and coupler
     solver_flow = pp.RT0MixedDim("flow")
@@ -112,7 +111,7 @@ def main_dd(pb_data, name):
     dd.extract_blocks(A, b)
 
     dd.factorize()
-    x, info = dd.solve(tol, maxiter, drop_tol, info=True)
+    x, info = dd.solve(tol, maxiter, info=True)
 
     folder = "dd_" + name + "_" + str(pb_data["mesh_size"])
     export(data.gb, x, folder, solver_flow)
