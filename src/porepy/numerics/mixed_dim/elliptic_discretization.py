7e2f02ef2ac9e9557eb229abfad4bb31f7b7d6d1
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2018-10-22 14:28:05 +0200

    Commented methods in supeclass for elliptic discretizations

--- src/porepy/numerics/mixed_dim/elliptic_discretization.py
+++ src/porepy/numerics/mixed_dim/elliptic_discretization.py
@@ -1,13 +1,10 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Created on Thu Oct 11 10:35:21 2018
+The module contains the base class for all elliptic discretization methods,
+to be used in both mono and mixed-dimensional problems.
 
-@author: eke001
 """
-import porepy as pp
-
 from porepy.numerics.mixed_dim.solver import Solver
+
 
 class EllipticDiscretization(Solver):
     """ This is the parent class of all discretizations for second order elliptic
@@ -28,11 +25,18 @@ class EllipticDiscretization(Solver):
     grids, an assembly will only be a partial (considering a single grid)- the
     full system matrix for multiple grids is handled somewhere else.
 
+    Known realizations of the elliptic discretization are:
+        Tpfa: Finite volume method using a wwo-point flux approximation.
+        Mpfa: Finite volume method using a multi-point flux approximation.
+        RT0: Mixed finite element method, using the lowest order Raviart-Thomas
+            elements.
+        MVEM: Mixed formulation of the lowest order virtual element method.
+
     Attributes:
-        keyword (str): This is used for
+        keyword (str): This is used to identify operators and parameters that
+            the discretization will work on.
 
     """
-
 
     def __init__(self, keyword):
         """ Set the discretization, with the keyword used for storing various
@@ -44,37 +48,35 @@ class EllipticDiscretization(Solver):
         """
         self.keyword = keyword
 
+        # The physics keyword is kept for consistency for now, but will soon be purged.
         self.physics = keyword
 
-
     def key(self):
-        """ Get
+        """ Get the keyword of this object, on a format friendly to access relevant
+        fields in the data dictionary
 
         Returns:
             String, on the form self.keyword + '_'.
+
         """
         return self.keyword + '_'
 
     def ndof(self, g):
-        """ Abstract method.
-        Return the number of degrees of freedom associated to the method.
+        """ Abstract method. Return the number of degrees of freedom associated to the method.
 
-        Parameter
-        ---------
-        g: grid, or a subclass.
+        Parameters
+            g (grid): Computational grid
 
-        Return
-        ------
-        dof: the number of degrees of freedom.
+        Returns:
+            int: the number of degrees of freedom.
 
         """
         raise NotImplementedError("Method not implemented")
 
     def extract_pressure(self, g, solution_array, d):
-        """ Extract the pressure part of a solution.
+        """ Abstract method. Extract the pressure part of a solution.
 
-        The method is trivial for finite volume methods, with the pressure
-        being the only primary variable.
+        The implementation will depend what are the primary variables of the specific implementation.
 
         Parameters:
             g (grid): To which the solution array belongs.
@@ -92,11 +94,11 @@ class EllipticDiscretization(Solver):
 
 
     def extract_flux(self, g, solution_array, d):
-        """ Extract the flux related to a solution.
+        """ Abstract method. Extract the pressure part of a solution.
 
-        The flux is computed from the discretization and the given pressure solution.
+        The implementation will depend what are the primary variables of the specific implementation.
 
-        @ALL: We should incrude the boundary condition as well?
+        TODO: We should incrude the boundary condition as well?
 
         Parameters:
             g (grid): To which the solution array belongs.
@@ -112,33 +114,41 @@ class EllipticDiscretization(Solver):
         raise NotImplementedError("Method not implemented")
 
     def assemble_matrix_rhs(self, g, data):
+        """ Return the matrix and right-hand side for a discretization of a second
+        order elliptic equation.
+
+        Also discretize the necessary operators if the data dictionary does not
+        contain the necessary fields. The decision on whether to discretize
+        will depend on the specific numerical method.
+
+        Parameters:
+            g (Grid): Computational grid, with geometry fields computed.
+            data (dictionary): With data stored.
+
+        Returns:
+            scipy.sparse.csr_matrix: System matrix of this discretization. The
+                size of the matrix will depend on the specific discretization.
+            np.ndarray: Right hand side vector with representation of boundary
+                conditions. The size of the vector will depend on the
+                discretization.
+
+        """
         raise NotImplementedError("Method not implemented")
 
     def assemble_matrix(self, g, data):
-        """
-        Return the matrix and right-hand side for a discretization of a second
-        order elliptic equation using a FV method with a multi-point flux
-        approximation.
+        """ Return the matrix for a discretization of a second order elliptic equation.
 
-        The name of data in the input dictionary (data) are:
-        k : second_order_tensor
-            Permeability defined cell-wise.
-        bc : boundary conditions (optional)
-        bc_val : dictionary (optional)
-            Values of the boundary conditions. The dictionary has at most the
-            following keys: 'dir' and 'neu', for Dirichlet and Neumann boundary
-            conditions, respectively.
+        Also discretize the necessary operators if the data dictionary does not
+        contain the necessary fields. The decision on whether to discretize
+        will depend on the specific numerical method.
 
-        Parameters
-        ----------
-        g : grid, or a subclass, with geometry fields computed.
-        data: dictionary to store the data. For details on necessary keywords,
-            see method discretize()
+        Parameters:
+            g (Grid): Computational grid, with geometry fields computed.
+            data (dictionary): With data stored.
 
-        Return
-        ------
-        matrix: sparse csr (g_num_cells, g_num_cells)
-            Discretization matrix.
+        Returns:
+            scipy.sparse.csr_matrix: System matrix of this discretization. The
+                size of the matrix will depend on the specific discretization.
 
         """
         raise NotImplementedError("Method not implemented")
@@ -146,25 +156,165 @@ class EllipticDiscretization(Solver):
     # ------------------------------------------------------------------------------#
 
     def assemble_rhs(self, g, data):
-        """
-        Return the righ-hand side for a discretization of a second order elliptic
-        equation using the MPFA method. See self.matrix_rhs for a detaild
-        description.
+        """ Return the right-hand side for a discretization of a second
+        order elliptic equation.
+
+        Also discretize the necessary operators if the data dictionary does not
+        contain the necessary fields. The decision on whether to discretize
+        will depend on the specific numerical method.
+
+        Parameters:
+            g (Grid): Computational grid, with geometry fields computed.
+            data (dictionary): With data stored.
+
+        Returns:
+            np.ndarray: Right hand side vector with representation of boundary
+                conditions. The size of the vector will depend on the
+                discretization.
+
         """
         raise NotImplementedError("Method not implemented")
 
     def assemble_int_bound_flux(self, g, data, data_edge, grid_swap, cc, matrix, self_ind):
+        """ Abstract method. Assemble the contribution from an internal
+        boundary, manifested as a flux boundary condition.
+
+        The intended use is when the internal boundary is coupled to another
+        node in a mixed-dimensional method. Specific usage depends on the
+        interface condition between the nodes; this method will typically be
+        used to impose flux continuity on a higher-dimensional domain.
+
+        Implementations of this method will use an interplay between the grid on
+        the node and the mortar grid on the relevant edge.
+
+        Parameters:
+            g (Grid): Grid which the condition should be imposed on.
+            data (dictionary): Data dictionary for the node in the
+                mixed-dimensional grid.
+            data_edge (dictionary): Data dictionary for the edge in the
+                mixed-dimensional grid.
+            grid_swap (boolean): If True, the grid g is identified with the @
+                slave side of the mortar grid in data_adge.
+            cc (block matrix, 3x3): Block matrix for the coupling condition.
+                The first and second rows and columns are identified with the
+                master and slave side; the third belongs to the edge variable.
+                The discretization of the relevant term is done in-place in cc.
+            matrix (block matrix 3x3): Discretization matrix for the edge and
+                the two adjacent nodes.
+            self_ind (int): Index in cc and matrix associated with this node.
+                Should be either 1 or 2.
+
+        """
         raise NotImplementedError("Method not implemented")
 
     def assemble_int_bound_source(self, g, data, data_edge, grid_swap, cc, matrix, self_ind):
+        """ Abstract method. Assemble the contribution from an internal
+        boundary, manifested as a source term.
+
+        The intended use is when the internal boundary is coupled to another
+        node in a mixed-dimensional method. Specific usage depends on the
+        interface condition between the nodes; this method will typically be
+        used to impose flux continuity on a lower-dimensional domain.
+
+        Implementations of this method will use an interplay between the grid on
+        the node and the mortar grid on the relevant edge.
+
+        Parameters:
+            g (Grid): Grid which the condition should be imposed on.
+            data (dictionary): Data dictionary for the node in the
+                mixed-dimensional grid.
+            data_edge (dictionary): Data dictionary for the edge in the
+                mixed-dimensional grid.
+            grid_swap (boolean): If True, the grid g is identified with the @
+                slave side of the mortar grid in data_adge.
+            cc (block matrix, 3x3): Block matrix for the coupling condition.
+                The first and second rows and columns are identified with the
+                master and slave side; the third belongs to the edge variable.
+                The discretization of the relevant term is done in-place in cc.
+            matrix (block matrix 3x3): Discretization matrix for the edge and
+                the two adjacent nodes.
+            self_ind (int): Index in cc and matrix associated with this node.
+                Should be either 1 or 2.
+
+        """
         raise NotImplementedError("Method not implemented")
 
     def assemble_int_bound_pressure_trace(self, g, data, data_edge, grid_swap, cc, matrix, self_ind):
+        """ Abstract method. Assemble the contribution from an internal
+        boundary, manifested as a condition on the boundary pressure.
+
+        The intended use is when the internal boundary is coupled to another
+        node in a mixed-dimensional method. Specific usage depends on the
+        interface condition between the nodes; this method will typically be
+        used to impose flux continuity on a higher-dimensional domain.
+
+        Implementations of this method will use an interplay between the grid on
+        the node and the mortar grid on the relevant edge.
+
+        Parameters:
+            g (Grid): Grid which the condition should be imposed on.
+            data (dictionary): Data dictionary for the node in the
+                mixed-dimensional grid.
+            data_edge (dictionary): Data dictionary for the edge in the
+                mixed-dimensional grid.
+            grid_swap (boolean): If True, the grid g is identified with the @
+                slave side of the mortar grid in data_adge.
+            cc (block matrix, 3x3): Block matrix for the coupling condition.
+                The first and second rows and columns are identified with the
+                master and slave side; the third belongs to the edge variable.
+                The discretization of the relevant term is done in-place in cc.
+            matrix (block matrix 3x3): Discretization matrix for the edge and
+                the two adjacent nodes.
+            self_ind (int): Index in cc and matrix associated with this node.
+                Should be either 1 or 2.
+
+        """
         raise NotImplementedError("Method not implemented")
 
     def assemble_int_bound_pressure_cell(self, g, data, data_edge, grid_swap, cc, matrix, self_ind):
+        """ Abstract method. Assemble the contribution from an internal
+        boundary, manifested as a condition on the cell pressure.
+
+        The intended use is when the internal boundary is coupled to another
+        node in a mixed-dimensional method. Specific usage depends on the
+        interface condition between the nodes; this method will typically be
+        used to impose flux continuity on a lower-dimensional domain.
+
+        Implementations of this method will use an interplay between the grid on
+        the node and the mortar grid on the relevant edge.
+
+        Parameters:
+            g (Grid): Grid which the condition should be imposed on.
+            data (dictionary): Data dictionary for the node in the
+                mixed-dimensional grid.
+            data_edge (dictionary): Data dictionary for the edge in the
+                mixed-dimensional grid.
+            grid_swap (boolean): If True, the grid g is identified with the @
+                slave side of the mortar grid in data_adge.
+            cc (block matrix, 3x3): Block matrix for the coupling condition.
+                The first and second rows and columns are identified with the
+                master and slave side; the third belongs to the edge variable.
+                The discretization of the relevant term is done in-place in cc.
+            matrix (block matrix 3x3): Discretization matrix for the edge and
+                the two adjacent nodes.
+            self_ind (int): Index in cc and matrix associated with this node.
+                Should be either 1 or 2.
+
+        """
         raise NotImplementedError("Method not implemented")
 
+    def enforce_neumann_int_bound(self, g, data, matrix):
+        """ Enforce Neumann boundary conditions on a given system matrix.
 
-    def enforce_neumann_int_bound(self, g_master, data_edge, matrix):
+        Methods based on a mixed variational form will need this function to
+        implement essential boundary conditions.
+
+        The discretization matrix should be modified in place.
+
+        Parameters:
+            g (Grid): On which the equation is discretized
+            data (dictionary): Of data related to the discretization.
+            matrix (scipy.sparse.matrix): Discretization matrix to be modified.
+
+        """
         raise NotImplementedError("Method not implemented")
