2bfb6a6ccb8f1aa759ef4068b4885bf6d870ec35
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2018-10-22 14:46:04 +0200

    Comments on elliptic discretizations

--- src/porepy/numerics/vem/dual_elliptic.py
+++ src/porepy/numerics/vem/dual_elliptic.py
@@ -159,7 +159,33 @@ class DualElliptic(pp.numerics.mixed_dim.elliptic_discretization.EllipticDiscret
 
 
     def assemble_int_bound_pressure_trace(self, g_master, data_master, data_edge, grid_swap, cc, matrix, self_ind=0):
-        """
+        """ Abstract method. Assemble the contribution from an internal
+        boundary, manifested as a condition on the boundary pressure.
+
+        The intended use is when the internal boundary is coupled to another
+        node in a mixed-dimensional method. Specific usage depends on the
+        interface condition between the nodes; this method will typically be
+        used to impose flux continuity on a higher-dimensional domain.
+
+        Implementations of this method will use an interplay between the grid on
+        the node and the mortar grid on the relevant edge.
+
+        Parameters:
+            g (Grid): Grid which the condition should be imposed on.
+            data (dictionary): Data dictionary for the node in the
+                mixed-dimensional grid.
+            data_edge (dictionary): Data dictionary for the edge in the
+                mixed-dimensional grid.
+            grid_swap (boolean): If True, the grid g is identified with the @
+                slave side of the mortar grid in data_adge.
+            cc (block matrix, 3x3): Block matrix for the coupling condition.
+                The first and second rows and columns are identified with the
+                master and slave side; the third belongs to the edge variable.
+                The discretization of the relevant term is done in-place in cc.
+            matrix (block matrix 3x3): Discretization matrix for the edge and
+                the two adjacent nodes.
+            self_ind (int): Index in cc and matrix associated with this node.
+                Should be either 1 or 2.
         """
         mg = data_edge['mortar_grid']
         hat_E_int = self._velocity_dof(g_master, mg)
@@ -169,6 +195,36 @@ class DualElliptic(pp.numerics.mixed_dim.elliptic_discretization.EllipticDiscret
 
 
     def assemble_int_bound_flux(self, g_master, data_master, data_edge, grid_swap, cc, matrix, self_ind=0):
+        """ Abstract method. Assemble the contribution from an internal
+        boundary, manifested as a flux boundary condition.
+
+        The intended use is when the internal boundary is coupled to another
+        node in a mixed-dimensional method. Specific usage depends on the
+        interface condition between the nodes; this method will typically be
+        used to impose flux continuity on a higher-dimensional domain.
+
+        Implementations of this method will use an interplay between the grid on
+        the node and the mortar grid on the relevant edge.
+
+        Parameters:
+            g (Grid): Grid which the condition should be imposed on.
+            data (dictionary): Data dictionary for the node in the
+                mixed-dimensional grid.
+            data_edge (dictionary): Data dictionary for the edge in the
+                mixed-dimensional grid.
+            grid_swap (boolean): If True, the grid g is identified with the @
+                slave side of the mortar grid in data_adge.
+            cc (block matrix, 3x3): Block matrix for the coupling condition.
+                The first and second rows and columns are identified with the
+                master and slave side; the third belongs to the edge variable.
+                The discretization of the relevant term is done in-place in cc.
+            matrix (block matrix 3x3): Discretization matrix for the edge and
+                the two adjacent nodes.
+            self_ind (int): Index in cc and matrix associated with this node.
+                Should be either 1 or 2.
+
+        """
+
         # The matrix must be the VEM discretization matrix.
         mg = data_edge['mortar_grid']
         hat_E_int = self._velocity_dof(g_master, mg)
@@ -176,7 +232,35 @@ class DualElliptic(pp.numerics.mixed_dim.elliptic_discretization.EllipticDiscret
         cc[self_ind, 2] += matrix[self_ind, self_ind] * hat_E_int
 
     def assemble_int_bound_pressure_cell(self, g_slave, data_slave, data_edge, grid_swap, cc, matrix, self_ind=1):
+        """ Abstract method. Assemble the contribution from an internal
+        boundary, manifested as a condition on the cell pressure.
 
+        The intended use is when the internal boundary is coupled to another
+        node in a mixed-dimensional method. Specific usage depends on the
+        interface condition between the nodes; this method will typically be
+        used to impose flux continuity on a lower-dimensional domain.
+
+        Implementations of this method will use an interplay between the grid on
+        the node and the mortar grid on the relevant edge.
+
+        Parameters:
+            g (Grid): Grid which the condition should be imposed on.
+            data (dictionary): Data dictionary for the node in the
+                mixed-dimensional grid.
+            data_edge (dictionary): Data dictionary for the edge in the
+                mixed-dimensional grid.
+            grid_swap (boolean): If True, the grid g is identified with the @
+                slave side of the mortar grid in data_adge.
+            cc (block matrix, 3x3): Block matrix for the coupling condition.
+                The first and second rows and columns are identified with the
+                master and slave side; the third belongs to the edge variable.
+                The discretization of the relevant term is done in-place in cc.
+            matrix (block matrix 3x3): Discretization matrix for the edge and
+                the two adjacent nodes.
+            self_ind (int): Index in cc and matrix associated with this node.
+                Should be either 1 or 2.
+
+        """
         mg = data_edge['mortar_grid']
         proj = mg.slave_to_mortar_avg()
 
@@ -185,9 +269,36 @@ class DualElliptic(pp.numerics.mixed_dim.elliptic_discretization.EllipticDiscret
 
         cc[2, self_ind] -= sps.bmat([[sps.csr_matrix(shape)], [A]]).T
 
-
     def assemble_int_bound_source(self, g_slave, data_slave, data_edge, grid_swap, cc, matrix, self_ind=1):
+        """ Abstract method. Assemble the contribution from an internal
+        boundary, manifested as a source term.
 
+        The intended use is when the internal boundary is coupled to another
+        node in a mixed-dimensional method. Specific usage depends on the
+        interface condition between the nodes; this method will typically be
+        used to impose flux continuity on a lower-dimensional domain.
+
+        Implementations of this method will use an interplay between the grid on
+        the node and the mortar grid on the relevant edge.
+
+        Parameters:
+            g (Grid): Grid which the condition should be imposed on.
+            data (dictionary): Data dictionary for the node in the
+                mixed-dimensional grid.
+            data_edge (dictionary): Data dictionary for the edge in the
+                mixed-dimensional grid.
+            grid_swap (boolean): If True, the grid g is identified with the @
+                slave side of the mortar grid in data_adge.
+            cc (block matrix, 3x3): Block matrix for the coupling condition.
+                The first and second rows and columns are identified with the
+                master and slave side; the third belongs to the edge variable.
+                The discretization of the relevant term is done in-place in cc.
+            matrix (block matrix 3x3): Discretization matrix for the edge and
+                the two adjacent nodes.
+            self_ind (int): Index in cc and matrix associated with this node.
+                Should be either 1 or 2.
+
+        """
         mg = data_edge['mortar_grid']
         proj = mg.slave_to_mortar_avg()
 
@@ -197,6 +308,19 @@ class DualElliptic(pp.numerics.mixed_dim.elliptic_discretization.EllipticDiscret
 
 
     def enforce_neumann_int_bound(self, g_master, data_edge, matrix):
+        """ Enforce Neumann boundary conditions on a given system matrix.
+
+        Methods based on a mixed variational form need this function to
+        implement essential boundary conditions.
+
+        The discretization matrix should be modified in place.
+
+        Parameters:
+            g (Grid): On which the equation is discretized
+            data (dictionary): Of data related to the discretization.
+            matrix (scipy.sparse.matrix): Discretization matrix to be modified.
+
+        """
         mg = data_edge['mortar_grid']
 
         hat_E_int = self._velocity_dof(g_master, mg)
