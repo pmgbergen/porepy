7fed6ed975536084a2a641e4d066aa21484b8d90
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2018-10-22 17:29:18 +0200

    add a new example in flow_upscaling which perform (in progress) the mpfa-upscaling

--- examples/papers/flow_upscaling/import_grid.py
+++ examples/papers/flow_upscaling/import_grid.py
@@ -11,7 +11,18 @@ def grid(file_geo, mesh_args, tol):
     domain = {'xmin': p[0].min(), 'xmax': p[0].max(),
               'ymin': p[1].min(), 'ymax': p[1].max()}
     frac_dict = {'points': p, 'edges': e}
+    print(frac_dict)
     gb = pp.meshing.simplex_grid(frac_dict, domain, tol=tol["geo"], **mesh_args)
+
+    return gb, domain
+
+# ------------------------------------------------------------------------------#
+
+def square_grid(mesh_args):
+
+    domain = {'xmin': 0, 'xmax': 1,
+              'ymin': 0, 'ymax': 1}
+    gb = pp.meshing.simplex_grid([], domain, **mesh_args)
 
     return gb, domain
 
@@ -24,3 +35,5 @@ def raw_from_csv(file_geo, mesh_args, tol):
     p, _ = pp.frac_utils.snap_fracture_set_2d(p, e, snap_tol=tol["snap"])
 
     return p, e, frac
+
+# ------------------------------------------------------------------------------#
