51dd10db194394146d32ce420afe87b4376100d8
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2018-10-23 14:07:59 +0200

    New fix of unit test of general assembler

--- test/unit/test_assembler.py
+++ test/unit/test_assembler.py
@@ -714,6 +714,3 @@ class MockEdgeDiscretizationOneSided(object):
 
 if __name__ == '__main__':
     unittest.main()
-#TestAssembler().test_single_variable_not_active()
-#TestAssembler().one_variable_one_sided_coupling_between_node_and_edge()
-#unittest.main()
