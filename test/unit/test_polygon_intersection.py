67b92df9fdd693e00564870bbea7f60cf2c29315
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2018-10-29 11:08:45 +0100

    handle also the case of fractures intersecting the boundary of a polygon, by not consider them.

--- test/unit/test_polygon_intersection.py
+++ test/unit/test_polygon_intersection.py
@@ -33,10 +33,10 @@ class BasicTest(unittest.TestCase):
         # non-convex polygon
         polygon = np.array([[0., 0.5, 0.75, 1., 1.5, 1.5, 0],
                             [0., 0.,  0.25, 0., 0,   1,   1]])
-        pts = np.array([[0., 1., 0., 2., 0.5, 0., -0.5, 0.3, -1, 0., 0.,  2],
-                        [0., 1., 0., 2., 1.,  0., -0.5, 0.6, -1, 0., 0.2, 0.2]])
-        lines = np.array([[0, 2, 4, 6, 8, 10],
-                          [1, 3, 5, 7, 9, 11]])
+        pts = np.array([[0., 1., 0., 2., 0.5, 0., -0.5, 0.3, -1, 0., 0.,  2.,  -0.1, 1.1],
+                        [0., 1., 0., 2., 1.,  0., -0.5, 0.6, -1, 0., 0.2, 0.2, 0.,   0.]])
+        lines = np.array([[0, 2, 4, 6, 8, 10, 12],
+                          [1, 3, 5, 7, 9, 11, 13]])
 
         new_pts, new_lines = cg.intersect_polygon_lines(polygon, pts, lines)
         pts_known = np.array([[0., 1., 0., 1., 0.5, 0., 0.,   0.3, 0.,  0.7, 0.8, 1.5],
@@ -46,7 +46,6 @@ class BasicTest(unittest.TestCase):
 
         self.assertTrue(np.allclose(new_pts, pts_known))
         self.assertTrue(np.allclose(new_lines, lines_known))
-
 
 if __name__ == "__main__":
     unittest.main()
