e934cdb88f277167645950e6f64264327848a883
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2018-11-20 16:25:31 +0100

    update example1 by make the bc easier to define in the dfn_transport

--- examples/papers/dfn_transport/example1/data.py
+++ examples/papers/dfn_transport/example1/data.py
@@ -19,14 +19,10 @@ def flow(gb, data, tol):
         # Boundaries
         b_faces = g.tags["domain_boundary_faces"].nonzero()[0]
         if b_faces.size:
-
-            b_face_centers = g.face_centers[:, b_faces]
-
-            left = b_face_centers[0] < data["domain"]["xmin"] + tol
-            right = b_face_centers[0] > data["domain"]["xmax"] - tol
+            (top, bottom, left, right), boundary = bc_flag(g, data["domain"], tol)
 
             labels = np.array(["neu"] * b_faces.size)
-            labels[left + right] = "dir"
+            labels[boundary] = "dir"
             param.set_bc(physics, pp.BoundaryCondition(g, b_faces, labels))
 
             bc_val = np.zeros(g.num_faces)
@@ -51,31 +47,33 @@ def advdiff(gb, data, tol):
 
         # Boundaries
         b_faces = g.tags["domain_boundary_faces"].nonzero()[0]
-        if b_faces.size != 0:
-            b_face_centers = g.face_centers[:, b_faces]
-
-            top = b_face_centers[1] > data["domain"]["ymax"] - tol
-            bottom = b_face_centers[1] < data["domain"]["ymin"] + tol
-            left = b_face_centers[0] < data["domain"]["xmin"] + tol
-            right = b_face_centers[0] > data["domain"]["xmax"] - tol
-            boundary = top + bottom + left + right
+        if b_faces.size:
+            (top, bottom, left, right), boundary = bc_flag(g, data["domain"], tol)
 
             labels = np.array(["neu"] * b_faces.size)
             labels[boundary] = ["dir"]
+            param.set_bc(physics, pp.BoundaryCondition(g, b_faces, labels))
 
             bc_val = np.zeros(g.num_faces)
             bc_val[b_faces[right]] = 1
-
-            param.set_bc(physics, pp.BoundaryCondition(g, b_faces, labels))
             param.set_bc_val(physics, bc_val)
         else:
             param.set_bc(physics, pp.BoundaryCondition(g, np.empty(0), np.empty(0)))
 
     lambda_flux = "lambda_" + data["flux"]
-    # Assign coupling discharge
     for _, d in gb.edges():
         d["flux_field"] = d[lambda_flux]
 
 # ------------------------------------------------------------------------------#
 
+def bc_flag(g, domain, tol):
+    b_faces = g.tags["domain_boundary_faces"].nonzero()[0]
+    b_face_centers = g.face_centers[:, b_faces]
 
+    top = b_face_centers[1] > domain["ymax"] - tol
+    bottom = b_face_centers[1] < domain["ymin"] + tol
+    left = b_face_centers[0] < domain["xmin"] + tol
+    right = b_face_centers[0] > domain["xmax"] - tol
+    boundary = top + bottom + left + right
+
+    return (top, bottom, left, right), boundary
