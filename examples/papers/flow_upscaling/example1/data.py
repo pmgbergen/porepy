281f0070fd6845eaa2ba40cf31b57fc222b56727
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2018-11-21 16:19:41 +0100

    update example 1 (Luisa geometries) for the new interface

--- examples/papers/flow_upscaling/example1/data.py
+++ examples/papers/flow_upscaling/example1/data.py
@@ -4,10 +4,11 @@ import porepy as pp
 # ------------------------------------------------------------------------------#
 
 
-def add_data(gb, data):
-    tol = data["tol"]
+def flow(gb, data, tol):
+    physics = data["physics"]
 
-    gb.add_node_props(['is_tangential', 'frac_num', 'porosity', 'aperture'])
+    mu = data["fluid"].dynamic_viscosity()
+
     for g, d in gb:
         param = pp.Parameters(g)
         d["is_tangential"] = True
@@ -16,53 +17,36 @@ def add_data(gb, data):
         zeros = np.zeros(g.num_cells)
         empty = np.empty(0)
 
-        if g.dim == 1:
-            d['frac_num'] = g.frac_num*unity
-        else:
-            d['frac_num'] = -1*unity
-
         # set the permeability
         if g.dim == 2:
-            kxx = data['km']*unity
+            kxx = data['km'] / mu * unity
             perm = pp.SecondOrderTensor(2, kxx=kxx, kyy=kxx, kzz=1)
         else: #g.dim == 1:
-            kxx = data['kf']*unity
+            kxx = data['kf'] / mu * unity
             perm = pp.SecondOrderTensor(1, kxx=kxx, kyy=1, kzz=1)
-        param.set_tensor("flow", perm)
-
-        # Source term
-        param.set_source("flow", zeros)
+        param.set_tensor(physics, perm)
 
         # Assign apertures
         aperture = np.power(data['aperture'], 2-g.dim)
-        d['aperture'] = aperture*unity
-        param.set_aperture(d['aperture'])
+        param.set_aperture(aperture)
 
         # Boundaries
         b_faces = g.tags["domain_boundary_faces"].nonzero()[0]
-        if b_faces.size != 0:
-
-            b_face_centers = g.face_centers[:, b_faces]
-
-            b_left = b_face_centers[0, :] < data["domain"]["xmin"] + tol
-            b_right = b_face_centers[0, :] > data["domain"]["xmax"] - tol
+        if b_faces.size:
+            (top, bottom, left, right), boundary = bc_flag(g, data["domain"], tol)
 
             labels = np.array(["neu"] * b_faces.size)
-            labels[b_left] = "dir"
-            labels[b_right] = "dir"
-            param.set_bc("flow", pp.BoundaryCondition(g, b_faces, labels))
+            labels[left + right] = "dir"
+            param.set_bc(physics, pp.BoundaryCondition(g, b_faces, labels))
 
             bc_val = np.zeros(g.num_faces)
-            bc_val[b_faces[b_left]] = 0 * pp.BAR
-            bc_val[b_faces[b_right]] = 20 * pp.BAR
-            param.set_bc_val("flow", bc_val)
+            bc_val[b_faces[right]] = data["bc_flow"]
+            param.set_bc_val(physics, bc_val)
         else:
-            param.set_bc("flow", pp.BoundaryCondition(g, empty, empty))
+            param.set_bc(physics, pp.BoundaryCondition(g, empty, empty))
 
         d["param"] = param
 
-    # Assign coupling permeability, the aperture is read from the lower dimensional grid
-    gb.add_edge_props("kn")
     for e, d in gb.edges():
         g_l = gb.nodes_of_edge(e)[0]
         mg = d["mortar_grid"]
@@ -70,100 +54,121 @@ def add_data(gb, data):
 
         aperture = gb.node_props(g_l, "param").get_aperture()
         gamma = check_P * np.power(aperture, 1/(2.-g.dim))
-        d["kn"] = data["kf"] * np.ones(mg.num_cells) / gamma
+        d["kn"] = data["kf"] / mu * np.ones(mg.num_cells) / gamma
 
 
 # ------------------------------------------------------------------------------#
 
+def advdiff(gb, data, tol):
+    physics = data["physics"]
 
-class AdvectiveDataAssigner(pp.ParabolicDataAssigner):
-    def __init__(self, gb, field_name, g, data_grid, data_problem):
-        self.gb = gb
-        self.field_name = field_name
-        self.data_problem = data_problem
-        self.tol = data_problem["tol"]
-        self.domain = data_problem["domain"]
+    cm = data["rock"].specific_heat_capacity()
+    cw = data["fluid"].specific_heat_capacity()
 
-        # define two pieces of the boundary, useful to impose boundary conditions
-        self.inflow = np.empty(0)
+    rhom = data["rock"].DENSITY
+    rhow = data["fluid"].density()
 
-        b_faces = g.tags["domain_boundary_faces"].nonzero()[0]
-        if b_faces.size > 0:
-            b_face_centers = g.face_centers[:, b_faces]
+    lm = data["rock"].thermal_conductivity()
+    lw = data["fluid"].thermal_conductivity()
 
-            self.inflow = b_face_centers[0, :] > self.domain["xmax"] - self.tol
-            self.outflow = b_face_centers[0, :] < self.domain["xmin"] + self.tol
+    for g, d in gb:
+        param = d["param"]
 
-        pp.ParabolicDataAssigner.__init__(self, g, data_grid, "transport")
+        unity = np.ones(g.num_cells)
+        zeros = np.zeros(g.num_cells)
+        empty = np.empty(0)
 
-    def porosity(self):
-        unity = np.ones(self.grid().num_cells)
-        if self.grid().dim == 2:
-            return self.data_problem["rock"].POROSITY * unity
+        # Assign the porosity
+        if g.dim == 2:
+            phi = data["rock"].POROSITY
         else:
-            return self.data_problem["porosity_f"] * unity
+            phi = data["porosity_f"]
 
-    def diffusivity(self):
+        ce = phi * rhow * cw + (1-phi) * rhom * cm
+        param.set_porosity(ce)
 
-        lm = self.data_problem["rock"].thermal_conductivity()
-        lw = self.data_problem["fluid"].thermal_conductivity()
-        phi = self.porosity()
+        # Assign the diffusivity
         l = np.power(lw, phi)*np.power(lm, 1-phi)
+        perm = pp.SecondOrderTensor(3, l * unity)
+        param.set_tensor(physics, perm)
 
-        unity = np.ones(self.grid().num_cells)
-        return pp.SecondOrderTensor(self.grid().dim, l*unity)
+        # Assign apertures
+        aperture = np.power(data['aperture'], 2-g.dim)
+        param.set_aperture(aperture)
 
-    def rock_specific_heat(self):
-        return self.data_problem["rock"].specific_heat_capacity()
+        # Flux
+        flux_name = data["keys_flow"][2]
+        d[flux_name] *= rhow * cw
 
-    def fluid_specific_heat(self):
-        return self.data_problem["fluid"].specific_heat_capacity()
+        # Boundaries
+        b_faces = g.tags["domain_boundary_faces"].nonzero()[0]
+        if b_faces.size:
+            (top, bottom, left, right), boundary = bc_flag(g, data["domain"], tol)
 
-    def rock_density(self):
-        return self.data_problem["rock"].DENSITY
+            labels = np.array(["neu"] * b_faces.size)
+            labels[left + right] = ["dir"]
+            param.set_bc(physics, pp.BoundaryCondition(g, b_faces, labels))
 
-    def fluid_density(self):
-        return self.data_problem["fluid"].density()
-
-    def bc(self):
-        b_faces = self.grid().tags["domain_boundary_faces"].nonzero()[0]
-        if b_faces.size == 0:
-            return pp.BoundaryCondition(self.grid(), np.empty(0), np.empty(0))
+            bc_val = np.zeros(g.num_faces)
+            bc_val[b_faces[right]] = data["bc_advdiff"]
+            param.set_bc_val(physics, bc_val)
         else:
-            labels = np.array(['neu'] * b_faces.size)
-            labels[np.logical_or(self.inflow, self.outflow)] = 'dir'
-        return pp.BoundaryCondition(self.grid(), b_faces, labels)
+            param.set_bc(physics, pp.BoundaryCondition(g, np.empty(0), np.empty(0)))
 
-    def bc_val(self, t):
-        # we are assuming that the solution variable is split and saved on the grid
-        # bucket every step. we avoid the first one by assuming t_0 = 0.
-        if t > 0:
-            sol = self.gb.node_props(self.grid(), self.field_name)
-        else:
-            sol = 80*np.ones(self.grid().num_cells)
+        d["param"] = param
 
-        bc_val = np.zeros(self.grid().num_faces)
-        b_faces = self.grid().tags["domain_boundary_faces"].nonzero()[0]
-        if b_faces.size > 0:
-            f_inflow = b_faces[self.inflow]
-            bc_val[f_inflow] = 20
+        # Assign time step
+        d["deltaT"] = data["deltaT"]
 
-            # reconstruct the cells on the outflow boundary, and then take the
-            # solution at previous time step and impose it as boundary condition
-            f_outflow = b_faces[self.outflow]
-            bc_val[f_outflow] = 80
-#            outflow = np.zeros(self.grid().num_faces, dtype=np.bool)
-#            outflow[f_outflow] = True
-#            c_outflow = np.where(self.grid().cell_faces.transpose()*outflow)[0]
-#            bc_val[f_outflow] = sol[c_outflow]
-#            print(sol[c_outflow])
-        return bc_val
+    lambda_flux = "lambda_flux" #+ data["flux"]
+    # Assign coupling discharge and diffusivity
+    for e, d in gb.edges():
+        d["flux_field"] = d[lambda_flux] * rhow * cw
 
-    def aperture(self):
-        aperture = np.power(self.data_problem['aperture'], 2-self.grid().dim)
-        unity = np.ones(self.grid().num_cells)
-        return aperture*unity
+        g_l = gb.nodes_of_edge(e)[0]
+        mg = d["mortar_grid"]
+        check_P = mg.slave_to_mortar_avg()
 
-    def initial_condition(self):
-        return 80*np.ones(self.grid().num_cells)
+        gamma = check_P * gb.node_props(g_l, "param").get_aperture()
+        k = check_P * gb.node_props(g_l, "param").get_tensor(physics).perm[0, 0, :]
+        d["kn"] = k * np.ones(mg.num_cells) / gamma
 
+
+#    def bc_val(self, t):
+#        # we are assuming that the solution variable is split and saved on the grid
+#        # bucket every step. we avoid the first one by assuming t_0 = 0.
+#        if t > 0:
+#            sol = self.gb.node_props(self.grid(), self.field_name)
+#        else:
+#            sol = 80*np.ones(self.grid().num_cells)
+#
+#        bc_val = np.zeros(self.grid().num_faces)
+#        b_faces = self.grid().tags["domain_boundary_faces"].nonzero()[0]
+#        if b_faces.size > 0:
+#            f_inflow = b_faces[self.inflow]
+#            bc_val[f_inflow] = 20
+#
+#            # reconstruct the cells on the outflow boundary, and then take the
+#            # solution at previous time step and impose it as boundary condition
+#            f_outflow = b_faces[self.outflow]
+#            bc_val[f_outflow] = 80
+##            outflow = np.zeros(self.grid().num_faces, dtype=np.bool)
+##            outflow[f_outflow] = True
+##            c_outflow = np.where(self.grid().cell_faces.transpose()*outflow)[0]
+##            bc_val[f_outflow] = sol[c_outflow]
+##            print(sol[c_outflow])
+#        return bc_val
+
+# ------------------------------------------------------------------------------#
+
+def bc_flag(g, domain, tol):
+    b_faces = g.tags["domain_boundary_faces"].nonzero()[0]
+    b_face_centers = g.face_centers[:, b_faces]
+
+    top = b_face_centers[1] > domain["ymax"] - tol
+    bottom = b_face_centers[1] < domain["ymin"] + tol
+    left = b_face_centers[0] < domain["xmin"] + tol
+    right = b_face_centers[0] > domain["xmax"] - tol
+    boundary = top + bottom + left + right
+
+    return (top, bottom, left, right), boundary
