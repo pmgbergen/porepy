b4ff605d6fd9f4e2730b28b485b477a7017ffe5c
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2018-11-29 09:28:05 +0100

    update the class to compute the mpfa upscaling and resolve a conflict from git stash

--- examples/papers/flow_upscaling/example3/main.py
+++ examples/papers/flow_upscaling/example3/main.py
@@ -4,91 +4,107 @@ import porepy as pp
 
 from examples.papers.flow_upscaling.import_grid import grid, square_grid, raw_from_csv
 
-from upscaling import MpfaUpscaling
+from mpfa_upscalig import MpfaUpscaling
 
-def slice_out(f, m):
-    return m.indices[m.indptr[f]: m.indptr[f+1]]
+def global_data(g, **kwargs):
+    km = 1.
+    physics = kwargs.get("physics", "flow")
 
-def set_data(gb, data):
+    param = pp.Parameters(g)
+    # set the permeability
+    perm = pp.SecondOrderTensor(3, km*np.ones(g.num_cells))
+    param.set_tensor("flow", perm)
+
+    # Boundaries
+    b_faces = g.tags["domain_boundary_faces"].nonzero()[0]
+    if b_faces.size:
+
+        labels = np.array(["dir"] * b_faces.size)
+        param.set_bc(physics, pp.BoundaryCondition(g, b_faces, labels))
+
+        bc_val = np.zeros(g.num_faces)
+        param.set_bc_val(physics, bc_val)
+    else:
+        param.set_bc(physics, pp.BoundaryCondition(g, empty, empty))
+
+    return param
+
+def local_node_data(g, d, gb, **kwargs):
+    kf = 1e4
+    km = 1.
+    aperture = 1e-3
+    physics = kwargs.get("physics", "flow")
+
+    param = pp.Parameters(g)
+    # set the permeability
+    if g.dim == 2:
+        kxx = km
+    else: #g.dim == 1:
+        kxx = kf
+    perm = pp.SecondOrderTensor(3, kxx*np.ones(g.num_cells))
+    param.set_tensor(physics, perm)
+
+    # Assign apertures
+    param.set_aperture(np.power(aperture, 2-g.dim))
+
+    return {"param": param}
+
+def local_edge_data(e, d, gb, **kwargs):
+    kf = 1e4
+
+    g_l = gb.nodes_of_edge(e)[0]
+    mg = d["mortar_grid"]
+    check_P = mg.low_to_mortar_avg()
+
+    aperture = gb.node_props(g_l, "param").get_aperture()
+    gamma = check_P * np.power(aperture, 1./(2.-g.dim))
+    return {"kn": kf * np.ones(mg.num_cells) / gamma}
+
+def compute_discharge(gb, **kwargs):
+
+    physics = kwargs.get("physics", "flow")
+    key = kwargs.get("key", "pressure")
+
+    flux = "flux"
+    flux_mortar = "lambda_" + flux
+
+    flux_discr = pp.Mpfa(physics)
+    flux_coupling = pp.RobinCoupling(key, flux_discr)
+
+    # define the dof and discretization for the grids
     for g, d in gb:
-        param = pp.Parameters(g)
+        d[pp.keywords.PRIMARY_VARIABLES] = {key: {"cells": 1}}
+        d[pp.keywords.DISCRETIZATION] = {key: {flux: flux_discr}}
 
-        # set the permeability
-        if g.dim == 2:
-            kxx = data["km"]
-        else: #g.dim == 1:
-            kxx = data["kf"]
-
-        perm = pp.SecondOrderTensor(3, kxx*np.ones(g.num_cells))
-        param.set_tensor("flow", perm)
-
-        # Source term
-        param.set_source("flow", np.zeros(g.num_cells))
-
-        # Assign apertures
-        aperture = np.power(data['aperture'], 2-g.dim)
-        param.set_aperture(aperture*np.ones(g.num_cells))
-
-        # Boundaries
-        b_faces = g.tags["domain_boundary_faces"].nonzero()[0]
-        if b_faces.size != 0:
-            labels = ["dir"] * b_faces.size
-            param.set_bc("flow", pp.BoundaryCondition(g, b_faces, labels))
-        else:
-            param.set_bc("flow", pp.BoundaryCondition(g, np.empty(0), np.empty(0)))
-
-        d["param"] = param
-
-    # Assign coupling permeability, the aperture is read from the lower dimensional grid
-    gb.add_edge_props("kn")
+    # define the interface terms to couple the grids
     for e, d in gb.edges():
-        g_l = gb.nodes_of_edge(e)[0]
-        mg = d["mortar_grid"]
-        check_P = mg.low_to_mortar_avg()
+        g_slave, g_master = gb.nodes_of_edge(e)
+        d[pp.keywords.PRIMARY_VARIABLES] = {flux_mortar: {"cells": 1}}
+        d[pp.keywords.COUPLING_DISCRETIZATION] = {
+            flux: {
+                g_slave:  (key, flux),
+                g_master: (key, flux),
+                e: (flux_mortar, flux_coupling)
+            }
+        }
 
-        aperture = gb.node_props(g_l, "param").get_aperture()
-        gamma = check_P * np.power(aperture, 1./(2.-g.dim))
-        d["kn"] = data["kf"] * np.ones(mg.num_cells) / gamma
+    # solution of the problem
+    assembler = pp.Assembler()
 
+    A, b, block_dof, full_dof = assembler.assemble_matrix_rhs(gb)
+    p = sps.linalg.spsolve(A, b)
 
-def set_boundary_conditions(gb, pts, pt, tol):
+    # compute the discharge and fix the sign with f
+    assembler.distribute_variable(gb, p, block_dof, full_dof)
+    pp.fvutils.compute_discharges(gb, physics=physics, p_name=key, lam_name=flux_mortar)
 
-    bd = np.array([[pt, (pt+1)%pts.shape[1], (pt+2)%pts.shape[1]],
-                   [pt, (pt-1)%pts.shape[1], (pt-2)%pts.shape[1]]])
+#    for g, d in gb:
+#        if g.dim == 2:
+#            print(d["pressure"].size, g.num_cells)
+#            pp.plot_grid(g, d["pressure"])
 
-    dist = lambda i, j: np.linalg.norm(pts[:, bd[j, i]] - pts[:, bd[j, i+1]])
-    length = np.array([[dist(i, j) for i in np.arange(2)] for j in np.arange(2)])
-    sum_length = np.sum(length, axis=1)
-
-    for g, d in gb:
-        # Boundaries
-        b_faces = g.tags["domain_boundary_faces"].nonzero()[0]
-        if b_faces.size != 0:
-            b_face_centers = g.face_centers[:, b_faces]
-            bc_val = np.zeros(g.num_faces)
-            for j in np.arange(2):
-                x_0 = np.array([0, length[j, 0]])
-                x_1 = np.array([length[j, 0], sum_length[j]])
-                y_0 = np.array([1, length[j, 0]/sum_length[j]])
-                y_1 = np.array([length[j, 0]/sum_length[j], 0])
-
-                for i in np.arange(2):
-                    start = pts[:, bd[j, i]]
-                    end = pts[:, bd[j, i+1]]
-
-                    # detect all the points aligned with the segment
-                    dist, _ = pp.cg.dist_points_segments(b_face_centers, start, end)
-                    mask = np.where(np.logical_and(dist < tol, dist >=-tol))[0]
-
-                    # compute the distance
-                    delta = np.tile(start, (mask.size, 1)).T - b_face_centers[:, mask]
-
-                    # define the boundary conditions
-                    val = (y_1[i] - y_0[i])/(x_1[i] - x_0[i])
-                    bc_val[b_faces[mask]] = np.linalg.norm(delta, axis=0)*val + y_0[i]
-
-            d["param"].set_bc_val("flow", bc_val)
-
+    # extract the discharge and return it
+    return gb.node_props(gb.grids_of_dimension(gb.dim_max())[0], "discharge")
 
 if __name__ == "__main__":
     file_geo = "network.csv"
@@ -97,10 +113,14 @@ if __name__ == "__main__":
     tol = 1e-3
     tol_small = 1e-5
 
-    mesh_args = {'mesh_size_frac': 1, "tol": tol}
+    mesh_args = {'mesh_size_frac': 0.25, "tol": tol}
 
-    gb, domain = square_grid(mesh_args)
-    pp.plot_grid(gb, alpha=0, info="all")
+    g = pp.StructuredTriangleGrid([2, 2], [1, 1])
+    g.compute_geometry()
+
+#    gb, domain = square_grid(mesh_args)
+#    g = gb.grids_of_dimension(2)[0]
+#    pp.plot_grid(g, alpha=0, info="fcn")
 
     np.set_printoptions(linewidth=9999)
 
@@ -109,88 +129,26 @@ if __name__ == "__main__":
     #fracs_pts, fracs_edges, _ = raw_from_csv(file_geo, mesh_args)
     #fracs_pts /= np.linalg.norm(np.amax(fracs_pts, axis=1))
 
-    # define the physical data
-
-    g = gb.grids_of_dimension(2)[0]
-    param = pp.Parameters(g)
-    param.set_aperture(1e-2)
-
-    data = {
-        "param": param,
-        "kf": 1e4,
-        "km": 1,
+    # the data for the local problem
+    data_local = {
+        "node": local_node_data,
+        "edge": local_edge_data,
         "tol": tol,
         "fractures": {"points": fracs_pts, "edges": fracs_edges},
-        "mesh_args_loc": {'mesh_size_frac': 0.125}
+        "mesh_args": {'mesh_size_frac': 1e-2},
+        "compute_discharge": compute_discharge
+        }
+
+    data = {
+        "param": global_data(g),
+        "tol": tol,
+        "local_problem": data_local
     }
+
+
+    #discr = pp.Mpfa("flow")
+    #discr.assemble_matrix_rhs(g, data)
+    #print(data["flow_flux"])
 
     discr = MpfaUpscaling("flow")
     discr.assemble_matrix_rhs(g, data)
-
-
-    #solver = pp.MpfaMixedDim("flow")
-    for g, d in gb:
-        # assuming that the grid max is 2
-        if g.dim == gb.dim_max():
-
-            face_faces = g.face_nodes.T*g.face_nodes
-            face_cells = g.face_nodes.T*g.cell_nodes()
-            b_nodes = g.get_all_boundary_nodes()
-
-            for f in g.get_internal_faces():
-                f_loc = slice_out(f, face_faces)
-                c_loc = slice_out(f, face_cells)
-                n_loc = slice_out(f, g.face_nodes)
-                f_nodes = g.nodes[:, n_loc]
-
-                f_loc = np.setdiff1d(f_loc, f)
-                # extract the faces (removing self), cells, and nodes
-                # to make the grid conforming consider the current edge as "fracture"
-                # TODO in principle also all the macro faces are constraints for the local grid
-                subdom = {"points": f_nodes[:2, :], "edges": np.array([0, 1]).reshape((2, 1))}
-                # keep the face nodes if are on the boundary
-                n_loc = n_loc[np.isin(n_loc, b_nodes, assume_unique=True)]
-                # collect from: faces, cells, node boundary (if)
-                pts = np.hstack((g.cell_centers[:, c_loc], g.face_centers[:, f_loc], g.nodes[:, n_loc]))
-                is_cell_centers = np.hstack(([True]*c_loc.size, [False]*(f_loc.size+n_loc.size)))
-
-                # sort the nodes by assuming star shaped region
-                mask = pp.utils.sort_points.sort_point_plane(pts, g.face_centers[:, f])
-                pts = pts[:, mask]
-                is_cell_centers = is_cell_centers[mask]
-
-                # consider the background fractures only limited to the current region
-                fracs_pts_int, fracs_edges_int = pp.cg.intersect_polygon_lines(pts, fracs_pts, fracs_edges)
-                fracs = {"points": fracs_pts_int, "edges": fracs_edges_int}
-                # prepare the data for gmsh
-                gb_loc = pp.fracs.meshing.simplex_grid(fracs, domain=pts[:2, :], subdomains=subdom, **mesh_args_loc)
-                pp.plot_grid(gb_loc, alpha=0)
-
-                # save the faces of the face f
-                g_h = gb_loc.grids_of_dimension(2)[0]
-
-                dist, _ = pp.cg.dist_points_segments(g_h.face_centers, f_nodes[:, 0], f_nodes[:, 1])
-                f_loc = np.where(np.logical_and(dist < tol_small, dist >=-tol_small))[0]
-                # compute the sign with respect to f and normalize the result
-                sign = np.sign(np.einsum('ij,i->j', g_h.face_normals[:, f_loc], g.face_normals[:, f]))
-
-                # assign common data
-                set_data(gb_loc, data)
-
-                continue
-                # need to compute the upscaled transmissibility
-                # we loop on all the cell_centers and give the data
-                for pt in np.where(is_cell_centers)[0]:
-                    # apply the boundary conditions for the current problem
-                    set_boundary_conditions(gb_loc, pts, pt, tol)
-
-                    # Discretize and solve the linear system
-                    p = sps.linalg.spsolve(*solver.matrix_rhs(gb_loc))
-
-                    # compute the discharge and fix the sign with f
-                    solver.split(gb_loc, "pressure", p)
-                    pp.fvutils.compute_discharges(gb_loc)
-                    discharge = sign * gb_loc.node_props(g_h, "discharge")[f_loc]
-                    print(np.sum(discharge))
-                    d
-                    # inherit from fv_elliptic called MfaElliptic
--- examples/papers/flow_upscaling/example3/mpfa_upscalig.py
+++ examples/papers/flow_upscaling/example3/mpfa_upscalig.py
@@ -41,11 +41,12 @@ class MpfaUpscaling(FVElliptic):
         param = data["param"]
         #k = param.get_tensor(self)
         k = 1
-        #bnd = param.get_bc(self)
-        bnd = None
+        bnd = param.get_bc(self)
+
+        data_patch = data["local_problem"]
 
         flux, bound_flux, bound_pressure_cell, bound_pressure_face = \
-            self._local_discr(g, k, bnd, data)
+            self._local_discr(g, k, bnd, data_patch)
 
         data[self._key() + "flux"] = flux
         data[self._key() + "bound_flux"] = bound_flux
@@ -58,21 +59,28 @@ class MpfaUpscaling(FVElliptic):
         # composed by a field "points" containing all the points and a field
         # "edges" containing all the fractures
         fracs = data["fractures"]
-        mesh_args_loc = data["mesh_args_loc"]
+        mesh_args = data["mesh_args"]
 
         # map of faces connected to a face via its nodes
-        face_faces = g.face_nodes.T*g.face_nodes
+        patch_face_faces = g.face_nodes.T*g.face_nodes
         # map of cells connected to a face via its nodes
-        face_cells = g.face_nodes.T*g.cell_nodes()
-        # all the nodes at the boundary of the grid
-        b_nodes = g.get_all_boundary_nodes()
+        patch_face_cells = g.face_nodes.T*g.cell_nodes()
 
-        # NOTE I'm assuming a 2d grid
+        node_faces = g.face_nodes.tocsr().T
+        face_cells = g.cell_faces.tocsr().T
+
+        # NOTE I'm assuming a 2d grid, I tryed to be general enough
         assert g.dim == 2
 
-        tol_small = 1e-5
+        #size =
+        #I =
+        #J =
+        #dataIJ =
+
 
         for f in g.get_internal_faces():
+
+            print("face", f)
 
             # nodes of the current face
             f_nodes = self._slice_out(f, g.face_nodes)
@@ -82,77 +90,152 @@ class MpfaUpscaling(FVElliptic):
             # the 3) local fractures
 
             # 1) get the patch composed by a list of ordered points
-            pts_patch, is_cell_centers = self._patch(g, f, face_faces, face_cells, b_nodes)
+            patch_pts, bc_type, bc_corner = self._patch(g, f, bnd, patch_face_faces, patch_face_cells, node_faces, face_cells)
 
             # 2) to make the grid conforming consider the current face as "subdomain"
-            # TODO in principle also all the macro faces are constraints for the local grid
-            subdom_edges = np.arange(f_nodes.size).reshape((g.dim, 1))
-            subdom = {"points": f_nodes_coord[:g.dim], "edges": subdom_edges}
+            # with auxiliary as tag
+            subdom = {"points": f_nodes_coord[:g.dim],
+                      "edges": np.arange(f_nodes.size).reshape((g.dim, 1))}
 
             # 3) consider the background fractures only limited to the current region
-            fracs_int = self._patch_fracs(pts_patch, fracs)
+            patch_fracs = self._patch_fracs(patch_pts, fracs)
 
             # construct the patch grid bucket
-            gb_patch = self._patch_gb(fracs_int, pts_patch[:g.dim], subdom, mesh_args_loc)
-            pp.plot_grid(gb_patch, alpha=0)
+            patch_gb = self._patch_gb(patch_fracs, patch_pts[:g.dim], subdom, mesh_args)
+            #pp.plot_grid(patch_gb, alpha=0, info="f")
 
             # we need to identify the faces in the gb_patch that belong to the original face
-            g_h = gb_patch.grids_of_dimension(g.dim)[0]
-
-            # here I assume that we are in 2d
-            dist, _ = pp.cg.dist_points_segments(g_h.face_centers, f_nodes_coord[:, 0], f_nodes_coord[:, 1])
-            f_loc = np.where(np.logical_and(dist < tol_small, dist >=-tol_small))[0]
+            # this is used afterward to compute the discharge
+            g_h = patch_gb.grids_of_dimension(g.dim)[0]
+            f_loc = [v for k, v in g_h.tags.items() if "auxiliary_" in k][0]
+            f_loc = np.where(f_loc)[0]
 
             # compute the sign with respect to f and normalize the result
             sign = np.sign(np.einsum('ij,i->j', g_h.face_normals[:, f_loc], g.face_normals[:, f]))
 
-            dd
+            # assign common data, defined by the user. Mostly it's the aperture and
+            # permeability for all the objects
+            self._patch_set_data(patch_gb, data)
 
-            # assign common data
-            set_data(gb_patch, data)
-
-            continue
             # need to compute the upscaled transmissibility
             # we loop on all the cell_centers and give the data
-            for pt in np.where(is_cell_centers)[0]:
+            for pt in np.where(bc_corner[0])[0]:
+
+                #import pdb; pdb.set_trace()
+
                 # apply the boundary conditions for the current problem
-                set_boundary_conditions(gb_patch, pts, pt, tol)
+                self._patch_set_bc(patch_gb, patch_pts, pt, bc_type, data["tol"])
 
-                # Discretize and solve the linear system
-                p = sps.linalg.spsolve(*solver.matrix_rhs(gb_patch))
+                # solution operator, given the patch grid bucket return the
+                # discharge computed
+                discharge = np.sum(sign * data["compute_discharge"](patch_gb)[f_loc])
 
-                # compute the discharge and fix the sign with f
-                solver.split(gb_patch, "pressure", p)
-                pp.fvutils.compute_discharges(gb_patch)
-                discharge = sign * gb_patch.node_props(g_h, "discharge")[f_loc]
+                print("discharge", discharge, bc_corner[:, pt])
 
 
-
-    def _patch(self, g, f, face_faces, face_cells, b_nodes):
+    def _patch(self, g, f, bnd, patch_face_faces, patch_face_cells, node_faces, face_cells):
         """
         Construct the local patch of the input face f
         """
+
+        # all the faces at the boundary of the grid
+        b_faces = g.tags["domain_boundary_faces"]
+
+        # all the nodes at the boundary of the grid
+        b_nodes = g.tags["domain_boundary_nodes"]
+
         # extract the faces (removing self), cells, and nodes
-        f_loc = self._slice_out(f, face_faces)
+        f_loc = self._slice_out(f, patch_face_faces)
         f_loc = np.setdiff1d(f_loc, f)
-        c_loc = self._slice_out(f, face_cells)
+        c_loc = self._slice_out(f, patch_face_cells)
         n_loc = self._slice_out(f, g.face_nodes)
 
         # keep the face nodes if are on the boundary
-        n_loc = n_loc[np.isin(n_loc, b_nodes, assume_unique=True)]
+        n_loc = n_loc[b_nodes[n_loc]]
 
-        # we need to construct the patch
-        # collect from: faces, cells, node boundary (if)
-        pts = np.hstack((g.cell_centers[:, c_loc], g.face_centers[:, f_loc], g.nodes[:, n_loc]))
-        # TODO need a better name here
-        is_cell_centers = np.hstack(([True]*c_loc.size, [False]*(f_loc.size+n_loc.size)))
+        # where to store the boundary ordered points of the patch
+        pts = np.zeros((g.dim, f_loc.size + c_loc.size + n_loc.size))
 
-        # sort the nodes by assuming star shaped region
-        mask = pp.utils.sort_points.sort_point_plane(pts, g.face_centers[:, f])
-        pts = pts[:, mask]
-        is_cell_centers = is_cell_centers[mask]
+        # second line 0 = cell, 1 = node. third line id
+        bc_corner = -1 * np.ones((3, pts.shape[1]))
+        bc_corner[0] = 0
 
-        return pts, is_cell_centers
+        # devo introdurre una logica per passare le informazioni dei bordi, magari
+        # appunto tramite gli eedges
+        # it connects pts i to i+1
+        bc_type = np.zeros((2, pts.shape[1]), dtype=np.object)
+        bc_type[0] = "dir"
+        bc_type[1] = False
+
+        pts_pos = 0
+        faces = np.array([f_loc[0]])
+
+        while f_loc.size:
+
+            # select the face
+            mask = np.where(np.isin(faces, f_loc))[0][0]
+            face = faces[mask]
+
+            # save the type of boundary condition associate with the current
+            # face
+            if b_faces[face]:
+                bc_tag = bnd.is_dir[face] * "dir" + \
+                         bnd.is_neu[face] * "neu" + \
+                         bnd.is_rob[face] * "rob"
+                bc_type[:, pts_pos] = [bc_tag, True]
+
+            # we add the face in the list
+            pts[:g.dim, pts_pos] = g.face_centers[:g.dim, face]
+            f_loc = f_loc[f_loc != face]
+            pts_pos += 1
+
+            # check also if the face is a bounday face or it has
+            # a node at the boundary
+            nodes = self._slice_out(face, g.face_nodes)
+            isin = np.isin(nodes, n_loc)
+
+            # check if it is a boundary face, in case we need to consider
+            # a node
+            if b_faces[face] and np.any(isin):
+                # identify the node
+                mask = np.where(isin)[0][0]
+                node = nodes[mask]
+                n_loc = n_loc[n_loc != node]
+
+                # save the node in the list of points
+                pts[:g.dim, pts_pos] = g.nodes[:g.dim, node]
+
+                # find the next connected faces at the boundary
+                faces = self._slice_out(node, node_faces)
+                faces = faces[b_faces[faces]]
+                faces = faces[faces != face]
+
+                # save the fact that it is a boundary corner
+                bc_corner[:, pts_pos] = [1, 1, node]
+
+            else:
+                # identify among the cells related to the current face
+                # the single one that is also in the patch
+                # select it and remove from the list of patch cells
+                cells = self._slice_out(face, face_cells)
+                mask = np.where(np.isin(cells, c_loc))[0][0]
+                cell = cells[mask]
+                c_loc = c_loc[c_loc != cell]
+
+                # save the cell center in the list of points
+                pts[:g.dim, pts_pos] = g.cell_centers[:g.dim, cell]
+
+                # identify among the faces related to the current cell
+                # the single one that is also in the patch
+                # select it and remove from the list of patch faces
+                faces = self._slice_out(cell, g.cell_faces)
+
+                # save the fact that it is a boundary corner
+                bc_corner[:, pts_pos] = [1, 0, cell]
+
+            pts_pos += 1
+
+        return pts, bc_type, bc_corner
 
     def _patch_fracs(self, pts_patch, fracs):
         """
@@ -163,12 +246,70 @@ class MpfaUpscaling(FVElliptic):
                                                    fracs["points"], fracs["edges"])
         return {"points": pts, "edges": edges}
 
-    def _patch_gb(self, fracs, patch, subdom, mesh_args_loc):
+    def _patch_gb(self, fracs, patch, subdom, mesh_args):
         """
         Construct the grid bucket for the current patch
         """
         return pp.fracs.meshing.simplex_grid(fracs, domain=patch, \
-                                             subdomains=subdom, **mesh_args_loc)
+                                             subdomains=subdom, **mesh_args)
+
+    def _patch_set_data(self, gb, data):
+        # the data are user specific, in principle only the permeability and aperture
+        # need to be inserted by the user and not a source term.
+        for g, d in gb:
+            d.update(data["node"](g, d, gb))
+
+        for e, d in gb.edges():
+            d.update(data["edge"](e, d, gb))
+
+    def _patch_set_bc(self, gb, pts, pt, bc_type, tol):
+
+        bd = np.array([[pt, (pt+1)%pts.shape[1], (pt+2)%pts.shape[1]],
+                       [pt, (pt-1)%pts.shape[1], (pt-2)%pts.shape[1]]])
+
+        dist = lambda i, j: np.linalg.norm(pts[:, bd[j, i]] - pts[:, bd[j, i+1]])
+        length = np.array([[dist(i, j) for i in np.arange(2)] for j in np.arange(2)])
+        sum_length = np.sum(length, axis=1)
+        for g, d in gb:
+            param = d["param"]
+
+            # Boundaries
+            b_faces = g.tags["domain_boundary_faces"].nonzero()[0]
+            if b_faces.size != 0:
+
+                labels = ["dir"] * b_faces.size
+                bc = pp.BoundaryCondition(g, b_faces, labels)
+                param.set_bc("flow", bc)
+
+                b_face_centers = g.face_centers[:, b_faces]
+                bc_val = np.zeros(g.num_faces)
+                for j in np.arange(2):
+                    x_0 = np.array([0, length[j, 0]])
+                    x_1 = np.array([length[j, 0], sum_length[j]])
+                    y_0 = np.array([1, length[j, 0]/sum_length[j]])
+                    y_1 = np.array([length[j, 0]/sum_length[j], 0])
+
+                    for i in np.arange(2):
+                        start = pts[:, bd[j, i]]
+                        end = pts[:, bd[j, i+1]]
+
+                        # detect all the points aligned with the segment
+                        dist, _ = pp.cg.dist_points_segments(b_face_centers[:2], start, end)
+                        mask = np.where(np.logical_and(dist < tol, dist >=-tol))[0]
+
+                        # compute the distance
+                        delta = np.tile(start, (mask.size, 1)).T - b_face_centers[:2, mask]
+
+                        # define the boundary conditions
+                        val = (y_1[i] - y_0[i])/(x_1[i] - x_0[i])
+                        bc_val[b_faces[mask]] = np.linalg.norm(delta, axis=0)*val + y_0[i]
+
+                param.set_bc_val("flow", bc_val)
+            else:
+                bc = pp.BoundaryCondition(g, np.empty(0), np.empty(0))
+                param.set_bc("flow", bc)
+
+            d["param"] = param
 
     def _slice_out(self, f, m):
         return m.indices[m.indptr[f]: m.indptr[f+1]]
