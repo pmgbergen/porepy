848dd75b204aa7140264efc8099874678486f9e7
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2018-12-03 19:17:09 +0100

    in dfn_transport, add first example and update example 2

--- examples/papers/dfn_transport/example1/main.py
+++ examples/papers/dfn_transport/example1/main.py
@@ -10,17 +10,14 @@ def bc_flag(g, domain, tol):
     b_face_centers = g.face_centers[:, b_faces]
 
     # define inflow type boundary conditions
-    out_flow_start = np.array([1.011125, 0.249154, 0.598708])
-    out_flow_end = np.array([1.012528, 0.190858, 0.886822])
+    out_flow_z = b_face_centers[2] > 0.4
+    out_flow_x = b_face_centers[0] < tol
 
-    # detect all the points aligned with the segment
-    dist, _ = pp.cg.dist_points_segments(b_face_centers, out_flow_start, out_flow_end)
-    dist = dist.flatten()
-    out_flow = np.logical_and(dist < tol, dist >=-tol)
+    out_flow = np.logical_and(out_flow_x, out_flow_z)
 
     # define outflow type boundary conditions
-    in_flow_start = np.array([0.206507, 0.896131, 0.183632])
-    in_flow_end = np.array([0.181980, 0.813947, 0.478618])
+    in_flow_start = np.array([0, 0, 0.3])
+    in_flow_end = np.array([0, 1, 0.3])
 
     # detect all the points aligned with the segment
     dist, _ = pp.cg.dist_points_segments(b_face_centers, in_flow_start, in_flow_end)
@@ -31,13 +28,13 @@ def bc_flag(g, domain, tol):
 
 def main():
 
-    input_folder = "../geometries/"
-    file_name = input_folder + "example3.fab"
-    file_inters = input_folder + "example3.dat"
+    input_folder = "../geometries/example1/"
+    file_name = input_folder + "DFN_2.fab"
+    file_inters = input_folder + "TRACES_2.dat"
 
-    mesh_size = 1 # for 1k triangles
+    #mesh_size = 1 # for 1k triangles
     #mesh_size = 0.9 * np.power(2., -4) # for 3k triangles
-    #mesh_size = 0.875 * np.power(2., -5) # for 10k triangles
+    mesh_size = 0.875 * np.power(2., -5) # for 10k triangles
     mesh_kwargs = {"mesh_size_frac": mesh_size, "mesh_size_min": mesh_size / 20}
     tol = 1e-4
     gb = pp.importer.dfn_3d_from_fab(file_name, file_inters, tol=tol, **mesh_kwargs)
@@ -49,7 +46,7 @@ def main():
     domain = gb.bounding_box(as_dict=True)
 
     param = {"domain": domain, "tol": tol, "k": 1,
-             "diff": 1e-3, "time_step": 0.05, "n_steps": 500,
+             "diff": 1e-4, "time_step": 0.05, "n_steps": 1000,
              "folder": "solution"}
 
     # the flow problem
@@ -59,6 +56,7 @@ def main():
 
     # the advection-diffusion problem
     discr.advdiff(gb, param, model_flow, bc_flag)
+
 
 if __name__ == "__main__":
     main()
