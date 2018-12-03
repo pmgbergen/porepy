9f784421904a3c4a622a84038e015bf66ab4315c
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2018-12-03 15:14:38 +0100

    update the script to run Luisa's networks

--- examples/papers/flow_upscaling/example1/data.py
+++ examples/papers/flow_upscaling/example1/data.py
@@ -4,13 +4,14 @@ import porepy as pp
 # ------------------------------------------------------------------------------#
 
 
-def flow(gb, data, tol):
-    physics = data["physics"]
+def flow(gb, model, data):
+    tol = data["tol"]
 
+    model_data = model + "_data"
     mu = data["fluid"].dynamic_viscosity()
 
     for g, d in gb:
-        param = pp.Parameters(g)
+        param = {}
         d["is_tangential"] = True
 
         unity = np.ones(g.num_cells)
@@ -24,43 +25,53 @@ def flow(gb, data, tol):
         else: #g.dim == 1:
             kxx = data['kf'] / mu * unity
             perm = pp.SecondOrderTensor(1, kxx=kxx, kyy=1, kzz=1)
-        param.set_tensor(physics, perm)
+        param["second_order_tensor"] = perm
 
         # Assign apertures
-        aperture = np.power(data['aperture'], 2-g.dim)
-        param.set_aperture(aperture)
+        param["aperture"] = np.power(data['aperture'], 2-g.dim) * unity
 
         # Boundaries
         b_faces = g.tags["domain_boundary_faces"].nonzero()[0]
+        bc_val = np.zeros(g.num_faces)
         if b_faces.size:
             (top, bottom, left, right), boundary = bc_flag(g, data["domain"], tol)
 
             labels = np.array(["neu"] * b_faces.size)
             labels[left + right] = "dir"
-            param.set_bc(physics, pp.BoundaryCondition(g, b_faces, labels))
+            param["bc"] = pp.BoundaryCondition(g, b_faces, labels)
 
-            bc_val = np.zeros(g.num_faces)
             bc_val[b_faces[right]] = data["bc_flow"]
-            param.set_bc_val(physics, bc_val)
         else:
-            param.set_bc(physics, pp.BoundaryCondition(g, empty, empty))
+            param["bc"] = pp.BoundaryCondition(g, empty, empty)
 
-        d["param"] = param
+        param["bc_values"] = bc_val
+
+        d[pp.PARAMETERS] = pp.Parameters(g, model_data, param)
+        d[pp.DISCRETIZATION_MATRICES] = {model_data: {}}
 
     for e, d in gb.edges():
         g_l = gb.nodes_of_edge(e)[0]
         mg = d["mortar_grid"]
         check_P = mg.slave_to_mortar_avg()
 
-        aperture = gb.node_props(g_l, "param").get_aperture()
+        aperture = gb.node_props(g_l, pp.PARAMETERS)[model_data]["aperture"]
         gamma = check_P * np.power(aperture, 1/(2.-g.dim))
-        d["kn"] = data["kf"] / mu * np.ones(mg.num_cells) / gamma
+        kn = data["kf"] / mu * np.ones(mg.num_cells) / gamma
 
+        param = {"normal_diffusivity": kn}
+
+        d[pp.PARAMETERS] = pp.Parameters(e, model_data, param)
+        d[pp.DISCRETIZATION_MATRICES] = {model_data: {}}
+
+    return model_data
 
 # ------------------------------------------------------------------------------#
 
-def advdiff(gb, data, tol):
-    physics = data["physics"]
+def advdiff(gb, model, model_flow, data):
+    tol = data["tol"]
+
+    model_data_adv = model + "_data_adv"
+    model_data_diff = model + "_data_diff"
 
     cm = data["rock"].specific_heat_capacity()
     cw = data["fluid"].specific_heat_capacity()
@@ -71,8 +82,12 @@ def advdiff(gb, data, tol):
     lm = data["rock"].thermal_conductivity()
     lw = data["fluid"].thermal_conductivity()
 
+    flux_discharge_name = data["flux"]
+    flux_mortar_name = data["mortar_flux"]
+
     for g, d in gb:
-        param = d["param"]
+        param_adv = {}
+        param_diff = {}
 
         unity = np.ones(g.num_cells)
         zeros = np.zeros(g.num_cells)
@@ -85,79 +100,72 @@ def advdiff(gb, data, tol):
             phi = data["porosity_f"]
 
         ce = phi * rhow * cw + (1-phi) * rhom * cm
-        param.set_porosity(ce)
+        param_adv["mass_weight"] = ce
 
         # Assign the diffusivity
-        l = np.power(lw, phi)*np.power(lm, 1-phi)
-        perm = pp.SecondOrderTensor(3, l * unity)
-        param.set_tensor(physics, perm)
+        l = np.power(lw, phi)*np.power(lm, 1-phi) * unity
+        param_diff["second_order_tensor"] = pp.SecondOrderTensor(3, l)
 
         # Assign apertures
-        aperture = np.power(data['aperture'], 2-g.dim)
-        param.set_aperture(aperture)
+        param_diff["aperture"] = d[pp.PARAMETERS][model_flow]["aperture"]
+        param_adv["aperture"] = d[pp.PARAMETERS][model_flow]["aperture"]
 
         # Flux
-        flux_name = data["keys_flow"][2]
-        d[flux_name] *= rhow * cw
+        param_adv[flux_discharge_name] = rhow * cw * d[flux_discharge_name]
 
         # Boundaries
         b_faces = g.tags["domain_boundary_faces"].nonzero()[0]
+        bc_val = np.zeros(g.num_faces)
         if b_faces.size:
             (top, bottom, left, right), boundary = bc_flag(g, data["domain"], tol)
 
-            labels = np.array(["neu"] * b_faces.size)
-            labels[left + right] = ["dir"]
-            param.set_bc(physics, pp.BoundaryCondition(g, b_faces, labels))
+            labels_adv = np.array(["neu"] * b_faces.size)
+            labels_adv[left + right] = ["dir"]
 
-            bc_val = np.zeros(g.num_faces)
+            labels_diff = np.array(["neu"] * b_faces.size)
+            labels_diff[right] = ["dir"]
+
+            param_adv["bc"] = pp.BoundaryCondition(g, b_faces, labels_adv)
+            param_diff["bc"] = pp.BoundaryCondition(g, b_faces, labels_diff)
+
             bc_val[b_faces[right]] = data["bc_advdiff"]
-            param.set_bc_val(physics, bc_val)
         else:
-            param.set_bc(physics, pp.BoundaryCondition(g, np.empty(0), np.empty(0)))
+            param_adv["bc"] = pp.BoundaryCondition(g, np.empty(0), np.empty(0))
+            param_diff["bc"] = pp.BoundaryCondition(g, np.empty(0), np.empty(0))
 
-        d["param"] = param
+        param_adv["bc_values"] = bc_val
+        param_diff["bc_values"] = bc_val
 
         # Assign time step
-        d["deltaT"] = data["deltaT"]
+        param_adv["time_step"] = data["time_step"]
 
-    lambda_flux = "lambda_flux" #+ data["flux"]
+        param = pp.Parameters(g, [model_data_adv, model_data_diff], [param_adv, param_diff])
+        d[pp.PARAMETERS] = param
+        d[pp.DISCRETIZATION_MATRICES] = {model_data_adv: {}, model_data_diff: {}}
+
     # Assign coupling discharge and diffusivity
     for e, d in gb.edges():
-        d["flux_field"] = d[lambda_flux] * rhow * cw
+        param_adv = {}
+        param_diff = {}
+
+        param_adv[flux_discharge_name] = d[flux_mortar_name] * rhow * cw
 
         g_l = gb.nodes_of_edge(e)[0]
         mg = d["mortar_grid"]
         check_P = mg.slave_to_mortar_avg()
 
-        gamma = check_P * gb.node_props(g_l, "param").get_aperture()
-        k = check_P * gb.node_props(g_l, "param").get_tensor(physics).perm[0, 0, :]
-        d["kn"] = k * np.ones(mg.num_cells) / gamma
+        aperture = gb.node_props(g_l, pp.PARAMETERS)[model_data_diff]["aperture"]
+        gamma = np.power(aperture, 1/(2.-g.dim))
 
+        k = gb.node_props(g_l, pp.PARAMETERS)[model_data_diff]["second_order_tensor"]
 
-#    def bc_val(self, t):
-#        # we are assuming that the solution variable is split and saved on the grid
-#        # bucket every step. we avoid the first one by assuming t_0 = 0.
-#        if t > 0:
-#            sol = self.gb.node_props(self.grid(), self.field_name)
-#        else:
-#            sol = 80*np.ones(self.grid().num_cells)
-#
-#        bc_val = np.zeros(self.grid().num_faces)
-#        b_faces = self.grid().tags["domain_boundary_faces"].nonzero()[0]
-#        if b_faces.size > 0:
-#            f_inflow = b_faces[self.inflow]
-#            bc_val[f_inflow] = 20
-#
-#            # reconstruct the cells on the outflow boundary, and then take the
-#            # solution at previous time step and impose it as boundary condition
-#            f_outflow = b_faces[self.outflow]
-#            bc_val[f_outflow] = 80
-##            outflow = np.zeros(self.grid().num_faces, dtype=np.bool)
-##            outflow[f_outflow] = True
-##            c_outflow = np.where(self.grid().cell_faces.transpose()*outflow)[0]
-##            bc_val[f_outflow] = sol[c_outflow]
-##            print(sol[c_outflow])
-#        return bc_val
+        param_diff["normal_diffusivity"] = check_P.dot(k.values[0, 0, :] / gamma)
+
+        param = pp.Parameters(e, [model_data_adv, model_data_diff], [param_adv, param_diff])
+        d[pp.PARAMETERS] = param
+        d[pp.DISCRETIZATION_MATRICES] = {model_data_adv: {}, model_data_diff: {}}
+
+    return model_data_adv, model_data_diff
 
 # ------------------------------------------------------------------------------#
 
