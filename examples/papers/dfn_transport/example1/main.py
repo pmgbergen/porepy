db1998c332f6167916f834c04ec613da7f2776c0
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2018-12-04 10:24:36 +0100

    dfn transport, the first example loops on all the considered geometries

--- examples/papers/dfn_transport/example1/main.py
+++ examples/papers/dfn_transport/example1/main.py
@@ -28,33 +28,40 @@ def bc_flag(g, domain, tol):
 def main():
 
     input_folder = "../geometries/example1/"
-    file_name = input_folder + "DFN_2.fab"
-    file_inters = input_folder + "TRACES_2.dat"
 
     #mesh_size = 1 # for 1k triangles
     #mesh_size = 0.9 * np.power(2., -4) # for 3k triangles
     mesh_size = 0.875 * np.power(2., -5) # for 10k triangles
     mesh_kwargs = {"mesh_size_frac": mesh_size, "mesh_size_min": mesh_size / 20}
-    tol = 1e-4
-    gb = pp.importer.dfn_3d_from_fab(file_name, file_inters, tol=tol, **mesh_kwargs)
+    tol = 1e-6
 
-    gb.remove_nodes(lambda g: g.dim == 0)
-    gb.compute_geometry()
-    gb.assign_node_ordering()
+    num_simul = 21
+    for simul in np.arange(1, num_simul+1):
 
-    domain = gb.bounding_box(as_dict=True)
+        file_name = input_folder + "DFN_" + str(simul) + ".fab"
+        file_inters = input_folder + "TRACES_" + str(simul) + ".dat"
 
-    param = {"domain": domain, "tol": tol, "k": 1,
-             "diff": 1e-4, "time_step": 0.05, "n_steps": 1000,
-             "folder": "solution"}
+        folder = "solution_" + str(simul)
 
-    # the flow problem
-    model_flow = discr.flow(gb, param, bc_flag)
+        gb = pp.importer.dfn_3d_from_fab(file_name, file_inters, tol=tol, **mesh_kwargs)
 
-    jump_flux(gb, param["mortar_flux"])
+        gb.remove_nodes(lambda g: g.dim == 0)
+        gb.compute_geometry()
+        gb.assign_node_ordering()
 
-    # the advection-diffusion problem
-    discr.advdiff(gb, param, model_flow, bc_flag)
+        domain = gb.bounding_box(as_dict=True)
+
+        param = {"domain": domain, "tol": tol, "k": 1,
+                 "diff": 1e-4, "time_step": 0.05, "n_steps": 300,
+                 "folder": folder}
+
+        # the flow problem
+        model_flow = discr.flow(gb, param, bc_flag)
+
+        jump_flux(gb, param["mortar_flux"])
+
+        # the advection-diffusion problem
+        discr.advdiff(gb, param, model_flow, bc_flag)
 
 
 if __name__ == "__main__":
