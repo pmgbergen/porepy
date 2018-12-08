fca2c2cce0bc0246be4397deb5ced43643383e64
IvarStefansson <ivarstef@gmail.com>
2018-12-08 18:10:15 +0100

    Add DivD and BiotStabilization classes

--- src/porepy/numerics/mixed_dim/__init__.py
+++ src/porepy/numerics/mixed_dim/__init__.py
@@ -2,4 +2,7 @@ from .abstract_coupling import AbstractCoupling
 from .coupler import Coupler
 from .solver import Solver
 from .abstract_assembler import AbstractAssembler
-from .elliptic_discretization import EllipticDiscretization
+from .elliptic_discretization import (
+    EllipticDiscretization,
+    VectorEllipticDiscretization,
+)
