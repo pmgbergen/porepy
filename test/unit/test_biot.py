bb31fbb141785c61bcf19dc01e412e577948408d
IvarStefansson <ivarstef@gmail.com>
2019-01-10 18:38:07 +0100

    New test on rhs of Biot and black formatting

--- test/unit/test_biot.py
+++ test/unit/test_biot.py
@@ -1,6 +1,5 @@
 import scipy.sparse as sps
 import numpy as np
-import numpy.linalg
 import unittest
 import porepy as pp
 from test.integration import setup_grids_mpfa_mpsa_tests as setup_grids
@@ -17,6 +16,16 @@ class BiotTest(unittest.TestCase):
             g, bound_faces.ravel("F"), ["dir"] * bound_faces.size
         )
         return bound_mech, bound_flow
+
+    def make_initial_conditions(self, g, x0, y0, p0):
+        """ Make uniform initial condition vectors.
+        """
+        initial_displacement = x0 * np.ones((g.dim, g.num_cells))
+        initial_displacement[1] = y0
+        initial_displacement = initial_displacement.ravel("F")
+        initial_pressure = p0 * np.ones(g.num_cells)
+        initial_state = np.concatenate((initial_displacement, initial_pressure))
+        return initial_displacement, initial_pressure, initial_state
 
     def test_no_dynamics_2d(self):
         g_list = setup_grids.setup_2d()
@@ -111,9 +120,16 @@ class BiotTest(unittest.TestCase):
         kw_m = "mechanics"
         kw_f = "flow"
         bound_mech, bound_flow = self.make_boundary_conditions(g)
-        pp.initialize_default_data(g, d, kw_m, {"bc": bound_mech, "biot_alpha": 1})
-        pp.initialize_default_data(g, d, kw_f, {"bc": bound_flow, "biot_alpha": 1})
+        initial_disp, initial_pressure, initial_state = self.make_initial_conditions(
+            g, x0=0, y0=0, p0=0
+        )
+        parameters_m = {"bc": bound_mech, "biot_alpha": 1, "state": initial_disp}
+
+        parameters_f = {"bc": bound_flow, "biot_alpha": 1, "state": initial_pressure}
+        pp.initialize_default_data(g, d, kw_m, parameters_m)
+        pp.initialize_default_data(g, d, kw_f, parameters_f)
         # Discretize the mechanics related terms using the Biot class
+        d["state"] = initial_state
         biot_discretizer = pp.Biot()
         biot_discretizer._discretize_mech(g, d)
 
@@ -155,6 +171,140 @@ class BiotTest(unittest.TestCase):
         self.assertTrue(np.all(np.isclose(A.A, A_class.A)))
         self.assertTrue(np.all(np.isclose(b, b_class)))
 
+    def test_assemble_biot_rhs_transient(self):
+        """ Test the assembly of a Biot problem with a non-zero rhs using the assembler.
+
+        The test checks whether the discretization matches that of the Biot class and
+        that the solution reaches the expected steady state.
+        """
+        gb = pp.meshing.cart_grid([], [3, 3], physdims=[1, 1])
+        g = gb.grids_of_dimension(2)[0]
+        d = gb.node_props(g)
+
+        # Parameters identified by two keywords
+        kw_m = "mechanics"
+        kw_f = "flow"
+        bound_mech, bound_flow = self.make_boundary_conditions(g)
+        val_mech = np.ones(g.dim * g.num_faces)
+        val_flow = np.ones(g.num_faces)
+        initial_displacement, initial_pressure, initial_state = self.make_initial_conditions(
+            g, x0=1, y0=2, p0=0
+        )
+        dt = 1e0
+        biot_alpha = 0.6
+
+        parameters_m = {
+            "bc": bound_mech,
+            "bc_values": val_mech,
+            "time_step": dt,
+            "biot_alpha": biot_alpha,
+            "state": initial_displacement,
+        }
+        parameters_f = {
+            "bc": bound_flow,
+            "bc_values": val_flow,
+            "time_step": dt,
+            "biot_alpha": biot_alpha,
+            "state": initial_pressure,
+            "mass_weight": 0.1 * np.ones(g.num_cells),
+        }
+        pp.initialize_default_data(g, d, kw_m, parameters_m)
+        pp.initialize_default_data(g, d, kw_f, parameters_f)
+
+        # Initial condition fot the Biot class
+        d["state"] = initial_state
+
+        # Discretize the mechanics related terms using the Biot class
+        biot_discretizer = pp.Biot()
+        biot_discretizer._discretize_mech(g, d)
+
+        # Set up the structure for the assembler. First define variables and equation
+        # term names.
+        v_0 = "displacement"
+        v_1 = "pressure"
+        term_00 = "stress_divergence"
+        term_01 = "pressure_gradient"
+        term_10 = "displacement_divergence"
+        term_11_0 = "fluid_mass"
+        term_11_1 = "fluid_flux"
+        term_11_2 = "stabilization"
+        d[pp.PRIMARY_VARIABLES] = {v_0: {"cells": g.dim}, v_1: {"cells": 1}}
+        d[pp.DISCRETIZATION] = {
+            v_0: {term_00: pp.Mpsa(kw_m)},
+            v_1: {
+                term_11_0: ImplicitMassMatrix(kw_f),
+                term_11_1: ImplicitMpfa(kw_f),
+                term_11_2: pp.BiotStabilization(kw_f),
+            },
+            v_0 + "_" + v_1: {term_01: pp.GradP(kw_m)},
+            v_1 + "_" + v_0: {term_10: pp.DivD(kw_m)},
+        }
+
+        times = np.arange(5)
+        for t in times:
+            # Assemble. Also discretizes the flow terms (fluid_mass and fluid_flux)
+            general_assembler = pp.Assembler()
+            A, b, block_dof, full_dof = general_assembler.assemble_matrix_rhs(gb)
+
+            # Assemble using the Biot class
+            A_class, b_class = biot_discretizer.matrix_rhs(g, d, discretize=False)
+
+            # Make sure the variable ordering of the matrix assembled by the assembler
+            # matches that of the Biot class.
+            grids = [g, g]
+            variables = [v_0, v_1]
+            A, b = permute_matrix_vector(A, b, block_dof, full_dof, grids, variables)
+
+            # Compare the matrices and rhs vectors
+            self.assertTrue(np.all(np.isclose(A.A, A_class.A)))
+            self.assertTrue(np.all(np.isclose(b, b_class)))
+
+            # Store the current solution for the next time step.
+            # First for the assembler version
+            x_i = sps.linalg.spsolve(A_class, b_class)
+            u_i = x_i[: (g.dim * g.num_cells)]
+            p_i = x_i[(g.dim * g.num_cells) :]
+            d[pp.PARAMETERS][kw_m]["state"] = u_i
+            d[pp.PARAMETERS][kw_f]["state"] = p_i
+            # ... and then for the Biot class
+            d["state"] = x_i
+
+        # Check that the solution has converged to the expected, uniform steady state
+        # dictated by the BCs.
+        assert np.all(np.isclose(x_i, np.ones((g.dim + 1) * g.num_cells)))
+
+        return
+
+
+class ImplicitMassMatrix(pp.MassMatrix):
+    def assemble_rhs(self, g, data):
+        """ Overwrite MassMatrix method to
+        1) Be consistent with the Biot dt convention.
+        2) Return the correct rhs for an IE time discretization of the Biot problem.
+
+        TODO: Implement more general solutions.
+        """
+
+        parameter_dictionary = data[pp.PARAMETERS][self.keyword]
+        matrix_dictionary = data[pp.DISCRETIZATION_MATRICES][self.keyword]
+        previous_pressure = parameter_dictionary["state"]
+
+        return matrix_dictionary["mass"] * previous_pressure
+
+
+class ImplicitMpfa(pp.Mpfa):
+    def assemble_matrix_rhs(self, g, data):
+        """ Overwrite MassMatrix method to
+        1) Be consistent with the Biot dt convention.
+        2) Return the correct rhs for an IE time discretization of the Biot problem.
+
+        TODO: Implement more general solutions.
+        """
+        a, b = super().assemble_matrix_rhs(g, data)
+        dt = data[pp.PARAMETERS][self.keyword]["time_step"]
+        return a * dt, b * dt
+
 
 if __name__ == "__main__":
+    #    BiotTest().test_assemble_biot_rhs()
     unittest.main()
