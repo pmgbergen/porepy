bb8d385d2ed263c4a98ea6d75af6e4ad55d73176
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2019-01-14 09:41:04 +0100

    Fix of point sorting method for case with non-circular lines.

--- test/unit/test_sort_points.py
+++ test/unit/test_sort_points.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 
 from porepy.utils import sort_points
+from test import test_utils
 
 
 class SortLinePairTest(unittest.TestCase):
@@ -12,8 +13,32 @@ class SortLinePairTest(unittest.TestCase):
         truth = np.array([[1, 2], [2, 7], [7, 5], [5, 1]]).T
         self.assertTrue(np.allclose(truth, sp))
 
-class SortTriangleEdges(unittest.TestCase):
+    def test_not_circular_1(self):
+        # The points are not circular, but the isolated points are contained in the
+        # first and last column, thus no rearrangement is needed
+        p = np.array([[1, 0], [1, 3], [3, 2]]).T
+        sp = sort_points.sort_point_pairs(p, is_circular=False)
+        truth = np.array([[0, 1], [1, 3], [3, 2]]).T
+        self.assertTrue(test_utils.compare_arrays(sp, truth))
 
+    def test_not_circular_2(self):
+        # The points are not circular, but the isolated points are contained in the
+        # first column, thus re-arrangement should be automatic
+        p = np.array([[1, 0], [3, 2], [1, 3]]).T
+        sp = sort_points.sort_point_pairs(p, is_circular=False)
+        truth = np.array([[0, 1], [1, 3], [3, 2]]).T
+        self.assertTrue(test_utils.compare_arrays(sp, truth))
+
+    def test_not_circular_3(self):
+        # The points are not circular, and the isolated points are not contained in the
+        # first column, thus re-arrangement is needed
+        p = np.array([[1, 0], [3, 2], [1, 3]]).T
+        sp = sort_points.sort_point_pairs(p, is_circular=False)
+        truth = np.array([[1, 3], [1, 0], [3, 2]]).T
+        self.assertTrue(test_utils.compare_arrays(sp, truth))
+
+
+class SortTriangleEdges(unittest.TestCase):
     def test_no_sorting(self):
 
         t = np.array([[0, 1, 2], [2, 1, 3]]).T
@@ -65,4 +90,5 @@ class SortTriangleEdges(unittest.TestCase):
 
 
 if __name__ == "__main__":
+    SortLinePairTest().test_not_circular_1()
     unittest.main()
