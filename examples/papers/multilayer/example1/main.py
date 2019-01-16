4e5ea16dca237782ef1144117c8251dcea8eef2a
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2019-01-16 11:35:37 +0100

    multilayer, add the multi layer Robin interface law; update the bc; add the possibility to have different values for the fault and the damaged layer

--- examples/papers/multilayer/example1/main.py
+++ examples/papers/multilayer/example1/main.py
@@ -12,11 +12,19 @@ def bc_flag(g, domain, tol):
     b_faces = g.tags["domain_boundary_faces"].nonzero()[0]
     b_face_centers = g.face_centers[:, b_faces]
 
+    # define outflow type boundary conditions
+    out_flow_y = b_face_centers[1] > 1 - tol
+    #out_flow_x = b_face_centers[0] < tol
+
+    #out_flow = np.logical_and(out_flow_x, out_flow_z)
+    out_flow = out_flow_y
+
     # define inflow type boundary conditions
-    out_flow = g.tags["domain_boundary_faces"]
-    in_flow = out_flow
+    in_flow_y = b_face_centers[1] < 0 + tol
+    in_flow = in_flow_y
 
     return in_flow, out_flow
+
 
 def unite_grids(gs):
     # it's simple index transformation
@@ -52,6 +60,12 @@ def multilayer_grid_bucket(gb):
 
         # we construct the grid for the new layer
         g_new = unite_grids([g for g in mg.side_grids.values()])
+
+        # update the names
+        gs.name += ["fault"]
+        g_new.name += ["layer"]
+
+        # add the new grid to the grid bucket
         gb_new.add_nodes(g_new)
 
         # we store the master-grid with the new layer grid with the
@@ -80,7 +94,7 @@ def multilayer_grid_bucket(gb):
 
 def main():
 
-    h = 6
+    h = 0.01
     tol = 1e-6
     mesh_args = {'mesh_size_frac': h}
     domain = {"xmin": 0, "xmax": 1, "ymin": 0, "ymax": 1}
@@ -89,23 +103,9 @@ def main():
 
     gb = pp.meshing.simplex_grid([f], domain, **mesh_args)
 
-    # construct the multi-layer grid bucket
+    # construct the multi-layer grid bucket, we give also a name to the fault and layer grids
     gb_ml = multilayer_grid_bucket(gb)
-    pp.plot_grid(gb_ml, alpha=0, info="cf")
-
-    save = pp.Exporter(gb_ml, "sol")
-    save.write_vtk()
-    print(gb_ml)
-
-    for g, d in gb_ml:
-        print(g)
-        print(d)
-        print("==============")
-
-    for e, d in gb_ml.edges():
-        print(e)
-        print(d)
-        print("+++++++++++++++")
+    #pp.plot_grid(gb_ml, alpha=0, info="cf")
 
     # nella discretizzazione tra master e nuovo strato devo mettere una legge di tipo
     # robin come nel caso usuale
@@ -117,7 +117,9 @@ def main():
 
     # the flow problem
     param = {"domain": gb_ml.bounding_box(as_dict=True), "tol": tol,
-             "k": 1, "bc": 0, "aperture": 1e-3, "kf": 1e3,
+             "k": 1, "bc_inflow": 0, "bc_outflow": 1,
+             "fault": {"aperture": 1e-3, "kf_t": 1, "kf_n": 1e-2},
+             "layer": {"aperture": 1e-3, "kf_t": 1, "kf_n": 1e2},
              "folder": "solution"}
     compute.flow(gb_ml, param, bc_flag)
 
