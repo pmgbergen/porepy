64d95a60aa45ae178bdbf9ce316dd2a10e978066
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2019-01-24 16:30:56 +0100

    update the pressure trace, by introducing new functions (abstract) and remove an inheritance from the assembler

--- src/porepy/numerics/mixed_dim/pressure_trace.py
+++ src/porepy/numerics/mixed_dim/pressure_trace.py
@@ -16,6 +16,26 @@ class PressureTrace(pp.Assembler):
 
     """
 
+    def __init__(self, keyword):
+        """ Set the discretization, with the keyword used for storing various
+        information associated with the discretization.
+
+        Paramemeters:
+            keyword (str): Identifier of all information used for this
+                discretization.
+        """
+        self.keyword = keyword
+
+    def _key(self):
+        """ Get the keyword of this object, on a format friendly to access relevant
+        fields in the data dictionary
+
+        Returns:
+            String, on the form self.keyword + '_'.
+
+        """
+        return self.keyword + "_"
+
     def ndof(self, g):
         """ Return the number of degrees of freedom, in this case the number of cells
 
@@ -59,7 +79,7 @@ class PressureTrace(pp.Assembler):
         """
         return np.empty(0)
 
-    def assemble_matrix_rhs(self, g, data):
+    def assemble_matrix_rhs(self, g, data=None):
         """ Return the matrix and right-hand side, no PDE are associate with
         this discretization so empty matrix and zero rhs is returned.
 
@@ -100,16 +120,16 @@ class PressureTrace(pp.Assembler):
         """
         return np.zeros(self.ndof(g))
 
-    def assemble_int_bound_pressure_cell(
+    def assemble_int_bound_flux(
         self, g, data, data_edge, grid_swap, cc, matrix, self_ind
     ):
-        """ Assemble the contribution from an internal
-        boundary, manifested as a condition on the cell pressure.
+        """ Abstract method. Assemble the contribution from an internal
+        boundary, manifested as a flux boundary condition.
 
         The intended use is when the internal boundary is coupled to another
         node in a mixed-dimensional method. Specific usage depends on the
         interface condition between the nodes; this method will typically be
-        used to impose flux continuity on a lower-dimensional domain.
+        used to impose flux continuity on a higher-dimensional domain.
 
         Implementations of this method will use an interplay between the grid on
         the node and the mortar grid on the relevant edge.
@@ -129,17 +149,10 @@ class PressureTrace(pp.Assembler):
             matrix (block matrix 3x3): Discretization matrix for the edge and
                 the two adjacent nodes.
             self_ind (int): Index in cc and matrix associated with this node.
-                Should be either 0 or 1.
+                Should be either 1 or 2.
 
         """
-        mg = data_edge["mortar_grid"]
-
-        if grid_swap:
-            proj = mg.master_to_mortar_avg()
-        else:
-            proj = mg.slave_to_mortar_avg()
-
-        cc[2, self_ind] -= proj
+        raise NotImplementedError("Method not implemented")
 
     def assemble_int_bound_source(
         self, g, data, data_edge, grid_swap, cc, matrix, self_ind
@@ -182,5 +195,97 @@ class PressureTrace(pp.Assembler):
 
         cc[self_ind, 2] -= proj.T
 
+    def assemble_int_bound_pressure_trace(
+        self, g, data, data_edge, grid_swap, cc, matrix, self_ind
+    ):
+        """ Abstract method. Assemble the contribution from an internal
+        boundary, manifested as a condition on the boundary pressure.
+
+        The intended use is when the internal boundary is coupled to another
+        node in a mixed-dimensional method. Specific usage depends on the
+        interface condition between the nodes; this method will typically be
+        used to impose flux continuity on a higher-dimensional domain.
+
+        Implementations of this method will use an interplay between the grid on
+        the node and the mortar grid on the relevant edge.
+
+        Parameters:
+            g (Grid): Grid which the condition should be imposed on.
+            data (dictionary): Data dictionary for the node in the
+                mixed-dimensional grid.
+            data_edge (dictionary): Data dictionary for the edge in the
+                mixed-dimensional grid.
+            grid_swap (boolean): If True, the grid g is identified with the @
+                slave side of the mortar grid in data_adge.
+            cc (block matrix, 3x3): Block matrix for the coupling condition.
+                The first and second rows and columns are identified with the
+                master and slave side; the third belongs to the edge variable.
+                The discretization of the relevant term is done in-place in cc.
+            matrix (block matrix 3x3): Discretization matrix for the edge and
+                the two adjacent nodes.
+            self_ind (int): Index in cc and matrix associated with this node.
+                Should be either 1 or 2.
+
+        """
+        raise NotImplementedError("Method not implemented")
+
+    def assemble_int_bound_pressure_cell(
+        self, g, data, data_edge, grid_swap, cc, matrix, self_ind
+    ):
+        """ Assemble the contribution from an internal
+        boundary, manifested as a condition on the cell pressure.
+
+        The intended use is when the internal boundary is coupled to another
+        node in a mixed-dimensional method. Specific usage depends on the
+        interface condition between the nodes; this method will typically be
+        used to impose flux continuity on a lower-dimensional domain.
+
+        Implementations of this method will use an interplay between the grid on
+        the node and the mortar grid on the relevant edge.
+
+        Parameters:
+            g (Grid): Grid which the condition should be imposed on.
+            data (dictionary): Data dictionary for the node in the
+                mixed-dimensional grid.
+            data_edge (dictionary): Data dictionary for the edge in the
+                mixed-dimensional grid.
+            grid_swap (boolean): If True, the grid g is identified with the @
+                slave side of the mortar grid in data_adge.
+            cc (block matrix, 3x3): Block matrix for the coupling condition.
+                The first and second rows and columns are identified with the
+                master and slave side; the third belongs to the edge variable.
+                The discretization of the relevant term is done in-place in cc.
+            matrix (block matrix 3x3): Discretization matrix for the edge and
+                the two adjacent nodes.
+            self_ind (int): Index in cc and matrix associated with this node.
+                Should be either 0 or 1.
+
+        """
+        mg = data_edge["mortar_grid"]
+
+        if grid_swap:
+            proj = mg.master_to_mortar_avg()
+        else:
+            proj = mg.slave_to_mortar_avg()
+
+        cc[2, self_ind] -= proj
+
+    def enforce_neumann_int_bound(
+        self, g_master, data_edge, matrix, swap_grid, self_ind
+    ):
+        """ Enforce Neumann boundary conditions on a given system matrix.
+
+        Methods based on a mixed variational form will need this function to
+        implement essential boundary conditions.
+
+        The discretization matrix should be modified in place.
+
+        Parameters:
+            g (Grid): On which the equation is discretized
+            data (dictionary): Of data related to the discretization.
+            matrix (scipy.sparse.matrix): Discretization matrix to be modified.
+
+        """
+        raise NotImplementedError("Method not implemented")
 
 # -----------------------------------------------------------------------------#
