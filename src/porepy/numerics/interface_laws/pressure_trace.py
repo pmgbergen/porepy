7ba02173bb00a2c199cf481d219c5cc52cdcb54a
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2019-01-24 16:31:58 +0100

    move pressure trace to interface law

--- src/porepy/numerics/interface_laws/pressure_trace.py
+++ src/porepy/numerics/interface_laws/pressure_trace.py
@@ -9,7 +9,7 @@ import scipy.sparse as sps
 import porepy as pp
 
 
-class PressureTrace(pp.Assembler):
+class PressureTrace(object):
     """ Implement a pressure trace discretization, no PDE are associated with this discretization.
     Only the interface operator are given. A possible usage is for the co-dimensional objects in a
     DFN discretization.
