2296fe1a691117206ce01be78647c6523a9b79a9
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2019-01-30 16:20:58 +0100

    after the merging of develop use the new interface to generate 2d grids

--- examples/papers/multilayer/example2/import_grid.py
+++ examples/papers/multilayer/example2/import_grid.py
@@ -4,7 +4,7 @@ import porepy as pp
 def import_grid(file_geo, tol):
 
     frac = pp.Fracture(np.array([[0, 10, 10, 0], [0, 0, 10, 10], [8, 2, 2, 8]]) * 10)
-    network = pp.FractureNetwork([frac], tol=tol)
+    network = pp.FractureNetwork3d([frac], tol=tol)
 
     domain = {"xmin": 0, "xmax": 100, "ymin": 0, "ymax": 100, "zmin": 0, "zmax": 100}
     network.impose_external_boundary(domain)
@@ -12,7 +12,7 @@ def import_grid(file_geo, tol):
     network.split_intersections()
     network.to_gmsh("dummy.geo")
 
-    gb = pp.importer.dfm_from_gmsh(file_geo, 3, network)
+    gb = pp.fracture_importer.dfm_from_gmsh(file_geo, 3, network)
     gb.compute_geometry()
 
     return gb, domain
