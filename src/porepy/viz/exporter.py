ed95051c23935b2252e42e0fa202ba6c411b9b65
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2019-01-31 09:53:17 +0100

    Handle empty data for vtk export of single grid (#206)
    * Handle empty data for vtk export of single grid
    
    * Remove superfluous assertion in vtk Exporter

--- src/porepy/viz/exporter.py
+++ src/porepy/viz/exporter.py
@@ -337,6 +337,12 @@ class Exporter:
         name = self._make_folder(self.folder, self.name)
         name = self._make_file_name(name, time_step)
 
+        # Convert data to list, or provide an empty list
+        if data is not None:
+            data = np.atleast_1d(data).tolist()
+        else:
+            data = list()
+
         fields = Fields()
         if len(data) > 0:
             if point_data:
@@ -364,11 +370,11 @@ class Exporter:
     # ------------------------------------------------------------------------------#
 
     def _export_vtk_gb(self, data, time_step, point_data):
+        # Convert data to list, or provide an empty list
         if data is not None:
             data = np.atleast_1d(data).tolist()
         else:
             data = list()
-        assert isinstance(data, list) or data is None
 
         fields = Fields()
         if len(data) > 0:
