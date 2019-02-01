2d326c8578e29bff172e2faf74f968ba91f7a72a
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2019-02-01 15:58:36 +0100

    benchmark 3d, update case3

--- examples/papers/arXiv_1809_06926/case3/main.py
+++ examples/papers/arXiv_1809_06926/case3/main.py
@@ -110,7 +110,7 @@ def main(grid_file, folder, solver, solver_name, dt):
         f.write(", ".join(map(str, results)))
 
     T, outflow, A, b, block_dof, full_dof = solvers.transport(
-        gb, data, solver_name, folder, save_every=20
+        gb, data, solver_name, folder, save_every=1
     )
 
 
