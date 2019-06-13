52b83d15501f34bc512ad09d15a1130a7c9c1402
Runar <runar.berge@uib.no>
2019-06-13 15:10:16 +0200

    Upgraded mortars.match_grids_1d to also work for 1d grids that are split in two. This typically happens when two 1d grids intersect.

--- src/porepy/fracs/mortars.py
+++ src/porepy/fracs/mortars.py
@@ -135,6 +135,11 @@ def update_physical_high_grid(mg, g_new, g_old, tol):
         new_nodes = g_new.face_centers[:, new_faces]
 
         # we assume only one old node
+        for i in range(1, old_nodes.shape[1]):
+            is_same = pp.distances.point_pointset(old_nodes[:, 0], old_nodes[:, i]) < tol
+            if not is_same:
+                raise ValueError('0d->1d mappings must map to the same physical point')
+        old_nodes = old_nodes[:, 0]
         mask = pp.distances.point_pointset(old_nodes, new_nodes) < tol
         new_faces = new_faces[mask]
 
@@ -233,35 +238,36 @@ def match_grids_1d(new_1d, old_1d, tol):
               grid.
 
     """
+    cell_nodes1 = new_1d.cell_nodes()
+    cell_nodes2 = old_1d.cell_nodes()
+    nodes1 = pp.utils.mcolon.mcolon(cell_nodes1.indptr[0:-1], cell_nodes1.indptr[1:])
+    nodes2 = pp.utils.mcolon.mcolon(cell_nodes2.indptr[0:-1], cell_nodes2.indptr[1:])
 
-    # Create a grid that contains all nodes of both the old and new grids.
-    combined, _, new_ind, old_ind, _, _ = non_conforming.merge_1d_grids(
-        new_1d, old_1d, tol=tol
-    )
-    combined.compute_geometry()
-    weights = combined.cell_volumes
+    p1 = new_1d.nodes
+    p2 = old_1d.nodes
+    lines1 = cell_nodes1.indices[nodes1].reshape((2, -1), order='F')
+    lines2 = cell_nodes2.indices[nodes2].reshape((2, -1), order='F')
 
-    switch_new = new_ind[0] > new_ind[-1]
-    if switch_new:
-        new_ind = new_ind[::-1]
-    switch_old = old_ind[0] > old_ind[-1]
-    if switch_old:
-        old_ind = old_ind[::-1]
+    intersect = pp.intersections.line_tesselation(p1, p2, lines1, lines2)
 
-    diff_new = np.diff(new_ind)
-    diff_old = np.diff(old_ind)
-    new_in_full = rldecode(np.arange(diff_new.size), diff_new)
-    old_in_full = rldecode(np.arange(diff_old.size), diff_old)
+    num = len(intersect)
+    new_g_ind = np.zeros(num, dtype=np.int)
+    old_g_ind = np.zeros(num, dtype=np.int)
+    weights = np.zeros(num)
 
-    if switch_new:
-        new_in_full = new_in_full.max() - new_in_full
-    if switch_old:
-        old_in_full = old_in_full.max() - old_in_full
+    for ind, i in enumerate(intersect):
+        new_g_ind[ind] = i[0]
+        old_g_ind[ind] = i[1]
+        weights[ind] = i[2]
+    weights /= old_1d.cell_volumes[old_g_ind]
 
-    old_1d.compute_geometry()
+    # Remove zero weight intersections
+    mask = weights > tol
+    new_g_ind = new_g_ind[mask]
+    old_g_ind = old_g_ind[mask]
+    weights = weights[mask]
 
-    weights /= old_1d.cell_volumes[old_in_full]
-    return weights, new_in_full, old_in_full
+    return weights, new_g_ind, old_g_ind
 
 
 # ------------------------------------------------------------------------------#
