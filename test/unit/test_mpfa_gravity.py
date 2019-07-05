6ee71f4abe21e8f228d01729304bb7db0c030a2e
mstarnoni <michele.starnoni@gmail.com>
2019-07-05 10:28:36 +0200

    Added standard gravity method to TPFA for 1D problems, with its unit test, and ran black

--- test/unit/test_mpfa_gravity.py
+++ test/unit/test_mpfa_gravity.py
@@ -20,6 +20,18 @@ class _SolutionHomogeneousDomainFlowWithGravity(object):
         self.gy_f = gy_f
 
 
+class _Solution1DFlowWithGravity(object):
+    """Convenience class for representing an analytical solution, and its
+    derivatives"""
+
+    def __init__(self, p, y):
+        p_f = sympy.lambdify(y, p, "numpy")
+        g = sympy.diff(p, y)
+        g_f = sympy.lambdify(y, g, "numpy")
+        self.p_f = p_f
+        self.g_f = g_f
+
+
 def perturb(g, rate, dx):
     rand = np.vstack((np.random.rand(g.dim, g.num_nodes), np.repeat(0.0, g.num_nodes)))
     r1 = np.ravel(
@@ -58,6 +70,66 @@ def make_grid(grid, grid_dims, domain):
 
 
 class TestMPFAgravity(unittest.TestCase):
+    def test_hydrostatic_pressure_1D(self):
+
+        # Test mpfa_gravity in 1D grid
+        # Solver uses TPFA + standard method
+        # Should be exact for hydrostatic pressure
+        # with stepwise gravity variation
+
+        x = sympy.symbols("x")
+        g1 = 10
+        g2 = 1
+        p0 = 1  # reference pressure
+        p = p0 + sympy.Piecewise(
+            ((1 - x) * g1, x >= 0.5), (0.5 * g1 + (0.5 - x) * g2, x < 0.5)
+        )
+        an_sol = _Solution1DFlowWithGravity(p, x)
+
+        g = pp.CartGrid(8, 1)
+        g.compute_geometry()
+        xc = g.cell_centers
+        xf = g.face_centers
+
+        k = pp.SecondOrderTensor(1, np.ones(g.num_cells))
+
+        # Gravity
+        gforce = an_sol.g_f(xc[0])
+
+        # Set type of boundary conditions
+        # 'dir' left, 'neu' right
+        p_bound = np.zeros(g.num_faces)
+        dir_faces = np.array([0])
+
+        bound_cond = pp.BoundaryCondition(g, dir_faces, ["dir"] * dir_faces.size)
+
+        # set value of boundary condition
+        p_bound[dir_faces] = an_sol.p_f(xf[0, dir_faces])
+
+        # GCMPFA discretization, and system matrix
+        flux, bound_flux, _, _, div_g = pp.Mpfa("flow").mpfa(
+            g, k, bound_cond, vector_source=True, inverter="python"
+        )
+        div = pp.fvutils.scalar_divergence(g)
+        a = div * flux
+
+        # take arithmetic mean of cell center gravities
+        fi, ci, sgn = scipy.sparse.find(g.cell_faces)
+        fc_cc = g.face_centers[::, fi] - g.cell_centers[::, ci]
+        dist_face_cell = np.linalg.norm(fc_cc, 2, axis=0)
+        gforce = gforce[ci]
+        fg = np.bincount(fi, weights=dist_face_cell * gforce)
+        flux_g = fg * div_g
+        # assemble rhs
+        b = -div * bound_flux * p_bound - div * flux_g
+        # solve system
+        p = scipy.sparse.linalg.spsolve(a, b)
+        q = flux * p + bound_flux * p_bound + flux_g
+        p_ex = an_sol.p_f(xc[0])
+        q_ex = np.zeros(g.num_faces)
+        self.assertTrue(np.allclose(p, p_ex))
+        self.assertTrue(np.allclose(q, q_ex))
+
     def test_hydrostatic_pressure(self):
 
         # Test mpfa_gravity in 2D Cartesian
@@ -109,7 +181,7 @@ class TestMPFAgravity(unittest.TestCase):
             p_bound[dir_faces] = an_sol.p_f(xf[0, dir_faces], xf[1, dir_faces])
 
             # GCMPFA discretization, and system matrix
-            flux, bound_flux, _, _, div_g = pp.Mpfa("flow")._local_discr(
+            flux, bound_flux, _, _, div_g = pp.Mpfa("flow").mpfa(
                 g, k, bound_cond, vector_source=True, inverter="python"
             )
             div = pp.fvutils.scalar_divergence(g)
