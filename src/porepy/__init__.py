efa37f344a38d08f450982aeab8aeee4e51fb1ac
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2019-08-20 11:57:02 +0200

    Version 1.0.0

--- src/porepy/__init__.py
+++ src/porepy/__init__.py
@@ -16,7 +16,7 @@ viz: Visualization; paraview, matplotlib.
 
 """
 
-__version__ = "0.5.0"
+__version__ = "1.0.0"
 
 # ------------------------------------
 # Simplified namespaces. The rue of thumb is that classes and modules that a
