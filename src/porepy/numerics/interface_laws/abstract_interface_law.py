a4e51f782216784224e8b002b789a1321b93c4f3
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2019-10-04 15:36:06 +0200

    Minor refactoring of interface laws

--- src/porepy/numerics/interface_laws/abstract_interface_law.py
+++ src/porepy/numerics/interface_laws/abstract_interface_law.py
@@ -3,10 +3,53 @@ Mother class for all interface laws.
 """
 import numpy as np
 import scipy.sparse as sps
-
+import porepy as pp
 
 class AbstractInterfaceLaw:
+    
+    def _key(self):
+        return self.keyword + "_"
+
+    def _discretization_key(self):
+        return self._key() + pp.DISCRETIZATION    
+    
     def ndof(self, mg):
+        raise NotImplementedError("Must be implemented by any real interface law")
+        
+    def discretize(self, g_h, g_l, data_h, data_l, data_edge):
+        """ Discretize the interface law and store the discretization in the
+        edge data.
+
+        Parameters:
+            g_h: Grid of the master domanin.
+            g_l: Grid of the slave domain.
+            data_h: Data dictionary for the master domain.
+            data_l: Data dictionary for the slave domain.
+            data_edge: Data dictionary for the edge between the domains.
+
+        """
+        raise NotImplementedError("Must be implemented by any real interface law")
+
+    def assemble_matrix_rhs(
+        self, g_master, g_slave, data_master, data_slave, data_edge, matrix
+    ):
+        """ Assemble the dicretization of the interface law, and its impact on
+        the neighboring domains.
+
+        Parameters:
+            g_master: Grid on one neighboring subdomain.
+            g_slave: Grid on the other neighboring subdomain.
+            data_master: Data dictionary for the master suddomain
+            data_slave: Data dictionary for the slave subdomain.
+            data_edge: Data dictionary for the edge between the subdomains
+            matrix_master: original discretization for the master subdomain
+            matrix_slave: original discretization for the slave subdomain
+
+            The discretization matrices must be included since they will be
+            changed by the imposition of Neumann boundary conditions on the
+            internal boundary in some numerical methods (Read: VEM, RT0)
+
+        """        
         raise NotImplementedError("Must be implemented by any real interface law")
 
     def _define_local_block_matrix(
@@ -40,13 +83,11 @@ class AbstractInterfaceLaw:
             )
         # We know the number of dofs from the master and slave side from their
         # discretizations
-        #        dof = np.array([dof_master, dof_slave, mg.num_cells])
         dof = np.array([dof_master, dof_slave, dof_mortar])
         cc = np.array([sps.coo_matrix((i, j)) for i in dof for j in dof])
         cc = cc.reshape((3, 3))
 
         # The rhs is just zeros
-        # EK: For some reason, the following lines were necessary to apease python
         rhs = np.empty(3, dtype=np.object)
         rhs[master_ind] = np.zeros(dof_master)
         rhs[slave_ind] = np.zeros(dof_slave)
