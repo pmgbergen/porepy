de1008d11f7fcd1e088ac6810f320c47099b9687
Ivar <ivar.stefansson@uib.no>
2019-11-26 14:30:42 +0100

    Updates to existing models
    Preparation for adding thm model

--- src/porepy/models/run_models.py
+++ src/porepy/models/run_models.py
@@ -18,6 +18,14 @@ def run_stationary_model(setup, params):
 
 def run_time_dependent_model(setup, params):
     """
+    Time loop for the model classes.
+    
+    Args:
+        setup: Model class containing all information on parameters, variables, discretization,
+           geometry. Various methods such as those relating to solving the system, see the appropriate
+           solver for documentation.
+        params: Parameters related to the solution proceedure. # Why not just set these as e.g. setup.solution_parameters, EK? And then specify (here, or at top level of the respective solvers) which parameters are required.
+        non_line
     """
     # Assign parameters, variables and discretizations. Discretize time-indepedent terms
     setup.prepare_simulation()
@@ -25,9 +33,10 @@ def run_time_dependent_model(setup, params):
     # Prepare for the time loop
     t_end = setup.end_time
     k = 0
-
-    nl_solver = pp.NewtonSolver(params)
-
+    if setup._is_nonlinear_problem():
+        solver = pp.NewtonSolver(params)
+    else:
+        solver = pp.LinearSolver(params)
     while setup.time < t_end:
         setup.time += setup.time_step
         k += 1
@@ -36,4 +45,4 @@ def run_time_dependent_model(setup, params):
                 k, setup.time, t_end, setup.time_step
             )
         )
-        nl_solver.solve(setup)
+        solver.solve(setup)
