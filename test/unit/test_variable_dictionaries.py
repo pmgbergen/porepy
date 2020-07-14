6ca5f23b34116840700008a132d8200b37d4de2b
Haakon Ludvig Langeland Ervik <haakon@caltech.edu>
2020-07-14 20:19:00 +0200

    test: Use pytest instead of unittest for testing

--- test/unit/test_variable_dictionaries.py
+++ test/unit/test_variable_dictionaries.py
@@ -1,58 +1,63 @@
 """ Tests for variable storage in the data fields "state" and "iterate".
 """
-import unittest
+from typing import Dict
+
+import pytest
 
 import porepy as pp
 
 
-class TestState(unittest.TestCase):
-    def setUp(self):
-        self.d = {}
+@pytest.fixture
+def empty_dict() -> Dict:
+    return {}
 
-    def test_add_empty_state(self):
+
+class TestState:
+
+    def test_add_empty_state(self, empty_dict):
         """ Add an empty state dictionary
         """
-        pp.set_state(self.d)
-        self.assertIn(pp.STATE, self.d)
+        d = empty_dict
+        pp.set_state(d)
+        assert pp.STATE in d
 
-    def test_add_empty_iterate(self):
+    def test_add_empty_iterate(self, empty_dict):
         """ Add an empty iterate dictionary
         """
-        pp.set_iterate(self.d)
-        self.assertIn(pp.STATE, self.d)
-        self.assertIn(pp.ITERATE, self.d[pp.STATE])
+        d = empty_dict
+        pp.set_iterate(d)
+        assert pp.STATE in d
+        assert pp.ITERATE in d[pp.STATE]
 
-    def test_add_state_twice(self):
+    def test_add_state_twice(self, empty_dict):
         """ Add two state dictionaries.
         
         The existing foo value should be overwritten, while bar should be kept.
         """
+        d = empty_dict
         d1 = {"foo": 1, "bar": 2}
         d2 = {"foo": 3, "spam": 4}
 
-        pp.set_state(self.d, d1)
-        pp.set_state(self.d, d2)
+        pp.set_state(d, d1)
+        pp.set_state(d, d2)
         for key, val in zip(["foo", "bar", "spam"], [3, 2, 4]):
-            self.assertIn(key, self.d[pp.STATE])
-            self.assertEqual(self.d[pp.STATE][key], val)
+            assert key in d[pp.STATE]
+            assert d[pp.STATE][key] == val
 
-    def test_add_iterate_twice_and_state(self):
+    def test_add_iterate_twice_and_state(self, empty_dict):
         """ Add two state dictionaries.
         
         The existing foo value should be overwritten, while bar should be kept.
         Setting values in pp.STATE should not affect the iterate values.
         """
+        d = empty_dict
         d1 = {"foo": 1, "bar": 2}
         d2 = {"foo": 3, "spam": 4}
 
-        pp.set_iterate(self.d, d1)
-        pp.set_iterate(self.d, d2)
-        pp.set_state(self.d, {"foo": 5})
+        pp.set_iterate(d, d1)
+        pp.set_iterate(d, d2)
+        pp.set_state(d, {"foo": 5})
         for key, val in zip(["foo", "bar", "spam"], [3, 2, 4]):
-            self.assertIn(key, self.d[pp.STATE][pp.ITERATE])
-            self.assertEqual(self.d[pp.STATE][pp.ITERATE][key], val)
-        self.assertEqual(self.d[pp.STATE]["foo"], 5)
-
-
-if __name__ == "__main__":
-    unittest.main()
+            assert key in d[pp.STATE][pp.ITERATE]
+            assert d[pp.STATE][pp.ITERATE][key] == val
+        assert d[pp.STATE]["foo"] == 5
