05f9972a72c186dfb418f9999d579742c9d1df6e
Ivar Stefansson <ist050@mi-loke.klientdrift.uib.no>
2020-07-14 13:52:24 +0200

    cellwise advection weight in ImplicitUpwind

--- src/porepy/utils/derived_discretizations/implicit_euler.py
+++ src/porepy/utils/derived_discretizations/implicit_euler.py
@@ -133,6 +133,7 @@ class ImplicitTpfa(pp.Tpfa):
         """ Overwrite MPFA method to be consistent with the Biot dt convention.
         """
         a, b = super().assemble_matrix_rhs(g, data)
+
         dt = data[pp.PARAMETERS][self.keyword]["time_step"]
         a = a * dt
         b = b * dt
@@ -209,7 +210,10 @@ class ImplicitTpfa(pp.Tpfa):
 
 class ImplicitUpwind(pp.Upwind):
     """
-    Multiply all contributions by the time step and advection weight.
+    Multiply all contributions by the time step and advection weight. 
+    The latter may be a scalar or cellwise values, in which case the upwind
+    value is used. Note that the interior cell value is taken for BCs, 
+    regardless of the direction of the flux on the boundary.
     """
 
     def assemble_matrix_rhs(self, g, data):
@@ -217,12 +221,16 @@ class ImplicitUpwind(pp.Upwind):
             data["flow_faces"] = sps.csr_matrix([0.0])
             return sps.csr_matrix([0.0]), np.array([0.0])
 
-        parameter_dictionary = data[pp.PARAMETERS][self.keyword]
-        dt = parameter_dictionary["time_step"]
-        w = parameter_dictionary["advection_weight"] * dt
+        parameter_dictionary = data[pp.PARAMETERS]
+        dt = parameter_dictionary[self.keyword]["time_step"]
+        # Obtain the cellwise advection weights
+        w = parameter_dictionary.expand_scalars(
+            g.num_cells, self.keyword, "advection_weight"
+        )[0]
+        *dt
         a, b = super().assemble_matrix_rhs(g, data)
-        a = a * w
-        b = b * w
+        a = a * sps.diags(w)
+        b = b * sps.diags(w)
         return a, b
 
 
@@ -258,10 +266,18 @@ class ImplicitUpwindCoupling(pp.UpwindCoupling):
         # Normal component of the velocity from the higher dimensional grid
 
         # @ALL: This should perhaps be defined by a globalized keyword
-        parameter_dictionary_master = data_master[pp.PARAMETERS][self.keyword]
+        parameter_dictionary_master = data_master[pp.PARAMETERS]
+        parameter_dictionary_slave = data_slave[pp.PARAMETERS]
         lam_flux = data_edge[pp.PARAMETERS][self.keyword]["darcy_flux"]
-        dt = parameter_dictionary_master["time_step"]
-        w = parameter_dictionary_master["advection_weight"] * dt
+        dt = parameter_dictionary_master[self.keyword]["time_step"]
+        w_master = parameter_dictionary_master.expand_scalars(
+            g_master.num_cells, self.keyword, "advection_weight"
+        )[0]
+        *dt
+        w_slave = parameter_dictionary_slave.expand_scalars(
+            g_slave.num_cells, self.keyword, "advection_weight"
+        )[0]
+        *dt
         # Retrieve the number of degrees of both grids
         # Create the block matrix for the contributions
         g_m = data_edge["mortar_grid"]
@@ -301,12 +317,14 @@ class ImplicitUpwindCoupling(pp.UpwindCoupling):
         # If fluid flux(lam_flux) is positive we use the upper value as weight,
         # i.e., T_masterat * fluid_flux = lambda.
         # We set cc[2, 0] = T_masterat * fluid_flux
-        cc[2, 0] = sps.diags(w * lam_flux * flag) * hat_P_avg * div.T
+        # import pdb
+        # pdb.set_trace()
+        cc[2, 0] = sps.diags(lam_flux * flag) * hat_P_avg * div.T * sps.diags(w_master)
 
         # If fluid flux is negative we use the lower value as weight,
         # i.e., T_check * fluid_flux = lambda.
         # we set cc[2, 1] = T_check * fluid_flux
-        cc[2, 1] = sps.diags(w * lam_flux * not_flag) * check_P_avg
+        cc[2, 1] = sps.diags(lam_flux * not_flag) * check_P_avg * sps.diags(w_slave)
 
         # The rhs of T * fluid_flux = lambda
         # Recover the information for the grid-grid mapping
