b0f20cf8b0817442d274ad08f44391c0b1a9e21c
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2020-08-26 20:50:53 +0200

    build: Version bump.

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ long_description = read("Readme.rst")
 
 setup(
     name="porepy",
-    version="1.2.1",
+    version="1.2.2",
     license="GPL",
     keywords=["porous media simulation fractures deformable"],
     author="Eirik Keilegavlen, Runar Berge, Alessio Fumagalli, Michele Starnoni, Ivar Stefansson and Jhabriel Varela",
