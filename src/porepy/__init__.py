8f6510573e05bb458a443cbc9f5a9a2356fde588
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2020-09-07 20:34:17 +0200

    build: Version bump.

--- src/porepy/__init__.py
+++ src/porepy/__init__.py
@@ -16,7 +16,7 @@ viz: Visualization; paraview, matplotlib.
 
 """
 
-__version__ = "1.2.4"
+__version__ = "1.2.5"
 
 # ------------------------------------
 # Simplified namespaces. The rue of thumb is that classes and modules that a
