2b7525eda0343ed0c43cb5391b468a512ffe3b09
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2020-11-09 17:26:58 +0100

    style: fix __init__ in ad submodule.

--- src/porepy/numerics/ad/__init__.py
+++ src/porepy/numerics/ad/__init__.py
@@ -1,3 +1,22 @@
-from porepy.numerics.ad.forward_mode import Ad_array, initAdArrays
-from porepy.numerics.ad.functions import abs, exp, log, sign
+""" Init file for all AD functionality.
+
+They should all be accessible through a calling
+   >>> import porepy as pp
+   >>> pp.ad.Matrix???
+etc.
+
+"""
+__all__ = []
+
+from . import operators
+from .operators import *
+from . import functions
+from .functions import *
+from . import forward_mode
+from .forward_mode import *
+
+__all__.extend(operators.__all__)
+__all__.extend(functions.__all__)
+__all__.extend(forward_mode.__all__)
+
 from porepy.numerics.ad.utils import concatenate
