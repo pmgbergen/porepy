39973fe162a0de9270695c350459979154748180
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2021-03-02 10:21:55 +0100

    BUG: Normal vector computation in polygon intersection.
    The check parameter is needed if the polygon has hanging nodes, thus
    colinear lines are drawn.

--- src/porepy/geometry/intersections.py
+++ src/porepy/geometry/intersections.py
@@ -508,7 +508,7 @@ def polygons_3d(polys, target_poly=None, tol=1e-8):
 
     # Pre-compute polygon normals to save computational time
     polygon_normals = [
-        pp.map_geometry.compute_normal(poly, check=False).reshape((-1, 1))
+        pp.map_geometry.compute_normal(poly).reshape((-1, 1))
         for poly in polys
     ]
 
