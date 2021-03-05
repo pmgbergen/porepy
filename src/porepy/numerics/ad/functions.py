638ee7f8bcc4d0941071e802914fb7f5c8b5f815
Jakub Both <jakub.both@uib.no>
2021-03-05 11:52:55 +0100

    ENH: Add functionality of evaluating functions in Local_Ad_array, and add some trigonometric functions.

--- src/porepy/numerics/ad/functions.py
+++ src/porepy/numerics/ad/functions.py
@@ -1,8 +1,10 @@
 import numpy as np
+import scipy.sparse as sps
 
 from porepy.numerics.ad.forward_mode import Ad_array
+from porepy.numerics.ad.local_forward_mode import Local_Ad_array
 
-__all__ = ["exp", "log", "sign", "abs"]
+__all__ = ["exp", "log", "sign", "abs", "sin", "cos", "tanh"]
 
 
 def exp(var):
@@ -10,17 +12,25 @@ def exp(var):
         val = np.exp(var.val)
         der = var.diagvec_mul_jac(np.exp(var.val))
         return Ad_array(val, der)
+    elif isinstance(var, Local_Ad_array):
+        val = np.exp(var.val)
+        der = np.exp(var.val) * var.jac
+        return Local_Ad_array(val, der)
     else:
         return np.exp(var)
 
 
 def log(var):
-    if not isinstance(var, Ad_array):
+    if isinstance(var, Ad_array):
+        val = np.log(var.val)
+        der = var.diagvec_mul_jac(1 / var.val)
+        return Ad_array(val, der)
+    elif isinstance(var, Local_Ad_array):
+        val = np.log(var.val)
+        der = (1 / var.val) * var.jac
+        return Local_Ad_array(val, der)
+    else:
         return np.log(var)
-
-    val = np.log(var.val)
-    der = var.diagvec_mul_jac(1 / var.val)
-    return Ad_array(val, der)
 
 
 def sign(var):
@@ -31,9 +41,54 @@ def sign(var):
 
 
 def abs(var):
-    if not isinstance(var, Ad_array):
-        return np.abs(var)
-    else:
+    if isinstance(var, Ad_array):
         val = np.abs(var.val)
         jac = var.diagvec_mul_jac(sign(var))
         return Ad_array(val, jac)
+    elif isinstance(var, Local_Ad_array):
+        val = np.abs(var.val)
+        jac = sign(var) * var.jac
+        return Local_Ad_array(val, jac)
+    else:
+        return np.abs(var)
+
+
+def sin(var):
+    if isinstance(var, Ad_array):
+        val = np.sin(var.val)
+        # TODO use capabilties offered by forward_mode.py
+        jac = sps.diags(np.cos(var.val)).tocsc() * var.jac
+        return Ad_array(val, jac)
+    elif isinstance(var, Local_Ad_array):
+        val = np.sin(var.val)
+        jac = np.cos(var.val) * var.jac
+        return Local_Ad_array(val, jac)
+    else:
+        return np.sin(var)
+
+
+def cos(var):
+    if isinstance(var, Ad_array):
+        val = np.cos(var.val)
+        # TODO use capabilties offered by forward_mode.py
+        jac = -sps.diags(np.sin(var.val)).tocsc() * var.jac
+        return Ad_array(val, jac)
+    elif isinstance(var, Local_Ad_array):
+        val = np.cos(var.val)
+        jac = -np.sin(var.val) * var.jac
+        return Local_Ad_array(val, jac)
+    else:
+        return np.cos(var)
+
+
+def tanh(var):
+    if isinstance(var, Ad_array):
+        val = np.tanh(var.val)
+        jac = sps.diags(1 - np.tanh(var.val)**2).tocsc() * var.jac
+        return Ad_array(val, jac)
+    elif isinstance(var, Local_Ad_array):
+        val = np.tanh(var.val)
+        jac = (1 - np.tanh(var.val) ** 2) * var.jac
+        return Local_Ad_array(val, jac)
+    else:
+        return np.tanh(var)
