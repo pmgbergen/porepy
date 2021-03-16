f095021c5141b5721b4c5dd6f06a5d1c2f8f83aa
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2021-03-16 09:15:06 +0100

    Update src/porepy/numerics/interface_laws/contact_mechanics_interface_laws.py

--- src/porepy/numerics/interface_laws/contact_mechanics_interface_laws.py
+++ src/porepy/numerics/interface_laws/contact_mechanics_interface_laws.py
@@ -479,7 +479,7 @@ class MatrixScalarToForceBalance(
 
         ## Reference pressure contribution
         # Rhs contribution of GradP, typically for nonzero reference temperature.
-        p_reference = data_primary[pp.PARAMETERS][self.keyword]["p_reference"]
+        p_reference: np.ndarray = data_primary[pp.PARAMETERS][self.keyword]["p_reference"]
         rhs[mortar_ind] += primary_scalar_to_primary_traction * p_reference
 
         return matrix, rhs
