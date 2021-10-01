6351f223752d4a623611726c68116efb4a548de1
Jhabriel Varela <jhabriel@gmail.com>
2021-10-01 20:13:02 +0200

    ENH/STY: Changed recomputation criteria / better representation of input parameters
    Now the method `new_time_step()` receives a mandatory boolean parameter `recompute_solution`. This gives more flexibility to which criteria should be met for recomputing a solution, which is now left to user.  In addition, some error messages were improved, and styling of input parameters improved.

--- src/porepy/numerics/time_step_control.py
+++ src/porepy/numerics/time_step_control.py
@@ -1,4 +1,4 @@
-from typing import Optional, Tuple, Union
+from typing import Tuple, Union
 
 __all__ = ["TimeSteppingControl"]
 
@@ -11,12 +11,12 @@ class TimeSteppingControl:
         schedule: list,
         dt_init: float,
         dt_min_max: Tuple[float, float],
-        iter_max: int,
-        iter_optimal_range: Tuple[int, int],
-        iter_lowupp_factor: Optional[Tuple[float, float]] = None,
-        recomp_factor: Optional[float] = None,
-        recomp_max: Optional[int] = None,
-        print_info: Optional[bool] = None,
+        iter_max: int = 15,
+        iter_optimal_range: Tuple[int, int] = (4, 7),
+        iter_lowupp_factor: Tuple[float, float] = (1.3, 0.7),
+        recomp_factor: float = 0.5,
+        recomp_max: int = 10,
+        print_info: bool = False,
     ):
         """Computes the next time step based on the number of non-linear iterations.
 
@@ -32,17 +32,20 @@ class TimeSteppingControl:
                   [1], [1, 0], [0, 1, 1, 2].
             dt_init (float): Initial time step.
             dt_min_max (Tuple of float): Minimum and maximum permissible time steps.
-            iter_max (int): Maximum number of iterations.
+            iter_max (int): Maximum number of iterations. Default is 15.
             iter_optimal_range (Tuple of int): Lower and upper optimal iteration range.
+                Default is (4, 7).
             iter_lowupp_factor (Tuple of float, optional): Lower and upper multiplication
                 factors. Default is (1.3, 0.7).
-            recomp_factor (float). Failed-to-converge recomputation factor. Default is 0.5.
-            recomp_max (int). Failed-to-converge maximum recomputation attempts. Default is 10.
-            print_info (bool). Print time-stepping information. Default is True.
+            recomp_factor (float). Failed-to-converge recomputation factor.
+                Default is 0.5.
+            recomp_max (int). Failed-to-converge maximum recomputation attempts.
+                Default is 10.
+            print_info (bool). Print time-stepping information. Default is False.
 
         """
 
-        # Sanity checks
+        # Sanity checks on input paramters
         if len(schedule) < 2:
             s = "Schedule list must have at least two items, representing the initial and"
             s += " final simualtion time."
@@ -74,7 +77,7 @@ class TimeSteppingControl:
             raise ValueError(s)
 
         if iter_max <= 0:
-            raise ValueError("Maximum amount of iterations must be a postive integer")
+            raise ValueError("Maximum number of iterations must be > 0")
 
         if iter_optimal_range[0] > iter_optimal_range[1]:
             s = "Lower optimal iteration range cannot be larger than"
@@ -82,21 +85,19 @@ class TimeSteppingControl:
             raise ValueError(s)
         elif iter_optimal_range[1] > iter_max:
             s = "Upper optimal iteration range cannot be larger than"
-            s += " maximum amount of iterations."
+            s += " maximum number of iterations."
             raise ValueError(s)
 
-        if iter_lowupp_factor is not None and (iter_lowupp_factor[0] <= 1):
+        if iter_lowupp_factor[0] <= 1:
             raise ValueError("Expected lower multiplication factor > 1")
-        elif iter_lowupp_factor is not None and (iter_lowupp_factor[1] >= 1):
+        elif iter_lowupp_factor[1] >= 1:
             raise ValueError("Expected upper multiplication factor < 1")
 
-        if (recomp_factor is not None) and recomp_factor >= 1:
-            raise ValueError("Expected recomputation factor < 1")
+        if recomp_factor >= 1:
+            raise ValueError("Expected recomputing multiplication factor < 1")
 
-        if (recomp_max is not None) and recomp_max <= 0:
-            raise ValueError(
-                "Number of recomputation attempts must be a positive integer"
-            )
+        if recomp_max <= 0:
+            raise ValueError("Number of recomputing attempts must be > 0")
 
         # Schedule, initial, and final times
         self.schedule = schedule
@@ -116,29 +117,13 @@ class TimeSteppingControl:
         self.iter_low, self.iter_upp = iter_optimal_range
 
         # Lower and upper multiplication factors
-        if iter_lowupp_factor is not None:
-            self.iter_low_factor, self.iter_upp_factor = iter_lowupp_factor
-        else:
-            self.iter_low_factor = 1.3
-            self.iter_upp_factor = 0.7
+        self.iter_low_factor, self.iter_upp_factor = iter_lowupp_factor
 
         # Re-computation multiplication factor
-        if recomp_factor is not None:
-            self.recomp_factor = recomp_factor
-        else:
-            self.recomp_factor = 0.5
+        self.recomp_factor = recomp_factor
 
         # Number of permissible re-computation attempts
-        if recomp_max is not None:
-            self.recomp_max = recomp_max
-        else:
-            self.recomp_max = 10
-
-        # Print information
-        if print_info is None:
-            self._print_info = True
-        else:
-            self._print_info = print_info
+        self.recomp_max = recomp_max
 
         # Time
         self.time = self.time_init
@@ -146,15 +131,15 @@ class TimeSteppingControl:
         # Time step. Initially, equal to the initial time step
         self.dt = self.dt_init
 
-        # Recompute solution flag
-        self.recomp_sol = False
-
         # Private attributes
         # Number of times the solution has been recomputed
         self._recomp_num = 0
 
         # Index of the next scheduled time
         self._scheduled_idx = 1
+
+        # Print information
+        self._print_info = print_info
 
     def __repr__(self) -> str:
 
@@ -169,11 +154,13 @@ class TimeSteppingControl:
         s += f"Lower multiplication factor = {self.iter_low_factor}\n"
         s += f"Upper multiplication factor = {self.iter_upp_factor}\n"
         s += f"Recompute solution multiplication factor = {self.recomp_factor}\n"
-        s += f"Maxximum recomputing attempts = {self.recomp_max}"
+        s += f"Maximum recomputing attempts = {self.recomp_max}"
 
         return s
 
-    def next_time_step(self, iters: int) -> Union[float, None]:
+    def next_time_step(
+        self, iterations: int, recompute_solution: bool
+    ) -> Union[float, None]:
         """
         Determines the next time step based on the previous amount of iterations needed
         to reach convergence. If convergence was not achieved, then the time step is
@@ -182,58 +169,62 @@ class TimeSteppingControl:
         stopped.
 
         Parameters
-        iters (int): Number of non-linear iterations. In time-dependent simulations,
-            this tipically represent the number of iterations for a time step.
+        iterations (int): Number of non-linear iterations. In time-dependent simulations,
+            this tipically represent the number of iterations for a given time step.
+        recompute_solution (bool): Wheter the solution needs to be recomputed or not. If
+            True, then the time step is multiplied by recomp_factor. If False, the time
+            step will be tune accordingly.
 
         Returns
-        -------
-        dt: float
-            Next time step
+        dt (float or None):  Next time step if time < final_simulation time. None otherwise.
+
         """
 
-        # First, check if we are allowed to recompute the solution
-        if self._recomp_num > self.recomp_max:
-            s = f"Solution did not convergece after {self.recomp_max}"
-            s += " recomputing attempts."
-            raise ValueError(s)
-
-        # Check if we reach final simulation time
+        # First, check if we reach final simulation time
         if self.time == self.time_final:
             return None
 
-        # If iters == max_iter:
+        # If the solution did not convergence and we are allow to recompute it:
+        #   Update simulation time (since solution will be recomputed)
         #   Decrease time step by the recomputing factor
-        #   Update time (since solution will be recomputed)
-        #   Set to True the re-computation flag
-        #   Increase counter that keeps track of how many times the solution was recomputed
-        if iters == self.iter_max:
-            if self._print_info:
-                s = "Solution did not converge. Reducing time step and recomputing solution."
-                s += f" Next dt = {self.dt}."
-                print(s)
-            self.time -= self.dt  # reduce time
-            self.dt = self.dt * self.recomp_factor  # reduce time step
-            self.recomp_sol = True
+        #   Increase counter keeping track of the number of times the solution was recomputed
+        #   Check if the time step is smaller that dt_min. Otherwise, use dt_min
+        if recompute_solution and self._recomp_num < self.recomp_max:
+            self.time -= self.dt
+            self.dt *= self.recomp_factor
             self._recomp_num += 1
+            if self._print_info:
+                s = "Solution did not converge and will be recomputed."
+                s += f" Recomputing attempt #{self._recomp_num}. Next dt = {self.dt}."
+                print(s)
+            if self.dt < self.dt_min:
+                self.dt = self.dt_min
+                if self._print_info:
+                    print(
+                        f"Calculated dt < dt_min. Using dt_min = {self.dt_min} instead."
+                    )
             return self.dt
-        else:
-            self.recomp_sol = False
+        elif not recompute_solution:  # we reach convergence, set recomp_num to zero
             self._recomp_num = 0
+        else:  # number of recomputing attempts has been exhausted
+            error_msg = f"Solution did not convergece after {self.recomp_max}"
+            error_msg += " recomputing attempts."
+            raise ValueError(error_msg)
 
         # If iters < max_iter. Proceed to determine the next time step using the
         # following criteria.
         # If iters is less than the lower optimal iteration range "iter_low", we can relax
         # the time step, and multiply by a lower multiplication factor greater than 1,
         # i.e., "factor_low". If the number of iterations is greater than the upper optimal
-        # iteration range "iter_upp", we have to decrease the time step by multiplying
-        # by an upper multiplication factor smaller than 1, i.e., "factor_upp". If neither
-        # of these situations occur, then the number iterations lies between the optimal
-        # iteration range, and the time step remains unchanged.
-        if iters <= self.iter_low:
+        # iteration range "iter_upp", we have to decrease the time step by multiplying by an
+        # upper multiplication factor smaller than 1, i.e., "factor_upp". If neither of these
+        # situations occur, then the number iterations lies between the optimal iteration
+        # range and the time step remains unchanged.
+        if iterations <= self.iter_low:
             self.dt = self.dt * self.iter_low_factor
             if self._print_info:
                 print(f"Relaxing time step. Next dt = {self.dt}.")
-        elif iters >= self.iter_upp:
+        elif iterations >= self.iter_upp:
             self.dt = self.dt * self.iter_upp_factor
             if self._print_info:
                 print(f"Restricting time step. Next dt = {self.dt}.")
@@ -250,7 +241,7 @@ class TimeSteppingControl:
             if self._print_info:
                 print(f"Calculated dt > dt_max. Using dt_max = {self.dt_max} instead.")
 
-        # Check if we reach a scheduled time, and match time step
+        # Check if we reach a scheduled time
         schedule_time = self.schedule[self._scheduled_idx]
         if (self.time + self.dt) > schedule_time:
             self.dt = schedule_time - self.time  # adapt time step
@@ -258,12 +249,13 @@ class TimeSteppingControl:
                 self._scheduled_idx += 1  # increase index to catch next scheduled time
                 if self._print_info:
                     print(
-                        f"Correcting dt to match scheduled time. Next dt = {self.dt}."
+                        f"Correcting time step to match scheduled time. Next dt = {self.dt}."
                     )
             else:
                 if self._print_info:
-                    print(f"Correcting dt to match final time. Final dt = {self.dt}.")
-
+                    print(
+                        f"Correcting time step to match final time. Next dt = {self.dt}."
+                    )
         return self.dt
 
     # Helpers
