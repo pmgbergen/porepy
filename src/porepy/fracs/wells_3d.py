11ca7807d79df42146424bfb91c5bcba3d6897fd
IvarStefansson <ivar.stefansson@uib.no>
2021-10-23 14:11:22 +0200

    Tag well grids with parent well index

--- src/porepy/fracs/wells_3d.py
+++ src/porepy/fracs/wells_3d.py
@@ -326,6 +326,7 @@ class WellNetwork3d:
                 g_w.compute_geometry()
                 gb.add_nodes(g_w)
                 g_w.well_num = well_num
+                g_w.tags["parent_well_index"] = w.index
                 well_num += 1
 
                 # Add intersection grid and interfaces if the second segment
