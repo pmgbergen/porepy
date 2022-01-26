74adb92da10d88f9fe1eb942079d7d839c8847c2
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2022-01-26 20:32:29 +0100

    BLD: Fix of github workflow

---
+++ src/porepy/fracs/frac_utils.py
@@ -0,0 +1,136 @@
+""" Frontend utility functions related to fractures and their meshing.
+
+"""
+import logging
+
+import numpy as np
+
+import porepy as pp
+
+__all__ = ["fracture_length_2d", "uniquify_points", "snap_fracture_set_2d"]
+
+# Module level logger
+logger = logging.getLogger(__name__)
+
+module_sections = ["gridding", "geometry"]
+
+
+@pp.time_logger(sections=module_sections)
+def fracture_length_2d(pts, edges):
+    """Find the length of 2D fracture traces.
+
+    Parameters:
+        pts (np.ndarray, 2 x n_pts): Coordinates of start and endpoints of
+            fractures.
+        edges (np.ndarary, 2 x n_fracs): Indices of start and endpoint of
+            fractures, referring to columns in pts.
+
+    Returns:
+        np.ndarray, length n_fracs: Length of each fracture.
+
+    """
+    start = pts[:, edges[0]]
+    end = pts[:, edges[1]]
+
+    length = np.sqrt(np.sum(np.power(end - start, 2), axis=0))
+    return length
+
+
+@pp.time_logger(sections=module_sections)
+def uniquify_points(pts, edges, tol):
+    """Uniquify a set of points by merging almost coinciding coordinates.
+
+    Also update fractures, and remove edges that consist of a single point
+    (either after the points were merged, or because the input was a point
+    edge).
+
+    Parameters:
+        pts (np.ndarary, n_dim x n_pts): Coordinates of start and endpoints of
+            the fractures.
+        edges (np.ndarray, n x n_fracs): Indices of start and endpoint of
+            fractures, referring to columns in pts. Should contain at least two
+            rows; additional rows representing fracture tags are also accepted.
+        tol (double): Tolerance used for merging points.
+
+    Returns:
+        np.ndarray (n_dim x n_pts_unique): Unique point array.
+        np.ndarray (2 x n_fracs_update): Updated start and endpoints of
+            fractures.
+        np.ndarray: Index (referring to input) of fractures deleted as they
+            effectively contained a single coordinate.
+
+    """
+
+    # uniquify points based on coordinates
+    p_unique, _, o2n = pp.utils.setmembership.unique_columns_tol(pts, tol=tol)
+    # update edges
+    e_unique_p = np.vstack((o2n[edges[:2]], edges[2:]))
+
+    # Find edges that start and end in the same point, and delete them
+    point_edge = np.where(np.diff(e_unique_p[:2], axis=0)[0] == 0)[0].ravel()
+    e_unique = np.delete(e_unique_p, point_edge, axis=1)
+
+    return p_unique, e_unique, point_edge
+
+
+@pp.time_logger(sections=module_sections)
+def snap_fracture_set_2d(pts, edges, snap_tol, termination_tol=1e-2, max_iter=100):
+    """Snap vertexes of a set of fracture lines embedded in 2D, so that small
+    distances between lines and vertexes are removed.
+
+    This is intended as a utility function to preprocess a fracture network
+    before meshing. The function may change both connectivity and orientation
+    of individual fractures in the network. Specifically, fractures that
+    almost form a T-intersection (or L), may be connected, while
+    X-intersections with very short ends may be truncated to T-intersections.
+
+    The modification snaps vertexes to the closest point on the adjacent line.
+    This will in general change the orientation of the fracture with the
+    snapped vertex. The alternative, to prolong the fracture along its existing
+    orientation, may result in very long fractures for almost intersecting
+    lines. Depending on how the fractures are ordered, the same point may
+    need to be snapped to a segment several times in an iterative process.
+
+    The algorithm is *not* deterministic, in the sense that if the ordering of
+    the fractures is permuted, the snapped fracture network will be slightly
+    different.
+
+    Parameters:
+        pts (np.array, 2 x n_pts): Array of start and endpoints for fractures.
+        edges (np.ndarray, n x n_fracs): First row contains index of start
+            of all fractures, referring to columns in pts. Second contains
+            index of endpoints.
+        snap_tol (double): Snapping tolerance. Distances below this will be
+            snapped.
+        termination_tol (double): Minimum point movement needed for the
+            iterations to continue.
+        max_iter (int, optional): Maximum number of iterations. Defaults to
+            100.
+
+    Returns:
+        np.array (2 x n_pts): Copy of the point array, with modified point
+            coordinates.
+        boolean: True if the iterations converged within allowed number of
+            iterations.
+
+    """
+    pts_orig = pts.copy()
+    counter = 0
+    pn = 0 * pts
+    while counter < max_iter:
+        pn = pp.constrain_geometry.snap_points_to_segments(pts, edges, tol=snap_tol)
+        diff = np.max(np.abs(pn - pts))
+        logger.debug("Iteration " + str(counter) + ", max difference" + str(diff))
+        pts = pn
+        if diff < termination_tol:
+            break
+        counter += 1
+
+    if counter < max_iter:
+        logger.info("Fracture snapping converged after " + str(counter) + " iterations")
+        logger.info("Maximum modification " + str(np.max(np.abs(pts - pts_orig))))
+        return pts, True
+    else:
+        logger.warning("Fracture snapping failed to converge")
+        logger.warning("Residual: " + str(diff))
+        return pts, False
---
+++ src/porepy/utils/tmp.py
@@ -0,0 +1,187 @@
+"""
+Various functions with set operations.
+"""
+import numba
+import numpy as np
+
+
+def unique_rows(data):
+    """
+    Function similar to Matlab's unique(...,'rows')
+
+    See also function unique_columns in this module; this is likely slower, but
+    is understandable, documented, and has a tolerance option.
+
+    Copied from
+    http://stackoverflow.com/questions/16970982/find-unique-rows-in-numpy-array/
+    (summary pretty far down on the page)
+    Note: I have no idea what happens here
+
+    """
+    b = np.ascontiguousarray(data).view(
+        np.dtype((np.void, data.dtype.itemsize * data.shape[1]))
+    )
+    _, ia = np.unique(b, return_index=True)
+    _, ic = np.unique(b, return_inverse=True)
+    #    return np.unique(b).view(data.dtype).reshape(-1, data.shape[1]), ia, ic
+    return data[ia], ia, ic
+
+
+def ismember_rows(a, b, sort=True):
+    """
+    Find *columns* of a that are also members of *columns* of b.
+
+    The function mimics Matlab's function ismember(..., 'rows').
+
+    If the numpy version is less than 1.13, this function will be slow for
+    large arrays.
+
+    TODO: Rename function, this is confusing!
+
+    Parameters:
+        a (np.array): Each column in a will search for an equal in b.
+        b (np.array): Array in which we will look for a twin
+        sort (boolean, optional): If true, the arrays will be sorted before
+            seraching, increasing the chances for a match. Defaults to True.
+
+    Returns:
+        np.array (boolean): For each column in a, true if there is a
+            corresponding column in b.
+        np.array (int): Indexes so that b[:, ind] is also found in a.
+
+    Examples:
+        >>> a = np.array([[1, 3, 3, 1, 7], [3, 3, 2, 3, 0]])
+        >>> b = np.array([[3, 1, 3, 5, 3], [3, 3, 2, 1, 2]])
+        >>> ismember_rows(a, b)
+        (array([ True,  True,  True,  True, False], dtype=bool), [1, 0, 2, 1])
+
+        >>> a = np.array([[1, 3, 3, 1, 7], [3, 3, 2, 3, 0]])
+        >>> b = np.array([[3, 1, 2, 5, 1], [3, 3, 3, 1, 2]])
+        >>> ismember_rows(a, b, sort=False)
+        (array([ True,  True, False,  True, False], dtype=bool), [1, 0, 1])
+
+    """
+
+    # Sort if required, but not if the input is 1d
+    if sort and a.ndim > 1:
+        sa = np.sort(a, axis=0)
+        sb = np.sort(b, axis=0)
+    else:
+        sa = a
+        sb = b
+
+    b = np.atleast_1d(b)
+    a = np.atleast_1d(a)
+    num_a = a.shape[-1]
+
+    # stack the arrays
+    c = np.hstack((sa, sb))
+    # Uniquify c. We don't care about the unique array itself, but rather
+    # the indices which maps the unique array back to the original c
+    if a.ndim > 1:
+        _, ind = np.unique(c, axis=1, return_inverse=True)
+    else:
+        _, ind = np.unique(c, return_inverse=True)
+
+    # Indices in a and b referring to the unique array.
+    # Elements in ind_a that are also in ind_b will correspond to rows
+    # in a that are also found in b
+    ind_a = ind[:num_a]
+    ind_b = ind[num_a:]
+
+    # Find common members
+    ismem_a = np.isin(ind_a, ind_b)
+
+    # Fonud this trick on
+    # https://stackoverflow.com/questions/8251541/numpy-for-every-element-in-one-array-find-the-index-in-another-array
+    # See answer by Joe Kington
+    sort_ind = np.argsort(ind_b)
+    ypos = np.searchsorted(ind_b[sort_ind], ind_a[ismem_a])
+    ia = sort_ind[ypos]
+
+    # Done
+    return ismem_a, ia
+
+
+def unique_columns_tol(mat, tol=1e-8):
+    """
+    Remove duplicates from a point set, for a given distance traveling.
+
+    Resembles Matlab's uniquetol function, as applied to columns. To rather
+    work at rows, use a transpose.
+
+    Parameters:
+        mat (np.ndarray, nd x n_pts): Columns to be uniquified
+        tol (double, optional): Tolerance for when columns are considered equal.
+            Should be seen in connection with distance between the points in
+            the points (due to rounding errors). Defaults to 1e-8.
+
+    Returns:
+        np.ndarray: Unique columns.
+        new_2_old: Index of which points that are preserved
+        old_2_new: Index of the representation of old points in the reduced
+            list.
+
+    Example (won't work as doctest):
+        >>> p_un, n2o, o2n = unique_columns(np.array([[1, 0, 1], [1, 0, 1]]))
+        >>> p_un
+        array([[1, 0], [1, 0]])
+        >>> n2o
+        array([0, 1])
+        >>> o2n
+        array([0, 1, 0])
+
+    """
+    # Treat 1d array as 2d
+    mat = np.atleast_2d(mat)
+
+    # Special treatment of the case with an empty array
+    if mat.shape[1] == 0:
+        return mat, np.array([], dtype=int), np.array([], dtype=int)
+
+    # If the matrix is integers, and the tolerance less than 1/2, we can use
+    # numpy's unique function
+    if issubclass(mat.dtype.type, np.int_) and tol < 0.5:
+        un_ar, new_2_old, old_2_new = np.unique(
+            mat, return_index=True, return_inverse=True, axis=1
+        )
+        return un_ar, new_2_old, old_2_new
+
+    @numba.jit("Tuple((b1[:],i8[:],i8[:]))(f8[:, :],f8)", nopython=True, cache=True)
+    def _numba_distance(mat, tol):
+        # Helper function for numba acceleration of unique_columns_tol
+        num_cols = mat.shape[0]
+        keep = np.zeros(num_cols, dtype=numba.types.bool_)
+        keep[0] = True
+        keep_counter = 1
+
+        # Map from old points to the unique subspace. Defaults to map to itself.
+        old_2_new = np.arange(num_cols)
+
+        # Loop over all points, check if it is already represented in the kept list
+        for i in range(1, num_cols):
+            d = np.sum((mat[i] - mat[keep]) ** 2, axis=1)
+            condition = d < tol ** 2
+
+            if np.any(condition):
+                # We will not keep this point
+                old_2_new[i] = np.argmin(d)
+            else:
+                # We have found a new point
+                keep[i] = True
+                old_2_new[i] = keep_counter
+                keep_counter += 1
+
+        # Finally find which elements we kept
+        new_2_old = np.nonzero(keep)[0]
+
+        return keep, new_2_old, old_2_new
+
+    mat_t = np.atleast_2d(mat.T).astype(float)
+
+    # IMPLEMENTATION NOTE: For small problems, it will likely not pay off to
+    # wait for the numba compile. Consider to make a python version of the numba
+    # function and use this for small (whatever that means) mat_t.
+    keep, new_2_old, old_2_new = _numba_distance(mat_t, tol)
+
+    return mat[:, keep], new_2_old, old_2_new
