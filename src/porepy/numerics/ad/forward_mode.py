cd57c7929767a68e59f7ea42ca08360d7d23c73a
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2022-01-26 15:23:48 +0100

    MAINT: Removed logging from forward Ad

--- src/porepy/numerics/ad/forward_mode.py
+++ src/porepy/numerics/ad/forward_mode.py
@@ -1,14 +1,9 @@
 import numpy as np
 import scipy.sparse as sps
 
-import porepy as pp
-
 __all__ = ["initAdArrays", "Ad_array"]
 
-module_sections = ["assembly", "numerics"]
 
-
-@pp.time_logger(sections=module_sections)
 def initAdArrays(variables):
     if not isinstance(variables, list):
         try:
@@ -16,7 +11,6 @@ def initAdArrays(variables):
         except AttributeError:
             num_val = 1
         return Ad_array(variables, sps.diags(np.ones(num_val)).tocsc())
-
     num_val = [v.size for v in variables]
     ad_arrays = []
     for i, val in enumerate(variables):
@@ -28,12 +22,10 @@ def initAdArrays(variables):
         # initiate Ad_array
         jac = sps.bmat([jac])
         ad_arrays.append(Ad_array(val, jac))
-
     return ad_arrays
 
 
 class Ad_array:
-    @pp.time_logger(sections=module_sections)
     def __init__(self, val=1.0, jac=0.0):
         self.val = val
         self.jac = jac
@@ -43,7 +35,6 @@ class Ad_array:
         s += f"Jacobian is of size {self.jac.shape} and has {self.jac.data.size} elements"
         return s
 
-    @pp.time_logger(sections=module_sections)
     def __add__(self, other):
         b = _cast(other)
         c = Ad_array()
@@ -51,42 +42,33 @@ class Ad_array:
         c.jac = self.jac + b.jac
         return c
 
-    @pp.time_logger(sections=module_sections)
     def __radd__(self, other):
         return self.__add__(other)
 
-    @pp.time_logger(sections=module_sections)
     def __sub__(self, other):
         b = _cast(other).copy()
         b.val = -b.val
         b.jac = -b.jac
         return self + b
 
-    @pp.time_logger(sections=module_sections)
     def __rsub__(self, other):
         return -self.__sub__(other)
 
-    @pp.time_logger(sections=module_sections)
     def __lt__(self, other):
         return self.val < _cast(other).val
 
-    @pp.time_logger(sections=module_sections)
     def __le__(self, other):
         return self.val <= _cast(other).val
 
-    @pp.time_logger(sections=module_sections)
     def __gt__(self, other):
         return self.val > _cast(other).val
 
-    @pp.time_logger(sections=module_sections)
     def __ge__(self, other):
         return self.val >= _cast(other).val
 
-    @pp.time_logger(sections=module_sections)
     def __eq__(self, other):
         return self.val == _cast(other).val
 
-    @pp.time_logger(sections=module_sections)
     def __mul__(self, other):
         if not isinstance(other, Ad_array):  # other is scalar
             val = self.val * other
@@ -99,7 +81,6 @@ class Ad_array:
             jac = self.diagvec_mul_jac(other.val) + other.diagvec_mul_jac(self.val)
         return Ad_array(val, jac)
 
-    @pp.time_logger(sections=module_sections)
     def __rmul__(self, other):
         if isinstance(other, Ad_array):
             # other is Ad_var, so should have called __mul__
@@ -108,7 +89,6 @@ class Ad_array:
         jac = self._other_mul_jac(other)
         return Ad_array(val, jac)
 
-    @pp.time_logger(sections=module_sections)
     def __pow__(self, other):
         if not isinstance(other, Ad_array):
             val = self.val ** other
@@ -120,33 +100,27 @@ class Ad_array:
             ) + other.diagvec_mul_jac(self.val ** other.val * np.log(self.val))
         return Ad_array(val, jac)
 
-    @pp.time_logger(sections=module_sections)
     def __rpow__(self, other):
         if isinstance(other, Ad_array):
             raise ValueError(
                 "Somthing went horrible wrong, should" "have called __pow__"
             )
-
         val = other ** self.val
         jac = self.diagvec_mul_jac(other ** self.val * np.log(other))
         return Ad_array(val, jac)
 
-    @pp.time_logger(sections=module_sections)
     def __truediv__(self, other):
         return self * other ** -1
 
-    @pp.time_logger(sections=module_sections)
     def __neg__(self):
         b = self.copy()
         b.val = -b.val
         b.jac = -b.jac
         return b
 
-    @pp.time_logger(sections=module_sections)
     def __len__(self):
         return len(self.val)
 
-    @pp.time_logger(sections=module_sections)
     def copy(self):
         b = Ad_array()
         try:
@@ -159,19 +133,16 @@ class Ad_array:
             b.jac = self.jac
         return b
 
-    @pp.time_logger(sections=module_sections)
     def diagvec_mul_jac(self, a):
         try:
             A = sps.diags(a)
         except TypeError:
             A = a
-
         if isinstance(self.jac, np.ndarray):
             return np.array([A * J for J in self.jac])
         else:
             return A * self.jac
 
-    @pp.time_logger(sections=module_sections)
     def jac_mul_diagvec(self, a):
         try:
             A = sps.diags(a)
@@ -182,27 +153,16 @@ class Ad_array:
         else:
             return self.jac * A
 
-    @pp.time_logger(sections=module_sections)
     def full_jac(self):
         return self.jac
 
-    #        return sps.hstack(self.jac[:])
-
-    @pp.time_logger(sections=module_sections)
     def _other_mul_jac(self, other):
         return other * self.jac
 
-    #        return np.array([other * J for J in self.jac])
-
-    @pp.time_logger(sections=module_sections)
     def _jac_mul_other(self, other):
         return self.jac * other
 
 
-#        return np.array([J * other for J in self.jac])
-
-
-@pp.time_logger(sections=module_sections)
 def _cast(variables):
     if isinstance(variables, list):
         out_var = []
