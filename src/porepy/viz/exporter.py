f37dc19ea58602afc3a50c5dc7a109b7bac6a7c1
jwboth <73653591+jwboth@users.noreply.github.com>
2022-02-18 11:43:05 +0100

    DOC: Fix mistake in comment
    Co-authored-by: Ivar Stefansson <ivar.stefansson@uib.no>

--- src/porepy/viz/exporter.py
+++ src/porepy/viz/exporter.py
@@ -398,7 +398,7 @@ class Exporter:
 
         self.gb.remove_node_props(extra_node_names)
 
-        # Extract data which is contained in nodes (and not edges).
+        # Extract data which is contained in edges (and not nodes).
         # IMPLEMENTATION NOTE: See the above loop to construct node_data for an explanation of this
         # elaborate construction of `edge_data`
         edge_data = list()
