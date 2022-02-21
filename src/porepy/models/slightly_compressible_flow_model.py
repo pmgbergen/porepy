54a00ba58d5968a72714c83643ce8306900137c2
Omar Duran <omaryesiduran@gmail.com>
2022-02-22 00:17:51 +0100

    MAINT: Refactor SlightlyCompressibleFlow

--- src/porepy/models/slightly_compressible_flow_model.py
+++ src/porepy/models/slightly_compressible_flow_model.py
@@ -3,16 +3,12 @@ including a time derivative of the pressure and constant compressibility.
 """
 
 import logging
-import time
-from typing import Dict, Optional
+from typing import Dict, List, Optional
 
 import numpy as np
-import scipy.sparse as sps
-
 import porepy as pp
 
 logger = logging.getLogger(__name__)
-module_sections = ["models", "numerics"]
 
 
 class SlightlyCompressibleFlow(pp.models.incompressible_flow_model.IncompressibleFlow):
@@ -34,6 +30,7 @@ class SlightlyCompressibleFlow(pp.models.incompressible_flow_model.Incompressibl
         end_time (float): Upper limit of considered time interval
         time_step (float): time step size
         time_index (int): number of time loops passed.
+        time (float): simulation time
     """
 
     def __init__(
@@ -57,7 +54,9 @@ class SlightlyCompressibleFlow(pp.models.incompressible_flow_model.Incompressibl
         self.end_time = abs(float(end_time))
         self.time_step = abs(float(time_step))
         self.time_index = 0
+        self.time: float = float(0)
 
+        
     def _set_parameters(self) -> None:
         """Set default (unitary/zero) parameters for the flow problem.
 
@@ -68,15 +67,14 @@ class SlightlyCompressibleFlow(pp.models.incompressible_flow_model.Incompressibl
 
         for g, d in self.gb:
 
-            mass_weight = self._compressibility(g)
-
             pp.initialize_data(
                 g,
                 d,
                 self.parameter_key,
                 {
-                    "mass_weight": mass_weight
-                },
+                    "mass_weight": self._compressibility(g),
+                    'time_step': self.time_step
+                }
             )
 
     def _compressibility(self, g: pp.Grid) -> np.ndarray:
@@ -85,19 +83,7 @@ class SlightlyCompressibleFlow(pp.models.incompressible_flow_model.Incompressibl
         Units: Pa^(-1)
         """
         return np.ones(g.num_cells)
-
-    def _source(self, g: pp.Grid) -> np.ndarray:
-        """Zero source term. TODO introduce source
-
-        Units: m^3 / s
-        """
-        return np.zeros(g.num_cells)
-
-    def _initial_condition(self) -> None:
-        """Set initial guess for the variables."""
-        initial_values = np.zeros(self.dof_manager.num_dofs())
-        self.dof_manager.distribute_variable(initial_values)
-        self.dof_manager.distribute_variable(initial_values, to_iterate=True)
+      
 
     def _assign_discretizations(self) -> None:
         """Define equations through discretizations.
@@ -109,63 +95,27 @@ class SlightlyCompressibleFlow(pp.models.incompressible_flow_model.Incompressibl
         """
 
         super()._assign_discretizations()
-        gb = self.gb
-        grid_list = [g for g, _ in gb.nodes()]
-
-
-        # Ad representation of discretizations
-        mass_mat = pp.MassMatrix(self.parameter_key)
-        mass_mat.discretize(gb)
-
-        # Ad variables
-        p = self._eq_manager.merge_variables([(g, self.variable) for g in grid_list])
-
-        # Update equations
-        subdomain_flow_eq = self._eq_manager.equations["subdomain_flow"]
-        interface_flow_eq = self._eq_manager.equations["interface_flow"]
-
-        mass = (
-            mass_mat * (p - p.previous_timestep()) / self.time_step
+        
+        if not hasattr(self, "dof_manager"):
+            self.dof_manager = pp.DofManager(self.gb)
+        
+        # Collection of subdomains
+        subdomains: List[pp.Grid] = [g for g, _ in self.gb]
+        
+        # AD representation of the mass operator
+        accumulation_term = pp.ad.MassMatrixAd(self.parameter_key, subdomains)
+        
+        # Need to find a way / refactor to access the superclass "and pressure" instead of create a new one
+        # If for some reason the mesh ordering change, this will be inconsistent
+        p = self._eq_manager.merge_variables([(g, self.variable) for g in subdomains])
+        time_step_ad = pp.ad.Scalar(self.time_step, "time step")
+        
+        accumulation_term = accumulation_term.mass * (
+            p - p.previous_timestep()
         )
-
-
-        subdomain_flow_eq += mass
-        interface_flow_eq += mass
-
-        # dict update not necessary if retrieved by reference
-        self._eq_manager.equations.update(
-            {
-                "subdomain_flow": subdomain_flow_eq,
-                "interface_flow": interface_flow_eq,
-            }
-        )
-
-    def assemble_and_solve_linear_system(self, tol: float) -> np.ndarray:
-        """Use a direct solver for the linear system."""
-        A, b = self._eq_manager.assemble()
-        logger.debug(f"Max element in A {np.max(np.abs(A)):.2e}")
-        logger.debug(
-            f"Max {np.max(np.sum(np.abs(A), axis=1)):.2e} and min"
-            + f" {np.min(np.sum(np.abs(A), axis=1)):.2e} A sum."
-        )
-        tic = time.time()
-        x = sps.linalg.spsolve(A, b)
-        logger.info("Solved linear system in {} seconds".format(time.time() - tic))
-        return x
-
-    def after_newton_convergence(
-        self, solution: np.ndarray, errors: float, iteration_counter: int
-    ) -> None:
-        if self._use_ad:
-            solution = self.dof_manager.assemble_variable(from_iterate=True)
-
-        self.assembler.distribute_variable(solution)
-        self.convergence_status = True
-        self._export()
-
-    ## Methods required by AbstractModel but irrelevant for static problems:
-    def before_newton_loop(self):
-        self._nonlinear_iteration = 0
-
-    def after_simulation(self):
-        pass
+        accumulation_term /= time_step_ad
+        
+        #  Adding accumulation term to incompressible flow equations 
+        self._eq_manager.equations["subdomain_flow"] += accumulation_term
+        
+        
