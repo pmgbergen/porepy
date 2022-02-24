438dc785b56fd8e7822418bc95124ca275a24ced
Omar Duran <omaryesiduran@gmail.com>
2022-02-24 15:34:54 +0100

    STY: Mypy, isort, black, flake8

--- src/porepy/models/slightly_compressible_flow_model.py
+++ src/porepy/models/slightly_compressible_flow_model.py
@@ -6,13 +6,14 @@ import logging
 from typing import Dict, List, Optional
 
 import numpy as np
+
 import porepy as pp
 
 logger = logging.getLogger(__name__)
 
 
 class SlightlyCompressibleFlow(pp.models.incompressible_flow_model.IncompressibleFlow):
-    """ This class extends the Incompressible flow model by including a
+    """This class extends the Incompressible flow model by including a
     cummulative term expressed through pressure and a constant compressibility
     coefficient. For a full documenation refer to the parent class.
 
@@ -33,16 +34,11 @@ class SlightlyCompressibleFlow(pp.models.incompressible_flow_model.Incompressibl
         time (float): simulation time
     """
 
-    def __init__(
-        self,
-        end_time: Optional[float] = 10.,
-        time_step: Optional[float] = 0.1,
-        params: Optional[Dict] = None
-    ) -> None:
-        """ 
+    def __init__(self, params: Optional[Dict] = None) -> None:
+        """
         Parameters:
             end_time (optional float): specifices end time for simulation.
-                Simulation stops when first time value bigger than end_time is 
+                Simulation stops when first time value bigger than end_time is
                 reached.
             time_step (optional float ): Timestep size. Currently only
                 uniform timestepping is supported.
@@ -50,13 +46,11 @@ class SlightlyCompressibleFlow(pp.models.incompressible_flow_model.Incompressibl
         super().__init__(params)
 
         # attributes
-        # assure positivity and correct type and format of input
-        self.end_time = abs(float(end_time))
-        self.time_step = abs(float(time_step))
+        self.end_time = float(0)
+        self.time_step = float(0)
+        self.time = float(0)
         self.time_index = 0
-        self.time: float = float(0)
 
-        
     def _set_parameters(self) -> None:
         """Set default (unitary/zero) parameters for the flow problem.
 
@@ -68,12 +62,7 @@ class SlightlyCompressibleFlow(pp.models.incompressible_flow_model.Incompressibl
         for g, d in self.gb:
 
             pp.initialize_data(
-                g,
-                d,
-                self.parameter_key,
-                {
-                    "mass_weight": self._compressibility(g)
-                }
+                g, d, self.parameter_key, {"mass_weight": self._compressibility(g)}
             )
 
     def _compressibility(self, g: pp.Grid) -> np.ndarray:
@@ -82,7 +71,6 @@ class SlightlyCompressibleFlow(pp.models.incompressible_flow_model.Incompressibl
         Units: Pa^(-1)
         """
         return np.ones(g.num_cells)
-      
 
     def _assign_discretizations(self) -> None:
         """Define equations through discretizations.
@@ -94,23 +82,20 @@ class SlightlyCompressibleFlow(pp.models.incompressible_flow_model.Incompressibl
         """
 
         super()._assign_discretizations()
-        
+
         # Collection of subdomains
         subdomains: List[pp.Grid] = [g for g, _ in self.gb]
-        
+
         # AD representation of the mass operator
         accumulation_term = pp.ad.MassMatrixAd(self.parameter_key, subdomains)
-        
+
         # Access to pressure ad variable
         p = self._ad.pressure
         time_step_ad = pp.ad.Scalar(self.time_step, "time step")
-        
-        accumulation_term = accumulation_term.mass * (
-            p - p.previous_timestep()
+
+        accumulation_term = (
+            accumulation_term.mass * (p - p.previous_timestep()) / time_step_ad
         )
-        accumulation_term /= time_step_ad
-        
-        #  Adding accumulation term to incompressible flow equations 
+
+        #  Adding accumulation term to incompressible flow equations
         self._eq_manager.equations["subdomain_flow"] += accumulation_term
-        
-        
