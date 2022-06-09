55c28dbfe21c8f47ce98133543567b0f81374e76
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2022-06-09 12:06:06 +0200

    DEL: Purged old cython function for block inversion.
    The code was never used and numba is more efficient.
    
    Also removed cython as a dependency.

--- setup.py
+++ setup.py
@@ -6,48 +6,6 @@ from os.path import basename, splitext
 
 from setuptools import find_packages, setup
 
-# ----------------------  Cython compilation
-# Build cython extensions as part of setup. Based on
-# https://stackoverflow.com/questions/4505747/how-should-i-structure-a-python-package-that-contains-cython-code
-USE_CYTHON = False
-from distutils.core import setup
-from distutils.extension import Extension
-
-if USE_CYTHON:
-    try:
-        # Trick from
-        # https://stackoverflow.com/questions/19919905/how-to-bootstrap-numpy-installation-in-setup-py
-        from Cython.Distutils import build_ext as _build_ext
-
-        class build_ext(_build_ext):
-            def finalize_options(self):
-                _build_ext.finalize_options(self)
-                # Prevent numpy from thinking it is still in its setup process:
-                __builtins__.__NUMPY_SETUP__ = False
-                import numpy
-
-                self.include_dirs.append(numpy.get_include())
-
-    except:
-        # Cython is apparently not available on the system. Make do without it
-        USE_CYTHON = False
-
-cmdclass = {}
-ext_modules = []
-
-# New Cython modules must be registered here
-if USE_CYTHON:
-    ext_modules += [
-        Extension(
-            "porepy.numerics.fv.cythoninvert",
-            ["src/porepy/numerics/fv/invert_diagonal_blocks.pyx"],
-        )
-    ]
-    cmdclass.update({"build_ext": build_ext})
-
-# --------------------- End of cython part
-
-
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
