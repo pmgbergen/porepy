64cb1bfee0aa05b6b84c08ea791f4b92c2328d2d
Ingrid K Jacobsen <ingrid.kristine.jacobsen@gmail.com>
2022-06-22 15:06:43 +0200

    First draft of 2d fracture object

---
+++ src/porepy/fracs/fracture_2d.py
@@ -0,0 +1,91 @@
+"""Contains classes representing fractures in 1D, i.e. manifolds of dimension 1 embedded in 2D.
+"""
+from __future__ import annotations
+
+from typing import Optional
+
+import numpy as np
+from sympy.geometry import Point, Polygon
+
+import porepy as pp
+
+from fracture_3d import Fracture3d
+from fracture import Fracture
+
+class Fracture2d(Fracture):
+    """A class representing linear fracture in 2D.
+    
+    """
+    # @abc.abstractmethod
+    def sort_points(self) -> np.ndarray:
+        """Abstract method to sort the vertices as needed for geometric algorithms.
+
+        Returns
+        -------
+        numpy.ndarray(dtype=int)
+            The indices corresponding to the sorting.
+        """
+        return self.p
+
+    # @abc.abstractmethod
+    def local_coordinates(self) -> np.ndarray:
+        """Abstract method for computing the vertex coordinates in a local system and its
+        local dimension `d`.
+        For now, `d` is expected to be `nd` - 1, i.e. the fracture to be of co-dimension 1.
+
+        Returns
+        -------
+        numpy.ndarray((nd-1) x npt)
+            The coordinates of the vertices in local dimensions.
+        """
+        rotation = pp.map_geometry.project_line_matrix(self.p)
+        points_2d = rotation.dot(self.p)
+
+        return points_2d[:2]
+
+    # @abc.abstractmethod
+    def is_convex(self) -> bool:
+        """Abstract method for implementing a convexity check.
+
+        Returns
+        -------
+        bool
+            True if the fracture is convex, False otherwise.
+        """
+        return True
+
+    # @abc.abstractmethod
+    def is_planar(self, tol: float = 1e-4) -> bool:
+        """Abstract method to check if the fracture is planar.
+
+        Parameters
+        ----------
+            tol : float
+                Tolerance for non-planarity. Treated as an absolute quantity
+                (no scaling with fracture extent).
+
+        Returns
+        -------
+        bool
+            True if the fracture is planar, False otherwise.
+        """
+        return True
+
+    # @abc.abstractmethod
+    def compute_centroid(self) -> np.ndarray:
+        """Method for computing and returning the centroid of the fracture.
+        Convexity is assumed.
+        """
+        x_coord = self.p[0]
+        y_coord = self.p[1]
+        centroid_x = sum(x_coord)/2
+        centroid_y = sum(y_coord)/2
+        return np.array([centroid_x,centroid_y,0])
+
+    # @abc.abstractmethod
+    def compute_normal(self) -> np.ndarray:
+        """See parent class docs."""
+        return pp.map_geometry.compute_normals_1d(self.p)[:,0]
+
+f = Fracture2d(np.array([[0,0],[0,1],[0,0]]))
+
