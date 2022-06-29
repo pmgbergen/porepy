02e3067fae381a5d2512255631630dcfa019980a
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2022-06-29 15:18:30 +0200

    TST: Test_DFN is running. Fixes to cell-to-dof-map discretization.

--- src/porepy/numerics/interface_laws/cell_dof_face_dof_map.py
+++ src/porepy/numerics/interface_laws/cell_dof_face_dof_map.py
@@ -5,11 +5,13 @@ multipliers for the interface law. Use this module for the co-dimensional object
 In this way no equations are explicitly associated
 and some of the interface operators are provided.
 """
+from typing import Optional
 import numpy as np
 import scipy.sparse as sps
+import porepy as pp
 
 
-class CellDofFaceDofMap(object):
+class CellDofFaceDofMap:
     """
     Only the methods int_bound_source and int_bound_pressure_cell are implemented to allow
     appropriate interface laws. No discretization is associated to the grid.
@@ -30,7 +32,7 @@ class CellDofFaceDofMap(object):
 
     """
 
-    def __init__(self, keyword):
+    def __init__(self, keyword: str) -> None:
         """Set the discretization, with the keyword used for storing various
         information associated with the discretization.
 
@@ -40,7 +42,7 @@ class CellDofFaceDofMap(object):
         """
         self.keyword = keyword
 
-    def _key(self):
+    def _key(self) -> str:
         """Get the keyword of this object, on a format friendly to access relevant
         fields in the data dictionary
 
@@ -50,19 +52,19 @@ class CellDofFaceDofMap(object):
         """
         return self.keyword + "_"
 
-    def ndof(self, g):
+    def ndof(self, sd: pp.Grid) -> int:
         """Return the number of degrees of freedom, in this case the number of cells
 
         Parameters
-            g (grid): Computational grid
+            sd (pp.Grid): Computational grid
 
         Returns:
             int: the number of degrees of freedom (number of cells).
 
         """
-        return g.num_cells
+        return sd.num_cells
 
-    def extract_pressure(self, g, solution_array, data=None):
+    def extract_pressure(self, sd: pp.Grid, solution_array, data=None) -> np.ndarray:
         """Return exactly the solution_array itself.
 
         Parameters:
@@ -78,11 +80,13 @@ class CellDofFaceDofMap(object):
         """
         return solution_array
 
-    def extract_flux(self, g, solution_array, data=None):
+    def extract_flux(
+        self, sd: pp.Grid, solution_array: np.ndarray, data=None
+    ) -> np.ndarray:
         """We return an empty vector for consistency with the previous method.
 
         Parameters:
-            g (grid): To which the solution array belongs.
+            sd (grid): To which the solution array belongs.
             solution_array (np.array): Solution for this grid obtained from
                 either a mono-dimensional or a mixed-dimensional problem.
             data (dictionary): Data dictionary associated with the grid. Not used.
@@ -93,22 +97,24 @@ class CellDofFaceDofMap(object):
         """
         return np.empty(0)
 
-    def discretize(self, g, data):
+    def discretize(self, sd: pp.Grid, data: dict) -> None:
         """Construct discretization matrices. Operation is void for this discretization.
 
         Parameters:
-            g (pp.Grid): Grid to be discretized.
+            sd (pp.Grid): Grid to be discretized.
             data (dictionary): With discretization parameters.
 
         """
         pass
 
-    def assemble_matrix_rhs(self, g, data=None):
+    def assemble_matrix_rhs(
+        self, sd: pp.Grid, data: Optional[dict] = None
+    ) -> tuple[sps.spmatrix, np.ndarray]:
         """Return the matrix and right-hand side, no PDE are associate with
         this discretization so empty matrix and zero rhs is returned.
 
         Parameters:
-            g (Grid): Computational grid, with geometry fields computed.
+            sd (pp.Grid): Computational grid, with geometry fields computed.
             data (dictionary): With data stored.
 
         Returns:
@@ -116,36 +122,36 @@ class CellDofFaceDofMap(object):
             np.ndarray: zero right hand side vector.
 
         """
-        return self.assemble_matrix(g, data), self.assemble_rhs(g, data)
+        return self.assemble_matrix(sd, data), self.assemble_rhs(sd, data)
 
-    def assemble_matrix(self, g, data=None):
+    def assemble_matrix(self, sd: pp.Grid, data: Optional[dict] = None) -> sps.spmatrix:
         """An empty matrix with size num_cells x num_cells.
 
         Parameters:
-            g (Grid): Computational grid, with geometry fields computed.
+            sd (pp.Grid): Computational grid, with geometry fields computed.
             data (dictionary): With data stored. Not used.
 
         Returns:
             scipy.sparse.csr_matrix: System matrix of this discretization.
 
         """
-        return sps.csr_matrix((self.ndof(g), self.ndof(g)))
+        return sps.csr_matrix((self.ndof(sd), self.ndof(sd)))
 
-    def assemble_rhs(self, g, data=None):
+    def assemble_rhs(self, sd: pp.Grid, data: Optional[dict] = None) -> np.ndarray:
         """Zero right-hand side vector.
 
         Parameters:
-            g (Grid): Computational grid, with geometry fields computed.
+            sd (pp.Grid): Computational grid, with geometry fields computed.
             data (dictionary): With data stored. Not used.
 
         Returns:
             np.ndarray: Zero right hand side vector.
 
         """
-        return np.zeros(self.ndof(g))
+        return np.zeros(self.ndof(sd))
 
     def assemble_int_bound_flux(
-        self, g, data, data_intf, cc, matrix, rhs, self_ind, use_secondary_proj
+        self, sd, data, data_intf, cc, matrix, rhs, self_ind, use_secondary_proj
     ):
         """Abstract method. Assemble the contribution from an internal
         boundary, manifested as a flux boundary condition.
@@ -159,7 +165,7 @@ class CellDofFaceDofMap(object):
         the node and the mortar grid on the relevant edge.
 
         Parameters:
-            g (Grid): Grid which the condition should be imposed on.
+            sd (Grid): Grid which the condition should be imposed on.
             data (dictionary): Data dictionary for the node in the
                 mixed-dimensional grid.
             data_intf (dictionary): Data dictionary for the edge in the
@@ -180,7 +186,9 @@ class CellDofFaceDofMap(object):
         """
         raise NotImplementedError("Method not implemented")
 
-    def assemble_int_bound_source(self, g, data, data_intf, cc, matrix, rhs, self_ind):
+    def assemble_int_bound_source(
+        self, sd, data, intf, data_intf, cc, matrix, rhs, self_ind
+    ):
         """Assemble the contribution from an internal boundary,
         manifested as a source term.
 
@@ -193,7 +201,7 @@ class CellDofFaceDofMap(object):
         the node and the mortar grid on the relevant edge.
 
         Parameters:
-            g (Grid): Grid which the condition should be imposed on.
+            sd (Grid): Grid which the condition should be imposed on.
             data (dictionary): Data dictionary for the node in the
                 mixed-dimensional grid.
             data_intf (dictionary): Data dictionary for the edge in the
@@ -210,14 +218,12 @@ class CellDofFaceDofMap(object):
                 Should be either 0 or 1.
 
         """
-        intf = data_intf["mortar_grid"]
-
         proj = intf.secondary_to_mortar_avg()
 
         cc[self_ind, 2] -= proj.T
 
     def assemble_int_bound_pressure_trace(
-        self, g, data, data_intf, cc, matrix, rhs, self_ind, use_secondary_proj
+        self, sd, data, data_intf, cc, matrix, rhs, self_ind, use_secondary_proj
     ):
         """Abstract method. Assemble the contribution from an internal
         boundary, manifested as a condition on the boundary pressure.
@@ -253,7 +259,7 @@ class CellDofFaceDofMap(object):
         raise NotImplementedError("Method not implemented")
 
     def assemble_int_bound_pressure_cell(
-        self, g, data, data_intf, cc, matrix, rhs, self_ind
+        self, sd, data, intf, data_intf, cc, matrix, rhs, self_ind
     ):
         """Assemble the contribution from an internal
         boundary, manifested as a condition on the cell pressure.
@@ -284,8 +290,6 @@ class CellDofFaceDofMap(object):
                 Should be either 0 or 1.
 
         """
-        intf = data_intf["mortar_grid"]
-
         proj = intf.secondary_to_mortar_avg()
 
         cc[2, self_ind] -= proj
