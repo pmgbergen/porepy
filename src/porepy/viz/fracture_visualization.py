2c1f73eb1e236e69e27033b6bcf553a96cc28e59
Jakub Both <jakub.both@uib.no>
2022-06-29 12:34:34 +0200

    STY: Add typing and more doc, fix mypy, black.

--- src/porepy/viz/fracture_visualization.py
+++ src/porepy/viz/fracture_visualization.py
@@ -1,21 +1,31 @@
 """
-Visualization tools for fracture networks. Plots 1d fractures as lines in a
+Visualization tools for fracture networks and wells. Plots 1d fractures as lines in a
 2d domain using pyplot. Also plots wells as points.
 """
 
+from typing import Optional
+
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 
 import porepy as pp
 
 
-def plot_fractures(pts, edges, domain=None, colortag=None, ax=None, **kwargs):
+def plot_fractures(
+    pts: np.ndarray,
+    edges: np.ndarray,
+    domain: Optional[dict] = None,
+    colortag: Optional[np.ndarray] = None,
+    ax: mpl.axis.Axis = None,
+    **kwargs
+) -> mpl.axis.Axis:
     """
     Plot 2d fractures as lines in a domain.
 
     The function is primarily intended for data exploration.
 
-    Parameters:
+    Args:
         pts (np.ndarray, dims 2 x npt): Coordinates of the fracture endpoints.
         edges (np.ndarray, dims 2 x n_edges): Indices of fracture start and
             endpoints.
@@ -26,15 +36,31 @@ def plot_fractures(pts, edges, domain=None, colortag=None, ax=None, **kwargs):
             asign to the different families. Defaults to all fractures being
             black.
         ax (matplotlib.axes.Axes, optional): If not given an axis, an axis will be created.
-        kwargs: Keyword arguments passed on to matplotlib.
+        kwargs (optional): Keyword arguments passed on to matplotlib.
+            fig_id: figure id
+            dpi:
+            plot: Boolean flag determining whether the plot is drawn to canvas
+            domain: Boolean flag determining whether the domain shall be plotted in red
+                (or white)
+            line_style: style of drawing lines
+            pts_coord: Boolean flag determining whether the point coordinates are plotted
+            axis_equal: Boolean flag determining whether both axes are treated equally
+            axis: Boolean flag determining whether the axes are conforming with domain
+            save: Boolean flag determining whether the figure is saved
 
     Returns:
         matplotlib.axes.Axes: The axis the fractures are plotted in
 
+    Raises:
+        ValueError if axis and figure id are provided both
+        ValueError if keywords dpi and ax are provided both
+
     """
+    # If not provided, determine the domain as bounding box
     if domain is None:
         domain = pp.bounding_box.from_points(pts)
 
+    # If no axis is provided, construct one
     if ax is None:
         plt.figure(kwargs.get("fig_id", 1), dpi=kwargs.get("dpi", 100))
         ax = plt.axes()
@@ -46,44 +72,54 @@ def plot_fractures(pts, edges, domain=None, colortag=None, ax=None, **kwargs):
         elif kwargs.get("dpi", None) is not None:
             raise ValueError("Cannot give both keyword argument 'dpi' and 'ax'")
         do_plot = kwargs.get("plot", False)
+
     # Assign a color to each tag. We define these by RBG-values (simplest
     # option in pyplot).
     # For the moment, some RBG values are hard coded, do something more
     # intelligent if necessary.
     if colortag is None:
         tagmap = np.zeros(edges.shape[1], dtype="int")
-        col = [(0, 0, 0)]
+        col = [(0.0, 0.0, 0.0)]
     else:
         utag, tagmap = np.unique(colortag, return_inverse=True)
         ntag = utag.size
         if ntag <= 3:
-            col = [(1, 0, 0), (0, 1, 0), (0, 0, 1)]
+            col = [(1.0, 0.0, 0.0), (0.0, 1.0, 0.0), (0.0, 0.0, 1.0)]
         elif ntag <= 6:
-            col = [(1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 0), (1, 0, 1), (0, 0, 1)]
+            col = [
+                (1.0, 0.0, 0.0),
+                (0.0, 1.0, 0.0),
+                (0.0, 0.0, 1.0),
+                (1.0, 1.0, 0.0),
+                (1.0, 0.0, 1.0),
+                (0.0, 0.0, 1.0),
+            ]
         elif ntag <= 12:
             # https://www.rapidtables.com/web/color/RGB_Color.html
             col = [
-                (1, 0, 0),
-                (0, 1, 0),
-                (0, 0, 1),
-                (1, 1, 0),
-                (1, 0, 1),
-                (0, 0, 1),
-                (0.5, 0, 0),
-                (0.5, 0.5, 0),
-                (0, 0.5, 0),
-                (0.5, 0, 0.5),
-                (0, 0.5, 0.5),
-                (0, 0, 0.5),
+                (1.0, 0.0, 0.0),
+                (0.0, 1.0, 0.0),
+                (0.0, 0.0, 1.0),
+                (1.0, 1.0, 0.0),
+                (1.0, 0.0, 1.0),
+                (0.0, 0.0, 1.0),
+                (0.5, 0.0, 0.0),
+                (0.5, 0.5, 0.0),
+                (0.0, 0.5, 0.0),
+                (0.5, 0.0, 0.5),
+                (0.0, 0.5, 0.5),
+                (0.0, 0.0, 0.5),
             ]
         else:
             col = plt.get_cmap("tab20")(np.mod(utag, 20))
 
+    # Fix the domain color
     if kwargs.get("domain", True):
         domain_color = "red"
     else:
         domain_color = "white"
 
+    # Plot the domain
     ax.plot(
         [
             domain["xmin"],
@@ -114,12 +150,14 @@ def plot_fractures(pts, edges, domain=None, colortag=None, ax=None, **kwargs):
             color=col[tagmap[i]],
         )
 
+    # Add point coordinates to the plot
     if kwargs.get("pts_coord", False):
         for i in range(pts.shape[1]):
             ax.text(
                 pts[0, i], pts[1, i], "(" + str(pts[0, i]) + ", " + str(pts[1, i]) + ")"
             )
 
+    # Set options for axes
     if kwargs.get("axis_equal", True):
         ax.axis("equal")
         ax.set_aspect("equal", adjustable="box")
@@ -139,20 +177,24 @@ def plot_fractures(pts, edges, domain=None, colortag=None, ax=None, **kwargs):
     return ax
 
 
-def plot_wells(d, w, colortag=None, **kwargs):
+def plot_wells(d: dict, w: np.ndarray, colortag: Optional[np.ndarray] = None, **kwargs):
     """
     Plot 2d wells as points in a domain.
 
     The function is primarily intended for data exploration.
 
-    Parameters:
-        d (dictionary): Domain size. Should contain fields xmin, xmax, ymin,
+    Args:
+        d (dict): Domain size. Should contain fields xmin, xmax, ymin,
             ymax.
         w (np.ndarray, dims 2 x npt): Coordinates of the wells.
         colortag (np.ndarray, dim n_w, optional): Colorcoding for wells.
             If provided, different colors will be asign to the different wells.
             Defaults to all wells being black.
         kwargs: Keyword arguments passed on to matplotlib.
+            plot: Boolean flag determining wether the figure is plotted
+
+    Raises:
+        NotImplementedError if more than 6 colors are requested
 
     """
 
@@ -173,6 +215,7 @@ def plot_wells(d, w, colortag=None, **kwargs):
         else:
             raise NotImplementedError("Have not thought of more than six colors")
 
+    # Setup figure and plot the domain
     plt.figure(kwargs.get("fig_id", 1))
     plt.axis([d["xmin"], d["xmax"], d["ymin"], d["ymax"]])
     plt.plot(
@@ -187,6 +230,6 @@ def plot_wells(d, w, colortag=None, **kwargs):
     for i, well in enumerate(w.T):
         plt.plot(*well, "o", color=col[tagmap[i]])
 
-    # Finally set axis
+    # Finally draw the plot
     if kwargs.get("plot", True):
         plt.show()
