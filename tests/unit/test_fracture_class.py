773fed5bafa4dac32d8bc3359cefa3f94032396e
Ivar Stefansson <ivar.stefansson@uib.no>
2022-07-04 12:44:58 +0200

    TST: Test Fracture2d

--- tests/unit/test_fracture_class.py
+++ tests/unit/test_fracture_class.py
@@ -5,19 +5,21 @@ import numpy as np
 import porepy as pp
 
 
-class TestFractureCenters(unittest.TestCase):
+class TestFractureGeometry(unittest.TestCase):
     """Test computation of fracture centroids."""
 
-    def test_frac_1(self):
+    def test_frac_3d_1(self):
         # Simple plane, known center.
         f_1 = pp.Fracture3d(
             np.array([[0, 2, 2, 0], [0, 2, 2, 0], [-1, -1, 1, 1]]),
             check_convexity=False,
         )
         c_known = np.array([1, 1, 0]).reshape((3, 1))
+        n_known = np.array([1, -1, 0]).reshape((1, 3))
         self.assertTrue(np.allclose(c_known, f_1.center))
+        self.assertTrue(np.allclose(np.cross(n_known, f_1.normal.T), 0))
 
-    def test_frac_2(self):
+    def test_frac_3d_2(self):
         # Center away from xy-plane
         f_1 = pp.Fracture3d(
             np.array([[0, 2, 2, 0], [0, 2, 2, 0], [0, 0, 1, 1]]), check_convexity=False
@@ -25,7 +27,7 @@ class TestFractureCenters(unittest.TestCase):
         c_known = np.array([1, 1, 0.5]).reshape((3, 1))
         self.assertTrue(np.allclose(c_known, f_1.center))
 
-    def test_frac_3(self):
+    def test_frac_3d_3(self):
         # Fracture plane defined by x + y + z = 1
         f_1 = pp.Fracture3d(
             np.array([[0, 1, 1, 0], [0, 0, 1, 1], [1, 0, -1, 0]]), check_convexity=False
@@ -33,7 +35,7 @@ class TestFractureCenters(unittest.TestCase):
         c_known = np.array([0.5, 0.5, 0]).reshape((3, 1))
         self.assertTrue(np.allclose(c_known, f_1.center))
 
-    def test_frac_4(self):
+    def test_frac_3d_4(self):
         # Fracture plane defined by x + y + z = 4
         f_1 = pp.Fracture3d(
             np.array([[0, 1, 1, 0], [0, 0, 1, 1], [4, 3, 2, 3]]), check_convexity=False
@@ -41,7 +43,7 @@ class TestFractureCenters(unittest.TestCase):
         c_known = np.array([0.5, 0.5, 3]).reshape((3, 1))
         self.assertTrue(np.allclose(c_known, f_1.center))
 
-    def test_frac_rand(self):
+    def test_frac_3d_rand(self):
         # Random normal vector.
         r = np.random.rand(4)
         x = np.array([0, 1, 1, 0])
@@ -52,6 +54,15 @@ class TestFractureCenters(unittest.TestCase):
         c_known = np.array([0.5, 0.5, z_cc]).reshape((3, 1))
         self.assertTrue(np.allclose(c_known, f.center))
 
+    def test_frac_2d(self):
+        # Simple line, known center.
+        f_1 = pp.Fracture2d(
+            np.array([[0, 2], [0, 2]]),
+        )
+        c_known = np.array([1, 1]).reshape((2, 1))
+        n_known = np.array([1, -1]).reshape((1, 2))
+        self.assertTrue(np.allclose(c_known, f_1.center))
+        self.assertTrue(np.allclose(np.cross(n_known, f_1.normal.T), 0))
 
 class TestFractureIndex(unittest.TestCase):
     """Test index attribute of Fracture.
@@ -78,6 +89,13 @@ class TestFractureIndex(unittest.TestCase):
         f1 = pp.Fracture3d(self.array(), index=1, check_convexity=False)
         f2 = pp.Fracture3d(self.array(), check_convexity=False)
         self.assertTrue(f1 != f2)
+
+    def test_2d(self):
+        f1 = pp.Fracture2d(self.array()[:2, :2], index=1, check_convexity=False)
+        f2 = pp.Fracture2d(self.array()[:2, :2], index=1, check_convexity=False)
+        f3 = pp.Fracture2d(self.array()[:2, :2], index=4, check_convexity=False)
+        self.assertTrue(f1 == f2)
+        self.assertTrue(f1 != f3)
 
 
 class TestFractureIsVertex(unittest.TestCase):
