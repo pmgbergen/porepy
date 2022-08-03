8a40823ba5c4220ed897fdc18491d431c1d27aae
Veljko Lipovac <lipovac.v@outlook.com>
2022-08-03 15:23:46 +0200

    MIN: changes to more consistent return types. Minor changes in variable allocations. Refactoring of some signatures. STY: Changing to Google docstring style. Adding extensive documentation for intended usage.

--- src/porepy/numerics/ad/operators.py
+++ src/porepy/numerics/ad/operators.py
@@ -444,7 +444,7 @@ class Operator:
             func_op = results[0]
 
             # if the callable can be fed with Ad_arrays, do it
-            if func_op.is_adarray_func:
+            if func_op.ad_compatible:
                 return func_op.func(*results[1:])
             else:
                 # This should be a Function with approximated Jacobian and value.
