6f30cbf9d3d334569c12619f2163bbae411939d0
Jakub Both <jakub.both@uib.no>
2022-08-25 15:15:49 +0200

    TST: Use parametrization from pytest.

--- tests/unit/test_vtk.py
+++ tests/unit/test_vtk.py
@@ -21,6 +21,11 @@ from deepdiff import DeepDiff
 
 import porepy as pp
 
+# Globally store location of reference files
+folder_reference = (
+    os.path.dirname(os.path.realpath(__file__)) + "/" + "test_vtk_reference"
+)
+
 
 class ExporterTestSetup:
     """Class to define where to store vtu files, and test the export functionality
@@ -31,10 +36,8 @@ class ExporterTestSetup:
 
         # Define ingredients of where to store vtu files for exporting during testing.
         self.folder = "./test_vtk/"
-        self.folder_reference = (
-            os.path.dirname(os.path.realpath(__file__)) + "/" + "test_vtk_reference"
-        )
         self.file_name = "grid"
+        self.folder_reference = folder_reference
 
 
 @pytest.fixture
@@ -46,7 +49,10 @@ def setup():
     yield setup
 
     # Teardown
-    shutil.rmtree(os.getcwd() + "/" + os.path.basename(os.path.normpath(setup.folder)))
+    shutil.rmtree(
+        os.getcwd() + "/" + os.path.basename(os.path.normpath(setup.folder)),
+        ignore_errors=True,
+    )
 
 
 def _compare_vtu_files(test_file: str, reference_file: str) -> bool:
@@ -77,87 +83,92 @@ def _compare_vtu_files(test_file: str, reference_file: str) -> bool:
     return diff == {}
 
 
-def test_single_subdomains(setup):
+# Helper for parametrization of test_single_subdomains. Define collection of
+# single subdomains incl. 1d, 2d, 3d grids, of simplicial, Cartesian and
+# polytopal element type.
+
+# Construct 2d polytopal grid
+sd_polytop_2d = pp.StructuredTriangleGrid([2] * 2, [1] * 2)
+sd_polytop_2d.compute_geometry()
+pp.coarsening.generate_coarse_grid(sd_polytop_2d, [0, 1, 3, 3, 1, 1, 2, 2])
+
+# Construct 3d polytopal grid
+sd_polytop_3d = pp.CartGrid([3, 2, 3], [1] * 3)
+sd_polytop_3d.compute_geometry()
+pp.coarsening.generate_coarse_grid(
+    sd_polytop_3d, [0, 0, 1, 0, 1, 1, 0, 2, 2, 3, 2, 2, 4, 4, 4, 4, 4, 4]
+)
+
+# Define data type for a single subdomain
+SingleSubdomain = namedtuple("SingleSubdomain", ["grid", "ref_vtu_file"])
+
+# Define the collection of subdomains
+single_subdomains = [
+    # 1d grid
+    SingleSubdomain(
+        pp.CartGrid(3, 1),
+        f"{folder_reference}/single_subdomain_1d.vtu",
+    ),
+    # 2d simplex grid
+    SingleSubdomain(
+        pp.StructuredTriangleGrid([3] * 2, [1] * 2),
+        f"{folder_reference}/single_subdomain_2d_simplex_grid.vtu",
+    ),
+    # 2d Cartesian grid
+    SingleSubdomain(
+        pp.CartGrid([4] * 2, [1] * 2),
+        f"{folder_reference}/single_subdomain_2d_cart_grid.vtu",
+    ),
+    # 2d polytopal grid
+    SingleSubdomain(
+        sd_polytop_2d,
+        f"{folder_reference}/single_subdomain_2d_polytop_grid.vtu",
+    ),
+    # 3d simplex grid
+    SingleSubdomain(
+        pp.StructuredTetrahedralGrid([3] * 3, [1] * 3),
+        f"{folder_reference}/single_subdomain_3d_simplex_grid.vtu",
+    ),
+    # 3d Cartesian grid
+    SingleSubdomain(
+        pp.CartGrid([4] * 3, [1] * 3),
+        f"{folder_reference}/single_subdomain_3d_cart_grid.vtu",
+    ),
+    # 3d polytopal grid
+    SingleSubdomain(
+        sd_polytop_3d,
+        f"{folder_reference}/single_subdomain_3d_polytop_grid.vtu",
+    ),
+]
+
+
+@pytest.mark.parametrize("subdomain", single_subdomains)
+def test_single_subdomains(setup, subdomain):
     # Test of the Exporter for single subdomains of different dimensionality,
     # and different grid type. Exporting of scalar and vectorial data is tested.
 
-    # Prepare polytopal grids
-    sd_polytop_2d = pp.StructuredTriangleGrid([2] * 2, [1] * 2)
-    sd_polytop_2d.compute_geometry()
-    pp.coarsening.generate_coarse_grid(sd_polytop_2d, [0, 1, 3, 3, 1, 1, 2, 2])
+    # Define grid
+    sd = subdomain.grid
+    sd.compute_geometry()
 
-    sd_polytop_3d = pp.CartGrid([3, 2, 3], [1] * 3)
-    sd_polytop_3d.compute_geometry()
-    pp.coarsening.generate_coarse_grid(
-        sd_polytop_3d, [0, 0, 1, 0, 1, 1, 0, 2, 2, 3, 2, 2, 4, 4, 4, 4, 4, 4]
+    # Define data
+    dummy_scalar = np.ones(sd.num_cells) * sd.dim
+    dummy_vector = np.ones((3, sd.num_cells)) * sd.dim
+
+    # Export data
+    save = pp.Exporter(
+        sd,
+        setup.file_name,
+        setup.folder,
+        export_constants_separately=False,
     )
+    save.write_vtu([("dummy_scalar", dummy_scalar), ("dummy_vector", dummy_vector)])
 
-    # Define collection of single subdomains
-    SingleSubdomain = namedtuple("SingleSubdomain", ["grid", "ref_vtu_file"])
-
-    single_subdomains = [
-        # 1d grid
-        SingleSubdomain(
-            pp.CartGrid(3, 1),
-            f"{setup.folder_reference}/single_subdomain_1d.vtu",
-        ),
-        # 2d simplex grid
-        SingleSubdomain(
-            pp.StructuredTriangleGrid([3] * 2, [1] * 2),
-            f"{setup.folder_reference}/single_subdomain_2d_simplex_grid.vtu",
-        ),
-        # 2d Cartesian grid
-        SingleSubdomain(
-            pp.CartGrid([4] * 2, [1] * 2),
-            f"{setup.folder_reference}/single_subdomain_2d_cart_grid.vtu",
-        ),
-        # 2d polytopal grid
-        SingleSubdomain(
-            sd_polytop_2d,
-            f"{setup.folder_reference}/single_subdomain_2d_polytop_grid.vtu",
-        ),
-        # 3d simplex grid
-        SingleSubdomain(
-            pp.StructuredTetrahedralGrid([3] * 3, [1] * 3),
-            f"{setup.folder_reference}/single_subdomain_3d_simplex_grid.vtu",
-        ),
-        # 3d Cartesian grid
-        SingleSubdomain(
-            pp.CartGrid([4] * 3, [1] * 3),
-            f"{setup.folder_reference}/single_subdomain_3d_cart_grid.vtu",
-        ),
-        # 3d polytopal grid
-        SingleSubdomain(
-            sd_polytop_3d,
-            f"{setup.folder_reference}/single_subdomain_3d_polytop_grid.vtu",
-        ),
-    ]
-
-    # Test each subdomain separately
-    for subdomain in single_subdomains:
-
-        # Define grid
-        sd = subdomain.grid
-        sd.compute_geometry()
-
-        # Define data
-        dummy_scalar = np.ones(sd.num_cells) * sd.dim
-        dummy_vector = np.ones((3, sd.num_cells)) * sd.dim
-
-        # Export data
-        save = pp.Exporter(
-            sd,
-            setup.file_name,
-            setup.folder,
-            export_constants_separately=False,
-        )
-        save.write_vtu([("dummy_scalar", dummy_scalar), ("dummy_vector", dummy_vector)])
-
-        # Check that exported vtu file and reference file are the same
-        assert _compare_vtu_files(
-            f"{setup.folder}/{setup.file_name}_{sd.dim}.vtu",
-            f"{subdomain.ref_vtu_file}",
-        )
+    # Check that exported vtu file and reference file are the same
+    assert _compare_vtu_files(
+        f"{setup.folder}/{setup.file_name}_{sd.dim}.vtu",
+        f"{subdomain.ref_vtu_file}",
+    )
 
 
 # NOTE: Suggest removing this test.
