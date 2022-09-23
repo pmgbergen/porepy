073fddeeb6a4a824d6f4068a1a7756c8a0c63042
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2022-09-23 15:47:05 +0200

    ENH: Upgrade of adaptive interpolation table
    The underlying data structure is now actually sparse. Also improved testing and documentation.

--- tests/unit/test_interpolation_tables.py
+++ tests/unit/test_interpolation_tables.py
@@ -61,47 +61,72 @@ class _QuadraticFunc:
 def test_interpolation(dim, factory):
     low = np.random.rand(dim)
     high = 2 + np.random.rand(dim)
-    resolution = np.array([4 + d for d in range(dim)])
-    max_size = (high - low) / resolution
+    low = np.zeros(dim)
+    high = 2 * np.ones(dim)
+    num_pt = np.array([4 + d for d in range(dim)])
+    max_size = (high - low) / (num_pt - 1)
     function = factory(dim, max_size)
 
-    table = pp.InterpolationTable(low, high, resolution, function.func())
+    # We have occationally had issues with these tests failing on random data, however,
+    # subsequent testing (on huge arrays) failed to uncover any issues. Fix the random
+    # seed so that if errors arise, they should be reproducible.
+    # TODO: We should rather use hypothesis for this part.
+    np.random.seed(42)
 
+    # Create interpolation tables
+    table = pp.InterpolationTable(low, high, num_pt, function.func())
     adaptive_table = pp.AdaptiveInterpolationTable(
-        low, high, resolution, function.func()
+        dx = (high - low) / (num_pt - 1), base_point = low, function = function.func(), dim=1
+    )
+    
+    coord = np.vstack(table._coord)
+    
+    adaptive_vals = adaptive_table.interpolate(coord)
+    table_vals = table.interpolate(coord)
+    known_vals = function.truth(coord)
+    
+    assert np.allclose(table_vals, adaptive_vals, atol=1e-10)
+    assert np.allclose(known_vals, adaptive_vals, atol=1e-10)
+
+    # Create a new adaptive table, so that we can evaluate it on a rather sparse point
+    # set and check that we have not used all the quadrature points.
+    adaptive_table = pp.AdaptiveInterpolationTable(
+        dx = (high - low) / (num_pt - 1), base_point = low, function = function.func(), dim=1
     )
 
-    npt = 100
+    npt = 10
 
     pts = low.reshape((-1, 1)) + np.random.rand(dim, npt) * (high - low).reshape(
         (-1, 1)
     )
 
-    vals = table.interpolate(pts)
     adaptive_vals = adaptive_table.interpolate(pts)
+    table_vals = table.interpolate(pts                                 )
 
     # Check that the adaptive table has used fewer points than the full version
     # Worst case scenario is that all evaluation points trigger a new
-    # evaluation.
-    num_quad_pts = adaptive_table._has_value.sum()
-    assert num_quad_pts <= npt * np.power(2, dim)
+    # evaluation, or that all points have been evaluated.
+    num_quad_pts = adaptive_table._values.size
+    assert num_quad_pts <= np.minimum(npt * np.power(2, dim), table._values.shape[-1])
 
-    known_val = function.truth(pts)
+    known_vals = function.truth(pts)
 
     # Function values should be reproduced with sufficient accuracy
-    assert np.max(np.abs(vals - known_val)) < function.tol
+    assert np.max(np.abs(table_vals - known_vals)) < function.tol
     # The two interpolation approaches should be identical
-    assert np.allclose(vals, adaptive_vals, atol=1e-10)
+    assert np.allclose(table_vals, adaptive_vals, atol=1e-10)
 
     for d in range(dim):
         diff = table.diff(pts, axis=d)
         adaptive_diff = adaptive_table.diff(pts, axis=d)
 
         # It should not be necessary to compute new function values
-        assert adaptive_table._has_value.sum() == num_quad_pts
+        assert adaptive_table._values.size == num_quad_pts
 
         known_diff = function.diff(pts, d)
         # Note that for the time being, this test is not active for the
         # Quadractic function (due to diff_tol)
         assert np.max(np.abs(diff - known_diff)) < function.diff_tol
         assert np.allclose(diff, adaptive_diff, atol=1e-10)
+
+test_interpolation(4, _QuadraticFunc)
