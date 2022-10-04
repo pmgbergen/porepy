b548674d71259b8925c8999d72be59a2f0da84a6
Jhabriel Varela <jhabriel@gmail.com>
2022-10-04 16:06:51 +0200

    MAINT: Use `tsc` in `_run_iterative_model()`

--- src/porepy/models/run_models.py
+++ src/porepy/models/run_models.py
@@ -1,6 +1,5 @@
-""" This module contains functions to run stationary and time-dependent models.
+""" This module contains functions to run stationary and time-dependent models."""
 
-"""
 import logging
 
 import porepy as pp
@@ -36,11 +35,13 @@ def run_time_dependent_model(model, params):
     if params.get("prepare_simulation", True):
         model.prepare_simulation()
 
-    # Prepare for the time loop
+    # Assign a solver
     if model._is_nonlinear_problem():
         solver = pp.NewtonSolver(params)
     else:
         solver = pp.LinearSolver(params)
+
+    # Time loop
     while model.tsc.time < model.tsc.time_final:
         model.tsc.increase_time()
         model.tsc.increase_time_index()
@@ -58,28 +59,29 @@ def run_time_dependent_model(model, params):
 def _run_iterative_model(model, params):
     """Intended use is for multi-step models with iterative couplings.
 
-    Only known instance so far is the combination of fracture deformation
-    and propagation.
+    Only known instance so far is the combination of fracture deformation and propagation.
+
     """
+
     # Assign parameters, variables and discretizations. Discretize time-indepedent terms
     if params.get("prepare_simulation", True):
         model.prepare_simulation()
 
-    # Prepare for the time loop
-    t_end = model.end_time
-    model.time_index = 0
+    # Assing a solver
     if model._is_nonlinear_problem():
         solver = pp.NewtonSolver(params)
     else:
         solver = pp.LinearSolver(params)
-    while model.time < t_end:
+
+    # Time loop
+    while model.tsc.time < model.tsc.time_final:
         model.propagation_index = 0
-        model.time += model.time_step
-        model.time_index += 1
+        model.tsc.increase_time()
+        model.tsc.increase_time_index()
         model.before_propagation_loop()
         logger.info(
             "\nTime step {} at time {:.1e} of {:.1e} with time step {:.1e}".format(
-                model.time_index, model.time, t_end, model.time_step
+                model.tsc.time_index, model.tsc.time, model.tsc.time_final, model.tsc.dt
             )
         )
         while model.keep_propagating():
@@ -87,4 +89,5 @@ def _run_iterative_model(model, params):
 
             solver.solve(model)
         model.after_propagation_loop()
+
     model.after_simulation()
