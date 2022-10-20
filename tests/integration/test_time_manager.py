debd14f616e5918c754364a412683bd1bcc26a98
Jhabriel Varela <11464732+jhabriel@users.noreply.github.com>
2022-10-20 21:33:51 +0200

    TST: Add integration test for constant dt

--- tests/integration/test_time_manager.py
+++ tests/integration/test_time_manager.py
@@ -7,31 +7,70 @@ import porepy as pp
 import numpy as np
 import pytest
 
+# Model classes to be tested
+all_models = [
+    pp.IncompressibleFlow,
+    pp.SlightlyCompressibleFlow,
+    pp.ContactMechanics,
+    pp.ContactMechanicsBiot,
+    pp.THM
+]
 
-class TestDefaultBehaviour:
-    """Class that checks the default behaviour of the models"""
+time_dependent_models = [
+    pp.SlightlyCompressibleFlow,
+    pp.ContactMechanicsBiot,
+    pp.THM
+]
 
-    default_models = [
-        pp.IncompressibleFlow(params={"use_ad": True}),
-        pp.SlightlyCompressibleFlow(params={"use_ad": True}),
-        pp.ContactMechanics(params={"use_ad": True}),
-        pp.ContactMechanicsBiot(params={"use_ad": True}),
-        pp.THM(params={"use_ad": True}),
-    ]
 
-    @pytest.mark.parametrize("model", default_models)
-    def test_default_behaviour(self, model):
-        """Checks default time behaviour of models"""
+# -----> Testing defaults
+@pytest.mark.parametrize("model_class", all_models)
+def test_default_behaviour(model_class):
+    """Test the default behaviour of the models"""
 
-        # Time-independent models should not have a time manager
-        if not model._is_time_dependent():
-            pp.run_stationary_model(model, model.params)
-            assert not hasattr(model, "time_manager")
-        # Time-dependent models should return default values after running the models
-        else:
-            pp.run_time_dependent_model(model, model.params)
-            assert model.time_manager.time_init == 0
-            assert model.time_manager.time_final == 1
-            assert model.time_manager.time == 1
-            assert model.time_manager.dt == 1
-            
+    model = model_class(params={"use_ad": True})
+
+    if not model._is_time_dependent():
+        pp.run_stationary_model(model, model.params)
+        assert not hasattr(model, "time_manager")
+    else:
+        pp.run_time_dependent_model(model, model.params)
+        assert model.time_manager.is_constant
+        assert model.time_manager.time_init == 0
+        assert model.time_manager.time_final == 1
+        assert model.time_manager.time == 1
+        assert model.time_manager.dt == 1
+        assert model._ad.time_step.evaluate(model.dof_manager) == model.time_manager.dt
+
+
+# -----> Testing constant time step
+tm0 = pp.TimeManager([0, 1, 2], 1, constant_dt=True)
+tm1 = pp.TimeManager([0, 0.01, 0.02, 0.05], 0.01, constant_dt=True)
+tm2 = pp.TimeManager([0, pp.HOUR, 2*pp.HOUR, 10*pp.HOUR], pp.HOUR, constant_dt=True)
+
+
+@pytest.mark.parametrize("model_class", time_dependent_models)
+@pytest.mark.parametrize("time_manager", [tm0, tm1, tm2])
+def test_constant_time_step(request, model_class, time_manager):
+    """Test models when constant time step is used"""
+
+    model = model_class(params={"use_ad": True, "time_manager": time_manager})
+    pp.run_time_dependent_model(model, model.params)
+    if "0" in request.node.callspec.id:
+        assert model.time_manager.is_constant
+        assert model.time_manager.time_init == 0
+        assert model.time_manager.time_final == 2
+        assert model.time_manager.dt == 1
+        assert model.time_manager.time == 2
+    elif "1" in request.node.callspec.id:
+        assert model.time_manager.is_constant
+        assert model.time_manager.time_init == 0
+        assert model.time_manager.time_final == 0.05
+        assert model.time_manager.dt == 0.01
+        assert model.time_manager.time == 0.05
+    elif "2" in request.node.callspec.id:
+        assert model.time_manager.is_constant
+        assert model.time_manager.time_init == 0
+        assert model.time_manager.time_final == 10 * pp.HOUR
+        assert model.time_manager.dt == pp.HOUR
+        assert model.time_manager.time == 10 * pp.HOUR
