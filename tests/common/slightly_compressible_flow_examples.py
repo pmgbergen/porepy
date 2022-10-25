c44d5e0a227b323f3a2127d521a1d1b60cf0cdc5
Jhabriel Varela <11464732+jhabriel@users.noreply.github.com>
2022-10-25 23:37:55 +0200

    TST: Model with pressure-dependent porosity in place

--- tests/common/slightly_compressible_flow_examples.py
+++ tests/common/slightly_compressible_flow_examples.py
@@ -3,8 +3,9 @@ This module contains examples of slightly compressible flow setups.
 """
 import porepy as pp
 import numpy as np
+import sympy as sym
 
-from typing import Union
+from typing import Callable, Union
 
 
 class NonLinearSCF(pp.SlightlyCompressibleFlow):
@@ -43,6 +44,16 @@ class NonLinearSCF(pp.SlightlyCompressibleFlow):
         sd.compute_geometry()
         self.mdg = pp.meshing.subdomains_to_mdg([[sd]])
 
+
+    def _reference_porosity(self):
+        return self.params.get("reference_porosity", 0.5)
+
+    def _reference_pressure(self):
+        return self.params.get("reference_pressure", 0.0)
+
+    def _specific_storativity(self):
+        return self.params.get("specific_storativity", 1E-1)
+
     def _porosity(
             self, p: Union[pp.ad.Ad_array, np.ndarray]
         ) -> Union[pp.ad.Ad_array, np.ndarray]:
@@ -57,9 +68,9 @@ class NonLinearSCF(pp.SlightlyCompressibleFlow):
 
         """
 
-        phi0 = 0.2  # Reference porosity
-        p0 = 1.0  # Reference pressure
-        c0 = 1E-03  # Compressibility
+        phi0 = self._reference_porosity()
+        p0 = self._reference_pressure()
+        c0 = self._specific_storativity()
 
         if isinstance(p, pp.ad.Ad_array):
             phi = phi0 * pp.ad.exp(c0 * (p - p0))
@@ -70,11 +81,41 @@ class NonLinearSCF(pp.SlightlyCompressibleFlow):
 
         return phi
 
+    def before_newton_loop(self) -> None:
+
+        super().before_newton_loop()
+
+        # Update source term
+        sd: pp.Grid = self.mdg.subdomains()[0]
+        data: dict = self.mdg.subdomain_data(sd)
+        vols: np.ndarray = sd.cell_volumes
+        cc: np.ndarray = sd.cell_centers
+        f: Callable = self.exact_source()
+        t: float = self.time_manager.time
+        source = vols * f(cc[0], cc[1], t)
+        data[pp.PARAMETERS][self.parameter_key]["source"] = source
+
     def before_newton_iteration(self) -> None:
         pass
 
     def after_newton_iteration(self, sol) -> None:
-        pass
+
+        # Distribute variables to iterate
+        self.dof_manager.distribute_variable(
+            values=sol,
+            grids=self.mdg.subdomains(),
+            variables=self.variable,
+            additive=True,
+            to_iterate=True
+        )
+
+    def after_newton_convergence(
+        self, solution: np.ndarray, errors: float, iteration_counter: int
+    ) -> None:
+        super().after_newton_convergence(solution, errors, iteration_counter)
+
+        # Distribute variables to state
+        self.dof_manager.transfer_variable(from_iterate_to_state=True)
 
     def _assign_equations(self) -> None:
         """Upgrade incompressible flow equations to non-linear slightly compressible
@@ -103,19 +144,46 @@ class NonLinearSCF(pp.SlightlyCompressibleFlow):
         self._eq_manager.equations["subdomain_flow"] += accumulation_term
 
     def after_simulation(self):
-        sd = self.mdg.subdomains()[0]
-        data = self.mdg.subdomain_data(sd)
-        p = data[pp.STATE][self.variable]
-        pp.plot_grid(sd, p, plot_2d=True)
+        if self.params.get("plot_sol", False):
+            sd = self.mdg.subdomains()[0]
+            data = self.mdg.subdomain_data(sd)
+            p = data[pp.STATE][self.variable]
+            pp.plot_grid(sd, p, plot_2d=True)
 
     def _is_nonlinear_problem(self) -> bool:
         """Specifies whether the Model problem is nonlinear."""
         return True
 
+    def exact_source(self) -> Callable:
+
+        # Declare symbolic variables
+        x, y, t = sym.symbols("x y t")
+
+        # Physical parameters for the model
+        phi0 = self._reference_porosity()
+        p0 = self._reference_pressure()
+        c0 = self._specific_storativity()
+
+        # Determine exact source
+        p_sym = t * x * (1 - x) * y * (1 - y)
+        q_sym = [-sym.diff(p_sym, x), -sym.diff(p_sym, y)]
+        divq_sym = sym.diff(q_sym[0], x) + sym.diff(q_sym[1], y)
+        phi_sym = phi0 * sym.exp(c0 * (p_sym - p0))
+        dphidt_sym = sym.diff(phi_sym, t)
+        f_sym = dphidt_sym + divq_sym
+        f_fun = sym.lambdify((x, y, t), f_sym, "numpy")
+
+        return f_fun
+
 
 #%% Runner
-time_manager = pp.TimeManager(schedule=[0, 10, 20], dt_init=1, constant_dt=True)
-params = {"use_ad": True, "num_cells": 20, "time_manager": time_manager}
+time_manager = pp.TimeManager(
+    schedule=[0, 10],
+    dt_init=0.1,
+    dt_min_max=(0.1, 6),
+    print_info=True
+)
+params = {"use_ad": True, "num_cells": 20, "time_manager": time_manager, "plot_sol": True}
 model = NonLinearSCF(params=params)
 pp.run_time_dependent_model(model, params)
 
