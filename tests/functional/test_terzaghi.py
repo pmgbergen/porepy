1e3c4892359ab74a00178e5f81c809b03e901233
Jhabriel Varela <11464732+jhabriel@users.noreply.github.com>
2022-11-06 02:46:12 +0100

    TST: Add functional test for Terzaghi's problem

--- tests/functional/test_terzaghi.py
+++ tests/functional/test_terzaghi.py
@@ -4,34 +4,43 @@ This module contains light-weighted functional tests for Terzaghi's consolidatio
 
 import porepy as pp
 import numpy as np
-import pytest
 
 from porepy.models.applications.terzaghi_model import Terzaghi
 
 
-# -----> Test pressure error
-def test_pressure_error():
-    """Checks L2-discrete relative error for the pressure solution"""
+def test_pressure_and_consolidation_degree():
+    """Checks error for pressure solution and degree of consolidation"""
+
     model_params = {
-        'alpha_biot': 1.0,  # [-]
-        'height': 1.0,  # [m]
-        'lambda_lame': 1.65E9,  # [Pa]
-        'mu_lame': 1.475E9,  # [Pa]
-        'num_cells': 20,
-        'permeability': 9.86E-14,  # [m^2]
-        'perturbation_factor': 1E-6,
-        'plot_results': False,
-        'specific_weight': 9.943E3,  # [Pa * m^-1]
-        'time_manager': pp.TimeManager([0, 0.05, 0.1, 0.3], 0.05, constant_dt=True),
-        'upper_limit_summation': 1000,
-        'use_ad': True,
-        'vertical_load': 6E8,  # [N * m^-1]
-        'viscosity': 1E-3,  # [Pa * s]
+        "alpha_biot": 1.0,  # [-]
+        "height": 1.0,  # [m]
+        "lambda_lame": 1.65e9,  # [Pa]
+        "mu_lame": 1.475e9,  # [Pa]
+        "num_cells": 10,
+        "permeability": 9.86e-14,  # [m^2]
+        "perturbation_factor": 1e-6,
+        "plot_results": False,
+        "specific_weight": 9.943e3,  # [Pa * m^-1]
+        "time_manager": pp.TimeManager([0, 0.05, 0.1, 0.3], 0.05, constant_dt=True),
+        "upper_limit_summation": 1000,
+        "use_ad": True,
+        "vertical_load": 6e8,  # [N * m^-1]
+        "viscosity": 1e-3,  # [Pa * s]
     }
     model = Terzaghi(params=model_params)
     pp.run_time_dependent_model(model, model.params)
-    desired = [0, 0.059455475183486366, 0.04111629146957893, 0.020505285438958992]
-    actual = [model.sol[key]["p_error"] for key in model.sol.keys()]
-    np.testing.assert_array_almost_equal(actual, desired, 1E-6)
 
+    # Check pressure error
+    desired_p = [0.0, 0.06884914805129305, 0.04553528965962772, 0.022392040856504078]
+    actual_p = [sol.pressure_error for sol in model.solutions]
+    np.testing.assert_allclose(actual_p, desired_p, rtol=1e-3, atol=1e-5)
 
+    # Check consolidation degree
+    desired_consol = [
+        0.0,
+        0.02700528627244403,
+        0.01883909437589315,
+        0.010927389539718946,
+    ]
+    actual_consol = [sol.consolidation_degree_error for sol in model.solutions]
+    np.testing.assert_allclose(actual_consol, desired_consol, rtol=1e-3, atol=1e-5)
