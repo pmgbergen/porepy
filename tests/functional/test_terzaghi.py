8e6aec6e15dd323993895b089833a533f7bc1121
Jhabriel Varela <jhabriel@gmail.com>
2022-11-08 12:32:38 +0100

    DOC: Improve documentation of test

--- tests/functional/test_terzaghi.py
+++ tests/functional/test_terzaghi.py
@@ -11,11 +11,26 @@ from porepy.models.applications.terzaghi_model import Terzaghi
 def test_pressure_and_consolidation_degree():
     """Checks error for pressure solution and degree of consolidation.
 
-    Model parameters have been taken from [1].
+    Physical parameters used in this test have been adapted from [1].
 
+    Tests should pass as long as a `desired_error` matches the `actual_error` up to
+    absolute and relative tolerances. In this particular case, we are comparing pressure
+    errors (measured using the L2-discrete relative error given in [2]) and degree of
+    consolidation errors (measured in absolute terms). The `desired_error` was obtained
+    by copying the simulation results at the time this test was written.
+
+    For this test, we require an absolute tolerance of 1e-5 and a relative tolerance of
+    1e-3 between the `desired_error` and the `actual_error`. The choice of such tolerances
+    aim at keeping the test meaningful while minimizing the chances of failure due to
+    floating-point arithmetic close to machine precision.
+
+    References:
     [1] Mikelić, A., Wang, B., & Wheeler, M. F. (2014). Numerical convergence study of
         iterative coupling for coupled flow and geomechanics. Computational Geosciences,
         18(3), 325-341.
+    [2] Nordbotten, J. M. (2016). Stable cell-centered finite volume discretization for
+        Biot equations. SIAM Journal on Numerical Analysis, 54(2), 942-968.
+
     """
 
     model_params = {
@@ -47,7 +62,7 @@ def test_pressure_and_consolidation_degree():
     actual_error_p = [sol.pressure_error for sol in model.solutions]
     np.testing.assert_allclose(actual_error_p, desired_error_p, rtol=1e-3, atol=1e-5)
 
-    # Check consolidation degree
+    # Check consolidation degree error
     desired_error_consol = [
         0.0,
         0.02700528627244403,
