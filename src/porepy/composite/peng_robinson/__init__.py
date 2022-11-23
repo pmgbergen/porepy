506a7737629977885137760f9bb25db7580a5df8
Veljko Lipovac <lipovac.v@outlook.com>
2022-11-23 14:24:14 +0100

    MOD: Added BIPs to peng-robinson sub-package init.

--- src/porepy/composite/peng_robinson/__init__.py
+++ src/porepy/composite/peng_robinson/__init__.py
@@ -7,14 +7,17 @@ References:
     [2]: `Ben Gharbia et al. (2021) <https://doi.org/10.1051/m2an/2021075>`_
     [3]: `Driesner (2007) Part I <https://doi.org/10.1016/j.gca.2006.01.033>`_
     [4]: `Driesner (2007) Part II <https://doi.org/10.1016/j.gca.2007.05.026>`_
+    [5]: `Soereide (1992) <https://doi.org/10.1016/0378-3812(92)85105-H>`_
 
 """
 
 __all__ = []
 
-from . import model_components, pr_composition
+from . import model_components, pr_composition, bip
 from .model_components import *
 from .pr_composition import *
+from .bip import *
 
 __all__.extend(model_components.__all__)
 __all__.extend(pr_composition.__all__)
+__all__.extend(bip.__all__)
