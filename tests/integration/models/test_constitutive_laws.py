e56628d8f543717174fc94d04b4e7dada5def9e2
Ivar Stefansson <ivar.stefansson@uib.no>
2022-12-07 17:27:51 +0100

    TST: Prepare test for values of constitutive laws

--- tests/integration/models/test_constitutive_laws.py
+++ tests/integration/models/test_constitutive_laws.py
@@ -9,70 +9,69 @@ compound classes contain the specified constitutive laws, thus revealing mistake
 the classes are refactored.
 
 """
-from inspect import signature
-
+import numpy as np
 import pytest
 
 import porepy as pp
 
-from .setup_utils import model
+from . import setup_utils
 
 
 @pytest.mark.parametrize(
     "model_type,method_name,domain_dimension",
     [  # Fluid mass balance
-        ("mass_balance", "bc_values_darcy_flux", []),
-        ("mass_balance", "bc_values_mobrho", []),
-        ("mass_balance", "fluid_viscosity", []),
-        ("mass_balance", "fluid_source", []),
-        ("mass_balance", "mobility", []),
-        ("mass_balance", "fluid_density", []),
-        ("mass_balance", "aperture", []),
-        ("mass_balance", "specific_volume", []),
-        ("mass_balance", "darcy_flux", []),
-        ("mass_balance", "interface_fluid_flux", []),
-        ("mass_balance", "fluid_flux", []),
-        ("mass_balance", "pressure_trace", []),
-        ("mass_balance", "porosity", []),
-        ("mass_balance", "reference_pressure", []),
+        ("mass_balance", "bc_values_darcy_flux", None),
+        ("mass_balance", "bc_values_mobrho", None),
+        ("mass_balance", "fluid_viscosity", None),
+        ("mass_balance", "fluid_source", None),
+        ("mass_balance", "mobility", None),
+        ("mass_balance", "fluid_density", None),
+        ("mass_balance", "aperture", None),
+        ("mass_balance", "specific_volume", None),
+        ("mass_balance", "darcy_flux", None),
+        ("mass_balance", "interface_fluid_flux", None),
+        ("mass_balance", "fluid_flux", None),
+        ("mass_balance", "pressure_trace", None),
+        ("mass_balance", "porosity", None),
+        ("mass_balance", "reference_pressure", None),
         # Momentum balance
-        ("momentum_balance", "bc_values_mechanics", []),
+        ("momentum_balance", "bc_values_mechanics", None),
         # The body force and stress are only meaningful in the top dimension
-        ("momentum_balance", "body_force", [2]),
-        ("momentum_balance", "stress", [2]),
-        ("momentum_balance", "solid_density", []),
-        ("momentum_balance", "lame_lambda", []),
-        ("momentum_balance", "shear_modulus", []),
-        ("momentum_balance", "youngs_modulus", []),
-        ("momentum_balance", "bulk_modulus", []),
+        ("momentum_balance", "body_force", 2),
+        ("momentum_balance", "stress", 2),
+        ("momentum_balance", "solid_density", None),
+        ("momentum_balance", "lame_lambda", None),
+        ("momentum_balance", "shear_modulus", None),
+        ("momentum_balance", "youngs_modulus", None),
+        ("momentum_balance", "bulk_modulus", None),
         # Reference gap, friction coefficient and friction bound are only meaningful for
         # a fracture of co-dimension 1.
-        ("momentum_balance", "reference_gap", [1]),
-        ("momentum_balance", "friction_coefficient", [1]),
-        ("momentum_balance", "friction_bound", [1]),
+        ("momentum_balance", "reference_gap", 1),
+        ("momentum_balance", "friction_coefficient", 1),
+        ("momentum_balance", "friction_bound", 1),
         # Energy balance
-        ("energy_balance", "bc_values_enthalpy_flux", []),
-        ("energy_balance", "bc_values_fourier_flux", []),
-        ("energy_balance", "energy_source", []),
-        ("energy_balance", "enthalpy_flux", []),
-        ("energy_balance", "fourier_flux", []),
-        ("energy_balance", "interface_enthalpy_flux", []),
-        ("energy_balance", "interface_fourier_flux", []),
-        ("energy_balance", "reference_temperature", []),
-        ("energy_balance", "normal_thermal_conductivity", []),
-        ("energy_balance", "aperture", []),
-        ("energy_balance", "specific_volume", []),
+        ("energy_balance", "bc_values_enthalpy_flux", None),
+        ("energy_balance", "bc_values_fourier_flux", None),
+        ("energy_balance", "energy_source", None),
+        ("energy_balance", "enthalpy_flux", None),
+        ("energy_balance", "fourier_flux", None),
+        ("energy_balance", "interface_enthalpy_flux", None),
+        ("energy_balance", "interface_fourier_flux", None),
+        ("energy_balance", "reference_temperature", None),
+        ("energy_balance", "normal_thermal_conductivity", None),
+        ("energy_balance", "aperture", None),
+        ("energy_balance", "specific_volume", None),
         # Poromechanics
-        ("poromechanics", "reference_porosity", []),
-        ("poromechanics", "biot_coefficient", [2]),
-        ("poromechanics", "matrix_porosity", [2]),
-        ("poromechanics", "porosity", [1]),
-        ("poromechanics", "porosity", []),
-        ("poromechanics", "pressure_stress", [2]),
-        ("poromechanics", "stress", [2]),
-        ("poromechanics", "specific_volume", []),
-        ("poromechanics", "aperture", []),
-        ("poromechanics", "fracture_pressure_stress", [1]),
+        ("poromechanics", "reference_porosity", None),
+        ("poromechanics", "biot_coefficient", 2),
+        ("poromechanics", "matrix_porosity", 2),
+        ("poromechanics", "porosity", 1),
+        ("poromechanics", "porosity", None),
+        ("poromechanics", "pressure_stress", 2),
+        ("poromechanics", "stress", 2),
+        ("poromechanics", "specific_volume", None),
+        ("poromechanics", "aperture", None),
+        ("poromechanics", "fracture_pressure_stress", 1),
     ],
 )
 @pytest.mark.parametrize("num_fracs", [0, 1, 2])
@@ -97,37 +96,18 @@ def test_parse_constitutive_laws(model_type, method_name, domain_dimension, num_
             domains.
 
     """
-    if len(domain_dimension) > 0:
-        if domain_dimension[0] == 2 and num_fracs > 0:
+    if domain_dimension is not None:
+        if domain_dimension == 2 and num_fracs > 0:
             return
-        elif domain_dimension[0] == 1 and num_fracs == 0:
+        elif domain_dimension == 1 and num_fracs == 0:
             return
 
     # Set up an object of the prescribed model
-    setup = model(model_type, num_fracs=num_fracs)
-    # Fetch the relevant method of this model.
+    setup = setup_utils.model(model_type, num_fracs=num_fracs)
+    # Fetch the relevant method of this model and extract the domains for which it is
+    # defined.
     method = getattr(setup, method_name)
-    # Fetch the signature of the method.
-    sig = signature(method)
-
-    # The assumption is that the method to be tested takes as input only its domain of
-    # definition, that is a list of subdomains or interfaces. The test framework is not
-    # compatible with methods that take other arguments (and such a method would also
-    # break the implicit contract of the constitutive laws).
-    assert len(sig.parameters) == 1
-
-    # The domain is a list of either subdomains or interfaces.
-    if "subdomains" in sig.parameters:
-        # If relevant, filter out the domains that are not to be tested.
-        if len(domain_dimension) > 0:
-            domains = setup.mdg.subdomains(dim=domain_dimension[0])
-        else:
-            domains = setup.mdg.subdomains()
-    elif "interfaces" in sig.parameters:
-        if len(domain_dimension) > 0:
-            domains = setup.mdg.interfaces(dim=domain_dimension[0])
-        else:
-            domains = setup.mdg.interfaces()
+    domains = setup_utils.domains_from_method_name(setup.mdg, method, domain_dimension)
 
     # Call the method with the domain as argument. An error here will indicate that
     # something is wrong with the way the method combines terms and factors (e.g., grids
@@ -145,4 +125,33 @@ def test_parse_constitutive_laws(model_type, method_name, domain_dimension, num_
     op.evaluate(setup.equation_system)
 
 
-test_parse_constitutive_laws("momentum_balance", "bc_values_mechanics", [], 0)
+@pytest.mark.parametrize(
+    "method_name, domain_dimension, expected",
+    [
+        # Viscosity is constant by default, so test should be trivial.
+        ("fluid_viscosity", None, setup_utils.water_values["viscosity"]),
+    ],
+)
+def test_return_values(method_name, domain_dimension, expected):
+    """Test that the value of the parsed operator is as expected."""
+    # The thermoporoelastic model covers most constitutive laws, so we use it for the
+    # test.
+    # Assign non-trivial values to the parameters to avoid masking errors. TODO: Same
+    # for variables?
+    fluid = pp.FluidConstants(setup_utils.water_values)
+    solid = pp.SolidConstants(setup_utils.granite_values)
+    params = {"material_constants": {"fluid": fluid, "solid": solid}}
+    setup = pp.thermoporomechanics.Thermoporomechanics(params)
+    setup.prepare_simulation()
+
+    # Fetch the relevant method for this test and the relevant domains.
+    method = getattr(setup, method_name)
+    domains = setup_utils.domains_from_method_name(setup.mdg, method, domain_dimension)
+
+    # Call the method with the domain as argument.
+    op = method(domains)
+    # Evaluate the operator.
+    value = op.evaluate(setup.equation_system)
+    if isinstance(value, pp.ad.Ad_array):
+        value = value.val
+    assert np.allclose(value, expected)
