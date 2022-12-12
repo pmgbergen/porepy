acb3144baf67018a36d37955469bde76575fdfa1
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2022-12-12 09:56:14 +0100

    BUG: Update momentum balance model.
    The method for sign of internal boundaries now takes subdomains, not
    interfaces.

--- src/porepy/models/momentum_balance.py
+++ src/porepy/models/momentum_balance.py
@@ -130,7 +130,7 @@ class MomentumBalanceEquations(pp.BalanceEquation):
         contact_from_primary_mortar = (
             mortar_projection.primary_to_mortar_int
             * self.subdomain_projections(self.nd).face_prolongation(matrix_subdomains)
-            * self.internal_boundary_normal_to_outwards(interfaces, self.nd)
+            * self.internal_boundary_normal_to_outwards(matrix_subdomains, self.nd)
             * self.stress(matrix_subdomains)
         )
         # Traction from the actual contact force.
