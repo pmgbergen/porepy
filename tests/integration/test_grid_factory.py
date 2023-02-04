5cbdd62a6af07f254f06fce6efc6776e34311a1a
Jhabriel Varela <11464732+jhabriel@users.noreply.github.com>
2023-02-04 19:08:26 +0100

    TEST: Fix test_grid_factory
    STATUS: 270 failed + 9 errors

--- tests/integration/test_grid_factory.py
+++ tests/integration/test_grid_factory.py
@@ -22,10 +22,12 @@ class TestSimpleMeshing(unittest.TestCase):
         }
 
         network_2d = pp.FractureNetwork2d(
-            domain={"xmin": 0, "xmax": 1, "ymin": 0, "ymax": 1}
+            domain=pp.Domain({"xmin": 0, "xmax": 1, "ymin": 0, "ymax": 1})
         )
         network_3d = pp.FractureNetwork2d(
-            domain={"xmin": 0, "xmax": 1, "ymin": 0, "ymax": 1, "zmin": 0, "zmax": 1}
+            domain=pp.Domain(
+                {"xmin": 0, "xmax": 1, "ymin": 0, "ymax": 1, "zmin": 0, "zmax": 1}
+            )
         )
         for network in [network_2d, network_3d]:
             factory = pp.refinement.GridSequenceFactory(network, params)
@@ -57,7 +59,7 @@ class TestSimpleMeshing(unittest.TestCase):
         # Add a single fracture to the network
         pts = np.array([[0.3, 0.7], [0.5, 0.5]])
         edges = np.array([[0], [1]])
-        domain = {"xmin": 0, "xmax": 1, "ymin": 0, "ymax": 1}
+        domain = pp.Domain({"xmin": 0, "xmax": 1, "ymin": 0, "ymax": 1})
         network = pp.FractureNetwork2d(pts, edges, domain)
 
         factory = pp.refinement.GridSequenceFactory(network, params)
@@ -74,7 +76,7 @@ class TestSimpleMeshing(unittest.TestCase):
         # Add a single fracture to the network
         pts = np.array([[0.3, 0.7], [0.5, 0.5]])
         edges = np.array([[0], [1]])
-        domain = {"xmin": 0, "xmax": 1, "ymin": 0, "ymax": 1}
+        domain = pp.Domain({"xmin": 0, "xmax": 1, "ymin": 0, "ymax": 1})
         network = pp.FractureNetwork2d(pts, edges, domain)
 
         params = {
