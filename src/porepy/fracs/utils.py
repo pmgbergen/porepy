51c392c6ae1122f247455cecf77648ae2f271bf5
Jhabriel Varela <11464732+jhabriel@users.noreply.github.com>
2023-02-09 01:36:28 +0100

    DOC: Minor improvements in documentation

--- src/porepy/fracs/utils.py
+++ src/porepy/fracs/utils.py
@@ -4,6 +4,7 @@
 from __future__ import annotations
 
 import logging
+from itertools import zip_longest
 
 import numpy as np
 
@@ -70,54 +71,96 @@ def uniquify_points(pts, edges, tol):
 
 
 def linefractures_to_pts_edges(
-    fractures: list[pp.LineFracture],
+    fractures: list[pp.LineFracture], tol: float = 1e-8
 ) -> tuple[np.ndarray, np.ndarray]:
-    """Convert a list of fractures into two numpy arrays of the corresponding points and
-    edges.
+    """Convert a list of fractures into arrays of the corresponding points and edges.
 
     Parameters:
         fractures: List of fractures.
+        tol: Absolute tolerance to decide if start-/endpoints of two different fractures
+            are equal. Defaults to 1e-8.
 
     Returns:
-        pts ``(shape=(2, np))``: Coordinates of the start- and endpoints of the
-        fractures.
-        edges ``(shape=(len(fractures), 2), dtype=int)``: Indices for the start- and
-        endpoint of each fracture. Note, that one point in ``pts`` may be the start-
-        and/or endpoint of multiple fractures.
+        pts: ``(shape=(2, num_points))``
+            Coordinates of the start- and endpoints of the fractures.
+        edges: ``(2 + num_tags, shape=(len(fractures)), dtype=int)``
+            Indices for the start- and endpoint of each fracture. Note that one point
+            in ``pts`` may be the start- and/or endpoint of multiple fractures.
+
+            Additional rows are optional tags of the fractures. In the standard form,
+            the third row (first row of tags) identifies the type of edges, referring to
+            the numbering system in GmshInterfaceTags. The second row of tags keeps
+            track of the numbering of the edges (referring to the original order of the
+            edges) in geometry processing like intersection removal. Additional tags can
+            be assigned by the user.
 
     """
     pts_list: list[np.ndarray] = []
-    edges_list: list[list[int]] = []
+    edges_list: list[np.ndarray] = []
     for frac in fractures:
-        edge = []
+        # Peter: I did not find a practical way to do this with numpy arrays only
+        # (without the code getting messy).
+        pt_indices: list[int] = []
         for point in frac.points():
-            compare_points = [np.allclose(point, x) for x in pts_list]
+            # Check if the point is already start-/endpoint of another fracture.
+            # TODO: Change the comparison to functions in numpy (e.g. ``np.any``).
+            compare_points = [
+                np.allclose(point.squeeze(), x, atol=tol) for x in pts_list
+            ]
             if not any(compare_points):
-                pts_list.append(point)
-                edge.append(len(pts_list) - 1)
+                pts_list.append(point.squeeze())
+                pt_indices.append(len(pts_list) - 1)
             else:
-                edge.append(compare_points.index(True))
-        edges_list.append(edge)
-    pts = np.array(pts_list).squeeze().T
-    edges = np.array(edges_list, dtype=int).T
+                pt_indices.append(compare_points.index(True))
+        # TODO: Perhaps remove this assertion, it's impact on computation time should be
+        # negligible though.
+        assert len(pt_indices) == 2
+        # Combine with tags of the fracture and store the full edge in a list.
+        edges_list.append(np.concatenate([np.array(pt_indices), frac.tags]))
+    pts = np.stack(pts_list, axis=-1)
+    # Determine the maximum number of tags. -> This determines the shape of the
+    # ``edges`` array.
+    max_edge_dim = max((np.shape(edge)[0] for edge in edges_list), default=2)
+    # Initialize the ``edges`` array with ``-1``. This value indicates that each edge
+    # has no tags. Fill in the first two rows with the fracture start-/endpoints and the
+    # rest of the rows with tags where they exist. All other tags keep their initial
+    # value of ``-1``, which is equal to the tag not existing.
+    # This seemingly complicated procedure is done to ensure that the ``edges`` array is
+    # not ragged.
+    edges = np.full((max_edge_dim, len(fractures)), -1, dtype=np.int8)
+    for row_index, edge in enumerate(edges_list):
+        edges[: edge.shape[0], row_index] = edge
     return pts, edges
 
 
 def pts_edges_to_linefractures(
     pts: np.ndarray, edges: np.ndarray
 ) -> list[pp.LineFracture]:
-    """Convert points and edges into a list of fractures
+    """Convert points and edges into a list of fractures.
 
     Parameters:
-        pts ``(shape=(2, np))``: _description_
-        edges ``(shape=(len(fractures), 2), dtype=int)``: _description_
+        pts: ``(shape=(2, np))``
+            Coordinates of the start- and endpoints of the
+            fractures.
+        edges: ``(2 + num_tags, shape=(len(fractures)), dtype=int)``
+            Indices for the start- and endpoint of each fracture. Note, that one point
+            in ``pts`` may be the start- and/or endpoint of multiple fractures.
+
+            Additional rows are optional tags of the fractures. In the standard form,
+            the third row (first row of tags) identifies the type of edges, referring to
+            the numbering system in GmshInterfaceTags. The second row of tags keeps
+            track of the numbering of the edges (referring to the original order of the
+            edges) in geometry processing like intersection removal. Additional tags can
+            be assigned by the user.
 
     Returns:
         List of fractures.
     """
     fractures: list[pp.LineFracture] = []
-    for start_index, end_index in zip(edges[0, :], edges[1, :]):
+    for start_index, end_index, *tags in edges.T:
         fractures.append(
-            pp.LineFracture(np.array([pts[:, start_index], pts[:, end_index]]).T)
+            pp.LineFracture(
+                np.array([pts[:, start_index], pts[:, end_index]]).T, tags=tags
+            )
         )
     return fractures
