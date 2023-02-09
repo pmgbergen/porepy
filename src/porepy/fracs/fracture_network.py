f3a0ecc507f98de4313769d31757425a512c19fb
Jhabriel Varela <11464732+jhabriel@users.noreply.github.com>
2023-02-09 23:20:35 +0100

    WIP: `create_fracture_network()` in place

--- src/porepy/fracs/fracture_network.py
+++ src/porepy/fracs/fracture_network.py
@@ -1,80 +1,119 @@
 """
-Module containing a wrapper method to generate fracture networks.
+Module containing a wrapper method to generate fracture networks in 2d and 3d.
 """
 
-import porepy as pp
 import warnings
 
-from porepy.fracs.fracture_network_2d import LineFracture
-from porepy.fracs.fracture_network_3d import PlaneFracture
-from typing import Literal, Union
+import porepy as pp
+from porepy.fracs.line_fracture import LineFracture
+from porepy.fracs.plane_fracture import PlaneFracture
+from porepy.fracs.fracture_network_2d import FractureNetwork2d
+from porepy.fracs.fracture_network_3d import FractureNetwork3d
 
+from typing import Optional, Union
+
+# Custom typings
 Fracture = Union[LineFracture, PlaneFracture]
-FractureNetwork = Union[pp.FractureNetwork2d, pp.FractureNetwork3d]
+FractureNetwork = Union[FractureNetwork2d, FractureNetwork3d]
 
 
 def create_fracture_network(
-        fractures: list[Fracture],
-        domain: dict[str, pp.number],
-        dimension: Literal[2, 3],
+        fractures: Optional[list[Fracture]] = None,
+        domain: Optional[pp.Domain] = None,
         tol: float = 1e-8,
         run_checks: bool = False
 ) -> FractureNetwork:
-    """
-    Create a fracture network.
+    """Create a fracture network.
+
+    Examples:
+
+        .. code: python3
+
+            import porepy as pp
+
+            # Set a domain
+            domain = pp.Domain({"xmin": 0, "xmax": 1, "ymin": 0, "ymax": 1})
+
+            # Create a list of fractures
+            horizontal_frac = pp.LineFracture(np.array([[0.5, 0.5], [0.25, 0.75]]))
+            vertical_frac = pp.LineFracture(np.array([[0.25, 0.75], [0.5, 0.5]]))
+            fractures = [horizontal_frac, vertical_frac]
+
+            # Construct the fracture network
+            fracture_network = pp.create_fracture_network(fractures, domain)
+
+    Raises:
+        - ValueError: If ``domain = None`` and ``fractures = None`` (or an empty list).
+        - TypeError: If not all items of ``fractures`` are of the same type.
+        - Warning: If ``run_checks = True`` when dimension is different from 3.
 
     Parameters:
-        fractures: List of fracture objects. ``PlaneFracture`` for ``nd = 3``, and
-            ``LineFracture`` for ``nd = 2``. 
-        domain: Bounding box dictionary with keys ``xmin``, ``xmax``, ``ymin``,
-            ``ymax``, ``zmin``, and ``zmax``.
-        dimension: Ambient dimension. Either `2` or `3`.
-        tol: Geometric tolerance used in computations.
+        fractures: List of fractures. Each item of the list should be either an
+            instance of :class:`~porepy.fracs.plane_fracture.PlaneFracture` for 3d or an
+            instance of :class:`~porepy.fracs.line_fracture.LineFracture` for 2d.
+            Default is ``None``, meaning, no fractures. If an empty list is given, it
+            will be treated as ``None``.
+        domain: Domain specfication. An instance of
+            :class:`~porepy.geometry.domain.Domain`.
+        tol: Geometric tolerance used in the computations. Default is ``1e-8``.
         run_checks: Run consistency checks during the network processing. Can be
-            considered a limited debug mode. Only valid for ``dimension=3``.
-
-    Raises
-        - TypeError: If not all items of ``fractures`` are of the same type.
-        - ValueError: If the fracture type does not match the given dimension.
-        - ValueError: If the dimension is not 2 or 3.
-        - Warning: If run_checks=True when dimension != 3.
+            considered a limited debug mode. Only used for three-dimensional fracture
+            networks. Default is ``False``.
 
     Returns:
         Fracture network object according to the dimensionality of the problem.
 
     """
+
+    # Interpret empty `fractures` list as None
+    fracs: Optional[list[Fracture]]
+    if isinstance(fractures, list) and len(fractures) == 0:
+        fracs = None
+    else:
+        fracs = fractures
+
+    # Both `fracs` and `domain` cannot be None at the same time
+    if fracs is None and domain is None:
+        raise ValueError("'fractures' and 'domain' cannot both be empty/None.")
+
     # Check that all items are of the same type if the fracture list is non-empty.
-    if len(fractures) > 0:
+    if fracs is not None:
         if not (
-            all(isinstance(frac, pp.FractureNetwork2d) for frac in fractures)
+            all(isinstance(frac, LineFracture) for frac in fracs)
         ) or not (
-            all(isinstance(frac, pp.FractureNetwork2d) for frac in fractures)
+            all(isinstance(frac, PlaneFracture) for frac in fracs)
         ):
             raise TypeError("All fracture objects must be of the same type.")
 
-    # Check that the fracture type matches the dimensionality of the problem if the
-    # list of fractures is non-empty,
-    if len(fractures) > 0:
-        if (
-                dimension == 2 and not isinstance(fractures[0], pp.FractureNetwork2d)
-        ) or (
-                dimension == 3 and not isinstance(fractures[0], pp.FractureNetwork3d)
-        ):
-            raise ValueError("Fracture type is not consistent with the dimension.")
+    # At this point, we can infer the dimension of the fracture network
+    if fracs is None and domain is not None:  # domain given, no fractures
+        dim = domain.dim
+    elif fracs is not None and domain is not None:  # both are given
+        dim_from_domain = domain.dim
+        dim_from_fracs = 2 if isinstance(fracs[0], LineFracture) else 3
+        assert dim_from_domain == dim_from_fracs  # make sure both dimensions match
+        dim = dim_from_domain
+    else:  # fractures are given, no domain
+        dim = 2 if isinstance(fracs[0], LineFracture) else 3
 
-    # Check correct dimension
-    if dimension not in [2, 3]:
-        raise ValueError("Expected dimension 2 or 3.")
+    # Warn if run_checks = True is given when dimension is different from 3
+    if run_checks and dim != 3:
+        warnings.warn("'run_checks=True' has no effect if dimension != 3.")
 
-    # Warn if run_checks = True is given when dimension != 3
-    if run_checks and dimension != 3:
-        warnings.warn("run_checks has no effect if dimension != 3.")
-
-    # Create fracture network
-    if dimension == 2:
-        fracture_network = pp.FractureNetwork2d(fractures, domain, tol)
+    # Now, we create the fracture network
+    if dim == 2:
+        fracture_network = pp.FractureNetwork2d(
+            fractures=fracs,
+            domain=domain,
+            tol=tol
+        )
     else:
-        fracture_network = pp.FractureNetwork3d(fractures, domain, tol, run_checks)
+        fracture_network = pp.FractureNetwork3d(
+            fractures=fracs,
+            domain=domain,
+            tol=tol,
+            run_checks=run_checks
+        )
 
     return fracture_network
 
