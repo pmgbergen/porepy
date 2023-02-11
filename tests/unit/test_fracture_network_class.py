77f3d3cb130f82b2e20903f8b0aa477addac060b
Jhabriel Varela <11464732+jhabriel@users.noreply.github.com>
2023-02-11 15:35:04 +0100

    TST: Use fixtures instead of class parameters

--- tests/unit/test_fracture_network_class.py
+++ tests/unit/test_fracture_network_class.py
@@ -15,24 +15,24 @@ from tests import test_utils
 from porepy.fracs.utils import pts_edges_to_linefractures
 from porepy.fracs.fracture_network_2d import FractureNetwork2d
 from porepy.fracs.fracture_network_3d import FractureNetwork3d
+from porepy.grids.standard_grids.utils import unit_domain
 
 
-# Fracture network generation
 class TestCreateFractureNetwork:
+    """Contain tests that check the correct behaviour of pp.create_fracture_network()"""
 
-    # Convert parameters to fixtures
-    unit_square = pp.Domain({"xmin": 0, "xmax": 1, "ymin": 0, "ymax": 1})
+    # Set line fracture plane fracture as fixture functions to reuse them later
+    @pytest.fixture(scope="class")
+    def line_fracture(self):
+        return pp.LineFracture(np.array([[0.5, 0.5], [0, 1]]))
 
-    unit_cube = pp.Domain(
-        {"xmin": 0, "xmax": 1, "ymin": 0, "ymax": 1, "zmin": 0, "zmax": 1}
-    )
+    @pytest.fixture(scope="class")
+    def plane_fracture(self):
+        return pp.PlaneFracture(
+            np.array([[0, 2, 2, 0], [0, 2, 2, 0], [-1, -1, 1, 1]])
+        )
 
-    line_frac = pp.LineFracture(np.array([[0.5, 0.5], [0, 1]]))
-
-    plane_frac = pp.PlaneFracture(
-        np.array([[0, 2, 2, 0], [0, 2, 2, 0], [-1, -1, 1, 1]])
-    )
-
+    # -----> Test #1
     @pytest.mark.parametrize("fractures", [[], None])
     def test_error_raised_if_fractures_and_domain_are_none(self, fractures):
         """Check that an error is raised when both fractures and domain are given as
@@ -42,79 +42,75 @@ class TestCreateFractureNetwork:
             pp.create_fracture_network(fractures, None)
         assert msg in str(excinfo.value)
 
-    @pytest.mark.parametrize(
-        "fractures",
-        [
-            [1],
-            [line_frac, "just_another_fracture"],
-            [line_frac, plane_frac],
-            [plane_frac, line_frac],
-        ]
-    )
-    def test_error_if_fractures_has_heterogeneous_types(self, fractures):
+    @pytest.fixture(scope="function")
+    def fractures_lists(self, line_fracture, plane_fracture):
+        fractures_1 = [1]
+        fractures_2 = [line_fracture, "just_another_fracture"]
+        fractures_3 = [line_fracture, plane_fracture]
+        fractures_4 = [plane_fracture, line_fracture]
+        return [fractures_1, fractures_2, fractures_3, fractures_4]
+
+    # -----> Test #2
+    def test_error_if_fractures_has_heterogeneous_types(self, fractures_lists):
         """Check that a type error is raised if the list of fractures contain
         non-homogeneous types, e.g., it should be all PlaneFractures or
         LineFractures."""
-        with pytest.raises(TypeError) as excinfo:
-            msg = "All fracture objects must be of the same type."
-            pp.create_fracture_network(
-                fractures,
-                None
-            )
-        assert msg in str(excinfo.value)
+        for fractures in fractures_lists:
+            with pytest.raises(TypeError) as excinfo:
+                msg = "All fracture objects must be of the same type."
+                pp.create_fracture_network(fractures, None)
+            assert msg in str(excinfo.value)
 
-    @pytest.mark.parametrize("domain", [unit_square, unit_cube])
-    def test_empty_list_and_none_create_the_same_network(self, domain, request):
+    # -----> Test #3
+    @pytest.mark.parametrize("domain", [unit_domain(2), unit_domain(3)])
+    def test_empty_list_and_none_create_the_same_network(self, domain):
         """Check that is the same giving fractures as an empty list or None."""
         # First in 2d
-        fn_none = pp.create_fracture_network(
-            None,
-            domain
-        )
-        fn_empty_list = pp.create_fracture_network(
-            [],
-            domain
-        )
+        fn_none = pp.create_fracture_network(None, domain)
+        fn_empty_list = pp.create_fracture_network([], domain)
         assert fn_none.num_frac() == fn_empty_list.num_frac()
         assert fn_none.domain == fn_empty_list.domain
 
+    # -----> Test #4
     def test_warning_raised_if_run_checks_true_for_dim_not_3(self):
         """Checks if the warning is properly raised when ``run_checks=True`` for a
         fracture network with a dimensionality different from 3."""
         warn_msg = "'run_checks=True' has no effect if dimension != 3."
         with pytest.warns() as record:
-            domain = pp.Domain({"xmin": 0, "xmax": 1, "ymin": 0, "ymax": 1})
-            pp.create_fracture_network(None, domain=domain, run_checks=True)
+            pp.create_fracture_network(None, domain=unit_domain(2), run_checks=True)
         assert str(record[0].message) == warn_msg
 
-    @pytest.mark.parametrize(
-        "fractures, domain, FractureNetwork",
-        [
-            ([], unit_square, FractureNetwork2d),
-            ([], unit_cube, FractureNetwork3d),
-            ([line_frac], unit_square, FractureNetwork2d),
-            ([plane_frac], unit_cube, FractureNetwork3d),
-        ],
-    )
-    def test_create_fracture_networks(
-            self,
-            fractures,
-            domain,
-            FractureNetwork,
-            request,
-    ):
-        """Checks if the same fracture networks are created by using the
-        ``create_fracture_network()`` function, and the FractureNetwork classes."""
-        fn_with_create = pp.create_fracture_network(
-            fractures,
-            domain,
-        )
-        fn_with_class = FractureNetwork(
-            fractures,
-            domain,
-        )
-        assert fn_with_create.num_frac() == fn_with_class.num_frac()
-        assert fn_with_create.domain == fn_with_class.domain
+    # -----> Test #5
+    @pytest.fixture(scope="function")
+    def fractures_list_2d(self, line_fracture):
+        list_1 = []
+        list_2 = [line_fracture]
+        return [list_1, list_2]
+
+    def test_create_fracture_network_2d(self, fractures_list_2d):
+        """Test if 2d fracture networks constructed with FractureNetwork2d and with
+        create_fracture_network() are the same."""
+        for fractures in fractures_list_2d:
+            fn_with_function = pp.create_fracture_network(fractures, unit_domain(2))
+            fn_with_class = FractureNetwork2d(fractures, unit_domain(2))
+            assert fn_with_function.num_frac() == fn_with_class.num_frac()
+            assert fn_with_function.domain == fn_with_class.domain
+
+    # -----> Test #6
+    @pytest.fixture(scope="function")
+    def fractures_list_3d(self, plane_fracture):
+        list_1 = []
+        list_2 = [plane_fracture]
+        return [list_1, list_2]
+
+    def test_create_fracture_network_3d(self, fractures_list_3d):
+        """Test if 3d fracture networks constructed with FractureNetwork3d and with
+        create_fracture_network() are the same."""
+        for fractures in fractures_list_3d:
+            fn_with_function = pp.create_fracture_network(fractures, unit_domain(3))
+            fn_with_class = FractureNetwork3d(fractures, unit_domain(3))
+            assert fn_with_function.num_frac() == fn_with_class.num_frac()
+            assert fn_with_function.domain == fn_with_class.domain
 
 
 class TestFractureNetwork2d(unittest.TestCase):
