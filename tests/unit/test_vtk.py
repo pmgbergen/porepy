7593d1d18c16d36743bd5074e0788dd1dd3c098c
Jakub Both <jakub.both@uib.no>
2023-02-23 20:44:00 +0100

    TST: Fill-in holes in pvd import test adding how to compare pvd files.

--- tests/unit/test_vtk.py
+++ tests/unit/test_vtk.py
@@ -12,6 +12,7 @@ necessarily mean that something is wrong.
 """
 import os
 import shutil
+import xml.etree.ElementTree as ET
 from collections import namedtuple
 from pathlib import Path
 
@@ -111,6 +112,84 @@ def _compare_vtu_files(
 
     # If the difference is empty, the meshio objects are identified as identical.
     return diff == {}
+
+
+def _compare_pvd_files(
+    test_file: str, reference_file: str, overwrite: bool = False
+) -> bool:
+    """ """
+
+    if overwrite:
+        shutil.copy(test_file, reference_file)
+        return True
+
+    # Read pvd files which are xml files and compare.
+    tree_test = ET.parse(test_file)
+    tree_ref = ET.parse(reference_file)
+
+    # NOTE: Here, we strictly assume that the pvd files subject to the comparison
+    # are created using the Exporter. Thus, they have a non-hierarchical xml-structure.
+    # Finally, there is just two different types of xml structures, either created by
+    # write_pvd() or _export_mdg_pvd(). The first contains the keyword "timestep",
+    # whereas the second does not. This characteristic is used to determine the type
+    # of pvd files. Assume consistency, and that the first entry is sufficient to
+    # check.
+    for dataset in tree_test.iter("DataSet"):
+        data = dataset.attrib
+        test_originates_from_write_pvd = "time" in data
+    for dataset in tree_ref.iter("DataSet"):
+        data = dataset.attrib
+        ref_originates_from_write_pvd = "time" in data
+    pvd_files_compatible = (
+        test_originates_from_write_pvd == ref_originates_from_write_pvd
+    )
+
+    if not pvd_files_compatible:
+        return False
+
+    # Here, we make a simple brute-force comparison, and search for
+    # each item in the test file a matching item in the reference file.
+    def _check_xml_subtrees(
+        tree1: ET.ElementTree, tree2: ET.ElementTree, keys: list[str]
+    ) -> bool:
+        """Check whether tree1 is a subtree of tree2."""
+        # Check each item of tree1
+        for dataset1 in tree1.iter("DataSet"):
+            data1 = dataset1.attrib
+
+            # Initialize item success
+            found_data1 = False
+
+            # Try to find corresponding entry in tree2
+            for dataset2 in tree2.iter("DataSet"):
+                data2 = dataset2.attrib
+                found_data1 = all([data1[key] == data2[key] for key in keys])
+
+                if found_data1:
+                    break
+
+            # Failure, if item not part of tree2.
+            if not found_data1:
+                return False
+
+        # Success, as each item of tree1 has been identified in tree2.
+        return True
+
+    def _check_xml_tree_equality(
+        tree1: ET.ElementTree, tree2: ET.ElementTree, keys: list[str]
+    ) -> bool:
+        """Check whether tree1 and tree2 are subtress of each other."""
+        return _check_xml_subtrees(tree1, tree2, keys) and _check_xml_subtrees(
+            tree2, tree1, keys
+        )
+
+    # Check both directions, to check equality. The keys are chosen depending
+    # on the origin.
+    if test_originates_from_write_pvd:
+        keys = ["part", "timestep", "file"]
+    else:
+        keys = ["part", "file"]
+    return _check_xml_tree_equality(tree_test, tree_ref, keys)
 
 
 @pytest.fixture(scope="function")
