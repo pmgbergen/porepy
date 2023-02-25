FIX: Initial guess for NPIPM. Root computation checks. Some scripts for plotting and flashing.

scripts_vl/testmodel.py
@@ -1,8 +1,5 @@
 from __future__ import annotations
 
-import sys
-sys.path.append("/mnt/c/Users/vl-work/Desktop/github/porepy/src")
-
 from datetime import datetime
 
 import numpy as np
@@ -51,17 +48,17 @@ class TestModel(pp.models.abstract_model.AbstractModel):
 
         ## Initial Conditions
         self.initial_p: float = 100
-        self.initial_sw: float = 0.
-        self.initial_sn: float = 1.
-        
+        self.initial_sw: float = 0.0
+        self.initial_sn: float = 1.0
+
         self.inflow_p: float = 150
         self.outflow_p: float = 100
-        
+
         self.inflow_sw: float = 0.7
         self.inflow_sn: float = 0.3
 
-        self.rho_w: float = 1.
-        self.rho_n: float = 1.
+        self.rho_w: float = 1.0
+        self.rho_n: float = 1.0
         self.permeability: float = 0.1
 
         self.mdg: pp.MixedDimensionalGrid
@@ -86,7 +83,6 @@ class TestModel(pp.models.abstract_model.AbstractModel):
         self.flow_keyword: str = "flow"
         self.upwind_keyword: str = "upwind"
         self.mass_keyword: str = "mass"
-
 
         ## References to discretization operators
         # they will be set during `prepare_simulation`
@@ -123,11 +119,11 @@ class TestModel(pp.models.abstract_model.AbstractModel):
         """Assigns a cartesian grid as computational domain.
         Overwrites/sets the instance variables 'mdg'.
         """
-        refinement = 15
         phys_dims = [3, 1]
         n_cells = [10, 5]
+        # refinement = 15
         # n_cells = [i * refinement for i in phys_dims]
-        bounding_box_points = np.array([[0, phys_dims[0]],[0, phys_dims[1]]])
+        bounding_box_points = np.array([[0, phys_dims[0]], [0, phys_dims[1]]])
         self.box = pp.geometry.bounding_box.from_points(bounding_box_points)
         sg = pp.CartGrid(n_cells, phys_dims)
         self.mdg = pp.MixedDimensionalGrid()
@@ -164,7 +160,9 @@ class TestModel(pp.models.abstract_model.AbstractModel):
 
         self._prolong_prim = self.dof_manager.projection_to(primary_vars).transpose()
         self._prolong_sec = self.dof_manager.projection_to(secondary_vars).transpose()
-        self._prolong_system = self.dof_manager.projection_to(self._system_vars).transpose()
+        self._prolong_system = self.dof_manager.projection_to(
+            self._system_vars
+        ).transpose()
 
         self._sn_by_unity = self.get_sn()
 
@@ -184,11 +182,11 @@ class TestModel(pp.models.abstract_model.AbstractModel):
         self._exporter.write_vtu(self._export_vars, time_dependent=True)
 
     def get_sn(self):
-        eq = pp.ad.Scalar(1.)
+        eq = pp.ad.Scalar(1.0)
         return eq - self.sw
 
     ### SET-UP --------------------------------------------------------------------------------
-    
+
     def _set_up(self) -> None:
 
         for sd, data in self.mdg.subdomains(return_data=True):
@@ -206,7 +204,9 @@ class TestModel(pp.models.abstract_model.AbstractModel):
             ### MASS BALANCE EQUATIONS
             # advective flux in mass balance
             bc, bc_vals = self._bc_advective_flux(sd)
-            transmissibility = pp.SecondOrderTensor(np.ones(sd.num_cells) * self.permeability)
+            transmissibility = pp.SecondOrderTensor(
+                np.ones(sd.num_cells) * self.permeability
+            )
             pp.initialize_data(
                 sd,
                 data,
@@ -275,15 +275,17 @@ class TestModel(pp.models.abstract_model.AbstractModel):
 
     def _bc_freeflow(self, sd: pp.Grid) -> np.ndarray:
         all_idx, idx_east, idx_west, *_ = self._domain_boundary_sides(sd)
-        
+
         vals = np.zeros(sd.num_faces, dtype=bool)
         vals[idx_east] = True
 
         return vals
 
-    def _bc_advective_flux(self, sd: pp.Grid) -> tuple[pp.BoundaryCondition, np.ndarray]:
+    def _bc_advective_flux(
+        self, sd: pp.Grid
+    ) -> tuple[pp.BoundaryCondition, np.ndarray]:
         _, idx_east, idx_west, *_ = self._domain_boundary_sides(sd)
-        
+
         vals = np.zeros(sd.num_faces)
         vals[idx_east] = self.outflow_p
 
@@ -295,19 +297,23 @@ class TestModel(pp.models.abstract_model.AbstractModel):
 
         return bc, vals
 
-    def _bc_advective_upwind_sw(self, sd: pp.Grid) -> tuple[pp.BoundaryCondition, np.ndarray]:
+    def _bc_advective_upwind_sw(
+        self, sd: pp.Grid
+    ) -> tuple[pp.BoundaryCondition, np.ndarray]:
         all_idx, idx_east, idx_west, *_ = self._domain_boundary_sides(sd)
-        
+
         vals = np.zeros(sd.num_faces)
         vals[idx_west] = self.inflow_sw
         bc = pp.BoundaryCondition(sd, idx_west, "dir")
 
         return bc, vals
 
-    def _bc_advective_upwind_sn(self, sd: pp.Grid) -> tuple[pp.BoundaryCondition, np.ndarray]:
+    def _bc_advective_upwind_sn(
+        self, sd: pp.Grid
+    ) -> tuple[pp.BoundaryCondition, np.ndarray]:
         """BC values for the scalar part in the advective flux in component mass balance."""
         all_idx, idx_east, idx_west, *_ = self._domain_boundary_sides(sd)
-        
+
         vals = np.zeros(sd.num_faces)
         vals[idx_west] = self.inflow_sn
         bc = pp.BoundaryCondition(sd, idx_west, "dir")
@@ -331,11 +337,15 @@ class TestModel(pp.models.abstract_model.AbstractModel):
         )
 
         for sd, data in self.mdg.subdomains(return_data=True):
-            flux  = data["parameters"][self.flow_keyword]["darcy_flux"]
+            flux = data["parameters"][self.flow_keyword]["darcy_flux"]
 
-            data["parameters"][f"{self.upwind_keyword}_sw"]["darcy_flux"] = np.copy(flux)
-            data["parameters"][f"{self.upwind_keyword}_sn"]["darcy_flux"] = np.copy(flux)
-        
+            data["parameters"][f"{self.upwind_keyword}_sw"]["darcy_flux"] = np.copy(
+                flux
+            )
+            data["parameters"][f"{self.upwind_keyword}_sn"]["darcy_flux"] = np.copy(
+                flux
+            )
+
         self.upwind_sw.upwind.discretize(self.mdg)
         # self.upwind_sw.bound_transport_dir.discretize(self.mdg)
         # self.upwind_sw.bound_transport_neu.discretize(self.mdg)
@@ -358,11 +368,11 @@ class TestModel(pp.models.abstract_model.AbstractModel):
                 DX = self._prolong_prim * solution_vector + self._prolong_sec * x_s
 
         self.dof_manager.distribute_variable(
-                values=DX,
-                variables=self._system_vars,
-                additive=True,
-                to_iterate=True,
-            )
+            values=DX,
+            variables=self._system_vars,
+            additive=True,
+            to_iterate=True,
+        )
         if self._use_pressure_equation:
             sn = self._sn_by_unity.evaluate(self.dof_manager).val
             self.ad_system.set_var_values("s_n", sn, False)
@@ -389,29 +399,27 @@ class TestModel(pp.models.abstract_model.AbstractModel):
         self._exporter.write_pvd()
 
     def assemble_and_solve_linear_system(self, tol: float) -> np.ndarray:
-        
+
         if self._use_pressure_equation:
-            A, b = self.ad_system.assemble_subsystem(variables=self.system["primary_vars"])
+            A, b = self.ad_system.assemble_subsystem(
+                variables=self.system["primary_vars"]
+            )
         else:
             if self._monolithic:
                 A, b = self.ad_system.assemble_subsystem(variables=self._system_vars)
             else:
                 # non-linear Schur complement elimination of secondary variables
                 A_pp, b_p = self.ad_system.assemble_subsystem(
-                    self.system["primary_equations"],
-                    self.system["primary_vars"]
+                    self.system["primary_equations"], self.system["primary_vars"]
                 )
                 A_sp, _ = self.ad_system.assemble_subsystem(
-                    self.system["secondary_equations"],
-                    self.system["primary_vars"]
+                    self.system["secondary_equations"], self.system["primary_vars"]
                 )
                 A_ps, _ = self.ad_system.assemble_subsystem(
-                    self.system["primary_equations"],
-                    self.system["secondary_vars"]
+                    self.system["primary_equations"], self.system["secondary_vars"]
                 )
                 A_ss, b_s = self.ad_system.assemble_subsystem(
-                    self.system["secondary_equations"],
-                    self.system["secondary_vars"]
+                    self.system["secondary_equations"], self.system["secondary_vars"]
                 )
 
                 inv_A_ss = np.linalg.inv(A_ss.A)
@@ -424,7 +432,9 @@ class TestModel(pp.models.abstract_model.AbstractModel):
 
         if np.linalg.norm(b) < tol:
             self.converged = True
-            x = self.dof_manager.assemble_variable(variables=self._system_vars, from_iterate=True)
+            x = self.dof_manager.assemble_variable(
+                variables=self._system_vars, from_iterate=True
+            )
             return x
 
         dx = spla.spsolve(A, b)
@@ -448,13 +458,15 @@ class TestModel(pp.models.abstract_model.AbstractModel):
         else:
             self.system["secondary_equations"].append(name)
 
-        unity = pp.ad.Scalar(1.)
+        unity = pp.ad.Scalar(1.0)
 
         # index reduction of algebraic unitarity constraint
         # demand exponential decay of rate of change
         time_derivative = [
-            self.sw - self.sw.previous_timestep()
-            +  self.sn - self.sn.previous_timestep()
+            self.sw
+            - self.sw.previous_timestep()
+            + self.sn
+            - self.sn.previous_timestep()
         ]
         unity -= self.sw
         unity -= self.sn
@@ -466,15 +478,15 @@ class TestModel(pp.models.abstract_model.AbstractModel):
         image_info = dict()
         for sd in self.mdg.subdomains():
             image_info.update({sd: {"cells": 1}})
-        
+
         self.ad_system.set_equation(name, equation, num_equ_per_dof=image_info)
 
     def set_pressure_equation(self) -> None:
 
         advective_flux = self.mpfa.flux * self.p + self.mpfa.bound_flux * self.p_bc
 
-        equation = self.div * advective_flux 
-        
+        equation = self.div * advective_flux
+
         equ_name = "pressure"
         image_info = dict()
         for sd in self.mdg.subdomains():
@@ -487,8 +499,8 @@ class TestModel(pp.models.abstract_model.AbstractModel):
         upwind_adv = self.upwind_sw
         upwind_adv_bc = self.upwind_sw_bc
 
-        accumulation = self.mass_matrix.mass * self.rho_w * (
-            self.sw - self.sw.previous_timestep()
+        accumulation = (
+            self.mass_matrix.mass * self.rho_w * (self.sw - self.sw.previous_timestep())
         )
 
         advection_scalar = self.rho_w * self.sw
@@ -501,7 +513,7 @@ class TestModel(pp.models.abstract_model.AbstractModel):
         )
 
         equation = accumulation + self.dt * (self.div * advection)
-        
+
         equ_name = "mass_sw"
         image_info = dict()
         for sd in self.mdg.subdomains():
@@ -511,12 +523,12 @@ class TestModel(pp.models.abstract_model.AbstractModel):
 
         if self._use_pressure_equation:
             return
-        
+
         upwind_adv = self.upwind_sn
         upwind_adv_bc = self.upwind_sn_bc
 
-        accumulation = self.mass_matrix.mass * self.rho_n * (
-            self.sn - self.sn.previous_timestep()
+        accumulation = (
+            self.mass_matrix.mass * self.rho_n * (self.sn - self.sn.previous_timestep())
         )
 
         advection_scalar = self.rho_n * self.sn
@@ -549,7 +561,7 @@ class TestModel(pp.models.abstract_model.AbstractModel):
         A = (self.rho_w * self.sw).evaluate(self.dof_manager).val
 
         ADVF = F + FBC
-        TOTAL = ADVF *(UP*A) - UPDIR * ADVF * UPBC
+        TOTAL = ADVF * (UP * A) - UPDIR * ADVF * UPBC
         FNEU = UPNEU * UPBC
 
         print(TOTAL)
@@ -557,6 +569,7 @@ class TestModel(pp.models.abstract_model.AbstractModel):
         print(TOTAL - FNEU)  # !!! NO OUTFLUX TODO
 
         return
+
 
 timestamp = datetime.now().strftime("%Y_%m_%d__%H_%M")
 file_name = "PR_test_"  # + timestamp
@@ -569,8 +582,8 @@ params = {
     "monolithic": True,
 }
 
-t = 0.
-T = 10.
+t = 0.0
+T = 10.0
 dt = 0.05
 max_iter = 200
 tol = 1e-7
@@ -606,10 +619,12 @@ while t < T:
         print(f"Halving timestep to {model.dt}")
         if model.dt < 0.001:
             model.after_simulation()
-            raise RuntimeError("Time step halving due to convergence failure reached critical value.")
+            raise RuntimeError(
+                "Time step halving due to convergence failure reached critical value."
+            )
     else:
         t += model.dt
-    
+
     if t >= T:
         print(f"Reached and of simulation: t={t}")
 

src/porepy/composite/flash.py
@@ -11,8 +11,8 @@ import scipy.sparse as sps
 import porepy as pp
 
 from .composition import Composition
-from .phase import Phase
 from .peng_robinson.pr_utils import Leaf
+from .phase import Phase
 
 # import time
 
@@ -79,7 +79,7 @@ class Flash:
         self._C: Composition = composition
         """The composition class passed at instantiation"""
 
-        self._max_history: int = 100
+        self._max_history: int = 25
         """Maximal number of flash history entries (FiFo)."""
 
         self._ss_min: pp.ad.Operator = pp.ad.SemiSmoothMin()
@@ -212,7 +212,7 @@ class Flash:
                 _, lagrange = self._C.get_complementary_condition_for(
                     self._C.reference_phase
                 )
-                constraint = self._C.get_reference_phase_fraction_by_unity()
+                constraint = self._y_R
             # for other phases, 'constraint' is the phase fraction
             else:
                 constraint, lagrange = self._C.get_complementary_condition_for(phase)
@@ -350,18 +350,22 @@ class Flash:
         coeff = pp.ad.Scalar(self.npipm_parameters["u"] / self._C.num_phases**2)
         neg = pp.ad.SemiSmoothNegative()
         pos = pp.ad.SemiSmoothPositive()
-        one = pp.ad.Scalar(1)
+        # one = pp.ad.Scalar(1)
         two = pp.ad.Scalar(2)
-        abs = pp.ad.Function(pp.ad.abs, "AD-abs")
         pow = pp.ad.Function(pp.ad.power, "AD-pow")
 
         norm_parts = list()
         dot_parts = list()
         test_parts = list()
+
         def smoother(X):
             return X / (X + 1)
-        
-        equi_eqn = [e for name, e in self._C.ad_system.equations.items() if "equilibrium" in name]
+
+        equi_eqn = [
+            e
+            for name, e in self._C.ad_system.equations.items()
+            if "equilibrium" in name
+        ]
         equi_eqn = equi_eqn[-1::-1]
 
         for phase, equ in zip(self._C.phases, equi_eqn):
@@ -372,19 +376,18 @@ class Flash:
             norm_parts.append(neg(v_e) * neg(v_e) + neg(w_e) * neg(w_e))
 
             test_parts.append(
-                smoother(neg(v_e) / (self._nu + one))
+                # smoother(neg(v_e) / (self._nu + one))
                 # v_e * equ
             )
 
         dot_part = pow(pos(sum(dot_parts)), two) * coeff
-        # dot_part *= dot_part * coeff
 
         equation = (
             eta * self._nu
             + self._nu * self._nu
             + (sum(norm_parts) + dot_part) / 2
             # + pow(sum(test_parts), two) * self._nu * coeff / 2
-            + sum(test_parts) * self._regularization_param
+            # + sum(test_parts) * self._regularization_param
         )
         equation.set_name("NPIPM_param")
         self._C.ad_system.set_equation(
@@ -689,19 +692,27 @@ class Flash:
             raise ValueError(f"Unknown method {method}.")
 
         # setting STATE to newly found solution
+        # evaluate reference phase fractions
+        y_R = self._y_R.evaluate(self._C.ad_system).val
         if copy_to_state and success:
             X = self._C.ad_system.get_variable_values(
                 variables=var_names, from_iterate=True
             )
             self._C.ad_system.set_variable_values(X, variables=var_names, to_state=True)
 
-            # evaluate reference phase fractions
-            y_R = self._y_R.evaluate(self._C.ad_system).val
             self._C.ad_system.set_variable_values(
                 y_R,
                 variables=[self._C.reference_phase.fraction_name],
                 to_iterate=True,
                 to_state=True,
+            )
+        # write in any case the current fraction of reference phase
+        else:
+            self._C.ad_system.set_variable_values(
+                y_R,
+                variables=[self._C.reference_phase.fraction_name],
+                to_iterate=True,
+                to_state=False,
             )
 
         return success
@@ -836,44 +847,44 @@ class Flash:
         """Sets the initial guesses for ``V_e``, ``W_e`` and ``nu`` according to
         Vu (2021), section 3.3."""
         # initial guess for nu is constructed from V and W
-        V_mat: list[np.ndarray] = list()
-        W_mat: list[np.ndarray] = list()
+        V: list[np.ndarray] = list()
+        W: list[np.ndarray] = list()
 
-        ad_system = self._C.ad_system
+        ads = self._C.ad_system
 
         for phase in self._C.phases:
-            # initial value for V_e, W_e
-            val_v = phase.fraction.evaluate(ad_system).val
-            val_w = self._C.get_composition_unity_for(phase).evaluate(ad_system).val
-
-            # if requested, set initial guess for auxiliary NPIPM vars
+            # if requested, set initial guess for auxiliary NPIPM vars V and W
             if self.use_auxiliary_npipm_vars:
+                if phase == self._C.reference_phase:
+                    v_e = self._y_R.evaluate(ads).val
+                else:
+                    v_e = phase.fraction.evaluate(ads).val
+                w_e = self._C.get_composition_unity_for(phase).evaluate(ads).val
+
                 v_name = f"{self._V_name}_{phase.name}"
                 w_name = f"{self._W_name}_{phase.name}"
-                ad_system.set_variable_values(
-                    val_v, variables=[v_name], to_iterate=True, to_state=True
+                ads.set_variable_values(
+                    v_e, variables=[v_name], to_iterate=True, to_state=True
                 )
-                ad_system.set_variable_values(
-                    val_w, variables=[w_name], to_iterate=True, to_state=True
+                ads.set_variable_values(
+                    w_e, variables=[w_name], to_iterate=True, to_state=True
                 )
+            # else evaluate respective operators which define V and W
+            else:
+                v_e = self._V_of_phase[phase].evaluate(ads).val
+                w_e = self._W_of_phase[phase].evaluate(ads).val
 
             # store value for initial guess for nu
-            V_mat.append(val_v)
-            W_mat.append(val_w)
+            V.append(v_e)
+            W.append(w_e)
 
         # initial guess for nu is cell-wise scalar product between concatenated V and W
-        # for each phase
-        V = np.vstack(V_mat).T  # num_cells X num_phases
-        W = np.vstack(W_mat)  # num_phases X num_cells
-        # the diagonal of the product of above returns the cell-wise scalar product
-        # TODO can this be optimized using a for loop over diagonal elements of product?
-        nu_mat = np.matmul(V, W)
-        nu = np.diag(nu_mat)
+        nu = sum([v * w for v, w in zip(V, W)])
         nu = nu / self._C.num_phases
-        nu[nu < 0] = 0
+        # nu[nu < 0] = 0
 
-        ad_system.set_variable_values(
-            nu, variables=[self._nu_name], to_iterate=True, to_state=True
+        ads.set_variable_values(
+            nu, variables=[self._nu_name], to_iterate=True, to_state=False
         )
 
         # some starting value for regularization
@@ -1020,14 +1031,17 @@ class Flash:
 
             def FunctionRR(Y, z_c, K):
                 # A New Algorithm for Rachford-Rice for Multiphase Compositional Simulation
-                # R. Okuno, R.T. Johns, and K. Sepehrnoori, SPE, The University of Texas at Austin
+                # R. Okuno, R.T. Johns, and K. Sepehrnoori,
+                # SPE, The University of Texas at Austin
                 # TODO: Generalization to n_phases
                 # Since the RR function becomes the function becomes monotonic
                 # the absence of one phase one can if the integral is strictly positive
                 # value is strictly
                 potential = np.zeros_like(Y)
                 for z_i, K_i in zip(z_c, K):
-                    potential = potential - z_i.val * np.log(np.abs((1 + Y * (K_i - 1))))
+                    potential = potential - z_i.val * np.log(
+                        np.abs((1 + Y * (K_i - 1)))
+                    )
                 return potential
 
             def YPhaseFraction(z_c, K):
@@ -1037,8 +1051,8 @@ class Flash:
 
                 # Since this is still a two-phase the inverse function is available
                 # For the three-phase the inverse is still possible but more complicated
-                d = (-1 + K[0])*(-1 + K[1])
-                n = z_c[0].val - K[0]*z_c[0].val + z_c[1].val  - K[1]*z_c[1].val
+                d = (-1 + K[0]) * (-1 + K[1])
+                n = z_c[0].val - K[0] * z_c[0].val + z_c[1].val - K[1] * z_c[1].val
                 y = n / d
                 return y
 
@@ -1083,8 +1097,10 @@ class Flash:
                 for i, pair in enumerate(zip(phi_L, phi_G)):
                     K[i] = (pair[0] / (pair[1] + 1.0e-12)).val
 
-            # TODO: It seems x_ce is contextual sometimes it is extended and sometimes partial.
-            # Consider the possibility of having separate instances for extended fractions and partial fractions
+            # TODO: It seems x_ce is contextual
+            # sometimes it is extended and sometimes partial.
+            # Consider the possibility of having separate instances
+            # for extended fractions and partial fractions
             for phase in self._C.phases:
                 composition[phase] = dict()
                 for i, comp in enumerate(self._C.components):