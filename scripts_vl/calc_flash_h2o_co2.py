FIX: Initial guess for NPIPM. Root computation checks. Some scripts for plotting and flashing.

scripts_vl/calc_flash_h2o_co2.py
@@ -2,7 +2,9 @@
 """
 import csv
 import pathlib
+
 import numpy as np
+
 import porepy as pp
 
 ### General settings and data import
@@ -15,18 +17,20 @@ co2_frac = 0.01
 # list of (filename, mode),
 # where mode indicates if only L (liquid), G (gas) or both (GL) data are available
 files = [
-    ('data/pr_data_thermo_isothermal_G_easy.csv', 'G'),
-    ('data/pr_data_thermo_isothermal_G_hard.csv', 'G'),
-    ('data/pr_data_thermo_isothermal_L_easy.csv', 'L'),
-    ('data/pr_data_thermo_isothermal_L_hard.csv', 'L'),
-    ('data/pr_data_thermo_isothermal_GL_easy.csv', 'GL'),
-    ('data/pr_data_thermo_isothermal_GL_hard.csv', 'GL'),
+    ("data/pr_data_thermo_isothermal_G_easy.csv", "G"),
+    ("data/pr_data_thermo_isothermal_G_hard.csv", "G"),
+    ("data/pr_data_thermo_isothermal_L_easy.csv", "L"),
+    ("data/pr_data_thermo_isothermal_L_hard.csv", "L"),
+    ("data/pr_data_thermo_isothermal_GL_easy.csv", "GL"),
+    ("data/pr_data_thermo_isothermal_GL_hard.csv", "GL"),
     # ('data/testdata.csv', 'G')
 ]
 # results stored here
-version = 'reg-veljko-new'
-output_file = f'data/results/pr_result_VL_{version}.csv'  # file with flash data
-identifier_file = f'data/results/pr_result_VL_{version}_ID.csv'  # file to identify thermo data
+version = "wo-reg-new"
+output_file = f"data/results/pr_result_VL_{version}.csv"  # file with flash data
+identifier_file = (
+    f"data/results/pr_result_VL_{version}_ID.csv"  # file to identify thermo data
+)
 path = pathlib.Path(__file__).parent.resolve()  # path to script for file i/o
 
 # lists containing pressure and Temperature data for test cases
@@ -38,7 +42,7 @@ pT_id: dict[tuple[float, float], tuple[str, str, int]] = dict()
 
 print("Reading data ...", flush=True)
 for filename, mode in files:
-      with open(f"{path}/{filename}") as file:
+    with open(f"{path}/{filename}") as file:
         file_reader = csv.reader(file, delimiter=",")
         _ = next(file_reader)  # get rid of header
 
@@ -52,9 +56,7 @@ for filename, mode in files:
             # get only unique points
             if pT not in pT_id:
                 identifier = (mode, filename, row_id)
-                pT_id.update({
-                    pT: identifier
-                })
+                pT_id.update({pT: identifier})
 
                 p_points.append(p)
                 T_points.append(T)
@@ -77,10 +79,16 @@ MIX.add_phases([LIQ, GAS])
 
 # setting feed fractions
 AD.set_variable_values(
-    h2o_frac * np.ones(nc), variables=[H2O.fraction_name], to_iterate=True, to_state=True
+    h2o_frac * np.ones(nc),
+    variables=[H2O.fraction_name],
+    to_iterate=True,
+    to_state=True,
 )
 AD.set_variable_values(
-    co2_frac * np.ones(nc), variables=[CO2.fraction_name], to_iterate=True, to_state=True
+    co2_frac * np.ones(nc),
+    variables=[CO2.fraction_name],
+    to_iterate=True,
+    to_state=True,
 )
 # Setting zero enthalpy to get the AD framework going (h is irrelevant here)
 AD.set_variable_values(
@@ -116,8 +124,10 @@ for f, pT in enumerate(zip(p_points, T_points)):
     # set thermodynamic state
     # scale from Pa to MPa
     AD.set_variable_values(
-        (1e-6 * p) * np.ones(nc, dtype=np.double), variables=[MIX.p_name],
-        to_iterate=True, to_state=True
+        (1e-6 * p) * np.ones(nc, dtype=np.double),
+        variables=[MIX.p_name],
+        to_iterate=True,
+        to_state=True,
     )
     AD.set_variable_values(
         T * np.ones(nc), variables=[MIX.T_name], to_iterate=True, to_state=True
@@ -125,85 +135,85 @@ for f, pT in enumerate(zip(p_points, T_points)):
 
     # flashing
     try:
-        print(f"\r... flash {f}/{nf}", end='', flush=True)
+        print(f"\r... flash {f}/{nf}", end="", flush=True)
         success_ = FLASH.flash(
-            flash_type='isothermal',
-            method='npipm',
-            initial_guess='rachford_rice',
+            flash_type="isothermal",
+            method="npipm",
+            initial_guess="rachford_rice",
             copy_to_state=False,  # don't overwrite the state, store as iterate
-            do_logging=False
+            do_logging=False,
         )
-    except Exception as err:  # if Flasher fails, flag as failed
+    except Exception:  # if Flasher fails, flag as failed
         success_ = False
 
     # compute thermodynamic properties from last iterate
     try:
         MIX.compute_roots()
-    except Exception as err:
+    except Exception:
         # if the flash failed, the root computation can fail too
         # store nans as compressibility factors
         Z_L.append(np.nan)
         Z_G.append(np.nan)
     else:
         # if successful, store values
-        Z_L.append(
-            LIQ.eos.Z.val[0]
-        )
-        Z_G.append(
-            GAS.eos.Z.val[0]
-        )
-    
+        Z_L.append(LIQ.eos.Z.val[0])
+        Z_G.append(GAS.eos.Z.val[0])
+
     # extract and store results from last iterate
     success.append(int(success_))  # store booleans as 0 and 1
-    y.append(
-        GAS.fraction.evaluate(AD).val[0]
-    )
-    x_h2o_L.append(
-        LIQ.fraction_of_component(H2O).evaluate(AD).val[0]
-    )
-    x_co2_L.append(
-        LIQ.fraction_of_component(CO2).evaluate(AD).val[0]
-    )
-    x_h2o_G.append(
-        GAS.fraction_of_component(H2O).evaluate(AD).val[0]
-    )
-    x_co2_G.append(
-        GAS.fraction_of_component(CO2).evaluate(AD).val[0]
-    )
-print('\n... flash done', flush=True)
+    y.append(GAS.fraction.evaluate(AD).val[0])
+    x_h2o_L.append(LIQ.fraction_of_component(H2O).evaluate(AD).val[0])
+    x_co2_L.append(LIQ.fraction_of_component(CO2).evaluate(AD).val[0])
+    x_h2o_G.append(GAS.fraction_of_component(H2O).evaluate(AD).val[0])
+    x_co2_G.append(GAS.fraction_of_component(CO2).evaluate(AD).val[0])
+print("\n... flash done", flush=True)
 
 ### Storing results in files
 
 # flash results
 print("Writing results ...", flush=True)
-with open(f"{path}/{output_file}", 'w', newline='') as csvfile:
-    result_writer = csv.writer(csvfile, delimiter=',')
+with open(f"{path}/{output_file}", "w", newline="") as csvfile:
+    result_writer = csv.writer(csvfile, delimiter=",")
     # header labeling column values
     header = [
-        'p [Pa]', 'T [K]', 'success', 'y', 'x_h2o_L', 'x_co2_L', 'x_h2o_G', 'x_co2_G', 'Z_L', 'Z_G'
+        "p [Pa]",
+        "T [K]",
+        "success",
+        "y",
+        "x_h2o_L",
+        "x_co2_L",
+        "x_h2o_G",
+        "x_co2_G",
+        "Z_L",
+        "Z_G",
     ]
     result_writer.writerow(header)
 
     for i, pT in enumerate(zip(p_points, T_points)):
         p, T = pT
         row = [
-            p, T, success[i], y[i], x_h2o_L[i], x_co2_L[i], x_h2o_G[i], x_co2_G[i], Z_L[i], Z_G[i]
+            p,
+            T,
+            success[i],
+            y[i],
+            x_h2o_L[i],
+            x_co2_L[i],
+            x_h2o_G[i],
+            x_co2_G[i],
+            Z_L[i],
+            Z_G[i],
         ]
         result_writer.writerow(row)
 
 # identifiers
-with open(f"{path}/{identifier_file}", 'w', newline='') as csvfile:
-    id_writer = csv.writer(csvfile, delimiter=',')
+with open(f"{path}/{identifier_file}", "w", newline="") as csvfile:
+    id_writer = csv.writer(csvfile, delimiter=",")
     # header labeling column values
-    header = [
-        'p [Pa]', 'T [K]', 'mode', 'file', 'row-id'
-    ]
+    header = ["p [Pa]", "T [K]", "mode", "file", "row-id"]
     id_writer.writerow(header)
 
     for pT, identifier in pT_id.items():
         p, T = pT
         mode, file, row_id = identifier
-        row = [
-            p, T, mode, file, row_id
-        ]
+        row = [p, T, mode, file, row_id]
         id_writer.writerow(row)

scripts_vl/dbg.py
@@ -1,11 +1,11 @@
-import numpy as np
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 kappa = 1e-5
 
+
 def Theta(t):
-    return t/ (t + 1)
+    return t / (t + 1)
 
 
 x = np.linspace(0, 1, 100)

src/porepy/composite/peng_robinson/pr_eos.py
@@ -111,7 +111,7 @@ class PR_EoS:
         gaslike: bool,
         mixingrule: Literal["VdW"] = "VdW",
         smoothing_factor: float = 1e-4,
-        eps: float = 1e-16,
+        eps: float = 1e-14,
     ) -> None:
 
         self.components: list[PR_Component] = []
@@ -597,8 +597,8 @@ class PR_EoS:
             z_triple = -c2_triple / 3
 
             assert np.all(
-                0.0 <= z_triple.val
-            ), "Negative root in triple-root-region detected."
+                0.0 < z_triple.val
+            ), "Non-positive root in triple-root-region detected."
 
             # store root where it belongs
             if self.gaslike:
@@ -635,8 +635,10 @@ class PR_EoS:
             # assert physical meaning
             # TODO: review all your checks with <=
             # using <= on floats could lead to instabilities and errors difficult to find
-            assert np.all(0.0 < z_1.val) or np.isclose(z_1.val,0.0,rtol=1.0e-14), "Negative root in 2-root-region detected."
-            assert np.all(0.0 < z_23.val) or np.isclose(z_23.val,0.0,rtol=1.0e-14), "Negative double-root in 2-root-region detected."
+            assert np.all(0 < z_1.val), "Non-positive root in 2-root-region detected."
+            assert np.all(
+                0 < z_23.val
+            ), "Non-positive double-root in 2-root-region detected."
 
             # store values in double-root-region
             nc_d = np.count_nonzero(double_root_region)
@@ -684,7 +686,9 @@ class PR_EoS:
                 z2_3.val <= z3_3.val
             ), "Roots in three-root-region improperly ordered."
             # assert positivity (only then  physically meaningful)
-            assert np.all(0.0 <= z1_3.val), "Negative roots in 3-root-region detected."
+            assert np.all(
+                0.0 < z1_3.val
+            ), "Non-positive roots in 3-root-region detected."
 
             ## Smoothing of roots close to double-real-root case
             # this happens when the phase changes, at the phase border the polynomial