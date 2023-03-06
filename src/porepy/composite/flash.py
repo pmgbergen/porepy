de051ce63dd6e77f13ddd10132479e415d2d0e89
Omar Duran <omaryesiduran@gmail.com>
2023-03-06 17:52:30 +0100

    ENH: Simplified phase stability logic

--- src/porepy/composite/flash.py
+++ src/porepy/composite/flash.py
@@ -1019,11 +1019,6 @@ class Flash:
             z_c = [comp.fraction.evaluate(ads) for comp in self._MIX.components]
 
             # Collects initial K values from Wilson's correlation
-            partial_Pcs = [z_c[i].val * comp.critical_pressure() for i, comp in enumerate(self._MIX.components)]
-            partial_Tcs = [z_c[i].val * comp.critical_temperature() for i, comp in enumerate(self._MIX.components)]
-            mixture_Pr = pressure.val / sum(partial_Pcs);
-            mixture_Tr = temperature.val/sum(partial_Tcs);
-
             K = [
                 comp.critical_pressure()
                 / pressure.val
@@ -1035,7 +1030,7 @@ class Flash:
                 + 1.0e-12
                 for comp in self._MIX.components
             ]
-            K = [np.power(K_i,1.0/3.0) for K_i in K]
+            K = [np.power(K_i, 1.0 / 3.0) for K_i in K]
 
             def ResidualRR(Y, z_c, K):
                 res = np.zeros_like(Y)
@@ -1060,10 +1055,12 @@ class Flash:
                 # Non-negativity of phase fracttion
                 # R. Okuno, R.T. Johns, and K. Sepehrnoori,
                 # SPE, The University of Texas at Austin
-                t_vals = 1 + Y * (np.array(K)-1.0)
-                cond_1 = np.array([t - z_c[i].val  for i, t in enumerate(t_vals)]) > 0
-                cond_2 = np.array([t - K[i] * z_c[i].val for i, t in enumerate(t_vals)]) > 0
-                return np.all(np.logical_and(cond_1,cond_2),axis=0)
+                t_vals = 1 + Y * (np.array(K) - 1.0)
+                cond_1 = np.array([t - z_c[i].val for i, t in enumerate(t_vals)]) > 0
+                cond_2 = (
+                    np.array([t - K[i] * z_c[i].val for i, t in enumerate(t_vals)]) > 0
+                )
+                return np.all(np.logical_and(cond_1, cond_2), axis=0)
 
             def YPhaseFraction(z_c, K):
                 # TODO: Generalize the case for multidimensional bisection
@@ -1085,14 +1082,14 @@ class Flash:
                 invalid_state = np.logical_or(0.0 > Y, Y > 1.0)
 
                 gas_feasible_q = YConstraints(np.ones(nc), z_c, K)
-                super_critical_q = np.logical_and(mixture_Pr > 1.0, mixture_Tr > 1.0)
                 function_RR_val = FunctionRR(np.ones(nc), z_c, K)
 
-                Y = np.where((function_RR_val > 0.0) & (invalid_state) & (super_critical_q), np.zeros(nc),Y)
-                Y = np.where((function_RR_val < 0.0) & (invalid_state) & (super_critical_q), np.ones(nc),Y)
-
-                Y = np.where((gas_feasible_q) & (invalid_state) & (np.logical_not(super_critical_q)), np.ones(nc), Y)
-                Y = np.where((np.logical_not(gas_feasible_q)) & (invalid_state) & (np.logical_not(super_critical_q)),np.zeros(nc), Y)
+                Y = np.where((function_RR_val > 0.0) & (invalid_state), np.zeros(nc), Y)
+                Y = np.where(
+                    (gas_feasible_q) & (function_RR_val < 0.0) & (invalid_state),
+                    np.ones(nc),
+                    Y,
+                )
 
                 assert not np.any(np.logical_or(0.0 > Y, Y > 1.0))
 
