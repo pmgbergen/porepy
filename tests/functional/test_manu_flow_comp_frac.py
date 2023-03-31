28c066878b523a55fecba40f79d4b02324dce7b5
Jhabriel Varela <jhabriel@gmail.com>
2023-03-31 12:42:17 +0200

    MAINT: Refactor test

--- tests/functional/test_manu_flow_comp_frac.py
+++ tests/functional/test_manu_flow_comp_frac.py
@@ -8,12 +8,12 @@ via the dependency of the fluid density with the fluid pressure as given in [3]:
 
 .. math:
 
-    \rho(p) = \rho_0 \exp{(c_f (p - p_0))},
+    \rho(p) = \rho_0 \\exp{(c_f (p - p_0))},
 
-where, for our setup, :math:`\rho_0 = 1` [kg * m^-3], :math:`c_f = 0.2` [Pa^-1],
+where, for our setup, :math:`\\rho_0 = 1` [kg * m^-3], :math:`c_f = 0.2` [Pa^-1],
 and :math:`p_0 = 0` [Pa]. The rest of the physical parameter are given unitary
-values, except for the reference porosity :math:`\phi_0 = 0.1` and normal
-permeability :math:`\kappa = 0.5`.
+values, except for the reference porosity :math:`\\phi_0 = 0.1` and normal
+permeability :math:`\\kappa = 0.5`.
 
 Tests should pass as long as the `desired_error` matches the `actual_error`,
 up to absolute (1e-5) and relative (1e-3) tolerances. The values of such tolerances
@@ -44,12 +44,11 @@ References:
 """
 from __future__ import annotations
 
-from collections import namedtuple
-
 import numpy as np
 import pytest
 
 import porepy as pp
+from tests.functional.setups.functional_utils import DesiredValuesFlow
 from tests.functional.setups.manu_flow_comp_frac import (
     ManuCompFlowSetup,
     ManuCompSaveData,
@@ -60,12 +59,12 @@ from tests.functional.setups.manu_flow_comp_frac import (
 
 @pytest.fixture(scope="module")
 def results() -> list[ManuCompSaveData]:
-    # Run verification setup and retrieve results for the scheduled times
+    """Run verification setup and retrieve results for the scheduled times."""
     material_constants = {
         "solid": pp.SolidConstants(manu_comp_solid),
         "fluid": pp.FluidConstants(manu_comp_fluid),
     }
-    mesh_arguments = {"mesh_size_frac": 0.05, "mesh_size_bound": 0.05}
+    mesh_arguments = {"mesh_size_frac": 0.125}
     time_manager = pp.TimeManager([0, 0.2, 0.8, 1.0], 0.2, True)
     params = {
         "material_constants": material_constants,
@@ -77,77 +76,52 @@ def results() -> list[ManuCompSaveData]:
     return setup.results
 
 
-DesiredError = namedtuple(
-    "DesiredError",
-    "error_matrix_pressure, "
-    "error_matrix_flux, "
-    "error_frac_pressure, "
-    "error_frac_flux, "
-    "error_intf_flux",
-)
-
-
-desired_errors: list[DesiredError] = [
-    # t = 0.2 [s]
-    DesiredError(
+@pytest.fixture(scope="module")
+def desired() -> list[DesiredValuesFlow]:
+    """Set desired errors."""
+    desired_error_0 = DesiredValuesFlow(  # t = 0.2 [s]
         error_matrix_pressure=0.0036348942968626903,
         error_matrix_flux=0.004269623457013436,
         error_frac_pressure=0.6583594878262145,
         error_frac_flux=0.0004147458073966614,
         error_intf_flux=0.4694294365244132,
-    ),
-    # t = 0.6 [s]
-    DesiredError(
+    )
+    desired_error_1 = DesiredValuesFlow(  # t = 0.8 [s]
         error_matrix_pressure=0.003028196249337853,
         error_matrix_flux=0.0029348589540767584,
         error_frac_pressure=0.6596212819386692,
         error_frac_flux=0.0005294349183034246,
         error_intf_flux=0.4943796145199786,
-    ),
-    # t = 1.0 [s]
-    DesiredError(
+    )
+    desired_error_2 = DesiredValuesFlow(  # t = 1.0 [s]
         error_matrix_pressure=0.0028115632547158387,
         error_matrix_flux=0.002959106297324448,
         error_frac_pressure=0.651546529657672,
         error_frac_flux=0.0005572271352671979,
         error_intf_flux=0.49619460256620995,
-    ),
-]
+    )
+    return [desired_error_0, desired_error_1, desired_error_2]
 
 
-# Now, we write the actual test
-@pytest.mark.parametrize("time_index", [0, 1, 2])
-def test_manufactured_flow_compressible_fractured_2d(time_index, results):
-    """Check errors for pressure and flux solutions.
+@pytest.mark.parametrize("tst_idx", [0, 1, 2])
+def test_manufactured_flow_compressible_fractured_2d(tst_idx, results, desired):
+    """Check L2-relative errors for primary and secondary variables.
 
     Nest test functions to avoid computing the solution multiple times.
 
     """
-    actual = results[time_index]
-    desired = desired_errors[time_index]
+    var_to_test = [
+        "error_matrix_pressure",
+        "error_matrix_flux",
+        "error_frac_pressure",
+        "error_frac_pressure",
+        "error_intf_flux",
+    ]
 
-    np.testing.assert_allclose(
-        actual.error_matrix_pressure,
-        desired.error_matrix_pressure,
-        atol=1e-5,
-        rtol=1e-3,
-    )
-
-    np.testing.assert_allclose(
-        actual.error_matrix_flux, desired.error_matrix_flux, atol=1e-5, rtol=1e-3
-    )
-
-    np.testing.assert_allclose(
-        actual.error_frac_pressure,
-        desired.error_frac_pressure,
-        atol=1e-5,
-        rtol=1e-3,
-    )
-
-    np.testing.assert_allclose(
-        actual.error_frac_flux, desired.error_frac_flux, atol=1e-5, rtol=1e-3
-    )
-
-    np.testing.assert_allclose(
-        actual.error_intf_flux, desired.error_intf_flux, atol=1e-5, rtol=1e-3
-    )
+    for attr in var_to_test:
+        np.testing.assert_allclose(
+            getattr(results[tst_idx], attr),
+            getattr(desired[tst_idx], attr),
+            atol=1e-5,
+            rtol=1e-3,
+        )
