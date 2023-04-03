9cde08b09d0dfb04075926eae5acd3908a71e35b
pschultzendorff <peter@schultzendorff.de>
2023-04-03 21:51:27 +0200

    DOC: Update documentation.

--- src/porepy/fracs/line_fracture.py
+++ src/porepy/fracs/line_fracture.py
@@ -1,4 +1,7 @@
-"""Contains classes representing fractures of 1D, i.e. manifolds of dimension 1 embedded in 2D.
+"""
+Contains classes representing 1D fractures, i.e., manifolds of dimension 1 embedded
+in 2d.
+
 """
 
 import numpy as np
@@ -12,41 +15,44 @@ class LineFracture(Fracture):
     def sort_points(self) -> np.ndarray:
         """Sort the vertices.
 
-        For fractures defined by a two-vertex line segment, sorting is trivial.
-        Returns
-        -------
-        numpy.ndarray(dtype=int)
-            The indices corresponding to the sorting.
+        For :class:~`LineFracture`s defined by a two-vertex line segment, sorting is
+        trivial.
+
+        Returns:
+            :obj:`~numpy.ndarray`: ``(dtype=int)``
+                The sorted vertex indices.
+
         """
         return np.arange(2)
 
     def local_coordinates(self) -> np.ndarray:
-        """Method for computing the 1d vertex coordinates in a local system.
+        """Compute the 1d vertex coordinates of the fracture in a local system.
 
-        The first coordinate is set at x=0, the second at x=self.length().
+        The first coordinate is set to ``x=0``, the second to ``x=self.length()``.
 
-        Returns
-        -------
-        numpy.ndarray(1 x 2)
-            The coordinates of the two vertices in the single local dimension.
+        Returns:
+            :obj:`~numpy.ndarray`: ``(shape=(1, 2))``
+                The coordinates of the two vertices in the 1d local dimension.
+
         """
         return np.reshape([0, self.length()], (1, 2))
 
     def compute_centroid(self) -> np.ndarray:
-        """Method for computing the centroid of the fracture.
+        """Compute the centroid of the fracture.
 
         Returns:
-            Array containing the 2D coordinates of the centroid.
+            :obj:`~numpy.ndarray`: Array containing the 2D coordinates of the centroid.
+
         """
         return np.mean(self.pts, axis=1)
 
     def compute_normal(self) -> np.ndarray:
-        """Method computing normal vectors of the fracture
+        """Compute the normal vector of the fracture
 
-        Returns
-        -------
-        numpy.ndarray(2 x 1)
-            Normal vector of the fracture.
+        Returns:
+            :obj:`~numpy.ndarray`: ``(shape=(2, 1))``
+                Normal vector of the fracture.
+
         """
 
         diff = np.diff(self.pts, axis=1)
@@ -54,14 +60,15 @@ class LineFracture(Fracture):
         return normal / np.linalg.norm(normal)
 
     def _check_pts(self):
-        """Consistency check for self.pts.
+        """Consistency check for ``self.pts``.
 
-        Defining a LineFracture with more than two points is not supported, as this violates
-        assumptions underlying som class methods.
+        Defining a LineFracture with more than two points is not supported, as this
+        violates assumptions underlying som class methods.
 
         Raises:
-            ValueError if self.pts does not have the expected shape.
-            ValueError if the two self.pts are identical.
+            ValueError: If ``self.pts`` does not have the expected shape.
+            ValueError: If the two ``self.pts`` are identical.
+
         """
         if self.pts.shape != (2, 2):
             raise ValueError(
@@ -70,12 +77,12 @@ class LineFracture(Fracture):
         if np.all(np.isclose(self.pts[:, 0], self.pts[:, 1])):
             raise ValueError("Need two distinct pts to define a LineFracture.")
 
-    def length(self):
+    def length(self) -> float:
         """Compute length of the fracture.
 
         Returns:
-            Fracture length as a float.
+            length: Fracture length.
 
         """
         diff = np.diff(self.pts, axis=1)
-        return np.linalg.norm(diff)
+        return float(np.linalg.norm(diff))
