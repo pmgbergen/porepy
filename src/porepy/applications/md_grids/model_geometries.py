3977d4962460d571f2ec3edee05ed1cc8b1bb715
Ivar Stefansson <ivar.stefansson@uib.no>
2023-04-13 18:24:15 +0200

    FEAT: 3d Model mixin for cube domain

--- src/porepy/applications/md_grids/model_geometries.py
+++ src/porepy/applications/md_grids/model_geometries.py
@@ -17,7 +17,7 @@ class SquareDomainOrthogonalFractures:
     params: dict
     """Parameters for the model geometry. Entries relevant for this mixin are:
         - domain_size: The side length of the square domain.
-        - fracture_indices: List of fracture indices to be included in the grid.
+        - fracture_indices: List of indices of fractures to be included in the grid.
 
     """
 
@@ -38,3 +38,38 @@ class SquareDomainOrthogonalFractures:
         fractures = [all_fractures[i] for i in fracture_indices]
         domain = domains.nd_cube_domain(2, self.domain_size)
         self.fracture_network = pp.FractureNetwork2d(fractures, domain)
+
+
+class CubeDomainOrthogonalFractures:
+    """Create a mixed-dimensional grid for a cube domain with up to three
+    orthogonal fractures.
+
+    To be used as a mixin taking precedence over
+    :class:`~porepy.models.geometry.ModelGeometry`.
+
+    """
+
+    params: dict
+    """Parameters for the model geometry. Entries relevant for this mixin are:
+        - domain_size: The side length of the cube domain.
+        - fracture_indices: List of indices of fractures to be included in the grid.
+
+    """
+
+    @property
+    def domain_size(self) -> pp.number:
+        """Return the side length of the cube domain."""
+        # Scale by length unit.
+        return self.params.get("domain_size", 1) / self.units.m
+
+    def set_fracture_network(self) -> None:
+        """Set the fracture network.
+
+        The fractures are assumed to be orthogonal to the domain boundaries.
+
+        """
+        fracture_indices = self.params.get("fracture_indices", [0])
+        all_fractures = fracture_sets.orthogonal_fractures_3d(self.domain_size)
+        fractures = [all_fractures[i] for i in fracture_indices]
+        domain = domains.nd_cube_domain(3, self.domain_size)
+        self.fracture_network = pp.FractureNetwork3d(fractures, domain)
