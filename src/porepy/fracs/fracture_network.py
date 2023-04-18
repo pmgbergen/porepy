2060d516a15b7ac577a59350b56066c5083fe543
Ivar Stefansson <ivar.stefansson@uib.no>
2023-04-18 17:31:36 +0200

    STY: Minor fixes

--- src/porepy/fracs/fracture_network.py
+++ src/porepy/fracs/fracture_network.py
@@ -9,8 +9,6 @@ from typing import Optional, Union
 import porepy as pp
 from porepy.fracs.fracture_network_2d import FractureNetwork2d
 from porepy.fracs.fracture_network_3d import FractureNetwork3d
-from porepy.fracs.line_fracture import LineFracture
-from porepy.fracs.plane_fracture import PlaneFracture
 
 # Custom typings
 FractureNetwork = Union[FractureNetwork2d, FractureNetwork3d]
@@ -89,7 +87,7 @@ def create_fracture_network(
         dim = domain.dim
     elif fracs is not None and domain is not None:  # CASE2 2: both are given
         # Infer dimension form the list of fractures
-        if isinstance(fracs[0], LineFracture):
+        if isinstance(fracs[0], pp.LineFracture):
             dim_from_fracs = 2
         else:
             dim_from_fracs = 3
@@ -102,7 +100,7 @@ def create_fracture_network(
         dim = dim_from_domain
     else:  # CASE 3: fractures given, no domain
         assert fracs is not None and domain is None  # needed to please mypy
-        if isinstance(fracs[0], LineFracture):
+        if isinstance(fracs[0], pp.LineFracture):
             dim = 2
         else:
             dim = 3
