0e3949d2d6305730abaff43e863f44a4e8077b6a
Jhabriel Varela <11464732+jhabriel@users.noreply.github.com>
2023-04-21 00:23:46 +0200

    STY: Please mypy in 3.10

--- src/porepy/numerics/mixed_dim/assembler_filters.py
+++ src/porepy/numerics/mixed_dim/assembler_filters.py
@@ -227,9 +227,13 @@ class ListFilter(AssemblerFilter):
     def _make_grid_filter(self, grid_list):
 
         subdomains, interfaces, couplings = self._parse_grid_list(grid_list)
-        self._subdomains: List[Grid] = subdomains
-        self._interfaces: List[Tuple[Grid, Grid]] = interfaces
-        self._couplings: List[Tuple[Grid, Grid, Tuple[Grid, Grid]]] = couplings
+        self._subdomains: List[Grid] = subdomains  # type: ignore
+        self._interfaces: List[Tuple[Grid, Grid]] = interfaces  # type: ignore
+        # fmt: off
+        self._couplings: List[Tuple[Grid, Grid, Tuple[Grid, Grid]]] = (  # type: ignore
+            couplings
+        )
+        # fmt: on
 
         def _grid_filter(gl):
             if not isinstance(gl, list):
