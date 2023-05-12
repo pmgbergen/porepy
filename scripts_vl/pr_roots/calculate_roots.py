FIX: Phase composition normalization. BREAKING: Initial guess needs valid duality gap.

scripts_vl/pr_roots/calculate_roots.py
@@ -8,11 +8,11 @@ import numpy as np
 
 import porepy as pp
 
-RESULTFILE: str = "roots_nosmooth_mod.csv"
+RESULTFILE: str = "roots.csv"
 DELIMITER: str = ","
 
-B_CRIT: float = pp.composite.B_CRIT
-A_CRIT: float = pp.composite.A_CRIT
+B_CRIT: float = pp.composite.peng_robinson.PengRobinsonEoS.B_CRIT
+A_CRIT: float = pp.composite.peng_robinson.PengRobinsonEoS.A_CRIT
 EPS: float = 1e-14
 SMOOTHING_FACTOR: float = 5e-1
 
@@ -21,24 +21,26 @@ APPLY_SMOOTHING: bool = False
 # define the upper limit for A and B by UPPER_LIM_FACTOR * A_CRIT (B_CRIT)
 UPPER_LIM_FACTOR: float = 3
 # Refinement between upper and lower limits
-REFINEMENT: int = 200
+REFINEMENT: int = 300
 # A flag to include A,B = 0
 INCLUDE_ZERO: bool = True
 
-A_LIMIT: list[float] = [-A_CRIT, UPPER_LIM_FACTOR * A_CRIT]
+A_LIMIT: list[float] = [-0, UPPER_LIM_FACTOR * A_CRIT]
+# A_LIMIT: list[float] = [-A_CRIT, 3]
 # A_LIMIT: list[float] = [-25, 25]
-B_LIMIT: list[float] = [-B_CRIT, UPPER_LIM_FACTOR * B_CRIT]
+B_LIMIT: list[float] = [-0, UPPER_LIM_FACTOR * B_CRIT]
+# B_LIMIT: list[float] = [-B_CRIT, 1]
 # B_LIMIT: list[float] = [-25, 25]
 
 
 REGION_ENCODING: list[int] = [
-    0,  # 1-real-root region
-    1,  # triple-root-region
+    0,  # triple-root-region
+    1,  # 1-real-root region
     2,  # 2-root-region with multiplicity
     3,  # 3-root-region
 ]
 
-HEADERS: list[str] = ["A", "B", "root case", "Z_L", "Z_I", "Z_G", "extended"]
+HEADERS: list[str] = ["A", "B", "root case", "Z_L", "Z_G", "extended"]
 
 
 def path():
@@ -73,18 +75,18 @@ def read_root_results(
             B_ = float(row[1])
             reg = int(row[2])
             Z_L = float(row[3])
-            Z_I = float(row[4])
-            Z_G = float(row[5])
+            # Z_I = float(row[4])
+            Z_G = float(row[4])
 
-            if row[6] == "None" or row[6] is None:
+            if row[5] == "None" or row[5] is None:
                 is_extended = None
             else:
-                is_extended = int(row[6])
+                is_extended = int(row[5])
 
             A.append(A_)
             B.append(B_)
 
-            ab_map.update({(A_, B_): [reg, Z_L, Z_I, Z_G, is_extended]})
+            ab_map.update({(A_, B_): [reg, Z_L, Z_G, is_extended]})
     print(f"Reading root results: done", flush=True)
 
     A_vec = np.array(A)
@@ -816,6 +818,11 @@ if __name__ == "__main__":
 
     print("Calculating roots: ...", end="", flush=True)
     counter: int = 1
+    eos = pp.composite.peng_robinson.PengRobinsonEoS(
+        False, smoothing_factor=SMOOTHING_FACTOR, eps=EPS
+    )
+    func = eos._Z
+    # func = calc_Z_mod
     for i in range(n):
         for j in range(m):
             A_ = A_mesh[i, j]
@@ -824,12 +831,15 @@ if __name__ == "__main__":
             A_ij = np.array([A_])
             B_ij = np.array([B_])
 
-            reg, roots, is_extended = calc_Z_mod(A_ij, B_ij, apply_smoother=APPLY_SMOOTHING)
-            Z_L = roots[0][0]
-            Z_I = roots[1][0]
-            Z_G = roots[2][0]
+            Z_L, Z_G, reg = func(A_ij, B_ij, apply_smoother=APPLY_SMOOTHING)
+            is_extended = int(eos.is_extended[0])
+            Z_L = float(Z_L[0])
+            Z_G = float(Z_G[0])
+            # Z_L = roots[0][0]
+            # Z_I = roots[1][0]
+            # Z_G = roots[2][0]
 
-            rows.append([A_, B_, reg, Z_L, Z_I, Z_G, str(is_extended)])
+            rows.append([A_, B_, reg, Z_L, Z_G, is_extended])
 
             print(f"\rCalculating roots: {counter}/{nm}", end="", flush=True)
             counter += 1
@@ -849,9 +859,7 @@ if __name__ == "__main__":
         r = ab_map[(a, b)]
         assert isinstance(r[0], int), f"Root region not readable as int: row {row}"
         assert isinstance(r[1], float), f"Liquid root not readable as float: row {row}"
-        assert isinstance(r[2], float), f"Intermediate not readable as float: row {row}"
-        assert isinstance(r[3], float), f"Gas root not readable as float: row {row}"
-        assert (
-            isinstance(r[4], int) or r[4] is None
-        ), f"Extension flag not readable as int or None: row {row}"
+        # assert isinstance(r[2], float), f"Intermediate not readable as float: row {row}"
+        assert isinstance(r[2], float), f"Gas root not readable as float: row {row}"
+        assert isinstance(r[3], int), f"Extension flag not readable as int or None: row {row}"
     print("Done", flush=True)

scripts_vl/pr_roots/plot_roots.py
@@ -13,7 +13,7 @@ sys.path.append(str(pathlib.Path(__file__).parent.resolve()))
 
 from calculate_roots import A_CRIT, B_CRIT, REGION_ENCODING, path, read_root_results, EPS
 
-RESULTFILE: str = "roots_nosmooth_mod.csv"
+RESULTFILE: str = "roots.csv"
 FIGUREPATH: str = 'figs'
 FIGWIDTH: int = 15  # in inches, 1080 / 1920 ratio applied to height
 DPI: int = 400
@@ -60,7 +60,7 @@ def _plot_B_violation(
     violated = val_mesh <= B_mesh
     if np.any(violated):
         img = axis.plot(
-            A_mesh[violated], B_mesh[violated], 'v', markersize=0.1, color="magenta"
+            A_mesh[violated], B_mesh[violated], 'v', markersize=0.1, color="red"
         )
         return [img[0]], [f"{val_name} < B"]
     else:
@@ -97,7 +97,7 @@ def _plot_iszero(
 ):
     zero = np.isclose(val_mesh, 0., rtol=0, atol=EPS)
     if np.any(zero):
-        img = axis.plot(A_mesh[zero], B_mesh[zero], '*', markersize=4, color='indigo')
+        img = axis.plot(A_mesh[zero], B_mesh[zero], '*', markersize=1.5, color='indigo')
         return [img[0]], [f"{val_name} = 0 (eps = {EPS})"]
     else:
         return [], []
@@ -121,7 +121,7 @@ def plot_root_regions(
     cax = divider.append_axes("right", size="5%", pad=0.1)
     cb_rr = fig.colorbar(img_rr, cax=cax, orientation="vertical")
     cb_rr.set_ticks(REGION_ENCODING)
-    cb_rr.set_ticklabels(["1-real-root", "triple-root", "2-real-roots", "3-real-roots"])
+    cb_rr.set_ticklabels(["triple-root", "1-real-root", "2-real-roots", "3-real-roots"])
 
 
 def plot_extension_markers(
@@ -239,11 +239,9 @@ if __name__ == '__main__':
 
     GAS_RES = np.zeros(A_mesh.shape)
     LIQ_RES = np.zeros(A_mesh.shape)
-    INT_RES = np.zeros(A_mesh.shape)
     
     GAS_ROOT = np.zeros(A_mesh.shape)
     LIQ_ROOT = np.zeros(A_mesh.shape)
-    INT_ROOT = np.zeros(A_mesh.shape)
 
     GAS_EXTENDED = np.zeros(A_mesh.shape, dtype=bool)
     LIQ_EXTENDED = np.zeros(A_mesh.shape, dtype=bool)
@@ -258,45 +256,35 @@ if __name__ == '__main__':
             A_ = A_mesh[i, j]
             B_ = B_mesh[i, j]
 
-            reg, Z_L, Z_I, Z_G, is_extended = ab_map[(A_, B_)]
+            reg, Z_L, Z_G, is_extended = ab_map[(A_, B_)]
 
             # TODO delete this once the infinity issue is solved.
-            if Z_L in ignore or Z_I in ignore or Z_G in ignore:
+            if Z_L in ignore or Z_G in ignore:
                 continue
 
             REG[i, j] = reg
             GAS_ROOT[i, j] = Z_G
             LIQ_ROOT[i, j] = Z_L
-            INT_ROOT[i, j] = Z_I
 
             # data in the 1-real-root region
-            if reg == REGION_ENCODING[0]:
-                if is_extended == 2:
+            if reg == REGION_ENCODING[1]:
+                if is_extended == 0:
                     GAS_EXTENDED[i, j] = True
                     LIQ_RES[i, j] = res_compressibility(Z_L, A_, B_)
-                elif is_extended == 0:
+                elif is_extended == 1:
                     LIQ_EXTENDED[i, j] = True
                     GAS_RES[i, j] = res_compressibility(Z_G, A_, B_)
-            # data in the triple-root region
-            elif reg == REGION_ENCODING[1]:
-                LIQ_RES[i, j] = res_compressibility(Z_L, A_, B_)
-                GAS_RES[i, j] = res_compressibility(Z_G, A_, B_)
-            # data in the double-root region
-            elif reg == REGION_ENCODING[2]:
-                LIQ_RES[i, j] = res_compressibility(Z_L, A_, B_)
-                GAS_RES[i, j] = res_compressibility(Z_G, A_, B_)
-            # data in the 3-real-root region
-            elif reg == REGION_ENCODING[3]:
-                LIQ_RES[i, j] = res_compressibility(Z_L, A_, B_)
-                INT_RES[i, j] = res_compressibility(Z_I, A_, B_)
+            else:
+                if not B_ > B_CRIT / A_CRIT * A_:  # exclude because of correction
+                    LIQ_RES[i, j] = res_compressibility(Z_L, A_, B_)
                 GAS_RES[i, j] = res_compressibility(Z_G, A_, B_)
             
             print(f"\rCalculating Plot data: {counter}/{nm}", end='', flush=True)
             counter += 1
     print("\nCalculating Plot data: Done", flush=True)
 
-    NUM_ONEROOT_CASES = np.sum(REG == REGION_ENCODING[0])
-    NUM_TRIPPLEROOT_CASES = np.sum(REG == REGION_ENCODING[1])
+    NUM_TRIPPLEROOT_CASES = np.sum(REG == REGION_ENCODING[0])
+    NUM_ONEROOT_CASES = np.sum(REG == REGION_ENCODING[1])
     NUM_DOUBLEROOT_CASES = np.sum(REG == REGION_ENCODING[2])
     NUM_THREEROOT_CASES = np.sum(REG == REGION_ENCODING[3])
     print(f"Cases with a double root: {NUM_DOUBLEROOT_CASES} ({NUM_DOUBLEROOT_CASES / nm * 100} %)")
@@ -408,10 +396,11 @@ if __name__ == '__main__':
 
     plot_values(
         axis, A_mesh, B_mesh, diff, 'diff(Z_L, B)',
-        cmap='coolwarm_r', norm=norm,
+        cmap='coolwarm',
+        norm=norm,
         plot_B_violation=False,
         plot_neg=True,
-        plot_zero=True
+        # plot_zero=True
     )
 
     axis = fig.add_subplot(gs[0, 1])
@@ -437,7 +426,8 @@ if __name__ == '__main__':
     norm = mpl.colors.TwoSlopeNorm(vcenter=vcenter, vmin=vmin, vmax=vmax)
     plot_values(
         axis, A_mesh, B_mesh, diff, 'diff(Z_G, B)',
-        cmap='coolwarm_r', norm=norm,
+        cmap='coolwarm',
+        # norm=norm,
         plot_B_violation=False,
         plot_neg=True,
         plot_zero=True,
@@ -480,7 +470,8 @@ if __name__ == '__main__':
     norm = mpl.colors.TwoSlopeNorm(vcenter=vcenter, vmin=vmin, vmax=vmax)
     plot_values(
         axis, A_mesh, B_mesh, diff, 'diff(Z_G, Z_L)',
-        cmap='coolwarm_r', norm=norm,
+        cmap='coolwarm',
+        # norm=norm,
         plot_B_violation=False,
         plot_neg=True,
         plot_zero=True
@@ -495,26 +486,26 @@ if __name__ == '__main__':
     # endregion
 
     # region Plot 6: Intermediate root
-    print("\rPlotting: Intermediate root", end='', flush=True)
-    fig = plt.figure(figsize=(FIGWIDTH, 1080 / 1920 * FIGWIDTH))
-    gs = fig.add_gridspec(1, 2)
-    fig.suptitle(f"Intermediate root")
+    # print("\rPlotting: Intermediate root", end='', flush=True)
+    # fig = plt.figure(figsize=(FIGWIDTH, 1080 / 1920 * FIGWIDTH))
+    # gs = fig.add_gridspec(1, 2)
+    # fig.suptitle(f"Intermediate root")
 
-    axis = fig.add_subplot(gs[0, 0])
-    plot_values(
-        axis, A_mesh, B_mesh, INT_ROOT, 'Z_i',
-        plot_B_violation=False, plot_zero=False, plot_neg=True
-    )
+    # axis = fig.add_subplot(gs[0, 0])
+    # plot_values(
+    #     axis, A_mesh, B_mesh, INT_ROOT, 'Z_i',
+    #     plot_B_violation=False, plot_zero=False, plot_neg=True
+    # )
 
-    axis = fig.add_subplot(gs[0, 1])
-    plot_values(axis, A_mesh, B_mesh, INT_RES, 'Z_i residuals')
+    # axis = fig.add_subplot(gs[0, 1])
+    # plot_values(axis, A_mesh, B_mesh, INT_RES, 'Z_i residuals')
 
-    fig.tight_layout()
-    fig.savefig(
-        f"{str(path())}/{FIGUREPATH}/6_Z_i__{fname}.png",
-        format="png",
-        dpi=DPI,
-    )
+    # fig.tight_layout()
+    # fig.savefig(
+    #     f"{str(path())}/{FIGUREPATH}/6_Z_i__{fname}.png",
+    #     format="png",
+    #     dpi=DPI,
+    # )
     # endregion
 
     print('\nPlotting: done', flush=True)

src/porepy/composite/flash.py
@@ -27,7 +27,7 @@ from ._core import (
 from .composite_utils import safe_sum
 from .heuristics import K_val_Wilson
 from .mixture import NonReactiveMixture, ThermodynamicState
-from .phase import Phase, PhaseProperties
+from .phase import PhaseProperties
 
 __all__ = ["FlashSystemNR", "FlashNR"]
 
@@ -36,7 +36,7 @@ log_handler = logging.StreamHandler()
 log_handler.terminator = ""
 logger.addHandler(log_handler)
 
-_del_log = "\r" + " " * 150 + "\r"
+_del_log = "\r" + " " * 100 + "\r"
 
 
 def _pos(var: NumericType) -> NumericType:
@@ -298,7 +298,7 @@ class FlashSystemNR(ThermodynamicState):
                     nu = pp.ad.AdArray(nu, self._nu.jac)
 
         # computing properties necessary to assemble the system
-        phase_props: list[PhaseProperties] = self.mixture.compute_properties(
+        props: list[PhaseProperties] = self.mixture.compute_properties(
             p, T, X, store=False, normalize=True, **self.eos_kwargs
         )
 
@@ -318,14 +318,12 @@ class FlashSystemNR(ThermodynamicState):
         # between independent phases and reference phase
         for i in range(self._num_comp):
             for j in range(1, self._num_phases):
-                equ = (
-                    phase_props[j].phis[i] * X[j][i] - phase_props[0].phis[i] * X[0][i]
-                )
+                equ = X[j][i] * props[j].phis[i] - X[0][i] * props[0].phis[i]
                 equations.append(equ)
 
         # Third, enthalpy constraint if temperature unknown
         if self._T_unknown and self._assemble_h_constraint:
-            h_j = [phase_props[j].h for j in range(self._num_phases)]
+            h_j = [props[j].h for j in range(self._num_phases)]
             equ = self.mixture.evaluate_homogenous_constraint(self.h, y, h_j)
             # normalizing enthalpy constraint, without blowing it up
             h_norm = self.h.copy()
@@ -336,7 +334,7 @@ class FlashSystemNR(ThermodynamicState):
 
         # Third, volume constraint if pressure is unknown
         if self._p_unknown and self._assemble_v_constraint:
-            rho_j = [phase_props[j].rho for j in range(self._num_phases)]
+            rho_j = [props[j].rho for j in range(self._num_phases)]
             rho = self.mixture.evaluate_weighed_sum(rho_j, s)
             equ = rho ** (-1) - self.v
             # normalizing volume constraint, without blowing it up
@@ -361,6 +359,7 @@ class FlashSystemNR(ThermodynamicState):
                 vw = y[j] * unity_j
                 equ = vw - nu
                 regularization.append(vw)
+                # regularization.append(vw * self._u / self._num_phases**2)
                 equations.append(equ)
 
             # NPIPM slack equation
@@ -369,12 +368,12 @@ class FlashSystemNR(ThermodynamicState):
             # Regularizing the slack equation: Gauss-elimination steps using the
             # coupling equations and some factor
             reg = _pos(safe_sum(regularization)) * self._u / self._num_phases**2
-            reg = reg.val if isinstance(reg, pp.ad.AdArray) else reg
+            # reg = reg.val if isinstance(reg, pp.ad.AdArray) else reg
             if with_derivatives:
                 for j in range(self._num_phases):
                     equ_j = equations[-(1 + j)]
-                    equ.jac = equ.jac - sps.diags(reg) * equ_j.jac
-                    equ.val = equ.val - reg * equ_j.val
+                    equ.jac = equ.jac - sps.diags(reg.val) * equ_j.jac
+                    equ.val = equ.val - reg.val * equ_j.val
             else:
                 for j in range(self._num_phases):
                     equ_j = equations[-(1 + j)]
@@ -1345,17 +1344,6 @@ class FlashNR:
 
     ### Initial guess strategies -------------------------------------------------------
 
-    @staticmethod
-    def _normalize_fractions(X: list[NumericType]) -> list[NumericType]:
-        """Auxiliary function to re-normalize a family of fractions."""
-        s = safe_sum(X)
-        s = s.val if isinstance(s, pp.ad.AdArray) else s
-        X_n = [
-            pp.ad.AdArray(x.val / s, x.jac) if isinstance(x, pp.ad.AdArray) else x / s
-            for x in X
-        ]
-        return X_n
-
     def _solve_for_compositions(
         self, state: ThermodynamicState, K: list[list[NumericType]]
     ) -> ThermodynamicState:
@@ -1384,13 +1372,7 @@ class FlashNR:
                 # avoid division by zero, setting ref phase fraction to tiny value
                 is_zero = np.isclose(t_i, 0, 0, self.tolerance)
                 t_i[is_zero] = state.z[i][is_zero] / self.tolerance
-
-                # ensuring a duality gap
-                idx = np.logical_and(np.logical_not(nsj), t_i >= state.z[i])
-                idx = np.logical_or(idx, nsj)
-                x_i_0_[idx] = state.z[i][idx] / t_i[idx]
-                # special case TODO
-                # idx = np.logical_and(np.logical_not(nsj), t_i < state.z[i])
+                x_i_0_[nsj] = state.z[i][nsj] / t_i[nsj]
                 x_i_0.append(x_i_0_)
             # compute composition of reference phase by averaging
             x_i_0 = safe_sum(x_i_0) / len(x_i_0)
@@ -1429,7 +1411,7 @@ class FlashNR:
         # K-values per independent phase (nph - 1)
         K: list[list[NumericType]]
         # tolerance to bind K-values away from zero
-        K_tol: float = self.tolerance * 1e-2
+        K_tol: float = 1e-10
 
         if guess_K_values:
             # TODO can we use Wilson for all independent phases if more than 2 phases?
@@ -1443,41 +1425,88 @@ class FlashNR:
                 for _ in range(1, nph)
             ]
         else:
-            phase_props: list[PhaseProperties] = [
-                phase.compute_properties(state.p, state.T, state.X[j], store=False)
-                for j, phase in enumerate(self.mixture.phases)
-            ]
+            phase_props: list[PhaseProperties] = self.mixture.compute_properties(
+                state.p, state.T, state.X, store=False, normalize=True
+            )
             K = [
                 [
-                    phase_props[0].phis[i].val / phase_props[j].phis[i].val
+                    phase_props[0].phis[i].val / phase_props[j].phis[i].val + K_tol
                     for i in range(ncp)
                 ]
                 for j in range(1, nph)
             ]
 
-        independent_phases: list[Phase] = list(self.mixture.phases)[1:]
-
-        for i in range(3):
+        for _ in range(3):
             # Computing phase fraction estimates,
             # depending on number of independent phases
             if nph == 2:
                 y = rachford_rice_vle_inversion(state.z, K[0])
-                negative_flash = np.logical_or(y < 0.0, 1.0 < y)
+                negative = y < 0.0
+                exceeds = y > 1.0
+                invalid = np.logical_or(negative, exceeds)
 
                 feasible_reg = rachford_rice_feasible_region(
                     state.z, [np.ones(num_vals)], K
                 )
                 rr_pot = rachford_rice_potential(state.z, [np.ones(num_vals)], K)
 
-                y = np.where((rr_pot > 0.0) & negative_flash, np.zeros(num_vals), y)
+                y = np.where((rr_pot > 0.0) & invalid, np.zeros(num_vals), y)
                 y = np.where(
-                    (rr_pot < 0.0) & negative_flash & feasible_reg,
+                    (rr_pot < 0.0) & invalid & feasible_reg,
                     np.ones(num_vals),
                     y,
                 )
 
+                if np.any(invalid) and False:
+                    # Corrections based on the negative flash.
+                    # As long as the gas fraction is within the two inner-most poles,
+                    # it is a feasible solution.
+                    K_ = np.array(K[0])
+                    K_min = np.min(K_, axis=0)
+                    K_max = np.max(K_, axis=0)
+                    y_min_ = 1 / (1 - K_max)
+                    y_max_ = 1 / (1 - K_min)
+                    # for some unknown reasons, above formula does not always hold
+                    y_min = np.where(y_min_ < y_max_, y_min_, y_max_)
+                    y_max = np.where(y_min_ < y_max_, y_max_, y_min_)
+                    y_feasible = np.logical_and(y_min < y, y < y_max)
+                    corr_neg = np.logical_and(y_feasible, negative)
+                    y[corr_neg] = 0.0
+                    corr_pos = np.logical_and(y_feasible, exceeds)
+                    y[corr_pos] = 1.0
+                    corrected = np.logical_or(corr_neg, corr_pos)
+                    # If all K-values are smaller than 1 and gas fraction is negative,
+                    # the liquid phase is clearly saturated
+                    # Vice versa, if fraction above 1 and K-values greater than 1.
+                    # the gas phase is clearly saturated
+                    corr_neg = np.logical_and(negative, np.all(K_ < 1.0, axis=0))
+                    corrected = np.logical_or(corrected, corr_neg)
+                    corr_pos = np.logical_and(exceeds, np.all(K_ > 1.0, axis=0))
+                    corrected = np.logical_or(corrected, corr_pos)
+                    y[corr_neg] = 0.0
+                    y[corr_pos] = 1.0
+
+                    # Check if there are any invalid values which are not covered.
+                    # If there are, use special procedure from Okuno.
+                    need_correction = np.logical_and(np.logical_not(corrected), invalid)
+                    if np.any(need_correction):
+                        feasible_reg = rachford_rice_feasible_region(
+                            state.z, [np.ones(num_vals)], K
+                        )
+                        rr_pot = rachford_rice_potential(
+                            state.z, [np.ones(num_vals)], K
+                        )
+
+                        y_ = np.where((rr_pot > 0.0) & invalid, np.zeros(num_vals), y)
+                        y_ = np.where(
+                            (rr_pot < 0.0) & invalid & feasible_reg,
+                            np.ones(num_vals),
+                            y,
+                        )
+                        y[need_correction] = y_[need_correction]
+
                 assert not np.any(
-                    np.logical_or(0.0 > y, y > 1.0)
+                    np.logical_or(y < 0.0, 1.0 < y)
                 ), "y fraction estimate outside bound [0, 1]."
                 state.y[1].val = y
                 state.y[0].val = 1 - y
@@ -1489,24 +1518,17 @@ class FlashNR:
 
             state = self._solve_for_compositions(state, K)
 
-            # Re-normalizing compositions
-            # for j in range(nph):
-            #     state.X[j] = self._normalize_fractions(state.X[j])
-
             # update K values from EoS
-            x_0_sum = safe_sum([x.val for x in state.X[0]])
-            X = [x / x_0_sum for x in state.X[0]]
-            props_r: PhaseProperties = self.mixture.reference_phase.compute_properties(
-                state.p, state.T, X, store=False
+            phase_props: list[PhaseProperties] = self.mixture.compute_properties(
+                state.p, state.T, state.X, store=False, normalize=True
             )
-            for j, phase in enumerate(independent_phases):
-                x_j_sum = safe_sum([x.val for x in state.X[j + 1]])
-                X = [x / x_j_sum for x in state.X[j + 1]]
-                props: PhaseProperties = phase.compute_properties(
-                    state.p, state.T, X, store=False
-                )
-                for i in range(ncp):
-                    K[j][i] = props_r.phis[i].val / props.phis[i].val + K_tol
+            K = [
+                [
+                    phase_props[0].phis[i].val / phase_props[j].phis[i].val + K_tol
+                    for i in range(ncp)
+                ]
+                for j in range(1, nph)
+            ]
 
         # check if any phase saturated, and solve other fractions directly
         state = self._solve_for_compositions(state, K)
@@ -1549,8 +1571,7 @@ class FlashNR:
                     for i, comp in enumerate(self.mixture.components)
                 ]
             )
-            T_estimate = (T_pc - T_REF) / 2 + T_REF
-            state.T.val = T_pc
+            state.T.val = T_pc  # (T_pc - T_REF) / 2 + T_REF
         else:
             h_norm = state.h.copy()
             h_norm[np.abs(h_norm) <= 1] = 1.0
@@ -1567,7 +1588,7 @@ class FlashNR:
                 else:
                     # get only the derivative w.r.t to temperature and solve
                     # derivative w.r.t temperature is diagonal since it is local
-                    dT = -H.val / H.jac[:, :num_vals].diagonal()
+                    dT = -H.val / H.jac[:, :num_vals].diagonal() * 2e-1
                     # Correct descend direction assuming T has to increase if current
                     # enthalpy is below target (and vice versa)
                     corrector = np.logical_or(
@@ -1575,8 +1596,7 @@ class FlashNR:
                         np.logical_and(h_mix.val < state.h, dT < 0),
                     )
                     dT[corrector] *= -1
-                    dT *= (1 - np.abs(dT) / state.T.val) * 4e-1  # chop update
-                    state.T.val = state.T.val + dT
+                    state.T.val = state.T.val + dT * (1 - np.abs(dT) / state.T.val)
 
         return state, res_is_zero
 
@@ -1955,9 +1975,6 @@ class FlashNR:
         F_k = F(X_0, True)
         X_k = X_0
 
-        # DX_prev = None
-        # X_prev = []
-
         res_norm = np.linalg.norm(F_k.val)
         # if residual is already small enough
         if res_norm <= self.tolerance:
@@ -1983,16 +2000,6 @@ class FlashNR:
                     step_size = self._Armijo_line_search(X_k, -F_k.val, DX, F, i)
                     DX = step_size * DX
 
-                # if len(X_prev) == 2:
-                #     diff = DX - D_prev
-                #     g = np.dot(diff, DX) / np.dot(diff, diff)
-                #     D_prev = DX
-                #     X_k = X_prev[1] + DX - g * (X_prev[1] - X_prev[0] + DX - D_prev)
-                #     X_prev[0] = X_prev[1]
-                #     X_prev[1] = X_k
-                # else:
-                #     X_prev.append(X_k)
-                #     D_prev = DX
                 X_k = X_k + DX
 
                 F_k = F(X_k, True)

src/porepy/composite/peng_robinson/eos.py
@@ -482,7 +482,7 @@ class PengRobinsonEoS(AbstractEoS):
         A = self._A(a, p, T)
         B = self._B(b, p, T)
         # root
-        Z = self._Z(A, B, apply_smoother)
+        Z, Z_other = self._Z(A, B, apply_smoother)
         # density
         rho = self._rho(p, T, Z)
         rho_mass = self.get_rho_mass(rho, X)
@@ -492,12 +492,43 @@ class PengRobinsonEoS(AbstractEoS):
         h_dep = self._h_dep(T, Z, a, dT_a, b, B)
         h_ideal = self.get_h_ideal(p, T, X)
         h = h_ideal + h_dep
+
+        # Fugacity extensions as per Ben Gharbia 2021
+        dxi_a = list()
+        # if np.any(self.is_extended):
+        #     extend_phi: bool = True
+        #     rho_ext = self._rho(p, T, Z_other)
+        #     G = self._Z_polynom(Z, A, B)
+        #     Gamma_ext = Z * G / ((Z - B) * (Z**2 + 2 * Z * B - B**2))
+        #     dxi_Z_other = list()
+        #     for i in range(len(self.components)):
+        #         dxi_a_ = self._dXi_a(X, a_comps, bip, i)
+        #         dxi_a.append(dxi_a_)
+        #         dxi_Z_other_ = self._dxi_Z(T, rho_ext, a, b, self._b_vals[i], dxi_a[i])
+        #         dxi_Z_other.append(dxi_Z_other_)
+        #     dZ_other = safe_sum([x * dz for x, dz in zip(X, dxi_Z_other)])
+        # else:
+        #     extend_phi: bool = False
+        for i in range(len(self.components)):
+            dxi_a_ = self._dXi_a(X, a_comps, bip, i)
+            dxi_a.append(dxi_a_)
         # fugacity per present component
         phis: list[NumericType] = list()
         for i in range(len(self.components)):
-            B_i = self._B(self._b_vals[i], p, T)
-            A_i = self._dXi_a(X, a_comps, bip, i) * p / (R_IDEAL**2 * T * T)
-            phis.append(self._phi_i(Z, A_i, A, B_i, B))
+            b_i = self._b_vals[i]
+            B_i = self._B(b_i, p, T)
+            A_i = dxi_a[i] * p / (T * R_IDEAL) ** 2
+            phi_i = self._phi_i(Z, A_i, A, B_i, B)
+
+            # if extend_phi:
+            #     w1 = (B - B_i + dZ_other - dxi_Z_other[i]) / Z / 2
+            #     w2 = (B - B_i) / B
+            #     ext_i = Gamma_ext * (w1 + w2)
+
+            #     ext_i = ext_i[self.is_extended]
+            #     phi_i[self.is_extended] = phi_i[self.is_extended] + ext_i
+
+            phis.append(phi_i)
 
         # these two are open TODO
         kappa = self._kappa(p, T, Z)
@@ -850,9 +881,36 @@ class PengRobinsonEoS(AbstractEoS):
         )
         return pp.ad.exp(log_phi_i)
 
+    @staticmethod
+    def _dxi_Z(
+        T: NumericType,
+        rho: NumericType,
+        a: NumericType,
+        b: NumericType,
+        b_i: NumericType,
+        dxi_a: NumericType,
+    ) -> NumericType:
+        """Auxiliary function implementing the derivative of the compressibility factor
+        w.r.t. molar fraction ``x_i``."""
+        d = 1 + 2 * rho * b - (rho * b) ** 2
+        return rho * b_i / (1 - rho * b) ** 2 + (
+            rho * a * (2 * rho * b_i + 2 * rho**2 * b * b_i) / (d**2 * T * R_IDEAL)
+            - rho * dxi_a / (d * T * R_IDEAL)
+        )
+
+    @staticmethod
+    def _Z_polynom(Z: NumericType, A: NumericType, B: NumericType) -> NumericType:
+        """Auxiliary method implementing the compressibility polynomial."""
+        return (
+            Z**3
+            + Z**2 * (B - 1)
+            + Z * (A - 2 * B - 3 * B**2)
+            - (A * B - B**3 - B**2)
+        )
+
     def _Z(
         self, A: NumericType, B: NumericType, apply_smoother: bool = False
-    ) -> NumericType:
+    ) -> tuple[NumericType, NumericType]:
         """Auxiliary method to compute the compressibility factor based on Cardano
         formulas.
 
@@ -864,28 +922,10 @@ class PengRobinsonEoS(AbstractEoS):
                 Flag to apply smoothing procedure.
 
         Returns:
-            The root ``Z`` corresponding to the assigned phase label :attr:`gaslike`.
+            Returns two roots. The first one corresponds to the assigned phase label
+            :attr:`gaslike`. The second root is the other root.
 
         """
-        # NOTE A and B must in theory be strictly positive
-        # Numerically they can be NAN or non-positive.
-        # this must be checked.
-        # Non-positivity can happen due to numerical imprecision
-        # (e.g. slightly negative fractions)
-        # For now we try to work with it.
-
-        # # Positivity check
-        # assert np.all(A > 0.), "Cohesion A must be strictly positive"
-        # assert np.all(B > 0.), "Covolume B must be strictly positive"
-
-        # NAN check
-        # A_nan_vals = np.any(np.isnan(A.val))
-        # B_nan_vals = np.any(np.isnan(B.val))
-        # if A_nan_vals or B_nan_vals:
-        #     raise ValueError(
-        #         f"Discovered NANs in A ({A_nan_vals}) or B ({B_nan_vals})."
-        #     )
-
         shape = None  # will remain None if input is not ad
         # to determine the number of values
         n_a = None
@@ -1150,6 +1190,6 @@ class PengRobinsonEoS(AbstractEoS):
             Z_G.jac = Z_G.jac.tocsr()
 
         if self.gaslike:
-            return Z_G
+            return Z_G, Z_L
         else:
-            return Z_L
+            return Z_L, Z_G