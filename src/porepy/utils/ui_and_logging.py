722329aef8073a122b6e6d536765824ae4c4f931
pschultzendorff <peter@schultzendorff.de>
2023-05-12 17:00:35 +0200

    MAINT: Apply suggestions from code review

--- src/porepy/utils/ui_and_logging.py
+++ src/porepy/utils/ui_and_logging.py
@@ -6,7 +6,7 @@ import logging
 from contextlib import contextmanager
 from typing import Iterator, Optional, Type
 
-# Avoid some mpy trouble.
+# Avoid some mypy trouble.
 from tqdm.autonotebook import tqdm as std_tqdm  # type: ignore
 from tqdm.contrib.logging import _TqdmLoggingHandler  # type: ignore
 from tqdm.contrib.logging import logging_redirect_tqdm  # type: ignore
@@ -21,7 +21,17 @@ def logging_redirect_tqdm_with_level(
     tqdm_class: Type[std_tqdm] = std_tqdm,
 ) -> Iterator[None]:
     """Extend capability of ``tqdm.contrib.logging_redirect_tqdm`` s.t. the logging
-    handler level gets passed."""
+    handler level gets passed.
+
+    ``logging_redirect_tqdm`` provides functionality to make ``tqdm`` work together with
+    loggers. It takes a list of ``logging.logger`` as inputs and adds additional
+    handlers that work with ``tqdm``. However, currently (as of 12.05.2023) these
+    handlers do not account for the logging level of the passed logger. This issue is
+    known (see e.g., https://github.com/tqdm/tqdm/issues/1272) but not resolved yet.
+
+    This function provides a workaround until the issue is fixed.
+
+    """
     if loggers is None:
         loggers = [logging.root]
     # Save loggers and a corresponding handler for each
@@ -29,17 +39,17 @@ def logging_redirect_tqdm_with_level(
         logger: _get_first_found_console_logging_handler(logger.handlers)
         for logger in loggers
     }
-    # Let ``logging_redirect_tqdm`` do its work, and change the level of the
-    # handlers afterwards.
+    # Let ``logging_redirect_tqdm`` do its work and change the level of the handlers
+    # afterwards.
     with logging_redirect_tqdm(loggers, tqdm_class):
         try:
             for logger, orig_handler in original_handlers_dict.items():
                 for handler in logger.handlers:
                     if isinstance(handler, _TqdmLoggingHandler):
-                        # If the original logger has a handler copy its level.
+                        # If the original logger has a handler, copy its level.
                         if orig_handler is not None:
                             handler.level = orig_handler.level
-                        # Otherwise copy the level of the logger.
+                        # Otherwise, copy the level of the logger.
                         else:
                             handler.level = logger.level
 
