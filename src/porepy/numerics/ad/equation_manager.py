7788de406e8ed6f664d5a7ea32db73b321c7032d
Yuri Zabegaev <yuri.zabegaev@uib.no>
2023-05-26 15:56:14 +0200

    MAINT: Removed redeclarations of GridLike

--- src/porepy/numerics/ad/equation_manager.py
+++ src/porepy/numerics/ad/equation_manager.py
@@ -10,12 +10,11 @@ import numpy as np
 import scipy.sparse as sps
 
 import porepy as pp
+from porepy.utils.porepy_types import GridLike
 
 from . import _ad_utils, operators
 
 __all__ = ["EquationManager"]
-
-GridLike = Union[pp.Grid, pp.MortarGrid]
 
 
 class EquationManager:
