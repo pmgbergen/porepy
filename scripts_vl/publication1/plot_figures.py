MOD: Introduced Widom line for labeling algorithm. FIX: Scaling issues in PR EoS.

scripts_vl/publication1/plot_figures.py
@@ -26,24 +26,26 @@ sys.path.append(str(pathlib.Path(__file__).parent.resolve()))
 
 from _config import (
     DPI,
+    FEED,
     FIG_PATH,
     FIGURE_WIDTH,
+    ISOTHERM_DATA_PATH,
     ISOTHERMS,
     NAN_ENTRY,
     NUM_COMP,
+    PH_FLASH_DATA_PATH,
     PHASES,
     PT_FLASH_DATA_PATH,
     SPECIES,
+    T_HEADER,
     THERMO_DATA_PATH,
-    ISOTHERM_DATA_PATH,
-    PH_FLASH_DATA_PATH,
+    RESOLUTION_pT,
     composition_HEADER,
     create_index_map,
+    del_log,
     gas_frac_HEADER,
     liq_frac_HEADER,
-    T_HEADER,
     logger,
-    del_log,
     path,
     phases_HEADER,
     plot_abs_error_pT,
@@ -52,10 +54,7 @@ from _config import (
     read_px_data,
     read_results,
     success_HEADER,
-    FEED,
-    RESOLUTION_pT,
 )
-
 
 # some additional plots for debugging
 DEBUG: bool = False
@@ -140,7 +139,7 @@ if __name__ == "__main__":
         for j in range(num_T):
             x_ = X[i, j]
             T_ = T[i, j]
-            feed = [np.ones(1) * x_ , np.ones(1) * (1 - x_)]
+            feed = [np.ones(1) * x_, np.ones(1) * (1 - x_)]
             pv = np.ones(1) * 15
             Tv = np.ones(1) * T_
             prop_l = eos_l.compute(pv, Tv, feed)
@@ -197,13 +196,12 @@ if __name__ == "__main__":
                         split_pp[i, j] = 3
                 else:
                     y_pp = float(res_pp[gas_frac_HEADER][idx])
-                    if y_pp <= 0.:
+                    if y_pp <= 0.0:
                         split_pp[i, j] = 1
-                    elif 0 < y_pp < 1.:
+                    elif 0 < y_pp < 1.0:
                         split_pp[i, j] = 2
-                    elif y_pp >= 1.:
+                    elif y_pp >= 1.0:
                         split_pp[i, j] = 3
-
 
             # skip remainder if both failed
             if not (success_pp and success_thermo):
@@ -263,7 +261,7 @@ if __name__ == "__main__":
         for p_ in np.unique(np.sort(np.array(p_points))):
 
             if (p_, h_) not in idx_map_ph:
-                continue     
+                continue
 
             idx = idx_map_ph[(p_, h_)]
             success_ph = int(res_pp_ph[success_HEADER][idx])
@@ -274,14 +272,13 @@ if __name__ == "__main__":
                 if err > 1:
                     print("investigate: phT", p_, h_, T_)
             else:
-                err = 0.  # np.nan
+                err = 0.0  # np.nan
 
             err_T_isotherms[T_idx].append(err)
 
-
     # region Gibbs Energy plot
     logger.info(f"{del_log}Plotting Gibbs Energy plot ..")
-    fig, axis = plt.subplots(nrows=1, ncols=2, subplot_kw={'projection': '3d'})
+    fig, axis = plt.subplots(nrows=1, ncols=2, subplot_kw={"projection": "3d"})
     fig.set_size_inches(FIGURE_WIDTH, 1080 / 1920 * FIGURE_WIDTH)
     fig.suptitle(f"Gibbs energy")
     axis[0].set_title("Liquid phase")
@@ -289,9 +286,13 @@ if __name__ == "__main__":
     axis[0].set_ylabel("z_1 [-]")
     axis[0].set_zlabel("g [kJ]")
     img = axis[0].plot_surface(
-        T, X, Gibbs_energy_l,
+        T,
+        X,
+        Gibbs_energy_l,
         # T, p / pp.composite.PRESSURE_SCALE, Gibbs_energy_l,
-        cmap='coolwarm', linewidth=0, antialiased=False
+        cmap="coolwarm",
+        linewidth=0,
+        antialiased=False,
     )
 
     axis[1].set_title("Gas phase")
@@ -299,9 +300,13 @@ if __name__ == "__main__":
     axis[1].set_ylabel("z_1 [-]")
     axis[1].set_zlabel("g [kJ]")
     img = axis[1].plot_surface(
-        T, X, Gibbs_energy_g,
+        T,
+        X,
+        Gibbs_energy_g,
         # T, p / pp.composite.PRESSURE_SCALE, Gibbs_energy_g,
-        cmap='coolwarm', linewidth=0, antialiased=False
+        cmap="coolwarm",
+        linewidth=0,
+        antialiased=False,
     )
 
     fig.tight_layout()
@@ -397,13 +402,27 @@ if __name__ == "__main__":
         axis.set_xlabel("T [K]")
         axis.set_ylabel("p [MPa]")
         norm = _error_norm(err_gas_frac)
-        img = axis.pcolormesh(T, p, gas_frac, cmap="Greys", shading="nearest", norm=_error_norm(gas_frac))
-        overshoot = gas_frac > 1.
+        img = axis.pcolormesh(
+            T, p, gas_frac, cmap="Greys", shading="nearest", norm=_error_norm(gas_frac)
+        )
+        overshoot = gas_frac > 1.0
         if np.any(overshoot):
-            axis.plot((T[overshoot]).flat, (p[overshoot] / pp.composite.PRESSURE_SCALE).flat, "^", markersize=8, color="red")
-        undershoot = gas_frac < 0.
+            axis.plot(
+                (T[overshoot]).flat,
+                (p[overshoot] / pp.composite.PRESSURE_SCALE).flat,
+                "^",
+                markersize=8,
+                color="red",
+            )
+        undershoot = gas_frac < 0.0
         if np.any(undershoot):
-            axis.plot((T[undershoot]).flat, (p[undershoot] / pp.composite.PRESSURE_SCALE).flat, "v", markersize=6, color="orange")
+            axis.plot(
+                (T[undershoot]).flat,
+                (p[undershoot] / pp.composite.PRESSURE_SCALE).flat,
+                "v",
+                markersize=6,
+                color="orange",
+            )
         divider = make_axes_locatable(axis)
         cax = divider.append_axes("right", size="5%", pad=0.1)
         cb = fig.colorbar(
@@ -438,7 +457,7 @@ if __name__ == "__main__":
     errors = err_liq_comp[0, :, :]
     norm = _error_norm(errors)
     img = plot_abs_error_pT(axis, p, T, errors, norm=None)
-    axis.set_ylim(0, 25.)
+    axis.set_ylim(0, 25.0)
     cb = _error_cb(axis, img, fig, errors)
 
     axis = fig.add_subplot(gs[0, 1])
@@ -452,7 +471,7 @@ if __name__ == "__main__":
     errors = err_liq_comp[1, :, :]
     norm = _error_norm(errors)
     img = plot_abs_error_pT(axis, p, T, errors, norm=None)
-    axis.set_ylim(0, 25.)
+    axis.set_ylim(0, 25.0)
     cb = _error_cb(axis, img, fig, errors)
 
     axis = fig.add_subplot(gs[1, 0])
@@ -461,7 +480,7 @@ if __name__ == "__main__":
     errors = err_gas_comp[0, :, :]
     norm = _error_norm(errors)
     img = plot_abs_error_pT(axis, p, T, errors, norm=None)
-    axis.set_ylim(0, 25.)
+    axis.set_ylim(0, 25.0)
     cb = _error_cb(axis, img, fig, errors)
 
     axis = fig.add_subplot(gs[1, 1])
@@ -472,7 +491,7 @@ if __name__ == "__main__":
     errors = err_gas_comp[1, :, :]
     norm = _error_norm(errors)
     img = plot_abs_error_pT(axis, p, T, errors, norm=None)
-    axis.set_ylim(0, 25.)
+    axis.set_ylim(0, 25.0)
     cb = _error_cb(axis, img, fig, errors)
     fig.subplots_adjust(left=0.1)
     fig.tight_layout()
@@ -489,7 +508,7 @@ if __name__ == "__main__":
     # endregion
 
     # region Plotting L2 error for isotherms
-    err_T_l2 = [np.sqrt(np.sum(np.array(vec)**2)) for vec in err_T_isotherms]
+    err_T_l2 = [np.sqrt(np.sum(np.array(vec) ** 2)) for vec in err_T_isotherms]
     err_T_l2 = np.array(err_T_l2)
 
     logger.info(f"{del_log}Plotting L2 error for temperature ..")
@@ -499,9 +518,9 @@ if __name__ == "__main__":
     axis = fig.add_subplot(gs[0, 0])
     axis.set_xlabel("T [K]")
     # axis.set_ylabel("L-2 error")
-    axis.set_yscale('log')
+    axis.set_yscale("log")
 
-    axis.plot(T_vec_isotherms, err_T_l2, '-*', color='black')
+    axis.plot(T_vec_isotherms, err_T_l2, "-*", color="black")
 
     fig.tight_layout()
     fig.savefig(

src/porepy/composite/flash.py
@@ -313,11 +313,8 @@ class FlashSystemNR(ThermodynamicState):
 
         # Second, evaluate equilibrium equations for each component
         # between independent phases and reference phase
-        sigma_r = safe_sum(X[0])
         for j in range(1, self._num_phases):
-            sigma_j = safe_sum(X[j])
             for i in range(self._num_comp):
-                # equ = X[j][i] * props[j].phis[i] * sigma_j - X[0][i] * props[0].phis[i] * sigma_r
                 equ = X[j][i] * props[j].phis[i] - X[0][i] * props[0].phis[i]
                 equations.append(equ)
 
@@ -1423,8 +1420,12 @@ class FlashNR:
         K_tol: float = 1e-10
 
         # pseudo-critical pressure and temperature
-        T_cp = self.mixture.evaluate_weighed_sum(state.z, [c.T_crit for c in self.mixture.components])
-        p_cp = self.mixture.evaluate_weighed_sum(state.z, [c.p_crit for c in self.mixture.components])
+        T_cp = self.mixture.evaluate_weighed_sum(
+            state.z, [c.T_crit for c in self.mixture.components]
+        )
+        p_cp = self.mixture.evaluate_weighed_sum(
+            state.z, [c.p_crit for c in self.mixture.components]
+        )
 
         if guess_K_values:
             # TODO can we use Wilson for all independent phases if more than 2 phases?
@@ -1470,11 +1471,10 @@ class FlashNR:
 
                 # clearly liquid
                 correction_3 = (state.T < T_cp) & (state.p > p_cp) & invalid
-                y[correction_3] = 0.
+                y[correction_3] = 0.0
                 # clearly gas
                 correction_4 = (state.T > T_cp) & (state.p < p_cp) & invalid
-                y[correction_4] = 1.
-
+                y[correction_4] = 1.0
 
                 K_ = np.array(K[0])
                 K_min = np.min(K_, axis=0)
@@ -1500,7 +1500,6 @@ class FlashNR:
                 corr_pos_2 = exceeds & np.all(K_ > 1.0, axis=0)
                 y[corr_neg_2] = 0.0
                 y[corr_pos_2] = 1.0
-
 
                 corrected = correction_1 | correction_2 | correction_3 | correction_4
 
@@ -1634,17 +1633,15 @@ class FlashNR:
                     # get only the derivative w.r.t to temperature and solve
                     # derivative w.r.t temperature is diagonal since it is local
                     dT = -H.val / H.jac[:, :num_vals].diagonal() * 2e-1
-                    
+
                     # get update based on approximate changes in internal energy
                     s = FlashSystemNR.evaluate_saturations(
-                        [y.val for y in state.y],
-                        [prop.rho.val for prop in phase_props]
+                        [y.val for y in state.y], [prop.rho.val for prop in phase_props]
                     )
                     rho = self.mixture.evaluate_weighed_sum(
-                        [prop.rho for prop in phase_props],
-                        s
+                        [prop.rho for prop in phase_props], s
                     )
-                    v = rho**(-1)
+                    v = rho ** (-1)
                     pw = v * state.p
                     u_target = (pw - state.h) * (-1)  # avoid numpy overload
                     u_is = h_mix - pw
@@ -1656,12 +1653,12 @@ class FlashNR:
                         np.logical_and(u_is < u_target, dT < 0),
                     )
                     dT[corrector] *= -1
-                    dT *= (1 - np.abs(dT) / state.T.val)
+                    dT *= 1 - np.abs(dT) / state.T.val
 
                     dT = (u_target.val - u_is.val) / u_is.jac[:, :nc].diagonal()
                     check = np.abs(dT) > state.T.val
                     dT[check] = (0.1 * state.T.val * np.sign(dT))[check]
-                    dT *= (1 - np.abs(dT) / state.T.val)
+                    dT *= 1 - np.abs(dT) / state.T.val
                     state.T.val = state.T.val + dT
 
         return state, res_is_zero

src/porepy/composite/peng_robinson/eos.py
@@ -14,7 +14,7 @@ import porepy as pp
 from porepy.numerics.ad.operator_functions import NumericType
 
 from .._core import ENERGY_SCALE, PRESSURE_SCALE, R_IDEAL
-from ..composite_utils import truncexp, safe_sum
+from ..composite_utils import safe_sum, truncexp
 from ..phase import AbstractEoS, PhaseProperties
 from .mixing import VanDerWaals
 from .pr_bip import load_bip
@@ -492,7 +492,7 @@ class PengRobinsonEoS(AbstractEoS):
         # departure enthalpy
         h_dep = self._h_dep(T, Z, a, dT_a, b, B)
         h_ideal = self.get_h_ideal(p, T, X)
-        h = h_ideal + h_dep
+        h = (h_ideal + h_dep) / ENERGY_SCALE
 
         # Fugacity extensions as per Ben Gharbia 2021
         # dxi_a = list()
@@ -518,9 +518,7 @@ class PengRobinsonEoS(AbstractEoS):
         for i in range(len(self.components)):
             b_i = self._b_vals[i]
             B_i = self._B(b_i, p, T)
-            dxi_a_ = self._dXi_a(X, a_comps, bip, i)
-            # dxi_a_ = dxi_a[i]
-            A_i = dxi_a_ * p / (T * R_IDEAL) ** 2
+            A_i = self._A(self._dXi_a(X, a_comps, bip, i), p, T)
             phi_i = self._phi_i(Z, A_i, A, B_i, B)
 
             # if extend_phi:
@@ -575,22 +573,20 @@ class PengRobinsonEoS(AbstractEoS):
 
         """
         # construct first output from the constant bips and their trivial derivative
-        bips: list[list[NumericType]] = self._bip_vals.tolist()  # type: ignore
-        dT_bips: list[list[NumericType]] = np.zeros(
-            self._bip_vals.shape
-        ).tolist()  # type: ignore
+        bips: list[list[NumericType]] = (self._bip_vals + self._bip_vals.T).tolist()
+        dT_bips: list[list[NumericType]] = np.zeros(self._bip_vals.shape).tolist()
 
         # if any custom bips are to be used, update the respective entries
         if self._custom_bips:
-            for (
-                idx,
-                bip_c,
-            ) in self._custom_bips.items():
+            for idx, bip_c in self._custom_bips.items():
                 i, j = idx
                 bip, dT_bip = bip_c(T)
 
+                # values are symmetric
                 bips[i][j] = bip
+                bips[j][i] = bip
                 dT_bips[i][j] = dT_bip
+                dT_bips[j][i] = dT_bip
 
         return bips, dT_bips
 
@@ -708,7 +704,8 @@ class PengRobinsonEoS(AbstractEoS):
             The component-specific critical covolume.
 
         """
-        return cls.B_CRIT * (R_IDEAL * T_crit) / p_crit
+        b = cls.B_CRIT * (R_IDEAL * T_crit) / p_crit
+        return b * ENERGY_SCALE / PRESSURE_SCALE
 
     @classmethod
     def _a_crit(cls, p_crit: float, T_crit: float) -> float:
@@ -725,7 +722,8 @@ class PengRobinsonEoS(AbstractEoS):
             The component-specific critical cohesion.
 
         """
-        return cls.A_CRIT * (R_IDEAL**2 * T_crit**2) / p_crit
+        a = cls.A_CRIT * (R_IDEAL**2 * T_crit**2) / p_crit
+        return a * ENERGY_SCALE**2 / PRESSURE_SCALE
 
     @staticmethod
     def _a_cor(omega: float) -> float:
@@ -742,7 +740,7 @@ class PengRobinsonEoS(AbstractEoS):
             acentric factor.
 
         """
-        if omega < 0.49:
+        if omega < 0.491:
             return 0.37464 + 1.54226 * omega - 0.26992 * omega**2
         else:
             return (
@@ -784,17 +782,21 @@ class PengRobinsonEoS(AbstractEoS):
     def _A(a: NumericType, p: NumericType, T: NumericType) -> NumericType:
         """Auxiliary method implementing formula for non-dimensional cohesion."""
         if isinstance(T, pp.ad.AdArray):
-            return T ** (-2) * a * p / R_IDEAL**2
+            A = T ** (-2) * a * p / R_IDEAL**2
         else:
-            return a * p / (R_IDEAL**2 * T**2)
+            A = a * p / (R_IDEAL**2 * T**2)
+
+        return A * PRESSURE_SCALE / ENERGY_SCALE**2
 
     @staticmethod
     def _B(b: NumericType, p: NumericType, T: NumericType) -> NumericType:
         """Auxiliary method implementing formula for non-dimensional covolume."""
         if isinstance(T, pp.ad.AdArray):
-            return T ** (-1) * b * p / R_IDEAL
+            B = T ** (-1) * b * p / R_IDEAL
         else:
-            return b * p / (R_IDEAL * T)
+            B = b * p / (R_IDEAL * T)
+
+        return B * PRESSURE_SCALE / ENERGY_SCALE
 
     # TODO
     def _kappa(self, p: NumericType, T: NumericType, Z: NumericType) -> NumericType:
@@ -850,20 +852,22 @@ class PengRobinsonEoS(AbstractEoS):
             * (dT_a * T - a)
             / b
             * pp.ad.log((Z + (1 + np.sqrt(2)) * B) / (Z + (1 - np.sqrt(2)) * B))
-            + (Z - 1) * T * R_IDEAL
+            + (Z - 1) * T * R_IDEAL * ENERGY_SCALE
         )
 
     @staticmethod
     def _g_dep(A: NumericType, B: NumericType, Z: NumericType):
         """Auxiliary function for computing the Gibbs departure function."""
-        return pp.ad.log(Z - B) - pp.ad.log(
+        return pp.ad.log(pp.ad.maximum(Z - B, 1e-5)) - pp.ad.log(
             (Z + (1 + np.sqrt(2)) * B) / (Z + (1 - np.sqrt(2)) * B)
         ) * A / B / np.sqrt(8)
 
     @staticmethod
     def _g_ideal(T: NumericType, X: list[NumericType]) -> NumericType:
         """Auxiliary function to compute the ideal part of the Gibbs energy."""
-        return safe_sum([x * pp.ad.log(x) for x in X]) * T * R_IDEAL
+        return (
+            safe_sum([x * pp.ad.log(pp.ad.maximum(x, 1e-5)) for x in X]) * T * R_IDEAL
+        )
 
     @staticmethod
     def _phi_i(
@@ -876,7 +880,7 @@ class PengRobinsonEoS(AbstractEoS):
         """Auxiliary method implementing the formula for the fugacity coefficient."""
         log_phi_i = (
             (Z - 1) / B * B_i
-            - pp.ad.log(Z - B)
+            - pp.ad.log(pp.ad.maximum(Z - B, 1e-5))
             - A
             / (B * np.sqrt(8))
             * (A_i / A - B ** (-1) * B_i)
@@ -983,6 +987,9 @@ class PengRobinsonEoS(AbstractEoS):
         # AD-arrays
         # identify super-critical line
         self.is_supercritical = B >= self.B_CRIT / self.A_CRIT * A
+        # identify approximated sub pseudo-critical line (approximates Widom line)
+        widom_line = B <= self.B_CRIT + 0.8 * 0.3381965009398633 * (A - self.A_CRIT)
+
         # At A,B=0 we have 2 real roots, one with multiplicity 2
         zero_point = np.logical_and(
             np.logical_and(A >= -self.eps, A <= self.eps),
@@ -1044,6 +1051,7 @@ class PengRobinsonEoS(AbstractEoS):
             q_ = q[one_root_region]
             delta_ = delta[one_root_region]
             c2_ = c2[one_root_region]
+            B_ = B[one_root_region]
 
             # delta has only positive values in this case by logic
             t = -q_ / 2 + pp.ad.sqrt(delta_)
@@ -1069,13 +1077,15 @@ class PengRobinsonEoS(AbstractEoS):
 
             # real part of the conjugate imaginary roots
             # used for extension of vanished roots
-            w = -real_part / 2 - c2_ / 3
+            # w = -real_part / 2 - c2_ / 3 + 2.0 * B + self.B_CRIT
+            # w = (1 - B - z_1) / 2 + B + self.B_CRIT
+            w = (1 - B_ - z_1) / 2 + B_
 
-            extension_is_bigger = z_1 < w
+            extension_is_bigger = widom_line[one_root_region]  # z_1 < w
 
-            correction = ~(acbc_rect[one_root_region])
-            w[correction] = z_1[correction]
-            extension_is_bigger[correction] = False
+            # correction = ~(acbc_rect[one_root_region])
+            # w[correction] = z_1[correction]
+            # extension_is_bigger[correction] = False
 
             # slice indices such that w is the smaller root
             z_1_small = z_1[extension_is_bigger]
@@ -1176,19 +1186,19 @@ class PengRobinsonEoS(AbstractEoS):
             Z_L[region] = z_23
             Z_G[region] = z_1
 
-        # Correct the smaller root if it violates the lower bound B
-        correction = Z_L <= B
-        if np.any(correction):
-            Z_L[correction] = B[correction] + self.eps
+        # # Correct the smaller root if it violates the lower bound B
+        # correction = Z_L <= B
+        # if np.any(correction):
+        #     Z_L[correction] = B[correction] + self.eps
 
         # assert physical meaningfulness
         # assert np.all(
         #     Z_L > B
         # ), "Liquid root violates lower physical bound given by covolume B."
         # assert gas root is bigger than liquid root
-        assert np.all(
-            Z_G >= Z_L
-        ), "Liquid root violates upper physical bound given by gas root."
+        # assert np.all(
+        #     Z_G >= Z_L
+        # ), "Liquid root violates upper physical bound given by gas root."
 
         # convert Jacobians to csr
         if isinstance(Z_L, pp.ad.AdArray):