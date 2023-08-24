0c3233231b0b23470d597f5b366aaf6a3e635d31
Ingrid Kristine Jacobsen <ingrid.kristine.jacobsen@gmail.com>
2023-08-24 11:57:38 +0200

    MAINT: Include units to solid value dics. Typo fix.

--- src/porepy/applications/material_values/solid_values.py
+++ src/porepy/applications/material_values/solid_values.py
@@ -1,4 +1,4 @@
-"""This file contains values for fluid parameters.
+"""This file contains values for solid parameters.
 
 
 Granite values.
@@ -8,13 +8,37 @@ TODO: Add values and references.
 """
 
 granite = {
-    "biot_coefficient": None,  # []
-    "density": None,  # []
-    "lame_lambda": None,  # []
-    "permeability": None,  # []
-    "porosity": None,  # []
-    "shear_modulus": None,  # []
-    "specific_heat_capacity": None,  # []
-    "thermal_conductivity": None,  # []
-    "thermal_expansion": None,  # []
+    "biot_coefficient": None,  # [-]
+    "density": None,  # [kg * m^-3]
+    "friction_coefficient": None,  # [-]
+    "lame_lambda": None,  # [Pa]
+    "permeability": None,  # [m^2]
+    "porosity": None,  # [-]
+    "shear_modulus": None,  # [Pa]
+    "specific_heat_capacity": None,  # [J * kg^-1 * K^-1]
+    "specific_storage": None,  # [Pa^-1]
+    "thermal_conductivity": None,  # [W * m^-1 * K^-1]
+    "thermal_expansion": None,  # [K^-1]
+}
+
+
+"""
+Basalt values.
+
+TODO: Add values and references.
+
+"""
+
+basalt = {
+    "biot_coefficient": None,  # [-]
+    "density": None,  # [kg * m^-3]
+    "friction_coefficient": None,  # [-]
+    "lame_lambda": None,  # [Pa]
+    "permeability": None,  # [m^2]
+    "porosity": None,  # [-]
+    "shear_modulus": None,  # [Pa]
+    "specific_heat_capacity": None,  # [J * kg^-1 * K^-1]
+    "specific_storage": None,  # [Pa^-1]
+    "thermal_conductivity": None,  # [W * m^-1 * K^-1]
+    "thermal_expansion": None,  # [K^-1]
 }
