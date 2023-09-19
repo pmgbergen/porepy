fffb637a5ffe5381509010230c5d03b337c6200f
taj004 <shin_banshoya@hotmail.com>
2023-09-19 11:00:11 +0200

    fixed colourbar problem

--- src/porepy/viz/plot_grid.py
+++ src/porepy/viz/plot_grid.py
@@ -203,7 +203,7 @@ def plot_sd(
 
     # Add color map if provided.
     if kwargs.get("color_map"):
-        fig.colorbar(kwargs["color_map"])
+        fig.colorbar(kwargs["color_map"], ax=ax)
 
     # Draw and potentially show the plot.
     plt.draw()
@@ -323,7 +323,7 @@ def plot_mdg(
 
     # Add color map if provided
     if kwargs.get("color_map"):
-        fig.colorbar(kwargs["color_map"])
+        fig.colorbar(kwargs["color_map"], ax=ax)
 
     # Draw and potentially show the plot.
     plt.draw()
