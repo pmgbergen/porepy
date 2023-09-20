d1a20112855b86deabae1554b7729fe28ef1632c
Veljko Lipovac <lipovac.v@outlook.com>
2023-09-20 16:09:45 +0000

    FIX: Parallel Newton. BUG: CPU not fully loaded.

--- scripts_vl/prc.py
+++ scripts_vl/prc.py
@@ -1,11 +1,16 @@
 import numpy as np
 
 import porepy as pp
+import sympy as sm
 import numba
+import time
 
 numba.warnings.simplefilter('ignore', numba.NumbaExperimentalFeatureWarning)
 
 from typing import Callable
+
+from porepy.composite import safe_sum
+from matplotlib import pyplot as plt
 
 vec = np.ones(1)
 z = [vec * 0.99,  vec * 0.01]
@@ -13,7 +18,7 @@ p = vec * 5e6
 T = vec * 500
 verbosity = 2
 
-t = np.array([0.01, 5e6, 500, 0.9, 0.1, 0.2, 0.3, 0.4])
+x_test = np.array([0.01, 5e6, 500, 0.9, 0.1, 0.2, 0.3, 0.4])
 
 chems = ["H2O", "CO2"]
 species = pp.composite.load_species(chems)
@@ -41,18 +46,13 @@ mix.set_up()
 
 PRC = pp.composite.peng_robinson.PR_Compiler(mix)
 
-print("test call ---")
-print(PRC.equations['p-T'](t))
-print(PRC.jacobians['p-T'](t))
-print("-------------")
-
 flash = pp.composite.FlashNR(mix)
 flash.use_armijo = True
-flash.armijo_parameters["rho"] = 0.9
+flash.armijo_parameters["rho"] = 0.99
 flash.armijo_parameters["j_max"] = 150
 flash.armijo_parameters["return_max"] = True
 flash.newton_update_chop = 1.0
-flash.tolerance = 1e-5
+flash.tolerance = 1e-7
 flash.max_iter = 150
 
 # success, results_pT = flash.flash(
@@ -64,9 +64,6 @@ _, oldsys = flash.flash(
     quickshot=True,
     return_system=True,
 )
-# print("Results p-T:\n" + "------------")
-# print(str(results_pT))
-# print("------------")
 
 initstate = oldsys.state
 oldsys_eval = oldsys(initstate, True)
@@ -74,10 +71,10 @@ print("old value", oldsys_eval.val)
 print("old matrix")
 print(oldsys_eval.jac.todense()[:-1, :-1])
 
-t[3:] = initstate[:-1]
+x_test[3:] = initstate[:-1]
 
-newval = PRC.equations['p-T'](t)
-newjac = PRC.jacobians['p-T'](t)
+newval = PRC.equations['p-T'](x_test)
+newjac = PRC.jacobians['p-T'](x_test)
 print('new val')
 print(newval)
 print('new matrix')
@@ -86,6 +83,212 @@ print(newjac)
 print("diff vals", np.linalg.norm(newval - oldsys_eval.val[:-1]))
 print("diff jac", np.linalg.norm(newjac - oldsys_eval.jac.todense()[:-1, :-1]))
 
+# NPIPM
+ncomp = mix.num_components
+nphase = mix.num_phases
+
+u = 1.
+eta = .5
+
+F_pT = PRC.equations['p-T']
+DF_pT = PRC.jacobians['p-T']
+
+parser_xyz = PRC._sysfuncs['parser-xyz']
+
+
+def pos_symbolic(a: sm.Expr) -> sm.Expr:
+    return sm.Piecewise((a, a > 0.), (0, a <= 0.))
+
+def neg_symbolic(a: sm.Expr) -> sm.Expr:
+    return sm.Piecewise((a, a < 0.), (0, a >= 0.))
+
+
+V_s = [sm.Symbol(f'v_{j}') for j in range(1, nphase + 1)]
+W_s = [sm.Symbol(f'w_{j}') for j in range(1, nphase + 1)]
+nu_s = sm.Symbol('nu')
+
+slack_arg = [V_s, W_s, nu_s]  # two vector args, one scalar arg
+
+# decrease of nu
+slack_equ_e = eta * nu_s + nu_s ** 2
+# punishment of negative parts (original inequality cosntraint)
+slack_equ_e +=  safe_sum([neg_symbolic(v) ** 2 + neg_symbolic(w) ** 2 for v, w in zip(V_s, W_s)]) / 2
+# punishment of positive parts (CC violation)
+slack_equ_e += (pos_symbolic(safe_sum([v * w for v, w in zip(V_s, W_s)])))**2 * u / nphase**2 / 2
+
+d_slack_equ_e = [slack_equ_e.diff(_) for _ in V_s] + [slack_equ_e.diff(_) for _ in W_s] + [slack_equ_e.diff(nu_s)]
+
+slack_equ_c = sm.lambdify(slack_arg, slack_equ_e, 'math')
+slack_equ_c = numba.njit('float64(float64[:], float64[:], float64)', fastmath=True)(slack_equ_c)
+
+def _njit_diff(f, **njit_kwargs):
+    """Helper function for special wrapping of some compiled derivative of slack equation."""
+
+    f = numba.njit(f, **njit_kwargs)
+
+    @numba.njit("float64[:](float64[:], float64[:], float64)", **njit_kwargs)
+    def inner(a, b, c):
+        return np.array(f(a, b, c))
+
+    return inner
+
+d_slack_equ_c = sm.lambdify(slack_arg, d_slack_equ_e, 'math')
+d_slack_equ_c = _njit_diff(d_slack_equ_c, fastmath=True)
+
+
+@numba.njit('float64[:](float64[:])')
+def F_npipm(X: np.ndarray) -> np.ndarray:
+
+    X_thd = X[:-1]
+    nu = X[-1]
+    x, y, _ = parser_xyz(X_thd)
+
+    f_flash = F_pT(X_thd)
+
+    # couple complementary conditions with nu
+    f_flash[-nphase:] -= nu
+
+    # NPIPM equation
+    unity_j = np.zeros(nphase)
+    for j in range(nphase):
+        unity_j[j] = 1. - np.sum(x[j])
+
+    # complete vector of fractions
+    v = np.zeros(nphase)
+    v[1:] = y
+    v[0] = 1 - np.sum(y)
+    slack = slack_equ_c(v, unity_j, nu)
+
+    # NPIPM system has one equation more at end
+    f_npipm = np.zeros(f_flash.shape[0] + 1)
+    f_npipm[:-1] = f_flash
+    f_npipm[-1] = slack
+
+    return f_npipm
+
+
+@numba.njit('float64[:,:](float64[:])')
+def DF_npipm(X: np.ndarray) -> np.ndarray:
+
+    X_thd = X[:-1]
+    nu = X[-1]
+    x, y, _ = parser_xyz(X_thd)
+
+    df_flash = DF_pT(X_thd)
+
+    # NPIPM matrix has one row and one column more
+    df_npipm = np.zeros((df_flash.shape[0] + 1, df_flash.shape[1] + 1))
+    df_npipm[:-1, :-1] = df_flash
+    # relaxed complementary conditions read as y * (1 - sum x) - nu
+    # add the -1 for the derivative w.r.t. nu
+    df_npipm[-(nphase + 1): -1, -1] = np.ones(nphase)*(-1)
+
+    # derivative NPIPM equation
+    unity_j = np.zeros(nphase)
+    for j in range(nphase):
+        unity_j[j] = 1. - np.sum(x[j])
+
+    # complete vector of fractions
+    v = np.zeros(nphase)
+    v[1:] = y
+    v[0] = 1 - np.sum(y)
+    d_slack = d_slack_equ_c(v, unity_j, nu)
+    # d slack has derivatives w.r.t. y_j and w_j
+    # d w_j must be expanded since w_j = 1 - sum x_j
+    # d y_0 must be expanded since reference phase is eliminated by unity
+    expand_yr = np.ones(nphase - 1) * (-1)
+    expand_x_in_j = np.ones(ncomp) * (-1)
+
+    # expansion of y_0 and cut of redundant value
+    d_slack[1: nphase - 1] += d_slack[0] * expand_yr
+    d_slack = d_slack[1:]
+
+    # expand it also to include possibly other derivatives
+    d_slack_expanded = np.zeros(df_npipm.shape[1])
+    # last derivative is w.r.t. nu
+    d_slack_expanded[-1] = d_slack[-1]
+    
+    for j in range(nphase):
+        # derivatives w.r.t. x_ij, +2 because nu must be skipped and j starts with 0
+        vec = expand_x_in_j * d_slack[-(j + 2)]
+        d_slack_expanded[-(1 + (j + 1) * ncomp):-(1 + j * ncomp)] = vec
+
+    # derivatives w.r.t y_j. j != r
+    d_slack_expanded[-(1 + nphase * ncomp + nphase - 1) : -(1 + nphase * ncomp)] = d_slack[:nphase - 1]
+
+    df_npipm[-1] = d_slack_expanded
+
+    return df_npipm
+
+
+@numba.njit('Tuple((float64[:,:], float64[:]))(float64[:], float64[:,:], float64[:])', fastmath=True, cache=True)
+def npipm_regularization(X, A, b):
+
+    x, y_, _ = parser_xyz(X[:-1])
+
+    reg = 0.
+    y = np.zeros(nphase)
+    y[1:] = y_
+    y[0] = 1 - np.sum(y_)
+    for j in range(nphase):
+        reg += y[j] * (1 - np.sum(x[j]))
+
+    reg = 0. if reg < 0 else reg
+    reg *= u / ncomp**2
+
+    # subtract all relaxed complementary conditions multiplied with reg from the slack equation
+    b[-1] = b[-1] - reg * np.sum(b[-(nphase + 1): -1])
+    # do the same for respective rows in the Jacobian
+    for j in range(nphase):
+        # +2 to skip slack equation and because j start with 0
+        v = A[-(j + 2)] * reg
+        A[-1] = A[-1] - v
+
+    return A, b
+
+
+# Numerical solvers
+@numba.njit('float64(float64[:])', fastmath=True, cache=True)
+def l2_potential(vec: np.ndarray) -> float:
+    # return np.dot(vec, vec) / 2.
+    return np.sum(vec * vec) / 2.
+
+
+@numba.njit
+def Armijo_line_search(
+    Xk: np.ndarray,
+    dXk: np.ndarray,
+    F: Callable[[np.ndarray], np.ndarray],
+) -> float:
+    
+    fk = F(Xk)
+    potk = l2_potential(fk)
+    rho_j = 0.99
+    kappa = 0.4
+    j_max = 150
+
+    for j in range(1, j_max + 1):
+        rho_j = rho_j ** j
+
+        try:
+            fk_j = F(Xk + rho_j * dXk)
+        except:
+            continue
+
+        potk_j = l2_potential(fk_j)
+
+        if potk_j <= (1 - 2 * kappa * rho_j) * potk:
+            return rho_j
+
+    # return max
+    return rho_j
+
+
+@numba.njit
+def dummy():
+    j = 1.
+    for i in range(1000):
+        j = j ** 2
 
 @numba.njit
 def newton(
@@ -101,6 +304,8 @@ def newton(
     success = 1  # 1 means max iter reached
 
     X = X0.copy()
+    DX = np.zeros_like(X0)
+
     f_i = F(X)
 
     if np.linalg.norm(f_i) <= tol:
@@ -108,11 +313,22 @@ def newton(
         return X, success, num_iter
     else:
         for i in range(1, max_iter + 1):
-            num_iter += i
+            num_iter += 1
 
             df_i = DF(X)
 
-            X -= np.linalg.solve(df_i, -f_i)
+            A, b = npipm_regularization(X, df_i, -f_i)
+
+            dx = np.linalg.solve(A, b)
+
+            # X contains also parameters (p, T, z_i, ...)
+            # exactly ncomp - 1 feed fractions and 2 state definitions (p-T, p-h, ...)
+            # for broadcasting insert solution into new vector
+            DX[ncomp - 1 + 2:] = dx
+
+            s = Armijo_line_search(X, DX, F)
+
+            X = X + s * DX
 
             f_i = F(X)
 
@@ -123,7 +339,11 @@ def newton(
         return X, success, num_iter
 
 
-@numba.njit(parallel=True)
+@numba.njit(
+    parallel=True,
+    nogil=True,
+    boundscheck=False,
+)
 def par_newton(
     X0: np.ndarray,
     F: Callable[[np.ndarray], np.ndarray],
@@ -140,15 +360,190 @@ def par_newton(
     I.e., ``len(F(X0[i])) == F_dim`` and ``DF(X0[i]).shape == (F_dim, F_dim)``"""
 
     N = X0.shape[0]
+    # done = np.zeros(N, dtype=np.int32)
+
     result = np.empty((N, F_dim))
-    num_iter = np.zeros(N, dtype=int)
-    converged = np.zeros(N, dtype=int)
+    num_iter = np.empty(N, dtype=np.int32)
+    converged = np.empty(N, dtype=np.int32)
 
     for n in numba.prange(N):
+    # for n in range(N):
 
         res_i, conv_i, n_i = newton(X0[n], F, DF, tol, max_iter)
+        # res_i = np.ones(F_dim)
+        # conv_i = 0
+        # n_i = 4
+        # for i in range(10000000):
+        #     res_i = res_i ** 9.5
         converged[n] = conv_i
         num_iter[n] = n_i
-        result[n] = res_i
+        result[n] = res_i[-F_dim:]
+
+        # done[n] = 1
+        # print(f"\rPar Newton done: {np.sum(done)} / {N}")
     
     return result, converged, num_iter
+
+NF = 20
+# dimension of  per flash: 1 z_i, 2 thd state (p, T), 1 y_j, 4 x_ij, 1 nu (npipm)
+xdim = 1 + 2 + 1 + 4 + 1
+
+x_, y_, _ = parser_xyz(x_test)
+
+y_ = np.concatenate([np.array([1 - np.sum(y_)]), y_])
+
+nu0 = safe_sum([y__ * (1 - np.sum(x__)) for y__, x__ in zip(y_, x_)]) / nphase
+
+x_test = np.concatenate([x_test, np.array([nu0])])
+X_total = np.repeat(x_test.reshape((1, len(x_test))), NF, axis=0)
+
+newval = F_npipm(x_test)
+newjac = DF_npipm(x_test)
+print('new val')
+print(newval)
+print('old val')
+print(oldsys_eval.val)
+print('diff')
+print(np.abs(newval - oldsys_eval.val))
+print('new matrix')
+print(newjac)
+print('old matrix')
+print(oldsys_eval.jac.todense())
+print('diff')
+print(np.abs(newjac - oldsys_eval.jac.todense()))
+
+result, conv, num_iter = par_newton(X_total, F_npipm, DF_npipm, 6, 1e-7, 150)
+
+success, results_old = flash.flash(
+    state={"p": p, "T": T},
+    eos_kwargs={"apply_smoother": True},
+    feed=z,
+    verbosity=verbosity,
+)
+old_iter = flash.history[-1]['iterations']
+
+tolerance = 1e-5
+
+if not np.all(conv == 0):
+    print(f"{len(conv[np.logical_not(conv==0)])} cases not converged")
+else:
+    print(f"All cases converged.")
+
+idx = num_iter == old_iter
+if not np.all(idx):
+    print(f"{len(num_iter[np.logical_not(idx)])} mismatches in number of iterations.")
+    print(f"Should be {old_iter}, got {num_iter[:4]} ...")
+else:
+    print("Number of iterations matches")
+
+if len(np.unique(num_iter)) != 1:
+    print(f"Heterogeneous number of iterations: {len(np.unique(num_iter))} different results.")
+
+idx = np.isclose(result[:, 0], results_old.y[1][0], rtol = 0, atol = tolerance)
+if not np.all(idx):
+    print(f"{len(result[:, 0][np.logical_not(idx)])} mismatches in results for y.")
+else:
+    print("No mismatch for y")
+
+idx = np.isclose(result[:, 1], results_old.X[0][0][0], rtol = 0, atol = tolerance)
+if not np.all(idx):
+    print(f"{len(result[:, 1][np.logical_not(idx)])} mismatches in results for x_11.")
+else:
+    print("No mismtach for x_11.")
+
+idx = np.isclose(result[:, 2], results_old.X[0][1][0], rtol = 0, atol = tolerance)
+if not np.all(idx):
+    print(f"{len(result[:, 2][np.logical_not(idx)])} mismatches in results for x_21.")
+else:
+    print("No mismtach for x_21.")
+
+idx = np.isclose(result[:, 3], results_old.X[1][0][0], rtol = 0, atol = tolerance)
+if not np.all(idx):
+    print(f"{len(result[:, 3][np.logical_not(idx)])} mismatches in results for x_12.")
+else:
+    print("No mismtach for x_12.")
+
+idx = np.isclose(result[:, 4], results_old.X[1][1][0], rtol = 0, atol = tolerance)
+if not np.all(idx):
+    print(f"{len(result[:, 4][np.logical_not(idx)])} mismatches in results for x_22.")
+else:
+    print("No mismtach for x_22.")
+
+idx = np.isclose(result[:, 5], 0., rtol = 0, atol = tolerance)
+if not np.all(idx):
+    print(f"{len(result[:, 5][np.logical_not(idx)])} violations for NPIPM slack.")
+else:
+    print("No violation for NPIPM slack.")
+
+
+
+NF_l = [100, 1000, 10000, 100000, 1000000, 5000000, 10000000]
+NF_l = [100, 1000, 10000]
+times_l = []
+
+for n in NF_l:
+
+    X_total = np.repeat(x_test.reshape((1, len(x_test))), n, axis=0)
+
+    start = time.time()
+    result, conv, num_iter = par_newton(X_total, F_npipm, DF_npipm, 6, 1e-7, 1000)
+    end = time.time()
+
+    times_l.append(end - start)
+
+    print(f"----- RESULTS FOR n={n}, tol = {tolerance}, time = {times_l[-1]} s")
+
+    if not np.all(conv == 0):
+        print(f"{len(conv[np.logical_not(conv==0)])} cases not converged")
+    else:
+        print(f"All cases converged.")
+
+    idx = num_iter == old_iter
+    if not np.all(idx):
+        print(f"{len(num_iter[np.logical_not(idx)])} mismatches in number of iterations.")
+        print(f"Should be {old_iter}, got {num_iter[:4]} ...")
+    else:
+        print("Number of iterations matches")
+
+    if len(np.unique(num_iter)) != 1:
+        print(f"Heterogeneous number of iterations: {len(np.unique(num_iter))} different results.")
+
+    idx = np.isclose(result[:, 0], results_old.y[1][0], rtol = 0, atol = tolerance)
+    if not np.all(idx):
+        print(f"{len(result[:, 0][np.logical_not(idx)])} mismatches in results for y.")
+    else:
+        print("No mismatch for y")
+
+    idx = np.isclose(result[:, 1], results_old.X[0][0][0], rtol = 0, atol = tolerance)
+    if not np.all(idx):
+        print(f"{len(result[:, 1][np.logical_not(idx)])} mismatches in results for x_11.")
+    else:
+        print("No mismtach for x_11.")
+
+    idx = np.isclose(result[:, 2], results_old.X[0][1][0], rtol = 0, atol = tolerance)
+    if not np.all(idx):
+        print(f"{len(result[:, 2][np.logical_not(idx)])} mismatches in results for x_21.")
+    else:
+        print("No mismtach for x_21.")
+
+    idx = np.isclose(result[:, 3], results_old.X[1][0][0], rtol = 0, atol = tolerance)
+    if not np.all(idx):
+        print(f"{len(result[:, 3][np.logical_not(idx)])} mismatches in results for x_12.")
+    else:
+        print("No mismtach for x_12.")
+
+    idx = np.isclose(result[:, 4], results_old.X[1][1][0], rtol = 0, atol = tolerance)
+    if not np.all(idx):
+        print(f"{len(result[:, 4][np.logical_not(idx)])} mismatches in results for x_22.")
+    else:
+        print("No mismtach for x_22.")
+
+    idx = np.isclose(result[:, 5], 0., rtol = 0, atol = tolerance)
+    if not np.all(idx):
+        print(f"{len(result[:, 5][np.logical_not(idx)])} violations for NPIPM slack.")
+    else:
+        print("No violation for NPIPM slack.")
+
+    print(f"-----")
+
+par_newton.parallel_diagnostics(level=4)
