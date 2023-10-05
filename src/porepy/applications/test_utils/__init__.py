2f80e6e45e4d60e7e08a7f5bf04080b7a8e8062f
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2023-10-05 08:48:10 +0000

    ENH: __init__ file in test_utils folder

---
+++ src/porepy/applications/test_utils/__init__.py
@@ -0,0 +1,8 @@
+"""The module contains utility functions for testing.
+
+Utility functions that are not specific to a single test module should be placed here.
+Functions which are relevant also outside tests should go elsewhere.
+
+"""
+from . import arrays
+from . import grids
