203261f03ae2bb1017878300ccca9b76bea8fd3b
Yury Zabegaev <yuri.zabegaev@uib.no>
2023-10-05 12:33:34 +0000

    TST: Updated `test_intersections.py`

--- tests/geometry/test_intersections.py
+++ tests/geometry/test_intersections.py
@@ -1,55 +1,50 @@
 """Test triangulation matching in intersections module
 """
-import unittest
-
 import numpy as np
 
 import porepy as pp
 
 
-class TestTriangulationMatching(unittest.TestCase):
-    def test_identical_triangles(self):
-        p = np.array([[0, 1, 0], [0, 0, 1]])
-        t = np.array([[0, 1, 2]]).T
+def test_identical_triangles():
+    p = np.array([[0, 1, 0], [0, 0, 1]])
+    t = np.array([[0, 1, 2]]).T
 
-        l = pp.intersections.triangulations(p, p, t, t)
-        self.assertTrue(len(l) == 1)
-        self.assertTrue(l[0][0] == 0)
-        self.assertTrue(l[0][1] == 0)
-        self.assertTrue(l[0][2] == 0.5)
-
-    def test_two_and_one(self):
-        p1 = np.array([[0, 1, 1, 0], [0, 0, 1, 1]])
-        t1 = np.array([[0, 1, 3], [1, 2, 3]]).T
-
-        p2 = np.array([[0, 1, 0], [0, 1, 1]])
-        t2 = np.array([[0, 1, 2]]).T
-
-        l = pp.intersections.triangulations(p1, p2, t1, t2)
-        self.assertTrue(len(l) == 2)
-        self.assertTrue(l[0][0] == 0)
-        self.assertTrue(l[0][1] == 0)
-        self.assertTrue(l[0][2] == 0.25)
-        self.assertTrue(l[1][0] == 1)
-        self.assertTrue(l[1][1] == 0)
-        self.assertTrue(l[1][2] == 0.25)
-
-    def test_one_and_two(self):
-        p1 = np.array([[0, 1, 1, 0], [0, 0, 1, 1]])
-        t1 = np.array([[0, 1, 3], [1, 2, 3]]).T
-
-        p2 = np.array([[0, 1, 0], [0, 1, 1]])
-        t2 = np.array([[0, 1, 2]]).T
-
-        l = pp.intersections.triangulations(p2, p1, t2, t1)
-        self.assertTrue(len(l) == 2)
-        self.assertTrue(l[0][0] == 0)
-        self.assertTrue(l[0][1] == 0)
-        self.assertTrue(l[0][2] == 0.25)
-        self.assertTrue(l[1][1] == 1)
-        self.assertTrue(l[1][0] == 0)
-        self.assertTrue(l[1][2] == 0.25)
+    triangulation = pp.intersections.triangulations(p, p, t, t)
+    assert len(triangulation) == 1
+    assert triangulation[0][0] == 0
+    assert triangulation[0][1] == 0
+    assert triangulation[0][2] == 0.5
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_two_and_one():
+    p1 = np.array([[0, 1, 1, 0], [0, 0, 1, 1]])
+    t1 = np.array([[0, 1, 3], [1, 2, 3]]).T
+
+    p2 = np.array([[0, 1, 0], [0, 1, 1]])
+    t2 = np.array([[0, 1, 2]]).T
+
+    triangulation = pp.intersections.triangulations(p1, p2, t1, t2)
+    assert len(triangulation) == 2
+    assert triangulation[0][0] == 0
+    assert triangulation[0][1] == 0
+    assert triangulation[0][2] == 0.25
+    assert triangulation[1][0] == 1
+    assert triangulation[1][1] == 0
+    assert triangulation[1][2] == 0.25
+
+
+def test_one_and_two():
+    p1 = np.array([[0, 1, 1, 0], [0, 0, 1, 1]])
+    t1 = np.array([[0, 1, 3], [1, 2, 3]]).T
+
+    p2 = np.array([[0, 1, 0], [0, 1, 1]])
+    t2 = np.array([[0, 1, 2]]).T
+
+    triangulation = pp.intersections.triangulations(p2, p1, t2, t1)
+    assert len(triangulation) == 2
+    assert triangulation[0][0] == 0
+    assert triangulation[0][1] == 0
+    assert triangulation[0][2] == 0.25
+    assert triangulation[1][1] == 1
+    assert triangulation[1][0] == 0
+    assert triangulation[1][2] == 0.25
