8d903650655ceed831e5817a8b6384e6c25d5551
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2023-10-06 11:08:29 +0000

    TST: fix test_meshing

--- tests/fracs/test_meshing.py
+++ tests/fracs/test_meshing.py
@@ -1,3 +1,8 @@
+"""Tests for the fracs.meshing module.
+
+So far, only a test of the function _tag_faces() is implemented.
+
+"""
 import unittest
 
 import numpy as np
@@ -5,106 +10,113 @@ import numpy as np
 from porepy.fracs import meshing
 
 
-class TestFaceTags(unittest.TestCase):
-    def test_x_intersection_2d(self):
-        """Check that the faces has correct tags for a 2D grid."""
+def test_fracture_and_boundary_face_tags_2d_domain_x_intersection():
+    """For a 2d Cartesian grid with two intersecting fractures, verify that the
+    boundary, fracture, and fracture tip face tags are correctly set.
 
-        f_1 = np.array([[0, 2], [1, 1]])
-        f_2 = np.array([[1, 1], [0, 2]])
+    The test is based on comparing the constructed and known tags.
 
-        f_set = [f_1, f_2]
-        nx = [3, 3]
+    """
 
-        grids = meshing.cart_grid(f_set, nx, physdims=nx)
+    # Define fractures, domain boundaries, and grid
+    f_1 = np.array([[0, 2], [1, 1]])
+    f_2 = np.array([[1, 1], [0, 2]])
 
-        # 2D grid:
-        g_2d = grids.subdomains(dim=2)[0]
+    f_set = [f_1, f_2]
+    nx = [3, 3]
+    # Generate grid. This will also tag the faces.
+    grids = meshing.cart_grid(f_set, nx, physdims=nx)
 
-        f_tags_2d = np.array(
-            [
-                False,
-                True,
-                False,
-                False,  # first row
-                False,
-                True,
-                False,
-                False,  # Second row
-                False,
-                False,
-                False,
-                False,  # third row
-                False,
-                False,
-                False,  # Bottom column
-                True,
-                True,
-                False,  # Second column
-                False,
-                False,
-                False,  # Third column
-                False,
-                False,
-                False,  # Top column
-                True,
-                True,
-                True,
-                True,
-            ]
-        )  # Added faces
+    # 2D grid:
+    g_2d = grids.subdomains(dim=2)[0]
 
-        d_tags_2d = np.array(
-            [
-                True,
-                False,
-                False,
-                True,  # first row
-                True,
-                False,
-                False,
-                True,  # Second row
-                True,
-                False,
-                False,
-                True,  # third row
-                True,
-                True,
-                True,  # Bottom column
-                False,
-                False,
-                False,  # Second column
-                False,
-                False,
-                False,  # Third column
-                True,
-                True,
-                True,  # Top column
-                False,
-                False,
-                False,
-                False,
-            ]
-        )  # Added Faces
-        t_tags_2d = np.zeros(f_tags_2d.size, dtype=bool)
+    f_tags_2d = np.array(
+        [
+            False,
+            True,
+            False,
+            False,  # first row
+            False,
+            True,
+            False,
+            False,  # Second row
+            False,
+            False,
+            False,
+            False,  # third row
+            False,
+            False,
+            False,  # Bottom column
+            True,
+            True,
+            False,  # Second column
+            False,
+            False,
+            False,  # Third column
+            False,
+            False,
+            False,  # Top column
+            True,
+            True,
+            True,
+            True,
+        ]
+    )  # Added faces
 
-        self.assertTrue(np.all(g_2d.tags["tip_faces"] == t_tags_2d))
-        self.assertTrue(np.all(g_2d.tags["fracture_faces"] == f_tags_2d))
-        self.assertTrue(np.all(g_2d.tags["domain_boundary_faces"] == d_tags_2d))
+    d_tags_2d = np.array(
+        [
+            True,
+            False,
+            False,
+            True,  # first row
+            True,
+            False,
+            False,
+            True,  # Second row
+            True,
+            False,
+            False,
+            True,  # third row
+            True,
+            True,
+            True,  # Bottom column
+            False,
+            False,
+            False,  # Second column
+            False,
+            False,
+            False,  # Third column
+            True,
+            True,
+            True,  # Top column
+            False,
+            False,
+            False,
+            False,
+        ]
+    )  # Added Faces
 
-        # 1D grids:
-        for g_1d in grids.subdomains(dim=1):
-            f_tags_1d = np.array([False, True, False, True])
-            if g_1d.face_centers[0, 0] > 0.1:
-                t_tags_1d = np.array([True, False, False, False])
-                d_tags_1d = np.array([False, False, True, False])
-            else:
-                t_tags_1d = np.array([False, False, True, False])
-                d_tags_1d = np.array([True, False, False, False])
+    # No fracture tips for the highest-dimensional grid
+    t_tags_2d = np.zeros(f_tags_2d.size, dtype=bool)
 
-            self.assertTrue(np.all(g_1d.tags["tip_faces"] == t_tags_1d))
-            self.assertTrue(np.all(g_1d.tags["fracture_faces"] == f_tags_1d))
-            self.assertTrue(np.all(g_1d.tags["domain_boundary_faces"] == d_tags_1d))
+    assert np.all(g_2d.tags["tip_faces"] == t_tags_2d)
+    assert np.all(g_2d.tags["fracture_faces"] == f_tags_2d)
+    assert np.all(g_2d.tags["domain_boundary_faces"] == d_tags_2d)
 
+    # 1D grids:
+    for g_1d in grids.subdomains(dim=1):
+        f_tags_1d = np.array([False, True, False, True])
+        # Use the coordinate of the first face to determine which of the fractures
+        # this is.
+        if g_1d.face_centers[0, 0] > 0.1:
+            t_tags_1d = np.array([True, False, False, False])
+            d_tags_1d = np.array([False, False, True, False])
+        else:
+            t_tags_1d = np.array([False, False, True, False])
+            d_tags_1d = np.array([True, False, False, False])
 
-if __name__ == "__main__":
-    unittest.main()
+        assert np.all(g_1d.tags["tip_faces"] == t_tags_1d)
+        assert np.all(g_1d.tags["fracture_faces"] == f_tags_1d)
+        assert np.all(g_1d.tags["domain_boundary_faces"] == d_tags_1d)
+
+    # The 0D grid in the intersection point has no faces, thus no tags.
