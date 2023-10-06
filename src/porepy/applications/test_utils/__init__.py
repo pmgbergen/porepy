1673fcab543c89920d2d129bff4b08c49ef45c32
Ivar Stefansson <ivar.stefansson@uib.no>
2023-10-06 17:18:18 +0200

    TST: Mono-dim xpfa

--- src/porepy/applications/test_utils/__init__.py
+++ src/porepy/applications/test_utils/__init__.py
@@ -4,4 +4,12 @@ Utility functions that are not specific to a single test module should be placed
 Functions which are relevant also outside tests should go elsewhere.
 
 """
-from . import arrays, grids, vtk, partial_discretization
+from . import (
+    arrays,
+    grids,
+    vtk,
+    partial_discretization,
+    common_xpfa_tests,
+    expected_arrays,
+    expected_matrices,
+)
---
+++ src/porepy/applications/test_utils/expected_arrays.py
@@ -0,0 +1,7 @@
+"""Expected values for the tests are collected in this file.
+
+The array should be named with the name of the test function, and the suffix
+_expected_{name_of_quantity}.
+
+"""
+import numpy as np
