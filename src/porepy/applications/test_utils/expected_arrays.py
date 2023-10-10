03cea09f20526a6bf9f24f7b388f9ed11d1d8235
Ivar Stefansson <ivar.stefansson@uib.no>
2023-10-10 10:03:09 +0200

    TST: Rework mpsa tests

--- src/porepy/applications/test_utils/expected_arrays.py
+++ src/porepy/applications/test_utils/expected_arrays.py
@@ -1,7 +1,60 @@
 """Expected values for the tests are collected in this file.
 
-The array should be named with the name of the test function, and the suffix
-_expected_{name_of_quantity}.
+For each test file, there should be a dictionary with the name of the test file. Inside
+this dictionary, there should be a dictionary with the name of the test function/class,
+containing the expected values for the test named according to what they are compared
+to.
 
 """
 import numpy as np
+
+
+# test_mpfa.py
+grad_bound_known = np.array(
+    [
+        [0.10416667, 0.0, 0.0, 0.0, 0.0, -0.02083333, 0.0, 0.0],
+        [0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        [0.10416667, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.02083333],
+        [0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.10416667, 0.0, 0.0, 0.02083333, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.10416667, 0.0, 0.0, 0.0, 0.0, -0.02083333],
+        [0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0],
+        [-0.02083333, 0.0, 0.0, 0.0, 0.0, 0.10416667, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0],
+        [0.0, 0.0, 0.02083333, 0.0, 0.0, 0.10416667, 0.0, 0.0],
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0],
+        [0.02083333, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.10416667],
+        [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0],
+        [0.0, 0.0, -0.02083333, 0.0, 0.0, 0.0, 0.0, 0.10416667],
+    ]
+)
+grad_cell = np.array(
+    [
+        [1.0, 0.0],
+        [0.0, 1.0],
+        [1.0, 0.0],
+        [0.0, 1.0],
+        [1.0, 0.0],
+        [0.0, 1.0],
+        [1.0, 0.0],
+        [0.0, 1.0],
+        [1.0, 0.0],
+        [0.0, 1.0],
+        [1.0, 0.0],
+        [0.0, 1.0],
+        [1.0, 0.0],
+        [0.0, 1.0],
+        [1.0, 0.0],
+        [0.0, 1.0],
+    ]
+)
+test_mpsa = {
+    "MpsaReconstructBoundaryDisplacement": {
+        "test_cart_2d": {
+            "grad_bound_known": grad_bound_known,
+            "grad_cell_known": grad_cell,
+        },
+    }
+}
