929bb5fd642c46321c961c77f8159a649431c339
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2023-10-13 11:23:04 +0000

    TST: Refactored tests for gmsh tags in 2d domains

--- tests/integration/test_tag_gmsh.py
+++ tests/integration/test_tag_gmsh.py
@@ -1,4 +1,5 @@
 import unittest
+import pytest
 
 import numpy as np
 
@@ -13,7 +14,37 @@ them.
 """
 
 
-class BasicsTest(unittest.TestCase):
+def check_boundary_faces(g: pp.Grid, num_auxiliary: int):
+    tag = np.where(g.tags[f"domain_boundary_line_{num_auxiliary}_faces"])[0]
+    assert np.allclose(g.face_centers[1, tag], 0)
+
+    tag = np.where(g.tags[f"domain_boundary_line_{num_auxiliary + 1}_faces"])[0]
+    assert np.allclose(g.face_centers[0, tag], 1)
+
+    tag = np.where(g.tags[f"domain_boundary_line_{num_auxiliary + 2}_faces"])[0]
+    assert np.allclose(g.face_centers[1, tag], 1)
+
+    tag = np.where(g.tags[f"domain_boundary_line_{num_auxiliary + 3}_faces"])[0]
+    assert np.allclose(g.face_centers[0, tag], 0)
+
+
+def check_auxiliary_fracture_faces(
+    g: pp.Grid,
+    key: str,
+    start: list[np.ndarray],
+    end: list[np.ndarray],
+    offset: int = 0,
+):
+    num_lines = len(start)
+    for i in range(num_lines):
+        tag = g.tags[f"{key}_{i + offset}_faces"]
+        dist, _ = pp.distances.points_segments(g.face_centers[:2], start[i], end[i])
+
+        assert np.allclose(dist[tag, 0], 0)
+        assert np.all(np.logical_not(np.allclose(dist[np.logical_not(tag), 0], 0)))
+
+
+class TestGmshTags:
     def test_face_tags_from_gmsh_before_grid_split(self):
         """Check that the faces of the grids returned from gmsh has correct tags
         before the grid is split. The domain is the unit square with two fractures
@@ -49,60 +80,34 @@ class BasicsTest(unittest.TestCase):
 
                     # Test position of the fracture faces. Fracture 1 is aligned with
                     # the y-axis, fracture 2 is aligned with the x-axis
-                    self.assertTrue(np.allclose(f_cc0[1], 0.5))
-                    self.assertTrue(np.allclose(f_cc1[0], 0.5))
+                    assert np.allclose(f_cc0[1], 0.5)
+                    assert np.allclose(f_cc1[0], 0.5)
 
                 if g.dim > 0:
                     db_cc = g.face_centers[:, g.tags["domain_boundary_faces"]]
                     # Test that domain boundary faces are on the boundary
-                    self.assertTrue(
-                        np.all(
-                            (np.abs(db_cc[0]) < 1e-10)
-                            | (np.abs(db_cc[1]) < 1e-10)
-                            | (np.abs(db_cc[0] - 1) < 1e-10)
-                            | (np.abs(db_cc[1] - 1) < 1e-10)
-                        )
+                    assert np.all(
+                        (np.abs(db_cc[0]) < 1e-10)
+                        | (np.abs(db_cc[1]) < 1e-10)
+                        | (np.abs(db_cc[0] - 1) < 1e-10)
+                        | (np.abs(db_cc[1] - 1) < 1e-10)
                     )
                 # No tip faces in this test case
-                self.assertTrue(np.all(g.tags["tip_faces"] == 0))
+                assert np.all(g.tags["tip_faces"] == 0)
 
     def test_boundary(self):
+        """No fractures, test that the boundary faces are correct."""
         network = pp.create_fracture_network(None, unit_domain(2))
         mesh_args = {"mesh_size_frac": 1}
         mdg = network.mesh(mesh_args)
         g = mdg.subdomains(dim=2)[0]
+        check_boundary_faces(g, 0)
 
-        tag = np.where(g.tags["domain_boundary_line_0_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[1, tag], 0))
-
-        tag = np.where(g.tags["domain_boundary_line_1_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[0, tag], 1))
-
-        tag = np.where(g.tags["domain_boundary_line_2_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[1, tag], 1))
-
-        tag = np.where(g.tags["domain_boundary_line_3_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[0, tag], 0))
-
-    def test_boundary_refined(self):
-        network = pp.create_fracture_network(None, unit_domain(2))
-        mesh_args = {"mesh_size_frac": 1}
-        mdg = network.mesh(mesh_args)
-        g = mdg.subdomains(dim=2)[0]
-
-        tag = np.where(g.tags["domain_boundary_line_0_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[1, tag], 0))
-
-        tag = np.where(g.tags["domain_boundary_line_1_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[0, tag], 1))
-
-        tag = np.where(g.tags["domain_boundary_line_2_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[1, tag], 1))
-
-        tag = np.where(g.tags["domain_boundary_line_3_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[0, tag], 0))
-
-    def test_auxiliary(self):
+    @pytest.mark.parametrize(
+        "mesh_args", [{"mesh_size_frac": 1}, {"mesh_size_frac": 0.33}]
+    )
+    def test_single_constraint(self, mesh_args):
+        """A single auxiliary line (constraints)"""
         subdomain_start = np.array([[0.25], [0.5]])
         subdomain_end = np.array([[0.5], [0.5]])
         p = np.hstack((subdomain_start, subdomain_end))
@@ -114,63 +119,15 @@ class BasicsTest(unittest.TestCase):
         mdg = network.mesh(mesh_args, constraints=np.array([0]))
         g = mdg.subdomains(dim=2)[0]
 
-        tag = np.where(g.tags["domain_boundary_line_1_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[1, tag], 0))
-
-        tag = np.where(g.tags["domain_boundary_line_2_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[0, tag], 1))
-
-        tag = np.where(g.tags["domain_boundary_line_3_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[1, tag], 1))
-
-        tag = np.where(g.tags["domain_boundary_line_4_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[0, tag], 0))
-
-        fc = g.face_centers[:2]
-
-        tag = g.tags["auxiliary_line_0_faces"]
-        dist, _ = pp.distances.points_segments(fc, subdomain_start, subdomain_end)
-
-        self.assertTrue(np.allclose(dist[tag, 0], 0))
-        self.assertTrue(
-            np.all(np.logical_not(np.allclose(dist[np.logical_not(tag), 0], 0)))
+        check_boundary_faces(g, 1)
+        check_auxiliary_fracture_faces(
+            g, "auxiliary_line", [subdomain_start], [subdomain_end]
         )
 
-    def test_auxiliary_refined(self):
-        subdomain_start = np.array([[0.25], [0.5]])
-        subdomain_end = np.array([[0.5], [0.5]])
-        p = np.hstack((subdomain_start, subdomain_end))
-        e = np.array([[0], [1]])
-        fractures = pts_edges_to_linefractures(p, e)
-        network = pp.create_fracture_network(fractures, unit_domain(2))
-
-        mesh_args = {"mesh_size_frac": 0.33}
-        mdg = network.mesh(mesh_args, constraints=np.array([0]))
-        g = mdg.subdomains(dim=2)[0]
-
-        tag = np.where(g.tags["domain_boundary_line_1_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[1, tag], 0))
-
-        tag = np.where(g.tags["domain_boundary_line_2_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[0, tag], 1))
-
-        tag = np.where(g.tags["domain_boundary_line_3_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[1, tag], 1))
-
-        tag = np.where(g.tags["domain_boundary_line_4_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[0, tag], 0))
-
-        fc = g.face_centers[:2]
-
-        tag = g.tags["auxiliary_line_0_faces"]
-        dist, _ = pp.distances.points_segments(fc, subdomain_start, subdomain_end)
-
-        self.assertTrue(np.allclose(dist[tag, 0], 0))
-        self.assertTrue(
-            np.all(np.logical_not(np.allclose(dist[np.logical_not(tag), 0], 0)))
-        )
-
-    def test_auxiliary_2(self):
+    @pytest.mark.parametrize(
+        "mesh_args", [{"mesh_size_frac": 1}, {"mesh_size_frac": 0.25}]
+    )
+    def test_two_constraints(self, mesh_args):
         constraint_start_0 = np.array([[0.0], [0.5]])
         constraint_end_0 = np.array([[0.75], [0.5]])
         constraint_start_1 = np.array([[0.5], [0.25]])
@@ -182,190 +139,34 @@ class BasicsTest(unittest.TestCase):
         fractures = pts_edges_to_linefractures(p, e)
         network = pp.create_fracture_network(fractures, unit_domain(2))
 
-        mesh_args = {"mesh_size_frac": 1}
         mdg = network.mesh(mesh_args, constraints=np.arange(2))
         g = mdg.subdomains(dim=2)[0]
 
-        tag = np.where(g.tags["domain_boundary_line_2_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[1, tag], 0))
+        check_boundary_faces(g, 2)
 
-        tag = np.where(g.tags["domain_boundary_line_3_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[0, tag], 1))
-
-        tag = np.where(g.tags["domain_boundary_line_4_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[1, tag], 1))
-
-        tag = np.where(g.tags["domain_boundary_line_5_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[0, tag], 0))
-
-        fc = g.face_centers[:2]
-
-        tag = g.tags["auxiliary_line_0_faces"]
-        dist, _ = pp.distances.points_segments(fc, constraint_start_0, constraint_end_0)
-
-        self.assertTrue(np.allclose(dist[tag, 0], 0))
-        self.assertTrue(
-            np.all(np.logical_not(np.allclose(dist[np.logical_not(tag), 0], 0)))
+        check_auxiliary_fracture_faces(
+            g,
+            "auxiliary_line",
+            [constraint_start_0, constraint_start_1],
+            [constraint_end_0, constraint_end_1],
         )
 
-        tag = g.tags["auxiliary_line_1_faces"]
-        dist, _ = pp.distances.points_segments(fc, constraint_start_1, constraint_end_1)
-
-        self.assertTrue(np.allclose(dist[tag, 0], 0))
-        self.assertTrue(
-            np.all(np.logical_not(np.allclose(dist[np.logical_not(tag), 0], 0)))
-        )
-
-    def test_auxiliary_2_refined(self):
-        constraint_start_0 = np.array([[0.0], [0.5]])
-        constraint_end_0 = np.array([[0.75], [0.5]])
-        constraint_start_1 = np.array([[0.5], [0.25]])
-        constraint_end_1 = np.array([[0.5], [0.75]])
-        p = np.hstack(
-            (constraint_start_0, constraint_end_0, constraint_start_1, constraint_end_1)
-        )
-        e = np.array([[0, 2], [1, 3]])
-        fractures = pts_edges_to_linefractures(p, e)
-        network = pp.create_fracture_network(fractures, unit_domain(2))
-
-        mesh_args = {"mesh_size_frac": 1}
-        mdg = network.mesh(mesh_args, constraints=np.array([0, 1]))
-        g = mdg.subdomains(dim=2)[0]
-
-        tag = np.where(g.tags["domain_boundary_line_2_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[1, tag], 0))
-
-        tag = np.where(g.tags["domain_boundary_line_3_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[0, tag], 1))
-
-        tag = np.where(g.tags["domain_boundary_line_4_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[1, tag], 1))
-
-        tag = np.where(g.tags["domain_boundary_line_5_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[0, tag], 0))
-
-        fc = g.face_centers[:2]
-
-        tag = g.tags["auxiliary_line_0_faces"]
-        dist, _ = pp.distances.points_segments(fc, constraint_start_0, constraint_end_0)
-
-        self.assertTrue(np.allclose(dist[tag, 0], 0))
-        self.assertTrue(
-            np.all(np.logical_not(np.allclose(dist[np.logical_not(tag), 0], 0)))
-        )
-
-        tag = g.tags["auxiliary_line_1_faces"]
-        dist, _ = pp.distances.points_segments(fc, constraint_start_1, constraint_end_1)
-
-        self.assertTrue(np.allclose(dist[tag, 0], 0))
-        self.assertTrue(
-            np.all(np.logical_not(np.allclose(dist[np.logical_not(tag), 0], 0)))
-        )
-
-    def test_fracture(self):
+    @pytest.mark.parametrize(
+        "mesh_args", [{"mesh_size_frac": 1}, {"mesh_size_frac": 0.25}]
+    )
+    def test_single_fracture(self, mesh_args):
         frac_start = np.array([[0.25], [0.5]])
         frac_end = np.array([[0.75], [0.5]])
         mdg, _ = pp.md_grids_2d.single_horizontal(
-            {"mesh_size_frac": 1}, x_endpoints=np.array([0.25, 0.75])
+            mesh_args, x_endpoints=np.array([0.25, 0.75])
         )
         g = mdg.subdomains(dim=2)[0]
 
-        tag = np.where(g.tags["domain_boundary_line_1_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[1, tag], 0))
+        check_boundary_faces(g, 1)
 
-        tag = np.where(g.tags["domain_boundary_line_2_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[0, tag], 1))
+        check_auxiliary_fracture_faces(g, "fracture", [frac_start], [frac_end])
 
-        tag = np.where(g.tags["domain_boundary_line_3_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[1, tag], 1))
-
-        tag = np.where(g.tags["domain_boundary_line_4_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[0, tag], 0))
-
-        fc = g.face_centers[:2]
-        tag = g.tags["fracture_0_faces"]
-
-        dist, _ = pp.distances.points_segments(fc, frac_start, frac_end)
-
-        self.assertTrue(np.allclose(dist[tag, 0], 0))
-        self.assertTrue(
-            np.all(np.logical_not(np.allclose(dist[np.logical_not(tag), 0], 0)))
-        )
-
-    def test_fracture_refined(self):
-        frac_start = np.array([[0.25], [0.5]])
-        frac_end = np.array([[0.75], [0.5]])
-
-        mdg, _ = pp.md_grids_2d.single_horizontal(
-            {"mesh_size_frac": 0.125}, x_endpoints=np.array([0.25, 0.75])
-        )
-        g = mdg.subdomains(dim=2)[0]
-
-        tag = np.where(g.tags["domain_boundary_line_1_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[1, tag], 0))
-
-        tag = np.where(g.tags["domain_boundary_line_2_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[0, tag], 1))
-
-        tag = np.where(g.tags["domain_boundary_line_3_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[1, tag], 1))
-
-        tag = np.where(g.tags["domain_boundary_line_4_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[0, tag], 0))
-
-        fc = g.face_centers[:2]
-        tag = g.tags["fracture_0_faces"]
-
-        dist, _ = pp.distances.points_segments(fc, frac_start, frac_end)
-
-        self.assertTrue(np.allclose(dist[tag, 0], 0))
-        self.assertTrue(
-            np.all(np.logical_not(np.allclose(dist[np.logical_not(tag), 0], 0)))
-        )
-
-    def test_fracture_2(self):
-        frac_start_0 = np.array([[0.0], [0.5]])
-        frac_end_0 = np.array([[0.75], [0.5]])
-        frac_start_1 = np.array([[0.5], [0.25]])
-        frac_end_1 = np.array([[0.5], [0.75]])
-
-        mdg, _ = pp.md_grids_2d.two_intersecting(
-            {"mesh_size_frac": 1}, x_endpoints=[0.0, 0.75], y_endpoints=[0.25, 0.75]
-        )
-        g = mdg.subdomains(dim=2)[0]
-
-        tag = np.where(g.tags["domain_boundary_line_2_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[1, tag], 0))
-
-        tag = np.where(g.tags["domain_boundary_line_3_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[0, tag], 1))
-
-        tag = np.where(g.tags["domain_boundary_line_4_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[1, tag], 1))
-
-        tag = np.where(g.tags["domain_boundary_line_5_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[0, tag], 0))
-
-        fc = g.face_centers[:2]
-        tag = g.tags["fracture_0_faces"]
-
-        dist, _ = pp.distances.points_segments(fc, frac_start_0, frac_end_0)
-
-        self.assertTrue(np.allclose(dist[tag, 0], 0))
-        self.assertTrue(
-            np.all(np.logical_not(np.allclose(dist[np.logical_not(tag), 0], 0)))
-        )
-
-        tag = g.tags["fracture_1_faces"]
-
-        dist, _ = pp.distances.points_segments(fc, frac_start_1, frac_end_1)
-
-        self.assertTrue(np.allclose(dist[tag, 0], 0))
-        self.assertTrue(
-            np.all(np.logical_not(np.allclose(dist[np.logical_not(tag), 0], 0)))
-        )
-
-    def test_fracture_auxiliary(self):
+    def test_fracture_and_constraints(self):
         frac_start = np.array([[0.5], [0.25]])
         frac_end = np.array([[0.5], [0.75]])
         constraint_start = np.array([[0.25], [0.5]])
@@ -380,83 +181,9 @@ class BasicsTest(unittest.TestCase):
         mdg = network.mesh(mesh_args, constraints=constraints)
         g = mdg.subdomains(dim=2)[0]
 
-        tag = np.where(g.tags["domain_boundary_line_2_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[1, tag], 0))
+        check_boundary_faces(g, 2)
 
-        tag = np.where(g.tags["domain_boundary_line_3_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[0, tag], 1))
-
-        # domain_boundary_line_2 should be at y=1
-        tag = np.where(g.tags["domain_boundary_line_4_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[1, tag], 1))
-
-        tag = np.where(g.tags["domain_boundary_line_5_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[0, tag], 0))
-
-        fc = g.face_centers[:2]
-        tag = g.tags["fracture_0_faces"]
-
-        dist, _ = pp.distances.points_segments(fc, frac_start, frac_end)
-
-        self.assertTrue(np.allclose(dist[tag, 0], 0))
-        self.assertTrue(
-            np.all(np.logical_not(np.allclose(dist[np.logical_not(tag), 0], 0)))
+        check_auxiliary_fracture_faces(g, "fracture", [frac_start], [frac_end])
+        check_auxiliary_fracture_faces(
+            g, "auxiliary_line", [constraint_start], [constraint_end], 1
         )
-
-        tag = g.tags["auxiliary_line_1_faces"]
-        dist, _ = pp.distances.points_segments(fc, constraint_start, constraint_end)
-
-        self.assertTrue(np.allclose(dist[tag, 0], 0))
-        self.assertTrue(
-            np.all(np.logical_not(np.allclose(dist[np.logical_not(tag), 0], 0)))
-        )
-
-    def test_auxiliary_intersect(self):
-        constraint_start_0 = np.array([[0.0], [0.5]])
-        constraint_end_0 = np.array([[0.75], [0.5]])
-        constraint_start_1 = np.array([[0.5], [0.25]])
-        constraint_end_1 = np.array([[0.5], [0.75]])
-        p = np.hstack(
-            (constraint_start_0, constraint_end_0, constraint_start_1, constraint_end_1)
-        )
-        e = np.array([[0, 2], [1, 3]])
-        fractures = pts_edges_to_linefractures(p, e)
-        network = pp.create_fracture_network(fractures, unit_domain(2))
-
-        mesh_args = {"mesh_size_frac": 1}
-        mdg = network.mesh(mesh_args, constraints=np.array([0, 1]))
-        g = mdg.subdomains(dim=2)[0]
-
-        tag = np.where(g.tags["domain_boundary_line_2_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[1, tag], 0))
-
-        tag = np.where(g.tags["domain_boundary_line_3_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[0, tag], 1))
-
-        tag = np.where(g.tags["domain_boundary_line_4_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[1, tag], 1))
-
-        tag = np.where(g.tags["domain_boundary_line_5_faces"])[0]
-        self.assertTrue(np.allclose(g.face_centers[0, tag], 0))
-
-        fc = g.face_centers[:2]
-
-        tag = g.tags["auxiliary_line_0_faces"]
-        dist, _ = pp.distances.points_segments(fc, constraint_start_0, constraint_end_0)
-
-        self.assertTrue(np.allclose(dist[tag, 0], 0))
-        self.assertTrue(
-            np.all(np.logical_not(np.allclose(dist[np.logical_not(tag), 0], 0)))
-        )
-
-        tag = g.tags["auxiliary_line_1_faces"]
-        dist, _ = pp.distances.points_segments(fc, constraint_start_1, constraint_end_1)
-
-        self.assertTrue(np.allclose(dist[tag, 0], 0))
-        self.assertTrue(
-            np.all(np.logical_not(np.allclose(dist[np.logical_not(tag), 0], 0)))
-        )
-
-
-if __name__ == "__main__":
-    unittest.main()
