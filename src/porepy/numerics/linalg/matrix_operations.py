ea5a915b22cf5afabf2f2c1f317afd0c4adadae6
Ingrid Kristine Jacobsen <ingrid.kristine.jacobsen@gmail.com>
2023-10-13 09:01:13 +0200

    STY: 88 character lines in docs

--- src/porepy/numerics/linalg/matrix_operations.py
+++ src/porepy/numerics/linalg/matrix_operations.py
@@ -839,14 +839,13 @@ def rldecode(A: np.ndarray, n: np.ndarray) -> np.ndarray:
 def sparse_kronecker_product(matrix: sps.spmatrix, nd: int) -> sps.spmatrix:
     """Convert the scalar projection to a vector quantity.
 
-    Used to expand projection matrices from scalar versions to a form
-    applicable for projection of nd-vector quantities.
+    Used to expand projection matrices from scalar versions to a form applicable for
+    projection of nd-vector quantities.
 
     Parameters:
         matrix: Matrix to be expanded using a kronecker product.
         nd: The dimension to which matrix is expanded. If the prescribed dimension
             is 1, the projection matrix is returned without changes.
-
 
     """
     if nd == 1:
