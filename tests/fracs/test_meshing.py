b2202a1d14ecf5b37bfd01a3c28bf038046cd660
Ivar Stefansson <ivar.stefansson@uib.no>
2023-10-13 15:59:27 +0200

    TST: Move reference arrays

--- tests/fracs/test_meshing.py
+++ tests/fracs/test_meshing.py
@@ -3,11 +3,10 @@
 So far, only a test of the function _tag_faces() is implemented.
 
 """
-import unittest
-
 import numpy as np
 
 from porepy.fracs import meshing
+from porepy.applications.test_utils import reference_dense_arrays
 
 
 def test_fracture_and_boundary_face_tags_2d_domain_x_intersection():
@@ -18,7 +17,7 @@ def test_fracture_and_boundary_face_tags_2d_domain_x_intersection():
 
     """
 
-    # Define fractures, domain boundaries, and grid
+    # Define fractures, domain boundaries, and grid.
     f_1 = np.array([[0, 2], [1, 1]])
     f_2 = np.array([[1, 1], [0, 2]])
 
@@ -29,79 +28,18 @@ def test_fracture_and_boundary_face_tags_2d_domain_x_intersection():
 
     # 2D grid:
     g_2d = grids.subdomains(dim=2)[0]
+    reference_tags = reference_dense_arrays.test_meshing[
+        "test_fracture_and_boundary_face_tags_2d_domain_x_intersection"
+    ]
 
-    f_tags_2d = np.array(
-        [
-            False,
-            True,
-            False,
-            False,  # first row
-            False,
-            True,
-            False,
-            False,  # Second row
-            False,
-            False,
-            False,
-            False,  # third row
-            False,
-            False,
-            False,  # Bottom column
-            True,
-            True,
-            False,  # Second column
-            False,
-            False,
-            False,  # Third column
-            False,
-            False,
-            False,  # Top column
-            True,
-            True,
-            True,
-            True,
-        ]
-    )  # Added faces
-
-    d_tags_2d = np.array(
-        [
-            True,
-            False,
-            False,
-            True,  # first row
-            True,
-            False,
-            False,
-            True,  # Second row
-            True,
-            False,
-            False,
-            True,  # third row
-            True,
-            True,
-            True,  # Bottom column
-            False,
-            False,
-            False,  # Second column
-            False,
-            False,
-            False,  # Third column
-            True,
-            True,
-            True,  # Top column
-            False,
-            False,
-            False,
-            False,
-        ]
-    )  # Added Faces
-
-    # No fracture tips for the highest-dimensional grid
-    t_tags_2d = np.zeros(f_tags_2d.size, dtype=bool)
+    # No fracture tips for the highest-dimensional grid.
+    t_tags_2d = np.zeros(reference_tags["fracture_tags"].size, dtype=bool)
 
     assert np.all(g_2d.tags["tip_faces"] == t_tags_2d)
-    assert np.all(g_2d.tags["fracture_faces"] == f_tags_2d)
-    assert np.all(g_2d.tags["domain_boundary_faces"] == d_tags_2d)
+    assert np.all(g_2d.tags["fracture_faces"] == reference_tags["fracture_tags"])
+    assert np.all(
+        g_2d.tags["domain_boundary_faces"] == reference_tags["domain_boundary_tags"]
+    )
 
     # 1D grids:
     for g_1d in grids.subdomains(dim=1):
