30e082ecd24c73a4b8e5b21d6d871162d8db05dc
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2023-10-20 08:08:55 +0000

    DOC: Typing etc. TangentialNormalProjection

--- src/porepy/utils/tangential_normal_projection.py
+++ src/porepy/utils/tangential_normal_projection.py
@@ -17,111 +17,95 @@ class TangentialNormalProjection:
 
     The spaces are defined by the normal vector (see __init__ documentation).
     The basis for the tangential space is arbitrary (arbitrary direction in 2d,
-    rotation angle in 3d). The basis for the tangential is stored in the attribute
-    tangential_basis.
+    rotation angle in 3d).
 
-    Attributes:
-        num_vecs (int): Number of tangent/normal spaces represented by this object.
-        dim (int): Dimension of the ambient space.
-        tangential_basis (np.array, size: dim x dim-1 x num_vec): Basis vectors for the
-            tangential space.
-        projection (np.array, size dim x dim x num_vecs): Projection matrices onto the
-            tangential and normal space. The first dim-1 rows represent projection to the
-            tangential space, the final row is the normal component.
-        normals (np.array, size dim x num_vecs): Unit normal vectors.
+    Parameters:
+        normals: ``shape=(dim, num_vecs)``
+
+            Normal vectors that define the local coordinate systems.
 
     """
 
-    def __init__(self, normals, dim=None):
-        if dim is None:
-            dim = normals.shape[0]
-
+    def __init__(self, normals: np.ndarray) -> None:
         # Normalize vectors
         normals = normals / np.linalg.norm(normals, axis=0)
 
-        self.num_vecs = normals.shape[1]
-        self.dim = dim
+        self.num_vecs: int = normals.shape[1]
+        """Number of normal vectors represented by this object."""
+        self.dim: int = normals.shape[0]
+        """Dimension of the ambient space."""
 
         # Compute normal and tangential basis
         basis, normal = self._decompose_vector(normals)
 
-        basis = basis.reshape((dim, dim, self.num_vecs))
-        self.tangential_basis = basis[:, :-1, :]
+        basis = basis.reshape((self.dim, self.dim, self.num_vecs))
 
         # The projection is found by inverting the basis vectors
-        self.projection = self._invert_3d_matrix(basis)
-        self.normals = normal
+        self._projection = self._invert_3d_matrix(basis)
+        """Projection matrix to the local coordinate system."""
+
+        self._normals = normal
+        """Normal vectors, ``shape=(dim, num_vecs)``."""
 
     ## Methods for genertation of projection matrices
 
-    def project_tangential_normal(self, num=None):
+    def project_tangential_normal(self, num: Optional[int] = None) -> sps.spmatrix:
         """Define a projection matrix to decompose a matrix into tangential
         and normal components.
 
-        The intended usage is to decompose a grid-based vector variable into the
-        tangent and normal spaces of the grid, with the tacit understanding that there is
-        a single normal vector shared for all the cells (or faces) in the grid.
+        The intended usage is to decompose a vector defined in the global coordinate
+        sytsem variable into the tangent and normal spaces of a local coordinate system.
 
         The method can also create projection matrix based on unequal normal vectors.
-        One projection will be generated per column in self.normal. To activate
-        this behavior, set num=None.
+        One projection will be generated per column in self.normal. To activate this
+        behavior, set num=None.
 
         Parameters:
-            num (int, optional): Number of (equal) projections to be generated.
-                Will correspond to the number of cells / faces in the grid.
-                The projection matrix will have num * self.dim columns. If not
-                specified (default), one projection will be generated per vector in
-                self.normals.
-                NOTE: If self.num_vecs > 1, but num is not None, only the first
+            num: Number of projections to be generated. Will correspond
+                to the number of cells / faces in the grid. The projection matrix will
+                have ``num * self.dim columns``. If not specified, one
+                projection will be generated per vector in ``self._normals``.
+                NOTE: If ``self.num_vecs > 1``, but num is not None, only the first
                 given normal vector will be used to generate the tangential space.
 
         Returns:
-            scipy.sparse.csc_matrix: Projection matrix, structure as a block
-                diagonal matrix, with block size dim x dim.
-                For each block, the first dim-1 rows projects onto the tangent
-                space, the final row projects onto the normal space.
-                size: ((self.dim * num) x (self.dim * num). If num is not None,
-                size: ((self.dim * num_vecs) x (self.dim * num_vecs)
+            Projection matrix, structure as a block diagonal matrix, with block size
+                ``dim x dim``. For each block, the first ``dim - 1`` rows projects onto
+                the tangent space, the final row projects onto the normal space.
 
         """
         if num is None:
-            num = self.projection.shape[-1]
+            num = self._projection.shape[-1]
             data = np.array(
-                [self.projection[:, :, i].ravel("f") for i in range(num)]
+                [self._projection[:, :, i].ravel("f") for i in range(num)]
             ).ravel()
         else:
-            data = np.tile(self.projection[:, :, 0].ravel(order="F"), num)
+            data = np.tile(self._projection[:, :, 0].ravel(order="F"), num)
 
         mat = pp.matrix_operations.csc_matrix_from_blocks(data, self.dim, num)
 
         return mat
 
-    def project_tangential(self, num=None):
-        """Define a projection matrix of a specific size onto the tangent space.
+    def project_tangential(self, num: Optional[int] = None) -> sps.spmatrix:
+        """Define a projection matrix of a specific size onto the tangential space.
 
-        The intended usage is to project a grid-based vector variable onto the
-        tangent space of the grid, with the tacit understanding that there is
-        a single normal vector shared for all the cells (or faces) in the grid.
+        The projection is from global coordinates to the local coordinates.
 
         The method can also create projection matrix based on unequal normal vectors.
-        One projection will be generated per column in self.normal. To activate
-        this behavior, set num=None.
+        One projection will be generated per column in self.normal. To activate this
+        behavior, set num=None.
 
         Parameters:
             num (int, optional): Number of (equal) projections to be generated.
-                Will correspond to the number of cells / faces in the grid.
-                The projection matrix will have num * self.dim columns. If not
-                specified (default), one projection will be generated per vector in
-                self.normals.
-                NOTE: If self.num_vecs > 1, but num is not None, only the first
-                given normal vector will be used to generate the tangential space.
+                The projection matrix will have ``num * self.dim columns``. If not
+                specified, one projection will be generated per vector in
+                ``self._normals.``
+                NOTE: If ``self.num_vecs > 1``, but ``num`` is not ``None``,
+                only the first given normal vector will be used to generate the normal
+                space.
 
         Returns:
-            scipy.sparse.csc_matrix: Tangential projection matrix, structure as a block
-                diagonal matrix. The first (dim-1) x dim block projects onto the first
-                tangent space, etc.
-                size: ((self.dim - 1) * num) x (self.dim * num). If num is not None,
-                size: ((self.dim - 1) * num_vecs) x (self.dim * num_vecs)
+            Tangential projection matrix, structured as a block diagonal matrix.
 
         """
         # Construct the full projection matrix - tangential and normal
@@ -145,29 +129,26 @@ class TangentialNormalProjection:
         # Return the restricted matrix.
         return remove_normal_components * full_projection
 
-    def project_normal(self, num=None):
+    def project_normal(self, num: Optional[int] = None) -> sps.spmatrix:
         """Define a projection matrix of a specific size onto the normal space.
 
-        The projection is from global coordinates to the local coordinates defined
+        The projection is from global coordinates to the local coordinates.
 
         The method can also create projection matrix based on unequal normal vectors.
-        One projection will be generated per column in self.normal. To activate
-        this behavior, set num=None.
+        One projection will be generated per column in self.normal. To activate this
+        behavior, set num=None.
 
         Parameters:
             num (int, optional): Number of (equal) projections to be generated.
-                The projection matrix will have num * self.dim columns. If not
-                specified (default), one projection will be generated per vector in
-                self.normals.
-                NOTE: If self.num_vecs > 1, but num is not None, only the first
-                given normal vector will be used to generate the normal space.
+                The projection matrix will have ``num * self.dim columns``. If not
+                specified, one projection will be generated per vector in
+                ``self.normals.``
+                NOTE: If ``self.num_vecs > 1``, but ``num`` is not ``None``,
+                only the first given normal vector will be used to generate the normal
+                space.
 
         Returns:
-            scipy.sparse.csc_matrix: Tangential projection matrix, structure as a block
-                diagonal matrix. The first 1 x dim block projects onto the first
-                tangent space, etc.
-                size: num x (self.dim * num). If num is not None.
-                size: num_vecs x (self.dim * num_vecs) els.
+            Normal projection matrix, structured as a block diagonal matrix.
 
         """
         # Generate full projection matrix
@@ -190,25 +171,25 @@ class TangentialNormalProjection:
         # Return the restricted matrix
         return remove_tangential_components * full_projection
 
-    def local_projection(self, ind=None):
-        """Get the local projection matrix (reference)
+    ### Helper functions below
+    def _decompose_vector(self, nc: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+        """Decompose a set of normal vectors into a basis for the tangential space
+        and the normal space.
 
         Parameters:
-            ind (int, optional): Index (referring to the order of the normal vectors
-                provided to __init__) of the basis to return. Defaults to the first one.
+            nc: Normal vectors ``shape=(dim, num_vecs)``.
 
         Returns:
-            np.array (self.dim x self.dim): Local projection matrix. Multiplication
-                gives projection to the tangential space (first self.dim - 1 rows)
-                and normal space (last)
+            Basis for the tangential and normal space, and the normal vector itself.
+
+            np.ndarray: ``shape=(dim, dim, num_vecs)``. The first dim-1 columns are
+                the basis for the tangential space, the final column is the normal
+                vector.
+
+            np.ndarray: ``shape=(dim, num_vecs)``. The normal vector.
 
         """
-        if ind is None:
-            ind = 0
-        return self.projection[:, :, ind]
 
-    ### Helper functions below
-    def _decompose_vector(self, nc):
         if self.dim == 2:
             # Normalize the normal vector, just to be sure
             normal = nc / np.linalg.norm(nc, axis=0)
@@ -287,16 +268,17 @@ class TangentialNormalProjection:
 
         return basis, normal
 
-    def _invert_3d_matrix(self, M):
-        """
-        Find the inverse of the (m,m,k) 3D ndArray M. The inverse is interpreted as the
-        2d inverse of M[:, :, i] for i = 0...k
+    def _invert_3d_matrix(self, M: np.ndarray) -> np.ndarray:
+        """Find the inverse of the ``(m,m,k)`` 3D array ``M``.
+
+        The inverse is interpreted as the 2d inverse of ``M[:, :, i]`` for ``i = 0...k``
 
         Parameters:
-        M: (m, m, k) ndArray
+            M: Array with ``shape=(m, m, k)``.
 
         Returns:
-        M_inv: Inverse of M
+            Inverse of ``M``.
+
         """
         M_inv = np.zeros(M.shape)
         for i in range(M.shape[-1]):
@@ -310,11 +292,6 @@ def set_local_coordinate_projections(
     """Define a local coordinate system, and projection matrices, for all
     grids of co-dimension 1.
 
-    Parameters:
-        mdg: Mixed-dimensional grid.
-        interfaces: List of MortarGrids. If not provided, all interfaces of co-dimension
-            1 will be considered.
-
     The function adds one item to the data dictionary of all MixedDimensionalGrid edges
     that neighbors a co-dimension 1 grid, defined as:
         key: Literal["tangential_normal_projection"],
@@ -326,15 +303,20 @@ def set_local_coordinate_projections(
 
     It is assumed that the surface is planar.
 
+    Parameters:
+        mdg: Mixed-dimensional grid.
+        interfaces: List of MortarGrids. If not provided, all interfaces of co-dimension
+            1 will be considered.
+
     """
     if interfaces is None:
         interfaces = mdg.interfaces(dim=mdg.dim_max() - 1)
 
-    # Information on the vector normal to the surface is not available directly
-    # from the surface grid (it could be constructed from the surface geometry,
-    # which spans the tangential plane). We instead get the normal vector from
-    # the adjacent higher dimensional grid.
-    # We therefore access the grids via the edges of the mixed-dimensional grid.
+    # Information on the vector normal to the surface is not available directly from the
+    # surface grid (it could be constructed from the surface geometry, which spans the
+    # tangential plane). We instead get the normal vector from the adjacent higher
+    # dimensional grid. We therefore access the grids via the edges of the
+    # mixed-dimensional grid.
     for intf in interfaces:
         # Only consider edges where the lower-dimensional neighbor is of co-dimension 1
         if not intf.dim == (mdg.dim_max() - 1):
@@ -343,10 +325,9 @@ def set_local_coordinate_projections(
         # Neighboring grids
         sd_primary, sd_secondary = mdg.interface_to_subdomain_pair(intf)
 
-        # Find faces of the higher dimensional grid that coincide with the mortar
-        # grid. Go via the primary to mortar projection.
-        # Convert matrix to csr, then the relevant face indices are found from
-        # the (column) indices.
+        # Find faces of the higher dimensional grid that coincide with the mortar grid.
+        # Go via the primary to mortar projection. Convert matrix to csr, then the
+        # relevant face indices are found from the (column) indices.
         faces_on_surface = intf.primary_to_mortar_int().tocsr().indices
 
         # Find out whether the boundary faces have outwards pointing normal vectors
@@ -370,16 +351,15 @@ def set_local_coordinate_projections(
             intf.mortar_to_secondary_int().dot(outwards_unit_vector_mortar.T).T
         )
 
-        # NOTE: The normal vector is based on the first cell in the mortar grid,
-        # and will be pointing from that cell towards the other side of the
-        # mortar grid. This defines the positive direction in the normal direction.
-        # Although a simpler implementation seems to be possible, going via the
-        # first element in faces_on_surface, there is no guarantee that this will
-        # give us a face on the positive (or negative) side, hence the more general
-        # approach is preferred.
+        # NOTE: The normal vector is based on the first cell in the mortar grid, and
+        # will be pointing from that cell towards the other side of the mortar grid.
+        # This defines the positive direction in the normal direction. Although a
+        # simpler implementation seems to be possible, going via the first element in
+        # faces_on_surface, there is no guarantee that this will give us a face on the
+        # positive (or negative) side, hence the more general approach is preferred.
         #
-        # NOTE: The basis for the tangential direction is determined by the
-        # construction internally in TangentialNormalProjection.
+        # NOTE: The basis for the tangential direction is determined by the construction
+        # internally in TangentialNormalProjection.
         projection = pp.TangentialNormalProjection(normal_lower)
 
         d_l = mdg.subdomain_data(sd_secondary)
