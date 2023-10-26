bbb5ea31b6669949309e9cb460a906b4d43b0769
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2023-10-26 19:20:08 +0000

    STY: black

--- src/porepy/applications/test_utils/common_xpfa_tests.py
+++ src/porepy/applications/test_utils/common_xpfa_tests.py
@@ -84,7 +84,7 @@ def create_grid_mpfa_mpsa_reproduce_known_values(
     nx = np.array([4, 4])
     domain = np.array([1, 1])
     if grid_type == "cart":
-        g:pp.Grid = pp.CartGrid(nx, physdims=domain)
+        g: pp.Grid = pp.CartGrid(nx, physdims=domain)
     elif grid_type == "simplex":
         g = pp.StructuredTriangleGrid(nx, physdims=domain)
     # Perturbation rates, same notation as in setup_grids.py
