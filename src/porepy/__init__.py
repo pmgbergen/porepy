79682a65a4878a251a8ab7c8771fa79aea01a25a
Ivar Stefansson <ivar.stefansson@uib.no>
2023-11-03 14:01:25 +0100

    MAINT: Rename functions for wrapping ad arrays and use plot_2d

--- src/porepy/__init__.py
+++ src/porepy/__init__.py
@@ -155,7 +155,7 @@ from porepy.models.run_models import (
 
 
 from porepy.numerics import ad
-from porepy.numerics.ad.operators import wrap_as_ad_array, wrap_as_ad_matrix
+from porepy.numerics.ad.operators import wrap_as_dense_ad_array, wrap_as_sparse_ad_array
 from porepy.numerics.ad.equation_system import EquationSystem
 from porepy.numerics.ad._ad_utils import set_solution_values
 from porepy.numerics.ad._ad_utils import get_solution_values
