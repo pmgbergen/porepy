04cfdaa3e5b22a91b1b5ac161b8d33d7d847f891
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2023-11-22 11:55:39 +0000

    STY: Mypy

--- src/porepy/applications/test_utils/common_xpfa_tests.py
+++ src/porepy/applications/test_utils/common_xpfa_tests.py
@@ -900,10 +900,13 @@ def test_split_discretization_into_subproblems(
             },
             pp.DISCRETIZATION_MATRICES: {flow_keyword: {}, mechanics_keyword: {}},
         }
-        data_partition[pp.PARAMETERS][flow_keyword]["partition_arguments"] = {
+        data_partition[pp.PARAMETERS][flow_keyword][  # type: ignore[index]
+            "partition_arguments"] = {
             "num_subproblems": 2
         }
-        data_partition[pp.PARAMETERS][mechanics_keyword]["partition_arguments"] = {
+        data_partition[pp.PARAMETERS][mechanics_keyword][   # type: ignore[index]
+            "partition_arguments"
+        ] = {
             "num_subproblems": 2
         }
         # Discretize
