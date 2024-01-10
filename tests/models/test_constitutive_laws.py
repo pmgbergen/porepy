20f3370009162be7d46b552b670e0efd384164ad
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2024-01-10 13:22:43 +0000

    TST: Relocate tests for diff-tpfa

--- tests/models/test_constitutive_laws.py
+++ tests/models/test_constitutive_laws.py
@@ -19,6 +19,7 @@ from __future__ import annotations
 from typing import Any, Literal, Type
 
 import numpy as np
+import scipy.sparse as sps
 import pytest
 
 import porepy as pp
@@ -26,6 +27,9 @@ import porepy.models.constitutive_laws as c_l
 from porepy.applications.test_utils import models
 from porepy.applications.test_utils.reference_dense_arrays import (
     test_constitutive_laws as reference_dense_arrays,
+)
+from porepy.applications.md_grids.model_geometries import (
+    SquareDomainOrthogonalFractures,
 )
 
 solid_values = pp.solid_values.granite
@@ -427,3 +431,309 @@ def test_dimension_reduction_values(
     for grids, expected_value in zip([subdomains, interfaces], expected[1:]):
         specific_volume = setup.specific_volume(grids).value(setup.equation_system)
         assert np.allclose(specific_volume.data, expected_value)
+
+
+class PoromechanicalTestDiffTpfa(
+    SquareDomainOrthogonalFractures,
+    pp.constitutive_laws.CubicLawPermeability,
+    pp.constitutive_laws.AdDarcyFlux,
+    pp.poromechanics.Poromechanics,
+):
+    """Helper class to test the derivative of the Darcy flux with respect to the mortar
+    displacement.
+    """
+
+    def __init__(self, params):
+        params.update(
+            {
+                "fracture_indices": [1],
+                "grid_type": "cartesian",
+                "meshing_arguments": {"cell_size_x": 0.5, "cell_size_y": 0.5},
+            }
+        )
+
+        super().__init__(params)
+
+    def matrix_permeability(self, subdomains: list[pp.Grid]) -> pp.ad.Operator:
+        """Non-constant permeability tensor, the y-component depends on pressure."""
+        if len(subdomains) == 0:
+            return pp.wrap_as_dense_ad_array(0, size=0)
+
+        nc = sum([sd.num_cells for sd in subdomains])
+        # K is a second order tensor having nd^2 entries per cell. 3d:
+        # Kxx, Kxy, Kxz, Kyx, Kyy, Kyz, Kzx, Kzy, Kzz
+        # 0  , 1  , 2  , 3  , 4  , 5  , 6  , 7  , 8
+        tensor_dim = 3**2
+
+        # Set constant component of the permeability - isotropic unit tensor
+        all_vals = np.zeros(nc * tensor_dim, dtype=float)
+        all_vals[0::tensor_dim] = 1
+        all_vals[4::tensor_dim] = 1
+        all_vals[8::tensor_dim] = 1
+
+        # Basis vector for the yy-component
+        e_yy = self.e_i(subdomains, i=4, dim=tensor_dim)
+
+        return (
+            pp.wrap_as_dense_ad_array(all_vals, name="Constant_permeability_component")
+            + e_yy @ self.pressure(subdomains) ** 2
+        )
+
+    def initial_condition(self):
+        """Set the initial condition for the problem.
+
+        The interface displacement is non-zero in the y-direction to trigger a non-zero
+        contribution from the derivative of the permeability with respect to the mortar
+        displacement.
+        """
+        super().initial_condition()
+
+        # Fetch the mortar interface and the subdomains.
+        intf = self.mdg.interfaces()[0]
+        g_2d, g_1d = self.mdg.subdomains()
+
+        # Projection from the mortar to the primary grid, and directly from the mortar
+        # cells to the high-dimensional cells. The latter uses an np.abs to avoid issues
+        # with + and - in g.cell_faces.
+        proj_high = intf.mortar_to_primary_int()
+        mortar_to_high_cell = np.abs(g_2d.cell_faces.T @ proj_high)
+
+        self.mortar_to_high_cell = mortar_to_high_cell
+
+        # Set the mortar displacement, firs using the ordering of the 2d cells
+        u_2d_x = np.array([0, 0, 0, 0])
+        u_2d_y = np.array([0, 0, 1, 2])
+        # .. and then map down to the mortar cells.
+        u_mortar_x = mortar_to_high_cell.T @ u_2d_x
+        u_mortar_y = mortar_to_high_cell.T @ u_2d_y
+
+        # Define the full mortar displacement vector and set it in the equation system.
+        u_mortar = np.vstack([u_mortar_x, u_mortar_y]).ravel("F")
+        self.equation_system.set_variable_values(
+            u_mortar, [self.interface_displacement_variable], iterate_index=0
+        )
+
+        # Store the y-component of the mortar displacement, using a Cartesian ordering
+        # of the mortar cells (i.e., the same as the ordering of the 2d cells).
+        self.u_mortar = u_2d_y
+
+        # Fetch the global dof of the mortar displacement in the y-direction.
+        dof_u_mortar_y = self.equation_system.dofs_of(
+            [self.interface_displacement_variable]
+        )[1::2]
+        # Reorder the global dof to match the ordering of the 2d cells, and store it
+        r, *_ = sps.find(mortar_to_high_cell)
+        self.global_dof_u_mortar_y = dof_u_mortar_y[r]
+
+        # Set the pressure variable in the 1d domain: The pressure is 2 in the leftmost
+        # fracture cell, 0 in the rightmost fracture cell. This should give a flux
+        # pointing to the right.
+        if np.diff(g_1d.cell_centers[0])[0] > 0:
+            p_1d = np.array([2, 0])
+        else:
+            p_1d = np.array([0, 2])
+
+        p_1d_var = self.equation_system.get_variables(
+            [self.pressure_variable], grids=[g_1d]
+        )
+        self.equation_system.set_variable_values(p_1d, p_1d_var, iterate_index=0)
+        self.p_1d = p_1d
+
+        # Set the interface Darcy flux to unity on all mortar cells.
+        interface_flux = np.arange(intf.num_cells)
+        self.equation_system.set_variable_values(
+            interface_flux, [self.interface_darcy_flux_variable], iterate_index=0
+        )
+        self.interface_flux = interface_flux
+
+        # Set the pressure in the 2d grid
+        p_2d_var = self.equation_system.get_variables(
+            [self.pressure_variable], grids=[g_2d]
+        )
+        p_2d = np.arange(g_2d.num_cells)
+        self.equation_system.set_variable_values(p_2d, p_2d_var, iterate_index=0)
+        self.p_2d = p_2d
+
+        self.global_intf_ind = self.equation_system.dofs_of(
+            [self.interface_darcy_flux_variable]
+        )
+        self.global_p_2d_ind = self.equation_system.dofs_of(p_2d_var)
+
+    def darcy_flux_discretization(self, subdomains: list[pp.Grid]) -> pp.ad.MpfaAd:
+        """Discretization object for the Darcy flux term.
+
+        Parameters:
+            subdomains: List of subdomains where the Darcy flux is defined.
+
+        Returns:
+            Discretization of the Darcy flux.
+
+        """
+        if self.params["base_discr"] == "tpfa":
+            return pp.ad.TpfaAd(self.darcy_keyword, subdomains)
+        else:
+            return pp.ad.MpfaAd(self.darcy_keyword, subdomains)
+
+
+@pytest.mark.parametrize("base_discr", ["tpfa", "mpfa"])
+def test_derivatives_darcy_flux_potential_trace(base_discr: str):
+    """Test the derivative of the Darcy flux with respect to the mortar displacement,
+    and of the potential reconstruciton with respect to the interface flux.
+
+    The test for the mortar displacement is done for a 2d domain with a single fracture,
+    and a 1d domain with two cells. The mortar displacement is set to a constant value
+    in the x-direction, but varying value in the y-direction. The permeability is given
+    by the cubic law, and the derivative of the permeability with respect to the mortar
+    displacement is non-zero. The test checks that the derivative of the Darcy flux with
+    respect to the mortar displacement is correctly computed.
+
+    The test for the interface flux uses the same overall setup, but the focus is on the
+    2d domain, and the potential reconstruction on the internal boundary to the
+    fracture. The permeability in the 2d domain is a function of the pressure, thus the
+    pressure reconstruction operator should be dependent both on the 2d pressure and on
+    the interface flux.
+
+    """
+
+    # Set up and discretize model
+    model = PoromechanicalTestDiffTpfa({"base_discr": base_discr})
+    model.prepare_simulation()
+    model.discretize()
+
+    # Fetch the mortar interface and the 1d subdomain.
+    g_2d, g_1d = model.mdg.subdomains()
+
+    ### First the test for the derivative of the Darcy flux with respect to the mortar
+    # displacement.
+    #
+    # Get the y-component of the mortar displacement, ordered in the same way as the 2d
+    # cells, that is
+    #   2, 3
+    #   0, 1
+    # Thus the jumps in the mortar displacement are cell 3 - cell 2, and cell 1 - cell
+    # 0.
+    u_m = model.u_mortar
+
+    # The permeability is given by the cubic law, calculate this and its derivative.
+    resid_ap = model.residual_aperture([g_1d]).value(model.equation_system)
+    k_0 = ((u_m[2] - u_m[0]) + resid_ap) ** 3 / 12
+    k_1 = (u_m[3] - u_m[1] + resid_ap) ** 3 / 12
+
+    # Derivative of the permeability with respect to the mortar displacement
+    dk0_du2 = (u_m[2] - u_m[0] + resid_ap) ** 2 / 4
+    dk0_du0 = -((u_m[2] - u_m[0] + resid_ap) ** 2) / 4
+    dk1_du3 = (u_m[3] - u_m[1] + resid_ap) ** 2 / 4
+    dk1_du1 = -((u_m[3] - u_m[1] + resid_ap) ** 2) / 4
+
+    # Calculate the transmissibility. First, get the distance between the cell center and
+    # the face center (will be equal on the two sides of the face).
+    dist = np.abs(g_1d.face_centers[0, 1] - g_1d.cell_centers[0, 0])
+
+    # Half transmissibility
+    trm_0 = k_0 / dist
+    trm_1 = k_1 / dist
+
+    # The derivative of the transmissibility with respect to the mortar displacement is
+    # given by the chain rule (a warm thanks to copilot):
+    dtrm_du0 = (dk0_du0 / dist) * trm_1 / (trm_0 + trm_1) - trm_0 * trm_1 * dk0_du0 / (
+        dist * (trm_0 + trm_1) ** 2
+    )
+    dtrm_du2 = (dk0_du2 / dist) * trm_1 / (trm_0 + trm_1) - trm_0 * trm_1 * dk0_du2 / (
+        dist * (trm_0 + trm_1) ** 2
+    )
+    dtrm_du1 = (dk1_du1 / dist) * trm_0 / (trm_0 + trm_1) - trm_0 * trm_1 * dk1_du1 / (
+        dist * (trm_0 + trm_1) ** 2
+    )
+    dtrm_du3 = (dk1_du3 / dist) * trm_0 / (trm_0 + trm_1) - trm_0 * trm_1 * dk1_du3 / (
+        dist * (trm_0 + trm_1) ** 2
+    )
+
+    # We also need the pressure difference. Multiply with the sign of the divergence to
+    # account for the direction of the normal vector.
+    dp = (model.p_1d[1] - model.p_1d[0]) * g_1d.cell_faces[1, 1]
+
+    # Finally, the true values that should be compared with the discretization.
+    true_derivatives = dp * np.array([dtrm_du0, dtrm_du1, dtrm_du2, dtrm_du3])
+
+    # The computed flux
+    computed_flux = model.darcy_flux([g_1d]).value_and_jacobian(model.equation_system)
+    # Pick out the middle face, and only those faces that are associated with the mortar
+    # displacement in the y-direction. The column indices must also be reordered to
+    # match the ordering of the 2d cells (which was used to set 'true_derivatives').
+    dt_du_computed = computed_flux.jac[
+        1, (model.mortar_to_high_cell @ model.global_dof_u_mortar_y).astype(int)
+    ].A.ravel()
+
+    assert np.allclose(dt_du_computed, true_derivatives)
+
+    ### Now the test for the derivative of the potential reconstruction with respect to
+    # the interface flux.
+    #
+    # The potential reconstruction method
+    potential_trace = model.potential_trace(
+        model.mdg.subdomains(), model.pressure, model.permeability, "darcy_flux"
+    ).value_and_jacobian(model.equation_system)
+
+    # Fetch the permeability tensor from the data dictionary.
+    data_2d = model.mdg.subdomain_data(model.mdg.subdomains()[0])
+    k = data_2d[pp.PARAMETERS][model.darcy_keyword]["second_order_tensor"]
+    # Only the yy-component is needed to calculate the reconstructed pressure, since the
+    # face centers on the fracutre, and their respective 2d cell centers are aligned
+    # with the x-axis.
+    k_yy = k.values[1, 1, :]
+
+    # Fetch the pressure in the 2d domain and the interface flux.
+    p_2d = model.p_2d
+    # The interface flux is mapped to the Cartesian ordering of the 2d cells.
+    interface_flux = model.mortar_to_high_cell @ model.interface_flux
+
+    # We know the distance from cell to face center (same for all cells and faces)
+    dist = 0.5
+
+    # The ordering of the fracture faces in the 2d domain is different from the
+    # Cartesian ordering of the 2d cells, and from the ordering of the mortar cells. The
+    # below code gives the fracture faces in the order corresponding to that of the 2d
+    # cells.
+    fracture_faces = np.where(g_2d.tags["fracture_faces"])[0]
+    fracture_faces_cart_ordering = fracture_faces[
+        g_2d.cell_faces[fracture_faces].indices
+    ]
+
+    # The reconstructed pressure is given by the sum of the pressure in the cell and the
+    # pressure difference that drives the flux. The minus sign is needed since the
+    # interface flux is defined as positive out of the high-dimensional cell.
+    p_reconstructed = p_2d - interface_flux * dist / k_yy
+    assert np.allclose(
+        p_reconstructed, potential_trace.val[fracture_faces_cart_ordering]
+    )
+
+    # The Jacobian for the potential reconstruction should have two terms: One relating
+    # to the pressure in the cell, and one relating to the interface flux.
+
+    # We know that the yy-component of the permeability tensor contains a p^2 term
+    dk_yy_dp = 2 * p_2d / dist
+    # The Jacobian with respect to the pressure, represented as a diagonal matrix.
+    true_jac_dp = np.diag(1 + interface_flux / (k_yy / dist) ** 2 * dk_yy_dp)
+
+    # For ease of comparison, we reorder the Jacobian to match the ordering of the 2d
+    # cells. No need to reorder the columns, as the 2d pressure already has the correct
+    # ordering.
+    assert np.allclose(
+        true_jac_dp,
+        potential_trace.jac[fracture_faces_cart_ordering][:, model.global_p_2d_ind].A,
+    )
+
+    # The computed Jacobian. Here we also need to reorder the columns from mortar to
+    # high-dimensional cell ordering.
+    computed_dp_dl = potential_trace.jac[fracture_faces_cart_ordering][
+        :, model.mortar_to_high_cell @ model.global_intf_ind
+    ].A
+    # The true Jacobian with respect to the interface flux is found by differentiating
+    # p_reconstructed with respect to the interface flux.
+    true_dp_dl = np.diag(-dist / k_yy)
+    assert np.allclose(computed_dp_dl, true_dp_dl)
+
+    # Finally check there are no more terms in the Jacobian. We do no filtering of
+    # elements that are essentially zero; EK would not have been surprised if that
+    # turned out to be needed for Mpfa, but it seems to work without.
+    assert potential_trace.jac[fracture_faces_cart_ordering].data.size == 8
