13318c834d2aa4cf687f9b4e0d2ef2c76f5839cd
Ivar Stefansson <ivar.stefansson@uib.no>
2024-01-19 16:12:13 +0100

    DOC + STY: Minor fixes

--- tests/functional/test_benchmark_3d_case_3.py
+++ tests/functional/test_benchmark_3d_case_3.py
@@ -23,19 +23,17 @@ from examples.flow_benchmark_3d_case_3 import (
 
 
 class ModelWithEffectivePermeability(FlowBenchmark3dCase3Model):
-    """
-    Mixin that contains the computation of effective normal permeabilities.
-    """
+    """Mixin that contains the computation of effective normal permeabilities."""
 
     def effective_normal_permeability(
-            self, interfaces: list[pp.MortarGrid]
-            ) -> pp.ad.Operator:
+        self, interfaces: list[pp.MortarGrid]
+    ) -> pp.ad.Operator:
         """
         Computes the effective normal permeability, see Eq. 6b from [1].
 
         Note:
-            The effective normal permeability is the scalar that multiplies the
-            pressure jump in the continuous interface law.
+            The effective normal permeability is the scalar that multiplies the pressure
+            jump in the continuous interface law.
 
         Reference:
             [1] https://doi.org/10.1016/j.advwatres.2020.103759
@@ -44,8 +42,8 @@ class ModelWithEffectivePermeability(FlowBenchmark3dCase3Model):
             interfaces: List of interfaces.
 
         Returns:
-            Wrapped ad operator containing the effective normal permeabilities for
-            the given list of interfaces.
+            Wrapped ad operator containing the effective normal permeabilities for the
+            given list of interfaces.
         """
         subdomains = self.interfaces_to_subdomains(interfaces)
         projection = pp.ad.MortarProjections(self.mdg, subdomains, interfaces, dim=1)
@@ -66,8 +64,7 @@ class ModelWithEffectivePermeability(FlowBenchmark3dCase3Model):
 
 @pytest.fixture(scope="module")
 def model() -> ModelWithEffectivePermeability:
-    """
-    Run the benchmark model with the coarsest mesh resolution.
+    """Run the benchmark model with the coarsest mesh resolution.
 
     Returns:
         The solved model, an instance of `ModelWithEffectivePermeability`.
@@ -82,8 +79,7 @@ def model() -> ModelWithEffectivePermeability:
 
 @pytest.mark.skipped  # reason: slow
 def test_effective_tangential_permeability_values(model) -> None:
-    """
-    Test if the permeability values are consistent with the benchmark specification.
+    """Test if the permeability values are consistent with the benchmark specification.
 
     Parameters:
         model: ModelWithEffectivePermeability
@@ -97,15 +93,14 @@ def test_effective_tangential_permeability_values(model) -> None:
         if sd.dim == 3:
             np.testing.assert_allclose(val, 1)
         elif sd.dim == 2:
-            np.testing.assert_allclose(val, 1E2)
+            np.testing.assert_allclose(val, 1e2)
         else:  # sd.dim == 1
             np.testing.assert_allclose(val, 1)
 
 
 @pytest.mark.skipped  # reason: slow
 def test_effective_normal_permeability_values(model) -> None:
-    """
-    Test if the permeability values are consistent with the benchmark specification.
+    """Test if the permeability values are consistent with the benchmark specification.
 
     Parameters:
         model: ModelWithEffectivePermeability
@@ -115,7 +110,7 @@ def test_effective_normal_permeability_values(model) -> None:
     for intf in model.mdg.interfaces():
         val = np.mean(
             model.effective_normal_permeability([intf]).value(model.equation_system)
-            )
+        )
         if intf.dim == 2:
             np.testing.assert_allclose(val, 2e6)
         else:
@@ -124,8 +119,7 @@ def test_effective_normal_permeability_values(model) -> None:
 
 @pytest.mark.skipped  # reason: slow
 def test_boundary_specification(model) -> None:
-    """
-    Check that the inlet and outlet boundaries are correctly specified.
+    """Check that the inlet and outlet boundaries are correctly specified.
 
     Note:
         At the inlet boundary, we check if the total amount of fluid is entering into
@@ -137,7 +131,7 @@ def test_boundary_specification(model) -> None:
     # Inlet boundary
     south_side = model.domain_boundary_sides(bg).south
     inlet_flux = np.sum(data_bg["iterate_solutions"]["darcy_flux"][0][south_side])
-    np.testing.assert_allclose(inlet_flux, desired=-1/3, atol=1e-5)
+    np.testing.assert_allclose(inlet_flux, desired=-1 / 3, atol=1e-5)
 
     # Outlet boundary
     north_side = model.domain_boundary_sides(bg).north
