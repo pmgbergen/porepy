2ac9dfa9592cfe1aff9c09e3b07a9e5e1ee2f934
Ivar Stefansson <ivar.stefansson@uib.no>
2024-01-23 13:01:16 +0100

    DOC: Init file for examples module

---
+++ examples/__init__.py
@@ -0,0 +1,12 @@
+"""Simulation examples for PorePy.
+
+This module contains a number of examples of how to run a simulation using PorePy. Each
+example contains functionality specific to the setup in question.
+
+The simulation setups serve the following purposes:
+    - Reuse in testing
+    - Use in documentation
+    - Use in run scripts and development
+    - Promotion of specific test cases (e.g. benchmarks)
+
+"""
