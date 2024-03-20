29ddda8d752410bde65d818486aa139591e8f817
mariusnevland <mne029@uib.no>
2024-03-20 14:21:51 +0100

    MAINT: Update linear_solvers.py
    Simplify linear_solvers.py a bit.

--- src/porepy/numerics/linear_solvers.py
+++ src/porepy/numerics/linear_solvers.py
@@ -53,13 +53,11 @@ class LinearSolver:
         errors: dict = {"residual_error": [], "increment_error": []}
 
         setup.assemble_linear_system()
-        _, init_res = setup.linear_system  # Extract initial residual
+        res = setup.equation_system.assemble(evaluate_jacobian=False)
         sol = setup.solve_linear_system()
-        setup.assemble_linear_system()
-        _, res = setup.linear_system  # Extract residual
 
         error_res, error_inc, is_converged, _ = setup.check_convergence(
-            sol, prev_sol, prev_sol, res, init_res, self.params
+            sol, prev_sol, prev_sol, res, res, self.params
         )
         errors["residual_error"].append(error_res)
         errors["increment_error"].append(error_inc)
