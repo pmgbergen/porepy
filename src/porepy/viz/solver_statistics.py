d03d91cd4a070eb661c3d2f78ec915a5b62576fc
Jakub Both <jakub.both@uib.no>
2024-04-15 12:51:05 +0200

    STY: mypy

--- src/porepy/viz/solver_statistics.py
+++ src/porepy/viz/solver_statistics.py
@@ -141,19 +141,21 @@ class SolverStatistics:
                 self.history["increments"][var["printed_name"] + f" {nd}D"] = []
         for nd in range(self.nd):
             for var in self._variable_data[f"interfaces_{nd}"]:
-                self.increments[var["printed_name"] + f" {nd}D, intf."] = []
+                self.increments[var["printed_name"] + f" {nd}D, intf."]: list[
+                    float
+                ] = []
                 self.history["increments"][var["printed_name"] + f" {nd}D, intf."] = []
 
         # Initialize the subresiduals dictionary and associated history
         for key in ["dense", "sparse"]:
             for eq in self._equations_data[key]:
-                self.residuals[eq["printed_name"]] = []
+                self.residuals[eq["printed_name"]]: list[float] = []
                 self.init_residuals[eq["printed_name"]] = []
                 self.history["residuals"][eq["printed_name"]] = []
                 self.history["init_residuals"][eq["printed_name"]] = []
         for nd in range(self.nd + 1):
             for eq in self._equations_data[f"subdomains_{nd}"]:
-                self.residuals[eq["printed_name"] + f" {nd}D"] = []
+                self.residuals[eq["printed_name"] + f" {nd}D"]: list[float] = []
                 self.init_residuals[eq["printed_name"] + f" {nd}D"] = []
                 self.history["residuals"][eq["printed_name"] + f" {nd}D"] = []
                 self.history["init_residuals"][eq["printed_name"] + f" {nd}D"] = []
@@ -258,14 +260,14 @@ class SolverStatistics:
     def reset(self) -> None:
         """Reset the statistics object."""
         self.num_iteration = 0
-        self.increment_errors = []
-        self.residual_errors = []
+        self.increment_errors.clear()
+        self.residual_errors.clear()
         for key in self.increments:
-            self.increments[key] = []
+            self.increments[key].clear()
         for key in self.residuals:
-            self.residuals[key] = []
+            self.residuals[key].clear()
         for key in self.init_residuals:
-            self.init_residuals[key] = []
+            self.init_residuals[key] = 0
 
     def _save(self) -> None:
         """Save the statistics object to a file in json format."""
