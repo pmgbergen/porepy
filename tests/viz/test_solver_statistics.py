ef18e80e81e6bce735b15d3af47d0828c4f95cbd
Jakub Both <jakub.both@uib.no>
2024-04-23 20:41:27 +0200

    TST: Fix test

--- tests/viz/test_solver_statistics.py
+++ tests/viz/test_solver_statistics.py
@@ -1,7 +1,5 @@
 """ Tests of functionality of :class:`~porepy.viz.solver_statistics.SolverStatistics`."""
 
-import pytest
-
 import porepy as pp
 from porepy.applications.test_utils.models import Poromechanics
 
@@ -18,18 +16,17 @@ def test_solver_statistic_attributes():
     assert hasattr(model.nonlinear_solver_statistics, "num_iteration")
     assert hasattr(model.nonlinear_solver_statistics, "increment_errors")
     assert hasattr(model.nonlinear_solver_statistics, "residual_errors")
+    assert hasattr(model.nonlinear_solver_statistics, "path")
+    assert model.nonlinear_solver_statistics.path is None
 
 
-@pytest.mark.parametrize("path", [None, "solver_statistics.json"])
-def test_solver_statistics_save(path):
+def test_solver_statistics_save():
     """Check whether solver statistics are saved to file."""
+    path = "solver_statistics.json"
     params = {"solver_statistics_file_name": path}
     model = Poromechanics(params)
     pp.run_time_dependent_model(model, params)
-    if path is not None:
-        # Check whether file was saved
-        assert model.nonlinear_solver_statistics.path.exists()
-        # Clean up
-        model.nonlinear_solver_statistics.path.unlink()
-    else:
-        assert model.nonlinear_solver_statistics.path is None
+    # Check whether file was saved
+    assert model.nonlinear_solver_statistics.path.exists()
+    # Clean up
+    model.nonlinear_solver_statistics.path.unlink()
