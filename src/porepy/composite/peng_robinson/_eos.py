MOD+FIX: Compiled computation of compressibility factor in PR and NPIPM solver.

src/porepy/composite/peng_robinson/_eos.py
@@ -38,6 +38,18 @@ logger = logging.getLogger(__name__)
 DeprecationWarning("The module porepy.composite.peng_robinson.eos is deprecated.")
 
 
+def _sqrt(a):
+    return a ** (1 / 2)
+
+
+def _power(a, b):
+    return a**b
+
+
+def _cbrt(a):
+    return a ** (1 / 3)
+
+
 @dataclass
 class ThermodynamicState:
     """Data class for storing the thermodynamic state of a mixture..
@@ -522,12 +534,12 @@ class VanDerWaals:
             dT_a_parts.append(X[i] ** 2 * dT_a[i])
             for j in range(i + 1, nc):
                 x_ij = X[i] * X[j]
-                a_ij_ = pp.ad.sqrt(a[i] * a[j])
+                a_ij_ = _sqrt(a[i] * a[j])
                 delta_ij = 1 - bip[i][j]
 
                 a_ij = a_ij_ * delta_ij
                 dT_a_ij = (
-                    pp.ad.power(a[i] * a[j], -1 / 2)
+                    _power(a[i] * a[j], -1 / 2)
                     / 2
                     * (dT_a[i] * a[j] + a[i] * dT_a[j])
                     * delta_ij
@@ -560,7 +572,7 @@ class VanDerWaals:
             The derivative of the mixture cohesion w.r.t to ``X[i]``
         """
         return 2.0 * safe_sum(
-            [X[j] * pp.ad.sqrt(a[i] * a[j]) * (1 - bip[i][j]) for j in range(len(X))]
+            [X[j] * _sqrt(a[i] * a[j]) * (1 - bip[i][j]) for j in range(len(X))]
         )
 
     @staticmethod
@@ -1223,8 +1235,10 @@ class PengRobinson(AbstractEoS):
         self.eps: float = eps
         """Passed at instantiation."""
 
-        self.smoothing_factor: float = smoothing_factor
+        self.smooth_3: float = smoothing_factor
         """Passed at instantiation."""
+
+        self.smooth_e = 1e-3
 
         self.regions: list[np.ndarray] = [np.zeros(1, dtype=bool)] * 4
         """A list of root-region indicates.
@@ -1557,11 +1571,11 @@ class PengRobinson(AbstractEoS):
             else:
                 alpha_i = self.a_correction(a_cor_i, T_r_i)
 
-            a_i = a_crit_i * pp.ad.power(alpha_i, 2)
+            a_i = a_crit_i * _power(alpha_i, 2)
             # outer derivative
             dT_a_i = 2 * a_crit_i * alpha_i
             # inner derivative
-            dT_a_i *= (-a_cor_i / (2 * comp.T_crit)) * pp.ad.power(T_r_i, -1 / 2)
+            dT_a_i *= (-a_cor_i / (2 * comp.T_crit)) * _power(T_r_i, -1 / 2)
 
             a.append(a_i)
             dT_a.append(dT_a_i)
@@ -1697,7 +1711,7 @@ class PengRobinson(AbstractEoS):
             The root of the linearized correction for the cohesion term.
 
         """
-        return 1 + kappa * (1 - pp.ad.sqrt(T_r))
+        return 1 + kappa * (1 - _sqrt(T_r))
 
     def _dXi_a(
         self,
@@ -1952,16 +1966,16 @@ class PengRobinson(AbstractEoS):
         n = np.max((n_a, n_b))  # determine the number of vectorized values
 
         # the coefficients of the compressibility polynomial
-        c0 = pp.ad.power(B, 3) + pp.ad.power(B, 2) - A * B
-        c1 = A - 2 * B - 3 * pp.ad.power(B, 2)
+        c0 = _power(B, 3) + _power(B, 2) - A * B
+        c1 = A - 2 * B - 3 * _power(B, 2)
         c2 = B - 1
 
         # the coefficients of the reduced polynomial (elimination of 2-monomial)
-        r = c1 - pp.ad.power(c2, 2) / 3
-        q = 2 / 27 * pp.ad.power(c2, 3) - c2 * c1 / 3 + c0
+        r = c1 - _power(c2, 2) / 3
+        q = 2 / 27 * _power(c2, 3) - c2 * c1 / 3 + c0
 
         # discriminant to determine the number of roots
-        delta = pp.ad.power(q, 2) / 4 + pp.ad.power(r, 3) / 27
+        delta = _power(q, 2) / 4 + _power(r, 3) / 27
 
         if shape:
             Z_L = pp.ad.AdArray(np.zeros(n), sps.lil_matrix(shape))
@@ -2038,17 +2052,17 @@ class PengRobinson(AbstractEoS):
             B_ = B[one_root_region]
 
             # delta has only positive values in this case by logic
-            t = -q_ / 2 + pp.ad.sqrt(delta_)
+            t = -q_ / 2 + _sqrt(delta_)
 
             # t_1 might be negative, in this case we must choose the real cubic root
             # by extracting cbrt(-1), where -1 is the real cubic root.
             im_cube = t < 0.0
             if np.any(im_cube):
                 t[im_cube] = t[im_cube] * (-1)
-                u = pp.ad.cbrt(t)
+                u = _cbrt(t)
                 u[im_cube] = u[im_cube] * (-1)
             else:
-                u = pp.ad.cbrt(t)
+                u = _cbrt(t)
 
             # TODO In rare, un-physical areas of A,B, u can become zero,
             # causing infinity here, e.g.
@@ -2066,7 +2080,6 @@ class PengRobinson(AbstractEoS):
             # using asymmetric, supercritical extension
             gas_ext_supc = gas_ext_supc[one_root_region]
             liq_ext_supc = liq_ext_supc[one_root_region]
-            smoothing_distance = 1e-1
             if np.any(gas_ext_supc) and asymmetric_extension:
                 w_g = self.extended_root_gas_sc(B_, z_1)[gas_ext_supc]
                 w_sub = w[gas_ext_supc]
@@ -2090,8 +2103,8 @@ class PengRobinson(AbstractEoS):
                 )
 
                 # smoothing towards subcritical region (Gharbia extension)
-                smooth = (d_g < smoothing_distance) & (b >= B_CRIT)
-                d = d_g / smoothing_distance  # normalize distance
+                smooth = (d_g < self.smooth_e) & (b >= B_CRIT)
+                d = d_g / self.smooth_e  # normalize distance
                 w_g[smooth] = (w_sub * (1 - d) + w_g * d)[smooth]
                 w[gas_ext_supc] = w_g
             if np.any(liq_ext_supc) and asymmetric_extension:
@@ -2120,12 +2133,12 @@ class PengRobinson(AbstractEoS):
 
                 # smoothing towards supercritical gas extension
                 # Smoothing using a convex combination of extended gas root
-                smooth = (d_w < smoothing_distance) & (b >= B_CRIT)
-                d = d_w / smoothing_distance  # normalize distance
+                smooth = (d_w < self.smooth_e) & (b >= B_CRIT)
+                d = d_w / self.smooth_e  # normalize distance
                 w_l[smooth] = (w_g * (1 - d) + w_l * d)[smooth]
                 # smoothing towards subcritical Ben Gharbia extension
-                smooth = (d_s < smoothing_distance) & (b < B_CRIT)
-                d = d_s / smoothing_distance
+                smooth = (d_s < self.smooth_e) & (b < B_CRIT)
+                d = d_s / self.smooth_e
                 w_l[smooth] = (w_sub * (1 - d) + w_l * d)[smooth]
 
                 w[liq_ext_supc] = w_l
@@ -2160,8 +2173,8 @@ class PengRobinson(AbstractEoS):
 
             # compute roots in three-root-region using Cardano formula,
             # Casus Irreducibilis
-            t_2 = pp.ad.arccos(-q_ / 2 * pp.ad.sqrt(-27 * pp.ad.power(r_, -3))) / 3
-            t_1 = pp.ad.sqrt(-4 / 3 * r_)
+            t_2 = pp.ad.arccos(-q_ / 2 * _sqrt(-27 * _power(r_, -3))) / 3
+            t_1 = _sqrt(-4 / 3 * r_)
 
             z3 = t_1 * pp.ad.cos(t_2) - c2_ / 3
             z2 = -t_1 * pp.ad.cos(t_2 + np.pi / 3) - c2_ / 3
@@ -2171,7 +2184,7 @@ class PengRobinson(AbstractEoS):
             # roots are positive and bound from below by B
             smoothable = gharbia_ext[three_root_region]
             if apply_smoother and np.any(smoothable):
-                z1_s, z3_s = root_smoother(z1, z2, z3, self.smoothing_factor)
+                z1_s, z3_s = root_smoother(z1, z2, z3, self.smooth_3)
 
                 z3[smoothable] = z3_s[smoothable]
                 z1[smoothable] = z1_s[smoothable]

src/porepy/examples/soereide_geothermal.py
@@ -28,8 +28,6 @@ import logging
 import pathlib
 import time
 
-from matplotlib import pyplot as plt
-
 logging.basicConfig(level=logging.WARNING)
 numba_logger = logging.getLogger("numba")
 numba_logger.setLevel(logging.WARNING)
@@ -53,8 +51,8 @@ class SoereideMixture:
         chems = ["H2O", "CO2"]
         species = ppc.load_species(chems)
         components = [
-            ppc.peng_robinson.H2O.from_species(species[0]),
-            ppc.peng_robinson.CO2.from_species(species[1]),
+            ppcpr.H2O.from_species(species[0]),
+            ppcpr.CO2.from_species(species[1]),
         ]
         return components
 
@@ -80,14 +78,13 @@ class CompiledFlash(ppc.FlashMixin):
 
         # Compiling the flash and the EoS
         eos.compile()
-        # pre-compile solvers for given mixture to avoid waiting times in
-        # prepare simulation and the first iteration
-        flash.compile(precompile_solvers=False)
+        flash.compile()
 
         # NOTE There is place to configure the solver here
-        flash.armijo_parameters["j_max"] = 30
+        flash.armijo_parameters["max_iter"] = 30
         flash.tolerance = 1e-8
         flash.max_iter = 150
+        flash.heavy_ball_momentum = False
 
         # Setting the attribute of the mixin
         self.flash = flash
@@ -164,7 +161,7 @@ class CompiledFlash(ppc.FlashMixin):
 
 class ModelGeometry:
     def set_domain(self) -> None:
-        size = self.solid.convert_units(2, "m")
+        size = self.solid.convert_units(1, "m")
         self._domain = nd_cube_domain(2, size)
 
     # def set_fractures(self) -> None:
@@ -191,26 +188,22 @@ class ModelGeometry:
 class InitialConditions:
     """Define initial pressure, temperature and compositions."""
 
+    _p_INIT: float = 15e6
+    _T_INIT: float = 550.0
+    _z_INIT: dict[str, float] = {"H2O": 0.995, "CO2": 0.005}
+
     def initial_pressure(self, sd: pp.Grid) -> np.ndarray:
         # Initial pressure of 10 MPa
-        return np.ones(sd.num_cells) * 15e6
+        return np.ones(sd.num_cells) * self._p_INIT
 
     def initial_temperature(self, sd: pp.Grid) -> np.ndarray:
         # Initial temperature of 550 K
-        return np.ones(sd.num_cells) * 550.0
+        return np.ones(sd.num_cells) * self._T_INIT
 
     def initial_overall_fraction(
         self, component: ppc.Component, sd: pp.Grid
     ) -> np.ndarray:
-        # Homogenous initial composition, with 0.5 % CO2
-        if component.name == "H2O":
-            return np.ones(sd.num_cells) * 0.995
-        elif component.name == "CO2":
-            return np.ones(sd.num_cells) * 0.005
-        else:
-            raise NotImplementedError(
-                f"Initial overlal fraction not implemented for component {component.name}"
-            )
+        return np.ones(sd.num_cells) * self._z_INIT[component.name]
 
 
 class BoundaryConditions:
@@ -235,14 +228,28 @@ class BoundaryConditions:
     has_time_dependent_boundary_equilibrium = False
     """Constant BC for primary variables, hence constant BC for all other."""
 
+    _p_INIT: float
+    _T_INIT: float
+    _z_INIT: dict[str, float]
+
+    _p_IN: float = 16e6
+    _p_OUT: float = 15e6
+
+    _T_IN: float = 550.0
+    _T_OUT: float = 550.0
+    _T_HEATED: float = 550.0
+
+    _z_IN: dict[str, float] = {"H2O": 0.99, "CO2": 0.01}
+    _z_OUT: dict[str, float] = {"H2O": 0.995, "CO2": 0.005}
+
     def _inlet_faces(self, sd: pp.Grid) -> np.ndarray:
         """Define inlet."""
         sides = self.domain_boundary_sides(sd)
 
         inlet = np.zeros(sd.num_faces, dtype=bool)
         inlet[sides.west] = True
-        inlet &= sd.face_centers[1] > 0.5
-        inlet &= sd.face_centers[1] < 1.5
+        inlet &= sd.face_centers[1] > 0.2
+        inlet &= sd.face_centers[1] < 0.8
 
         return inlet
 
@@ -253,8 +260,8 @@ class BoundaryConditions:
 
         outlet = np.zeros(sd.num_faces, dtype=bool)
         outlet[sides.east] = True
-        outlet &= sd.face_centers[1] > 0.5
-        outlet &= sd.face_centers[1] < 1.5
+        outlet &= sd.face_centers[1] > 0.2
+        outlet &= sd.face_centers[1] < 0.8
 
         return outlet
 
@@ -289,23 +296,19 @@ class BoundaryConditions:
             return pp.BoundaryCondition(sd)
 
     def bc_values_pressure(self, boundary_grid: pp.BoundaryGrid) -> np.ndarray:
-        # need to define pressure on east and west side of matrix
-        p_init = 15e6
-        p_in = 16e5
-        p_out = 15e6
+
         sd = boundary_grid.parent
         sides = self.domain_boundary_sides(sd)
 
-        # non-trivial BC on matrix
         if sd.dim == 2:
-            vals = np.ones(sd.num_faces) * p_init
+            vals = np.ones(sd.num_faces) * self._p_INIT
 
             inlet = self._inlet_faces(sd)
             outlet = self._outlet_faces(sd)
 
             # vals[sides.west] = 10e6
-            vals[inlet] = p_in
-            vals[outlet] = p_out
+            vals[inlet] = self._p_IN
+            vals[outlet] = self._p_OUT
 
             vals = vals[sides.all_bf]
         else:
@@ -314,21 +317,20 @@ class BoundaryConditions:
         return vals
 
     def bc_values_temperature(self, boundary_grid: pp.BoundaryGrid) -> np.ndarray:
-        T_init = 550.0
-        T_in = 550.0
-        T_heat = 550.0
+
         sd = boundary_grid.parent
         sides = self.domain_boundary_sides(sd)
 
-        # non-trivial BC on matrix
         if sd.dim == 2:
-            vals = np.ones(sd.num_faces) * T_init
+            vals = np.ones(sd.num_faces) * self._T_INIT
 
             inlet = self._inlet_faces(sd)
+            outlet = self._outlet_faces(sd)
             heated = self._heated_faces(sd)
 
-            vals[inlet] = T_in
-            vals[heated] = T_heat
+            vals[inlet] = self._T_IN
+            vals[outlet] = self._T_OUT
+            vals[heated] = self._T_HEATED
 
             vals = vals[sides.all_bf]
         else:
@@ -339,30 +341,18 @@ class BoundaryConditions:
     def bc_values_overall_fraction(
         self, component: ppc.Component, boundary_grid: pp.BoundaryGrid
     ) -> np.ndarray:
+
         sd = boundary_grid.parent
         sides = self.domain_boundary_sides(sd)
 
-        if component.name == "H2O":
-            z_init = 0.995
-            z_in = 0.995
-            z_out = z_init
-        elif component.name == "CO2":
-            z_init = 0.005
-            z_in = 0.005
-            z_out = z_init
-        else:
-            NotImplementedError(
-                f"Initial overlal fraction not implemented for component {component.name}"
-            )
-
         if sd.dim == 2:
-            vals = np.ones(sd.num_faces) * z_init
+            vals = np.ones(sd.num_faces) * self._z_INIT[component.name]
 
             inlet = self._inlet_faces(sd)
             outlet = self._outlet_faces(sd)
 
-            vals[inlet] = z_in
-            vals[outlet] = z_out
+            vals[inlet] = self._z_IN[component.name]
+            vals[outlet] = self._z_OUT[component.name]
 
             vals = vals[sides.all_bf]
         else:
@@ -394,13 +384,6 @@ dt_init = 1 * days * t_scale
 max_iterations = 80
 newton_tol = 1e-5
 
-# time_manager = pp.TimeManager(
-#     schedule=[0.0, 100.0 * days * t_scale],
-#     dt_init=1.0 * days * t_scale,
-#     constant_dt=True,
-#     iter_max=80,
-#     print_info=True,
-# )
 time_manager = pp.TimeManager(
     schedule=[0, T_end],
     dt_init=dt_init,
@@ -414,7 +397,7 @@ time_manager = pp.TimeManager(
 )
 
 solid_constants = pp.SolidConstants(
-    {"permeability": 1e-8, "porosity": 0.2, "thermal_conductivity": 1.92}
+    {"permeability": 1e-11, "porosity": 0.2, "thermal_conductivity": 1.92}
 )
 material_constants = {"solid": solid_constants}
 
@@ -442,9 +425,18 @@ params = {
 }
 model = GeothermalFlow(params)
 
+model.equilibrium_type = "p-T"
+
 start = time.time()
 model.prepare_simulation()
 print(f"Finished prepare_simulation in {time.time() - start} seconds")
+
+
+result, success, num_iter = model.flash.flash(
+    z=np.array([0.995, 0.005]),
+    p=np.array(15e6),
+    T=np.array([550.0]),
+)
 
 pp.run_time_dependent_model(model, params)
 pp.plot_grid(model.mdg, "pressure", figsize=(10, 8), plot_2d=True)