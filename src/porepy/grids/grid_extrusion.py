b5202e81fc6a2c203fae3d4be066dd866978f882
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2024-06-11 13:42:28 +0000

    MAINT: Rename grid_bucket -> mdg in grid extrusion module
    It will never die!

--- src/porepy/grids/grid_extrusion.py
+++ src/porepy/grids/grid_extrusion.py
@@ -6,7 +6,7 @@ dimension of the highest-dimensional grid should be 2 at most.
 
 The two functions performing above are
 
-- :func:`extrude_grid_bucket`,
+- :func:`extrude_mdg`,
 - :func:`extrude_grid`.
 
 """
@@ -24,7 +24,7 @@ from porepy.grids import mortar_grid
 from porepy.numerics.linalg.matrix_operations import sparse_array_to_row_col_data
 
 
-def extrude_grid_bucket(
+def extrude_mdg(
     mdg: pp.MixedDimensionalGrid, z: np.ndarray
 ) -> tuple[pp.MixedDimensionalGrid, dict]:
     """Extrude a mixed-dimensional grid by extending all fixed-dimensional grids in the
@@ -804,7 +804,7 @@ def _create_mappings(
     g: pp.Grid, g_new: pp.Grid, num_cell_layers: int
 ) -> tuple[np.ndarray, np.ndarray]:
     """Auxiliary function to create the extruded cell and face maps for
-    :func:`extrude_grid` and :func:`extrude_grid_bucket`."""
+    :func:`extrude_grid` and :func:`extrude_mdg`."""
 
     cell_map = np.empty(g.num_cells, dtype=object)
     for c in range(g.num_cells):
