012252fc82c16b3eec31b0c796b8ae759fdac107
Omar Duran <omaryesiduran@gmail.com>
2024-06-17 09:23:42 +0200

    WIP: ongoing work on comparing FV with a pure mixed approach

--- src/porepy/examples/geothermal_flow_via_correlations/TracerConstitutiveDescription.py
+++ src/porepy/examples/geothermal_flow_via_correlations/TracerConstitutiveDescription.py
@@ -167,7 +167,7 @@ class LiquidLikeCorrelations(ppc.AbstractEoS):
     ) -> tuple[np.ndarray, np.ndarray]:
 
         nc = len(thermodynamic_dependencies[0])
-        vals = (0.5) * np.ones(nc)
+        vals = (1.8) * np.ones(nc)
         # row-wise storage of derivatives, (4, nc) array
         diffs = np.zeros((len(thermodynamic_dependencies), nc))
         return vals, diffs
