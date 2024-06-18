a756d1df5940fa599a298bfaf85481fd6c19fd1a
mariusnevland <mne029@uib.no>
2024-06-18 13:32:36 +0200

    Include Uzawa algorithm into run_models

--- src/porepy/models/run_models.py
+++ src/porepy/models/run_models.py
@@ -146,6 +146,251 @@ def run_time_dependent_model(model, params: dict) -> None:
     model.after_simulation()
 
 
+def run_stationary_uzawa_model(model, params: dict) -> None:
+    """Run a stationary model using an implicit Uzawa algorithm. Must be combined with
+    mixin defining the Uzawa equations and solution strategy"""
+
+    # Assign parameters, variables and discretizations. Discretize time-indepedent terms
+    if params.get("prepare_simulation", True):
+        model.prepare_simulation()
+
+    # Assign a solver
+    solver = _choose_solver(model, params)
+
+    # Implicit Uzawa loop.
+    error = 1
+    uzawa_iteration_counter = 0
+    total_iteration_counter = 0
+    while error >= 1e-9: # Could maybe introduce convergence tol in parameter dict
+        # One iteration of the Uzawa loop consists of replacing the complementary
+        # functions with regularized versions, and solving the resulting nonlinear
+        # system.
+        # The regularizations depend on the solution at the previous Uzawa iteration.
+        # The loop continues until the difference in solutions from two consecutive
+        # Uzawa iterations falls below a given tolerance.
+
+        val_prev = model.equation_system.get_variable_values(iterate_index=0)
+        _, itr = solver.solve(model)
+        val_current = model.equation_system.get_variable_values(iterate_index=0)
+        error = float(np.linalg.norm(val_current-val_prev))
+
+        # Update numerical constants. Lots of options for how to update.
+        model.c_n += 0
+        model.c_t += 0
+
+        # Update normal and tangential tractions, to be used as constants in the next
+        # Uzawa iteration.
+        subdomains = model.mdg.subdomains(dim=model.nd - 1)
+        t_n = model.normal_component(subdomains) @ model.contact_traction(subdomains)
+        t_t = model.tangential_component(subdomains) @ model.contact_traction(
+            subdomains)
+        model.t_n_prev = t_n.value(model.equation_system)
+        model.t_t_prev = t_t.value(model.equation_system)
+
+        # Update equations with new regularizations
+        eqn_lst = list(model.equation_system.equations.keys())
+        for eqn in eqn_lst:
+            model.equation_system.remove_equation(eqn)
+        model.set_equations()
+
+        uzawa_iteration_counter += 1
+        total_iteration_counter += itr
+
+    # Note: Should also have an after_uzawa_failure() method
+    model.after_uzawa_convergence(uzawa_iteration_counter)
+    print("Uzawa iterations:", uzawa_iteration_counter)
+    print("Total nonlinear iterations:", total_iteration_counter)
+
+    model.after_simulation()
+
+
+def run_time_dependent_uzawa_model(model, params: dict) -> None:
+    """Run a time-dependent model using an implicit Uzawa algorithm. Must be combined
+    with mixin defining the Uzawa equations and solution strategy"""
+
+    # Assign parameters, variables and discretizations. Discretize time-indepedent terms
+    if params.get("prepare_simulation", True):
+        model.prepare_simulation()
+
+    # Assign a solver
+    solver = _choose_solver(model, params)
+
+    uzawa_iteration_counter = 0
+    # Uzawa iteration
+    def uzawa_algorithm() -> None:
+        uzawa_error = 1
+        nonlocal uzawa_iteration_counter
+        total_iteration_counter = 0
+
+        while uzawa_error >= 1e-9:
+            # One iteration of the Uzawa loop consists of replacing the complementary
+            # functions with regularized versions, and solving the resulting nonlinear
+            # system.
+            # The regularizations depend on the solution at the previous Uzawa iteration.
+            # The loop continues until the difference in solutions from two consecutive
+            # Uzawa iterations falls below a given tolerance.
+
+            val_prev = model.equation_system.get_variable_values(iterate_index=0)
+            _, itr = solver.solve(model)
+            val_current = model.equation_system.get_variable_values(iterate_index=0)
+
+            uzawa_error = float(np.linalg.norm(val_current - val_prev))
+
+            # Update numerical constants. Lots of options for how to update.
+            model.c_n += 0
+            model.c_t += 0
+
+            # Update normal and tangential tractions, to be used as constants in the next
+            # Uzawa iteration.
+            subdomains = model.mdg.subdomains(dim=model.nd - 1)
+            t_n = model.normal_component(subdomains) @ model.contact_traction(
+                subdomains)
+            t_t = model.tangential_component(subdomains) @ model.contact_traction(
+                subdomains)
+            model.t_n_prev = t_n.value(model.equation_system)
+            model.t_t_prev = t_t.value(model.equation_system)
+
+            # Update equations with new regularizations
+            eqn_lst = list(model.equation_system.equations.keys())
+            for eqn in eqn_lst:
+                model.equation_system.remove_equation(eqn)
+            model.set_equations()
+
+            uzawa_iteration_counter += 1
+            print("Uzawa error:", uzawa_error)
+            total_iteration_counter += itr
+
+        print("Uzawa iteration counter:", uzawa_iteration_counter)
+        print("Total iteration counter:", total_iteration_counter)
+
+    # Define a function that does all the work during one time step.
+    def time_step() -> None:
+        model.time_manager.increase_time()
+        model.time_manager.increase_time_index()
+        logger.info(
+            f"\nTime step {model.time_manager.time_index} at time"
+            + f" {model.time_manager.time:.1e}"
+            + f" of {model.time_manager.time_final:.1e}"
+            + f" with time step {model.time_manager.dt:.1e}"
+        )
+        uzawa_algorithm()
+        model.after_uzawa_convergence(uzawa_iteration_counter)
+
+    while model.time_manager.time < model.time_manager.time_final:
+        time_step()
+        eqn_lst = list(model.equation_system.equations.keys())
+        for eqn in eqn_lst:
+            model.equation_system.remove_equation(eqn)
+        model.set_equations()
+
+    model.after_simulation()
+
+
+def run_time_dependent_uzawa_aa_model(model, params: dict, depth: int) -> tuple(list, int):
+    """Run a time-dependent model using an implicit Uzawa algorithm together with
+    Anderson acceleration. Must be combined with mixin defining the Uzawa equations and
+    solution strategy"""
+
+    # Assign parameters, variables and discretizations. Discretize time-indepedent terms
+    if params.get("prepare_simulation", True):
+        model.prepare_simulation()
+
+    # Assign a solver
+    solver = _choose_solver(model, params)
+    uzawa_errors = []
+    uzawa_iteration_counter = 0
+
+    # Uzawa iteration
+    def uzawa_algorithm() -> None:
+        uzawa_error = 1
+        nonlocal uzawa_errors
+        nonlocal uzawa_iteration_counter
+        total_iteration_counter = 0
+
+        # dimension = model.equation_system.num_dofs()
+        # AA on just the contact traction
+        dimension = len(model.equation_system.dofs_of(["t"]))
+        aa = AndersonAcceleration(dimension, depth)
+        while uzawa_error >= 1e-9:
+            # One iteration of the Uzawa loop consists of replacing the complementary
+            # functions with regularized versions, and solving the resulting nonlinear
+            # system.
+            # The regularizations depend on the solution at the previous Uzawa iteration.
+            # The loop continues until the difference in solutions from two consecutive
+            # Uzawa iterations falls below a given tolerance.
+
+            val_prev = model.equation_system.get_variable_values(iterate_index=0)
+            val_prev_trac = model.equation_system.get_variable_values(["t"], iterate_index=0)
+            _, itr = solver.solve(model)
+            val_intermediate = model.equation_system.get_variable_values(iterate_index=0)
+
+            # Anderson acceleration on traction
+            val_int_trac = model.equation_system.get_variable_values(["t"], iterate_index=0)
+            g_i = val_int_trac
+            f_i = val_int_trac-val_prev_trac
+            val_current_trac = aa.apply(g_i, f_i, uzawa_iteration_counter)
+            model.equation_system.set_variable_values(val_current_trac, ["t"], iterate_index=0)
+            val_current = model.equation_system.get_variable_values(iterate_index=0)
+
+            # Anderson acceleration
+            # g_i = val_intermediate
+            # f_i = val_intermediate-val_prev
+            # val_current = aa.apply(g_i, f_i, uzawa_iteration_counter)
+
+            uzawa_error = float(np.linalg.norm(val_current - val_prev))
+            uzawa_errors.append(uzawa_error)
+
+            # Update numerical constants. Lots of options for how to update.
+            model.c_n += 0
+            model.c_t += 0
+
+            # Update normal and tangential tractions, to be used as constants in the next
+            # Uzawa iteration.
+            subdomains = model.mdg.subdomains(dim=model.nd - 1)
+            t_n = model.normal_component(subdomains) @ model.contact_traction(
+                subdomains)
+            t_t = model.tangential_component(subdomains) @ model.contact_traction(
+                subdomains)
+            model.t_n_prev = t_n.value(model.equation_system)
+            model.t_t_prev = t_t.value(model.equation_system)
+
+            # Update equations with new regularizations
+            eqn_lst = list(model.equation_system.equations.keys())
+            for eqn in eqn_lst:
+                model.equation_system.remove_equation(eqn)
+            model.set_equations()
+
+            uzawa_iteration_counter += 1
+            print("Uzawa error:", uzawa_error, "Uzawa_counter:", uzawa_iteration_counter)
+            total_iteration_counter += itr
+
+        print("Uzawa iteration counter:", uzawa_iteration_counter)
+        print("Total iteration counter:", total_iteration_counter)
+
+    # Define a function that does all the work during one time step.
+    def time_step() -> None:
+        model.time_manager.increase_time()
+        model.time_manager.increase_time_index()
+        logger.info(
+            f"\nTime step {model.time_manager.time_index} at time"
+            + f" {model.time_manager.time:.1e}"
+            + f" of {model.time_manager.time_final:.1e}"
+            + f" with time step {model.time_manager.dt:.1e}"
+        )
+        uzawa_algorithm()
+        model.after_uzawa_convergence(uzawa_iteration_counter)
+
+    while model.time_manager.time < model.time_manager.time_final:
+        time_step()
+        eqn_lst = list(model.equation_system.equations.keys())
+        for eqn in eqn_lst:
+            model.equation_system.remove_equation(eqn)
+        model.set_equations()
+
+    model.after_simulation()
+    return uzawa_errors, uzawa_iteration_counter
+
+
 def _run_iterative_model(model, params: dict) -> None:
     """Run an iterative model.
 
