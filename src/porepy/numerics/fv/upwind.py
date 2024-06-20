2ed38047bb24cbbc02ec2138878a541441c43adc
Veljko Lipovac <lipovac.v@outlook.com>
2024-06-20 10:51:33 +0200

    MOD: Changing sing convention for Upwind BC matrices.

--- src/porepy/numerics/fv/upwind.py
+++ src/porepy/numerics/fv/upwind.py
@@ -109,7 +109,7 @@ class Upwind(Discretization):
                 with multiple components is only supported in Ad mode.
                 """
             )
-        rhs = div * (bc_discr_neu + bc_discr_dir * flux_mat) * bc_values
+        rhs = -div * (bc_discr_neu + bc_discr_dir * flux_mat) * bc_values
         return matrix, rhs
 
     def discretize(self, sd: pp.Grid, data: dict) -> None:
@@ -253,11 +253,11 @@ class Upwind(Discretization):
         # Need minus signs on both Neumann and Dirichlet data to ensure that
         # accumulation follows from negative fluxes.
         bc_discr_neu = sps.coo_matrix(
-            (-sgn_div[neumann_ind], (neumann_ind, neumann_ind)),
+            (sgn_div[neumann_ind], (neumann_ind, neumann_ind)),
             shape=(sd.num_faces, sd.num_faces),
         ).tocsr()
         bc_discr_dir = sps.coo_matrix(
-            (-np.ones(inflow_ind.size), (inflow_ind, inflow_ind)),
+            (np.ones(inflow_ind.size), (inflow_ind, inflow_ind)),
             shape=(sd.num_faces, sd.num_faces),
         ).tocsr()
 
