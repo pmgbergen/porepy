a0c3c382123683d680e8b8f8b41595b62235ef9e
Micheal Oguntola <micheal.b.oguntola@gmail.com>
2024-08-29 13:16:42 +0200

    Fix: params split into solver_param and model_params in tests in numerics and viz folders

--- tests/viz/test_solver_statistics.py
+++ tests/viz/test_solver_statistics.py
@@ -6,9 +6,8 @@ from porepy.applications.test_utils.models import Poromechanics
 
 def test_solver_statistic_attributes():
     """Runs default Poromechanics simulation and tests availability of solver statistics."""
-    params = {}
     model = Poromechanics()
-    pp.run_time_dependent_model(model, params)
+    pp.run_time_dependent_model(model)
 
     # Unit tests
     assert hasattr(model, "nonlinear_solver_statistics")
