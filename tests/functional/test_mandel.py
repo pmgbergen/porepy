f24509647e69845e8195db2f51045cc5a6ea4f05
Micheal Oguntola <micheal.b.oguntola@gmail.com>
2024-08-30 12:18:26 +0200

    Fix naming convection in test_mandel

--- tests/functional/test_mandel.py
+++ tests/functional/test_mandel.py
@@ -38,12 +38,12 @@ def results() -> list[MandelSaveData]:
         "solid": pp.SolidConstants(mandel_solid_constants),
     }
     time_manager = pp.TimeManager([0, 25, 50], 25, True)
-    params = {
+    model_params = {
         "material_constants": material_constants,
         "time_manager": time_manager,
     }
-    setup = MandelSetup(params)
-    pp.run_time_dependent_model(setup, params)
+    setup = MandelSetup(model_params)
+    pp.run_time_dependent_model(setup)
     return setup.results
 
 
@@ -150,12 +150,12 @@ def test_scaled_vs_unscaled_systems():
         "solid": pp.SolidConstants(mandel_solid_constants),
     }
     time_manager_unscaled = pp.TimeManager([0, 10], 10, True)
-    params_unscaled = {
+    model_params_unscaled = {
         "material_constants": material_constants_unscaled,
         "time_manager": time_manager_unscaled,
     }
-    unscaled = MandelSetup(params=params_unscaled)
-    pp.run_time_dependent_model(unscaled)
+    unscaled_model = MandelSetup(params=model_params_unscaled)
+    pp.run_time_dependent_model(unscaled_model)
 
     # The scaled problem
     material_constants_scaled = {
@@ -170,17 +170,17 @@ def test_scaled_vs_unscaled_systems():
         "time_manager": time_manager_scaled,
         "units": units,
     }
-    scaled = MandelSetup(params=model_params_scaled)
-    pp.run_time_dependent_model(model=scaled)
+    scaled_model = MandelSetup(params=model_params_scaled)
+    pp.run_time_dependent_model(model=scaled_model)
 
     # Compare results
     np.testing.assert_almost_equal(
-        unscaled.results[-1].error_pressure,
-        scaled.results[-1].error_pressure,
+        unscaled_model.results[-1].error_pressure,
+        scaled_model.results[-1].error_pressure,
         decimal=5,
     )
     np.testing.assert_almost_equal(
-        unscaled.results[-1].error_displacement,
-        scaled.results[-1].error_displacement,
+        unscaled_model.results[-1].error_displacement,
+        scaled_model.results[-1].error_displacement,
         decimal=5,
     )
