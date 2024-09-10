e07d553d9e17d2ce786aaa83ef9b21bcd7452fec
Ivar Stefansson <ivar.stefansson@uib.no>
2024-09-10 10:23:50 +0000

    BUG: Use unscaled stiffness for -1 check

--- src/porepy/models/constitutive_laws.py
+++ src/porepy/models/constitutive_laws.py
@@ -4447,8 +4447,9 @@ class ElastoPlasticFractureDeformation:
         t_t = nd_vec_to_tangential @ self.contact_traction(subdomains)
 
         stiffness = self.fracture_tangential_stiffness(subdomains)
-        stiffness_value = stiffness.value(self.equation_system)
-        if np.any(np.isclose(stiffness_value, -1.0, atol=1e-12, rtol=1e-12)):
+        # Retrieve the *unscaled* stiffness value for the check below.
+        stiffness_value = self.solid.fracture_tangential_stiffness()
+        if np.isclose(stiffness_value, -1.0, atol=1e-12, rtol=1e-12):
             # Stiffness=-1 indicates no elastic tangential deformation. Small tolerances
             # are used to avoid numerical issues, but allowing for a float value.
             num_cells = sum(sd.num_cells for sd in subdomains)
