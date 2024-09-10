BUG: New fix of scaling for -1 check

src/porepy/models/constitutive_laws.py
@@ -4429,8 +4429,10 @@ class ElastoPlasticFractureDeformation:
 
         stiffness = self.fracture_tangential_stiffness(subdomains)
         # Retrieve the *unscaled* stiffness value for the check below.
-        stiffness_value = self.solid.fracture_tangential_stiffness()
-        if np.isclose(stiffness_value, -1.0, atol=1e-12, rtol=1e-12):
+        stiffness_value = self.solid.convert_units(
+            stiffness.value(self.equation_system), "Pa*m^-1", to_si=True
+        )
+        if np.any(np.isclose(stiffness_value, -1.0, atol=1e-12, rtol=1e-12)):
             # Stiffness=-1 indicates no elastic tangential deformation. Small tolerances
             # are used to avoid numerical issues, but allowing for a float value.
             num_cells = sum(sd.num_cells for sd in subdomains)