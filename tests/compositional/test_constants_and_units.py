FIX: Remove connection between units of SolidConstants and FractureSolidConstants.

tests/compositional/test_constants_and_units.py
@@ -17,6 +17,8 @@ import porepy as pp
 
 from dataclasses import FrozenInstanceError
 
+from porepy.examples.flow_benchmark_2d_case_1 import FractureSolidConstants
+
 # TODO remove
 @pytest.fixture
 def subdomains() -> list[pp.Grid]:
@@ -198,7 +200,8 @@ def test_convert_units(modify_dict, base_units):
         pp.Constants,
         pp.FluidComponent,
         pp.SolidConstants,
-        pp.ReferenceVariableValues
+        FractureSolidConstants,
+        pp.ReferenceVariableValues,
     ]
 )
 def test_class_of_constants(constants_type: type[pp.Constants], scaled_units: pp.Units):
@@ -208,7 +211,7 @@ def test_class_of_constants(constants_type: type[pp.Constants], scaled_units: pp
 
     1. Creation with default values (should be unscaled)
     2. Constants declared with SI units are frozen
-    3. Constants are numbers (float, int)
+    3. Default field values are numbers (float, int)
     4. The conversion to new, scaled units works as intended.
 
     """
@@ -226,10 +229,11 @@ def test_class_of_constants(constants_type: type[pp.Constants], scaled_units: pp
         # Testing that the default values match with the default units (SI)
         c = getattr(constants, name)
         c_default = getattr(constants_default, name)
+        assert np.isclose(c, c_default)
 
+        # test that the values are or intended format.
         assert isinstance(c, (float, int))
         assert isinstance(c_default, (float, int))
-        assert np.isclose(c, c_default)
 
         # Testing that the constants cannot be reset anymore
         with pytest.raises(FrozenInstanceError):

tests/models/test_constitutive_laws.py
@@ -25,6 +25,7 @@ import pytest
 
 import porepy as pp
 import porepy.models.constitutive_laws as c_l
+from porepy.models.protocol import PorePyModel
 from porepy.applications.test_utils import models
 from porepy.applications.test_utils.reference_dense_arrays import (
     test_constitutive_laws as reference_dense_arrays,
@@ -60,7 +61,6 @@ solid_values.update(
         ("mass_balance", "fluid_flux", None),
         ("mass_balance", "pressure_trace", None),
         ("mass_balance", "porosity", None),
-        # ("mass_balance", "reference_pressure", None),
         # The body force and stress are only meaningful in the top dimension
         ("momentum_balance", "body_force", 0),
         ("momentum_balance", "stress", 0),
@@ -80,7 +80,6 @@ solid_values.update(
         ("energy_balance", "fourier_flux", None),
         ("energy_balance", "interface_enthalpy_flux", None),
         ("energy_balance", "interface_fourier_flux", None),
-        # ("energy_balance", "reference_temperature", None), TODO modify test
         ("energy_balance", "normal_thermal_conductivity", None),
         ("energy_balance", "aperture", None),
         # Poromechanics
@@ -403,6 +402,9 @@ def test_evaluated_values(
     # perturbations are small relative to
     assert np.allclose(val, expected, rtol=1e-8, atol=1e-10)
 
+
+def test_perturbation_from_reference(model: type[PorePyModel], quantities: list[str]):
+    ...
 
 @pytest.mark.parametrize(
     "geometry, domain_dimension, expected",