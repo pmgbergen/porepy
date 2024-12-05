90206f21163b58a3a123ed82e3ac39ed8d5b7a51
Isak Hammer <isakhammer@gmail.com>
2024-12-05 20:06:00 +0100

    Formatted with black

--- tests/functional/test_sneddon_2d.py
+++ tests/functional/test_sneddon_2d.py
@@ -1,4 +1,3 @@
-
 """
 This is a setup class for solving linear elasticity with contact between the fractures.
 We do not consider any fluid, and solve only for the linear elasticity coupled to the contact.
@@ -24,15 +23,17 @@ def compute_eta(pointset_centers: np.ndarray, center: np.ndarray) -> np.ndarray:
     Parameter::
     pointset: Array containing coordinates on the fracture
     center: fracture centre
-     
+
     Return:
-        Array of distances each point in pointset to the center 
+        Array of distances each point in pointset to the center
 
     """
     return pp.geometry.distances.point_pointset(pointset_centers, center)
 
 
-def get_bem_centers(a: float, h: float, n: int, theta: float, center: np.ndarray) -> np.ndarray:
+def get_bem_centers(
+    a: float, h: float, n: int, theta: float, center: np.ndarray
+) -> np.ndarray:
     """
     Compute coordinates of the centers of the bem segments
 
@@ -42,11 +43,11 @@ def get_bem_centers(a: float, h: float, n: int, theta: float, center: np.ndarray
         n: number of bem segments
         theta: orientation of the fracture
         center: center of the fracture
-    
+
     Return:
         Array of BEM centers
     """
-    
+
     # Coordinate system (4.5.1) in page 57 in in book Crouch Starfield 1983 Boundary Element Methods in Solid Mechanics
 
     bem_centers = np.zeros((3, n))
@@ -59,11 +60,13 @@ def get_bem_centers(a: float, h: float, n: int, theta: float, center: np.ndarray
     return bem_centers
 
 
-def analytical_displacements(a: float, eta: np.ndarray, p0: float, G: float, poi: float ) -> np.ndarray:
+def analytical_displacements(
+    a: float, eta: np.ndarray, p0: float, G: float, poi: float
+) -> np.ndarray:
     """
     Compute Sneddon's analytical solution for the pressurized fracture.
-    
-    Source: Sneddon Fourier transforms 1951 page 425 eq 92 
+
+    Source: Sneddon Fourier transforms 1951 page 425 eq 92
 
     Parameter:
         a: half fracture length
@@ -71,9 +74,9 @@ def analytical_displacements(a: float, eta: np.ndarray, p0: float, G: float, poi
         p0: pressure
         G: shear modulus
         poi: poisson ratio
-    
+
     Return
-        List of analytical normal displacement jumps. 
+        List of analytical normal displacement jumps.
     """
     cons = (1 - poi) / G * p0 * a * 2
     return cons * np.sqrt(1 - np.power(eta / a, 2))
@@ -88,7 +91,7 @@ def transform(xc: np.ndarray, x: np.ndarray, alpha: float) -> np.ndarray:
         x: Coordinates of boundary faces
         alpha: Fracture orientation
     Return:
-        Transformed coordinates 
+        Transformed coordinates
     """
     x_bar = np.zeros_like(x)
     # Terms in (7.4.6) in Crouch Starfield 1983 Boundary Element Methods in Solid Mechanics page 168
@@ -97,12 +100,20 @@ def transform(xc: np.ndarray, x: np.ndarray, alpha: float) -> np.ndarray:
     return x_bar
 
 
-def get_bc_val(g: pp.GridLike, bound_faces: np.ndarray, xf: np.ndarray, h: float, poi: float, alpha: float, du: float) -> np.ndarray:
+def get_bc_val(
+    g: pp.GridLike,
+    bound_faces: np.ndarray,
+    xf: np.ndarray,
+    h: float,
+    poi: float,
+    alpha: float,
+    du: float,
+) -> np.ndarray:
     """
     Compute semi-analytical displacement on the boundary using the BEM method for the Sneddon problem.
-    
+
     Parameter
-        g: Grid object 
+        g: Grid object
         bound_faces: Index lists for boundary faces
         xf: Coordinates of boundary faces
         h: BEM segment length
@@ -112,7 +123,7 @@ def get_bc_val(g: pp.GridLike, bound_faces: np.ndarray, xf: np.ndarray, h: float
     Return:
         Boundary values for the displacement
     """
-    
+
     # Equations for f2,f3,f4.f5 can be found in book Crouch Starfield 1983 Boundary Element Methods in Solid Mechanics pages 57, 84-92, 168
     f2 = np.zeros(bound_faces.size)
     f3 = np.zeros(bound_faces.size)
@@ -122,7 +133,7 @@ def get_bc_val(g: pp.GridLike, bound_faces: np.ndarray, xf: np.ndarray, h: float
     u = np.zeros((g.dim, g.num_faces))
 
     # Constant term in (7.4.5)
-    m = 1 / (4 * np.pi * (1 - poi)) 
+    m = 1 / (4 * np.pi * (1 - poi))
 
     # Second term in (7.4.5)
     f2[:] = m * (
@@ -161,7 +172,15 @@ def get_bc_val(g: pp.GridLike, bound_faces: np.ndarray, xf: np.ndarray, h: float
     return u
 
 
-def assign_bem(g: pp.Grid, h:float, bound_faces: np.ndarray, theta: float, bem_centers: np.ndarray, u_a: np.ndarray, poi: float) -> np.ndarray:
+def assign_bem(
+    g: pp.Grid,
+    h: float,
+    bound_faces: np.ndarray,
+    theta: float,
+    bem_centers: np.ndarray,
+    u_a: np.ndarray,
+    poi: float,
+) -> np.ndarray:
     """
     Compute analytical displacement using the BEM method for the Sneddon problem.
 
@@ -173,9 +192,9 @@ def assign_bem(g: pp.Grid, h:float, bound_faces: np.ndarray, theta: float, bem_c
         bem_centers: bem segments centers
         u_a: Sneddon's analytical relative normal displacement
         poi: Poisson ratio
-    
+
     Return:
-        Semi-analytical boundary displacement values 
+        Semi-analytical boundary displacement values
     """
 
     bc_val = np.zeros((g.dim, g.num_faces))
@@ -197,13 +216,20 @@ def assign_bem(g: pp.Grid, h:float, bound_faces: np.ndarray, theta: float, bem_c
     return bc_val
 
 
-
-def run_analytical_displacements(gb: pp.GridLike, a: float, p0: float, G: float, poi: float, height: float, length: float) -> tuple:
+def run_analytical_displacements(
+    gb: pp.GridLike,
+    a: float,
+    p0: float,
+    G: float,
+    poi: float,
+    height: float,
+    length: float,
+) -> tuple:
     """
     Compute Sneddon's analytical solution for the pressurized crack
     problem in question.
-    
-    Source: Sneddon Fourier transforms 1951 page 425 eq 92 
+
+    Source: Sneddon Fourier transforms 1951 page 425 eq 92
 
     Parameter
         gb: Grid object
@@ -213,8 +239,8 @@ def run_analytical_displacements(gb: pp.GridLike, a: float, p0: float, G: float,
         G: Shear modulus
         poi: Poisson ratio
         height,length: Height and length of domain
-    
-    Return:  
+
+    Return:
         A tuple containing two vectors: a list of distances from the fracture center to each fracture coordinate, and the corresponding analytical apertures.
     """
     ambient_dim = gb.dim_max()
@@ -222,19 +248,26 @@ def run_analytical_displacements(gb: pp.GridLike, a: float, p0: float, G: float,
     fracture_center = np.array([length / 2, height / 2, 0])
 
     fracture_faces = g_1.cell_centers
-    
-    # compute distances from fracture centre with its corresponding apertures 
-    eta = compute_eta(fracture_faces, fracture_center)   
-    apertures = analytical_displacements(a, eta, p0,  G, poi)
-    
+
+    # compute distances from fracture centre with its corresponding apertures
+    eta = compute_eta(fracture_faces, fracture_center)
+    apertures = analytical_displacements(a, eta, p0, G, poi)
+
     return eta, apertures
 
 
 class ModifiedGeometry:
     def set_domain(self) -> None:
         """Defining a two-dimensional square domain with sidelength 2."""
-        
-        self._domain = pp.Domain(bounding_box={"xmin": 0, "ymin": 0, "xmax": self.params["length"], "ymax": self.params["height"]})
+
+        self._domain = pp.Domain(
+            bounding_box={
+                "xmin": 0,
+                "ymin": 0,
+                "xmax": self.params["length"],
+                "ymax": self.params["height"],
+            }
+        )
 
     def grid_type(self) -> str:
         """Choosing the grid type for our domain."""
@@ -269,10 +302,19 @@ class ModifiedBoundaryConditions:
         western boundaries and ones for the eastern boundary.
 
         """
-       
-        a, p0, G, poi = self.params["a"], self.params["p0"], self.params["material_constants"]["solid"].shear_modulus, self.params["poi"]
-        theta, length, height = self.params["theta"], self.params["length"], self.params["height"]
- 
+
+        a, p0, G, poi = (
+            self.params["a"],
+            self.params["p0"],
+            self.params["material_constants"]["solid"].shear_modulus,
+            self.params["poi"],
+        )
+        theta, length, height = (
+            self.params["theta"],
+            self.params["length"],
+            self.params["height"],
+        )
+
         sd = bg.parent
         # bg = self.mdg.subdomain_to_boundary_grid(sd) # technique to extract bg via sd
 
@@ -289,7 +331,7 @@ class ModifiedBoundaryConditions:
         center = np.array([length / 2, height / 2, 0])
         bem_centers = get_bem_centers(a, h, n, theta, center)
         eta = compute_eta(bem_centers, center)
-        
+
         u_a = -analytical_displacements(a, eta, p0, G, poi)
 
         u_bc = assign_bem(sd, h / 2, box_faces, theta, bem_centers, u_a, poi)
@@ -300,7 +342,9 @@ class ModifiedBoundaryConditions:
 class PressureStressMixin:
     def pressure(self, subdomains: pp.Grid):
         # Return pressure in the fracture domain
-        mat = self.params["p0"] * np.ones(sum(subdomain.num_cells for subdomain in subdomains))
+        mat = self.params["p0"] * np.ones(
+            sum(subdomain.num_cells for subdomain in subdomains)
+        )
         return pp.ad.DenseArray(mat)
 
     def stress_discretization(
@@ -327,17 +371,21 @@ class MomentumBalanceGeometryBC(
 ):
     ...
     """Adding geometry and modified boundary conditions to the default model."""
-    
 
 
-
-
-def simulation(frac_pts: np.ndarray, theta_rad: np.ndarray, h: np.ndarray,
-               a: float =1.5, height: float = 1., length: float= 1.,
-               p0: float =1e-4, G: float = 1.0, poi: float=0.25) -> float:
-    
+def simulation(
+    frac_pts: np.ndarray,
+    theta_rad: np.ndarray,
+    h: np.ndarray,
+    a: float = 1.5,
+    height: float = 1.0,
+    length: float = 1.0,
+    p0: float = 1e-4,
+    G: float = 1.0,
+    poi: float = 0.25,
+) -> float:
     """
-    Simulates a 2D fracture linear elasticity problem using a momentum balance model and 
+    Simulates a 2D fracture linear elasticity problem using a momentum balance model and
     computes the relative L2 error between numerical and analytical displacement jumps.
 
     Parameters:
@@ -355,23 +403,24 @@ def simulation(frac_pts: np.ndarray, theta_rad: np.ndarray, h: np.ndarray,
         e: The relative L2 error between the analytical and numerical displacement jumps
         on the fracture.
     """
-    lam = 2 * G * poi / (1 - 2 * poi)  # Convertion formula from shear modulus and poission to lame lambda parameter 
-    solid = pp.SolidConstants(shear_modulus = G, lame_lambda = lam)
+    lam = (
+        2 * G * poi / (1 - 2 * poi)
+    )  # Convertion formula from shear modulus and poission to lame lambda parameter
+    solid = pp.SolidConstants(shear_modulus=G, lame_lambda=lam)
 
-    
     # Clean this up!! This is made so I can easily access the params in arbitary functions
     params = {
-                   "meshing_arguments": {"cell_size": h},
-                   "prepare_simulation": True,
-                    "material_constants": {"solid": solid},
-                    "frac_pts": frac_pts,
-                    "theta": theta_rad,
-                    "p0": p0, 
-                    "a": a,
-                    "poi": poi,
-                    "length": length, #this info can be accessed via box, so remove these as well.
-                    "height": height
-                }
+        "meshing_arguments": {"cell_size": h},
+        "prepare_simulation": True,
+        "material_constants": {"solid": solid},
+        "frac_pts": frac_pts,
+        "theta": theta_rad,
+        "p0": p0,
+        "a": a,
+        "poi": poi,
+        "length": length,  # this info can be accessed via box, so remove these as well.
+        "height": height,
+    }
 
     model = MomentumBalanceGeometryBC(params)
     pp.run_time_dependent_model(model, params)
@@ -382,22 +431,23 @@ def simulation(frac_pts: np.ndarray, theta_rad: np.ndarray, h: np.ndarray,
     # Comuting the numerical displacement jump along the fracture on the fracture cell centers.
     u_n: pp.ad.Operator = nd_vec_to_normal @ model.displacement_jump(frac_sd)
     u_n = u_n.value(model.equation_system)
-    
+
     # Checking convergence specifically on the fracture
     u_a = run_analytical_displacements(model.mdg, a, p0, G, poi, height, length)[1]
     e = ConvergenceAnalysis.l2_error(
-                    frac_sd[0], u_a, u_n, is_scalar=False, is_cc=True, relative=True
-                    )
-    
+        frac_sd[0], u_a, u_n, is_scalar=False, is_cc=True, relative=True
+    )
+
     return e
 
 
-  
-def compute_frac_pts(theta_rad: float, a: float, height: float, length:float) -> np.ndarray:
+def compute_frac_pts(
+    theta_rad: float, a: float, height: float, length: float
+) -> np.ndarray:
     """
     Assuming the fracture center is at the coordinate (height/2, length/2),
     compute the endpoints of a fracture given its orientation and fracture length.
-    
+
     Parameters:
     theta_rad: Angle of the fracture in radians
     a: Half-length of the fracture.
@@ -415,15 +465,18 @@ def compute_frac_pts(theta_rad: float, a: float, height: float, length:float) ->
     x_0 = length / 2 - a * np.sin(theta_rad)
     y_1 = height / 2 + a * np.cos(theta_rad)
     x_1 = length / 2 + a * np.sin(theta_rad)
-    
+
     def vector_length(x0, y0, x1, y1):
         point1 = np.array([x0, y0])
         point2 = np.array([x1, y1])
         return np.linalg.norm(point2 - point1)
-    
-    print(f"Frac points, ({x_0, y_0} ), ({x_1, y_1} ) with half length {vector_length(x_0,y_0, x_1, y_1)/2}")
+
+    print(
+        f"Frac points, ({x_0, y_0} ), ({x_1, y_1} ) with half length {vector_length(x_0,y_0, x_1, y_1)/2}"
+    )
     frac_pts = np.array([[x_0, y_0], [x_1, y_1]]).T
     return frac_pts
+
 
 def compute_eoc(h: np.ndarray, e: np.ndarray) -> np.ndarray:
     """
@@ -436,21 +489,20 @@ def compute_eoc(h: np.ndarray, e: np.ndarray) -> np.ndarray:
     Returns:
         eoc: Array of EOC values computed for consecutive levels of refinement.
     """
-    
+
     # Compute the Error Order of Convergence (EOC)
     eoc = np.zeros(len(e) - 1)
     for i in range(len(e) - 1):
-        eoc[i] = np.log(e[i+1] / e[i]) / np.log(h[i+1] / h[i])
+        eoc[i] = np.log(e[i + 1] / e[i]) / np.log(h[i + 1] / h[i])
     return eoc
 
-    
 
 def compute_convergence(
-    h_list: np.ndarray, 
-    theta_list: np.ndarray, 
-    a: float = 1, 
-    height: float = 1, 
-    length: float = 1
+    h_list: np.ndarray,
+    theta_list: np.ndarray,
+    a: float = 1,
+    height: float = 1,
+    length: float = 1,
 ) -> np.ndarray:
     """
     Compute the convergence behavior for the Sneddon problem across mesh refinements and orientations.
@@ -460,31 +512,35 @@ def compute_convergence(
         theta_list: Array of fracture orientations (in degrees).
         a: Half-length of the fracture.
         height: Height of the simulation domain.
-        length: Length of the simulation domain. 
+        length: Length of the simulation domain.
 
     Returns:
         A 2D array where rows correspond to different orientations and columns to mesh sizes.
-    """    # Compute Error for big fracture length
-    err = np.zeros(( len(theta_list), len(h_list)))  
+    """  # Compute Error for big fracture length
+    err = np.zeros((len(theta_list), len(h_list)))
     for k in range(0, len(theta_list)):
         theta_rad = math.radians(90 - theta_list[k])
-        frac_pts = compute_frac_pts(theta_rad,a, height=height, length=length)   
+        frac_pts = compute_frac_pts(theta_rad, a, height=height, length=length)
         for i in range(0, len(h_list)):
-            e = simulation(frac_pts, theta_rad, h_list[i], a=a, height=height, length=length)
+            e = simulation(
+                frac_pts, theta_rad, h_list[i], a=a, height=height, length=length
+            )
             err[k, i] = e
-        print(f"a: {a}, (height,length) = ({height}, {length}) theta: {theta_list[k]}, EOC: {compute_eoc(h_list, err[k, :])}")
-    
+        print(
+            f"a: {a}, (height,length) = ({height}, {length}) theta: {theta_list[k]}, EOC: {compute_eoc(h_list, err[k, :])}"
+        )
+
     return err
 
 
 def test_sneddon_2d():
     """
-    Test function to verify 2D Sneddon problem convergence.
+    Test function to 2D Sneddon problem convergence for the MPSA method.
 
     The function performs a convergence experiment for a 2D fracture problem
     by iterating over fracture orientations and mesh refinements.
 
-    It computes the average error across orientations and evaluates the 
+    It computes the average error across orientations and evaluates the
     convergence rate (EOC) through log-log regression.
 
     Raises:
@@ -492,38 +548,36 @@ def test_sneddon_2d():
     AssertionError
         If the estimated EOC is not greater than 1.0.
     """
-    
-    # Simulation settings    
-    num_refs = 4 # Number of refinements
-    num_theta = 6 # Number of iteration of theta
-    
-    theta0 = 0 # Inital orientation of fracture
-    delta_theta = 10 # The difference in theta between each refinement
-    theta_list = theta0 + np.arange(0,num_theta)*delta_theta # List of all the fracture orientations we run simulation on
-  
+
+    # Simulation settings
+    num_refs = 4  # Number of refinements
+    num_theta = 6  # Number of iteration of theta
+
+    theta0 = 0  # Inital orientation of fracture
+    delta_theta = 10  # The difference in theta between each refinement
+    theta_list = (
+        theta0 + np.arange(0, num_theta) * delta_theta
+    )  # List of all the fracture orientations we run simulation on
+
     # Mesh refinement
-    nf0 = 6 
-    nf = nf0 * 2**np.arange(0, num_refs)
+    nf0 = 6
+    nf = nf0 * 2 ** np.arange(0, num_refs)
     h_list = 2 * 1.5 / nf  # Mesh size for simulations
 
+    # In this test we define the
+    height, length = 1, 1
 
-    # In this test we define the 
-    height, length = 1,1
-    
     # Here we define the half length of the fracture to be sufficientl big to avoid any fracture tip effects
     a = 1
-   
+
     # Run convergence experiment
     err = compute_convergence(h_list, theta_list, a=a, height=height, length=length)
-    
+
     # Computing average error of each theta
-    avg_err = np.mean(err, axis=0) 
+    avg_err = np.mean(err, axis=0)
 
     # Perform linear regression on the log-log scale and compute EOC
     log_h = np.log(h_list)
     log_avg_err = np.log(avg_err)
     slope, _ = np.polyfit(log_h, log_avg_err, 1)
     assert slope > 1.0, f"Estimated EOC is {slope}, which is not greater than 1"
-
-   
-    
