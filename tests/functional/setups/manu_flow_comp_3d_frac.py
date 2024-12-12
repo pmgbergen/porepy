96c5001e7f1c9fdac3ba6e9017da74356053d6a9
Jhabriel Varela <jhabriel@gmail.com>
2024-12-12 13:03:49 -0300

    DOC: Update reference of manufactured solution

--- tests/functional/setups/manu_flow_comp_3d_frac.py
+++ tests/functional/setups/manu_flow_comp_3d_frac.py
@@ -3,8 +3,7 @@ This module contains a code verification implementation for a manufactured solut
 the three-dimensional, compressible, single-phase flow with a single, fully embedded
 vertical fracture in the middle of the domain.
 
-The exact solution was obtained by extending the solution from the incompressible
-case as given in Appendix D.2 from [1].
+The exact solution is presented in Section 6.1 from [1].
 
 In particular, we have added a pressure-dependent density which obeys the following
 constitutive relationship:
@@ -19,9 +18,9 @@ fluid compressibility.
 
 References:
 
-    - [1] Varela, J., Ahmed, E., Keilegavlen, E., Nordbotten, J. M., & Radu, F. A.
-      (2022). A posteriori error estimates for hierarchical mixed-dimensional
-      elliptic equations. Journal of Numerical Mathematics.
+    - [1] Stefansson, I., Varela, J., Keilegavlen, E., & Berre, I. (2024). Flexible and
+      rigorous numerical modelling of multiphysics processes in fractured porous
+      media using PorePy. Results in Applied Mathematics, 21, 100428.
 
 """
 from __future__ import annotations
