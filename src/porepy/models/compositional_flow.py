7907b5ac7e653a0c976770791f48eb2ce0909946
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2024-12-20 13:09:22 +0000

    DOC: Minor changes of documentation

--- src/porepy/models/compositional_flow.py
+++ src/porepy/models/compositional_flow.py
@@ -341,8 +341,8 @@ class TwoVariableTotalEnergyBalanceEquations(
                 cast(Sequence[pp.BoundaryGrid], domains),
             )
         elif compositional.is_fractional_flow(self):
-            # TODO is it worth reducing the operator tree size, by pulling the division
-            # by total mobility out of the sum?
+            # Implementation note: The operator tree size can be reduced, by pulling the
+            # division by total mobility out of the sum. Consider at some point.
             op = pp.ad.sum_operator_list(
                 [
                     phase.specific_enthalpy(domains)
@@ -352,10 +352,10 @@ class TwoVariableTotalEnergyBalanceEquations(
                     for phase in self.fluid.phases
                 ],
             )  # / self.total_mobility(domains)
-        # If the fractional-flow framework is not used, the weight corresponds to the
-        # advected enthalpy and a super call is performed (where the respective term is
-        # implemented).
         else:
+            # If the fractional-flow framework is not used, the weight corresponds to
+            # the advected enthalpy and a super call is performed (where the respective
+            # term is implemented).
             op = super().advection_weight_energy_balance(domains)
 
         op.set_name("advected_enthalpy")
