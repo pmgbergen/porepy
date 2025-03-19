17e2e268c325f597d1c30eba74270fd07779ce92
Veljko Lipovac <lipovac.v@outlook.com>
2025-03-19 12:35:20 +0100

    MIN: Improving docs according review.

--- src/porepy/models/fluid_mass_balance.py
+++ src/porepy/models/fluid_mass_balance.py
@@ -573,9 +573,6 @@ class InitialConditionsSinglePhaseFlow(pp.InitialConditionMixin):
             - :meth:`ic_values_well_flux`
 
         """
-        # This super call will execute set_initial_values_primary_variables first and
-        # provide IC values for pressure, which can be accessed in ic_values_well_flux
-        # for example.
         # NOTE IMPORTANT: Super-call placed on top to ensure that variables considered
         # primary in the initialization (like pressure) are available before various
         # fluxes are initialized (see also set_initial_values_primary_variables, whose
