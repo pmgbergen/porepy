b3a8cbc53225581f7894832df58cfd2ce4d34502
Ingrid Kristine Jacobsen <ingrid.kristine.jacobsen@gmail.com>
2025-03-27 15:15:45 +0000

    TST: Move tests from test_setmembership and delete test_setmembership

--- tests/utils/test_array_operations.py
+++ tests/utils/test_array_operations.py
@@ -410,6 +410,90 @@ def test_unique_columns(params):
 
 
 @pytest.mark.parametrize(
+    "a, b, ma_known, ia_known, sort",
+    [
+        (
+            np.array([[1, 3, 3, 1, 7], [3, 3, 2, 3, 0]]),
+            np.array([[3, 1, 3, 5, 3], [3, 3, 2, 1, 2]]),
+            np.array([1, 1, 1, 1, 0], dtype=bool),
+            np.array([1, 0, 2, 1]),
+            True,
+        ),
+        # unequal sizes
+        (
+            np.array([[1, 3, 3, 1, 7], [3, 3, 2, 3, 0]]),
+            np.array([[3, 1, 2, 5], [3, 3, 3, 1]]),
+            np.array([1, 1, 1, 1, 0], dtype=bool),
+            np.array([1, 0, 2, 1]),
+            True,
+        ),
+        (
+            np.array([[1, 3, 3, 1, 7], [3, 3, 2, 3, 0]]),
+            np.array([[3, 1, 2, 5, 3, 4, 7], [3, 3, 3, 1, 9, 9, 9]]),
+            np.array([1, 1, 1, 1, 0], dtype=bool),
+            np.array([1, 0, 2, 1]),
+            True,
+        ),
+        # double occurences
+        (
+            np.array([[1, 3, 3, 1, 7], [3, 3, 2, 3, 0]]),
+            np.array([[3, 2, 5], [3, 3, 1]]),
+            np.array([0, 1, 1, 0, 0], dtype=bool),
+            np.array([0, 1]),
+            True,
+        ),
+        (
+            np.array([[1, 3, 3, 1, 7], [3, 3, 2, 3, 0]]),
+            np.array([[3, 1, 2, 5, 3], [3, 3, 3, 1, 1]]),
+            np.array([1, 1, 1, 1, 0], dtype=bool),
+            np.array([1, 0, 2, 1]),
+            True,
+        ),
+        # 1D
+        (
+            np.array([0, 2, 1, 3, 0]),
+            np.array([2, 4, 3]),
+            np.array([0, 1, 0, 1, 0], dtype=bool),
+            np.array([0, 2]),
+            True,
+        ),
+        (
+            np.array([0, 2, 1, 13, 0]),
+            np.array([2, 4, 13, 0]),
+            np.array([1, 1, 0, 1, 1], dtype=bool),
+            np.array([3, 0, 2, 3]),
+            True,
+        ),
+        # test issue #123
+        (
+            np.array(
+                [
+                    [0.0, 1.0, 1.0, 0.0, 0.2, 0.8, 0.5, 0.5, 0.5],
+                    [0.0, 0.0, 1.0, 1.0, 0.5, 0.5, 0.8, 0.2, 0.5],
+                ]
+            ),
+            np.array([[0, 1, 1, 0], [0, 0, 1, 1]]),
+            np.array([1, 1, 1, 1, 0, 0, 0, 0, 0], dtype=bool),
+            np.array([0, 1, 2, 1]),
+            True,
+        ),
+        # Test no sorting:
+        (
+            np.array([[1, 3, 3, 1, 7], [3, 3, 2, 3, 0]]),
+            np.array([[3, 1, 2, 5, 3], [3, 3, 3, 1, 1]]),
+            np.array([1, 1, 0, 1, 0], dtype=bool),
+            np.array([1, 0, 1]),
+            False,
+        ),
+    ],
+)
+def test_ismember_rows(a, b, ma_known, ia_known, sort):
+    ma, ia = pp.array_operations.ismember_rows(a, b, sort=sort)
+    assert np.allclose(ma, ma_known)
+    assert np.allclose(ia, ia_known)
+
+
+@pytest.mark.parametrize(
     "params",
     [
         # Expand indices with C order.
