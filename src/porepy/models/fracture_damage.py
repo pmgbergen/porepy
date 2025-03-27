57143fc0ad53a8e4f76778105b0d9086c1562587
Ivar Stefansson <ivar.stefansson@uib.no>
2025-03-27 09:49:55 +0000

    MAINT: Add tolerance parameter to safe power and improve checks of zero terms in convolution integrals

--- src/porepy/models/fracture_damage.py
+++ src/porepy/models/fracture_damage.py
@@ -7,6 +7,15 @@ import porepy as pp
 
 
 class DamageHistoryVariable(pp.PorePyModel):
+    """Base class for damage history variables.
+
+    Sets up the damage history variable for the model. The damage history is defined on
+    fractures and used to compute damage evolution of fracture parameters such as
+    dilation and friction. The damage history variable is computed from a history
+    equation, see :class:`DamageHistoryEquation`.
+
+    """
+
     damage_history_variable = "damage_history"
 
     interface_displacement_variable: str
@@ -20,10 +29,10 @@ class DamageHistoryVariable(pp.PorePyModel):
 
         Parameters:
             subdomains: List of subdomains where the damage history is defined. Should
-            be of co-dimension one, i.e. fractures.
+                be of co-dimension one, i.e. fractures.
 
         Returns:
-            Variable for nondimensionalized fracture damage history.
+            Variable for fracture damage history.
 
         """
         # Check that the subdomains are fractures.
@@ -62,7 +71,6 @@ class DamageHistoryVariable(pp.PorePyModel):
         # needed if this is not the case. Specifically, some variables may need to be
         # stored at, say, two time steps for other purposes than computing the damage
         # history.
-        # import inspect
         for cls in self.__class__.__mro__:
             if cls is DamageHistoryVariable:
                 continue
@@ -101,10 +109,15 @@ class DamageHistoryVariable(pp.PorePyModel):
 
         This method defines which variables to store at all time steps for computation
         of the damage history. The default implementation includes the contact traction
-        and interface displacement variables. Note that if used with a pure contact
-        mechanics model, the contact traction variable is the only variable stored at
-        all time steps, since the interface displacement is not included in the model.
-        In that case, the method should be overridden.
+        and interface displacement variables. The reason we need traction in addition to
+        displacement is that the damage history is computed from the plastic
+        displacement jump, which requires the contact traction in the case of a nonzero
+        elastic jump.
+
+        Note that if used with a pure contact mechanics model, the contact traction
+        variable is the only variable stored at all time steps, since the interface
+        displacement is not included in the model. In that case, the method should be
+        overridden.
 
         Returns:
             List of variables.
@@ -137,7 +150,8 @@ class DamageHistoryEquation(pp.PorePyModel):
         self.equation_system.set_equation(eq, fractures, {"cells": 1})
 
     def before_nonlinear_loop(self):
-        """Reset the damage history equation to include new term.
+        """Reset the damage history equation to include new term from previous time
+        step.
 
         This needs to be done *after* time manager is updated, which happens in the
         super class method.
@@ -170,16 +184,17 @@ class AnisotropicHistoryEquation(DamageHistoryEquation):
 
     .. math::
 
-        h = \int{\tau} max(m_t \cdot \delta u_t, 0) \, d\tau,
+        h = \int{\tau} H(m_t \cdot u_t) ||m_t \cdot \delta u_t|| \, d\tau,
 
-    where :math:`\delta u_t` is the tangential plastic displacement jump increment and
-    :math:`m_t` is the normalized tangential plastic displacement jump, while h is the
-    damage history variable.
+    where :math:`u_t` is the tangential plastic displacement jump, :math:`\delta u_t` is
+    its increment, and :math:`m_t` is the normalized tangential plastic displacement
+    jump, i.e. unit vector in that direction. h is the damage history variable. See J.
+    White (2014) https://doi.org/10.1002/nag.2247 for more details.
 
     """
 
     damage_history: Callable[[list[pp.Grid]], pp.ad.Variable]
-    """Damage history variable on ad form."""
+    """Damage history variable on AD form."""
 
     plastic_displacement_jump: Callable[[list[pp.Grid]], pp.ad.Operator]
     """Method returning plastic displacement jump."""
@@ -202,31 +217,45 @@ class AnisotropicHistoryEquation(DamageHistoryEquation):
         )
         # The time increment of the tangential displacement jump.
         u_t_increment: pp.ad.Operator = pp.ad.time_increment(u_t)
-        # Prepare for taking the inner product sum.
+        # Prepare for taking the inner product sums below.
         tangential_basis = self.basis(subdomains, dim=self.nd - 1)
         tangential_to_scalar = pp.ad.sum_projection_list(
             [e_i.T for e_i in tangential_basis]
         )
 
+        # Compute the normalized tangential displacement jump (current time step).
         m_t = self._normalized_tangential_plastic_jump(subdomains)
-        num_steps = self.time_manager.time_index
         # The first term in the equation, the implicit part.
         current_inner = tangential_to_scalar @ (m_t * u_t)
-        # We are only interested in positive values of the inner product.
-        f_heaviside = pp.ad.Function(partial(pp.ad.functions.heaviside, 0.5), "max")
+        # We are only interested in non-negative values of the inner product. Specify
+        # the value of the heaviside function at zero. The choice follows White (2014).
+        zero_value = 1.0
+        f_heaviside = pp.ad.Function(
+            partial(pp.ad.functions.heaviside, zero_value), "max"
+        )
         f_abs = pp.ad.Function(pp.ad.functions.abs, "abs")
+        # Initialize the equation with the implicit part and the history variable.
         eq = self.damage_history(subdomains) - f_heaviside(current_inner) * f_abs(
             tangential_to_scalar @ (m_t * u_t_increment)
         )
-        # Then add the explicit part, i.e., the sum of the inner product of m with the u_t
-        # increment from all previous time steps. The sum starts at 1 since the first
-        # increment is already included in the implicit part.
+        # Then add the explicit part, i.e., the sum of the inner product of m with the
+        # u_t increment from all previous time steps. The sum starts at 1 since the
+        # first increment is already included in the implicit part.
+        num_steps = self.time_manager.time_index
         for i in range(1, num_steps):
             u_t_i = u_t.previous_timestep(i)
             u_t_increment_i = u_t_i - u_t.previous_timestep(i + 1)
-            if np.allclose(
-                self.equation_system.evaluate(u_t_increment_i), 0, atol=1e-12
-            ):
+            # Check if the contribution is zero. If it is, we skip the term to avoid
+            # unnecessary computations. Set a strict tolerance to avoid neglecting
+            # small terms. It's better to err on the side of caution here. Note that the
+            # contribution is linear in the increment, so small increments will give
+            # small contributions and should not impact the solution significantly. For
+            # long simulations with deformation on some time steps only, this could save
+            # non-negligible amounts of time.
+            tol = 1e-12 * self.equation_system.evaluate(
+                self.characteristic_displacement(subdomains)
+            )
+            if np.allclose(self.equation_system.evaluate(u_t_increment_i), 0, atol=tol):
                 # The contribution is zero, so we skip it to avoid unnecessary
                 # computations.
                 continue
@@ -254,17 +283,23 @@ class AnisotropicHistoryEquation(DamageHistoryEquation):
         tangential_basis = self.basis(subdomains, dim=self.nd - 1)
         nd_vec_to_tangential = self.tangential_component(subdomains)
 
+        # Get the tangential component of the plastic displacement jump.
         u_t = nd_vec_to_tangential @ self.plastic_displacement_jump(subdomains)
         scalar_to_tangential = pp.ad.sum_projection_list(tangential_basis)
+
+        # Define the functions for the norm and safe power.
         f_norm = pp.ad.Function(partial(pp.ad.l2_norm, self.nd - 1), "norm_function")
-        # TODO: Reconsider this function and its implementation.
-        f_power = pp.ad.Function(
-            partial(pp.ad.safe_power, -1, 1 / np.sqrt(self.nd - 1)), "safe power"
+        zero_tol = 1e-12 * self.equation_system.evaluate(
+            self.characteristic_displacement(subdomains)
         )
-        # Compute normalised tangential displacement increment. First, compute the norm
-        # of the increment.
+        f_power = pp.ad.Function(
+            partial(pp.ad.safe_power, -1, 1 / np.sqrt(self.nd - 1), zero_tol),
+            "safe power",
+        )
+        # Compute normalized tangential displacement. First, compute the norm of the
+        # displacement jump.
         norm_u_t = scalar_to_tangential @ f_norm(u_t)
-        # Then, normalise the increment by multiplying it by the inverse of the norm.
+        # Then, normalize the jump by multiplying it by the inverse of the norm.
         m_t = f_power(norm_u_t) * u_t
         return m_t
 
@@ -279,11 +314,13 @@ class IsotropicHistoryEquation(pp.PorePyModel):
         h = \int_{\tau} ||\delta u_t|| \, d\tau,
 
     where :math:`\delta u_t` is the plastic displacement jump increment and h is the
-    damage history variable.
+    damage history variable. See J. White (2014) https://doi.org/10.1002/nag.2247 for
+    more details.
+
     """
 
     damage_history: Callable[[list[pp.Grid]], pp.ad.Variable]
-    """Damage history variable on ad form."""
+    """Damage history variable on AD form."""
 
     plastic_displacement_jump: Callable[[list[pp.Grid]], pp.ad.Operator]
     """Method returning plastic displacement jump."""
@@ -303,24 +340,32 @@ class IsotropicHistoryEquation(pp.PorePyModel):
         u_t: pp.ad.Operator = nd_vec_to_tangential @ self.plastic_displacement_jump(
             subdomains
         )
-        # The time increment of the tangential displacement jump
+        # The time increment of the tangential displacement jump.
         u_t_increment: pp.ad.Operator = pp.ad.time_increment(u_t)
         f_norm = pp.ad.Function(partial(pp.ad.l2_norm, self.nd - 1), "norm_function")
 
         num_steps = self.time_manager.time_index
         # The first term in the equation, the implicit part.
         eq = self.damage_history(subdomains) - f_norm(u_t_increment)
-        # Then add the explicit part, i.e., the sum of the inner product of m with the u_t
-        # increment from all previous time steps. The sum starts at 1 since the first
-        # increment is already included in the implicit part.
+        # Then add the explicit part, i.e., the sum of the inner product of m with the
+        # u_t increment from all previous time steps. The sum starts at 1 since the
+        # first increment is already included in the implicit part.
         for i in range(1, num_steps):
             u_t_increment_i = u_t.previous_timestep(i) - u_t.previous_timestep(i + 1)
-            # if np.allclose(
-            #     self.equation_system.evaluate(u_t_increment_i), 0, atol=1e-12
-            # ):
-            #     # The contribution is zero, so we skip it to avoid unnecessary
-            #     # computations.
-            #     continue
+            # Check if the contribution is zero. If it is, we skip the term to avoid
+            # unnecessary computations. Set a strict tolerance to avoid neglecting
+            # small terms. It's better to err on the side of caution here. Note that the
+            # contribution is linear in the increment, so small increments will give
+            # small contributions and should not impact the solution significantly. For
+            # long simulations with deformation on some time steps only, this could save
+            # non-negligible amounts of time.
+            tol = 1e-12 * self.equation_system.evaluate(
+                self.characteristic_displacement(subdomains)
+            )
+            if np.allclose(self.equation_system.evaluate(u_t_increment_i), 0, atol=tol):
+                # The contribution is zero, so we skip it to avoid unnecessary
+                # computations.
+                continue
             eq -= f_norm(u_t_increment_i)
 
         return eq
