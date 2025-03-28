472e9c5dd46568045f58cbdc879776efb8038c96
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2025-03-28 09:38:07 +0000

    SPEED: Restructured evaluation of Darcy flux in non-linear loop
    For problems with many subdomains and interfaces, this can yield a significant speedup

--- src/porepy/models/energy_balance.py
+++ src/porepy/models/energy_balance.py
@@ -1192,13 +1192,37 @@ class SolutionStrategyEnergyBalance(pp.SolutionStrategy):
 
         """
         # Update parameters *before* the discretization matrices are re-computed.
-        for sd, data in self.mdg.subdomains(return_data=True):
-            vals = self.equation_system.evaluate(self.darcy_flux([sd]))
+
+        # Evaluate the Darcy flux for all subdomains together, then distribute the
+        # values to individual subdomains.
+        subdomains = self.mdg.subdomains()
+        darcy_flux = self.equation_system.evaluate(self.darcy_flux(subdomains))
+        # Compute offsets for the individual subdomains in the concatenated Darcy flux.
+        subdomain_offsets = np.cumsum([0] + [sd.num_faces for sd in subdomains])
+
+        for id, sd in enumerate(subdomains):
+            # Update the data dictionary with the Darcy flux for the current subdomain.
+            data = self.mdg.subdomain_data(sd)
+            vals = darcy_flux[subdomain_offsets[id] : subdomain_offsets[id + 1]]
             data[pp.PARAMETERS][self.enthalpy_keyword].update({"darcy_flux": vals})
 
-        for intf, data in self.mdg.interfaces(return_data=True, codim=1):
-            vals = self.equation_system.evaluate(self.interface_darcy_flux([intf]))
+        # Evaluate the Darcy flux for all interfaces together. Same procedure as for
+        # subdomains.
+        interfaces = self.mdg.interfaces(codim=1)
+        interface_darcy_flux = self.equation_system.evaluate(
+            self.interface_darcy_flux(interfaces)
+        )
+        interface_offsets = np.cumsum([0] + [intf.num_cells for intf in interfaces])
+
+        for id, intf in enumerate(interfaces):
+            # Update the data dictionary with the Darcy flux for the current interface.
+            data = self.mdg.interface_data(intf)
+            vals = interface_darcy_flux[
+                interface_offsets[id] : interface_offsets[id + 1]
+            ]
             data[pp.PARAMETERS][self.enthalpy_keyword].update({"darcy_flux": vals})
+
+        # The well fluxes are so few that we evaluate them one interface at a time.
         for intf, data in self.mdg.interfaces(return_data=True, codim=2):
             vals = self.equation_system.evaluate(self.well_flux([intf]))
             data[pp.PARAMETERS][self.enthalpy_keyword].update({"darcy_flux": vals})
