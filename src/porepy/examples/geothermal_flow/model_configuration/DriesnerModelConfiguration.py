cabcea00fc689326c7580f5cf0099507456a2a9b
Omar Duran <omaryesiduran@gmail.com>
2025-06-24 19:02:06 -0700

    WIP: making Dreisner run scripts robust.

--- src/porepy/examples/geothermal_flow/model_configuration/DriesnerModelConfiguration.py
+++ src/porepy/examples/geothermal_flow/model_configuration/DriesnerModelConfiguration.py
@@ -1,7 +1,7 @@
 from typing import Callable, Literal, Union, cast
 
 import numpy as np
-
+import time
 import porepy as pp
 import porepy.compositional as ppc
 
@@ -164,3 +164,215 @@ class DriesnerBrineFlowModel(  # type:ignore[misc]
     def set_nonlinear_discretizations(self):
         super().set_nonlinear_discretizations()
         self.set_nonlinear_buoyancy_discretization()
+
+    def solve_linear_system(self) -> np.ndarray:
+        """After calling the parent method, the global solution is calculated by Schur
+        expansion."""
+
+        eq_idx = self.equation_system.assembled_equation_indices
+        equation_names = list(eq_idx.keys())
+        # retrieve equations names
+        t_name = [name for name in equation_names if name.startswith("elimination_of_temperature")][0]
+        s_name = [name for name in equation_names if name.startswith("elimination_of_s_gas")][0]
+        xs_v_name = [name for name in equation_names if name.startswith("elimination_of_x_NaCl_gas")][0]
+        xs_l_name = [name for name in equation_names if name.startswith("elimination_of_x_NaCl_liq")][0]
+
+        # differential
+        p_eq_idx = eq_idx['mass_balance_equation']
+        z_eq_idx = eq_idx['component_mass_balance_equation_NaCl']
+        h_eq_idx = eq_idx['energy_balance_equation']
+        t_eq_idx = eq_idx[t_name]
+        # algebraic
+        s_eq_idx = eq_idx[s_name]
+        xs_v_eq_idx = eq_idx[xs_v_name]
+        xs_l_eq_idx = eq_idx[xs_l_name]
+
+        tb = time.time()
+        _, res_g = self.linear_system
+        sol = super().solve_linear_system()
+        reduce_linear_system_q = self.params.get("reduce_linear_system_q", False)
+        if reduce_linear_system_q:
+            raise ValueError("Case not implemented yet.")
+        te = time.time()
+        print("Overall residual norm: ", np.linalg.norm(res_g))
+        print("Variables involving differential operators:")
+        print("Pressure residual norm: ", np.linalg.norm(res_g[p_eq_idx]))
+        print("Composition residual norm: ", np.linalg.norm(res_g[z_eq_idx]))
+        print("Enthalpy residual norm: ", np.linalg.norm(res_g[h_eq_idx]))
+        print("Temperature residual norm: ", np.linalg.norm(res_g[t_eq_idx]))
+        print("Variables involving algebraic operators:")
+        print("Saturation residual norm: ", np.linalg.norm(res_g[s_eq_idx]))
+        print("Xs_v residual norm: ", np.linalg.norm(res_g[xs_v_eq_idx]))
+        print("Xs_l residual norm: ", np.linalg.norm(res_g[xs_l_eq_idx]))
+        print("Elapsed time linear solve: ", te - tb)
+        print("")
+
+        res_norm = np.linalg.norm(res_g)
+        p_res_norm = np.linalg.norm(res_g[p_eq_idx])
+        z_res_norm = np.linalg.norm(res_g[z_eq_idx])
+        h_res_norm = np.linalg.norm(res_g[h_eq_idx])
+        primary_res_norm = np.max([p_res_norm,z_res_norm,h_res_norm])
+        self.postprocessing_overshoots(sol)
+        eps_tol = 1.0e-4
+        p_idx = np.where(res_g[p_eq_idx] > eps_tol)[0]
+        z_idx = np.where(res_g[z_eq_idx] > eps_tol)[0]
+        h_idx = np.where(res_g[h_eq_idx] > eps_tol)[0]
+        alpha_idx = np.unique(np.concatenate([p_idx,z_idx,h_idx]))
+        self.postprocessing_thermal_overshoots(sol, alpha_idx)
+
+        # if self.nonlinear_solver_statistics.num_iteration > 10:
+        dx_scale = np.max([0.05, 0.98 ** self.nonlinear_solver_statistics.num_iteration])
+        print("Searching ...")
+        print("Newton correction factor: ", dx_scale)
+        print("")
+        sol *= dx_scale
+
+        return sol
+
+    def postprocessing_overshoots(self, delta_x):
+
+        zmin, zmax, hmin, hmax, pmin, pmax = self.vtk_sampler.search_space.bounds
+        z_scale, h_scale, p_scale = self.vtk_sampler.conversion_factors
+        zmin /= z_scale
+        zmax /= z_scale
+        hmin /= h_scale
+        hmax /= h_scale
+        pmin /= p_scale
+        pmax /= p_scale
+
+        tb = time.time()
+        x0 = self.equation_system.get_variable_values(iterate_index=0)
+        p_dof_idx = self.equation_system.dofs_of(['pressure'])
+        z_dof_idx = self.equation_system.dofs_of(['z_NaCl'])
+        h_dof_idx = self.equation_system.dofs_of(['enthalpy'])
+        t_dof_idx = self.equation_system.dofs_of(['temperature'])
+        s_dof_idx = self.equation_system.dofs_of(['s_gas'])
+        xw_v_dof_idx = self.equation_system.dofs_of(['x_H2O_gas'])
+        xw_l_dof_idx = self.equation_system.dofs_of(['x_H2O_liq'])
+        xs_v_dof_idx = self.equation_system.dofs_of(['x_NaCl_gas'])
+        xs_l_dof_idx = self.equation_system.dofs_of(['x_NaCl_liq'])
+
+        p_0 = x0[p_dof_idx]
+        z_0 = x0[z_dof_idx]
+        h_0 = x0[h_dof_idx]
+        t_0 = x0[t_dof_idx]
+
+        # control overshoots in:
+        # pressure
+        new_p = delta_x[p_dof_idx] + p_0
+        new_p = np.clip(new_p, 1.0e-3, 70.0)
+        delta_x[p_dof_idx] = new_p - p_0
+
+        # composition
+        new_z = delta_x[z_dof_idx] + z_0
+        new_z = np.clip(new_z, 0.0, 0.35)
+        delta_x[z_dof_idx] = new_z - z_0
+
+        # enthalpy
+        new_h = delta_x[h_dof_idx] + h_0
+        new_h = np.clip(new_h, 0.0, 4.0)
+        delta_x[h_dof_idx] = new_h - h_0
+
+        # temperature
+        new_t = delta_x[t_dof_idx] + t_0
+        new_t = np.clip(new_t, 0.0, 1273.15)
+        delta_x[t_dof_idx] = new_t - t_0
+
+        # secondary fractions
+        for dof_idx in [s_dof_idx, xw_v_dof_idx, xw_l_dof_idx, xs_v_dof_idx, xs_l_dof_idx]:
+            new_q = delta_x[dof_idx] + x0[dof_idx]
+            new_q = np.clip(new_q, 0.0, 1.0)
+            delta_x[dof_idx] = new_q - x0[dof_idx]
+
+        te = time.time()
+        print("Elapsed time for postprocessing overshoots: ", te - tb)
+        return
+
+    def postprocessing_thermal_overshoots(self, delta_x, alpha_idx):
+
+        if alpha_idx.size == 0:
+            return
+
+        zmin, zmax, hmin, hmax, pmin, pmax = self.vtk_sampler.search_space.bounds
+        z_scale, h_scale, p_scale = self.vtk_sampler.conversion_factors
+        zmin /= z_scale
+        zmax /= z_scale
+        hmin /= h_scale
+        hmax /= h_scale
+        pmin /= p_scale
+        pmax /= p_scale
+
+        tb = time.time()
+        x0 = self.equation_system.get_variable_values(iterate_index=0)
+        p_dof_idx = self.equation_system.dofs_of(['pressure'])
+        z_dof_idx = self.equation_system.dofs_of(['z_NaCl'])
+        h_dof_idx = self.equation_system.dofs_of(['enthalpy'])
+        t_dof_idx = self.equation_system.dofs_of(['temperature'])
+        s_dof_idx = self.equation_system.dofs_of(['s_gas'])
+        xw_v_dof_idx = self.equation_system.dofs_of(['x_H2O_gas'])
+        xw_l_dof_idx = self.equation_system.dofs_of(['x_H2O_liq'])
+        xs_v_dof_idx = self.equation_system.dofs_of(['x_NaCl_gas'])
+        xs_l_dof_idx = self.equation_system.dofs_of(['x_NaCl_liq'])
+
+        p_0 = x0[p_dof_idx]
+        z_0 = x0[z_dof_idx]
+        h_0 = x0[h_dof_idx]
+        t_0 = x0[t_dof_idx]
+        s_0 = x0[s_dof_idx]
+
+        # control overshoots in:
+        # pressure
+        new_p = delta_x[p_dof_idx] + p_0
+        new_p = np.clip(new_p, 1.0e-3, 70.0)
+        delta_x[p_dof_idx] = new_p - p_0
+
+        # composition
+        new_z = delta_x[z_dof_idx] + z_0
+        new_z = np.clip(new_z, 0.0, 0.35)
+        delta_x[z_dof_idx] = new_z - z_0
+
+        # enthalpy
+        new_h = delta_x[h_dof_idx] + h_0
+        new_h = np.clip(new_h, 0.0, 4.0)
+        delta_x[h_dof_idx] = new_h - h_0
+
+        # temperature
+        new_t = delta_x[t_dof_idx] + t_0
+        new_t = np.clip(new_t, 0.0, 1273.15)
+        delta_x[t_dof_idx] = new_t - t_0
+
+        # temperature
+        new_t = delta_x[t_dof_idx] + t_0
+        new_t = np.clip(new_t, 0.0, 1273.15)
+        delta_x[t_dof_idx] = new_t - t_0
+
+        # saturation
+        new_s = delta_x[s_dof_idx] + s_0
+        new_s = np.clip(new_s, 0.0, 1.0)
+        idx_tp = np.where((new_s[alpha_idx] > 0.0) & (new_s[alpha_idx] < 1.0))[0]
+        # Get indexes of values that are close to 0.0 or close to 1.0
+        idx_sp = np.where(np.isclose(new_s[alpha_idx], 0.0) | np.isclose(new_s[alpha_idx], 1.0))[0]
+
+        if idx_tp.size != 0:
+            # correct temperature from enthalpy
+            par_points = np.array((new_z[idx_tp], new_h[idx_tp], new_p[idx_tp])).T
+            self.vtk_sampler.sample_at(par_points)
+            star_t = self.vtk_sampler.sampled_could.point_data["Temperature"]
+            delta_x[t_dof_idx[idx_tp]] = star_t - t_0[idx_tp]
+            star_s = self.vtk_sampler.sampled_could.point_data["S_v"]
+            star_s = np.clip(star_s, 0.0, 1.0)
+            delta_x[s_dof_idx[idx_tp]] = star_s - s_0[idx_tp]
+        if idx_sp.size != 0:
+            # correct enthalpy from temperature
+            par_points = np.array((new_z[idx_sp], new_t[idx_sp], new_p[idx_sp])).T
+            self.vtk_sampler_ptz.sample_at(par_points)
+            star_h = self.vtk_sampler_ptz.sampled_could.point_data["H"] * 1.0e-6
+            delta_x[h_dof_idx[idx_sp]] = star_h - h_0[idx_sp]
+            star_s = self.vtk_sampler_ptz.sampled_could.point_data["S_v"]
+            star_s = np.clip(star_s, 0.0, 1.0)
+            delta_x[s_dof_idx[idx_sp]] = star_s - s_0[idx_sp]
+
+
+        te = time.time()
+        print("Elapsed time for postprocessing overshoots: ", te - tb)
+        return
