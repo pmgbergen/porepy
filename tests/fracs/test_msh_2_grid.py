493d4f74ad9969d5601c6e2e51663e1889f6d5f0
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2025-09-19 15:16:31 +0000

    TST: Updates to test_msh_2_grid after code review

--- tests/fracs/test_msh_2_grid.py
+++ tests/fracs/test_msh_2_grid.py
@@ -94,6 +94,7 @@ def create_gmsh_file(
     # during PorePy mesh generation from the gmsh file.
 
     msh_file: pathlib.Path = tmp_path / "test.msh"
+    msh_file = pathlib.Path("test.msh")
 
     # Step 1: Create a gmsh geometry for the fracture network.
     # Use functionality from pp.create_mdg and fracture_network.mesh to create a gmsh
@@ -101,8 +102,6 @@ def create_gmsh_file(
 
     # The following are going to get shifted to extra_args by _preprocess_simplex_args.
     kwargs: dict = {"file_name": str(msh_file), "write_geo": False}
-
-    dim: int
 
     # We need to differ between 2d and 3d fracture networks.
     if isinstance(fracture_network, FractureNetwork2d):
@@ -124,8 +123,14 @@ def create_gmsh_file(
         clear_gmsh=False,
     )
 
-    # Step 2: Add an inclusion to the gmsh geometry.
-    gmsh.open(str(msh_file))
+    # Step 2: Add an inclusion to the gmsh geometry. Need to open the .geo file created
+    # (which we generate by deault when calling fracture_network.mesh above). If we
+    # instead had opened the .msh file, we would not be able to add new geometry in a
+    # meaningful way.
+    geo_file = msh_file.with_suffix(".geo_unrolled")
+    gmsh.open(str(geo_file))
+
+    domain_tag = gmsh.model.get_entities(dim)[0][1]
 
     # Define the geometry for the inclusion.
     domain = fracture_network.domain
@@ -135,25 +140,38 @@ def create_gmsh_file(
     y_max = domain.bounding_box["ymax"]
     dx = x_max - x_min
     dy = y_max - y_min
+
+    box_size = 0.1
+    box_offset = 0.1
+
     if dim == 3:
         z_min = domain.bounding_box["zmin"]
         z_max = domain.bounding_box["zmax"]
         dz = z_max - z_min
+        box_min = np.array(
+            [x_min + box_offset * dx, y_min + box_offset * dy, z_min + box_offset * dz]
+        )
+        box_max = box_min + box_size * np.array([dx, dy, dz])
     else:
         z_min = 0.0
         z_max = 0.0
         dz = 0.0
+        box_min = np.array([x_min + box_offset * dx, y_min + box_offset * dy])
+        box_max = box_min + box_size * np.array([dx, dy])
 
     # Hard-coded geometry for the inclusion. It is placed away from the fractures.
     if dim == 2:
-        p_0 = gmsh.model.geo.addPoint(x_min + 0.1 * dx, y_min + 0.1 * dy, 0)
-        p_1 = gmsh.model.geo.addPoint(x_min + 0.2 * dx, y_min + 0.1 * dy, 0)
-        p_2 = gmsh.model.geo.addPoint(x_min + 0.2 * dx, y_min + 0.2 * dy, 0)
-        p_3 = gmsh.model.geo.addPoint(x_min + 0.1 * dx, y_min + 0.2 * dy, 0)
+        p_0 = gmsh.model.geo.addPoint(box_min[0], box_min[1], z_min)
+        p_1 = gmsh.model.geo.addPoint(box_max[1], box_min[1], z_min)
+        p_2 = gmsh.model.geo.addPoint(box_max[1], box_max[1], z_min)
+        p_3 = gmsh.model.geo.addPoint(box_min[0], box_max[1], z_min)
         line_0 = gmsh.model.geo.addLine(p_0, p_1)
         line_1 = gmsh.model.geo.addLine(p_1, p_2)
         line_2 = gmsh.model.geo.addLine(p_2, p_3)
         line_3 = gmsh.model.geo.addLine(p_3, p_0)
+        # Embed the lines in the domain so that the grid conforms to the inclusion.
+        gmsh.model.geo.synchronize()
+        gmsh.model.mesh.embed(1, [line_0, line_1, line_2, line_3], 2, domain_tag)
         loop = gmsh.model.geo.addCurveLoop([line_0, line_1, line_2, line_3])
         inclusion = gmsh.model.geo.addPlaneSurface([loop])
         gmsh.model.geo.synchronize()
@@ -168,30 +186,14 @@ def create_gmsh_file(
 
     else:  # dim == 3
         # Add points spanning a cube.
-        p_0 = gmsh.model.geo.addPoint(
-            x_min + 0.1 * dx, y_min + 0.1 * dy, z_min + 0.1 * dz
-        )
-        p_1 = gmsh.model.geo.addPoint(
-            x_min + 0.2 * dx, y_min + 0.1 * dy, z_min + 0.1 * dz
-        )
-        p_2 = gmsh.model.geo.addPoint(
-            x_min + 0.2 * dx, y_min + 0.2 * dy, z_min + 0.1 * dz
-        )
-        p_3 = gmsh.model.geo.addPoint(
-            x_min + 0.1 * dx, y_min + 0.2 * dy, z_min + 0.1 * dz
-        )
-        p_4 = gmsh.model.geo.addPoint(
-            x_min + 0.1 * dx, y_min + 0.1 * dy, z_min + 0.2 * dz
-        )
-        p_5 = gmsh.model.geo.addPoint(
-            x_min + 0.2 * dx, y_min + 0.1 * dy, z_min + 0.2 * dz
-        )
-        p_6 = gmsh.model.geo.addPoint(
-            x_min + 0.2 * dx, y_min + 0.2 * dy, z_min + 0.2 * dz
-        )
-        p_7 = gmsh.model.geo.addPoint(
-            x_min + 0.1 * dx, y_min + 0.2 * dy, z_min + 0.2 * dz
-        )
+        p_0 = gmsh.model.geo.addPoint(box_min[0], box_min[1], box_min[2])
+        p_1 = gmsh.model.geo.addPoint(box_max[1], box_min[1], box_min[2])
+        p_2 = gmsh.model.geo.addPoint(box_max[1], box_max[1], box_min[2])
+        p_3 = gmsh.model.geo.addPoint(box_min[0], box_max[1], box_min[2])
+        p_4 = gmsh.model.geo.addPoint(box_min[0], box_min[1], box_max[2])
+        p_5 = gmsh.model.geo.addPoint(box_max[1], box_min[1], box_max[2])
+        p_6 = gmsh.model.geo.addPoint(box_max[1], box_max[1], box_max[2])
+        p_7 = gmsh.model.geo.addPoint(box_min[0], box_max[1], box_max[2])
         # Add lines forming the wire basket of the cube.
         line_0 = gmsh.model.geo.addLine(p_0, p_1)
         line_1 = gmsh.model.geo.addLine(p_1, p_2)
@@ -218,6 +220,14 @@ def create_gmsh_file(
         surface_3 = gmsh.model.geo.addPlaneSurface([loop_3])
         surface_4 = gmsh.model.geo.addPlaneSurface([loop_4])
         surface_5 = gmsh.model.geo.addPlaneSurface([loop_5])
+        # Embed the surfaces in the domain so that the grid conforms to the inclusion.
+        gmsh.model.geo.synchronize()
+        gmsh.model.mesh.embed(
+            2,
+            [surface_0, surface_1, surface_2, surface_3, surface_4, surface_5],
+            3,
+            domain_tag,
+        )
         surface_loop = gmsh.model.geo.addSurfaceLoop(
             [surface_0, surface_1, surface_2, surface_3, surface_4, surface_5]
         )
@@ -237,9 +247,10 @@ def create_gmsh_file(
     gmsh.model.geo.synchronize()
     gmsh.model.mesh.generate(dim)
     gmsh.write(str(msh_file))
+    gmsh.write("final.geo_unrolled")
     gmsh.clear()
 
-    return str(msh_file)
+    return str(msh_file), box_min, box_max
 
 
 @pytest.mark.parametrize(
@@ -276,7 +287,9 @@ def test_create_grids_with_high_dim_inclusion(
 
     """
     # Read the gmsh file to get points, cells, cell_info and physical names.
-    pts, cells, cell_info, phys_names = _read_gmsh_file(create_gmsh_file)
+    msh_file, box_min, box_max = create_gmsh_file
+
+    pts, cells, cell_info, phys_names = _read_gmsh_file(msh_file)
 
     nd = len(dims)
 
@@ -303,6 +316,50 @@ def test_create_grids_with_high_dim_inclusion(
     for g in grids:
         assert isinstance(g, expected_grid_type)
         if g.dim == nd:
+            # Check that we preserved the inclusion tag.
             assert INCLUSION_NAME in g.tags
+
+            # Also check that the cells with the inclusion tag are indeed inside the
+            # inclusion box. This is complicated, since we add the inclusion in a way
+            # that seems not to be compatible with the ways of gmsh: The inclusion was
+            # added on top of the existing domain, instead of by carving out a piece of
+            # the domain and then adding the inclusion. This has two consequences:
+            # 1) The inclusion is somehow "floating" on top of the existing domain,
+            #    which means that the inclusion is covered by two sets of cells (don't
+            #    ask!). Hence, we cannot do a 1-1 comparison between those cells that
+            #    are inside the box and those that have the inclusion tag. This should
+            #    not be a problem for a properly constructed geometry, but it does give
+            #    some hints on how to construct the geometry if we want to avoid this.
+            # 2) Somehow, the overlapping cells are oriented in a way that does not fit
+            #    with our geometry computation, which results in some cells having
+            #    negative volume (EK did a quick check, and it seems that the
+            #    overlapping cells break with some tacit assumptions in our geometry
+            #    computations; however, since we have not encountered any practical
+            #    problems related to this, it seems highly plausible that this is caused
+            #    by Gmsh's reaction to our non-standard way of adding the inclusion).
+            #    Therefore, to get the cell centers, we cannot use g.compute_geometry(),
+            #    but rather need to compute the cell centers manually based on the
+            #    average coordinates of the cells' nodes.
+            cn = np.reshape(g.cell_nodes().tocsc().indices, (-1, g.dim + 1))
+            cx = np.mean(g.nodes[0, cn], axis=1)
+            cy = np.mean(g.nodes[1, cn], axis=1)
+            cz = np.mean(g.nodes[2, cn], axis=1)
+
+            if nd == 2:
+                inside_box = np.logical_and(
+                    np.logical_and(cx >= box_min[0], cx <= box_max[0]),
+                    np.logical_and(cy >= box_min[1], cy <= box_max[1]),
+                )
+            else:
+                inside_box = np.logical_and(
+                    np.logical_and(cx >= box_min[0], cx <= box_max[0]),
+                    np.logical_and(cy >= box_min[1], cy <= box_max[1]),
+                    np.logical_and(cz >= box_min[2], cz <= box_max[2]),
+                )
+
+            assert np.all(
+                np.isin(np.where(g.tags[INCLUSION_NAME]), np.where(inside_box))
+            )
+
         else:
             assert INCLUSION_NAME not in g.tags
