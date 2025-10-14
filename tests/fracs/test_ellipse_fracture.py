1ee5a8da482244def0dedfd16ecae17ecb49da17
Yuhe Zhang <514680256@qq.com>
2025-10-14 10:00:48 +0000

    ENH: Add tests for ellipse_fracture functionality with parameterized cases

---
+++ tests/fracs/test_ellipse_fracture.py
@@ -0,0 +1,28 @@
+"""Test functionality related to plane_fracture module."""
+
+import numpy as np
+import pytest
+
+from porepy.fracs import ellipse_fracture
+
+
+@pytest.mark.parametrize(
+    "ellipse_fracture_params",
+    [
+        # Center away from xy-plane
+        (np.array([3.0, 4.0, 5.0]), 2.0, 1.0, np.pi / 6.0, np.pi / 4.0, np.pi / 8.0),
+        # Fracture plane defined by x + y + z = 1
+        (np.array([8.0, 7.0, 6.0]), 2.5, 0.5, np.pi / 6.0, np.pi / 4.0, np.pi / 8.0),
+    ],
+)
+def test_ellipse_fracture_center(ellipse_fracture_params):
+    # parse the parameters
+    center, major_axis, minor_axis, major_axis_angle, strike_angle, dip_angle = (
+        ellipse_fracture_params
+    )
+
+    center_known = ellipse_fracture_params[0]
+    fracture = ellipse_fracture.EllipticFracture(
+        center, major_axis, minor_axis, major_axis_angle, strike_angle, dip_angle
+    )
+    assert np.allclose(center_known, fracture.center)
