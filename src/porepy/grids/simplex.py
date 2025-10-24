5688c11d4531b66eb43aafb83d2c7130624e208c
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2025-10-24 06:52:07 +0000

    STY: Documentation formatting, removed unused variable

--- src/porepy/grids/simplex.py
+++ src/porepy/grids/simplex.py
@@ -85,14 +85,12 @@ class TriangleGrid(Grid):
 
         # Uniquify the face-nodes (match the faces on two neighboring cells). Sort of
         # each row, so that faces with the same nodes but different orientation are
-        # recognized as the same face. Each face is recognized by its nodes from
-        # low to high node index.
-        # Also return the mapping back to the original ordering,
-        # this will give us the cell-face relation.
-        face_nodes, _, cell_face_mapping = np.unique(
+        # recognized as the same face. Each face is recognized by its nodes from low to
+        # high node index. Also return the mapping back to the original ordering, this
+        # will give us the cell-face relation.
+        face_nodes, cell_face_mapping = np.unique(
             np.sort(cell_wise_face_nodes, axis=1),
             axis=0,
-            return_index=True,
             return_inverse=True,
         )
 
