FIX: Missing case in cubic root computation.

src/porepy/compositional/peng_robinson/cubic_polynomial.py
@@ -442,24 +442,27 @@ def one_root(c2: float, c1: float, c0: float) -> np.ndarray:
     r1 = get_r1(c2, c1)
     r0 = get_r0(c2, c1, c0)
 
-    g = _get_Gamma(r1, r0)
-
-    if r1 < 0.0:
-        absg = np.abs(g)
-        t1 = -_get_t1(-r1)
-
-        # Special case for numerical stability.
-        if 1.0 - 1e-14 < absg < 1.0 + 1e-14:
-            t1 *= -1.0
-            t2 = 1.0
-        else:
-            t2 = np.sign(r0) * np.cosh(np.arccosh(absg) / 3.0)
-
-    elif r1 > 0.0:
-        t1 = _get_t1(r1)
-        t2 = np.sinh(np.arcsinh(g) / 3.0)
+    if r1 == 0.0:
+        t1 = 1.0 / 3.0
+        t2_ = c2**3 - 27.0 * c0
+        t2 = np.cbrt(np.abs(t2_)) * np.sign(t2_)
     else:
-        raise ValueError("r1 cannot be zero for one real root.")
+        g = _get_Gamma(r1, r0)
+
+        if r1 < 0.0:
+            absg = np.abs(g)
+            t1 = -_get_t1(-r1)
+
+            # Special case for numerical stability.
+            if 1.0 - 1e-14 < absg < 1.0 + 1e-14:
+                t1 *= -1.0
+                t2 = 1.0
+            else:
+                t2 = np.sign(r0) * np.cosh(np.arccosh(absg) / 3.0)
+
+        elif r1 > 0.0:
+            t1 = _get_t1(r1)
+            t2 = np.sinh(np.arcsinh(g) / 3.0)
 
     return np.array([t1 * t2]) - c2 / 3.0
 
@@ -485,48 +488,60 @@ def d_one_root(c2: float, c1: float, c0: float) -> np.ndarray:
     r1 = get_r1(c2, c1)
     r0 = get_r0(c2, c1, c0)
 
-    dr1 = get_dr1(c2)
-    dr0 = get_dr0(c2, c1)
+    if r1 == 0.0:
+        t2_ = c2**3 - 27.0 * c0
 
-    g = _get_Gamma(r1, r0)
-    dg = _get_dGamma(r1, r0)
-    dg = dg[0] * dr1 + dg[1] * dr0
+        t1 = 1.0 / 3.0
+        t2 = np.cbrt(np.abs(t2_))
 
-    if r1 < 0.0:
-        absg = np.abs(g)
-
-        t1 = -_get_t1(-r1)
-        dt1 = _get_dt1(-r1) * dr1
-
-        dt2 = np.zeros(3)
-
-        # Special case for numerical stability.
-        if 1.0 - 1e-14 < absg < 1.0 + 1e-14:
-            t1 *= -1.0
-            dt1 *= -1.0
-            t2 = 1.0
-        else:
-            t = np.cosh(np.arccosh(absg) / 3.0)
-            t2 = np.sign(r0) * t
-            dt2 = (
-                np.sign(r0)
-                * np.sinh(np.arccosh(absg) / 3.0)
-                / np.sqrt(absg**2 - 1.0)
-                * np.sign(g)
-                * dg
-                / 3.0
-            )
-            if np.abs(r0) <= 1e-14:
-                dt2 += t * dr0
-
-    elif r1 > 0.0:
-        t1 = _get_t1(r1)
-        t2 = np.sinh(np.arcsinh(g) / 3.0)
-
-        dt1 = _get_dt1(r1) * dr1
-        dt2 = np.cosh(np.arcsinh(g) / 3.0) / np.sqrt(g**2 + 1.0) * dg / 3.0
+        dt1 = np.zeros(3)
+        dt2 = (
+            1.0
+            / np.cbrt(np.abs(t2_) ** 2)
+            * np.sign(t2_)
+            * np.array([3 * c2**2, 0.0, -27.0])
+        )
     else:
-        raise ValueError("r1 cannot be zero for one real root.")
+        dr1 = get_dr1(c2)
+        dr0 = get_dr0(c2, c1)
+
+        g = _get_Gamma(r1, r0)
+        dg = _get_dGamma(r1, r0)
+        dg = dg[0] * dr1 + dg[1] * dr0
+
+        if r1 < 0.0:
+            absg = np.abs(g)
+
+            t1 = -_get_t1(-r1)
+            dt1 = _get_dt1(-r1) * dr1
+
+            dt2 = np.zeros(3)
+
+            # Special case for numerical stability.
+            if 1.0 - 1e-14 < absg < 1.0 + 1e-14:
+                t1 *= -1.0
+                dt1 *= -1.0
+                t2 = 1.0
+            else:
+                t = np.cosh(np.arccosh(absg) / 3.0)
+                t2 = np.sign(r0) * t
+                dt2 = (
+                    np.sign(r0)
+                    * np.sinh(np.arccosh(absg) / 3.0)
+                    / np.sqrt(absg**2 - 1.0)
+                    * np.sign(g)
+                    * dg
+                    / 3.0
+                )
+                if np.abs(r0) <= 1e-14:
+                    dt2 += t * dr0
+
+        elif r1 > 0.0:
+            t1 = _get_t1(r1)
+            t2 = np.sinh(np.arcsinh(g) / 3.0)
+
+            dt1 = _get_dt1(r1) * dr1
+            dt2 = np.cosh(np.arcsinh(g) / 3.0) / np.sqrt(g**2 + 1.0) * dg / 3.0
 
     z = t1 * dt2 + dt1 * t2 - np.array([1.0 / 3.0, 0.0, 0.0])
     return z.reshape((1, 3))

src/porepy/compositional/peng_robinson/eos.py
@@ -903,7 +903,7 @@ class CompiledPengRobinson(CompiledEoS):
         h_ideal_c = self._cfuncs["h_ideal"]
 
         @_COMPILER(PROPERTY_FUNC_SIGNATURE)
-        def h_c(prearg: np.ndarray, p: float, T: float, xn: np.ndarray) -> np.ndarray:
+        def h_c(prearg: np.ndarray, p: float, T: float, xn: np.ndarray) -> float:
             return h_ideal_c(p, T, xn) + h_dep_c(
                 p, T, xn, prearg[0], prearg[1], prearg[2]
             )
@@ -941,7 +941,7 @@ class CompiledPengRobinson(CompiledEoS):
         rho_c_ = self._cfuncs["rho"]
 
         @_COMPILER(PROPERTY_FUNC_SIGNATURE)
-        def rho_c(prearg: np.ndarray, p: float, T: float, xn: np.ndarray) -> np.ndarray:
+        def rho_c(prearg: np.ndarray, p: float, T: float, xn: np.ndarray) -> float:
             return rho_c_(p, T, prearg[2])
 
         return rho_c

tests/compositional/peng_robinson/test_cubic_polynomial.py
@@ -8,8 +8,9 @@ from typing import Callable
 import numpy as np
 import pytest
 
-# import os
-# os.environ["NUMBA_DISABLE_JIT"] = "1"
+import os
+
+os.environ["NUMBA_DISABLE_JIT"] = "1"
 
 from porepy.compositional.peng_robinson.compressibility_factor import (
     A_CRIT,
@@ -223,6 +224,21 @@ def _get_random_coeffs_for_two_root_case() -> np.ndarray:
             np.array([-3 * np.sqrt(2), 6, -2 * np.sqrt(2)]),
             np.array([np.sqrt(2)]),
             0,
+        ),
+        (  # x**3 + 1
+            np.array([0.0, 0.0, 1.0]),
+            np.array([-1]),
+            1,
+        ),
+        (  # x**3 - 1
+            np.array([0.0, 0.0, -1.0]),
+            np.array([1]),
+            1,
+        ),
+        (
+            np.array([2.0, 4.0 / 3.0, 1.0]),
+            np.array([-2 - np.cbrt(19)]) / 3.0,
+            1,
         ),
         (  # Peng-Robinson EoS critical point
             np.array(