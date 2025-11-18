9bf26947a2d8dd81a4a423277b7fc1e83cdc7970
Yuhe Zhang <514680256@qq.com>
2025-11-18 10:38:30 +0000

    FEAT: Implement geometry loading in LoadGeometryMixin

--- src/porepy/examples/flow_benchmark_3d_case_2.py
+++ src/porepy/examples/flow_benchmark_3d_case_2.py
@@ -156,7 +156,7 @@ class PermeabilitySpecification(Permeability):
 
 
 class BoundaryConditions(pp.PorePyModel):
-    r"""Define inlet and oulet boundary conditions as specified by the benchmark.
+    """Define inlet and oulet boundary conditions as specified by the benchmark.
 
     The inlet boundary is defined as:
 
