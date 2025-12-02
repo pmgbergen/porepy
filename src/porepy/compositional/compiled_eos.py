FIX: Sign in u_dep and pressure contribution in cohesion.

src/porepy/compositional/compiled_eos.py
@@ -613,7 +613,7 @@ def _evaluate_vectorized_property_derivatives_func(
     """
     N = p.shape[0]
     num_comp = xn.shape[1]
-    diffs = np.empty((2 + num_comp if num_comp > 1 else 2, N))
+    diffs = np.empty((2 + num_comp, N))
     for i in nb.prange(N):
         diffs[:, i] = property_diffs_func(
             prearg_val[i], prearg_jac[i], p[i], T[i], xn[i]
@@ -728,7 +728,7 @@ def _evaluate_vectorized_fug_coeff_diff_func(
 
     """
     n, ncomp = xn.shape
-    dphis = np.empty((ncomp, 2 + ncomp if ncomp > 1 else 2, n))
+    dphis = np.empty((ncomp, 2 + ncomp, n))
     for i in nb.prange(n):
         dphis[:, :, i] = fug_coeff_diff_func(
             prearg_val[i], prearg_jac[i], p[i], T[i], xn[i]
@@ -1195,7 +1195,7 @@ class CompiledEoS(EquationOfState):
             thermodynamic_input = (thermodynamic_input[0], thermodynamic_input[1], x)
         # If only 1 component
         elif len(thermodynamic_input) == 2 and self.nc == 1:
-            x = np.ones(thermodynamic_input[0].size)
+            x = np.ones((1, thermodynamic_input[0].size))
             thermodynamic_input = (thermodynamic_input[0], thermodynamic_input[1], x)
         else:
             raise ValueError(
@@ -1203,12 +1203,19 @@ class CompiledEoS(EquationOfState):
                 "(p, T, x_1, .., x_n) or (p, T) in case only 1 component."
             )
 
-        # Reshape to matrix in the single-component case for compatibility reasons.
-        if self.nc == 1 and x.ndim == 1:
-            x = x.reshape((1, x.shape[0]))
+        # Reshape to matrix in the single-value case for compatibility reasons.
+        if x.ndim == 1 and thermodynamic_input[0].size == 1:
+            x = x.reshape((self.nc, 1))
 
+        # Assert basic format to avoid unexpected behavior in compiled computations.
+        assert thermodynamic_input[0].shape == thermodynamic_input[1].shape, (
+            "Expecting thermodynamic input to be equally shaped."
+        )
         assert x.ndim == 2, (
             "Could not extract partial fractions as 2D array from thermodynamic input."
+        )
+        assert x.shape == (self.nc, thermodynamic_input[0].size), (
+            "Unexpected shape of partial fractions."
         )
 
         # NOTE: The vectorized functions expect fractions column-wise, while the

src/porepy/compositional/peng_robinson/eos.py
@@ -73,6 +73,11 @@ Uses :func:`~porepy.compositional._numba_interface.njit`.
 def covolume_dep(Z: float, B: float) -> float:
     r"""Special treatment of departure term to remain well-defined.
 
+    Note:
+        Consider switching to expression in terms of specific volume and
+        (dimensiona) covolume. Literature says more robust for near-critical
+        applications.
+
     Parameters:
         Z: Compressibility factor.
         B: Dimensionless covolume.
@@ -240,7 +245,7 @@ def ac_component(pc: float, Tc: float) -> float:
 
     """
     RT = R_U * Tc
-    return A_CRIT * RT**2 / pc**2
+    return A_CRIT * RT**2 / pc
 
 
 @_COMPILER(nb.f8(nb.f8), fastmath=NUMBA_FAST_MATH, cache=True)
@@ -682,7 +687,7 @@ def u_dep(
         The departure internal energy.
 
     """
-    _c = -R_U / np.sqrt(8)
+    _c = R_U / np.sqrt(8)
     lnZB1 = covolume_dep(Z, B)
     iB = 1.0 / B
     return _c * T * (A + T * dAdT) * lnZB1 * iB
@@ -701,7 +706,7 @@ def grad_u_dep(
     dAdT: float,
 ) -> np.ndarray:
     """Gradient of :func:`u_dep` with respect to its arguments."""
-    _c = -R_U / np.sqrt(8)
+    _c = R_U / np.sqrt(8)
     lnZB1 = covolume_dep(Z, B)
     dlnZB1 = grad_covolume_dep(Z, B)
 
@@ -811,7 +816,7 @@ class CompiledPengRobinson(CompiledEoS):
         )
         self.ideal_fluids: Sequence[IdealFluid] = ideal_fluids
 
-        self._ideal_funcs: PropertyFunctionDict = {}
+        self.ideal_funcs: PropertyFunctionDict = {}
         """Contains ideal parts for thermodynamic properties."""
 
     def get_prearg_for_values(self) -> VectorFunction:
@@ -1027,7 +1032,7 @@ class CompiledPengRobinson(CompiledEoS):
         return dphi_mix_c
 
     def get_h_function(self) -> ScalarFunction:
-        h_id_c = self._ideal_funcs["h"]
+        h_id_c = self.ideal_funcs["h"]
 
         @_COMPILER(PROPERTY_FUNC_SIGNATURE)
         def h_c(prearg: np.ndarray, p: float, T: float, xn: np.ndarray) -> float:
@@ -1044,7 +1049,7 @@ class CompiledPengRobinson(CompiledEoS):
         return h_c
 
     def get_grad_h_function(self) -> VectorFunction:
-        dh_id_c = self._ideal_funcs["dh"]
+        dh_id_c = self.ideal_funcs["dh"]
 
         @_COMPILER(PROPERTY_DERIVATIVE_FUNC_SIGNATURE)
         def dh_c(
@@ -1084,7 +1089,7 @@ class CompiledPengRobinson(CompiledEoS):
         return dh_c
 
     def get_u_function(self) -> ScalarFunction:
-        u_id_c = self._ideal_funcs["u"]
+        u_id_c = self.ideal_funcs["u"]
 
         @_COMPILER(PROPERTY_FUNC_SIGNATURE)
         def u_c(prearg: np.ndarray, p: float, T: float, xn: np.ndarray) -> float:
@@ -1099,7 +1104,7 @@ class CompiledPengRobinson(CompiledEoS):
         return u_c
 
     def get_grad_u_function(self) -> VectorFunction:
-        du_id_c = self._ideal_funcs["du"]
+        du_id_c = self.ideal_funcs["du"]
 
         @_COMPILER(PROPERTY_DERIVATIVE_FUNC_SIGNATURE)
         def du_c(
@@ -1254,7 +1259,7 @@ class CompiledPengRobinson(CompiledEoS):
 
         # endregion
 
-        self._ideal_funcs = {
+        self.ideal_funcs = {
             "h": h_ideal,
             "dh": dh_ideal,
             "u": u_ideal,

tests/compositional/peng_robinson/__init__.py
@@ -143,10 +143,7 @@ class PRLBC(pr.CompiledPengRobinson, pr.LBCViscosity):
             T: float,
             xn: np.ndarray,
         ) -> np.ndarray:
-            if xn.size > 1:
-                return np.zeros(2 + xn.shape[0])
-            else:
-                return np.zeros(2)
+            return np.zeros(2 + xn.shape[0])
 
         return dkappa_c
 

tests/compositional/peng_robinson/test_eos.py
@@ -12,6 +12,10 @@ from porepy.applications.test_utils.derivative_testing import (
     assert_order_at_least,
     get_EOC_taylor,
 )
+from porepy.compositional.ideal.collection import (
+    H_FORMATION_H2O_G_deNevers,
+    H_FORMATION_H2O_L_deNevers,
+)
 from porepy.compositional.peng_robinson.eos import (
     a_dl,
     a_VdW,
@@ -438,14 +442,14 @@ def test_ideal_mixture_energies(
         T = x[0]
         xn = np.array(x[1:])
         assert xn.size == ncomp, "Invalid number of components."
-        propfunc = pr_eos._ideal_funcs[property_name]
+        propfunc = pr_eos.ideal_funcs[property_name]
         return propfunc(T, xn)
 
     def dfunc(x):
         T = x[0]
         xn = np.array(x[1:])
         assert xn.size == ncomp, "Invalid number of components."
-        dpropfunc = pr_eos._ideal_funcs[f"d{property_name}"]
+        dpropfunc = pr_eos.ideal_funcs[f"d{property_name}"]
         return dpropfunc(T, xn)
 
     x0 = np.array([400.0] + ([1.0 / ncomp] * ncomp))
@@ -559,8 +563,8 @@ def test_property_derivatives(
 
     x0 = np.hstack((x0_pT, np.ones(ncomp) / ncomp))
 
-    # NOTE: Precision loss from ideal part is propagated to the real energies.
-    orders = get_EOC_taylor(func, dfunc, x0, d, h, tol=1e-9)
+    # NOTE: Precision loss from ideal part and cohesion is propagated and worsened.
+    orders = get_EOC_taylor(func, dfunc, x0, d, h, tol=1e-7)
     expected_order = calculate_expected_order(
         True if state == pp.compositional.PhysicalState.gas else False,
         tol,
@@ -572,6 +576,126 @@ def test_property_derivatives(
     assert_order_at_least(
         orders,
         expected_order,
-        tol=5e-2,
+        tol=6e-2,
+        asymptotic=4,
         err_msg=f"{property_name}; x0 = {x0[:2]}; d = {d}; state={state}",
+    )
+
+
+@pytest.mark.skipped(reason="slow due to compilation")
+@pytest.mark.parametrize(
+    "comps_and_phases",
+    [(1, "V"), (2, "V"), (3, "V")],
+    indirect=["comps_and_phases"],
+)
+def test_ideal_mixture_obeys_reference_state(
+    comps_and_phases: tuple[int, str],
+    pr_eos: pr.CompiledPengRobinson,
+) -> None:
+    """Tests that the ideal part of the fluid mixtures obey PorePy's reference state
+    when evaluated with a partial fraction for water of 1.
+
+    Note:
+        Very similar to ``test_ideal/test_ideal_water_obeys_reference_state``, but it is
+        an essential test for mixing and compilation.
+        Makes sure that the anchoring to the reference state is not messed up by the
+        parallelization framework.
+
+    """
+    H = pp.compositional.THD_REF.H
+    U = pp.compositional.THD_REF.U
+    R = pp.compositional.THD_REF.R_U
+
+    T = pp.compositional.THD_REF.T
+    xn = np.zeros(comps_and_phases[0])
+    xn[0] = 1.0
+
+    h_ideal = pr_eos.ideal_funcs["h"]
+    latent_heat = H_FORMATION_H2O_G_deNevers - H_FORMATION_H2O_L_deNevers
+
+    # NOTE: We test the positivity of latent heat and delta_u every time, because the
+    # sign is crucial in the rest of the test. Don't use absolute values.
+    assert latent_heat > 0, "Expecting latent heat to be positive."
+
+    np.testing.assert_allclose(h_ideal(T, xn) - H, latent_heat, atol=1e-15, rtol=0.0)
+
+    # The ideal gas internal energy should obey ideal gas law, where the difference
+    # in volume is equal to RT, and we use the definition of h = u + RT
+    # This holds only if the reference state is correctly used.
+    u_ideal = pr_eos.ideal_funcs["u"]
+    delta_u = latent_heat - R * T
+    assert delta_u > 0, (
+        "Expecting change in internal energy upon evaporation to be positive."
+    )
+
+    np.testing.assert_allclose(u_ideal(T, xn) - U, delta_u)
+
+
+@pytest.mark.skipped(reason="slow due to compilation")
+@pytest.mark.parametrize(
+    "comps_and_phases",
+    [(1, "V"), (2, "V"), (3, "V")],
+    indirect=["comps_and_phases"],
+)
+@pytest.mark.parametrize("N", [1, 2, 10])
+def test_real_mixture_obeys_reference_state(
+    N: int,
+    comps_and_phases: tuple[int, str],
+    pr_eos: pr.CompiledPengRobinson,
+) -> None:
+    """Tests that the real properrties of fluid mixtures obey PorePy's reference state
+    when evaluated with a partial fraction for water of 1.
+
+    Note however, that the Peng-Robinson departure functions dramatically
+    underpredict the departure from ideal state for water, especially at low
+    pressure.
+
+    In essence, the only thing we can test is that the latent heat is positive,
+    the change in internal energy is positive, and that the gas energies are close to
+    ideal values.
+
+    This test is also conducted with multiple evaluation ``N``, indirectly testing the
+    ``compute_property`` method for consistency in terms of parallelized evaluation.
+
+    """
+    H = pp.compositional.THD_REF.H
+    U = pp.compositional.THD_REF.U
+    R = pp.compositional.THD_REF.R_U
+
+    p = pp.compositional.THD_REF.P * np.ones(N)
+    T = pp.compositional.THD_REF.T * np.ones(N)
+    xn = np.zeros((comps_and_phases[0], N))
+    xn[0] = 1.0
+    # Avoid smoothing since triple point has two stable phases.
+    params = [0.0, 0.0]
+
+    prop_l = pr_eos.compute_phase_properties(
+        pp.compositional.PhysicalState.liquid, p, T, xn, params=params
+    )
+    prop_g = pr_eos.compute_phase_properties(
+        pp.compositional.PhysicalState.gas, p, T, xn, params=params
+    )
+
+    h_id = pr_eos.ideal_funcs["h"](pp.compositional.THD_REF.T, xn[:, 0])
+    u_id = pr_eos.ideal_funcs["u"](pp.compositional.THD_REF.T, xn[:, 0])
+    h_l = prop_l.h
+    u_l = prop_l.u
+    h_g = prop_g.h
+    u_g = prop_g.u
+
+    assert np.all(h_l < h_g), (
+        "Expecting liquid enthalpy to be smaller than gas enthalpy."
+    )
+    assert np.all(u_l < u_g), (
+        "Expecting liquid int. energy to be smaller than gas int. energy."
+    )
+
+    assert np.allclose(h_g, h_id, atol=1.0, rtol=0.0), (
+        "Expecting water vapor enthalpy to be close to ideal."
+    )
+    assert np.allclose(u_g, u_id, atol=1.0, rtol=0.0), (
+        "Expecting water vapor int. energy to be close to ideal."
+    )
+    assert np.allclose(np.abs(u_g - h_g), R * T, atol=1.0, rtol=0.0), (
+        "Expecting difference in vapor enthalpy and int. energy to be close to RT."
     )

tests/compositional/test_ideal.py
@@ -50,7 +50,8 @@ def test_ideal_water_obeys_reference_state(
     # Assert that the ideal gas water enthalpy at the defined reference temperature is
     # equal to the difference in the given formation enthalpies.
     h = h2o.funcs["h"]
-    latent_heat = np.abs(H_FORMATION_H2O_G_deNevers - H_FORMATION_H2O_L_deNevers)
+    latent_heat = H_FORMATION_H2O_G_deNevers - H_FORMATION_H2O_L_deNevers
+    assert latent_heat > 0, "Expecting latent heat to be positive."
     np.testing.assert_allclose(
         h(pc.THD_REF.T) - pc.THD_REF.H, latent_heat, atol=1e-15, rtol=0.0
     )
@@ -59,8 +60,11 @@ def test_ideal_water_obeys_reference_state(
     # in volume is equal to RT, and we use the definition of h = u + RT
     # This holds only if the reference state is correctly used.
     u = h2o.funcs["u"]
-    delta_u = pc.THD_REF.U + np.abs(latent_heat - pc.THD_REF.R_U * pc.THD_REF.T)
-    np.testing.assert_allclose(u(pc.THD_REF.T), delta_u)
+    delta_u = latent_heat - pc.THD_REF.R_U * pc.THD_REF.T
+    assert delta_u > 0, (
+        "Expecting change in internal energy upon evaporation to be positive."
+    )
+    np.testing.assert_allclose(u(pc.THD_REF.T) - pc.THD_REF.U, delta_u)
 
 
 @pytest.mark.parametrize("name", ["u", "h"])