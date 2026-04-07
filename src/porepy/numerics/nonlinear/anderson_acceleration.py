4bd1f6d3ee74e094b22d642f7ece8cce2790f5fb
Veljko Lipovac <lipovac.v@outlook.com>
2026-04-07 12:56:43 +0000

    STY: isort, ruff

--- src/porepy/numerics/nonlinear/anderson_acceleration.py
+++ src/porepy/numerics/nonlinear/anderson_acceleration.py
@@ -1,18 +1,20 @@
 import numpy as np
 from scipy.linalg import lstsq
 
+
 class AndersonAcceleration:
-    """Anderson acceleration Algorithm 4 as described by An and Jia and Walker in 
+    """Anderson acceleration Algorithm 4 as described by An and Jia and Walker in
     doi.org/10.1016/j.jcp.2017.06.031.
     """
 
-    def __init__(self, dimension, depth, 
-                 filtering: bool = True, drop_tol: float = 1e10) -> None:
+    def __init__(
+        self, dimension, depth, filtering: bool = True, drop_tol: float = 1e10
+    ) -> None:
         self._dimension = dimension
         self._depth = depth
         self._filtering = filtering
-        self._drop_tol = drop_tol # Tolerance for condition number of Fk
-        
+        self._drop_tol = drop_tol  # Tolerance for condition number of Fk
+
         # Initialize arrays for iterates.
         self.reset()
         self._fkm1: np.ndarray = self._Fk.copy()
@@ -25,7 +27,7 @@ class AndersonAcceleration:
         self._Gk: np.ndarray = np.zeros(
             (self._dimension, self._depth)
         )  # changes in fixed point applications
-        self._mk = 0 # Tracks current depth of the history
+        self._mk = 0  # Tracks current depth of the history
 
     def apply(self, gk: np.ndarray, fk: np.ndarray, iteration: int) -> np.ndarray:
         """Apply Anderson acceleration.
@@ -50,32 +52,33 @@ class AndersonAcceleration:
                 col = self._mk
                 self._mk += 1
             else:
-                self._Fk[:,:-1] = self._Fk[:, 1:]
-                self._Gk[:,:-1] = self._Gk[:, 1:]
-                col = self._depth-1
-            
+                self._Fk[:, :-1] = self._Fk[:, 1:]
+                self._Gk[:, :-1] = self._Gk[:, 1:]
+                col = self._depth - 1
+
             # Build matrices of changes.
             self._Fk[:, col] = fk - self._fkm1
             self._Gk[:, col] = gk - self._gkm1
-            
+
             # Drop the oldest columns if the condition number is too large.
             if self._filtering:
-                while (self._mk > 1 and 
-                np.linalg.cond(self._Fk[:, :self._mk]) > self._drop_tol):
+                while (
+                    self._mk > 1
+                    and np.linalg.cond(self._Fk[:, : self._mk]) > self._drop_tol
+                ):
                     # Drop the oldest column (index 0) and shift the rest left
-                    self._Fk[:, :self._mk - 1] = self._Fk[:, 1:self._mk]
-                    self._Gk[:, :self._mk - 1] = self._Gk[:, 1:self._mk]
+                    self._Fk[:, : self._mk - 1] = self._Fk[:, 1 : self._mk]
+                    self._Gk[:, : self._mk - 1] = self._Gk[:, 1 : self._mk]
                     self._mk -= 1
-                    
+
             # Solve least squares problem.
-            lstsq_solution = lstsq(self._Fk[:, 0:self._mk], fk)
+            lstsq_solution = lstsq(self._Fk[:, 0 : self._mk], fk)
             gamma_k = lstsq_solution[0]
             # Do the mixing
-            x_k_plus_1 = gk - np.dot(self._Gk[:, 0:self._mk], gamma_k)
+            x_k_plus_1 = gk - np.dot(self._Gk[:, 0 : self._mk], gamma_k)
 
         # Store values for next iteration.
         self._fkm1 = fk.copy()
         self._gkm1 = gk.copy()
 
         return x_k_plus_1
-
