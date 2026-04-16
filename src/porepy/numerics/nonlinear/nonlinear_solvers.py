e61f9d9966a04b3d5ccba65e705c9afbc2f25c7f
pschultzendorff <peter.schultzendorff@uib.no>
2026-04-16 12:26:54 +0000

    MAINT: isort

--- src/porepy/numerics/nonlinear/nonlinear_solvers.py
+++ src/porepy/numerics/nonlinear/nonlinear_solvers.py
@@ -19,10 +19,11 @@ from porepy.numerics.nonlinear.convergence_check import (
     DivergenceCriteria,
     SimulationStatus,
 )
-from porepy.utils.ui_and_logging import DummyProgressBar, progressbar_class
+from porepy.utils.ui_and_logging import DummyProgressBar
 from porepy.utils.ui_and_logging import (
     logging_redirect_tqdm_with_level as logging_redirect_tqdm,
 )
+from porepy.utils.ui_and_logging import progressbar_class
 
 # Module-wide logger
 logger = logging.getLogger(__name__)
