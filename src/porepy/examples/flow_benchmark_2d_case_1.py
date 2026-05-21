MOD: Correct units in flow benchmark cases

src/porepy/examples/flow_benchmark_2d_case_1.py
@@ -116,7 +116,7 @@ class BoundaryConditions:
         domain_sides = self.domain_boundary_sides(bg)
         values = np.zeros(bg.num_cells)
         # Inflow on the west boundary. Sign as per PorePy convention.
-        val = self.units.convert_units(-1, "m * s^-1")
+        val = self.units.convert_units(-1, "m * Pa")
         # Integrate over the boundary cell volumes.
         values[domain_sides.west] = val * bg.cell_volumes[domain_sides.west]
         # Scale with specific volume.