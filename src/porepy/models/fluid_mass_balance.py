5278876f7ce35da1bda1f025e4b5e2feb57561ff
Jakub Both <jakub.both@uib.no>
2026-05-28 14:06:15 +0200

    FEAT: Enable reference states for single phase flow

--- src/porepy/models/fluid_mass_balance.py
+++ src/porepy/models/fluid_mass_balance.py
@@ -543,15 +543,18 @@ class BoundaryConditionsSinglePhaseFlow(pp.BoundaryConditionMixin):
 class InitialConditionsSinglePhaseFlow(pp.InitialConditionMixin):
     """Mixin for providing initial values for pressure."""
 
-    pressure: Callable[[pp.SubdomainsOrBoundaries], pp.ad.Operator]
+    # TODO: OK to misuse the naming here and use ic_value_* for
+    # initial conditions of the primary variable which is relative?
+
+    relative_pressure: Callable[[pp.SubdomainsOrBoundaries], pp.ad.Operator]
     """See :class:`VariablesSinglePhaseFlow`."""
 
-    interface_darcy_flux: Callable[
+    relative_interface_darcy_flux: Callable[
         [list[pp.MortarGrid]], pp.ad.MixedDimensionalVariable
     ]
     """See :class:`VariablesSinglePhaseFlow`."""
 
-    well_flux: Callable[[list[pp.MortarGrid]], pp.ad.MixedDimensionalVariable]
+    relative_well_flux: Callable[[list[pp.MortarGrid]], pp.ad.MixedDimensionalVariable]
     """See :class:`VariablesSinglePhaseFlow`."""
 
     def initial_condition(self):
@@ -582,14 +585,14 @@ class InitialConditionsSinglePhaseFlow(pp.InitialConditionMixin):
             if intf.codim == 1:
                 self.equation_system.set_variable_values(
                     self.ic_values_interface_darcy_flux(intf),
-                    [cast(pp.ad.Variable, self.interface_darcy_flux([intf]))],
+                    [cast(pp.ad.Variable, self.relative_interface_darcy_flux([intf]))],
                     iterate_index=0,
                 )
 
             if intf.codim == 2:
                 self.equation_system.set_variable_values(
                     self.ic_values_well_flux(intf),
-                    [cast(pp.ad.Variable, self.well_flux([intf]))],
+                    [cast(pp.ad.Variable, self.relative_well_flux([intf]))],
                     iterate_index=0,
                 )
 
@@ -622,7 +625,7 @@ class InitialConditionsSinglePhaseFlow(pp.InitialConditionMixin):
             # operator.
             self.equation_system.set_variable_values(
                 self.ic_values_pressure(sd),
-                [cast(pp.ad.Variable, self.pressure([sd]))],
+                [cast(pp.ad.Variable, self.relative_pressure([sd]))],
                 iterate_index=0,
             )
 
@@ -714,6 +717,8 @@ class VariablesSinglePhaseFlow(pp.VariableMixin):
     :class:`~porepy.models.fluid_mass_balance.SolutionStrategySinglePhaseFlow`.
 
     """
+    has_single_phase_flow_reference_state: bool = False
+    """Flag whether reference states have been defined."""
 
     def create_variables(self) -> None:
         """Introduces the following variables into the system:
@@ -746,8 +751,61 @@ class VariablesSinglePhaseFlow(pp.VariableMixin):
             tags={"si_units": f"m^{self.nd} * Pa"},
         )
 
+    def reference_pressure(self, domains: pp.SubdomainsOrBoundaries) -> pp.ad.Operator:
+        """Reference pressure on subdomains."""
+        self.init_reference_single_phase_flow()
+        return pp.ad.TimeDependentDenseArray(
+            name="reference_" + self.pressure_variable,
+            domains=domains,
+        )
+
+    def reference_interface_darcy_flux(
+        self, interfaces: list[pp.MortarGrid]
+    ) -> pp.ad.Operator:
+        """Reference Darcy flux on codimension-1 interfaces."""
+        self.init_reference_single_phase_flow()
+        return pp.ad.TimeDependentDenseArray(
+            name="reference_" + self.interface_darcy_flux_variable,
+            domains=interfaces,
+        )
+
+    def reference_well_flux(self, interfaces: list[pp.MortarGrid]) -> pp.ad.Operator:
+        """Reference well flux on codimension-2 interfaces."""
+        self.init_reference_single_phase_flow()
+        return pp.ad.TimeDependentDenseArray(
+            name="reference_" + self.well_flux_variable,
+            domains=interfaces,
+        )
+
+    def relative_pressure(self, domains: pp.SubdomainsOrBoundaries) -> pp.ad.Operator:
+        """Pressure increment on subdomains."""
+        if len(domains) > 0 and isinstance(domains[0], pp.BoundaryGrid):
+            return self.create_boundary_operator(
+                name=self.pressure_variable,
+                domains=cast(Sequence[pp.BoundaryGrid], domains),
+            )
+        # Check that all domains are subdomains.
+        if not all(isinstance(g, pp.Grid) for g in domains):
+            raise ValueError("grids must consist entirely of subdomains.")
+        # Now we can cast the grids.
+        domains = cast(list[pp.Grid], domains)
+
+        return self.equation_system.md_variable(self.pressure_variable, domains)
+
+    def relative_interface_darcy_flux(
+        self, interfaces: list[pp.MortarGrid]
+    ) -> pp.ad.Operator:
+        """Interface Darcy flux increment on codimension-1 interfaces."""
+        return self.equation_system.md_variable(
+            self.interface_darcy_flux_variable, interfaces
+        )
+
+    def relative_well_flux(self, interfaces: list[pp.MortarGrid]) -> pp.ad.Operator:
+        """Well flux increment on codimension-2 interfaces."""
+        return self.equation_system.md_variable(self.well_flux_variable, interfaces)
+
     def pressure(self, domains: pp.SubdomainsOrBoundaries) -> pp.ad.Operator:
-        """Pressure term. Either a primary variable if subdomains are provided a
+        """Pressure term. Either a primary variable if subdomains are provided and
         boundary condition operator if boundary grids are provided.
 
         Parameters:
@@ -761,17 +819,13 @@ class VariablesSinglePhaseFlow(pp.VariableMixin):
 
         """
         if len(domains) > 0 and isinstance(domains[0], pp.BoundaryGrid):
-            return self.create_boundary_operator(
-                name=self.pressure_variable,
-                domains=cast(Sequence[pp.BoundaryGrid], domains),
-            )
+            return self.relative_pressure(domains)
         # Check that all domains are subdomains.
         if not all(isinstance(g, pp.Grid) for g in domains):
             raise ValueError("grids must consist entirely of subdomains.")
         # Now we can cast the grids
         domains = cast(list[pp.Grid], domains)
-
-        return self.equation_system.md_variable(self.pressure_variable, domains)
+        return self.reference_pressure(domains) + self.relative_pressure(domains)
 
     def interface_darcy_flux(
         self, interfaces: list[pp.MortarGrid]
@@ -787,10 +841,9 @@ class VariablesSinglePhaseFlow(pp.VariableMixin):
             Variable representing the interface Darcy flux [kg * m^2 * s^-2].
 
         """
-        flux = self.equation_system.md_variable(
-            self.interface_darcy_flux_variable, interfaces
-        )
-        return flux
+        return self.reference_interface_darcy_flux(
+            interfaces
+        ) + self.relative_interface_darcy_flux(interfaces)
 
     def well_flux(
         self, interfaces: list[pp.MortarGrid]
@@ -804,8 +857,127 @@ class VariablesSinglePhaseFlow(pp.VariableMixin):
             Variable representing the Darcy-like well flux [kg * m^2 * s^-2].
 
         """
-        flux = self.equation_system.md_variable(self.well_flux_variable, interfaces)
-        return flux
+        return self.reference_well_flux(interfaces) + self.relative_well_flux(
+            interfaces
+        )
+
+    def init_reference_single_phase_flow(self) -> None:
+        """Initialization of reference flow variables."""
+
+        # NOTE: The reference state is needed in the creation of the variables,
+        # but the initialization of the reference state needs to be done after the
+        # variables are created. This is a bit of a circular dependency, but it works
+        # in practice. The alternative would be to separate the creation of the
+        # variables and the initialization of the reference state into two separate
+        # steps, but that would be more cumbersome to use.
+
+        # Make sure to only initialize once.
+        if self.has_single_phase_flow_reference_state:
+            return
+        self.has_single_phase_flow_reference_state = True
+
+        # Set reference state for pressure on all subdomains
+        for sd, data in self.mdg.subdomains(return_data=True):
+            for index in [{"iterate_index": 0}, {"time_step_index": 0}]:
+                pp.set_solution_values(
+                    name="reference_" + self.pressure_variable,
+                    values=np.zeros(sd.num_cells),
+                    data=data,
+                    **index,
+                )
+
+        # Set reference state for interface Darcy flux on codim-1 interfaces
+        for intf, data in self.mdg.interfaces(return_data=True, codim=1):
+            for index in [{"iterate_index": 0}, {"time_step_index": 0}]:
+                pp.set_solution_values(
+                    name="reference_" + self.interface_darcy_flux_variable,
+                    values=np.zeros(intf.num_cells),
+                    data=data,
+                    **index,
+                )
+
+        # Set reference state for well flux on codim-2 interfaces
+        for intf, data in self.mdg.interfaces(return_data=True, codim=2):
+            for index in [{"iterate_index": 0}, {"time_step_index": 0}]:
+                pp.set_solution_values(
+                    name="reference_" + self.well_flux_variable,
+                    values=np.zeros(intf.num_cells),
+                    data=data,
+                    **index,
+                )
+
+        # Write logging info in yellow color
+        logger.info("\033[93m" + "Initialized reference state." + "\033[0m")
+
+    def update_reference(self) -> None:
+        """Updating of reference flow variables."""
+
+        # If super has an update_reference method, call it for compatibility with multi-physics.
+        if hasattr(super(), "update_reference"):
+            super().update_reference()
+
+        # Update reference state for pressure.
+        for sd, data in self.mdg.subdomains(return_data=True):
+            for index in [{"iterate_index": 0}, {"time_step_index": 0}]:
+                for name, values in [
+                    (
+                        "reference_" + self.pressure_variable,
+                        self.pressure([sd]).value(self.equation_system),
+                    ),
+                    (
+                        self.pressure_variable,
+                        np.zeros(sd.num_cells),
+                    ),
+                ]:
+                    pp.set_solution_values(
+                        name=name,
+                        values=values,
+                        data=data,
+                        **index,
+                    )
+
+        # Update reference state for interface Darcy flux.
+        for intf, data in self.mdg.interfaces(return_data=True, codim=1):
+            for index in [{"iterate_index": 0}, {"time_step_index": 0}]:
+                for name, values in [
+                    (
+                        "reference_" + self.interface_darcy_flux_variable,
+                        self.interface_darcy_flux([intf]).value(self.equation_system),
+                    ),
+                    (
+                        self.interface_darcy_flux_variable,
+                        np.zeros(intf.num_cells),
+                    ),
+                ]:
+                    pp.set_solution_values(
+                        name=name,
+                        values=values,
+                        data=data,
+                        **index,
+                    )
+
+        # Update reference state for well flux.
+        for intf, data in self.mdg.interfaces(return_data=True, codim=2):
+            for index in [{"iterate_index": 0}, {"time_step_index": 0}]:
+                for name, values in [
+                    (
+                        "reference_" + self.well_flux_variable,
+                        self.well_flux([intf]).value(self.equation_system),
+                    ),
+                    (
+                        self.well_flux_variable,
+                        np.zeros(intf.num_cells),
+                    ),
+                ]:
+                    pp.set_solution_values(
+                        name=name,
+                        values=values,
+                        data=data,
+                        **index,
+                    )
+
+        # Write logging info in yellow color.
+        logger.info("\033[93m" + "Updated reference state." + "\033[0m")
 
 
 class SolutionStrategySinglePhaseFlow(pp.SolutionStrategy):
@@ -1058,6 +1230,91 @@ class SolutionStrategySinglePhaseFlow(pp.SolutionStrategy):
         """
 
 
+class DataSavingFluidMassBalance:
+    """Auxiliary class for exporting absolute and relative values."""
+
+    def data_to_export(self):
+        """Add reference state variants to export data."""
+        data = super().data_to_export()
+
+        # Add variants of pressure
+        for sd in self.mdg.subdomains(return_data=False):
+            # Reference pressure
+            pressure_ref = self.evaluate_and_scale([sd], "reference_pressure", "Pa")
+            data.append((sd, self.pressure_variable + "_ref", pressure_ref))
+
+            # Delta pressure
+            pressure_inc = self.evaluate_and_scale([sd], "relative_pressure", "Pa")
+            data.append((sd, self.pressure_variable + "_inc", pressure_inc))
+
+            # Full pressure
+            # Find and remove the original (sd, self.pressure_variable) entry
+            data = [
+                entry
+                for entry in data
+                if not (entry[0] == sd and entry[1] == self.pressure_variable)
+            ]
+            # Append the new entry with updated pressure
+            pressure = self.evaluate_and_scale([sd], "pressure", "Pa")
+            data.append((sd, self.pressure_variable, pressure))
+
+        # Add variants of interface Darcy flux (codim-1)
+        for intf in self.mdg.interfaces(return_data=False, codim=1):
+            # Reference interface Darcy flux
+            flux_ref = self.evaluate_and_scale(
+                [intf], "reference_interface_darcy_flux", "kg * m^-2 * s^-1"
+            )
+            data.append((intf, self.interface_darcy_flux_variable + "_ref", flux_ref))
+
+            # Delta interface Darcy flux
+            flux_inc = self.evaluate_and_scale(
+                [intf], "relative_interface_darcy_flux", "kg * m^-2 * s^-1"
+            )
+            data.append((intf, self.interface_darcy_flux_variable + "_inc", flux_inc))
+
+            # Full interface Darcy flux
+            # Find and remove the original (intf, self.interface_darcy_flux_variable) entry
+            data = [
+                entry
+                for entry in data
+                if not (
+                    entry[0] == intf and entry[1] == self.interface_darcy_flux_variable
+                )
+            ]
+            # Append the new entry with updated flux
+            flux = self.evaluate_and_scale(
+                [intf], "interface_darcy_flux", "kg * m^-2 * s^-1"
+            )
+            data.append((intf, self.interface_darcy_flux_variable, flux))
+
+        # Add variants of well flux (codim-2)
+        for intf in self.mdg.interfaces(return_data=False, codim=2):
+            # Reference well flux
+            well_ref = self.evaluate_and_scale(
+                [intf], "reference_well_flux", "kg * m^-2 * s^-1"
+            )
+            data.append((intf, self.well_flux_variable + "_ref", well_ref))
+
+            # Delta well flux
+            well_inc = self.evaluate_and_scale(
+                [intf], "relative_well_flux", "kg * m^-2 * s^-1"
+            )
+            data.append((intf, self.well_flux_variable + "_inc", well_inc))
+
+            # Full well flux
+            # Find and remove the original (intf, self.well_flux_variable) entry
+            data = [
+                entry
+                for entry in data
+                if not (entry[0] == intf and entry[1] == self.well_flux_variable)
+            ]
+            # Append the new entry with updated well flux
+            well = self.evaluate_and_scale([intf], "well_flux", "kg * m^-2 * s^-1")
+            data.append((intf, self.well_flux_variable, well))
+
+        return data
+
+
 # Note that we ignore a mypy error here. There are some inconsistencies in the method
 # definitions of the mixins, related to the enforcement of keyword-only arguments. The
 # type Callable is poorly supported, except if protocols are used and we really do not
@@ -1073,6 +1330,7 @@ class SinglePhaseFlow(  # type: ignore[misc]
     BoundaryConditionsSinglePhaseFlow,
     InitialConditionsSinglePhaseFlow,
     SolutionStrategySinglePhaseFlow,
+    DataSavingFluidMassBalance,
     pp.FluidMixin,
     pp.ModelGeometry,
     pp.DataSavingMixin,
