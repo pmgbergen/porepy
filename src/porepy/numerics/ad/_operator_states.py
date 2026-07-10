5a669baf8f8156623ccdbe5ef76ead01eaaae719
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2026-07-10 12:16:59 +0000

    MAINT: Operator mixins source and target are not optional

--- src/porepy/numerics/ad/_operator_states.py
+++ src/porepy/numerics/ad/_operator_states.py
@@ -64,8 +64,8 @@ class TimeDependentOperator:
         operation: Optional[Operations] = None,
         children: Optional[Sequence[Operator]] = None,
         *,
-        source: Optional[OperatorSpace],
-        target: Optional[OperatorSpace],
+        source: OperatorSpace,
+        target: OperatorSpace,
     ) -> None:
         super().__init__(  # type: ignore[call-arg]
             name=name,
@@ -191,8 +191,9 @@ class IterativeOperator:
         name: str | None = None,
         operation: Optional[Operations] = None,
         children: Optional[Sequence[Operator]] = None,
-        source: Optional[OperatorSpace] = None,
-        target: Optional[OperatorSpace] = None,
+        *,
+        source: OperatorSpace,
+        target: OperatorSpace,
     ) -> None:
         super().__init__(  # type: ignore[call-arg]
             name=name,
@@ -309,8 +310,9 @@ class ReferenceOperator:
         name: str | None = None,
         operation: Optional[Operations] = None,
         children: Optional[Sequence[Operator]] = None,
-        source: Optional[OperatorSpace] = None,
-        target: Optional[OperatorSpace] = None,
+        *,
+        source: OperatorSpace,
+        target: OperatorSpace,
     ) -> None:
         super().__init__(  # type: ignore[call-arg]
             name=name,
