9caf47c2cd46c1a3fac4859af4aab157c60bb4d8
Yury Zabegaev <zabegaev99@gmail.com>
2026-07-20 16:25:58 +0000

    DOC: Resolved todos

--- src/porepy/numerics/ad/indexers.py
+++ src/porepy/numerics/ad/indexers.py
@@ -1,3 +1,10 @@
+"""Indexers for systems of discretized systems of equations and variables, the values of
+which are arranged in a contiguous array.
+
+Used by `EquationSystem` and nonlinear solvers.
+
+"""
+
 from __future__ import annotations
 from dataclasses import dataclass
 
@@ -39,25 +46,18 @@ class VariableIndexer:
         """
         self.num_dofs: int = sum(x.size for x in self.variable_dofs.values())
 
-    def projection_to(self, variables: list[pp.ad.Variable]) -> np.ndarray:
-        """TODO YZ
-        Create a projection matrix from the global vector of unknowns to a specified
-        subspace.
+    def projection_indices(self, variables: list[pp.ad.Variable]) -> np.ndarray:
+        """Create a projection index array from the vector, system vector, represented
+        by this indexer, to the requested subspace.
 
-        The transpose of the returned matrix can be used to slice respective columns out
-        of the global Jacobian.
-
-        The projection preserves the global order defined by the system, i.e. it
-        includes no permutation.
+        The projection preserves the order defined by the this indexer and neglects the
+        order of the input data. In other words, it includes no permutation.
 
         Parameters:
-            variables (optional): VariableType input for which the subspace is
-                requested. If no subspace is specified using ``variables``,
-                a null-space projection is returned.
+            variables (optional): Input for which the subspace is requested.
 
         Returns:
-            a sparse projection matrix of shape ``(M, num_dofs)``, where
-            ``0 <= M <= num_dofs``.
+            an index array of `shape=(M,)`, where `0 <= M <= num_dofs`.
 
         """
         variables_lookup = set(variables)
@@ -73,13 +73,22 @@ class VariableIndexer:
         )
 
     def construct_restricted_indexer(self, variables: list[pp.ad.Variable]):
-        """TODO YZ"""
+        """Constructs a new indexer based on requested subset of variables.
+
+        The order of the new indexer is defined by the input.
+
+        Raises:
+            ValueError: If the requested variable is not known to this indexer.
+
+        """
         new_variable_dofs: dict[pp.ad.Variable, np.ndarray] = {}
         offset = 0
         for variable in variables:
             dofs = self.variable_dofs.get(variable, None)
             if dofs is None:
-                raise ValueError(...)
+                raise ValueError(
+                    f"Requested variable is not known to this indexer: {variable}"
+                )
             new_variable_dofs[variable] = np.arange(dofs.size) + offset
             offset += dofs.size
         return VariableIndexer(variable_dofs=new_variable_dofs)
@@ -92,14 +101,22 @@ class EquationIndexer:
     For a data array with a different arrangement (e.g., produced by taking a subset of
     equations), a new indexer needs to be constructed.
 
+    Implementation note: There is an unfortunate assymetry between this and
+    :class:`VariableIndexer` in the way these classes are initialized and what fields
+    they expose. It is done so for convenient operations in `EquationSystem`. The
+    preferred way to interact with this class outside `EquationSystem` is by using
+    :class:`EquationOnDomain` to query DoFs and not
+    `dict[str, dict[pp.GridLike, np.ndarray]]` See also the docstring of
+    :attr:`equation_image_space_composition`.
+
     """
 
     def __init__(
-        self, equation_dofs_per_equation: dict[str, dict[pp.GridLike, np.ndarray]]
+        self, equation_image_composition: dict[str, dict[pp.GridLike, np.ndarray]]
     ) -> None:
         equation_dofs: dict[EquationOnDomain, np.ndarray] = {}
         global_offset = 0
-        for eq_name, dofs_on_domains in equation_dofs_per_equation.items():
+        for eq_name, dofs_on_domains in equation_image_composition.items():
             offset_per_equation = 0
             for domain, dofs in dofs_on_domains.items():
                 equation_dofs[EquationOnDomain(name=eq_name, domain=domain)] = (
@@ -114,6 +131,14 @@ class EquationIndexer:
         before DoFs of key B.
 
         """
-        self.equation_dofs_per_equation: dict[str, dict[pp.GridLike, np.ndarray]] = (
-            equation_dofs_per_equation
-        )
+        self.equation_image_space_composition: dict[
+            str, dict[pp.GridLike, np.ndarray]
+        ] = equation_image_composition
+        """A mapping `equation_name` -> `domains` -> `dofs`.
+        
+        The dofs stored here start from 0 for each equation, i.e. the offset is only
+        relative to domains. The DoFs with the "global" offset can be found in
+        :attr:`equation_dofs`. This is done to respect the implementation of
+        `EquationSystem`, where both types of offsets are needed. 
+
+        """
