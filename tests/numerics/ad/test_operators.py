181649a2c5c61da51d25348a04583d729c18b064
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2026-08-04 09:20:32 +0000

    TST: Move tests for Ad operations to separate module

--- tests/numerics/ad/test_operators.py
+++ tests/numerics/ad/test_operators.py
@@ -33,40 +33,6 @@ from porepy.applications.md_grids.model_geometries import (
 from porepy.models.fluid_mass_balance import SinglePhaseFlow
 
 AdType = Union[float, np.ndarray, sps.spmatrix, pp.ad.AdArray]
-_operations = pp.ad.operators.Operations
-
-operators = [
-    ("+", _operations.add),
-    ("-", _operations.sub),
-    ("*", _operations.mul),
-    ("/", _operations.div),
-    ("**", _operations.pow),
-]
-
-
-@pytest.mark.parametrize("operator", operators)
-def test_elementary_operations(operator):
-    """Test that performing elementary arithmetic operations on operators return
-    operator trees with the expected structure.
-
-    The test does not consider evaluation of the numerical values of the operators.
-    """
-    # Generate two generic operators
-    space = pp.ad.OperatorSpace.scalar()
-    a = pp.ad.Operator(source=space, target=space)
-    b = pp.ad.Operator(source=space, target=space)
-
-    # Combine the operators with the provided operation.
-    c = eval(f"a {operator[0]} b")
-
-    # Check that the combined operator has the expected structure.
-    assert c.operation == operator[1]
-
-    # Need to check the id of the objects since the equality of pp.ad.Operator (or
-    # rather the _key method which is called by eq) does not allow for generic void
-    # operators like a and b.
-    assert id(c.children[0]) == id(a)
-    assert id(c.children[1]) == id(b)
 
 
 class TestCopyOperatorTree:
