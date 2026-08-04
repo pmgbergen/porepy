345ae06f8bed9e200788c3ddd114dbf29aa20495
Eirik Keilegavlen <Eirik.Keilegavlen@uib.no>
2026-08-04 13:00:56 +0000

    MAINT: Deleted unused code in Ad Operator parsing
    Unwrapped numpy arrays and scipy matrices should not be combined with Ad operators

--- src/porepy/numerics/ad/operators.py
+++ src/porepy/numerics/ad/operators.py
@@ -869,28 +869,7 @@ class Operator:
     def _parse_other(self, other):
         if isinstance(other, float) or isinstance(other, int):
             return [self, Scalar(other)]
-        elif isinstance(other, np.ndarray):
-            # TODO EK: Can this be removed?
-            # A raw ndarray operand carries no grid/domain context whatsoever;
-            # this is a genuine domain ambiguity, not merely an unspecified one.
-            return [
-                self,
-                DenseArray(
-                    other,
-                    source=OperatorSpace.unclear(),
-                    target=OperatorSpace.unclear(),
-                ),
-            ]
-        elif isinstance(other, (sps.spmatrix, sps.sparray)):
-            # TODO EK: Can this be removed?
-            return [
-                self,
-                SparseArray(
-                    other,
-                    source=OperatorSpace.unclear(),
-                    target=OperatorSpace.unclear(),
-                ),
-            ]
+
         elif isinstance(other, AdArray):
             # This may happen when using nested pp.ad.Function.
             return [self, other]
