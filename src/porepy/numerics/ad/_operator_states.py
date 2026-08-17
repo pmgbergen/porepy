ab04c0c6c2c2e3570b410851bdd1ddfffcc8f9ed
pschultzendorff <peter.schultzendorff@uib.no>
2026-08-17 13:52:01 +0200

    DOC: Add an example use case to previous_iteration

--- src/porepy/numerics/ad/_operator_states.py
+++ src/porepy/numerics/ad/_operator_states.py
@@ -247,13 +247,21 @@ class IterativeOperator:
         """Returns a copy of the iterative operator with an advanced iterate index.
 
         Note:
-            Calling this method on an operator, with steps=1, on an operator
+            Calling this method on an operator, with ``steps=1``, on an operator
             representing the current iterate (the next iterate to be computed in an
             iterative scheme) will return an operator representing the last accepted
             iterate. When evaluated, the current and last accepted iterate will return
             the same values, but only the former will have a non-zero Jacobian. To get
             the difference between the current and second to last accepted iterate, call
-            this method with steps=2.
+            this method with ``steps=2``.
+
+        Example:
+            To obtain the Newton update of an operator
+            ```
+            op = ...
+            newton_update = op - op.previous_iteration(steps=2)
+            newton_update_value = newton_update.value(equation_system)
+            ```
 
         Note:
             You cannot create operators at the previous iterates from operators which
